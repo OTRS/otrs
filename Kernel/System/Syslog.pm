2e91f79b6bfd83db07033405bcff09b4701ea3b4
Martin Edenhofer <martin.edenhofer@otrs.com>
2001-12-02 18:28:30 +0000

    added format of logfile output

--- Kernel/System/Syslog.pm
+++ Kernel/System/Syslog.pm
@@ -2,7 +2,7 @@
 # Syslog.pm - a wrapper for xyz::Syslog 
 # Copyright (C) 2001 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Syslog.pm,v 1.1 2001-12-02 14:36:43 martin Exp $
+# $Id: Syslog.pm,v 1.2 2001-12-02 18:28:30 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -16,7 +16,7 @@ use Unix::Syslog qw(:macros);  # Syslog macros
 use Unix::Syslog qw(:subs);  # Syslog functions
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.1 $ ';
+$VERSION = '$Revision: 1.2 $ ';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/g;
 
 # --
@@ -40,19 +40,24 @@ sub Log {
     my $MSG = $Param{MSG} || '???';
 
     if ($Priority =~ /debug/i) {
-        syslog(LOG_DEBUG, $MSG);
+        my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(0);
+        syslog(LOG_DEBUG, "[Debug][$Package][$Line] $MSG");
     }
     elsif ($Priority =~ /info/i) {
-        syslog(LOG_INFO, $MSG);
+        my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(1);
+        syslog(LOG_INFO, "[Info][$Package] $MSG");
     }
     elsif ($Priority =~ /notice/i) {
-        syslog(LOG_NOTICE, $MSG);
+        my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(0);
+        syslog(LOG_NOTICE, "[Notice][$Package] $MSG");
     }
     elsif ($Priority =~ /error/i) {
-        syslog(LOG_ERR, $MSG);
+        my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(1);
+        syslog(LOG_ERR, "[Error][$Package][Line:$Line]: $MSG");
     }
     else {
-        syslog(LOG_ERR, "Priority: '$Param{Priority}' not defined! MSG: $MSG");
+        my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(1);
+        syslog(LOG_ERR, "[Error][$Package] Priority: '$Param{Priority}' not defined! MSG: $MSG");
     }
 
     return;
