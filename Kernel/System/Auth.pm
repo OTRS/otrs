c0c8ffc5218c453b5a15d6a5583982ffa9dc6232
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-04-13 11:09:18 +0000

    moved GetUserData to Kernel::Syste,::User.pm

--- Kernel/System/Auth.pm
+++ Kernel/System/Auth.pm
@@ -1,8 +1,8 @@
 # --
 # Auth.pm - provides the authentification and user data
-# Copyright (C) 2001,2002 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Auth.pm,v 1.5 2002-04-12 16:33:51 martin Exp $
+# $Id: Auth.pm,v 1.6 2002-04-13 11:09:18 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -14,7 +14,7 @@ package Kernel::System::Auth;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.5 $';
+$VERSION = '$Revision: 1.6 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -43,16 +43,6 @@ sub new {
       || 'pw';
     $Self->{UserTableUser} = $Self->{ConfigObject}->Get('DatabaseUserTableUser')
 	  || 'login';
-
-    # preferences table data
-    $Self->{PreferencesTable} = $Self->{ConfigObject}->Get('DatabasePreferencesTable') 
-      || 'user_preferences';
-    $Self->{PreferencesTableKey} = $Self->{ConfigObject}->Get('DatabasePreferencesTableKey')
-	  || 'preferences_key';
-    $Self->{PreferencesTableValue} = $Self->{ConfigObject}->Get('DatabasePreferencesTableValue')
-	  || 'preferences_value';
-    $Self->{PreferencesTableUserID} = $Self->{ConfigObject}->Get('DatabasePreferencesTableUserID') 
-	  || 'user_id';
 
     return $Self;
 }
@@ -115,60 +105,6 @@ sub Auth {
         ); 
         return;
     }
-}
-# --
-sub GetUserData {
-    my $Self = shift;
-    my %Param = @_;
-    my $User = $Param{User};
-    my %Data;
-
-    # --
-    # get inital data
-    # --
-    my $SQL = "SELECT su.$Self->{UserTableUserID}, su.salutation, su.first_name, su.last_name ".
-        " FROM " .
-        " $Self->{UserTable} as su " .
-        " WHERE " .
-        " su.$Self->{UserTableUser} = '$User'";
-
-    $Self->{DBObject}->Prepare(SQL => $SQL);
-    while (my @RowTmp = $Self->{DBObject}->FetchrowArray()) {
-        $Data{UserID} = $RowTmp[0];
-        $Data{UserLogin} = $User;
-        $Data{UserFirstname} = $RowTmp[2];
-        $Data{UserLastname} = $RowTmp[3];
-    }
-
-    # --
-    # check data
-    # --
-    if (! exists $Data{UserID}) {
-        $Self->{LogObject}->Log(
-          Priority => 'notice',
-          MSG => "Panic! No UserData for user: '$User'!!!",
-        );
-        return;
-    }
-
-    # --
-    # get preferences
-    # --
-    $SQL = "SELECT $Self->{PreferencesTableKey}, $Self->{PreferencesTableValue} " .
-        " FROM " .
-        " $Self->{PreferencesTable} ".
-        " WHERE " .
-        " $Self->{PreferencesTableUserID} = $Data{UserID}";
-
-    $Self->{DBObject}->Prepare(SQL => $SQL);
-    while (my @RowTmp = $Self->{DBObject}->FetchrowArray()) {
-        $Data{$RowTmp[0]} = $RowTmp[1];
-    }
-
-    # --
-    # return data
-    # --
-    return %Data;
 }
 # --
 
