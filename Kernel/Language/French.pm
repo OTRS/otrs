961561f90d5db019ac51a5c7f7551eeeab6ee90a
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-05-09 23:44:32 +0000

    new

---
+++ Kernel/Language/French.pm
@@ -0,0 +1,208 @@
+# --
+# French.pm - provides french language translations
+# Copyright (C) 2002 Martin Scherbaum <maddin@exsuse.de>
+# --
+# $Id: French.pm,v 1.1 2002-05-09 23:42:01 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+package Kernel::Language::French;
+
+use strict;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
+
+# --
+sub Data {
+    my $Self = shift;
+    my %Param = @_;
+
+    # some common words
+    $Self->{Lock} = 'Tirer';
+    $Self->{Unlock} = 'Rendre';
+    $Self->{unlock} = 'rendre';
+    $Self->{Zoom} = 'Contenu';
+    $Self->{History} = 'Histoire';
+    $Self->{'Add Note'} = 'Ajouter commentaire';
+    $Self->{Bounce} = 'Bounce';
+    $Self->{Age} = 'Age';
+    $Self->{Priority} = 'Priorité';
+    $Self->{State} = 'Status';
+    $Self->{From} = 'De';
+    $Self->{To} = 'Pour';
+    $Self->{Cc} = 'Cc';
+    $Self->{Subject} = 'Sujet';
+    $Self->{Move} = 'Mouvoir';
+    $Self->{Queues} = 'Queues';
+    $Self->{Close} = 'Fermer';
+    $Self->{Compose} = 'Creer';
+    $Self->{Pending} = 'Attendre';
+    $Self->{end} = 'Fin';
+    $Self->{top} = 'Début';
+    $Self->{day} = 'jour';
+    $Self->{days} = 'jours';
+    $Self->{hour} = 'heure';
+    $Self->{hours} = 'heures';
+    $Self->{minute} = 'minute';
+    $Self->{minutes} = 'minutes';
+    $Self->{Owner} = 'Propriétaire';
+    $Self->{Sender} = 'Envoyeur';
+    $Self->{Article} = 'Article';
+    $Self->{Ticket} = 'Ticket';
+    $Self->{Createtime} = 'Créé le';
+    $Self->{Created} = 'Créé';
+    $Self->{View} = 'Vue';
+    $Self->{Action} = 'Action';
+    $Self->{User} = 'Utilisateur';
+    $Self->{Back} = 'retourner';
+    $Self->{store} = 'sauvegarder';
+    $Self->{currently} = 'courrant';
+    $Self->{Customer} = 'Client';
+    $Self->{'Customer info'} = 'Info client';
+    $Self->{'Set customer id of a ticket'} = 'Assigner ID client pour un ticket';
+    $Self->{'All tickets of this customer'} = 'Tous tickets de ce client';
+    $Self->{'New CustomerID'} = 'ID client nouveau';
+    $Self->{'for ticket'} = 'pour ticket';
+    $Self->{'Start work'} = 'Début de travail';
+    $Self->{'Stop work'} = 'Fin de travail';
+    $Self->{'CustomerID'} = 'ID client';
+    $Self->{'Compose Answer'} = 'Composer une réponse';
+    $Self->{'Change queue'} = 'Changer de queue';
+    $Self->{'go!'} = 'allez!';
+    $Self->{'update!'} = 'actualiser!';
+    $Self->{'submit!'} = 'envoyer!';
+    $Self->{'change!'} = 'changer!';
+    $Self->{'change'} = 'changer';
+    $Self->{'Comment'} = 'Commentaire';
+    $Self->{'Valid'} = 'Valid';
+    $Self->{'Forward'} = 'Rediriger';
+    $Self->{'Name'} = 'Nom';
+    $Self->{'Group'} = 'Equipe';
+    $Self->{'Response'} = 'Réponse';
+    $Self->{'none!'} = 'aucun!';
+    $Self->{'German'} = 'Allemand';
+    $Self->{'English'} = 'Anglais';
+    $Self->{'French'} = 'Français';
+    $Self->{'Lite'} = 'Simple';
+    # admin area
+    $Self->{'Firstname'} = 'Prénom';
+    $Self->{'Lastname'} = 'Nom';
+    $Self->{'Add User'} = 'Ajouter utilisateur';
+    $Self->{'Languages'} = 'Langues';
+    $Self->{'Language'} = 'Langue';
+    $Self->{'Salutation'} = 'Salutation';
+    $Self->{'Signature'} = 'Signature';
+    $Self->{'Standart Responses'} = 'Réponse standard';
+    $Self->{'System Addresses'} = 'Adresses du système';
+    $Self->{'Admin Area'} = 'Administration';
+    $Self->{'Preferences'} = 'Préférences';
+    $Self->{'top'} = 'haut';
+    $Self->{'AgentFrontend'} = 'SurfaceAgent';
+    $Self->{'Groups'} = 'Equipes';
+    $Self->{'User'} = 'Utilisateur';
+    $Self->{'User <-> Groups'} = 'Utilisateur <-> Equipes';
+    $Self->{'Std. Responses <-> Queue'} = 'Réponses standards <-> Queue';
+    $Self->{'Add Group'} = 'Ajouter équipe';
+    $Self->{'Change Group settings'} = 'Modifier équipe';
+    $Self->{'Add Queue'} = 'Ajouter queue';
+    $Self->{'Systemaddress'} = 'Adresse du système';
+    $Self->{'Change Queue settings'} = 'Modifier une queue';
+    $Self->{'Add Response'} = 'Ajouter une réponse';
+    $Self->{'Change Response settings'} = 'Modifier une réponse';
+    $Self->{'Add Salutation'} = 'Ajouter une adresse';
+    $Self->{'Change Salutation settings'} = 'Modifier une adresse';
+    # nav bar
+    $Self->{Logout} = 'Quitter';
+    $Self->{QueueView} = 'Vue queue';
+    $Self->{PhoneView} = 'Vue téléphone';
+    $Self->{Utilities} = 'Utilitaire';
+    $Self->{AdminArea} = 'Administration';
+    $Self->{Preferences} = 'Préférences';
+    $Self->{'Locked tickets'} = 'Mes tickets';
+    $Self->{'new message'} = 'Message nouveau';
+    # ticket history
+    $Self->{'History of Ticket'} = 'Histoire de ticket';
+    # ticket note
+    $Self->{'Add note to ticket'} = 'Ajouter un commentaire au ticket';
+    $Self->{'Note type'} = 'type de commentaire';
+    # queue view
+    $Self->{'Tickets shown'} = 'Tickets montrés';
+    $Self->{'Ticket available'} = 'Ticket disponibles';
+    $Self->{'Show all'} = 'Tous montrés';
+    $Self->{'tickets'} = 'Tickets';
+    $Self->{'All tickets'} = 'Tous tickets';
+    # locked tickets
+    $Self->{'All locked Tickets'} = 'Mes tickets';
+    $Self->{'New message'} = 'Message nouveau';
+    $Self->{'New message!'} = 'Message nouveau!';
+    # util
+    $Self->{'Hit'} = 'Resultat';
+    $Self->{'Total hits'} = 'Resultat totale';
+    $Self->{'Search again'} = 'Recherche';
+    $Self->{'max viewable hits'} = 'max. resultats visibles';
+    $Self->{'Utilities/Search'} = 'Utilitaires/recherche';
+    $Self->{'Ticket# search (e. g. 10*5155 or 105658*)'} = 'Ticket# Suche (z. B. 10*5155 or 105658*)';
+    $Self->{'Fulltext search (e. g. "Mar*in" or "Baue*" or "martin+hallo")'} = 'Volltextsuche (z. B. "Mar*in" ode "Baue*" oder "martin+hallo")';
+    # compose
+    $Self->{'Compose message'} = 'Composer un message';
+    $Self->{'please do not edit!'} = 'Ne pas modifier!';
+    $Self->{'Send mail!'} = 'Envoyer message!';
+    $Self->{'wrote'} = 'écrit';
+    $Self->{'Compose answer for ticket'} = 'Composer réponse pour';
+    $Self->{'Ticket locked!'} = 'Ticket bloqué!';
+    # forward
+    $Self->{'Forward article of ticket'} = 'Acheminer l\'article du ticket';
+    $Self->{'Article type'} = 'Type d\'article';
+    $Self->{'Next ticket state'} = 'Status prochain du ticket';
+
+    # preferences
+    $Self->{'User Preferences'} = 'Préférences utilisateur';
+    $Self->{'Change Password'} = 'Changer mot de pass';
+    $Self->{'New password'} = 'Mot de pass nouveau';
+    $Self->{'New password again'} = 'Répéter nouveau mot de pass';
+    $Self->{'Select your custom queues'} = 'Selection de queues préférées';
+    $Self->{'Select your frontend language'} = 'Selection de la langue du surface';
+    $Self->{'Select your frontend Charset'} = 'Selection de police de charactère';
+    $Self->{'Select your frontend theme'} = 'Selection du schema de visualisation';
+    $Self->{'Frontend language'} = 'Selection de la langue du surface';
+    # change priority
+    $Self->{'Change priority of ticket'} = 'Changer la priorité de ticket';
+    # some other words ...
+    $Self->{AddLink} = 'Ajouter lien';
+#    $Self->{} = '';
+#    $Self->{} = '';
+#    $Self->{} = '';
+
+    # states
+    $Self->{'new'} = 'nouveau';
+    $Self->{'open'} = 'ouvert';
+    $Self->{'closed succsessful'} = 'fermé avec succès';
+    $Self->{'closed unsuccsessful'} = 'fermé avec succès';
+    $Self->{'removed'} = 'effacé';
+    # article types
+    $Self->{'email-external'} = 'Message à externe';
+    $Self->{'email-internal'} = 'Message à interne';
+    $Self->{'note-internal'} = 'Commentaire pour interne';
+    $Self->{'note-external'} = 'Commentaire pour externe';
+    $Self->{'note-report'} = 'Commentaire pour reports';
+
+#    $Self->{''} = '';
+#    $Self->{''} = '';
+
+    # priority
+    $Self->{'very low'} = 'très bas';
+    $Self->{'low'} = 'bas';
+    $Self->{'normal'} = 'normale';
+    $Self->{'high'} = 'haut';
+    $Self->{'very high'} = 'très haut';
+
+    return;
+}
+# --
+
+1;
+
---
+++ Kernel/Modules/AgentPhone.pm
@@ -0,0 +1,288 @@
+# --
+# AgentPhone.pm - to handle phone calls
+# Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
+# --
+# $Id: AgentPhone.pm,v 1.1 2002-05-09 23:44:17 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::Modules::AgentPhone;
+
+use strict;
+use Kernel::System::Article;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
+
+# --
+sub new {
+    my $Type = shift;
+    my %Param = @_;
+
+    # allocate new hash for object    
+    my $Self = {}; 
+    bless ($Self, $Type);
+    
+    foreach (keys %Param) {
+        $Self->{$_} = $Param{$_};
+    }
+
+    # check needed Opjects
+    foreach (
+       'ParamObject', 
+       'DBObject', 
+       'TicketObject', 
+       'LayoutObject', 
+       'LogObject', 
+       'QueueObject', 
+       'ConfigObject',
+    ) {
+        die "Got no $_!" if (!$Self->{$_});
+    }
+
+    return $Self;
+}
+# --
+sub Run {
+    my $Self = shift;
+    my %Param = @_;
+    my $Output;
+    my $TicketID = $Self->{TicketID};
+    my $QueueID = $Self->{QueueID};
+    my $Subaction = $Self->{Subaction};
+    my $NextScreen = $Self->{NextScreen} || 'AgentZoom';
+    my $BackScreen = $Self->{BackScreen};
+    my $UserID = $Self->{UserID};
+    my $UserLogin = $Self->{UserLogin};
+    
+    my $Tn = $Self->{TicketObject}->GetTNOfId(ID => $TicketID);
+    
+    if ($Subaction eq '' || !$Subaction) {
+        # --
+        # header
+        # --
+        $Output .= $Self->{LayoutObject}->Header(Title => 'Phone call');
+
+        # get possible notes
+        my %NoteTypes = $Self->{DBObject}->GetTableData(
+            Table => 'article_type',
+            Valid => 1,
+            What => 'id, name'
+        );
+        foreach (keys %NoteTypes) {
+            if ($NoteTypes{$_} !~ /^note/i) {
+                delete $NoteTypes{$_};
+            }
+        }
+        # get next states
+        my %NextStates;
+        my $NextComposeTypePossibleTmp =
+           $Self->{ConfigObject}->Get('DefaultPhoneNextStatePossible')
+             || die 'No Config entry "DefaultPhoneNextStatePossible"!';
+        my @NextComposeTypePossible = @$NextComposeTypePossibleTmp;
+        foreach (@NextComposeTypePossible) {
+            $NextStates{$Self->{TicketObject}->StateLookup(State => $_)} = $_;
+        }
+
+        # --
+        # if there is no ticket id!
+        # --
+        if (!$TicketID) {
+            my %LockedData = $Self->{UserObject}->GetLockedCount(UserID => $UserID);
+            $Output .= $Self->{LayoutObject}->NavigationBar(LockData => \%LockedData);
+    
+            my %Tos = $Self->{DBObject}->GetTableData(
+                Table => 'system_address',
+                What => 'queue_id, value1, value0',
+                Valid => 1,
+                Clamp => 1,
+            );
+
+            $Output .= $Self->{LayoutObject}->AgentPhoneNew(
+              QueueID => $QueueID,
+              BackScreen => $BackScreen,
+              NextScreen => $NextScreen,
+              NoteTypes => \%NoteTypes,
+              NextStates => \%NextStates,
+              To => \%Tos,
+           );
+            $Output .= $Self->{LayoutObject}->Footer();
+            return $Output;
+        }
+        # get lock state && permissions
+        my $LockState = $Self->{TicketObject}->GetLockState(TicketID => $TicketID) || 0;
+        if (!$LockState) {
+          # set owner
+          $Self->{TicketObject}->SetOwner(
+            TicketID => $TicketID,
+            UserID => $UserID,
+            UserLogin => $UserLogin,
+          );
+          # set lock
+          if ($Self->{TicketObject}->SetLock(
+            TicketID => $TicketID,
+            Lock => 'lock',
+            UserID => $UserID
+          )) {
+            # show lock state
+            $Output .= $Self->{LayoutObject}->TicketLocked(TicketID => $TicketID);
+          }
+        }
+        else {
+          my ($OwnerID, $OwnerLogin) = $Self->{TicketObject}->CheckOwner(
+            TicketID => $TicketID,
+          );
+
+          if ($OwnerID != $UserID) {
+            $Output .= $Self->{LayoutObject}->Error(
+                Message => "Sorry, the current owner is $OwnerLogin",
+                Comment => 'Please change the owner first.',
+            );
+            $Output .= $Self->{LayoutObject}->Footer();
+            return $Output;
+          }
+        }
+
+        # print form ...
+        $Output .= $Self->{LayoutObject}->AgentPhone(
+            TicketID => $TicketID,
+            QueueID => $QueueID,
+            BackScreen => $BackScreen,
+            NextScreen => $NextScreen,
+            TicketNumber => $Tn,
+            NoteTypes => \%NoteTypes,
+            NextStates => \%NextStates,
+        );
+        $Output .= $Self->{LayoutObject}->Footer();
+    }
+    elsif ($Subaction eq 'Store') {
+        my $Subject = $Self->{ParamObject}->GetParam(Param => 'Subject') || 'Note!';
+        my $Text = $Self->{ParamObject}->GetParam(Param => 'Note');
+        my $NextStateID = $Self->{ParamObject}->GetParam(Param => 'NextStateID') || '';
+        my $NextState = $Self->{TicketObject}->StateIDLookup(StateID => $NextStateID);
+        my $ArticleTypeID = $Self->{ParamObject}->GetParam(Param => 'NoteID');
+        my $ArticleObject = Kernel::System::Article->new(
+            DBObject => $Self->{DBObject},
+            ConfigObject => $Self->{ConfigObject},
+        );
+        my $ArticleID = $ArticleObject->CreateArticleDB(
+            TicketID => $TicketID,
+#            ArticleTypeID => $ArticleTypeID,
+            ArticleType => $Self->{ConfigObject}->Get('DefaultPhoneArticleType'),
+            SenderType => $Self->{ConfigObject}->Get('DefaultPhoneSenderType'),
+            From => $UserLogin,
+            To => $UserLogin,
+            Subject => $Subject,
+            Body => $Text,
+            CreateUserID => $UserID
+        );
+        $Self->{TicketObject}->AddHistoryRow(
+            TicketID => $TicketID,
+            ArticleID => $ArticleID,
+            HistoryType => $Self->{ConfigObject}->Get('DefaultPhoneHistoryType'),
+            Name => $Self->{ConfigObject}->Get('DefaultPhoneHistoryComment'),
+            CreateUserID => $UserID,
+        );
+
+        # set state
+        if ($Self->{TicketObject}->GetState(TicketID => $TicketID)  ne $NextState) {
+          $Self->{TicketObject}->SetState(
+            TicketID => $TicketID,
+            ArticleID => $ArticleID,
+            State => $NextState,
+            UserID => $UserID,
+          );
+        }
+        # should i set an unlock?
+        if ($NextState =~ /^close/i) {
+          $Self->{TicketObject}->SetLock(
+            TicketID => $TicketID,
+            Lock => 'unlock',
+            UserID => $UserID,
+          );
+        }
+
+        
+        $Output .= $Self->{LayoutObject}->Redirect(
+            OP => "&Action=$NextScreen&QueueID=$QueueID&TicketID=$TicketID",
+        );
+    }
+    elsif ($Subaction eq 'StoreNew') {
+        my $Subject = $Self->{ParamObject}->GetParam(Param => 'Subject') || 'Note!';
+        my $Text = $Self->{ParamObject}->GetParam(Param => 'Note');
+        my $NextStateID = $Self->{ParamObject}->GetParam(Param => 'NextStateID') || '';
+        my $NextState = $Self->{TicketObject}->StateIDLookup(StateID => $NextStateID);
+        my $ArticleTypeID = $Self->{ParamObject}->GetParam(Param => 'NoteID');
+        my $NewQueueID = $Self->{ParamObject}->GetParam(Param => 'NewQueueID') || 4;
+        my $From = $Self->{ParamObject}->GetParam(Param => 'From') || '??';
+        my $ArticleObject = Kernel::System::Article->new(
+            DBObject => $Self->{DBObject},
+            ConfigObject => $Self->{ConfigObject},
+        );
+
+        # create new ticket
+        my $NewTn = $Self->{TicketObject}->CreateTicketNr();
+
+        # do db insert
+        my $TicketID = $Self->{TicketObject}->CreateTicketDB(
+            TN => $NewTn,
+            QueueID => $NewQueueID,
+            Lock => 'unlock',
+            # FIXME !!!
+            GroupID => 1,
+            StateID => $NextStateID,
+            Priority => 'normal',
+            UserID => $Self->{UserID},
+            CreateUserID => $Self->{UserID},
+        );
+
+        my $ArticleID = $ArticleObject->CreateArticleDB(
+            TicketID => $TicketID,
+#            ArticleTypeID => $ArticleTypeID,
+            ArticleType => $Self->{ConfigObject}->Get('DefaultPhoneNewArticleType'),
+            SenderType => $Self->{ConfigObject}->Get('DefaultPhoneNewSenderType'),
+            From => $From,
+            To => $UserLogin,
+            Subject => $Subject,
+            Body => $Text,
+            CreateUserID => $UserID
+        );
+
+        $Self->{TicketObject}->AddHistoryRow(
+            TicketID => $TicketID,
+            ArticleID => $ArticleID,
+            HistoryType => $Self->{ConfigObject}->Get('DefaultPhoneNewHistoryType'),
+            Name => $Self->{ConfigObject}->Get('DefaultPhoneNewHistoryComment'),
+            CreateUserID => $UserID,
+        );
+
+        # should i set an unlock?
+        if ($NextState =~ /^close/i) {
+          $Self->{TicketObject}->SetLock(
+            TicketID => $TicketID,
+            Lock => 'unlock',
+            UserID => $UserID,
+          );
+        }
+        
+        $Output .= $Self->{LayoutObject}->Redirect(
+            OP => "&Action=$NextScreen&QueueID=$QueueID&TicketID=$TicketID",
+        );
+    }
+    else {
+        $Output .= $Self->{LayoutObject}->Header(Title => 'Error');
+        $Output .= $Self->{LayoutObject}->Error(
+            Message => 'No Subaction!!',
+            Comment => 'Please contact your admin',
+        );
+        $Output .= $Self->{LayoutObject}->Footer();
+    }
+    return $Output;
+}
+# --
+
+1;
