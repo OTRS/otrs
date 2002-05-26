f8832bc6a14020d18003158a6cb9ad676d5c58f0
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-05-26 21:30:17 +0000

    added "print STDERR" for error logs.

--- Kernel/System/Syslog.pm
+++ Kernel/System/Syslog.pm
@@ -1,8 +1,8 @@
 # --
 # Syslog.pm - a wrapper for xyz::Syslog 
-# Copyright (C) 2001 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Syslog.pm,v 1.3 2001-12-30 00:43:13 martin Exp $
+# $Id: Syslog.pm,v 1.4 2002-05-26 21:30:17 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -16,7 +16,7 @@ use Unix::Syslog qw(:macros);  # Syslog macros
 use Unix::Syslog qw(:subs);  # Syslog functions
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.3 $ ';
+$VERSION = '$Revision: 1.4 $ ';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/g;
 
 # --
@@ -26,8 +26,10 @@ sub new {
 
     my $Self = {}; # allocate new hash for object
     bless ($Self, $Type);
+ 
+    $Self->{LogSuffix} = 'OpenTRS';
 
-    openlog("OpenTRS", LOG_PID, LOG_USER);
+    openlog($Self->{LogSuffix}, LOG_PID, LOG_USER);
 
     return $Self;
 }
@@ -41,23 +43,26 @@ sub Log {
 
     if ($Priority =~ /debug/i) {
         my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(0);
-        syslog(LOG_DEBUG, "[Debug][$Package][$Line] $MSG");
+        syslog(LOG_DEBUG, "[Debug][$Subroutine][$Line] $MSG");
     }
     elsif ($Priority =~ /info/i) {
         my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(1);
-        syslog(LOG_INFO, "[Info][$Package] $MSG");
+        syslog(LOG_INFO, "[Info][$Subroutine] $MSG");
     }
     elsif ($Priority =~ /notice/i) {
         my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(0);
-        syslog(LOG_NOTICE, "[Notice][$Package] $MSG");
+        syslog(LOG_NOTICE, "[Notice][$Subroutine] $MSG");
     }
     elsif ($Priority =~ /error/i) {
         my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(1);
-        syslog(LOG_ERR, "[Error][$Package][Line:$Line]: $MSG");
+        # print error messages to STDERR
+        print STDERR "[$Self->{LogSuffix}][Error][$Subroutine][Line:$Line][MSG:$MSG]\n";
+        # and of coure to syslog
+        syslog(LOG_ERR, "[Error][$Subroutine][Line:$Line]: $MSG");
     }
     else {
         my ($Package, $Filename, $Line, $Subroutine, $Hasargs) = caller(1);
-        syslog(LOG_ERR, "[Error][$Package] Priority: '$Param{Priority}' not defined! MSG: $MSG");
+        syslog(LOG_ERR, "[Error][$Subroutine] Priority: '$Param{Priority}' not defined! MSG: $MSG");
     }
 
     return;
