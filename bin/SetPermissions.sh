5c6b686e6136b74f043b58ec1a410fb5df8b76ef
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-06-03 23:46:28 +0000

    added generic support.

--- bin/SetPermissions.sh
+++ bin/SetPermissions.sh
@@ -3,7 +3,7 @@
 # SetPermissions.sh - to set the otrs permissions 
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: SetPermissions.sh,v 1.3 2002-05-26 23:26:44 martin Exp $
+# $Id: SetPermissions.sh,v 1.4 2002-06-03 23:46:28 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,28 +19,66 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
-export WEBUSER=wwwrun
-export OTRSUSER=otrs
-export OTRSDEST=/opt/OpenTRS
 
+echo "SetPermissions.sh <\$Revision: 1.4 $> - set OpenTRS file permissions"
+echo "Copyright (c) 2002 Martin Edenhofer <martin@otrs.org>"
+
+if ! test $1 || ! test $2 || ! test $3; then 
+    # --
+    # check required options
+    # --
+    echo ""
+    echo "Usage: SetPermissions.sh <OTRS_HOME> <OTRS_USER> <WEBSERVER_USER>"
+    echo ""
+    echo "  Try: SetPermissions.sh /opt/OpenTRS otrs wwwrun"                      
+    echo ""
+    exit 1;
+else 
+    # --
+    # system settings
+    # --
+    WEBUSER=$3
+    OTRSUSER=$2
+    OTRSDEST=$1
+fi
+
+
+# --
 # set permission
+# --
 echo "Setting file permissions... "
-echo "root.root $OTRSDEST"
+# set all files to root.root (safe is safe)
+echo "chown -R root.root $OTRSDEST"
 chown -R root.root $OTRSDEST
 
-echo "$OTRSUSER.nogroup $OTRSDEST/var/" 
+# set the $HOME to the OpenTRS user
+echo "chown $OTRSUSER.nogroup $OTRSDEST"
+chown $OTRSUSER.nogroup $OTRSDEST
+
+# set the fetchmail rc to OpenTRS user
+echo "chown $OTRSUSER.nogroup $OTRSDEST/.fetchmailrc"
+chown $OTRSUSER.nogroup $OTRSDEST/.fetchmailrc
+
+# set the var directory to OpenTRS and webserver user
+echo "chown -R $OTRSUSER.nogroup $OTRSDEST/var/" 
 chown -R $OTRSUSER.nogroup $OTRSDEST/var/
 chmod -R 2775 $OTRSDEST/var/article/
 
-echo "$WEBUSER.nogroup $OTRSDEST/var/sessions/"
+# set the var/sessions directory to OpenTRS and webserver user
+echo "chown -R $WEBUSER.nogroup $OTRSDEST/var/sessions/"
 chown -R $WEBUSER $OTRSDEST/var/sessions/ 
 
+# set the var/log/TicketCounter.log file to OpenTRS and webserver user
 echo "$OTRSUSER.nogroup $OTRSDEST/var/log/TicketCounter.log"
 chmod -R 2775 $OTRSDEST/var/log/TicketCounter.log
 
-echo "$OTRSUSER.root $OTRSDEST/bin/DeleteSessionIDs.pl"
+# set the DeleteSessionIDs.pl just to OpenTRS user
+echo "chown $OTRSUSER.root $OTRSDEST/bin/DeleteSessionIDs.pl"
 chown $OTRSUSER.root $OTRSDEST/bin/DeleteSessionIDs.pl 
 
-echo "$OTRSUSER.root $OTRSDEST/bin/UnlockTickets.pl"
+# set the UnlockTickets.pl just to OpenTRS user
+echo "chown $OTRSUSER.root $OTRSDEST/bin/UnlockTickets.pl"
 chown $OTRSUSER.root $OTRSDEST/bin/UnlockTickets.pl
+
+exit;
 
