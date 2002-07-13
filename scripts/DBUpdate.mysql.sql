86b993673bcd6362c62db543effece6d57d10108
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-07-13 12:32:46 +0000

    added more articke types and more history types

--- scripts/DBUpdate.mysql.sql
+++ scripts/DBUpdate.mysql.sql
@@ -2,7 +2,7 @@
 -- Update an existing OpenTRS database to the current state.
 -- Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 -- --
--- $Id: DBUpdate.mysql.sql,v 1.3 2002-07-02 08:46:37 martin Exp $
+-- $Id: DBUpdate.mysql.sql,v 1.4 2002-07-13 12:32:46 martin Exp $
 -- --
 --
 -- usage: cat DBUpdate.mysql.sql | mysql -f -u root otrs
@@ -12,6 +12,28 @@
 -- --
 -- BETA 7 upgrate
 -- --
+
+-- new article types
+INSERT INTO article_type
+        (name, valid_id, create_by, create_time, change_by, change_time)
+        VALUES
+        ('email-notification-ext', 1, 1, current_timestamp, 1, current_timestamp);
+INSERT INTO article_type
+        (name, valid_id, create_by, create_time, change_by, change_time)
+        VALUES
+        ('email-notification-int', 1, 1, current_timestamp, 1, current_timestamp);
+
+-- new ticket history types
+INSERT INTO ticket_history_type
+        (name, valid_id, create_by, create_time, change_by, change_time)
+        VALUES
+        ('Forward', 1, 1, current_timestamp, 1, current_timestamp);
+INSERT INTO ticket_history_type
+        (name, valid_id, create_by, create_time, change_by, change_time)
+        VALUES
+        ('Bounce', 1, 1, current_timestamp, 1, current_timestamp);
+-- content_type to display the right charset and it is also used
+-- for utf-8 support.
 ALTER TABLE article ADD a_content_type VARCHAR (100);
 
 -- --
