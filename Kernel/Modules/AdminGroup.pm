0268452a0a0ad30ed8d9fccde92ccbda31fd3061
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-07-21 16:47:37 +0000

    use Kernel/System/Group.pm now

--- Kernel/Modules/AdminGroup.pm
+++ Kernel/Modules/AdminGroup.pm
@@ -1,8 +1,8 @@
 # --
-# AdminGroup.pm - to add/update/delete groups 
-# Copyright (C) 2001,2002 Martin Edenhofer <martin+code@otrs.org>
+# Kernel/Modules/AdminGroup.pm - to add/update/delete groups 
+# Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AdminGroup.pm,v 1.3 2002-04-08 20:40:12 martin Exp $
+# $Id: AdminGroup.pm,v 1.4 2002-07-21 16:47:37 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -13,8 +13,10 @@ package Kernel::Modules::AdminGroup;
 
 use strict;
 
+use Kernel::System::Group;
+
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.3 $';
+$VERSION = '$Revision: 1.4 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -32,9 +34,11 @@ sub new {
     }
 
     # check all needed objects
-    foreach ('ParamObject', 'DBObject', 'QueueObject', 'LayoutObject', 'ConfigObject', 'LogObject') {
+    foreach (qw(ParamObject DBObject LayoutObject ConfigObject LogObject PermissionObject)) {
         die "Got no $_" if (!$Self->{$_});
     }
+
+    $Self->{GroupObject} = Kernel::System::Group->new(%Param);   
 
     return $Self;
 }
@@ -44,95 +48,75 @@ sub Run {
     my %Param = @_;
     my $Output = '';
     $Param{NextScreen} = 'AdminGroup';
-
+    # --
     # permission check
+    # --
     if (!$Self->{PermissionObject}->Section(UserID => $Self->{UserID}, Section => 'Admin')) {
         $Output .= $Self->{LayoutObject}->NoPermission();
         return $Output;
     }
-
-    # get user data 2 form
+    # --
+    # get group data 
+    # --
     if ($Self->{Subaction} eq 'Change') {
         my $ID = $Self->{ParamObject}->GetParam(Param => 'ID') || '';
-        $Output .= $Self->{LayoutObject}->Header(Title => 'Group change');
+        my %GroupData = $Self->{GroupObject}->GroupGet(ID => $ID);
+        $Output = $Self->{LayoutObject}->Header(Title => 'Group change');
         $Output .= $Self->{LayoutObject}->AdminNavigationBar();
-        # get user data TODO: comment!!! 
-        my $SQL = "SELECT name, valid_id, comment " .
-           " FROM " .
-           " groups " .
-           " WHERE " .
-           " id = $ID";
-        $Self->{DBObject}->Prepare(SQL => $SQL);
-        my @Data = $Self->{DBObject}->FetchrowArray();
-        $Output .= $Self->{LayoutObject}->AdminGroupForm(
-                ID => $ID,
-                Name => $Data[0],
-                Comment => $Data[2], 
-                ValidID => $Data[1],
-            );
+        $Output .= $Self->{LayoutObject}->AdminGroupForm(%GroupData);
         $Output .= $Self->{LayoutObject}->Footer();
+        return $Output;
     }
+    # --
     # update action
+    # --
     elsif ($Self->{Subaction} eq 'ChangeAction') {
         my %GetParam;
         my @Params = ('ID', 'Name', 'Comment', 'ValidID');
         foreach (@Params) {
             $GetParam{$_} = $Self->{ParamObject}->GetParam(Param => $_) || '';
-            $GetParam{$_} = $Self->{DBObject}->Quote($GetParam{$_}) || '';
-            $GetParam{$_} = '' if (!exists $GetParam{$_});
         }
-        my $SQL = "UPDATE groups SET name = '$GetParam{Name}', " .
-          " comment = '$GetParam{Comment}', " .
-          " valid_id = $GetParam{ValidID}, " .
-          " change_time = current_timestamp, change_by = $Self->{UserID} " .
-          " WHERE id = $GetParam{ID}";
-        if ($Self->{DBObject}->Do(SQL => $SQL)) { 
-            $Output .= $Self->{LayoutObject}->Redirect(OP => "&Action=$Param{NextScreen}");
+        if ($Self->{GroupObject}->GroupUpdate(%GetParam, UserID => $Self->{UserID})) {
+            return $Self->{LayoutObject}->Redirect(OP => "&Action=$Param{NextScreen}");
         }
         else {
-        $Output .= $Self->{LayoutObject}->Header();
-        $Output .= $Self->{LayoutObject}->Error(
-                Message => 'DB Error!!',
-                Comment => 'Please contact your admin');
-        $Output .= $Self->{LayoutObject}->Footer();
+            $Output = $Self->{LayoutObject}->Header();
+            $Output .= $Self->{LayoutObject}->AdminNavigationBar();
+            $Output .= $Self->{LayoutObject}->Error();
+            $Output .= $Self->{LayoutObject}->Footer();
+            return $Output;
         }
     }
-    # add new user
+    # --
+    # add group
+    # --
     elsif ($Self->{Subaction} eq 'AddAction') {
         my %GetParam;
-        $GetParam{Pw} = '';
-        $GetParam{Pw} = crypt($GetParam{Pw}, $Self->{UserID});
         my @Params = ('Name', 'Comment', 'ValidID');
         foreach (@Params) {
             $GetParam{$_} = $Self->{ParamObject}->GetParam(Param => $_) || '';
-            $GetParam{$_} = $Self->{DBObject}->Quote($GetParam{$_}) || '';
         }
-        my $SQL = "INSERT INTO groups (name, comment, valid_id, ".
-                " create_time, create_by, change_time, change_by)" .
-                " VALUES " .
-                " ('$GetParam{Name}', '$GetParam{Comment}', " .
-                " $GetParam{ValidID}, current_timestamp, $Self->{UserID}, ".
-                " current_timestamp, $Self->{UserID})";
-        if ($Self->{DBObject}->Do(SQL => $SQL)) {        
-             $Output .= $Self->{LayoutObject}->Redirect(OP => "&Action=$Param{NextScreen}");
+        if($Self->{GroupObject}->GroupAdd(%GetParam, UserID => $Self->{UserID})) {
+             return $Self->{LayoutObject}->Redirect(OP => "&Action=$Param{NextScreen}");
         }
         else {
-        $Output .= $Self->{LayoutObject}->Header();
-        $Output .= $Self->{LayoutObject}->AdminNavigationBar();
-        $Output .= $Self->{LayoutObject}->Error(
-                Message => 'DB Error!!',
-                Comment => 'Please contact your admin');
-        $Output .= $Self->{LayoutObject}->Footer();
+            $Output = $Self->{LayoutObject}->Header();
+            $Output .= $Self->{LayoutObject}->AdminNavigationBar();
+            $Output .= $Self->{LayoutObject}->Error();
+            $Output .= $Self->{LayoutObject}->Footer();
+            return $Output;
         }
     }
+    # --
     # else ! print form 
+    # --
     else {
-        $Output .= $Self->{LayoutObject}->Header(Title => 'Group add');
+        $Output = $Self->{LayoutObject}->Header(Title => 'Group add');
         $Output .= $Self->{LayoutObject}->AdminNavigationBar();
         $Output .= $Self->{LayoutObject}->AdminGroupForm();
         $Output .= $Self->{LayoutObject}->Footer();
+        return $Output;
     }
-    return $Output;
 }
 # --
 
