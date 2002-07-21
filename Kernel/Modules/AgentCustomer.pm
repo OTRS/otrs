95a3753115a0b1b9c7f36f6a6ac0986b406d0eb8
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-07-21 18:50:01 +0000

    fixed message text

--- Kernel/Modules/AgentCustomer.pm
+++ Kernel/Modules/AgentCustomer.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentCustomer.pm - to set the ticket customer and show the customer history
 # Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentCustomer.pm,v 1.2 2002-07-18 23:29:50 martin Exp $
+# $Id: AgentCustomer.pm,v 1.3 2002-07-21 18:50:01 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Modules::AgentCustomer;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.2 $';
+$VERSION = '$Revision: 1.3 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -190,11 +190,17 @@ sub Run {
             );
           }
         }
-        if (!$OutputTables || $Self->{Search}) {
+        if (!$OutputTables && $Self->{Search}) {
           $Output .= $Self->{LayoutObject}->AgentUtilSearchAgain(
               %Param,
               CustomerID => $Self->{CustomerID},
               Message => 'No entry found!',
+          );
+        }
+        elsif ($Self->{Search}) {
+          $Output .= $Self->{LayoutObject}->AgentUtilSearchAgain(
+              %Param,
+              CustomerID => $Self->{CustomerID},
           );
         }
         if ($OutputTables) {
