39740b626dcf92bb624815bdbf2a767d37eb1f3e
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-07-23 22:13:10 +0000

    moved into module system Kernel::System::Auth::*

--- Kernel/System/Auth.pm
+++ Kernel/System/Auth.pm
@@ -1,8 +1,8 @@
 # --
-# Kernel/System/Auth.pm - provides the authentification and user data
+# Kernel/System/Auth.pm - provides the authentification 
 # Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Auth.pm,v 1.9 2002-07-15 22:16:47 martin Exp $
+# $Id: Auth.pm,v 1.10 2002-07-23 22:13:10 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -13,8 +13,8 @@ package Kernel::System::Auth;
 
 use strict;
 
-use vars qw($VERSION);
-$VERSION = '$Revision: 1.9 $';
+use vars qw(@ISA $VERSION);
+$VERSION = '$Revision: 1.10 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -29,95 +29,22 @@ sub new {
     # 0=off; 1=on;
     $Self->{Debug} = 0;
 
+    # --
     # check needed objects
+    # --
     foreach ('LogObject', 'ConfigObject', 'DBObject') {
         $Self->{$_} = $Param{$_} || die "No $_!";
     }
 
-    # get wduser table
-    $Self->{UserTable} = $Self->{ConfigObject}->Get('DatabaseUserTable') 
-      || 'user';
-    $Self->{UserTableUserID} = $Self->{ConfigObject}->Get('DatabaseUserTableUserID')
-	  || 'id';
-    $Self->{UserTableUserPW} = $Self->{ConfigObject}->Get('DatabaseUserTableUserPW')
-      || 'pw';
-    $Self->{UserTableUser} = $Self->{ConfigObject}->Get('DatabaseUserTableUser')
-	  || 'login';
+    # --
+    # load generator auth module
+    # --
+    my $GeneratorModule = $Self->{ConfigObject}->Get('AuthModule')
+      || 'Kernel::System::Auth::DB';
+    eval "require $GeneratorModule";
+    push(@ISA, $GeneratorModule);
 
     return $Self;
-}
-# --
-sub Auth {
-    my $Self = shift;
-    my %Param = @_;
-    my $User = $Param{User} || return;
-    my $Pw = $Param{Pw} || '';
-    my $RemoteAddr = $ENV{REMOTE_ADDR} || 'Got no REMOTE_ADDR env!';
-    my $UserID = '';
-    my $GetPw = '';
-    my $SQL = "SELECT $Self->{UserTableUserPW}, $Self->{UserTableUserID} ".
-      " FROM ".
-      " $Self->{UserTable} ".
-      " WHERE ". 
-      " valid_id in ( ${\(join ', ', $Self->{DBObject}->GetValidIDs())} ) ".
-      " AND ".
-      " $Self->{UserTableUser} = '$User'";
-    $Self->{DBObject}->Prepare(SQL => $SQL);
-    while (my @RowTmp = $Self->{DBObject}->FetchrowArray()) { 
-        $GetPw = $RowTmp[0];
-        $UserID = $RowTmp[1];
-    }
-
-    # --
-    # just in case!
-    # --
-    if ($Self->{Debug} > 0) {
-        $Self->{LogObject}->Log(
-          Priority => 'notice',
-          Message => "User: '$User' tried to login with Pw: '$Pw' ($UserID/$GetPw/$RemoteAddr)",
-        );
-    }
-
-    # --
-    # just a note 
-    # --
-    if (!$Pw) {
-        $Self->{LogObject}->Log(
-          Priority => 'notice',
-          Message => "User: $User without Pw!!! (REMOTE_ADDR: $RemoteAddr)",
-        );
-        return;
-    }
-    # --
-    # login note
-    # --
-    elsif ((($GetPw)&&($User)&&($UserID)) && crypt($Pw, $User) eq $GetPw) {
-        $Self->{LogObject}->Log(
-          Priority => 'notice',
-          Message => "User: $User logged in (REMOTE_ADDR: $RemoteAddr).",
-        );
-        return 1;
-    }
-    # --
-    # just a note
-    # --
-    elsif (($UserID) && ($GetPw)) {
-        $Self->{LogObject}->Log(
-          Priority => 'notice',
-          Message => "User: $User with wrong Pw!!! (REMOTE_ADDR: $RemoteAddr)"
-        ); 
-        return;
-    }
-    # --
-    # just a note
-    # --
-    else {
-        $Self->{LogObject}->Log(
-          Priority => 'notice',
-          Message => "User: $User doesn't exist or is invalid!!! (REMOTE_ADDR: $RemoteAddr)"
-        ); 
-        return;
-    }
 }
 # --
 
