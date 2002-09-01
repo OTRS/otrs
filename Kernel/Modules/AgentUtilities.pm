61b3e6214c61846de421bc9dbdc4021f7b454e6a
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-09-01 22:37:20 +0000

    changed sql query order for search by text

--- Kernel/Modules/AgentUtilities.pm
+++ Kernel/Modules/AgentUtilities.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentUtilities.pm - Utilities for tickets
 # Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentUtilities.pm,v 1.9 2002-09-01 20:24:04 martin Exp $
+# $Id: AgentUtilities.pm,v 1.10 2002-09-01 22:37:20 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Modules::AgentUtilities;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.9 $';
+$VERSION = '$Revision: 1.10 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -287,7 +287,8 @@ sub SearchByText {
     " sug.user_id = $UserID " .
     " AND " .
     " ($SqlExt) " .
-    " ORDER BY sa.incoming_time DESC";
+    " ORDER BY st.id DESC";
+#    " ORDER BY sa.incoming_time DESC";
     $Self->{DBObject}->Prepare(SQL => $SQL, Limit => $Self->{SearchLimitTxt});
     my @ViewableArticleIDs = ();
     my $Counter = 0;
