29b0e39ccc3dd6d72ed0151d9dc6885825f6bc6c
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-09-20 11:02:56 +0000

    moved this stuff from Kernel/System/PostMaster/AutoResponse.pm to Kernel/System/AutoResponse.pm

--- Kernel/System/AutoResponse.pm
+++ Kernel/System/AutoResponse.pm
@@ -2,7 +2,7 @@
 # Kernel/System/AutoResponse.pm - lib for auto responses
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AutoResponse.pm,v 1.2 2002-07-21 16:31:28 martin Exp $
+# $Id: AutoResponse.pm,v 1.3 2002-09-20 11:02:56 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -14,7 +14,7 @@ package Kernel::System::AutoResponse;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.2 $';
+$VERSION = '$Revision: 1.3 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -156,6 +156,49 @@ sub AutoResponseUpdate {
     else {
         return;
     }
+}
+# --
+sub AutoResponseGetByTypeQueueID {
+    my $Self = shift;
+    my %Param = @_;
+    my %Data;
+    # --
+    # check needed stuff
+    # --
+    foreach (qw(QueueID Type)) {
+      if (!$Param{$_}) {
+        $Self->{LogObject}->Log(Priority => 'error', Message => "Need $_!");
+        return;
+      }
+    }
+    # --
+    # SQL query
+    # --
+    my $SQL = "SELECT ar.text0, sa.value0, sa.value1, ar.text1, ch.charset" .
+    " FROM " .
+    " auto_response_type art, auto_response ar, queue_auto_response qar, ".
+    " system_address sa, charset ch " .
+    " WHERE " .
+    " qar.queue_id = $Param{QueueID} " .
+    " AND " .
+    " art.id = ar.type_id " .
+    " AND " .
+    " qar.auto_response_id = ar.id " .
+    " AND " .
+    " ar.system_address_id = sa.id" .
+    " AND " .
+    " ar.charset_id = ch.id ".
+    " AND " .
+    " art.name = '$Param{Type}'";
+    $Self->{DBObject}->Prepare(SQL => $SQL);
+    while (my @RowTmp = $Self->{DBObject}->FetchrowArray()) {
+        $Data{Text} = $RowTmp[0];
+        $Data{Address} = $RowTmp[1];
+        $Data{Realname} = $RowTmp[2];
+        $Data{Subject} = $RowTmp[3];
+        $Data{Charset} = $RowTmp[4];
+    }
+    return %Data;
 }
 # --
 
