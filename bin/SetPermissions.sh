c682872ad22febdca3f33d661a1e354fa1f3fd58
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-09-30 22:09:51 +0000

    changed group of chown -R $OTRSDEST/var/ to webserver group -=> helps if a new month dir is created by the otrs user.

--- bin/SetPermissions.sh
+++ bin/SetPermissions.sh
@@ -3,7 +3,7 @@
 # SetPermissions.sh - to set the otrs permissions 
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: SetPermissions.sh,v 1.9 2002-09-23 13:54:29 martin Exp $
+# $Id: SetPermissions.sh,v 1.10 2002-09-30 22:09:51 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
 
-echo "SetPermissions.sh <\$Revision: 1.9 $> - set OTRS file permissions"
+echo "SetPermissions.sh <\$Revision: 1.10 $> - set OTRS file permissions"
 echo "Copyright (c) 2002 Martin Edenhofer <martin@otrs.org>"
 
 if ! test $1 || ! test $2 || ! test $3; then 
@@ -76,8 +76,8 @@ chmod 0644 $OTRSDEST/.procmailrc
 # var/*
 # --
 # set the var directory to OTRS and webserver user
-echo "chown -R $OTRSUSER.$OTRSGROUP $OTRSDEST/var/" 
-chown -R $OTRSUSER.$OTRSGROUP $OTRSDEST/var/
+echo "chown -R $OTRSUSER.$WEBGROUP $OTRSDEST/var/" 
+chown -R $OTRSUSER.$WEBGROUP $OTRSDEST/var/
 chmod -R 2775 $OTRSDEST/var/article/
 
 # set the var/sessions directory to OTRS and webserver user
