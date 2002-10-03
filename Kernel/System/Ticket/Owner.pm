0b8f7250615f5295da63013c82aef1ebbce838f3
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-10-03 17:44:36 +0000

    moved owner notify to Kernel::System::SendNotifications

--- Kernel/System/Ticket/Owner.pm
+++ Kernel/System/Ticket/Owner.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket/Owner.pm - the sub module of the global ticket handle
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Owner.pm,v 1.3 2002-09-10 23:21:38 martin Exp $
+# $Id: Owner.pm,v 1.4 2002-10-03 17:44:36 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::System::Ticket::Owner;
 use strict;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.3 $';
+$VERSION = '$Revision: 1.4 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -119,53 +119,22 @@ sub SetOwner {
       # --
       if ($Param{UserID} ne $Param{NewUserID} && 
            $Param{NewUserID} ne $Self->{ConfigObject}->Get('PostmasterUserID')) {
-         if (!$Param{Comment}) {
-             $Param{Comment} = '';
-         }
-         # get login user data
-         my %PreferencesCurrentUser = $Self->{UserObject}->GetUserData(UserID => $Param{UserID});
-         # get user data of new owner
-         my %Preferences = $Self->{UserObject}->GetUserData(UserID => $Param{NewUserID});
-         # create notify stuff
-         my $TicketHook = $Self->{ConfigObject}->Get('TicketHook') || '';
-         my $Tn = $Self->GetTNOfId(ID => $Param{TicketID});
-         my $Subject = $Self->{ConfigObject}->Get('NotificationSubjectOwnerUpdate')
-              || 'No subject found in Config.pm!';
-         $Subject = "[$TicketHook: $Tn] $Subject";
-         my $Body = $Self->{ConfigObject}->Get('NotificationBodyOwnerUpdate')
-          || 'No body found in Config.pm!';
-         $Body =~ s/<OTRS_TICKET_ID>/$Param{TicketID}/g;
-         $Body =~ s/<OTRS_TICKET_NUMBER>/$Tn/g;
-         $Body =~ s/<OTRS_COMMENT>/$Param{Comment}/g;
-         $Body =~ s/<OTRS_USER_FIRSTNAME>/$Preferences{UserFirstname}/g;
-         $Body =~ s/<OTRS_CURRENT_USER_FIRSTNAME>/$PreferencesCurrentUser{UserFirstname}/g;
-         $Body =~ s/<OTRS_CURRENT_USER_LASTNAME>/$PreferencesCurrentUser{UserLastname}/g;
-         # --
-         # send notification
-         # --
-         $Self->{EmailObject} = Kernel::System::EmailSend->new(
-             ConfigObject => $Self->{ConfigObject},
-             LogObject => $Self->{LogObject},
-             DBObject => $Self->{DBObject},
-         );
-         $Self->{EmailObject}->Send(
-            ArticleObject => $Self->{ArticleObject},
-            ArticleType => 'email-notification-int',
-            SenderType => 'system',
-            TicketID => $Param{TicketID},
-            HistoryType => 'SendAgentNotification',
-            HistoryComment => "Sent notification to '$Preferences{UserEmail}'.",
-            From => $Self->{ConfigObject}->Get('NotificationSenderName').
-              ' <'.$Self->{ConfigObject}->Get('NotificationSenderEmail').'>',
-            Email => $Self->{ConfigObject}->Get('NotificationSenderEmail'),
+        if (!$Param{Comment}) {
+            $Param{Comment} = '';
+        }
+        # get user data
+        my %Preferences = $Self->{UserObject}->GetUserData(UserID => $Param{NewUserID});
+        # --
+        # send notification
+        # --
+        $Self->{SendNotification}->Send(
+            Type => 'OwnerUpdate',
             To => $Preferences{UserEmail},
-            Subject => $Subject,
-            UserID => $Self->{ConfigObject}->Get('PostmasterUserID'),
-            Body => $Body,
-            Loop => 1,
-         );
-
-
+            CustomerMessageParams => \%Param,
+            TicketNumber => $Self->GetTNOfId(ID => $Param{TicketID}),
+            TicketID => $Param{TicketID},
+            UserID => $Param{UserID},
+        );
       }
       return 1;
     }
