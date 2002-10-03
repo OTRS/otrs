e9c28765f2ad83ed2f5cfa305d02cd44cafcecdd
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-10-03 17:30:17 +0000

    added move and note notifications.

--- Kernel/Config/Notification.pm
+++ Kernel/Config/Notification.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Notification.pm - Notification config file for OTRS 
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Notification.pm,v 1.4 2002-09-23 18:56:09 martin Exp $
+# $Id: Notification.pm,v 1.5 2002-10-03 17:30:17 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -12,7 +12,7 @@ package Kernel::Config::Notification;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.4 $';
+$VERSION = '$Revision: 1.5 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -33,7 +33,9 @@ sub LoadNotification {
     # --
     # new ticket in queue
     # --
-    $Self->{NotificationSubjectNewTicket} = 'New ticket notification! (<OTRS_CUSTOMER_SUBJECT[10]>)';
+#    $Self->{NotificationAlwaysCcNewTicket} = 'Maybe a Mailinglist <list@example.com>';
+    $Self->{NotificationAlwaysCcNewTicket} = '';
+    $Self->{NotificationSubjectNewTicket} = 'New ticket notification! (<OTRS_CUSTOMER_SUBJECT[18]>)';
     $Self->{NotificationBodyNewTicket} = "
 Hi,
 
@@ -41,7 +43,7 @@ there is a new ticket in '<OTRS_QUEUE>'!
 
 <OTRS_CUSTOMER_FROM> wrote:
 <snip>
-<OTRS_CUSTOMER_EMAIL[10]>
+<OTRS_CUSTOMER_EMAIL[16]>
 <snip>
 
 http://$Self->{FQDN}/otrs/index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_ID>
@@ -52,15 +54,17 @@ Your OTRS Notification Master
     # --
     # ticket follow up from customer
     # --
-    $Self->{NotificationSubjectFollowUp} = 'You got follow up! (<OTRS_CUSTOMER_SUBJECT[10]>)';
+#    $Self->{NotificationAlwaysCcFollowUp} = 'Maybe a Mailinglist <list@example.com>';
+    $Self->{NotificationAlwaysCcFollowUp} = '';
+    $Self->{NotificationSubjectFollowUp} = 'You got follow up! (<OTRS_CUSTOMER_SUBJECT[18]>)';
     $Self->{NotificationBodyFollowUp} = "
-Hi <OTRS_USERFIRSTNAME>,
+Hi <OTRS_OWNER_USERFIRSTNAME>,
 
 you got a follow up!
 
 <OTRS_CUSTOMER_FROM> wrote:
 <snip>
-<OTRS_CUSTOMER_EMAIL[10]>
+<OTRS_CUSTOMER_EMAIL[16]>
 <snip>
 
 http://$Self->{FQDN}/otrs/index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_ID>
@@ -71,11 +75,18 @@ Your OTRS Notification Master
     # --
     # ticket lock timeout by system
     # --
-    $Self->{NotificationSubjectLockTimeout} = 'Lock Timeout! (<OTRS_CUSTOMER_SUBJECT[10]>)';
+#    $Self->{NotificationAlwaysCcLockTimeout} = 'Maybe a Mailinglist <list@example.com>';
+    $Self->{NotificationAlwaysCcLockTimeout} = '';
+    $Self->{NotificationSubjectLockTimeout} = 'Lock Timeout! (<OTRS_CUSTOMER_SUBJECT[18]>)';
     $Self->{NotificationBodyLockTimeout} = "
-Hi <OTRS_USER_FIRSTNAME>,
+Hi <OTRS_OWNER_USERFIRSTNAME>,
 
-unlocked your locked ticket [<OTRS_TICKET_NUMBER>].
+unlocked (lock timeout) your locked ticket [<OTRS_TICKET_NUMBER>].
+
+<OTRS_CUSTOMER_FROM> wrote:
+<snip>
+<OTRS_CUSTOMER_EMAIL[8]>
+<snip>
 
 http://$Self->{FQDN}/otrs/index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_ID>
 
@@ -86,11 +97,13 @@ Your OTRS Notification Master
     # --
     # mail to new owner by owner update
     # --
-    $Self->{NotificationSubjectOwnerUpdate} = 'Ticket assigned to you!';
+#    $Self->{NotificationAlwaysCcOwnerUpdate} = 'Maybe a Mailinglist <list@example.com>';
+    $Self->{NotificationAlwaysCcOwnerUpdate} = '';
+    $Self->{NotificationSubjectOwnerUpdate} = 'Ticket assigned to you! (<OTRS_CUSTOMER_SUBJECT[18]>)';
     $Self->{NotificationBodyOwnerUpdate} = "
-Hi <OTRS_USER_FIRSTNAME>,
+Hi <OTRS_OWNER_USERFIRSTNAME>,
 
-a ticket [<OTRS_TICKET_NUMBER>] is assigned to you by '<OTRS_CURRENT_USER_FIRSTNAME> <OTRS_CURRENT_USER_LASTNAME>'.
+a ticket [<OTRS_TICKET_NUMBER>] is assigned to you by '<OTRS_CURRENT_USERFIRSTNAME> <OTRS_CURRENT_USERLASTNAME>'.
 
 Comment: 
 <OTRS_COMMENT>
@@ -100,6 +113,41 @@ http://$Self->{FQDN}/otrs/index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_ID>
 Your OTRS Notification Master
 
 ";
+    # --
+    # mail to owner by note add
+    # --
+#    $Self->{NotificationAlwaysCcNote} = '';
+    $Self->{NotificationSubjectAddNote} = 'New note! (<OTRS_CUSTOMER_SUBJECT[18]>)';
+    $Self->{NotificationBodyAddNote} = "
+Hi <OTRS_OWNER_USERFIRSTNAME>,
+
+'<OTRS_CURRENT_USERFIRSTNAME> <OTRS_CURRENT_USERLASTNAME>' added a new note to ticket [<OTRS_TICKET_NUMBER>].
+
+Note: 
+<OTRS_CUSTOMER_BODY>
+
+http://$Self->{FQDN}/otrs/index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_ID>
+
+Your OTRS Notification Master
+
+";
+    # --
+    # mail to queue subscriber by move 
+    # --
+#    $Self->{NotificationAlwaysCcMove} = 'Maybe a Mailinglist <list@example.com>';
+    $Self->{NotificationAlwaysCcMove} = '';
+    $Self->{NotificationSubjectMove} = 'Moved ticket in "<OTRS_CUSTOMER_QUEUE>" queue! (<OTRS_CUSTOMER_SUBJECT[18]>)';
+    $Self->{NotificationBodyMove} = "
+Hi,
+
+'<OTRS_CURRENT_USERFIRSTNAME> <OTRS_CURRENT_USERLASTNAME>' moved a ticket [<OTRS_TICKET_NUMBER>] into '<OTRS_CUSTOMER_QUEUE>'.
+
+http://$Self->{FQDN}/otrs/index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_ID>
+
+Your OTRS Notification Master
+
+";
+
 }
 # --
 
