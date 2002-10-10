d59c997c7780997e46e6fcde826c8b1b84ad6943
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-10-10 18:58:37 +0000

    added MD5 message digest

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: auto_build.sh,v 1.5 2002-09-23 14:48:12 martin Exp $
+# $Id: auto_build.sh,v 1.6 2002-10-10 18:58:37 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,16 +20,20 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.5 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.6 $>"
 echo "Copyright (c) 2002 Martin Edenhofer <martin@otrs.org>"
 
-PACKAGE=otrs
+
 PATH_TO_CVS_SRC=$1
+PRODUCT=OTRS
 VERSION=$2
 RELEASE=$3
 #ARCHIVE_DIR="otrs-$VERSION-$RELEASE"
 ARCHIVE_DIR="OpenTRS"
-PRODUCT=OTRS
+PACKAGE=otrs
+PACKAGE_BUILD_DIR="/tmp/$PACKAGE-build"
+PACKAGE_DEST_DIR="/tmp/$PACKAGE-packages"
+PACKAGE_TMP_SPEC="/tmp/$PACKAGE-spec.$$"
 
 if ! test $PATH_TO_CVS_SRC || ! test $VERSION || ! test $RELEASE; then
     # --
@@ -81,29 +85,28 @@ rm -rf $SYSTEM_SRPM_DIR/$PACKAGE*$VERSION*$RELEASE*.src.rpm
 # --
 # RPM and SRPM dir
 # --
-OTRS_PACKAGES=/tmp/otrs_packages
-rm -rf $OTRS_PACKAGES
-mkdir $OTRS_PACKAGES
-mkdir $OTRS_PACKAGES/RPM
-mkdir $OTRS_PACKAGES/RPM/suse
-mkdir $OTRS_PACKAGES/RPM/redhat
-mkdir $OTRS_PACKAGES/SRPM
-mkdir $OTRS_PACKAGES/SRPM/suse
-mkdir $OTRS_PACKAGES/SRPM/redhat
+rm -rf $PACKAGE_DEST_DIR
+mkdir $PACKAGE_DEST_DIR
+mkdir $PACKAGE_DEST_DIR/RPM
+mkdir $PACKAGE_DEST_DIR/RPM/suse
+mkdir $PACKAGE_DEST_DIR/RPM/redhat
+mkdir $PACKAGE_DEST_DIR/SRPM
+mkdir $PACKAGE_DEST_DIR/SRPM/suse
+mkdir $PACKAGE_DEST_DIR/SRPM/redhat
 
 # --
 # build 
 # --
-rm -rf /tmp/otrs_build || exit 1;
-mkdir -p /tmp/otrs_build/$ARCHIVE_DIR/ || exit 1;
+rm -rf $PACKAGE_BUILD_DIR || exit 1;
+mkdir -p $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ || exit 1;
 
-cp -a $PATH_TO_CVS_SRC/.*rc /tmp/otrs_build/$ARCHIVE_DIR/ || exit 1;
-cp -a $PATH_TO_CVS_SRC/* /tmp/otrs_build/$ARCHIVE_DIR/ || exit 1;
+cp -a $PATH_TO_CVS_SRC/.*rc $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ || exit 1;
+cp -a $PATH_TO_CVS_SRC/* $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ || exit 1;
 
 # --
 # update RELEASE
 # --
-RELEASEFILE=/tmp/otrs_build/$ARCHIVE_DIR/RELEASE
+RELEASEFILE=$PACKAGE_BUILD_DIR/$ARCHIVE_DIR/RELEASE
 echo "PRODUCT = $PRODUCT" > $RELEASEFILE 
 echo "VERSION = $VERSION $RELEASE" >> $RELEASEFILE 
 echo "BUILDDATE = `date`" >> $RELEASEFILE 
@@ -112,9 +115,9 @@ echo "BUILDHOST = `hostname -f`" >> $RELEASEFILE
 # --
 # cleanup
 # --
-cd /tmp/otrs_build/$ARCHIVE_DIR/ || exit 1;
+cd $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ || exit 1;
 # remove CVS dirs
-find /tmp/otrs_build/$ARCHIVE_DIR/ -name CVS | xargs rm -rf || exit 1;
+find $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ -name CVS | xargs rm -rf || exit 1;
 # remove old sessions, articles and spool
 rm -f var/sessions/*
 rm -rf var/article/*
@@ -129,73 +132,82 @@ rm -rf doc/screenshots
 # --
 # create tar
 # --
-cd /tmp/otrs_build/ || exit 1;
+cd $PACKAGE_BUILD_DIR/ || exit 1;
 SOURCE_LOCATION=$SYSTEM_SOURCE_DIR/$PACKAGE-$VERSION-$RELEASE.tar.gz
 tar -czf $SOURCE_LOCATION $ARCHIVE_DIR/ $ARCHIVE_DIR/.*rc || exit 1;
-cp $SOURCE_LOCATION $OTRS_PACKAGES/
+cp $SOURCE_LOCATION $PACKAGE_DEST_DIR/
 
 # --
 # create bzip2
 # --
-cd /tmp/otrs_build/ || exit 1;
+cd $PACKAGE_BUILD_DIR/ || exit 1;
 SOURCE_LOCATION=$SYSTEM_SOURCE_DIR/$PACKAGE-$VERSION-$RELEASE.tar.bz2
 tar -cjf $SOURCE_LOCATION $ARCHIVE_DIR/ $ARCHIVE_DIR/.*rc || exit 1;
-cp $SOURCE_LOCATION $OTRS_PACKAGES/
+cp $SOURCE_LOCATION $PACKAGE_DEST_DIR/
 
 
 # --
 # build SuSE 8.x rpm 
 # --
-specfile=/tmp/specfile$$
+specfile=$PACKAGE_TMP_SPEC
 cat $ARCHIVE_DIR/scripts/suse-otrs.spec | sed "s/^Version:.*/Version:      $VERSION/" | sed "s/^Release:.*/Release:      $RELEASE/" > $specfile
 rpm -ba --clean $specfile || exit 1;
 rm -f $specfile
 
-mv $SYSTEM_RPM_DIR/*/$PACKAGE*$VERSION*$RELEASE*.rpm $OTRS_PACKAGES/RPM/suse/
-mv $SYSTEM_SRPM_DIR/$PACKAGE*$VERSION*$RELEASE*.src.rpm $OTRS_PACKAGES/SRPM/suse/
+mv $SYSTEM_RPM_DIR/*/$PACKAGE*$VERSION*$RELEASE*.rpm $PACKAGE_DEST_DIR/RPM/suse/
+mv $SYSTEM_SRPM_DIR/$PACKAGE*$VERSION*$RELEASE*.src.rpm $PACKAGE_DEST_DIR/SRPM/suse/
 
 # --
 # build SuSE 7.3 rpm 
 # --
-specfile=/tmp/specfile$$
+specfile=$PACKAGE_TMP_SPEC
 cat $ARCHIVE_DIR/scripts/suse-otrs-7.3.spec | sed "s/^Version:.*/Version:      $VERSION/" | sed "s/^Release:.*/Release:      $RELEASE/" > $specfile
 rpm -ba --clean $specfile || exit 1;
 rm -f $specfile
 
-mv $SYSTEM_RPM_DIR/*/$PACKAGE*$VERSION*$RELEASE*.rpm $OTRS_PACKAGES/RPM/suse/
-mv $SYSTEM_SRPM_DIR/$PACKAGE*$VERSION*$RELEASE*.src.rpm $OTRS_PACKAGES/SRPM/suse/
+mv $SYSTEM_RPM_DIR/*/$PACKAGE*$VERSION*$RELEASE*.rpm $PACKAGE_DEST_DIR/RPM/suse/
+mv $SYSTEM_SRPM_DIR/$PACKAGE*$VERSION*$RELEASE*.src.rpm $PACKAGE_DEST_DIR/SRPM/suse/
 
 # --
 # build Redhat 7.x rpm
 # --
 cp $ARCHIVE_DIR/scripts/redhat-rpmmacros ~/.rpmmacros
-specfile=/tmp/specfile$$
+specfile=$PACKAGE_TMP_SPEC
 cat $ARCHIVE_DIR/scripts/redhat-otrs.spec | sed "s/^Version:.*/Version:      $VERSION/" | sed "s/^Release:.*/Release:      $RELEASE/" > $specfile
 rpm -ba --clean $specfile || exit 1;
 rm -f $specfile
 rm -f ~/.rpmmacros
 
-mv $SYSTEM_RPM_DIR/*/$PACKAGE*$VERSION*$RELEASE*.rpm $OTRS_PACKAGES/RPM/redhat/
-mv $SYSTEM_SRPM_DIR/$PACKAGE*$VERSION*$RELEASE*.src.rpm $OTRS_PACKAGES/SRPM/redhat/
+mv $SYSTEM_RPM_DIR/*/$PACKAGE*$VERSION*$RELEASE*.rpm $PACKAGE_DEST_DIR/RPM/redhat/
+mv $SYSTEM_SRPM_DIR/$PACKAGE*$VERSION*$RELEASE*.src.rpm $PACKAGE_DEST_DIR/SRPM/redhat/
 
 # --
 # stats
 # --
 echo "-----------------------------------------------------------------";
 echo -n "Source code lines (*.sh) : "
-find /tmp/otrs_build/$ARCHIVE_DIR/ -name *.sh | xargs cat | wc -l
+find $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ -name *.sh | xargs cat | wc -l
 echo -n "Source code lines (*.pl) : "
-find /tmp/otrs_build/$ARCHIVE_DIR/ -name *.pl | xargs cat | wc -l
+find $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ -name *.pl | xargs cat | wc -l
 echo -n "Source code lines (*.pm) : "
-find /tmp/otrs_build/$ARCHIVE_DIR/ -name *.pm | xargs cat | wc -l
+find $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ -name *.pm | xargs cat | wc -l
 echo -n "Source code lines (*.dtl): "
-find /tmp/otrs_build/$ARCHIVE_DIR/ -name *.dtl | xargs cat | wc -l
+find $PACKAGE_BUILD_DIR/$ARCHIVE_DIR/ -name *.dtl | xargs cat | wc -l
 echo "-----------------------------------------------------------------";
-echo "You will find your tar.gz, RPMs and SRPMs in $OTRS_PACKAGES";
-ls -lR $OTRS_PACKAGES | grep $PACKAGE
+echo "You will find your tar.gz, RPMs and SRPMs in $PACKAGE_DEST_DIR";
+cd $PACKAGE_DEST_DIR
+find . -name "*$PACKAGE*" | xargs ls -lo
+echo "-----------------------------------------------------------------";
+if which md5sum >> /dev/null; then
+    echo "MD5 message digest (128-bit) checksums";
+    find . -name "*$PACKAGE*" | xargs md5sum
+else
+    echo "No md5sum found in \$PATH!"
+fi
 echo "-----------------------------------------------------------------";
 
 # --
 # cleanup
 # --
-rm -rf /tmp/otrs_build/
+rm -rf $PACKAGE_BUILD_DIR
+rm -rf $PACKAGE_TMP_SPEC
