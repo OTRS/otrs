3e7920d34c8894175d427ed6ff52552918454703
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-10-24 23:57:12 +0000

    improved web installer

--- Kernel/Modules/Installer.pm
+++ Kernel/Modules/Installer.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/Installer.pm - provides the DB installer
 # Copyright (C) 2001-2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Installer.pm,v 1.14 2002-10-22 12:37:02 martin Exp $
+# $Id: Installer.pm,v 1.15 2002-10-24 23:55:48 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -17,7 +17,7 @@ package Kernel::Modules::Installer;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.14 $';
+$VERSION = '$Revision: 1.15 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -46,6 +46,9 @@ sub Run {
     my %Param = @_;
     my $Output = '';
     my $Subaction = $Self->{Subaction} || ''; 
+    # --
+    # get sql source
+    # --
     my $DirOfSQLFiles = '../../install/database';
     if (! -d $DirOfSQLFiles) {
         $DirOfSQLFiles = '/usr/share/doc/packages/otrs/install/database';
@@ -56,14 +59,46 @@ sub Run {
             }
         }
     }
-
+    # --
+    # check dist
+    # --
+    my %Dist = ();
+    $Dist{Vendor} = "Unix/Linux";
+    $Dist{Webserver} = "no webserver detected, restart your webserver";
+    if (-f "/etc/SuSE-release") {
+        $Dist{Vendor} = "SuSE";
+        $Dist{Webserver} = "rcapache restart";
+    } 
+    if (-f "/etc/redhat-release") {
+        $Dist{Vendor} = "Redhat";
+        $Dist{Webserver} = "service httpd restart";
+    } 
+    
+    # --
     # print form
+    # --
     if ($Subaction eq '' || !$Subaction) {
-        $Output .= $Self->{LayoutObject}->Header();
-        $Output .= $Self->{LayoutObject}->InstallerStart();
-        $Output .= $Self->{LayoutObject}->Footer();
+        if ($Self->ReConfigure()) {
+           $Output .= $Self->{LayoutObject}->Header();
+           $Output .= $Self->{LayoutObject}->Warning(
+               Message => "Kernel/Config.pm isn't writable!",
+               Comment => 'If you want to use the installer, set the '.
+                 'Kernel/Config.pm writable for the werserver user!',
+           );
+           $Output .= $Self->{LayoutObject}->Footer();
+        }
+        else { 
+           $Output .= $Self->{LayoutObject}->Header();
+           $Output .= $Self->{LayoutObject}->InstallerStart(
+               Item => 'create database',
+               Step => '1/3',
+           );
+           $Output .= $Self->{LayoutObject}->Footer();
+        }
     }
-    # do select
+    # --
+    # do dem settings
+    # --
     elsif ($Subaction eq 'DB') {
         $Output .= $Self->{LayoutObject}->Header();
         # get params
@@ -95,66 +130,128 @@ sub Run {
 
         if ($DB{DBAction} eq 'Create') {
           # FIXME !!! use $DB{Type}!!!
-            # db
+            # --
+            # create db
+            # --
             my $CMD = $MYCMD . " -e 'create database $DB{Database}'";
-            $SetupOutput .= "<table border=\"0\">";
+            $SetupOutput .= "<table border='0' width='100%'>";
             $SetupOutput .= "<tr>";
             $SetupOutput .= "<td colspan=\"2\"><b>Create database CMD:</b></td></tr>";
 
             $SetupOutput .= "<tr><td>$CMD</td>";
-            $SetupOutput .= $Self->SystemCall($CMD);
+            my $CMDReturn = $Self->SystemCall($CMD);
             if (!$Self->{$CMD}) {
                 $SetupOutput .= "<td><font color='red'><b>false! :-(</b></font></td>";
-                $Output .= $SetupOutput . $Self->{LayoutObject}->Footer();
+                $SetupOutput .= "</table>";
+                $SetupOutput .= "$CMDReturn";
+                $Output .= $Self->{LayoutObject}->InstallerBody(
+                    Item => 'create database',
+                    Step => '1/3',
+                    Body => $SetupOutput,
+                ); 
+                $Output .= $Self->{LayoutObject}->Footer();
                 return $Output;
             }
             else {
                 $SetupOutput .= "<td><font color='green'><b>done.</b></font></td>";
             }
             $SetupOutput .= "</tr>";
-            # tables
+            # --
+            # create db tables
+            # --
             $CMD = $MYCMD . " $DB{Database} < $DirOfSQLFiles/OpenTRS-schema.mysql.sql";
             $SetupOutput .= "<tr>";
             $SetupOutput .= "<td colspan=\"2\"><b>Create tables CMD:</B></td></tr>";
             $SetupOutput .= "<tr><td>$CMD</td>";
-            $SetupOutput .= $Self->SystemCall($CMD);
+            $CMDReturn = $Self->SystemCall($CMD);
             if (!$Self->{$CMD}) {
                 $SetupOutput .= "<td><font color='red'><b>false! :-(</b></font></td>";
-                $SetupOutput .= $SetupOutput . $Self->{LayoutObject}->Footer();
+                $SetupOutput .= "</table>";
+                $SetupOutput .= "$CMDReturn";
+                $Output .= $Self->{LayoutObject}->InstallerBody(
+                    Item => 'create database',
+                    Step => '1/3',
+                    Body => $SetupOutput,
+                ); 
+                $Output .= $Self->{LayoutObject}->Footer();
                 return $Output;
             }
             else {
                 $SetupOutput .= "<td><font color='green'><b>done.</b></font></td>";
             } 
             $SetupOutput .= "</tr>";
-
+            # --
             # inital insert
+            # --
             $CMD = $MYCMD . " $DB{Database} < $DirOfSQLFiles/initial_insert.sql";
 
             $SetupOutput .= "<tr>";
             $SetupOutput .= "<td colspan=\"2\"><b>Inital inserts CMD:</b></td></tr>";
             $SetupOutput .= "<tr><td>$CMD </td>";
-            $SetupOutput .= $Self->SystemCall($CMD);
+            $CMDReturn = $Self->SystemCall($CMD);
             if (!$Self->{$CMD}) {
                 $SetupOutput .= "<td><font color='red'><b>false! :-(</b></font></td>";
-                $Output .= $SetupOutput . $Self->{LayoutObject}->Footer();
+                $SetupOutput .= "</table>";
+                $SetupOutput .= "$CMDReturn";
+                $Output .= $Self->{LayoutObject}->InstallerBody(
+                    Item => 'create database',
+                    Step => '1/3',
+                    Body => $SetupOutput,
+                ); 
+                $Output .= $Self->{LayoutObject}->Footer();
                 return $Output;
             }
             else {
                 $SetupOutput .= "<td><font color='green'><b>done.</b></font></td>";
             }
             $SetupOutput .= "</tr>";
-
+            # --
             # user add
+            # --
             $CMD = $MYCMD . " -e \"GRANT ALL PRIVILEGES ON $DB{Database}.* TO ".
                   " $DB{DatabaseUser}\@$DB{NewHost} IDENTIFIED BY '$DB{DatabasePw}' WITH GRANT OPTION\" ";
             $SetupOutput .= "<tr>";
             $SetupOutput .= "<td colspan=\"2\"><b>Create db user CMD:</b></td></tr>";
             $SetupOutput .= "<tr><td>$CMD </td>";
-            $SetupOutput .= $Self->SystemCall($CMD);
+            $CMDReturn = $Self->SystemCall($CMD);
             if (!$Self->{$CMD}) {
                 $SetupOutput .= "<td><font color='red'><b>false! :-(</b></font></td>";
-                $Output .= $SetupOutput . $Self->{LayoutObject}->Footer();
+                $SetupOutput .= "</table>";
+                $SetupOutput .= "$CMDReturn";
+                $Output .= $Self->{LayoutObject}->InstallerBody(
+                    Item => 'create database',
+                    Step => '1/3',
+                    Body => $SetupOutput,
+                ); 
+                $Output .= $Self->{LayoutObject}->Footer();
+                return $Output;
+            }
+            else {
+                $SetupOutput .= "<td><font color='green'><b>done.</b></font></td>";
+            }
+            $SetupOutput .= "</tr>";
+            # --
+            # Reload the grant tables of your mysql-daemon
+            # --
+            my $MYReloadCMD = "mysqladmin -u $DB{User} ";
+            $MYReloadCMD .= " -p$DB{Password} " if ($DB{Password});
+            $MYReloadCMD .= " -h $DB{DatabaseHost}  ";
+            $MYReloadCMD .= " reload ";
+
+            $SetupOutput .= "<tr>";
+            $SetupOutput .= "<td colspan=\"2\"><b>Reload the grant tables CMD:</b></td></tr>";
+            $SetupOutput .= "<tr><td>$MYReloadCMD </td>";
+            $CMDReturn = $Self->SystemCall($MYReloadCMD);
+            if (!$Self->{$MYReloadCMD}) {
+                $SetupOutput .= "<td><font color='red'><b>false! :-(</b></font></td>";
+                $SetupOutput .= "</table>";
+                $SetupOutput .= "$CMDReturn";
+                $Output .= $Self->{LayoutObject}->InstallerBody(
+                    Item => 'create database',
+                    Step => '1/3',
+                    Body => $SetupOutput,
+                ); 
+                $Output .= $Self->{LayoutObject}->Footer();
                 return $Output;
             }
             else {
@@ -163,46 +260,55 @@ sub Run {
             $SetupOutput .= "</tr>";
             $SetupOutput .= "</table>";
 
-
             # --
             # ReConfigure Config.pm
             # --
-            if ($Self->ReConfigure(%DB)) {
-                $SetupOutput .= "<u>Can't write Config.pm - please do some changes:</u><br>";
-                $SetupOutput .= "\$Self->{DatabaseHost} = '$DB{DatabaseHost}';<br>";
-                $SetupOutput .= "\$Self->{Database} = '$DB{Database}';<br>";
-                $SetupOutput .= "\$Self->{DatabaseUser} = '$DB{DatabaseUser}';<br>";
-                $SetupOutput .= "\$Self->{DatabasePw} = '$DB{DatabasePw}';<br>";
-                $SetupOutput .= "\$Self->{SecureMode} = 1;<br>";
-
+            if ($Self->ReConfigure(
+                DatabaseHost => $DB{DatabaseHost},
+                Database => $DB{Database},
+                DatabaseUser => $DB{DatabaseUser},
+                DatabasePw => $DB{DatabasePw},
+            )) {
+                $SetupOutput .= "<u>Can't write Config.pm!!! Fatal Error!:</u><br>";
+                $Output .= $SetupOutput;
             }
             else {
-                $SetupOutput .= "<br>To be able to use OTRS you have to enter the following line in your command line (Terminal/Shell):<br>"; 
-                $SetupOutput .= "<b><font color='red'><!--/opt/OpenTRS/bin/SetPermissions.sh /opt/OpenTRS otrs wwwrun<br>-->";
-                $SetupOutput .= "'rcapache restart' (SuSE)</bont> or <font color='red'>'service httpd restart' (Redhat)</font></b><br>";
-                $SetupOutput .= "<br>After doing so your OTRS is up and running.";
+                $SetupOutput .= "<p><b><font color='green'>Database setup successful!</font></b></p>";
+                $SetupOutput .= "<p><a href='installer.pl?Subaction=System'>Next Step</a></p>";
+                $Output .= $Self->{LayoutObject}->InstallerBody(
+                    Item => 'create database',
+                    Step => '1/3',
+                    Body => $SetupOutput,
+                ); 
             }
-
-            $Output .= $Self->{LayoutObject}->InstallerFinish(Ready => 1, Setup => $SetupOutput, %DB);
 
         }
         elsif ($DB{DBAction} eq 'Delete') {
           # FIXME !!! use $DB{Type}!!!
+            # --
             # drop database
+            # --
             my $CMD .=  $MYCMD . " -e 'drop database $DB{Database}'"; 
             $SetupOutput .= "<b>Drop database CMD:</b> $CMD ";
             $SetupOutput .= $Self->SystemCall($CMD);
             if (!$Self->{$CMD}) {
                 $SetupOutput .= "<font color='red'><b>false</b>.</font>";
-                $Output .= $SetupOutput . $Self->{LayoutObject}->Footer();
+                $Output .= $Self->{LayoutObject}->InstallerBody(
+                    Item => 'drop database',
+                    Step => '3/3',
+                    Body => $SetupOutput,
+                );
+                $Output .= $Self->{LayoutObject}->Footer();
                 return $Output;
             }
             else {
                 $SetupOutput .= "<b>done.</b><br><br> <b>Database deleted.</b>";
             }
-            # user delete
-
-            $Output .= $Self->{LayoutObject}->InstallerFinish(Setup => $SetupOutput, %DB);
+           $Output .= $Self->{LayoutObject}->InstallerBody(
+               Item => 'drop database',
+               Step => '3/3',
+               Body => $SetupOutput,
+           );
         }
         else {
             $Output .= $Self->{LayoutObject}->Error(
@@ -211,7 +317,60 @@ sub Run {
         }
         $Output .= $Self->{LayoutObject}->Footer();
     }
+    # --
+    # do system settings
+    # --
+    elsif ($Subaction eq 'System') {
+           $Output .= $Self->{LayoutObject}->Header();
+           $Output .= $Self->{LayoutObject}->InstallerBody(
+               Item => 'system settings',
+               Step => '2/3',
+               Body => $Self->{LayoutObject}->InstallerSystem(),
+           );
+           $Output .= $Self->{LayoutObject}->Footer();
+    }
+    # --
+    # do system settings action
+    # --
+    elsif ($Subaction eq 'Finish') {
+        $Output .= $Self->{LayoutObject}->Header();
+        # --
+        # ReConfigure Config.pm
+        # --
+        my %Config = ();
+        foreach (qw(SystemID FQDN AdminEmail Organization LogModule LogModule::LogFile 
+          TicketHook TicketNumberGenerator DefaultCharset DefaultLanguage)) {
+            $Config{$_} = $Self->{ParamObject}->GetParam(Param => $_) || '';
+        }
+        if ($Self->ReConfigure(
+            %Config,
+            SecureMode => 1,
+        )) {
+             $Output .= "<u>Can't write Config.pm - Fatal Error!</u><br>";
+        }
+        else {
+           my $SetPermission = $ENV{SCRIPT_FILENAME} || '/opt/OpenTRS/bin/SetPermissions.sh';
+           $SetPermission =~ s/(.+?)\/cgi-bin\/installer.pl/$1\/SetPermissions.sh/g;
+           my $BaseDir = $SetPermission;
+           $BaseDir =~ s/(.*\/)bin\/SetPermissions.sh/$1/;
+           my $OTRSHandle = $ENV{SCRIPT_NAME};
+           $OTRSHandle =~ s/\/(.*)\/installer\.pl/$1/;
+           $Output .= $Self->{LayoutObject}->InstallerBody(
+               Item => 'finish',
+               Step => '3/3',
+               Body => $Self->{LayoutObject}->InstallerFinish(
+                   SetPermission => $SetPermission,
+                   BaseDir => $BaseDir,
+                   OTRSHandle => $OTRSHandle,
+                   %Dist,
+               ),
+           );
+        }
+        $Output .= $Self->{LayoutObject}->Footer();
+    }
+    # --
     # else! error!
+    # --
     else {
         $Output .= $Self->{LayoutObject}->Header();
         $Output .= $Self->{LayoutObject}->Error(
@@ -243,17 +402,42 @@ sub ReConfigure {
     my $Self = shift;
     my %Param = @_;
     my $Config = '';
+    # --
+    # perl quote
+    # --
+    foreach (keys %Param) {
+        if ($Param{$_}) {
+            $Param{$_} =~ s/'/\\'/g;
+        }
+    }
+    # --
+    # read config file
+    # --
     open (IN, "< ../../Kernel/Config.pm") || return "Can't open ../../Kernel/Config.pm: $!";
     while (<IN>) {
-        $Config .= $_;
+        if ($_ =~ /^#/) {
+            $Config .= $_;
+        }
+        else {
+            my $NewConfig = $_;
+            # --
+            # replace config with %Param
+            # --
+            foreach (keys %Param) {
+                if ($Param{$_} =~ /^[0-9]+$/) {
+                    $NewConfig =~ s/(\$Self->{$_} =.+?);/\$Self->{$_} = $Param{$_};/g;  
+                }
+                else {
+                    $NewConfig =~ s/(\$Self->{$_} =.+?');/\$Self->{$_} = '$Param{$_}';/g;  
+                }
+            }
+            $Config .= $NewConfig;
+        }
     }
     close (IN);
-    $Config =~ s/(\$Self->{DatabaseHost} =.*');/\$Self->{DatabaseHost} = '$Param{DatabaseHost}';/g;  
-    $Config =~ s/(\$Self->{Database} =.*');/\$Self->{Database} = '$Param{Database}';/g;
-    $Config =~ s/(\$Self->{DatabaseUser} =.*');/\$Self->{DatabaseUser} = '$Param{DatabaseUser}';/g;
-    $Config =~ s/(\$Self->{DatabasePw} =.*');/\$Self->{DatabasePw} = '$Param{DatabasePw}';/g;
-    $Config =~ s/(\$Self->{SecureMode} =.*);/\$Self->{SecureMode} = 1;/g;
-
+    # --
+    # write new config file
+    # --
     open (OUT, "> ../../Kernel/Config.pm") || return "Can't open ../../Kernel/Config.pm: $!";
     print OUT $Config;
     close (OUT); 
--- Kernel/Output/HTML/Installer.pm
+++ Kernel/Output/HTML/Installer.pm
@@ -2,7 +2,7 @@
 # HTML/Installer.pm - provides installer HTML output
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Installer.pm,v 1.1 2002-02-03 18:01:43 martin Exp $
+# $Id: Installer.pm,v 1.2 2002-10-24 23:55:48 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Output::HTML::Installer;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.1 $';
+$VERSION = '$Revision: 1.2 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -23,7 +23,18 @@ sub InstallerStart {
     my %Param = @_;
 
     # create & return output
-    return $Self->Output(TemplateFile => 'InstallerStart', Data => \%Param);
+    return $Self->InstallerBody(
+        %Param,
+        Body => $Self->Output(TemplateFile => 'InstallerStart', Data => \%Param),
+    );
+}
+# --
+sub InstallerSystem {
+    my $Self = shift;
+    my %Param = @_;
+
+    # create & return output
+    return $Self->Output(TemplateFile => 'InstallerSystem', Data => \%Param);
 }
 # --
 sub InstallerFinish {
@@ -34,7 +45,14 @@ sub InstallerFinish {
     return $Self->Output(TemplateFile => 'InstallerFinish', Data => \%Param);
 }
 # --
+sub InstallerBody {
+    my $Self = shift;
+    my %Param = @_;
 
+    # create & return output
+    return $Self->Output(TemplateFile => 'InstallerBody', Data => \%Param);
+}
+# --
 
 1;
  
