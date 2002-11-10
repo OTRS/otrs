eedb5e165e302a2a5d0b506981b619e132db614a
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-11-10 23:57:45 +0000

    new

---
+++ Kernel/Modules/AgentSpelling.pm
@@ -0,0 +1,105 @@
+# --
+# Kernel/Modules/AgentSpelling.pm - spelling module
+# Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
+# --
+# $Id: AgentSpelling.pm,v 1.1 2002-11-10 23:54:29 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::Modules::AgentSpelling;
+
+use strict;
+use Kernel::System::Spelling;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
+
+# --
+sub new {
+    my $Type = shift;
+    my %Param = @_;
+   
+    # allocate new hash for object 
+    my $Self = {}; 
+    bless ($Self, $Type);
+    
+    # get common opjects
+    foreach (keys %Param) {
+        $Self->{$_} = $Param{$_};
+    }
+
+    # check all needed objects
+    foreach (
+      'TicketObject',
+      'ParamObject', 
+      'DBObject', 
+      'QueueObject', 
+      'LayoutObject', 
+      'ConfigObject', 
+      'LogObject',
+    ) {
+        die "Got no $_" if (!$Self->{$_});
+    }
+
+    $Self->{SpellingObject} = Kernel::System::Spelling->new(%Param);
+    # --
+    # get params
+    # --
+    foreach (qw(Body)) {
+        my $Value = $Self->{ParamObject}->GetParam(Param => $_);
+        $Self->{$_} = defined $Value ? $Value : '';
+    }
+    return $Self;
+}
+# --
+sub Run {
+    my $Self = shift;
+    my %Param = @_;
+    my $Output;
+    
+    $Param{Body} = $Self->{ParamObject}->GetParam(Param => 'Body');
+    $Param{SpellLanguage} = $Self->{ParamObject}->GetParam(Param => 'SpellLanguage');
+    # --
+    # get all wrong words
+    # --
+    my %Words = ();
+    foreach ($Self->{ParamObject}->GetArray(Param => 'SpellCheckReplace')) { 
+        my ($Word, $Value) = split(/::/, $_);
+        my $OrWord = $Self->{ParamObject}->GetParam(Param => "SpellCheckOrReplace::$Word") || '';
+        if ($OrWord) { 
+            $Value = $OrWord;
+        }
+        $Words{$Word} = $Value;
+    }
+    # --
+    # replace all wrong words
+    # --
+    foreach (keys %Words) {
+        if ($Words{$_} && $Self->{ParamObject}->GetParam(Param => "SpellCheck::$_") eq "Replace") {
+            $Param{Body} =~ s/^$_ /$Words{$_} /g;
+            $Param{Body} =~ s/ $_$/ $Words{$_}/g;
+            $Param{Body} =~ s/(\s)$_(\s|:|;|<|>|\/|\|\.|\!|%|&|\?)/$1$Words{$_}$2/gs;
+        }
+    }
+    my %SpellCheck = $Self->{SpellingObject}->Ckeck(
+        Text => $Param{Body},
+        SpellLanguage => $Param{SpellLanguage},
+    );
+    # -- 
+    # start with page ...
+    # --
+    $Output .= $Self->{LayoutObject}->Header(Title => 'Spell Checker');
+    $Output .= $Self->{LayoutObject}->AgentSpelling(
+        SpellCheck => \%SpellCheck,
+        %Param,
+    );
+    $Output .= $Self->{LayoutObject}->Footer();
+    return $Output;
+}
+# --
+
+1;
---
+++ Kernel/System/Spelling.pm
@@ -0,0 +1,143 @@
+# --
+# Kernel/System/Spelling.pm - the global spellinf module
+# Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
+# --
+# $Id: Spelling.pm,v 1.1 2002-11-10 23:54:55 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::System::Spelling;
+
+use strict;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
+
+# --
+sub new {
+    my $Type = shift;
+    my %Param = @_;
+
+    # allocate new hash for object
+    my $Self = {}; 
+    bless ($Self, $Type);
+
+    $Self->{Debug} = 0;
+
+    # --
+    # get needed objects
+    # --
+    foreach (qw(ConfigObject LogObject)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    $Self->{SpellChecker} = 'ispell -a ';
+#    $Self->{SpellChecker} = 'ispell -a -d deutsch';
+
+    return $Self;
+}
+# --
+sub Ckeck {
+    my $Self = shift;
+    my %Param = @_;
+    # --
+    # check needed stuff
+    # --
+    foreach (qw(Text)) {
+      if (!$Param{$_}) {
+        $Self->{LogObject}->Log(Priority => 'error', Message => "Need $_!");
+        return;
+      }
+    }
+    # --
+    # get needed dict
+    # --
+    my %Languages = ( 'english' => 'English', 'deutsch' => 'German' );
+    if ($Param{SpellLanguage}) {
+        foreach (keys %Languages) {
+            if ($Param{SpellLanguage} =~ /$Languages{$_}/i) {
+                $Self->{SpellChecker} .= " -d $_";    
+            }
+        }
+    }
+    # --
+    # ignored words
+    # --
+    my @Ignore = qw(com org de net Cc www Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+      Fwd DNS http CC ca html tm COM mmunity Co op https netscape webmail bcc jpg gif 
+      email Tel ie eg otrs suse redhat debian caldera php perl java unsubscribe);
+    # don't correct emails
+    $Param{Text} =~ s/<.+?\@.+?>//g;
+    $Param{Text} =~ s/\s.+?\@.+?\s/ /g;
+    # don't correct quoted text
+    $Param{Text} =~ s/^>.*$//gm;
+    # --
+    # get spell output
+    # --
+    if (open (SPELL, "echo \"$Param{Text}\" | $Self->{SpellChecker} |")) {
+        my $Output = '';
+        my %Data = ();
+        my $Lines = 1;
+        my $CurrentLine = 0;
+        while (<SPELL>) {
+            $CurrentLine++;
+            if ($_ =~ /^# (.+?) .+?$/) {
+                $Data{$CurrentLine} = {
+                    Word => $1, 
+                    Replace => '', 
+                    Line => $Lines,
+                };
+            }
+            elsif ($_ =~ /^& (.+?) .+?: (.*)$/) {
+                my @Replace = split(/, /, $2);
+                $Data{$CurrentLine} = {
+                    Word => $1, 
+                    Replace => \@Replace, 
+                    Line => $Lines,
+                };
+            }
+            elsif ($_ =~ /^\n$/) {
+                $Lines++;
+            }
+        }
+        # --
+        # drop double words and add line of double word
+        # --
+        my %DoubleWords;
+        foreach (sort {$a <=> $b} keys %Data) {
+            if ($DoubleWords{$Data{$_}->{Word}}) {
+                $DoubleWords{$Data{$_}->{Word}}->{Line} .= "/".$Data{$_}->{Line};
+                delete $Data{$_};
+            }
+            else {
+                $DoubleWords{$Data{$_}->{Word}} = $Data{$_};
+            }
+        }
+        # --
+        # remove ignored words
+        # --
+        foreach (sort keys %Data) {
+            foreach my $IgnoreWord (@Ignore) {
+                if ($Data{$_}->{Word} && $Data{$_}->{Word} =~ /^$IgnoreWord$/i) {
+                    delete $Data{$_};
+                }
+            }
+        }
+        close (SPELL);
+        return %Data;
+    }
+    else {
+        $Self->{LogObject}->Log(Priority => 'error', Message => "Can't open spell: $!");
+        return;
+    } 
+
+    
+}
+# --
+
+
+1;
