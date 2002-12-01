1631cb6858c0cf464b328cb33a495c228b85b018
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-12-01 14:38:04 +0000

    added LanguageString from ConfigObject! Not longer hard coded

--- Kernel/Output/HTML/Installer.pm
+++ Kernel/Output/HTML/Installer.pm
@@ -2,7 +2,7 @@
 # HTML/Installer.pm - provides installer HTML output
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Installer.pm,v 1.2 2002-10-24 23:55:48 martin Exp $
+# $Id: Installer.pm,v 1.3 2002-12-01 14:38:04 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Output::HTML::Installer;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.2 $';
+$VERSION = '$Revision: 1.3 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -32,6 +32,12 @@ sub InstallerStart {
 sub InstallerSystem {
     my $Self = shift;
     my %Param = @_;
+
+    $Param{LanguageString} = $Self->OptionStrgHashRef(
+        Data => $Self->{ConfigObject}->Get('DefaultUsedLanguages'),
+        Name => 'DefaultLanguage',
+        SelectedID => $Self->{UserLanguage},
+    ); 
 
     # create & return output
     return $Self->Output(TemplateFile => 'InstallerSystem', Data => \%Param);
