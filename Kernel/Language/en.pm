cd8adb903c7b73ea72b5aaa94a6ed660ee68e5dc
Martin Edenhofer <martin.edenhofer@otrs.com>
2002-12-01 16:24:17 +0000

    added possible charsets!

--- Kernel/Language/en.pm
+++ Kernel/Language/en.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/en.pm - provides en languag translation
 # Copyright (C) 2001 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: en.pm,v 1.1 2002-11-24 23:54:47 martin Exp $
+# $Id: en.pm,v 1.2 2002-12-01 16:24:17 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -13,13 +13,16 @@ package Kernel::Language::en;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.1 $';
+$VERSION = '$Revision: 1.2 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
 sub Data {
     my $Self = shift;
     my %Param = @_;
+
+    # possible charsets
+    $Self->{Charset} = ['us-ascii', 'UTF-8', 'iso-8859-1', 'iso-8859-15'];
 
     # maybe nothing ... or help texts
 
