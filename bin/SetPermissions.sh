9b7c650bc1f858daf8127bc0b9f9bbe7f44df76f
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-01-05 16:05:13 +0000

    chamged OpenTRS to otrs

--- bin/SetPermissions.sh
+++ bin/SetPermissions.sh
@@ -3,7 +3,7 @@
 # SetPermissions.sh - to set the otrs permissions 
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: SetPermissions.sh,v 1.14 2002-10-30 16:49:22 martin Exp $
+# $Id: SetPermissions.sh,v 1.15 2003-01-05 16:05:13 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
 
-echo "SetPermissions.sh <\$Revision: 1.14 $> - set OTRS file permissions"
+echo "SetPermissions.sh <\$Revision: 1.15 $> - set OTRS file permissions"
 echo "Copyright (c) 2002 Martin Edenhofer <martin@otrs.org>"
 
 if ! test $1 || ! test $2 || ! test $3; then 
@@ -30,7 +30,7 @@ if ! test $1 || ! test $2 || ! test $3; then
     echo ""
     echo "Usage: SetPermissions.sh <OTRS_HOME> <OTRS_USER> <WEBSERVER_USER> [OTRS_GROUP] [WEB_GROUP]"
     echo ""
-    echo "  Try: SetPermissions.sh /opt/OpenTRS otrs wwwrun"                      
+    echo "  Try: SetPermissions.sh /opt/otrs otrs wwwrun"                      
     echo ""
     exit 1;
 else 
