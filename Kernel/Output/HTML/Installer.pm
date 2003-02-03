70822057d6eeb9fa98f25495658c1631ff8cb63f
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-02-03 21:18:29 +0000

    added selection/option field for SystemID

--- Kernel/Output/HTML/Installer.pm
+++ Kernel/Output/HTML/Installer.pm
@@ -2,7 +2,7 @@
 # HTML/Installer.pm - provides installer HTML output
 # Copyright (C) 2002-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Installer.pm,v 1.4 2003-01-03 00:21:25 martin Exp $
+# $Id: Installer.pm,v 1.5 2003-02-03 21:18:29 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Output::HTML::Installer;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.4 $';
+$VERSION = '$Revision: 1.5 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 # --
@@ -33,6 +33,16 @@ sub InstallerSystem {
     my $Self = shift;
     my %Param = @_;
 
+    my %SystemIDs = ();
+    foreach (1..99) {
+        my $Tmp = sprintf("%02d", $_);
+        $SystemIDs{"$Tmp"} = "$Tmp";
+    }
+    $Param{SystemIDString} = $Self->OptionStrgHashRef(
+        Data => \%SystemIDs, 
+        Name => 'SystemID',
+        SelectedID => $Self->{ConfigObject}->Get('SystemID'),
+    ); 
     $Param{LanguageString} = $Self->OptionStrgHashRef(
         Data => $Self->{ConfigObject}->Get('DefaultUsedLanguages'),
         Name => 'DefaultLanguage',
