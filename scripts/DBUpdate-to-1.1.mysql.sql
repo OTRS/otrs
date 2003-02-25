808b05ea2fef218e5de1370e9cc167aede24f29c
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-02-25 18:48:28 +0000

    added delete of "" queue

--- scripts/DBUpdate-to-1.1.mysql.sql
+++ scripts/DBUpdate-to-1.1.mysql.sql
@@ -2,13 +2,17 @@
 -- Update an existing OTRS database from 1.0 to 1.1 
 -- Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 -- --
--- $Id: DBUpdate-to-1.1.mysql.sql,v 1.1 2003-02-09 20:51:53 martin Exp $
+-- $Id: DBUpdate-to-1.1.mysql.sql,v 1.2 2003-02-25 18:48:28 martin Exp $
 -- --
 --
 -- usage: cat DBUpdate-to-1.1.mysql.sql | mysql -f -u root otrs
 --
 -- --
 
+-- 
+-- delete not needed queue (important for sub queue)
+--
+DELETE FROM queue WHERE name = '';
 --
 -- modify table ticket
 --
