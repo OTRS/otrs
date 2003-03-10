d5de816196f878a4def5f4fa7653627d4e650245
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-03-10 15:01:43 +0000

    added $OTRSDEST/var/tmp/ dir

--- bin/SetPermissions.sh
+++ bin/SetPermissions.sh
@@ -3,7 +3,7 @@
 # SetPermissions.sh - to set the otrs permissions 
 # Copyright (C) 2002 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: SetPermissions.sh,v 1.17 2003-02-15 11:52:11 martin Exp $
+# $Id: SetPermissions.sh,v 1.18 2003-03-10 15:01:43 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
 
-echo "SetPermissions.sh <\$Revision: 1.17 $> - set OTRS file permissions"
+echo "SetPermissions.sh <\$Revision: 1.18 $> - set OTRS file permissions"
 echo "Copyright (c) 2002 Martin Edenhofer <martin@otrs.org>"
 
 if ! test $1 || ! test $2 || ! test $3; then 
@@ -91,6 +91,7 @@ echo "chown -R $OTRSUSER:$WEBGROUP $OTRSDEST/var/"
 chown -R $OTRSUSER:$WEBGROUP $OTRSDEST/var/
 chmod -R 2775 $OTRSDEST/var/article/
 chmod -R 2775 $OTRSDEST/var/log/
+chmod -R 2775 $OTRSDEST/var/tmp/
 
 # set the var/sessions directory to OTRS and webserver user
 echo "chown -R $WEBUSER:$WEBGROUP $OTRSDEST/var/sessions/"
