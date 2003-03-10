20b285052d8e63282d16fdd84400b130bcca9620
wiktor <>
2003-03-10 10:02:04 +0000

    fix loglevel

--- Kernel/System/PostMaster/NewTicket.pm
+++ Kernel/System/PostMaster/NewTicket.pm
@@ -2,7 +2,7 @@
 # Kernel/System/PostMaster/NewTicket.pm - sub part of PostMaster.pm
 # Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: NewTicket.pm,v 1.34 2003-02-16 23:27:19 martin Exp $
+# $Id: NewTicket.pm,v 1.35 2003-03-10 10:02:04 wiktor Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -18,7 +18,7 @@ use Kernel::System::Queue;
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.34 $';
+$VERSION = '$Revision: 1.35 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -124,7 +124,7 @@ sub Run {
         AutoResponseType => $AutoResponseType,
         Queue => $Queue,
     );
-    # --
+    # --    
     # get customer user and no if not given
     # --
     if (!$GetParam{'X-OTRS-CustomerUser'}) {
@@ -157,15 +157,15 @@ sub Run {
             $GetParam{'X-OTRS-CustomerNo'} = $CustomerData{UserCustomerID};
         }
         else {
-            my @EmailAddresses = $Self->{ParseObject}->SplitAddressLine(
-                Line => $GetParam{From},
+        my @EmailAddresses = $Self->{ParseObject}->SplitAddressLine(
+            Line => $GetParam{From},
+        );
+        foreach (@EmailAddresses) {
+            $GetParam{'X-OTRS-CustomerNo'} = $Self->{ParseObject}->GetEmailAddress(
+                Email => $_,
             );
-            foreach (@EmailAddresses) {
-                $GetParam{'X-OTRS-CustomerNo'} = $Self->{ParseObject}->GetEmailAddress(
-                    Email => $_,
-                );
-            }
         }
+    }
     }
     # --    
     # set customer no
@@ -177,11 +177,11 @@ sub Run {
             User => $GetParam{'X-OTRS-CustomerUser'},
             UserID => $InmailUserID,
         );
-        # --
-        # debug
-        # --
-        if ($Self->{Debug} > 0) {
-            print "CustomerID: $GetParam{'X-OTRS-CustomerNo'}\n";
+    # --
+    # debug
+    # --
+    if ($Self->{Debug} > 0) {
+        print "CustomerID: $GetParam{'X-OTRS-CustomerNo'}\n";
             print "CustomerUser: $GetParam{'X-OTRS-CustomerUser'}\n";
         }
     }
@@ -262,6 +262,7 @@ sub Run {
     
     # do log
     $Self->{LogObject}->Log(
+	Priority => 'notice',
         Message => "New Ticket [$NewTn] created (TicketID=$TicketID, " .
         "ArticleID=$ArticleID). $Comment"
     );
