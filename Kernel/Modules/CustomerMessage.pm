2cf4da7551d05271cdc34b8cb77c4d9156521a3e
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-03-24 12:47:56 +0000

    addd fallback config option for new ticket state on follow up

--- Kernel/Modules/CustomerMessage.pm
+++ Kernel/Modules/CustomerMessage.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/CustomerMessage.pm - to handle customer messages
 # Copyright (C) 2002-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: CustomerMessage.pm,v 1.12 2003-03-10 15:03:04 martin Exp $
+# $Id: CustomerMessage.pm,v 1.13 2003-03-24 12:47:56 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -17,7 +17,7 @@ use Kernel::System::Queue;
 use Kernel::System::State;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.12 $';
+$VERSION = '$Revision: 1.13 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -186,7 +186,8 @@ sub Run {
           # --
           # set state
           # --
-          my $State = $Self->{TicketObject}->StateIDLookup(StateID => $StateID) || 'open';
+          my $State = $Self->{TicketObject}->StateIDLookup(StateID => $StateID) || 
+            $Self->{ConfigObject}->Get('CustomerPanelDefaultNextComposeType') || 'open';
           $Self->{TicketObject}->SetState(
             TicketID => $Self->{TicketID},
             ArticleID => $ArticleID,
