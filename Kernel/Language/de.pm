5728734bb1da3441a7fb3138b7f75d9e9e04a263
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-05-19 17:22:50 +0000

    fixed bug 145 - http://bugs.otrs.org/show_bug.cgi?id=145

--- Kernel/Language/de.pm
+++ Kernel/Language/de.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de.pm - provides de language translation
 # Copyright (C) 2002-2003 Martin Edenhofer <martin at otrs.org>
 # --
-# $Id: de.pm,v 1.25.2.2 2003-05-12 12:20:02 martin Exp $
+# $Id: de.pm,v 1.25.2.3 2003-05-19 17:22:50 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -13,7 +13,7 @@ package Kernel::Language::de;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.25.2.2 $';
+$VERSION = '$Revision: 1.25.2.3 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -141,6 +141,7 @@ sub Data {
       'wrote' => 'schrieb',
       'yes' => 'ja',
       'Yes' => 'Ja',
+      'Please go away!' => 'Bitte zurück gehen!',
       'You got new message!' => 'Sie haben eine neue Nachricht bekommen!',
       'You have %s new message(s)!' => 'Sie haben %s neue Nachricht(en) bekommen!',
       'You have %s reminder ticket(s)!' => 'Sie haben %s Erinnerungs-Ticket(s)!',
