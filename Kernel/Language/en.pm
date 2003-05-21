b8c6eaba0211dd53ca816c8debbba2a7e446ffa2
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-05-21 13:31:16 +0000

    changed date format from %M-%D-%Y to %M/%D/%Y

--- Kernel/Language/en.pm
+++ Kernel/Language/en.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/en.pm - provides en languag translation
 # Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: en.pm,v 1.6 2003-02-08 15:07:29 martin Exp $
+# $Id: en.pm,v 1.7 2003-05-21 13:31:16 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -13,7 +13,7 @@ package Kernel::Language::en;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.6 $';
+$VERSION = '$Revision: 1.7 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -27,9 +27,9 @@ sub Data {
     # possible charsets
     $Self->{Charset} = ['us-ascii', 'UTF-8', 'iso-8859-1', 'iso-8859-15'];
 
-    $Self->{DateFormat} = '%M-%D-%Y %T';
+    $Self->{DateFormat} = '%M/%D/%Y %T';
     $Self->{DateFormatLong} = '%A %B %D %T %Y';
-    $Self->{DateInputFormat} = '%M-%D-%Y - %T';
+    $Self->{DateInputFormat} = '%M/%D/%Y - %T';
 
     # maybe nothing ... or help texts
 
