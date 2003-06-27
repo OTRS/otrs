be2a8f7ef81e01d02b251aa3905241e59970b514
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-06-27 08:07:30 +0000

    Fix of J R Matthews for ->login() - need to be defined()!

--- bin/PostMasterPOP3.pl
+++ bin/PostMasterPOP3.pl
@@ -3,7 +3,7 @@
 # PostMasterPOP3.pl - the global eMail handle for email2db
 # Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: PostMasterPOP3.pl,v 1.10 2003-05-18 20:23:09 martin Exp $
+# $Id: PostMasterPOP3.pl,v 1.11 2003-06-27 08:07:30 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin)."/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.10 $';
+$VERSION = '$Revision: 1.11 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 use strict;
@@ -135,7 +135,7 @@ sub FetchMail {
     }
     # authentcation
     my $NOM = $PopObject->login($User, $Password);
-    if (!$NOM) {
+    if (!defined($NOM)) {
         $PopObject->quit();
         $CommonObject{LogObject}->Log(
             Priority => 'error',
