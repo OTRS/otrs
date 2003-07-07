95ee4bd4230f97dcdbc66f5488bad56fc03cb4d7
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-07-07 13:46:11 +0000

    improved support of different databases

--- Kernel/System/DB.pm
+++ Kernel/System/DB.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DB.pm - the global database wrapper to support different databases 
 # Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: DB.pm,v 1.27 2003-05-21 22:41:10 martin Exp $
+# $Id: DB.pm,v 1.28 2003-07-07 13:46:11 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use DBI;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.27 $';
+$VERSION = '$Revision: 1.28 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -30,9 +30,7 @@ sub new {
     # 0=off; 1=updates; 2=+selects; 3=+Connects;
     $Self->{Debug} = 0;
 
-    # --
     # check needed objects
-    # --
     foreach (qw(ConfigObject LogObject)) {
         if ($Param{$_}) {
             $Self->{$_} = $Param{$_};
@@ -41,55 +39,98 @@ sub new {
             die "Got no $_!";
         }
     } 
-    # --
     # get config data
-    # --
     $Self->{DSN}  = $Param{DatabaseDSN} || $Self->{ConfigObject}->Get('DatabaseDSN');
     $Self->{USER} = $Param{DatabaseUser} || $Self->{ConfigObject}->Get('DatabaseUser');
     $Self->{PW}   = $Param{DatabasePw} || $Self->{ConfigObject}->Get('DatabasePw');
-    # --
-    # get database type and functions
-    # --
+    # get database type (auto detection)
     if ($Self->{DSN} =~ /:mysql/i) {
         $Self->{'DB::Type'} = 'mysql';
-        $Self->{'DB::Limit'} = 'limit';
-        $Self->{'DB::DirectBlob'} = 1;
     }
     elsif ($Self->{DSN} =~ /:Pg/i) {
         $Self->{'DB::Type'} = 'postgresql';
-        $Self->{'DB::Limit'} = 'limit';
-        $Self->{'DB::DirectBlob'} = 0;
     }
     elsif ($Self->{DSN} =~ /:db2/i) {
         $Self->{'DB::Type'} = 'db2';
-        $Self->{'DB::Limit'} = 'fetch';
-        $Self->{'DB::DirectBlob'} = 0;
     }
     elsif ($Self->{DSN} =~ /:odbc/i) {
         $Self->{'DB::Type'} = 'odbc';
-        $Self->{'DB::Limit'} = 0;
-        $Self->{'DB::DirectBlob'} = 0;
     }
     else {
-        $Self->{'DB::Type'} = 'unknown';
+        $Self->{'DB::Type'} = 'generic';
+    }
+    # get database type (config option)
+    if ($Self->{ConfigObject}->Get("Database::Type")) {
+        $Self->{'DB::Type'} = $Self->{ConfigObject}->Get("Database::Type");
+    }
+    # set database functions
+    if ($Self->{'DB::Type'} eq 'mysql') {
+        $Self->{'DB::Limit'} = 'limit';
+        $Self->{'DB::DirectBlob'} = 1;
+        $Self->{'DB::QuoteSignle'} = '\\';
+        $Self->{'DB::QuoteBack'} = '\\';
+        $Self->{'DB::Attribute'} = {};
+    }
+    elsif ($Self->{'DB::Type'} eq 'postgresql') {
+        $Self->{'DB::Limit'} = 'limit';
+        $Self->{'DB::DirectBlob'} = 0;
+        $Self->{'DB::QuoteSignle'} = '\\';
+        $Self->{'DB::QuoteBack'} = '\\';
+        $Self->{'DB::Attribute'} = {};
+    }
+    elsif ($Self->{'DB::Type'} eq 'db2') {
+        $Self->{'DB::Limit'} = 'fetch';
+        $Self->{'DB::DirectBlob'} = 0;
+        $Self->{'DB::QuoteSignle'} = '\\';
+        $Self->{'DB::QuoteBack'} = '\\';
+        $Self->{'DB::Attribute'} = {};
+    }
+    elsif ($Self->{'DB::Type'} eq 'sapdb') {
         $Self->{'DB::Limit'} = 0;
         $Self->{'DB::DirectBlob'} = 0;
+        $Self->{'DB::QuoteSignle'} = '\'';
+        $Self->{'DB::QuoteBack'} = '';
+        $Self->{'DB::Attribute'} = {
+            LongTruncOk => 1,
+            LongReadLen => 100*1024,
+        };
+        $Self->{'DB::CurrentTimestamp'} = 'timestamp';
     }
-    # --
-    # do db connect 
-    # --
+    elsif ($Self->{'DB::Type'} eq 'generic') {
+        $Self->{'DB::QuoteSignle'} = '\\';
+        $Self->{'DB::QuoteBack'} = '\\';
+        $Self->{'DB::Limit'} = 0;
+        $Self->{'DB::DirectBlob'} = 0;
+        $Self->{'DB::Attribute'} = {
+            LongTruncOk => 1,
+            LongReadLen => 100*1024,
+        };
+    }
+    else {
+        $Self->{LogObject}->Log(
+          Priority => 'Error',
+          Message => "Unknown database type $Self->{'DB::Type'}! Set config ".
+              "option DB::Type to (mysql|postgresql|db2|sapdb|generic)"
+        );
+        return;
+    }
+    # check/get extra database config options
+    # (overwrite auto detection)
+    foreach (qw(Type Limit DirectBlob Attribute QuoteSingle QuoteBack)) {
+        if (defined($Self->{ConfigObject}->Get("Database::$_"))) {
+            $Self->{"DB::$_"} = $Self->{ConfigObject}->Get("Database::$_");
+        }
+    }
+    # do database connect 
     if (!$Self->Connect()) {
         return;
     }
-    
     return $Self;
 }
 # --
 sub Connect {
     my $Self = shift;
-    # --
     # debug
-    # --
     if ($Self->{Debug} > 2) {
         $Self->{LogObject}->Log(
           Caller => 1,
@@ -97,10 +138,8 @@ sub Connect {
           Message => "DB.pm->Connect: DSN: $Self->{DSN}, User: $Self->{USER}, Pw: $Self->{PW}, DB Type: $Self->{'DB::Type'};",
         );
     }
-    # --
     # db connect
-    # --
-    if (!($Self->{dbh} = DBI->connect("$Self->{DSN}", $Self->{USER}, $Self->{PW}))) { 
+    if (!($Self->{dbh} = DBI->connect("$Self->{DSN}", $Self->{USER}, $Self->{PW}, $Self->{'DB::Attribute'}))) { 
         $Self->{LogObject}->Log(
           Caller => 1,
           Priority => 'Error',
@@ -113,9 +152,7 @@ sub Connect {
 # --
 sub Disconnect {
     my $Self = shift;
-    # --
     # debug
-    # --
     if ($Self->{Debug} > 2) {
         $Self->{LogObject}->Log(
           Caller => 1,
@@ -123,11 +160,8 @@ sub Disconnect {
           Message => "DB.pm->Disconnect",
         );
     }
-    # --
     # do disconnect
-    # --
     $Self->{dbh}->disconnect() if ($Self->{dbh});
-
     return 1;
 }
 # --
@@ -143,11 +177,13 @@ sub Quote {
     if (!defined $Text) {
         return;
     }
-    # --
     # do quote
-    # --
-    $Text =~ s/\\/\\\\/g;
-    $Text =~ s/'/\\'/g;
+    if ($Self->{'DB::QuoteBack'}) {
+        $Text =~ s/\\/$Self->{'DB::QuoteBack'}\\/g;
+    }
+    if ($Self->{'DB::QuoteSignle'}) {
+        $Text =~ s/'/$Self->{'DB::QuoteSignle'}'/g;
+    }
     return $Text;
 }
 # --
@@ -155,9 +191,7 @@ sub Do {
     my $Self = shift;
     my %Param = @_;
     my $SQL = $Param{SQL};
-    # --
     # debug
-    # --
     if ($Self->{Debug} > 0) {
         $Self->{DoCounter}++;
         $Self->{LogObject}->Log(
@@ -166,9 +200,10 @@ sub Do {
           Message => "DB.pm->Do ($Self->{DoCounter}) SQL: '$SQL'",
         );
     }
-    # --
     # doing
-    # --
+    if ($Self->{'DB::CurrentTimestamp'}) {
+        $SQL =~ s/current_timestamp/$Self->{'DB::CurrentTimestamp'}/g;
+    }
     if (!$Self->{dbh}->do($SQL)) {
         $Self->{LogObject}->Log(
           Caller => 1,
@@ -187,9 +222,7 @@ sub Prepare {
     my $Limit = $Param{Limit} || '';
     $Self->{Limit} = 0;
     $Self->{LimitCounter} = 0;
-    # --
     # build finally select query
-    # --
     if ($Limit) {
         if ($Self->{'DB::Limit'} eq 'limit') {
             $SQL .= " LIMIT $Limit";
@@ -201,9 +234,7 @@ sub Prepare {
             $Self->{Limit} = $Limit;
         }
     }
-    # --
     # debug
-    # --
     if ($Self->{Debug} > 1) {
         $Self->{PrepareCounter}++;
         $Self->{LogObject}->Log(
@@ -212,9 +243,7 @@ sub Prepare {
           Message => "DB.pm->Prepare ($Self->{PrepareCounter}/".time().") SQL: '$SQL'",
         );
     }
-    # --
     # do
-    # --
     if (!($Self->{Curser} = $Self->{dbh}->prepare($SQL))) {
         $Self->{LogObject}->Log(
           Caller => 1,
