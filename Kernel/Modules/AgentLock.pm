b0f5ce8d1d0f393e0a32aa556fd639da881481d7
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-07-10 20:37:11 +0000

    fixed bug 171 - No lock check if two Agents try to lock ticket at same time (or later)

--- Kernel/Modules/AgentLock.pm
+++ Kernel/Modules/AgentLock.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentLock.pm - to set or unset a lock for tickets
 # Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentLock.pm,v 1.11 2003-03-10 21:27:12 martin Exp $
+# $Id: AgentLock.pm,v 1.12 2003-07-10 20:37:11 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Modules::AgentLock;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.11 $';
+$VERSION = '$Revision: 1.12 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -75,32 +75,26 @@ sub Run {
     # start with actions
     # --
     if ($Self->{Subaction} eq 'Unlock') {
-        # --
         # check if I'm the owner
-        # --
         my ($OwnerID, $OwnerLogin) = $Self->{TicketObject}->CheckOwner(
             TicketID => $Self->{TicketID},
         );
         if ($OwnerID != $Self->{UserID}) {
             $Output .= $Self->{LayoutObject}->Header(Title => 'Error');
-            $Output .= $Self->{LayoutObject}->Error(
+            $Output .= $Self->{LayoutObject}->Warning(
                 Message => "Sorry, the current owner is $OwnerLogin",
                 Comment => 'Please change the owner first.',
             );
             $Output .= $Self->{LayoutObject}->Footer();
             return $Output;
         }
-        # --
         # set unlock
-        # --
         if ($Self->{TicketObject}->SetLock(
           TicketID => $Self->{TicketID},
           Lock => 'unlock',
           UserID => $Self->{UserID},
         )) {
-          # --
           # redirekt
-          # --
           if ($Self->{QueueID}) {
              return $Self->{LayoutObject}->Redirect(OP => "QueueID=$Self->{QueueID}");
           }
@@ -116,25 +110,32 @@ sub Run {
         } 
     }
     else {
-        # --
+        # check if the agent is ablee to lock
+        if ($Self->{TicketObject}->IsTicketLocked(TicketID => $Self->{TicketID})) {
+            my ($OwnerID, $OwnerLogin) = $Self->{TicketObject}->CheckOwner(
+                TicketID => $Self->{TicketID},
+            );
+            $Output = $Self->{LayoutObject}->Header(Title => 'Error');
+            $Output .= $Self->{LayoutObject}->Warning(
+                Message => "Ticket (ID=$Self->{TicketID}) is locked for $OwnerLogin!",
+                Comment => "Change the owner!",
+            );
+            $Output .= $Self->{LayoutObject}->Footer();
+            return $Output;
+        }
         # set lock
-        # --
         if ($Self->{TicketObject}->SetLock(
           TicketID => $Self->{TicketID},
           Lock => 'lock',
           UserID => $Self->{UserID},
         ) &&
-        # --
 	    # set user id
-        # --
     	$Self->{TicketObject}->SetOwner(
             TicketID => $Self->{TicketID},
 	        UserID => $Self->{UserID},
 	        NewUserID => $Self->{UserID},
         )) {
-          # --
           # redirekt
-          # --
           if ($Self->{QueueID}) {
              return $Self->{LayoutObject}->Redirect(OP => "&QueueID=$Self->{QueueID}");
           }
