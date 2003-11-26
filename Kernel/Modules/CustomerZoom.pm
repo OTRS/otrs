86934d566d29ee1fb70c656416dce4e9bbd95a50
Martin Edenhofer <martin.edenhofer@otrs.com>
2003-11-26 00:52:10 +0000

    moved to Kernel::System::Ticket module and added customer group permission checks

--- Kernel/Modules/CustomerZoom.pm
+++ Kernel/Modules/CustomerZoom.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/CustomerZoom.pm - to get a closer view
-# Copyright (C) 2002-2003 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: CustomerZoom.pm,v 1.11 2003-04-16 21:13:21 martin Exp $
+# $Id: CustomerZoom.pm,v 1.12 2003-11-26 00:52:10 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::State;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.11 $';
+$VERSION = '$Revision: 1.12 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -47,18 +47,22 @@ sub Run {
     my %Param = @_;
     my $Output;
     my $QueueID = $Self->{TicketObject}->GetQueueIDOfTicketID(TicketID => $Self->{TicketID});
-    # --
     # check needed stuff
-    # --
     if (!$Self->{TicketID} || !$QueueID) {
       $Output = $Self->{LayoutObject}->CustomerHeader(Title => 'Error');
       $Output .= $Self->{LayoutObject}->CustomerError();
       $Output .= $Self->{LayoutObject}->CustomerFooter();
       return $Output;
     }
-    # --
+    # check permissions
+    if (!$Self->{TicketObject}->CustomerPermission(
+        Type => 'ro',
+        TicketID => $Self->{TicketID},
+        UserID => $Self->{UserID})) {
+        # error screen, don't show ticket
+        return $Self->{LayoutObject}->CustomerNoPermission(WithHeader => 'yes');
+    }  
     # store last screen
-    # --
     if ($Self->{Subaction} ne 'ShowHTMLeMail') {
       if (!$Self->{SessionObject}->UpdateSessionID(
         SessionID => $Self->{SessionID},
@@ -74,132 +78,20 @@ sub Run {
     # --
     # fetch all std. responses
     # --
-    
-    my %Ticket;
-    $Ticket{TicketID} = $Self->{TicketID};
-    $Ticket{Age} = '?';
+    my %Ticket = $Self->{TicketObject}->GetTicket(TicketID => $Self->{TicketID});
     $Ticket{TmpCounter} = 0;
-    $Ticket{FreeKey1} = '';
-    $Ticket{FreeValue1} = '';
-    $Ticket{FreeKey2} = '';
-    $Ticket{FreeValue2} = '';
     $Ticket{TicketTimeUnits} = $Self->{TicketObject}->GetAccountedTime(
         TicketID => $Ticket{TicketID},
     );
-    # --
-    # grep all atricle of this ticket
-    # --
-    my @ArticleBox;
-    my $SQL = "SELECT sa.id, st.tn, sa.a_from, sa.a_to, sa.a_cc, sa.a_subject, sa.a_body, ".
-    " st.create_time_unix, st.tn, st.user_id, st.ticket_state_id, st.ticket_priority_id, ". 
-    " sa.create_time, stt.name as sender_type, at.name as article_type, ".
-    " su.$Self->{ConfigObject}->{DatabaseUserTableUser}, ".
-    " sl.name as lock_type, sp.name as priority, tsd.name as state, sa.content_path, ".
-    " sq.name as queue, st.create_time as ticket_create_time, ".
-    " sa.a_freekey1, sa.a_freetext1, sa.a_freekey2, sa.a_freetext2, ".
-    " sa.a_freekey3, sa.a_freetext3, st.freekey1, st.freekey2, st.freetext1, ".
-    " st.freetext2, st.customer_id, sq.group_id, st.ticket_answered, sq.escalation_time, ".
-    " sa.a_content_type, sa.incoming_time ".
-    " FROM ".
-    " article sa, ticket st, article_sender_type stt, article_type at, ".
-    " $Self->{ConfigObject}->{DatabaseUserTable} su, ticket_lock_type sl, " .
-    " ticket_priority sp, ticket_state tsd, queue sq " .
-    " WHERE " .
-    " sa.ticket_id = st.id " .
-    " AND " .
-    " sq.id = st.queue_id " .
-    " AND " .
-    " stt.id = sa.article_sender_type_id " .
-    " AND " .
-    " at.id = sa.article_type_id " .
-    " AND " .
-    " sp.id = st.ticket_priority_id " .
-    " AND " .
-    " sl.id = st.ticket_lock_id " .
-    " AND " .
-    " tsd.id = st.ticket_state_id " .
-    " AND " .
-    " sa.ticket_id = $Self->{TicketID} " .
-    " AND " .
-    " su.$Self->{ConfigObject}->{DatabaseUserTableUserID} = st.user_id " .
-    " GROUP BY sa.id, st.tn, sa.a_from, sa.a_to, sa.a_cc, sa.a_subject, sa.a_body, ".
-    " st.create_time_unix, st.tn, st.user_id, st.ticket_state_id, st.ticket_priority_id, ".
-    " sa.create_time, stt.name, at.name, ".
-    " su.$Self->{ConfigObject}->{DatabaseUserTableUser}, ".
-    " sl.name, sp.name, tsd.name, sa.content_path, ".
-    " sq.name, st.create_time, ".
-    " sa.a_freekey1, sa.a_freetext1, sa.a_freekey2, sa.a_freetext2, ".
-    " sa.a_freekey3, sa.a_freetext3, st.freekey1, st.freekey2, st.freetext1, ".
-    " st.freetext2, st.customer_id, sq.group_id, st.ticket_answered, sq.escalation_time, ".
-    " sa.a_content_type, sa.incoming_time ";
-    $Self->{DBObject}->Prepare(SQL => $SQL);
-    while (my $Data = $Self->{DBObject}->FetchrowHashref() ) {
-        # get escalation_time
-        if ($$Data{escalation_time} && $$Data{sender_type} eq 'customer') {
-            $Ticket{TicketOverTime} = (time() - ($$Data{incoming_time} + ($$Data{escalation_time}*60)));
-        }
-        # ticket data
-        $Ticket{TicketNumber} = $$Data{tn};
-        $Ticket{State} = $$Data{state};
-        $Ticket{CustomerID} = $$Data{customer_id};
-        $Ticket{Queue} = $$Data{queue};
-        $Ticket{QueueID} = $QueueID;
-        $Ticket{Lock} = $$Data{lock_type};
-        $Ticket{Owner} = $$Data{login};
-        $Ticket{Priority} = $$Data{priority};
-        $Ticket{TicketFreeKey1} = $$Data{freekey1};
-        $Ticket{TicketFreeValue1} = $$Data{freetext1};
-        $Ticket{TicketFreeKey2} = $$Data{freekey2};
-        $Ticket{TicketFreeValue2} = $$Data{freetext2};
-        $Ticket{Created} = $$Data{ticket_create_time};
-        $Ticket{GroupID} = $$Data{group_id};
-        $Ticket{Age} = time() - $$Data{create_time_unix};
-        $Ticket{Answered} = $$Data{ticket_answered};
-        # article data
-        my %Article;
-        $Article{ArticleType} = $$Data{article_type};
-        $Article{SenderType} = $$Data{sender_type};
-        $Article{ArticleID} = $$Data{id};
-        $Article{From} = $$Data{a_from} || ' ';
-        $Article{To} = $$Data{a_to} || ' ';
-        $Article{Cc} = $$Data{a_cc} || ' ';
-        $Article{Subject} = $$Data{a_subject} || ' ';
-        $Article{Body} = $$Data{a_body};
-        $Article{CreateTime} = $$Data{create_time};
-        $Article{FreeKey1} = $$Data{a_freekey1};
-        $Article{FreeValue1} = $$Data{a_freetext1};
-        $Article{FreeKey2} = $$Data{a_freekey2};
-        $Article{FreeValue2} = $$Data{a_freetext2};
-        $Article{FreeKey3} = $$Data{a_freekey3};
-        $Article{FreeValue3} = $$Data{a_freetext3};
-        if ($$Data{a_content_type} && $$Data{a_content_type} =~ /charset=(.*)(| |\n)/i) {
-            $Article{ContentCharset} = $1;
-        }
-        if ($$Data{a_content_type} && $$Data{a_content_type} =~ /^(.+?\/.+?)( |;)/i) {
-            $Article{MimeType} = $1;
-        }
-        push (@ArticleBox, \%Article);
-    }
-    # --
-    # article attachments
-    # --
+    # get all atricle of this ticket
+    my @ArticleBox = $Self->{TicketObject}->GetArticleContentIndex(TicketID => $Self->{TicketID});
+    # get article attachments
     foreach my $Article (@ArticleBox) {
         my %AtmIndex = $Self->{TicketObject}->GetArticleAtmIndex(
             ContentPath => $Article->{ContentPath},
             ArticleID => $Article->{ArticleID},
         );
         $Article->{Atms} = \%AtmIndex;
-    }
-    # --
-    # check permission
-    # --
-    my $CustomerID = $Ticket{CustomerID};
-    $CustomerID =~ s/\+/\\+/g;
-    if ($Self->{UserCustomerID} !~ /^$CustomerID$/i) {
-        $Output = $Self->{LayoutObject}->CustomerHeader(Title => 'Error');
-        $Output .= $Self->{LayoutObject}->CustomerError(Message => 'No permission!');
-        $Output .= $Self->{LayoutObject}->CustomerFooter();
-        return $Output;
     }
     # --
     # genterate output
