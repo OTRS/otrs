a295dc03b8c6da5bfc38521cdfab05b1941e7316
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-01-21 22:49:39 +0000

    cvs

---
+++ Kernel/Modules/CustomerFAQ.pm
@@ -0,0 +1,192 @@
+# --
+# Kernel/Modules/FAQ.pm - faq module
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
+# --
+# $Id: CustomerFAQ.pm,v 1.1 2004-01-21 22:49:39 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::Modules::CustomerFAQ;
+
+use strict;
+use Kernel::System::FAQ;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+# --
+sub new {
+    my $Type = shift;
+    my %Param = @_;
+
+    # allocate new hash for object
+    my $Self = {}; 
+    bless ($Self, $Type);
+
+    foreach (keys %Param) {
+        $Self->{$_} = $Param{$_};
+    }
+
+    # check needed Opjects
+    foreach (qw(ParamObject DBObject LayoutObject LogObject 
+      ConfigObject UserObject)) {
+        die "Got no $_!" if (!$Self->{$_});
+    }
+
+    $Self->{FAQObject} = Kernel::System::FAQ->new(%Param);
+
+    return $Self;
+}
+# --
+sub Run {
+    my $Self = shift;
+    my %Param = @_;
+    my $Output;
+    my $ID = $Self->{ParamObject}->GetParam(Param => 'ID') || '';
+
+    $Output .= $Self->{LayoutObject}->CustomerHeader(Area => 'FAQ');
+    $Output .= $Self->{LayoutObject}->CustomerNavigationBar() if ($Self->{Action});
+
+    $Param{What} = $Self->{ParamObject}->GetParam(Param => 'What') || '';
+    $Param{Keyword} = $Self->{ParamObject}->GetParam(Param => 'Keyword') || '';
+    my @LanguageIDs = $Self->{ParamObject}->GetArray(Param => 'LanguageIDs');
+    my @CategoryIDs = $Self->{ParamObject}->GetArray(Param => 'CategoryIDs');
+
+    $Param{LanguageOption} = $Self->{LayoutObject}->OptionStrgHashRef(
+        Data => { $Self->{FAQObject}->LanguageList(UserID => $Self->{UserID}) },
+        Size => 6,
+        Name => 'LanguageIDs',
+        Multiple => 1,
+        SelectedIDRefArray => \@LanguageIDs,
+        HTMLQuote => 0,
+    );
+
+    $Param{CategoryOption} = $Self->{LayoutObject}->OptionStrgHashRef(
+        Data => { $Self->{FAQObject}->CategoryList(UserID => $Self->{UserID}) },
+        Size => 6,
+        Name => 'CategoryIDs',
+        Multiple => 1,
+        SelectedIDRefArray => \@CategoryIDs,
+        HTMLQuote => 0,
+    );
+
+
+    # search
+    if (!$ID && !$Self->{Subaction}) {
+        $Output = $Self->{LayoutObject}->CustomerHeader(Area => 'FAQ', Title => 'Search');
+        $Output .= $Self->{LayoutObject}->CustomerNavigationBar() if ($Self->{Action});
+        $Output .= $Self->{LayoutObject}->Output(
+            TemplateFile => 'CustomerFAQSearch', 
+            Data => { %Param },
+        );
+        # build an overview
+        my %Categories = $Self->{FAQObject}->CategoryList(UserID => $Self->{UserID});
+        foreach (sort {$Categories{$a} cmp $Categories{$b}} keys %Categories) {
+            $Param{Overview} .= "<b>$Categories{$_}</b><br>";
+            my @FAQIDs = $Self->{FAQObject}->Search(
+                %Param,
+                States => ['external (customer)', 'public (all)'],
+#                LanguageIDs => \@LanguageIDs,
+                CategoryIDs => [$_],
+                UserID => $Self->{UserID},
+            );
+            my %AllArticle = ();
+            foreach (@FAQIDs) {
+                my %Data = $Self->{FAQObject}->ArticleGet(ID => $_, UserID => $Self->{UserID});
+                $AllArticle{$Data{ID}} = "<a href='\$Env{\"Baselink\"}Action=\$Env{\"Action\"}&ID=$_'>";
+                $AllArticle{$Data{ID}} .= "[$Data{Language}/$Data{Category}] $Data{Subject} (\$Text{\"modified\"} \$TimeLong{\"$Data{Changed}\"})</a><br>";
+            }
+            foreach (sort {$AllArticle{$a} cmp $AllArticle{$b}} keys %AllArticle) {
+                $Param{Overview} .= $AllArticle{$_};
+            }
+        }
+        $Output .= $Self->{LayoutObject}->Output(
+            TemplateFile => 'CustomerFAQOverview',
+            Data => { %Param },
+        );
+    }
+    # search action
+    elsif ($Self->{Subaction} eq 'Search') {
+        $Output = $Self->{LayoutObject}->CustomerHeader(Area => 'FAQ', Title => 'Search');
+        $Output .= $Self->{LayoutObject}->CustomerNavigationBar() if ($Self->{Action});
+
+        my @FAQIDs = $Self->{FAQObject}->Search(
+            %Param,
+            States => ['external (customer)', 'public (all)'],
+            LanguageIDs => \@LanguageIDs,
+            CategoryIDs => \@CategoryIDs,
+            UserID => $Self->{UserID},
+        );
+        my %AllArticle = ();
+        foreach (@FAQIDs) {
+            my %Data = $Self->{FAQObject}->ArticleGet(ID => $_, UserID => $Self->{UserID}); 
+            $AllArticle{$Data{ID}} = "[$Data{Language}/$Data{Category}] $Data{Subject}</td><td> (\$Text{\"modified\"} \$TimeLong{\"$Data{Changed}\"})";
+        }
+        foreach (sort {$AllArticle{$a} cmp $AllArticle{$b}} keys %AllArticle) {
+            my %Data = $Self->{FAQObject}->ArticleGet(ID => $_, UserID => $Self->{UserID}); 
+            $Param{List} .= "<tr><td><a href='\$Env{\"Baselink\"}Action=\$Env{\"Action\"}&ID=$_'>$AllArticle{$Data{ID}}</a></td></tr>\n";
+        }
+        $Output .= $Self->{LayoutObject}->Output(
+            TemplateFile => 'CustomerFAQSearch', 
+            Data => { %Param },
+        );
+        $Output .= $Self->{LayoutObject}->Output(
+            TemplateFile => 'CustomerFAQSearchResult', 
+            Data => { %Param },
+        );
+#        $Output .= $List;
+    }
+    # system history
+    elsif ($Self->{Subaction} eq 'SystemHistory') {
+        $Output = $Self->{LayoutObject}->CustomerHeader(Area => 'FAQ', Title => 'History');
+        $Output .= $Self->{LayoutObject}->CustomerNavigationBar() if ($Self->{Action});
+        my @History = $Self->{FAQObject}->HistoryGet(
+            UserID => $Self->{UserID},
+        );
+        foreach my $Row (@History) {
+            my %Data = $Self->{FAQObject}->ArticleGet(ID => $Row->{ID}, UserID => $Self->{UserID}); 
+            $Param{HistoryList} .= "<tr><td><a href=\"\$Env{\"Baselink\"}Action=\$Env{\"Action\"}&ID=$Row->{ID}\">[$Data{Language}/$Data{Category}] $Data{Subject}</a></td><td> (\$Text{\"$Row->{Name}\"} - \$TimeLong{\"$Data{Changed}\"})</td></tr>";
+        }
+        $Output .= $Self->{LayoutObject}->Output(
+            TemplateFile => 'CustomerFAQArticleSystemHistory', 
+            Data => { 
+                ID => $ID,
+                %Param 
+            },
+        );
+
+    }
+    # view
+    elsif ($ID && $Self->{Subaction} eq 'Print') {
+        my %Data = $Self->{FAQObject}->ArticleGet(ID => $ID, UserID => $Self->{UserID});
+        $Output = $Self->{LayoutObject}->PrintHeader(Area => 'FAQ', Title => $Data{Subject});
+        if ($Data{State} =~ /(external \(customer\)|public \(all\))/i) {
+            $Output .= $Self->{LayoutObject}->Output(
+                TemplateFile => 'CustomerFAQArticlePrint', 
+                Data => { %Param, %Data },
+            );
+        }
+        $Output .= $Self->{LayoutObject}->PrintFooter();
+        return $Output;
+    }
+    elsif ($ID) {
+        my %Data = $Self->{FAQObject}->ArticleGet(ID => $ID, UserID => $Self->{UserID});
+        $Output = $Self->{LayoutObject}->CustomerHeader(Area => 'FAQ', Title => $Data{Subject});
+        $Output .= $Self->{LayoutObject}->CustomerNavigationBar() if ($Self->{Action});
+        if ($Data{State} =~ /(external \(customer\)|public \(all\))/i) {
+            $Output .= $Self->{LayoutObject}->Output(
+                TemplateFile => 'CustomerFAQArticleView', 
+                Data => { %Param, %Data },
+            );
+        }
+    }
+    $Output .= $Self->{LayoutObject}->CustomerFooter();
+    return $Output;
+}
+# --
+
+1;
---
+++ bin/cgi-bin/faq.pl
@@ -0,0 +1,144 @@
+#!/usr/bin/perl -w
+# --
+# faq.pl - the global CGI handle file for OTRS
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
+# --
+# $Id: faq.pl,v 1.1 2004-01-21 22:49:39 martin Exp $
+# --
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# --
+
+# use ../../ as lib location
+use FindBin qw($Bin);
+use lib "$Bin/../..";
+use lib "$Bin/../../Kernel/cpan-lib";
+
+use strict;
+
+use vars qw($VERSION @INC);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+# --
+# 0=off;1=on;
+# --
+my $Debug = 0; 
+
+# --
+# check @INC for mod_perl (add lib path for "require module"!)
+# --
+push (@INC, "$Bin/../..", "$Bin/../../Kernel/cpan-lib");
+
+# --
+# all framework needed  modules 
+# (if you use mod_perl with startup.pl, drop this "use Kernel::.." and add
+# this to your startup.pl) 
+# --
+use Kernel::Config;
+use Kernel::System::Log;
+use Kernel::System::WebRequest;
+use Kernel::System::DB;
+use Kernel::System::User;
+use Kernel::Output::HTML::Generic;
+use Kernel::Modules::CustomerFAQ;
+
+# --
+# create common framework objects 1/2
+# --
+my %CommonObject = ();
+$CommonObject{ConfigObject} = Kernel::Config->new();
+$CommonObject{LogObject} = Kernel::System::Log->new(
+    LogPrefix => $CommonObject{ConfigObject}->Get('CGILogPrefix'),
+    %CommonObject,
+);
+$CommonObject{DBObject} = Kernel::System::DB->new(%CommonObject);
+$CommonObject{ParamObject} = Kernel::System::WebRequest->new(%CommonObject);
+$CommonObject{UserObject} = Kernel::System::User->new(%CommonObject);
+# --
+# debug info
+# --
+if ($Debug) {
+    $CommonObject{LogObject}->Log(
+        Priority => 'debug', 
+        Message => 'Global handle started...',
+    );
+}
+my %Param = ();
+# definde frame work params
+my $FramworkPrams = {
+    Lang => '',
+    Action => '',
+    Subaction => '',
+};
+foreach my $Key (keys %{$FramworkPrams}) {
+    $Param{$Key} = $CommonObject{ParamObject}->GetParam(Param => $Key) 
+      || $FramworkPrams->{$Key};
+}
+# --
+# create common framework objects 2/2
+# --
+$CommonObject{LayoutObject} = Kernel::Output::HTML::Generic->new(
+    %CommonObject, 
+    Lang => $Param{Lang},
+);
+# --
+# check common objects
+# --
+if (!$CommonObject{DBObject}) {
+    print $CommonObject{LayoutObject}->CustomerHeader(Title => 'Error!');
+    print $CommonObject{LayoutObject}->CustomerError(
+        Message => $DBI::errstr,
+        Comment => 'Please contact your admin'
+    );
+    print $CommonObject{LayoutObject}->CustomerFooter();
+    exit (1);
+}
+if ($CommonObject{ParamObject}->Error()) {
+    print $CommonObject{LayoutObject}->CustomerHeader(Title => 'Error!');
+    print $CommonObject{LayoutObject}->CustomerError(
+        Message => $CommonObject{ParamObject}->Error(),
+        Comment => 'Please contact your admin'
+    );
+    print $CommonObject{LayoutObject}->CustomerFooter();
+    exit (1);
+}
+# --
+# prove of concept! - create $GenericObject
+# --
+my $GenericObject = ('Kernel::Modules::CustomerFAQ')->new(
+    UserID => 1,
+    %CommonObject,
+    %Param,
+);
+# --
+# ->Run $Action with $GenericObject
+# --
+print $GenericObject->Run();
+# --
+# debug info
+# --
+if ($Debug) {
+    $CommonObject{LogObject}->Log(
+        Priority => 'debug',
+        Message => 'Global handle stopped.',
+    );
+}
+# --
+# db disconnect && undef %CommonObject %% undef %
+# --
+$CommonObject{DBObject}->Disconnect();
+undef %Param;
+undef %CommonObject;
+
