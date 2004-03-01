508276dbe08bf5558d792d8fe6a0679eddcdc4e1
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-03-01 14:01:58 +0000

    improved

--- Kernel/Modules/AdminCustomerUser.pm
+++ Kernel/Modules/AdminCustomerUser.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminCustomerUser.pm - to add/update/delete customer user and preferences
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AdminCustomerUser.pm,v 1.9 2004-02-09 01:40:29 martin Exp $
+# $Id: AdminCustomerUser.pm,v 1.10 2004-03-01 14:01:58 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see 
 # the enclosed file COPYING for license information (GPL). If you 
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.9 $ ';
+$VERSION = '$Revision: 1.10 $ ';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -48,6 +48,9 @@ sub Run {
     my $NavBar = '';
     my $Nav = $Self->{ParamObject}->GetParam(Param => 'Nav') || 0;
     my $Source = $Self->{ParamObject}->GetParam(Param => 'Source') || 'CustomerUser';
+    my $Search = $Self->{ParamObject}->GetParam(Param => 'Search');
+
+    my %UserList = ();
     # check nav bar
     if (!$Nav) {
         if ($ENV{HTTP_REFERER} && $ENV{HTTP_REFERER} !~ /Admin/) {
@@ -63,32 +66,44 @@ sub Run {
     }
     else {
         $NavBar = $Self->{LayoutObject}->Header(Area => 'Agent', Title => 'Customer User');
-        $NavBar .= $Self->{LayoutObject}->NavigationBar();
+        # get user lock data
+        my %LockedData = $Self->{TicketObject}->GetLockedCount(UserID => $Self->{UserID});
+        # build NavigationBar 
+        $NavBar .= $Self->{LayoutObject}->NavigationBar(LockData => \%LockedData);
     }
-
-    # -- 
+    # search user list
+    if (defined($Search)) {
+        my $Filter = "*$Search*";
+        $Filter =~ s/\*\*/\*/g;
+        $Filter =~ s/\*\*/\*/g;
+        %UserList = $Self->{CustomerUserObject}->CustomerSearch(
+            Search => $Filter,
+        );
+    }
+    # build user result list
+    my $Link = '';
+    if (%UserList) {
+        foreach (sort keys %UserList) {
+            $Link .= "<tr><td>$_</td><td><a href='?Action=AdminCustomerUser&Subaction=Change&ID=$_&Search=$Search&Nav=$Nav'>\$Quote{\"$UserList{$_}\"}</a></td></tr>";
+        }
+    }
     # get user data 2 form
-    # --
     if ($Self->{Subaction} eq 'Change') {
         my $User = $Self->{ParamObject}->GetParam(Param => 'ID') || '';
-        # --
         # get user data
-        # --
         my %UserData = $Self->{CustomerUserObject}->CustomerUserDataGet(User => $User);
-        my %UserList = $Self->{CustomerUserObject}->CustomerUserList(Valid => 0);
         my $Output = $NavBar.$Self->{LayoutObject}->AdminCustomerUserForm(
             Nav => $Nav,
-            UserList => \%UserList,
+            UserLinkList => $Link,
             SourceList => {$Self->{CustomerUserObject}->CustomerSourceList()},
             Source => $Source,
+            Search => $Search,
             %UserData,
         );
         $Output .= $Self->{LayoutObject}->Footer();
         return $Output;
     }
-    # --
     # update action
-    # --
     elsif ($Self->{Subaction} eq 'ChangeAction') {
         # get params
         my %GetParam;
@@ -118,7 +133,7 @@ sub Run {
             }
             # redirect
             return $Self->{LayoutObject}->Redirect(
-                OP => "Action=AdminCustomerUser&Nav=$Nav",
+                OP => "Action=AdminCustomerUser&Nav=$Nav&Search=$Search",
             );
         }
         else {
@@ -127,9 +142,19 @@ sub Run {
             return $Output;
         }
     }
-    # --
+    # search
+    elsif ($Self->{Subaction} eq 'Search') {
+        my $Output .= $NavBar.$Self->{LayoutObject}->AdminCustomerUserForm(
+            Nav => $Nav,
+            UserLinkList => $Link,
+            SourceList => {$Self->{CustomerUserObject}->CustomerSourceList()}, 
+            Search => $Search,
+            Source => $Source,
+        );
+        $Output .= $Self->{LayoutObject}->Footer();
+        return $Output;
+    }
     # add new user
-    # --
     elsif ($Self->{Subaction} eq 'AddAction') {
         # get params
         my %GetParam;
@@ -158,7 +183,7 @@ sub Run {
             }
             # redirect
             return $Self->{LayoutObject}->Redirect(
-                OP => "Action=AdminCustomerUser&Nav=$Nav",
+                OP => "Action=AdminCustomerUser&Nav=$Nav&Search=$Search",
             );
         }
         else {
@@ -167,15 +192,13 @@ sub Run {
             return $Output;
         }
     }
-    # --
     # else ! print form
-    # --
     else {
-        my %UserList = $Self->{CustomerUserObject}->CustomerUserList(Valid => 0);
         my $Output .= $NavBar.$Self->{LayoutObject}->AdminCustomerUserForm(
             Nav => $Nav,
-            UserList => { $Self->{CustomerUserObject}->CustomerUserList(Valid => 0) },
+            UserLinkList => $Link,
             SourceList => {$Self->{CustomerUserObject}->CustomerSourceList()}, 
+            Search => $Search,
             Source => $Source,
         );
         $Output .= $Self->{LayoutObject}->Footer();
