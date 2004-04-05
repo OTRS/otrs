84ccc36ab646da39d93d6fa6ec766915469ea56e
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-04-05 17:17:40 +0000

    fixed pod doc

--- Kernel/System/Ticket.pm
+++ Kernel/System/Ticket.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket.pm - the global ticket handle
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Ticket.pm,v 1.76 2004-04-05 17:10:54 martin Exp $
+# $Id: Ticket.pm,v 1.77 2004-04-05 17:17:40 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -34,7 +34,7 @@ use Kernel::System::CustomerUser;
 use Kernel::System::Notification;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.76 $';
+$VERSION = '$Revision: 1.77 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -462,7 +462,7 @@ Lock, LockID, Queue, QueueID, CustomerID, CustomerUserID, UserID, ...)
       TicketID => 123,
       UserID => 123,
   );
-    
+
 =cut
 
 sub TicketGet {
@@ -2571,6 +2571,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.76 $ $Date: 2004-04-05 17:10:54 $
+$Revision: 1.77 $ $Date: 2004-04-05 17:17:40 $
 
 =cut
