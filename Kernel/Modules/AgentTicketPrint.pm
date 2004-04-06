be5de4c325f9a4e3da1bfe8fa311a0f704b24505
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-04-06 07:58:45 +0000

    moved to Kernel::System::Ticket lib

--- Kernel/Modules/AgentTicketPrint.pm
+++ Kernel/Modules/AgentTicketPrint.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AgentTicketPrint.pm - to get a closer view
-# Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentTicketPrint.pm,v 1.8 2004-04-05 17:14:11 martin Exp $
+# $Id: AgentTicketPrint.pm,v 1.9 2004-04-06 07:58:45 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Modules::AgentTicketPrint;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.8 $';
+$VERSION = '$Revision: 1.9 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -32,7 +32,7 @@ sub new {
 
     # check needed Opjects
     foreach (qw(ParamObject DBObject TicketObject LayoutObject LogObject
-      QueueObject ConfigObject UserObject SessionObject)) {
+      QueueObject ConfigObject UserObject)) {
         die "Got no $_!" if (!$Self->{$_});
     }
 
@@ -65,119 +65,15 @@ sub Run {
         # --
         return $Self->{LayoutObject}->NoPermission(WithHeader => 'yes');
     }  
-    my %Ticket;
-    $Ticket{TicketID} = $Self->{TicketID};
-    $Ticket{Age} = '?';
-    $Ticket{TmpCounter} = 0;
-    $Ticket{FreeKey1} = '';
-    $Ticket{FreeValue1} = '';
-    $Ticket{FreeKey2} = '';
-    $Ticket{FreeValue2} = '';
+    # get content
+    my %Ticket = $Self->{TicketObject}->TicketGet(TicketID => $Self->{TicketID});
+    my %TicketLink = $Self->{TicketObject}->TicketLinkGet(
+        TicketID => $Self->{TicketID},
+        UserID => $Self->{UserID},
+    );
+    my @ArticleBox = $Self->{TicketObject}->GetArticleContentIndex(TicketID => $Self->{TicketID});
     $Ticket{TicketTimeUnits} = $Self->{TicketObject}->GetAccountedTime(TicketID => $Ticket{TicketID});
-    # --
-    # grep all atricle of this ticket
-    # --
-    my @ArticleBox;
-    my $SQL = "SELECT sa.id, st.tn, sa.a_from, sa.a_to, sa.a_cc, sa.a_subject, sa.a_body, ".
-    " st.create_time_unix, st.tn, st.user_id, st.ticket_state_id, st.ticket_priority_id, ". 
-    " sa.create_time, stt.name as sender_type, at.name as article_type, ".
-    " su.$Self->{ConfigObject}->{DatabaseUserTableUser}, ".
-    " sl.name as lock_type, sp.name as priority, tsd.name as state, sa.content_path, ".
-    " sq.name as queue, st.create_time as ticket_create_time, ".
-    " sa.a_freekey1, sa.a_freetext1, sa.a_freekey2, sa.a_freetext2, ".
-    " sa.a_freekey3, sa.a_freetext3, st.freekey1, st.freekey2, st.freetext1, ".
-    " st.freetext2, st.customer_id, sq.group_id, st.ticket_answered, sq.escalation_time, ".
-    " sa.a_content_type, sa.incoming_time, st.until_time ".
-    " FROM ".
-    " article sa, ticket st, article_sender_type stt, article_type at, ".
-    " $Self->{ConfigObject}->{DatabaseUserTable} su, ticket_lock_type sl, " .
-    " ticket_priority sp, ticket_state tsd, queue sq " .
-    " WHERE " .
-    " sa.ticket_id = st.id " .
-    " AND " .
-    " sq.id = st.queue_id " .
-    " AND " .
-    " stt.id = sa.article_sender_type_id " .
-    " AND " .
-    " at.id = sa.article_type_id " .
-    " AND " .
-    " sp.id = st.ticket_priority_id " .
-    " AND " .
-    " sl.id = st.ticket_lock_id " .
-    " AND " .
-    " tsd.id = st.ticket_state_id " .
-    " AND " .
-    " sa.ticket_id = $Self->{TicketID} " .
-    " AND " .
-    " su.$Self->{ConfigObject}->{DatabaseUserTableUserID} = st.user_id " .
-    " GROUP BY sa.id, st.tn, sa.a_from, sa.a_to, sa.a_cc, sa.a_subject, sa.a_body, ".
-    " st.create_time_unix, st.tn, st.user_id, st.ticket_state_id, st.ticket_priority_id, ".
-    " sa.create_time, stt.name, at.name, ".
-    " su.$Self->{ConfigObject}->{DatabaseUserTableUser}, ".
-    " sl.name, sp.name, tsd.name, sa.content_path, ".
-    " sq.name, st.create_time, ".
-    " sa.a_freekey1, sa.a_freetext1, sa.a_freekey2, sa.a_freetext2, ".
-    " sa.a_freekey3, sa.a_freetext3, st.freekey1, st.freekey2, st.freetext1, ".
-    " st.freetext2, st.customer_id, sq.group_id, st.ticket_answered, sq.escalation_time, ".
-    " sa.a_content_type, sa.incoming_time, st.until_time ";
-    $Self->{DBObject}->Prepare(SQL => $SQL);
-    while (my $Data = $Self->{DBObject}->FetchrowHashref() ) {
-        # get escalation_time
-        if ($$Data{escalation_time} && $$Data{sender_type} eq 'customer') {
-            $Ticket{TicketOverTime} = (($$Data{incoming_time} + ($$Data{escalation_time}*60)) - time());
-        }
-        if (!$$Data{until_time} || $$Data{state} !~ /^pending/i) {
-            $Ticket{UntilTime} = 0;
-        }
-        else {
-            $Ticket{UntilTime} = $$Data{until_time} - time();
-        }
-        # ticket data
-        $Ticket{TicketNumber} = $$Data{tn};
-        $Ticket{State} = $$Data{state};
-        $Ticket{CustomerID} = $$Data{customer_id};
-        $Ticket{Queue} = $$Data{queue};
-        $Ticket{QueueID} = $QueueID;
-        $Ticket{Lock} = $$Data{lock_type};
-        $Ticket{Owner} = $$Data{login};
-        $Ticket{Priority} = $$Data{priority};
-        $Ticket{TicketFreeKey1} = $$Data{freekey1};
-        $Ticket{TicketFreeValue1} = $$Data{freetext1};
-        $Ticket{TicketFreeKey2} = $$Data{freekey2};
-        $Ticket{TicketFreeValue2} = $$Data{freetext2};
-        $Ticket{Created} = $$Data{ticket_create_time};
-        $Ticket{GroupID} = $$Data{group_id};
-        $Ticket{Age} = time() - $$Data{create_time_unix};
-        $Ticket{Answered} = $$Data{ticket_answered};
-        # article data
-        my %Article;
-        $Article{ContentPath} = $$Data{content_path},
-        $Article{ArticleType} = $$Data{article_type};
-        $Article{SenderType} = $$Data{sender_type};
-        $Article{ArticleID} = $$Data{id};
-        $Article{From} = $$Data{a_from} || ' ';
-        $Article{To} = $$Data{a_to} || ' ';
-        $Article{Cc} = $$Data{a_cc} || ' ';
-        $Article{Subject} = $$Data{a_subject} || ' ';
-        $Article{Body} = $$Data{a_body};
-        $Article{CreateTime} = $$Data{create_time};
-        $Article{FreeKey1} = $$Data{a_freekey1};
-        $Article{FreeValue1} = $$Data{a_freetext1};
-        $Article{FreeKey2} = $$Data{a_freekey2};
-        $Article{FreeValue2} = $$Data{a_freetext2};
-        $Article{FreeKey3} = $$Data{a_freekey3};
-        $Article{FreeValue3} = $$Data{a_freetext3};
-        if ($$Data{a_content_type} && $$Data{a_content_type} =~ /charset=(.*)(| |\n)/i) {
-            $Article{ContentCharset} = $1;
-        }
-        if ($$Data{a_content_type} && $$Data{a_content_type} =~ /^(.+?\/.+?)( |;)/i) {
-            $Article{MimeType} = $1;
-        }
-        push (@ArticleBox, \%Article);
-    }
-    # --
     # article attachments
-    # --
     foreach my $Article (@ArticleBox) {
         my %AtmIndex = $Self->{TicketObject}->GetArticleAtmIndex(
             ContentPath => $Article->{ContentPath},
@@ -185,26 +81,26 @@ sub Run {
         );
         $Article->{Atms} = \%AtmIndex;
     }
-    # --
     # user info
-    # --
     my %UserInfo = $Self->{UserObject}->GetUserData(
         User => $Ticket{Owner},
         Cached => 1
     );
-    # --
     # genterate output
-    # --
     $Output .= $Self->{LayoutObject}->PrintHeader(Title => $Ticket{TicketNumber});
-    $Output .= $Self->_MaskHeader(%Ticket, %UserInfo);
-    # --
+    $Output .= $Self->_MaskHeader(
+        %UserInfo,
+        %Ticket,
+        %TicketLink,
+    );
     # show ticket
-    # --
     $Output .= $Self->_Mask(
         TicketID => $Self->{TicketID},
         QueueID => $QueueID,
         ArticleBox => \@ArticleBox,
-        %Ticket
+        %UserInfo,
+        %Ticket,
+        %TicketLink,
     );
     # add footer 
     $Output .= $Self->{LayoutObject}->PrintFooter();
@@ -237,20 +133,17 @@ sub _Mask {
              ' return true;" onmouseout="window.status=\'\';">'.
              $AtmIndex{$_}.'</a><br> ';
         }
-        # do some strips && quoting
-        $Article{CreateTime} = $Self->{LayoutObject}->{LanguageObject}->FormatTimeString($Article{CreateTime});
         foreach (qw(To Cc From Subject FreeKey1 FreeKey2 FreeKey3 FreeValue1 FreeValue2 
           FreeValue3 CreateTime SenderType ArticleType)) {
             $Article{$_} = $Self->{LayoutObject}->{LanguageObject}->CharsetConvert(
                 Text => $Article{$_},
                 From => $Article{ContentCharset},
             );
-            $Param{"Article::$_"} = $Self->{LayoutObject}->Ascii2Html(Text => $Article{$_}, Max => 300);
         }
         # check if just a only html email
         if (my $MimeTypeText = $Self->{LayoutObject}->CheckMimeType(%Param, %Article, Action => 'AgentZoom')) {
-            $Param{"Article::TextNote"} = $MimeTypeText;
-            $Param{"Article::Text"} = '';
+            $Param{"TextNote"} = $MimeTypeText;
+            $Article{"Body"} = '';
         }
         else {
             # charset quoting
@@ -259,7 +152,7 @@ sub _Mask {
                 From => $Article{ContentCharset},
             );
             # html quoting
-            $Param{"Article::Text"} = $Self->{LayoutObject}->Ascii2Html(
+            $Article{Body} = $Self->{LayoutObject}->Ascii2Html(
                 NewLine => $Self->{ConfigObject}->Get('ViewableTicketNewLine') || 85,
                 Text => $Article{Body},
                 VMax => $Self->{ConfigObject}->Get('ViewableTicketLinesZoom') || 5000,
@@ -273,12 +166,10 @@ sub _Mask {
                 $Param{"Article::TextNote"} = $CharsetText;
             }
         }
-        # get article id
-        $Param{"Article::ArticleID"} = $Article{ArticleID};
 
         # select the output template
         if ($Article{ArticleType} ne 'email-notification-int') {
-            $Output .= $Self->{LayoutObject}->Output(TemplateFile => 'AgentTicketPrint', Data => \%Param);
+            $Output .= $Self->{LayoutObject}->Output(TemplateFile => 'AgentTicketPrint', Data => {%Param,%Article});
         }
     }
     # return output
@@ -289,14 +180,7 @@ sub _MaskHeader {
     my $Self = shift;
     my %Param = @_;
     # do some html quoting
-    foreach (qw(State Priority Lock)) {
-        $Param{$_} = $Self->{LayoutObject}->{LanguageObject}->Get($Param{$_});
-    }
-    foreach (qw(Priority State Owner Queue CustomerID Lock)) {
-        $Param{$_} = $Self->{LayoutObject}->Ascii2Html(Text => $Param{$_}, Max => 25) || '';
-    }
     $Param{Age} = $Self->{LayoutObject}->CustomerAge(Age => $Param{Age}, Space => ' ');
-    $Param{Created} = $Self->{LayoutObject}->{LanguageObject}->FormatTimeString($Param{Created});
     if ($Param{UntilTime}) {
         $Param{PendingUntil} = $Self->{LayoutObject}->CustomerAge(Age => $Param{UntilTime}, Space => ' ');
     }
