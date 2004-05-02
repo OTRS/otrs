6be7c0d0e0bc8d0fa38bf2b4d06d92bfafd44ed3
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-05-02 10:31:02 +0000

    removed * for search because it's defined in customer map

--- Kernel/Modules/AgentEmail.pm
+++ Kernel/Modules/AgentEmail.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentEmail.pm - to compose inital email to customer 
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentEmail.pm,v 1.24 2004-05-01 14:31:20 martin Exp $
+# $Id: AgentEmail.pm,v 1.25 2004-05-02 10:31:02 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::State;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.24 $';
+$VERSION = '$Revision: 1.25 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -201,7 +201,7 @@ sub Run {
             # search customer 
             my %CustomerUserList = ();
             %CustomerUserList = $Self->{CustomerUserObject}->CustomerSearch(
-                Search => $To.'*',
+                Search => $To,
             );
             # check if just one customer user exists
             # if just one, fillup CustomerUserID and CustomerID
