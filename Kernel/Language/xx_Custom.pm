a8e2e85e772b0046a18796a3f5881a34462e3843
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-06-04 08:50:41 +0000

    example custom and Kernel/Modules/*.pm translation files

---
+++ Kernel/Language/xx_Custom.pm
@@ -0,0 +1,32 @@
+# --
+# Kernel/Language/xx_Custom.pm - provides xx custom language translation
+# Copyright (C) 2001-2004 Martin Edenhofer <martin at otrs.org>
+# --
+# $Id: xx_Custom.pm,v 1.1 2004-06-04 08:50:41 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+package Kernel::Language::xx_Custom;
+
+use strict;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+# --
+sub Data {
+    my $Self = shift;
+    my %Param = @_;
+
+    # $$START$$
+
+    $Self->{Translation}->{'Lock'} = 'Lala';
+    $Self->{Translation}->{'Unlock'} = 'Lulu';
+
+    # $$STOP$$
+}
+# --
+1;
