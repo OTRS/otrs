2b2b70b27b904bf00a3aa74f8ba742edcebdd7fc
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-06-22 09:40:34 +0000

    added multi customer id support

--- Kernel/Modules/CustomerAttachment.pm
+++ Kernel/Modules/CustomerAttachment.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/CustomerAttachment.pm - to get the attachments 
-# Copyright (C) 2002-2003 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: CustomerAttachment.pm,v 1.8 2004-04-14 15:54:40 martin Exp $
+# $Id: CustomerAttachment.pm,v 1.9 2004-06-22 09:40:34 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Modules::CustomerAttachment;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.8 $';
+$VERSION = '$Revision: 1.9 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -31,15 +31,7 @@ sub new {
     }
 
     # check needed Opjects
-    foreach (
-      'ParamObject',
-      'DBObject',
-      'TicketObject',
-      'LayoutObject',
-      'LogObject',
-      'ConfigObject',
-      'UserObject',
-    ) {
+    foreach (qw(ParamObject DBObject TicketObject LayoutObject LogObject ConfigObject UserObject)) {
         die "Got no $_!" if (!$Self->{$_});
     }
 
@@ -54,9 +46,7 @@ sub Run {
     my $Self = shift;
     my %Param = @_;
     my $Output = '';
-    # --
     # check params
-    # --
     if (!$Self->{FileID} || !$Self->{ArticleID}) {
         $Output .= $Self->{LayoutObject}->CustomerHeader(Title => 'Error');
         $Output .= $Self->{LayoutObject}->CustomerError(
@@ -71,9 +61,7 @@ sub Run {
         return $Output;
     }
 
-    # --
     # check permissions
-    # --
     my %ArticleData = $Self->{TicketObject}->ArticleGet(ArticleID => $Self->{ArticleID});
     if (!$ArticleData{TicketID}) {
         $Output .= $Self->{LayoutObject}->CustomerHeader(Title => 'Error');
@@ -88,42 +76,32 @@ sub Run {
         $Output .= $Self->{LayoutObject}->CustomerFooter();
         return $Output;
     }
-
-    # --
     # check permission
-    # --
-    my $CustomerID = $Self->{UserCustomerID}; 
-    $CustomerID =~ s/\+/\\+/g;
-    if ($ArticleData{CustomerID} && $ArticleData{CustomerID} =~ /^$CustomerID$/i) {
-        # --
-        # geta attachment & strip file path
-        # --
-        if (my %Data = $Self->{TicketObject}->ArticleAttachment(
-          ArticleID => $Self->{ArticleID},
-          FileID => $Self->{FileID},
-        )) {
-            return $Self->{LayoutObject}->Attachment(%Data);  
-        }
-        else {
-            $Output .= $Self->{LayoutObject}->CustomerHeader(Title => 'Error');
-            $Output .= $Self->{LayoutObject}->CustomerError(
-              Message => "No such attacment ($Self->{FileID})!",
-              Comment => 'Please contact your admin'
-            );
-            $Self->{LogObject}->Log(
-              Message => "No such attacment ($Self->{FileID})! May be an attack!!!",
-              Priority => 'error',
-            );
-            $Output .= $Self->{LayoutObject}->CustomerFooter();
-            return $Output;
-        }
+    if (!$Self->{TicketObject}->CustomerPermission(
+        Type => 'ro',
+        TicketID => $ArticleData{TicketID},
+        UserID => $Self->{UserID})) {
+        # error screen, don't show ticket
+        return $Self->{LayoutObject}->CustomerNoPermission(WithHeader => 'yes');
+    }  
+
+    # geta attachment 
+    if (my %Data = $Self->{TicketObject}->ArticleAttachment(
+        ArticleID => $Self->{ArticleID},
+        FileID => $Self->{FileID},
+    )) {
+        return $Self->{LayoutObject}->Attachment(%Data);  
     }
     else {
-        # --
-        # error screen
-        # --
-        $Output = $Self->{LayoutObject}->CustomerHeader(Title => 'Error');
-        $Output .= $Self->{LayoutObject}->CustomerError(Message => 'No permission!');
+        $Output .= $Self->{LayoutObject}->CustomerHeader(Title => 'Error');
+        $Output .= $Self->{LayoutObject}->CustomerError(
+            Message => "No such attacment ($Self->{FileID})!",
+            Comment => 'Please contact your admin'
+        );
+        $Self->{LogObject}->Log(
+            Message => "No such attacment ($Self->{FileID})! May be an attack!!!",
+            Priority => 'error',
+        );
         $Output .= $Self->{LayoutObject}->CustomerFooter();
         return $Output;
     }
