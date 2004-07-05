dc8439f3eda75cfa076ece5aa27f5fa01a0081b1
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-07-05 08:07:28 +0000

    fixed perl syntax

--- Kernel/System/Ticket.pm
+++ Kernel/System/Ticket.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket.pm - the global ticket handle
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Ticket.pm,v 1.124 2004-07-05 08:05:55 martin Exp $
+# $Id: Ticket.pm,v 1.125 2004-07-05 08:07:28 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -32,7 +32,7 @@ use Kernel::System::CustomerUser;
 use Kernel::System::Notification;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.124 $';
+$VERSION = '$Revision: 1.125 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -2766,7 +2766,7 @@ sub HistoryTicketGet {
             } 
         }
         elsif ($Row[1] eq 'OwnerUpdate') {
-            if ($Row[0] =~ /^\%\%(.+?)\%\%(.+?)/ || $Row[0] =~ /^New Owner is '(+.?)'/) {
+            if ($Row[0] =~ /^\%\%(.+?)\%\%(.+?)/ || $Row[0] =~ /^New Owner is '(.+?)'/) {
                 $Ticket{Owner} = $1;
             }
         }
@@ -3314,6 +3314,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.124 $ $Date: 2004-07-05 08:05:55 $
+$Revision: 1.125 $ $Date: 2004-07-05 08:07:28 $
 
 =cut
