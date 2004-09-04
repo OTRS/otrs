03f6fe74c32db008e66b6169b7527a22313b79b7
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-09-04 21:44:42 +0000

    added block example

--- Kernel/Modules/Test.pm
+++ Kernel/Modules/Test.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/Test.pm - a simple test module
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Test.pm,v 1.8 2004-02-18 09:37:32 martin Exp $
+# $Id: Test.pm,v 1.9 2004-09-04 21:44:42 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Modules::Test;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.8 $';
+$VERSION = '$Revision: 1.9 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -23,7 +23,7 @@ sub new {
     my %Param = @_;
 
     # allocate new hash for object
-    my $Self = {}; 
+    my $Self = {};
     bless ($Self, $Type);
 
     # get common opjects
@@ -41,7 +41,78 @@ sub Run {
     # get test page header
     my $Output = $Self->{LayoutObject}->Header( Title => 'OTRS Test Page' );
 
-    # get test page 
+    # example blocks
+    $Self->{LayoutObject}->Block(
+        Name => 'Row',
+        Data => {
+            Text => 'Some Text for the first line',
+            Counter => 1,
+        },
+    );
+    $Self->{LayoutObject}->Block(
+        Name => 'Row',
+        Data => {
+            Text => 'Some Text for the next line',
+            Counter => 2,
+        },
+    );
+
+    foreach (1..2) {
+        # fist block
+        $Self->{LayoutObject}->Block(
+            Name => 'System',
+            Data => {
+                Type => 'System',
+                Counter => $_,
+            },
+        );
+        # sub block of System
+        $Self->{LayoutObject}->Block(
+            Name => 'User',
+            Data => {
+                Type => 'User',
+                Counter => $_,
+            },
+        );
+        # sub blocks of User
+        $Self->{LayoutObject}->Block(
+            Name => 'UserID',
+            Data => {
+                Type => 'UserID',
+                Counter => $_,
+            },
+        );
+        # just if $_ > 1
+        if ($_ > 1) {
+            $Self->{LayoutObject}->Block(
+                Name => 'UserID',
+                Data => {
+                    Type => 'UserID',
+                    Counter => $_,
+                },
+            );
+        }
+        # add this block 3 times
+        foreach (4..6) {
+            $Self->{LayoutObject}->Block(
+                Name => 'UserIDA',
+                Data => {
+                    Type => 'UserIDA',
+                    Counter => $_,
+                },
+            );
+        }
+    }
+    # add the times block at least
+    $Self->{LayoutObject}->Block(
+        Name => 'Times',
+        Data => {
+            Type => 'Times',
+            Counter => 443,
+        },
+    );
+
+    # get test page
     $Output .= $Self->{LayoutObject}->Output(TemplateFile => 'Test', Data => \%Param);
 
     # get test page footer
