bd68ef2c443e71f477a8cd3cbbbbcecaeec6f4d7
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-09-09 11:08:59 +0000

    db update scripts for OTRS 2.0

---
+++ scripts/DBUpdate-to-2.0.postgresql.sql
@@ -0,0 +1,73 @@
+-- --
+-- Update an existing OTRS database from 1.3 to 2.0
+-- Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
+-- --
+-- $Id: DBUpdate-to-2.0.postgresql.sql,v 1.1 2004-09-09 11:08:59 martin Exp $
+-- --
+--
+-- usage: cat DBUpdate-to-1.3.mysql.sql | mysql -f -u root otrs
+--
+-- --
+
+--
+-- ticket
+--
+ALTER TABLE ticket ADD title varchar (255);
+
+--
+-- object_link
+--
+CREATE TABLE object_link
+(
+    object_link_a_id BIGINT NOT NULL,
+    object_link_b_id BIGINT NOT NULL,
+    object_link_a_object VARCHAR (200) NOT NULL,
+    object_link_b_object VARCHAR (200) NOT NULL,
+    object_link_type VARCHAR (200) NOT NULL
+);
+
+--
+-- group_role
+--
+CREATE TABLE group_role
+(
+    role_id INTEGER NOT NULL,
+    group_id INTEGER NOT NULL,
+    permission_key VARCHAR (20),
+    permission_value SMALLINT NOT NULL,
+    create_time timestamp(0) NOT NULL,
+    create_by INTEGER NOT NULL,
+    change_time timestamp(0) NOT NULL,
+    change_by INTEGER NOT NULL
+);
+
+--
+-- roles
+--
+CREATE TABLE roles
+(
+    id serial,
+    name VARCHAR (100) NOT NULL,
+    comments VARCHAR (250),
+    valid_id SMALLINT NOT NULL,
+    create_time timestamp(0) NOT NULL,
+    create_by INTEGER NOT NULL,
+    change_time timestamp(0) NOT NULL,
+    change_by INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE (name)
+);
+
+--
+-- role_user
+--
+CREATE TABLE role_user
+(
+    role_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    create_time timestamp(0) NOT NULL,
+    create_by INTEGER NOT NULL,
+    change_time timestamp(0) NOT NULL,
+    change_by INTEGER NOT NULL
+);
+
