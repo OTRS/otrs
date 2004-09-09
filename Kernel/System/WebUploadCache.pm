5690f50dfd8bb789a081868cc5b0efdcefbbf80e
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-09-09 14:48:36 +0000

    added pod and added cleanup function

--- Kernel/System/WebUploadCache.pm
+++ Kernel/System/WebUploadCache.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Kernel/System/WebUploadCache.pm - a fs upload cache
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: WebUploadCache.pm,v 1.2 2004-07-22 05:39:34 martin Exp $
+# $Id: WebUploadCache.pm,v 1.2.2.1 2004-09-09 14:48:36 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 
 use vars qw($VERSION);
 
-$VERSION = '$Revision: 1.2 $ ';
+$VERSION = '$Revision: 1.2.2.1 $ ';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -37,16 +37,20 @@ All upload cache functions.
 create param object
 
   use Kernel::Config;
+  use Kernel::System::Log;
   use Kernel::System::WebUploadCache;
 
   my $ConfigObject = Kernel::Config->new();
-  my $ParamObject = Kernel::System::WebUploadCache->new(
+  my $LogObject    = Kernel::System::Log->new(
       ConfigObject => $ConfigObject,
+  );
+  my $UploadParamObject = Kernel::System::WebUploadCache->new(
+      ConfigObject => $ConfigObject,
+      LogObject => $LogObject,
   );
 
 =cut
 
-# --
 sub new {
     my $Type = shift;
     my %Param = @_;
@@ -67,11 +71,30 @@ sub new {
     return $Self;
 }
 
+=item FormIDCreate()
+
+create a new form id
+
+  my $FormID = $UploadParamObject->FormIDCreate();
+
+=cut
+
 sub FormIDCreate {
     my $Self = shift;
     my %Param = @_;
-    return time().rand(12341241);
+    # cleanup temp form ids
+    $Self->FormIDCleanUp();
+    # return requested form id
+    return time().'.'.rand(12341241);
 }
+
+=item FormIDRemove()
+
+remove all data with form id
+
+  $UploadParamObject->FormIDRemove(FormID => 123456);
+
+=cut
 
 sub FormIDRemove {
     my $Self = shift;
@@ -89,6 +112,19 @@ sub FormIDRemove {
         unlink "$File" || die "$!";
     }
 }
+
+=item FormIDAddFile()
+
+add a file to the form id
+
+  $UploadParamObject->FormIDAddFile(
+      FormID => 12345,
+      Filename => 'somefile.html',
+      Content => $FileInSting,
+      ContentType => 'text/html',
+  );
+
+=cut
 
 sub FormIDAddFile {
     my $Self = shift;
@@ -110,6 +146,17 @@ sub FormIDAddFile {
     return 1;
 }
 
+=item FormIDRemoveFile()
+
+removes a file to the form id
+
+  $UploadParamObject->FormIDRemoveFile(
+      FormID => 12345,
+      FileID => 1,
+  );
+
+=cut
+
 sub FormIDRemoveFile {
     my $Self = shift;
     my %Param = @_;
@@ -126,6 +173,18 @@ sub FormIDRemoveFile {
     unlink "$Self->{TempDir}/$Param{FormID}.$File{Filename}.ContentType" || die "$!: /tmp/up/$File{Filename}.ContentType";
     return 1;
 }
+
+=item FormIDGetAllFilesData()
+
+returns a array with hash ref of all form id files
+
+  my @Data = $UploadParamObject->FormIDGetAllFilesData(
+      FormID => 12345,
+  );
+
+  Return data of on hash is Content, ContentType, Filename, Filesize, FileID;
+
+=cut
 
 sub FormIDGetAllFilesData {
     my $Self = shift;
@@ -184,6 +243,20 @@ sub FormIDGetAllFilesData {
 
 }
 
+=item FormIDGetAllFilesMeta()
+
+returns a array with hash ref of all form id files
+
+Note: No Content will be returned, just meta data.
+
+  my @Data = $UploadParamObject->FormIDGetAllFilesMeta(
+      FormID => 12345,
+  );
+
+  Return data of on hash is Filename, Filesize, FileID;
+
+=cut
+
 sub FormIDGetAllFilesMeta {
     my $Self = shift;
     my %Param = @_;
@@ -226,7 +299,37 @@ sub FormIDGetAllFilesMeta {
     return @Data;
 }
 
-# --
+=item FormIDCleanUp()
+
+Removed no longer needed tmp file.
+
+Each file older then 2 days will be removed.
+
+  $UploadParamObject->FormIDCleanUp();
+
+=cut
+
+sub FormIDCleanUp {
+    my $Self = shift;
+    my %Param = @_;
+    my $CurrentTile = time() - (60*60*24*2);
+    my @List = glob("$Self->{TempDir}/*");
+    my %RemoveFormIDs = ();
+    foreach my $File (@List) {
+        # get FormID
+        $File =~ s/^.*\/(.+?)\..+?$/$1/;
+        if ($CurrentTile > $File) {
+            if (!$RemoveFormIDs{$File}) {
+                $RemoveFormIDs{$File} = 1;
+            }
+        }
+    }
+    foreach (keys %RemoveFormIDs) {
+        $Self->FormIDRemove(FormID => $_);
+    }
+    return 1;
+}
+
 1;
 
 =head1 TERMS AND CONDITIONS
@@ -241,6 +344,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.2 $ $Date: 2004-07-22 05:39:34 $
+$Revision: 1.2.2.1 $ $Date: 2004-09-09 14:48:36 $
 
 =cut
