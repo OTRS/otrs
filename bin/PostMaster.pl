6cd9d91d884d73446a203fd0ceb0955332d4e6c5
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-09-16 07:50:51 +0000

    fixed bug 519 - PostMaster.pl bounces mail if database is down

--- bin/PostMaster.pl
+++ bin/PostMaster.pl
@@ -1,20 +1,20 @@
 #!/usr/bin/perl -w
 # --
 # PostMaster.pl - the global eMail handle for email2db
-# Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: PostMaster.pl,v 1.16 2003-10-29 21:07:41 martin Exp $
+# $Id: PostMaster.pl,v 1.17 2004-09-16 07:50:51 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -29,13 +29,13 @@ use lib dirname($RealBin)."/Kernel/cpan-lib";
 use strict;
 
 # --
-# to get it readable for the webserver user and writable for otrs 
+# to get it readable for the webserver user and writable for otrs
 # group (just in case)
 # --
 umask 002;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.16 $';
+$VERSION = '$Revision: 1.17 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 use Getopt::Std;
@@ -65,49 +65,65 @@ if (!$Opts{'q'}) {
     $Opts{'q'} = '';
 }
 
-# --
-# create common objects 
-# --
-my %CommonObject = ();
-$CommonObject{ConfigObject} = Kernel::Config->new();
-$CommonObject{LogObject} = Kernel::System::Log->new(
-    LogPrefix => 'OTRS-PM',
-    %CommonObject,
-);
-$CommonObject{DBObject} = Kernel::System::DB->new(%CommonObject);
-# debug info
-if ($Opts{'d'}) {
-    $CommonObject{LogObject}->Log(
-        Priority => 'debug',
-        Message => 'Global OTRS email handle (PostMaster.pl) started...',
+# Wrap the majority of the script in an "eval" block so that any
+# unexpected (but probably transient) fatal errors (such as the
+# database being unavailable) can be trapped without causing a
+# bounce
+eval {
+    # --
+    # create common objects
+    # --
+    my %CommonObject = ();
+    $CommonObject{ConfigObject} = Kernel::Config->new();
+    $CommonObject{LogObject} = Kernel::System::Log->new(
+        LogPrefix => 'OTRS-PM',
+        %CommonObject,
     );
-}
-# get email from SDTIN 
-my @Email = <STDIN>;
-if (!@Email) {
-    $CommonObject{LogObject}->Log(
-        Priority => 'error',
-        Message => 'Got not email on STDIN!',
+    $CommonObject{DBObject} = Kernel::System::DB->new(%CommonObject);
+    # debug info
+    if ($Opts{'d'}) {
+        $CommonObject{LogObject}->Log(
+            Priority => 'debug',
+            Message => 'Global OTRS email handle (PostMaster.pl) started...',
+        );
+    }
+    # get email from SDTIN
+    my @Email = <STDIN>;
+    if (!@Email) {
+        $CommonObject{LogObject}->Log(
+            Priority => 'error',
+            Message => 'Got not email on STDIN!',
+        );
+        exit (1);
+    }
+    # common objects
+    $CommonObject{PostMaster} = Kernel::System::PostMaster->new(
+        %CommonObject,
+        Email => \@Email,
+        Trusted => $Opts{'t'},
+        Debug => $Opts{'d'},
     );
-    exit (1);
-}
-# common objects
-$CommonObject{PostMaster} = Kernel::System::PostMaster->new(
-    %CommonObject, 
-    Email => \@Email,
-    Trusted => $Opts{'t'},
-    Debug => $Opts{'d'},
-);
-$CommonObject{PostMaster}->Run(
-    Queue => $Opts{'q'},
-);
+    $CommonObject{PostMaster}->Run(
+        Queue => $Opts{'q'},
+    );
 
-# debug info
-if ($Opts{'d'}) {
-    $CommonObject{LogObject}->Log(
-        Priority => 'debug',
-        Message => 'Global OTRS email handle (PostMaster.pl) stoped.',
-    );
+    # debug info
+    if ($Opts{'d'}) {
+        $CommonObject{LogObject}->Log(
+            Priority => 'debug',
+            Message => 'Global OTRS email handle (PostMaster.pl) stoped.',
+        );
+    }
+};
+
+if ($@) {
+    # An unexpected problem occurred (for example, the database was
+    # unavailable). Return an EX_TEMPFAIL error to cause the mail
+    # program to requeue the message instead of immediately bouncing
+    # it; see sysexits.h. Most mail programs will retry an
+    # EX_TEMPFAIL delivery for about four days, then bounce the
+    # message.)
+    exit 75;
 }
-# --
+
 exit (0);
