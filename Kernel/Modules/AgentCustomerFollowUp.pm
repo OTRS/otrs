ff87e279cc8f774570dc7d629b25133ef1a28ca0
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-10-12 10:40:56 +0000

    fixed agent can be customer modus

--- Kernel/Modules/AgentCustomerFollowUp.pm
+++ Kernel/Modules/AgentCustomerFollowUp.pm
@@ -3,7 +3,7 @@
 # if the agent is customer
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentCustomerFollowUp.pm,v 1.10 2004-09-16 22:04:00 martin Exp $
+# $Id: AgentCustomerFollowUp.pm,v 1.11 2004-10-12 10:40:55 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,15 +18,15 @@ use Kernel::System::Queue;
 use Kernel::System::State;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.10 $';
+$VERSION = '$Revision: 1.11 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
 sub new {
     my $Type = shift;
     my %Param = @_;
-    # allocate new hash for object    
-    my $Self = {}; 
+    # allocate new hash for object
+    my $Self = {};
     bless ($Self, $Type);
     # get common objects
     foreach (keys %Param) {
@@ -49,54 +49,62 @@ sub Run {
     my $Self = shift;
     my %Param = @_;
     my $Output;
-    
+
     if ($Self->{Subaction} eq '' || !$Self->{Subaction}) {
         # --
         # if there is no ticket id!
         # --
         if (!$Self->{TicketID}) {
-            $Output .= $Self->{LayoutObject}->Header(Title => 'Error');
-            $Output .= $Self->{LayoutObject}->Error(
+            return $Self->{LayoutObject}->ErrorScreen(
                 Message => 'No TicketID!',
                 Comment => 'Please contact your admin',
             );
-            $Output .= $Self->{LayoutObject}->Footer();
-            return $Output;
         }
         else {
-            # --
             # header
-            # --
             $Output .= $Self->{LayoutObject}->Header(Area => 'Agent', Title => 'Compose Follow up');
+            # get user lock data
+            my %LockedData = $Self->{TicketObject}->GetLockedCount(UserID => $Self->{UserID});
             # build NavigationBar
-            $Output .= $Self->{LayoutObject}->NavigationBar();
-            my $Tn = $Self->{TicketObject}->TicketNumberLookup(TicketID => $Self->{TicketID});
-            # --
-            # print form ...
-            # --
-            $Output .= $Self->{LayoutObject}->AgentCustomerMessage(
+            $Output .= $Self->{LayoutObject}->NavigationBar(LockData => \%LockedData);
+
+            my %Ticket = $Self->{TicketObject}->TicketGet(TicketID => $Self->{TicketID});
+            # next stats
+            my %NextStates = $Self->{TicketObject}->StateList(
+                Type => 'CustomerPanelDefaultNextCompose',
                 TicketID => $Self->{TicketID},
-                QueueID => $Self->{QueueID},
-                TicketNumber => $Tn,
+                Action => $Self->{Action},
+                CustomerUserID => $Self->{UserID},
             );
+            $Param{'NextStatesStrg'} = $Self->{LayoutObject}->OptionStrgHashRef(
+                Data => \%NextStates,
+                Name => 'StateID',
+                Selected => $Self->{ConfigObject}->Get('CustomerPanelDefaultNextComposeType')
+            );
+
+            # get output back
+            $Output .= $Self->{LayoutObject}->Notify(
+                Info =>
+                  $Self->{LayoutObject}->{LanguageObject}->Get('You are the customer user of this message - customer modus!'),
+            );
+            $Output .= $Self->{LayoutObject}->Output(
+                TemplateFile => 'AgentCustomerMessage',
+                Data => { %Param, %Ticket },
+            );
+
             $Output .= $Self->{LayoutObject}->Footer();
             return $Output;
         }
     }
     elsif ($Self->{Subaction} eq 'Store') {
-        # check if it is possible to do the follow up
-        my $QueueID = $Self->{TicketObject}->TicketQueueID(
-            TicketID => $Self->{TicketID},
-        );
-        # get follow up option (possible or not)
-        my $FollowUpPossible = $Self->{QueueObject}->GetFollowUpOption(
-            QueueID => $QueueID,
-        );
         # get ticket data
         my %Ticket = $Self->{TicketObject}->TicketGet(
             TicketID => $Self->{TicketID},
         );
-
+        # get follow up option (possible or not)
+        my $FollowUpPossible = $Self->{QueueObject}->GetFollowUpOption(
+            QueueID => $Ticket{QueueID},
+        );
         if ($FollowUpPossible =~ /(new ticket|reject)/i && $Ticket{StateType} =~ /^close/i) {
             $Output = $Self->{LayoutObject}->Header(Title => 'Error');
             $Output .= $Self->{LayoutObject}->Warning(
@@ -107,19 +115,18 @@ sub Run {
             return $Output;
         }
         my $Subject = $Self->{ParamObject}->GetParam(Param => 'Subject') || 'Follow up!';
-        my $Text = $Self->{ParamObject}->GetParam(Param => 'Note');
-        my $StateID = $Self->{ParamObject}->GetParam(Param => 'ComposeStateID');
-        my $From = "$Self->{UserFirstname} $Self->{UserLastname} <$Self->{UserEmail}>"; 
+        my $Text = $Self->{ParamObject}->GetParam(Param => 'Body');
+        my $StateID = $Self->{ParamObject}->GetParam(Param => 'StateID');
+        my $From = "$Self->{UserFirstname} $Self->{UserLastname} <$Self->{UserEmail}>";
         if (my $ArticleID = $Self->{TicketObject}->ArticleCreate(
             TicketID => $Self->{TicketID},
             ArticleType => $Self->{ConfigObject}->Get('CustomerPanelArticleType'),
             SenderType => $Self->{ConfigObject}->Get('CustomerPanelSenderType'),
             From => $From,
-            To => $Self->{UserLogin},
             Subject => $Subject,
             Body => $Text,
             ContentType => "text/plain; charset=$Self->{LayoutObject}->{'UserCharset'}",
-            UserID => $Self->{UserID}, 
+            UserID => $Self->{UserID},
             OrigHeader => {
                 From => $From,
                 To => 'System',
@@ -127,7 +134,7 @@ sub Run {
                 Body => $Text,
             },
             HistoryType => $Self->{ConfigObject}->Get('CustomerPanelHistoryType'),
-            HistoryComment => $Self->{ConfigObject}->Get('CustomerPanelHistoryComment'),
+            HistoryComment => $Self->{ConfigObject}->Get('CustomerPanelHistoryComment') || '%%',
         )) {
           # --
           # set state
@@ -138,55 +145,41 @@ sub Run {
           my $NextState = $StateData{Name} ||
             $Self->{ConfigObject}->Get('CustomerPanelDefaultNextComposeType') || 'open';;
           $Self->{TicketObject}->StateSet(
-            TicketID => $Self->{TicketID},
-            ArticleID => $ArticleID,
-            State => $NextState,
-            UserID => $Self->{UserID},
+              TicketID => $Self->{TicketID},
+              ArticleID => $ArticleID,
+              State => $NextState,
+              UserID => $Self->{UserID},
           );
-          # --
           # set answerd
-          # --
           $Self->{TicketObject}->TicketSetAnswered(
-            TicketID => $Self->{TicketID},
-            UserID => $Self->{UserID},
-            Answered => 0,
+              TicketID => $Self->{TicketID},
+              UserID => $Self->{UserID},
+              Answered => 0,
           );
-          # --
           # get attachment
-          # -- 
           my %UploadStuff = $Self->{ParamObject}->GetUploadAll(
-              Param => 'file_upload', 
+              Param => 'file_upload',
               Source => 'String',
           );
           if (%UploadStuff) {
               $Self->{TicketObject}->ArticleWriteAttachment(
                   %UploadStuff,
-                  ArticleID => $ArticleID, 
+                  ArticleID => $ArticleID,
                   UserID => $Self->{UserID},
               );
           }
-         # --
          # redirect to zoom view
-         # --        
-         return $Self->{LayoutObject}->Redirect(
-              OP => "Action=AgentZoom&TicketID=$Self->{TicketID}",
-         );
+         return $Self->{LayoutObject}->Redirect(OP => $Self->{LastScreenView});
       }
       else {
-        $Output = $Self->{LayoutObject}->Header(Title => 'Error');
-        $Output .= $Self->{LayoutObject}->Error();
-        $Output .= $Self->{LayoutObject}->Footer();
-        return $Output;
+        return $Self->{LayoutObject}->ErrorScreen();
       }
     }
     else {
-        $Output .= $Self->{LayoutObject}->Header(Title => 'Error');
-        $Output .= $Self->{LayoutObject}->Error(
+        return $Self->{LayoutObject}->ErrorScreen(
             Message => 'No Subaction!!',
             Comment => 'Please contact your admin',
         );
-        $Output .= $Self->{LayoutObject}->Footer();
-        return $Output;
     }
 }
 # --
