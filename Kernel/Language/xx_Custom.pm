f626b33ba56d16931e1a92783b70e089c8a7bc8a
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-11-04 11:05:28 +0000

    added other syntax example

--- Kernel/Language/xx_Custom.pm
+++ Kernel/Language/xx_Custom.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Language/xx_Custom.pm - provides xx custom language translation
-# Copyright (C) 2001-2004 Martin Edenhofer <martin at otrs.org>
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: xx_Custom.pm,v 1.1 2004-06-04 08:50:41 martin Exp $
+# $Id: xx_Custom.pm,v 1.2 2004-11-04 11:05:28 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -13,7 +13,7 @@ package Kernel::Language::xx_Custom;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.1 $';
+$VERSION = '$Revision: 1.2 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -23,8 +23,17 @@ sub Data {
 
     # $$START$$
 
+    # own translations
     $Self->{Translation}->{'Lock'} = 'Lala';
     $Self->{Translation}->{'Unlock'} = 'Lulu';
+
+    # or a other syntax would be
+#    $Self->{Translation} = {
+#        %{$Self->{Translation}},
+#        # own translations
+#        Lock => 'Lala',
+#        UnLock => 'Lulu',
+#    };
 
     # $$STOP$$
 }
