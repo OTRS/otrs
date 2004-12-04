3fe5184098dcd304e76f1eca4f7affcd1d43c29e
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-12-04 18:29:34 +0000

    new permissions for package manager

--- bin/SetPermissions.sh
+++ bin/SetPermissions.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 # --
-# SetPermissions.sh - to set the otrs permissions 
-# Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
+# SetPermissions.sh - to set the otrs permissions
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: SetPermissions.sh,v 1.22 2004-02-03 01:01:09 martin Exp $
+# $Id: SetPermissions.sh,v 1.23 2004-12-04 18:29:34 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
 
-echo "SetPermissions.sh <\$Revision: 1.22 $> - set OTRS file permissions"
-echo "Copyright (c) 2001-2003 Martin Edenhofer <martin@otrs.org>"
+echo "SetPermissions.sh <\$Revision: 1.23 $> - set OTRS file permissions"
+echo "Copyright (c) 2001-2004 Martin Edenhofer <martin@otrs.org>"
 
-if ! test $1 || ! test $2 || ! test $3; then 
+if ! test $1 || ! test $2 || ! test $3; then
     # --
     # check required options
     # --
     echo ""
     echo "Usage: SetPermissions.sh <OTRS_HOME> <OTRS_USER> <WEBSERVER_USER> [OTRS_GROUP] [WEB_GROUP]"
     echo ""
-    echo "  Try: SetPermissions.sh /opt/otrs otrs wwwrun"                      
+    echo "  Try: SetPermissions.sh /opt/otrs otrs wwwrun"
     echo ""
     exit 1;
-else 
+else
     # --
     # system settings
     # --
@@ -51,9 +51,11 @@ fi
 # set permission
 # --
 echo "Setting file permissions... "
-# set all files to root.root (safe is safe)
-echo "chown -R 0:0 $OTRSDEST"
-chown -R 0:0 $OTRSDEST
+# set all files writeabel for webserver user (needed for package manager)
+echo "chown -R $WEBUSER:$OTRSGROUP $OTRSDEST"
+chown -R $WEBUSER:$OTRSGROUP $OTRSDEST
+echo "chmod -R og+w $OTRSDEST/Kernel/"
+chmod -R og+w $OTRSDEST/Kernel/
 
 # set the $HOME to the OTRS user
 echo "chown $OTRSUSER:$OTRSGROUP $OTRSDEST"
@@ -65,13 +67,13 @@ chown $OTRSUSER:$OTRSGROUP $OTRSDEST/.fetchmailrc
 echo "chmod 0710 $OTRSDEST/.fetchmailrc"
 chmod 0710 $OTRSDEST/.fetchmailrc
 
-# set procmailrc 
+# set procmailrc
 echo "chown $OTRSUSER:$OTRSGROUP $OTRSDEST/.procmailrc"
 chown $OTRSUSER:$OTRSGROUP $OTRSDEST/.procmailrc
 echo "chmod 0644 $OTRSDEST/.procmailrc"
 chmod 0644 $OTRSDEST/.procmailrc
 
-# set mailfilter 
+# set mailfilter
 echo "chown $OTRSUSER:$OTRSGROUP $OTRSDEST/.mailfilter"
 chown $OTRSUSER:$OTRSGROUP $OTRSDEST/.mailfilter
 echo "chmod 0600 $OTRSDEST/.mailfilter"
@@ -87,7 +89,7 @@ fi
 # var/*
 # --
 # set the var directory to OTRS and webserver user
-echo "chown -R $OTRSUSER:$WEBGROUP $OTRSDEST/var/" 
+echo "chown -R $OTRSUSER:$WEBGROUP $OTRSDEST/var/"
 chown -R $OTRSUSER:$WEBGROUP $OTRSDEST/var/
 chmod -R 2775 $OTRSDEST/var/article/
 chmod -R 2775 $OTRSDEST/var/log/
@@ -95,7 +97,7 @@ chmod -R 2775 $OTRSDEST/var/tmp/
 
 # set the var/sessions directory to OTRS and webserver user
 echo "chown -R $WEBUSER:$WEBGROUP $OTRSDEST/var/sessions/"
-chown -R $WEBUSER:$WEBGROUP $OTRSDEST/var/sessions/ 
+chown -R $WEBUSER:$WEBGROUP $OTRSDEST/var/sessions/
 
 # set the var/log/TicketCounter.log file to OTRS and webserver user
 echo "touch && chown $OTRSUSER:$WEBGROUP $OTRSDEST/var/log/TicketCounter.log"
@@ -113,7 +115,7 @@ chmod -R 755 $OTRSDEST/bin/
 # set the DeleteSessionIDs.pl just to OTRS user
 echo "(chown && chmod 700) $OTRSUSER:0 $OTRSDEST/bin/DeleteSessionIDs.pl"
 chmod 700 $OTRSDEST/bin/DeleteSessionIDs.pl
-chown $OTRSUSER:0 $OTRSDEST/bin/DeleteSessionIDs.pl 
+chown $OTRSUSER:0 $OTRSDEST/bin/DeleteSessionIDs.pl
 
 # set the UnlockTickets.pl just to OTRS user
 echo "(chown && chmod 700) $OTRSUSER:0 $OTRSDEST/bin/UnlockTickets.pl"
