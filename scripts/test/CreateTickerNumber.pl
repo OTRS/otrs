80ae3f951c3e501609f7d9db0bc1d8863d3e933f
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-12-06 22:22:12 +0000

    updated to new TicketObject, TicketSubjectBuild()

--- scripts/test/CreateTickerNumber.pl
+++ scripts/test/CreateTickerNumber.pl
@@ -1,20 +1,20 @@
 #!/usr/bin/perl -w
 # --
 # scripts/test/CreateTickerNumber.pl - test script to generate a ticket number
-# Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: CreateTickerNumber.pl,v 1.1 2003-10-13 20:29:32 martin Exp $
+# $Id: CreateTickerNumber.pl,v 1.2 2004-12-06 22:22:12 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -29,12 +29,13 @@ use lib dirname($RealBin).'/../Kernel/cpan-lib';
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.1 $';
+$VERSION = '$Revision: 1.2 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 use Kernel::Config;
 use Kernel::System::Log;
 use Kernel::System::DB;
+use Kernel::System::Time;
 use Kernel::System::Ticket;
 
 # --
@@ -46,6 +47,7 @@ $CommonObject{LogObject} = Kernel::System::Log->new(
     LogPrefix => 'OTRS-test-CreateTickerNumber.pl',
     %CommonObject,
 );
+$CommonObject{TimeObject} = Kernel::System::Time->new(%CommonObject);
 $CommonObject{DBObject} = Kernel::System::DB->new(%CommonObject);
 $CommonObject{TicketObject} = Kernel::System::Ticket->new(%CommonObject);
 
@@ -62,7 +64,10 @@ print "TicketNumber: $Tn\n";
 print "\n";
 print "Match test with current settings - GetTNByString():\n";
 print "---------------------------------------------------\n";
-my $String = $Hook.': '.$Tn;
+my $String = "Re: ".$CommonObject{TicketObject}->TicketSubjectBuild(
+    TicketNumber => $Tn,
+    Subject => 'Some Test',
+);
 if ($CommonObject{TicketObject}->GetTNByString($String)) {
     print "OK ($String).\n";
 }
