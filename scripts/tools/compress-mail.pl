4de6438781e5e091a8332f204bf81c29f166026a
Martin Edenhofer <martin.edenhofer@otrs.com>
2004-12-07 14:33:34 +0000

    added config options CompressAttachmentMinSize, CompressAttachmentMaxSize and CompressAttachmentNoTouch and the checks for his settings

--- scripts/tools/compress-mail.pl
+++ scripts/tools/compress-mail.pl
@@ -3,7 +3,7 @@
 # scripts/tools/compress-mail.pl - compress email, zip attachments
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: compress-mail.pl,v 1.2 2004-12-07 09:27:55 martin Exp $
+# $Id: compress-mail.pl,v 1.3 2004-12-07 14:33:34 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@ use lib dirname($RealBin)."/../Kernel/cpan-lib";
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.2 $';
+$VERSION = '$Revision: 1.3 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -60,48 +60,71 @@ $CommonObject{ParseObject} = Kernel::System::EmailParser->new(
 );
 my $MSGID = $CommonObject{ParseObject}->GetParam(WHAT => 'Message-ID') || 'No Message-ID';
 my $Touch = 0;
+my $NoTouch = 0;
 my $Counter = 0;
+my $CompressAttachmentMinSize = $CommonObject{ConfigObject}->Get('CompressAttachmentMinSize') || (1014*3);
+my $CompressAttachmentMaxSize = $CommonObject{ConfigObject}->Get('CompressAttachmentMaxSize') || (1024*1024*60);
+my $CompressAttachmentNoTouch = $CommonObject{ConfigObject}->Get('CompressAttachmentNoTouch') || '(application/(x-pkcs.-signature|pgp-signature))';
 my @Attachments = $CommonObject{ParseObject}->GetAttachments();
 my $Entity;
 my $Header = $CommonObject{ParseObject}->{HeaderObject}->header_hashref();
+# check if this mail can be modified (not touch signed or crypted emails)
 foreach my $Attachment (@Attachments) {
-    $Counter++;
-    my $Compress = 0;
-    foreach (@Compress) {
-        if ($Attachment->{Filename} =~ /\.$_$/i) {
-            $Compress = 1;
-            $Touch = 1;
-        }
+    if ($Attachment->{MimeType} =~ /$CompressAttachmentNoTouch/i) {
+        $NoTouch = 1;
     }
-#    if ($Counter > 1 && $Attachment->{ContentType} && $Attachment->{ContentType} =~ /^text\//i) {
-#        $Compress = 1;
-#    }
-    if ($Compress) {
-        $CommonObject{LogObject}->Log(
-            Priority => 'notice',
-            Message => "Compress $Attachment->{Filename} ($MSGID)!",
-        );
-        $Attachment->{Filename} .= '.gz';
-        $Attachment->{Content} = Compress::Zlib::memGzip($Attachment->{Content});
-        $Attachment->{ContentType} = 'application/x-gzip';
-    }
-    if (!$Entity) {
-        $Entity = MIME::Entity->build(
-            %{$Header},
-#            Filename => $Attachment->{Filename},
-            Data     => $Attachment->{Content},
-#            Type     => $Attachment->{ContentType},
-#            Encoding => "base64",
-        );
-    }
-    $Entity->attach(
-        Filename => $Attachment->{Filename},
-        Data     => $Attachment->{Content},
-        Type     => $Attachment->{ContentType},
-        Encoding => "base64",
-    );
 }
-
+# just compress touchable emails
+if (!$NoTouch) {
+    foreach my $Attachment (@Attachments) {
+        $Counter++;
+        my $Compress = 0;
+        # just compress attachments after the first one
+        if ($Counter > 1) {
+            # check if attachment should be compresed
+            foreach (@Compress) {
+                if ($Attachment->{Filename} =~ /\.$_$/i) {
+                    # just compress attachments within a size range
+                    use bytes;
+                    my $FileSize = length($Attachment->{Content});
+                    no bytes;
+    	            if ($FileSize > $CompressAttachmentMinSize && $FileSize < $CompressAttachmentMaxSize) {
+                        $Compress = 1;
+                        $Touch = 1;
+                    }
+            	}
+            }
+        }
+        # compress attachment
+        if ($Compress) {
+            $CommonObject{LogObject}->Log(
+                Priority => 'notice',
+                Message => "Compress $Attachment->{Filename} ($MSGID)!",
+            );
+            $Attachment->{Filename} .= '.gz';
+            $Attachment->{Content} = Compress::Zlib::memGzip($Attachment->{Content});
+            $Attachment->{ContentType} = 'application/x-gzip';
+        }
+        # build new entity
+        if (!$Entity) {
+            $Entity = MIME::Entity->build(
+                %{$Header},
+#               Filename => $Attachment->{Filename},
+                Data     => $Attachment->{Content},
+#               Type     => $Attachment->{ContentType},
+#               Encoding => "base64",
+            );
+        }
+        # add attachment to entity
+        $Entity->attach(
+            Filename => $Attachment->{Filename},
+            Data     => $Attachment->{Content},
+            Type     => $Attachment->{ContentType},
+            Encoding => "base64",
+        );
+    }
+}
+# return old email 1:1
 if (!$Touch) {
     $CommonObject{LogObject}->Log(
         Priority => 'notice',
@@ -114,6 +137,7 @@ if (!$Touch) {
         print $_;
     }
 }
+# return modified email
 else {
     $CommonObject{LogObject}->Log(
         Priority => 'notice',
