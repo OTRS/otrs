488383c905d26779c6a85ec0bf1425b774a38791
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-04-19 07:28:07 +0000

    added CreateItemUpdate()

--- Kernel/System/Config.pm
+++ Kernel/System/Config.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Config.pm - all system config tool functions
 # Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Config.pm,v 1.5 2005-04-14 13:10:17 martin Exp $
+# $Id: Config.pm,v 1.6 2005-04-19 07:28:07 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::XML;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.5 $';
+$VERSION = '$Revision: 1.6 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -77,6 +77,8 @@ sub new {
 
     # read all config files
     $Self->_Init();
+    # reorga of old config
+    $Self->CreateConfig();
 
     return $Self;
 }
@@ -162,17 +164,64 @@ sub CreateConfig {
             my %Config = $Self->ConfigItemGet(
                 Name => $ConfigItem->{Name}, 
             );
+            $ConfigItem->{Name} =~ s/\\/\\\\/g;
+            $ConfigItem->{Name} =~ s/'/\'/g;
+            $ConfigItem->{Name} =~ s/###/'}->{'/g;
             if ($ConfigItem->{Setting}->[1]->{String}) {
                 print OUT "    \$Self->{'$ConfigItem->{Name}'} = '$ConfigItem->{Setting}->[1]->{String}->[1]->{Content}';\n";
             }
             if ($ConfigItem->{Setting}->[1]->{Option}) {
                 print OUT "    \$Self->{'$ConfigItem->{Name}'} = '$ConfigItem->{Setting}->[1]->{Option}->[1]->{SelectedID}';\n";
             }
+            print OUT "    \$Self->{'$ConfigItem->{Name}::Valid'} = '$ConfigItem->{Valid}';\n";
         }
     }
 
     close(OUT);
 
+}
+
+=item ConfigItemUpdate()
+
+submit config settings and save it
+
+    $ConfigToolObject->ConfigItemUpdate(
+        Valid => 1,
+        Key => 'WebUploadCacheModule',
+        Value => 'Kernel::System::Web::UploadCache::DB',
+    );
+    
+=cut
+
+sub CreateItemUpdate {
+    my $Self = shift;
+    my %Param = @_;
+    my %UsedKeys = ();
+    my $Home = $Self->{ConfigObject}->Get('Home');
+    # check needed stuff
+    foreach (qw(Valid Key Value)) {
+        if (!defined($Param{$_})) {
+            $Self->{LogObject}->Log(Priority => 'error', Message => "Need $_!");
+            return;
+        }
+    }
+    if (!open(OUT, ">> $Home/Kernel/Config/Files/ZZZAuto.pm")) {
+        $Self->{LogObject}->Log(Priority => 'error', Message => "Can't write $Home/Kernel/Config/Files/ZZZAuto.pm!");
+        return;
+    }
+    else {
+        $Param{Key} =~ s/\\/\\\\/g;
+        $Param{Key} =~ s/'/\'/g;
+        $Param{Key} =~ s/###/'}->{'/g;
+        require Data::Dumper;
+        my $Dump = Data::Dumper::Dumper($Param{Value});
+        $Dump =~ s/\$VAR1/\$Self->{'$Param{Key}'}/;
+        print OUT $Dump;
+        print OUT "\$Self->{'$Param{Key}::Valid'} = '$Param{Valid}';\n";
+
+        close(OUT);
+        return 1;
+    }
 }
 
 =item ConfigItemGet()
@@ -198,13 +247,24 @@ sub ConfigItemGet {
 
     foreach my $ConfigItem (reverse @{$Self->{XMLConfig}}) {
         if ($ConfigItem->{Name} && $ConfigItem->{Name} eq $Param{Name}) {
+#            $ConfigItem->{Name} =~ s/\\/\\\\/g;
+#            $ConfigItem->{Name} =~ s/'/\'/g;
+#            $ConfigItem->{Name} =~ s/###/'}->{'/g;
+            # add current valid state
+            if (defined($Self->{ConfigObject}->Get($ConfigItem->{Name}.'::Valid'))) {
+                $ConfigItem->{Valid} = $Self->{ConfigObject}->Get($ConfigItem->{Name}.'::Valid');
+            }
             # update xml with current config setting
             if ($ConfigItem->{Setting}->[1]->{String}) {
+                # fill default
+                $ConfigItem->{Setting}->[1]->{String}->[1]->{Default} = $ConfigItem->{Setting}->[1]->{String}->[1]->{Content}; 
                 if (defined($Self->{ConfigObject}->Get($ConfigItem->{Name}))) {
                     $ConfigItem->{Setting}->[1]->{String}->[1]->{Content} = $Self->{ConfigObject}->Get($ConfigItem->{Name});
                 }
             }
             if ($ConfigItem->{Setting}->[1]->{Option}) {
+                # fill default
+                $ConfigItem->{Setting}->[1]->{Option}->[1]->{Default} = $ConfigItem->{Setting}->[1]->{Option}->[1]->{SelectedID}; 
                 if (defined($Self->{ConfigObject}->Get($ConfigItem->{Name}))) {
                     $ConfigItem->{Setting}->[1]->{Option}->[1]->{SelectedID} = $Self->{ConfigObject}->Get($ConfigItem->{Name});
                 }
@@ -313,7 +373,10 @@ sub ConfigSubGroupList {
 
 get a list of config items of a sub group
 
-    my %ConfigItemList = $ConfigToolObject->ConfigSubGroupConfigItemList(Name => 'Web');
+    my %ConfigItemList = $ConfigToolObject->ConfigSubGroupConfigItemList(
+        Group => 'Framework',
+        SubGroup => 'Web',
+    );
     
 =cut
 
@@ -321,23 +384,33 @@ sub ConfigSubGroupConfigItemList {
     my $Self = shift;
     my %Param = @_;
     # check needed stuff
-    foreach (qw(Name)) {
+    foreach (qw(Group SubGroup)) {
         if (!$Param{$_}) { 
             $Self->{LogObject}->Log(Priority => 'error', Message => "Need $_!");
             return;
         }
     }
-    my %List = ();
+    my @List = ();
     foreach my $ConfigItem (@{$Self->{XMLConfig}}) {
-        if ($ConfigItem->{SubGroup} && ref($ConfigItem->{SubGroup}) eq 'ARRAY') {
-            foreach my $SubGroup (@{$ConfigItem->{SubGroup}}) {
-                if ($SubGroup->{Content} && $SubGroup->{Content} eq $Param{Name}) {
-                    $List{$ConfigItem->{Name}} = $ConfigItem;
+        if ($ConfigItem->{Group} && ref($ConfigItem->{Group}) eq 'ARRAY') {
+            my $Hit = 0;
+            foreach my $Group (@{$ConfigItem->{Group}}) {
+                if ($Group->{Content} && $Group->{Content} eq $Param{Group}) {
+                    $Hit = 1;
+                }
+            }
+            if ($Hit) {
+                if ($ConfigItem->{SubGroup} && ref($ConfigItem->{SubGroup}) eq 'ARRAY') {
+                    foreach my $SubGroup (@{$ConfigItem->{SubGroup}}) {
+                        if ($SubGroup->{Content} && $SubGroup->{Content} eq $Param{SubGroup}) {
+                            push (@List, $ConfigItem->{Name});
+                        }
+                    }
                 }
             }
         }
     }
-    return %List;
+    return @List;
 }
 
 1;
@@ -354,6 +427,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.5 $ $Date: 2005-04-14 13:10:17 $
+$Revision: 1.6 $ $Date: 2005-04-19 07:28:07 $
 
 =cut
