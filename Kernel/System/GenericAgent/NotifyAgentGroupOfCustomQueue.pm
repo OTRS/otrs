51f723fd330c74ae63bfe3f004ba4f7a767fdad7
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-05-01 17:43:21 +0000

    just send notifications in working times

--- Kernel/System/GenericAgent/NotifyAgentGroupOfCustomQueue.pm
+++ Kernel/System/GenericAgent/NotifyAgentGroupOfCustomQueue.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/GenericAgent/NotifyAgentGroupOfCustomQueue.pm - generic agent notifications
-# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: NotifyAgentGroupOfCustomQueue.pm,v 1.5 2004-12-10 09:17:43 martin Exp $
+# $Id: NotifyAgentGroupOfCustomQueue.pm,v 1.6 2005-05-01 17:43:21 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,10 +15,9 @@ use strict;
 use Kernel::System::User;
 use Kernel::System::Email;
 use Kernel::System::Queue;
-use Kernel::System::Notification;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.5 $';
+$VERSION = '$Revision: 1.6 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -31,9 +30,11 @@ sub new {
     bless ($Self, $Type);
 
     # check needed objects
-    foreach (qw(DBObject ConfigObject LogObject TicketObject)) {
+    foreach (qw(DBObject ConfigObject LogObject TicketObject TimeObject)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
+    # 0=off; 1=on;
+    $Self->{Debug} = $Param{Debug} || 0;
 
     $Self->{UserObject} = Kernel::System::User->new(%Param);
     $Self->{EmailObject} = Kernel::System::Email->new(%Param);
@@ -48,11 +49,25 @@ sub Run {
 
     # get ticket data
     my %Ticket = $Self->{TicketObject}->TicketGet(%Param);
+    # check if bussines hours is, then send escalation info
+    my $CountedTime = $Self->{TimeObject}->WorkingTime(
+        StartTime => $Self->{TimeObject}->SystemTime()-(30*60),
+        StopTime => $Self->{TimeObject}->SystemTime(),
+    );
+    if (!$CountedTime) {
+        if ($Self->{Debug}) {
+            $Self->{LogObject}->Log(
+                Priority => 'debug',
+                Message => "Send not escalation for Ticket $Ticket{TicketNumber}/$Ticket{TicketID} because currently no working hours!",
+            );
+        }
+        return 1;
+    }
     # get agentss who are sucscribed the ticket queue to the custom queues
     my @UserIDs = $Self->{TicketObject}->GetSubscribedUserIDsByQueueID(
         QueueID => $Ticket{QueueID},
     );
-    # send each agent the escalation notification 
+    # send each agent the escalation notification
     foreach (@UserIDs) {
         my %User = $Self->{UserObject}->GetUserData(UserID => $_, Valid => 1);
         # send agent notification
