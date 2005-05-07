3048079e75dcfeca11e1791f62e5136f931d2c72
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-05-07 12:46:04 +0000

    added html access keys

--- Kernel/Output/HTML/NavBarLockedTickets.pm
+++ Kernel/Output/HTML/NavBarLockedTickets.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/NavBarLockedTickets.pm
 # Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: NavBarLockedTickets.pm,v 1.2 2005-02-17 07:09:01 martin Exp $
+# $Id: NavBarLockedTickets.pm,v 1.3 2005-05-07 12:46:04 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Output::HTML::NavBarLockedTickets;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.2 $';
+$VERSION = '$Revision: 1.3 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -42,19 +42,21 @@ sub Run {
 
     my $Text = $Self->{LayoutObject}->{LanguageObject}->Get('Locked tickets')." ($LockedData{All})";
     $Return{'0999999'} = {
-            Block => 'ItemPersonal',
-            Description => $Text,
-            Name => $Text,
-            Image => 'personal.png',
-            Link => 'Action=AgentTicketMailbox',
+        Block => 'ItemPersonal',
+        Description => $Text,
+        Name => $Text,
+        Image => 'personal.png',
+        Link => 'Action=AgentTicketMailbox',
+        AccessKey => 'k',
     };
     $Text = $Self->{LayoutObject}->{LanguageObject}->Get('new message')." ($LockedData{New})";
     $Return{'0999989'} = {
-            Block => 'ItemPersonal',
-            Description => $Text,
-            Name => $Text,
-            Image => 'new-message.png',
-            Link => 'Action=AgentTicketMailbox&Subaction=New',
+        Block => 'ItemPersonal',
+        Description => $Text,
+        Name => $Text,
+        Image => 'new-message.png',
+        Link => 'Action=AgentTicketMailbox&Subaction=New',
+        AccessKey => 'm',
     };
     return %Return;
 }
