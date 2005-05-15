24c9dbde62f37462b6a70b8a0fc8b67e95880557
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-05-15 09:08:59 +0000

    added DestQueue as param

--- Kernel/Modules/AgentTicketMove.pm
+++ Kernel/Modules/AgentTicketMove.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketMove.pm - move tickets to queues
 # Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentTicketMove.pm,v 1.4 2005-05-15 09:07:17 martin Exp $
+# $Id: AgentTicketMove.pm,v 1.5 2005-05-15 09:08:59 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::State;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.4 $';
+$VERSION = '$Revision: 1.5 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -42,6 +42,7 @@ sub new {
 
     # get params
     $Self->{DestQueueID} = $Self->{ParamObject}->GetParam(Param => 'DestQueueID');
+    $Self->{DestQueue} = $Self->{ParamObject}->GetParam(Param => 'DestQueue');
     $Self->{TicketUnlock} = $Self->{ParamObject}->GetParam(Param => 'TicketUnlock');
     $Self->{ExpandQueueUsers} = $Self->{ParamObject}->GetParam(Param => 'ExpandQueueUsers') || 0;
     $Self->{AllUsers} = $Self->{ParamObject}->GetParam(Param => 'AllUsers') || 0;
