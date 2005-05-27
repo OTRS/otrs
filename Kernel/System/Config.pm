2c1ee48b044f62613884a6f05b940cdee0dfd89c
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-05-27 16:09:55 +0000

    changed ConfigSubGroupList to show elements in subgroup

--- Kernel/System/Config.pm
+++ Kernel/System/Config.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Config.pm - all system config tool functions
 # Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Config.pm,v 1.27 2005-05-25 15:22:48 martin Exp $
+# $Id: Config.pm,v 1.28 2005-05-27 16:09:55 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::XML;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.27 $';
+$VERSION = '$Revision: 1.28 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -741,7 +741,12 @@ sub ConfigSubGroupList {
             if ($Hit) {
                 foreach my $SubGroup (@{$ConfigItem->{SubGroup}}) {
                     if ($SubGroup->{Content}) {
-                        $List{$SubGroup->{Content}} = $SubGroup->{Content};
+                        # get sub count
+                        my @List = $Self->ConfigSubGroupConfigItemList(
+                            Group => $Param{Name},
+                            SubGroup => $SubGroup->{Content},
+                        );
+                        $List{$SubGroup->{Content}} = ($#List+1);
                     }
                 }
             }
@@ -754,7 +759,7 @@ sub ConfigSubGroupList {
 
 get a list of config items of a sub group
 
-    my %ConfigItemList = $ConfigToolObject->ConfigSubGroupConfigItemList(
+    my @ConfigItemList = $ConfigToolObject->ConfigSubGroupConfigItemList(
         Group => 'Framework',
         SubGroup => 'Web',
     );
@@ -832,6 +837,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.27 $ $Date: 2005-05-25 15:22:48 $
+$Revision: 1.28 $ $Date: 2005-05-27 16:09:55 $
 
 =cut
