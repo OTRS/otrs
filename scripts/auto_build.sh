8c7e1609e716835c13b2d3652bb5aebd73e72236
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-09-27 21:45:23 +0000

    removed auto_docbuild.sh - till new build system is working

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: auto_build.sh,v 1.31 2005-08-04 09:53:24 martin Exp $
+# $Id: auto_build.sh,v 1.32 2005-09-27 21:45:23 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.31 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.32 $>"
 echo "Copyright (c) 2002-2004 Martin Edenhofer <martin@otrs.org>"
 
 PATH_TO_CVS_SRC=$1
@@ -166,7 +166,7 @@ rm -rf Kernel/cpan-lib/CGI.pm
 rm -rf Kernel/cpan-lib/CGI/
 
 # build html docu
-$PATH_TO_CVS_SRC/scripts/auto_docbuild.sh $PATH_TO_CVS_SRC/../doc/ > /dev/null
+#$PATH_TO_CVS_SRC/scripts/auto_docbuild.sh $PATH_TO_CVS_SRC/../doc/ > /dev/null
 mkdir doc/manual
 cp -R /tmp/OTRSDOC-package/* doc/manual/
 
