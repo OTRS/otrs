214495ce52df03118e8b3bb39c8dd37c184d1d6a
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-11-07 15:28:31 +0000

    fixed Quote() with negative numbers

--- Kernel/System/DB.pm
+++ Kernel/System/DB.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DB.pm - the global database wrapper to support different databases
 # Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: DB.pm,v 1.46 2005-10-31 10:07:03 martin Exp $
+# $Id: DB.pm,v 1.47 2005-11-07 15:28:31 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -17,7 +17,7 @@ use Kernel::System::Time;
 use Kernel::System::Encode;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.46 $';
+$VERSION = '$Revision: 1.47 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -260,7 +260,7 @@ sub Quote {
     }
     # numbers
     elsif ($Type && $Type eq 'Number') {
-        if ($Text !~ /^(\d{1,20}|\d{1,20}\.\d{1,20})$/) {
+        if ($Text !~ /^(\+|\-|)(\d{1,20}|\d{1,20}\.\d{1,20})$/) {
             $Self->{LogObject}->Log(
                 Caller => 1,
                 Priority => 'error',
@@ -704,6 +704,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.46 $ $Date: 2005-10-31 10:07:03 $
+$Revision: 1.47 $ $Date: 2005-11-07 15:28:31 $
 
 =cut
