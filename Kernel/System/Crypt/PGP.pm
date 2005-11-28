712d2daf2cf0e42e7ef7fec70125f5348f9fcd57
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-11-28 00:10:40 +0000

    added , between identfiers

--- Kernel/System/Crypt/PGP.pm
+++ Kernel/System/Crypt/PGP.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Crypt/PGP.pm - the main crypt module
 # Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: PGP.pm,v 1.10 2005-10-17 20:41:55 martin Exp $
+# $Id: PGP.pm,v 1.11 2005-11-28 00:10:40 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::System::Crypt::PGP;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.10 $';
+$VERSION = '$Revision: 1.11 $';
 $VERSION =~ s/^.*:\s(\d+\.\d+)\s.*$/$1/;
 
 =head1 NAME
@@ -352,9 +352,10 @@ sub PrivateKeySearch {
             $Key{Key} .= $3;
             $Key{Created} .= $4;
             $Key{Identifier} .= $5;
+            $Key{IdentifierMaster} .= $5;
         }
         if ($InKey && $Line =~ /^uid\s+(.*)/) {
-            $Key{Identifier} .= $1;
+            $Key{Identifier} .= ', '.$1;
         }
         if ($InKey && $Line =~ /^(ssb)\s(.+?)\/(.+?)\s(.+?)\s/) {
             $Key{Bit} = $2;
@@ -407,9 +408,10 @@ sub PublicKeySearch {
             $Key{Key} .= $3;
             $Key{Created} .= $4;
             $Key{Identifier} .= $5;
+            $Key{IdentifierMaster} .= $5;
         }
         if ($InKey && $Line =~ /^uid\s+(.*)/) {
-            $Key{Identifier} .= $1;
+            $Key{Identifier} .= ', '.$1;
         }
         if ($InKey && $Line =~ /\[expires:\s(.+?)\]/) {
             $Key{Expires} = $1;
@@ -589,6 +591,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.10 $ $Date: 2005-10-17 20:41:55 $
+$Revision: 1.11 $ $Date: 2005-11-28 00:10:40 $
 
 =cut
