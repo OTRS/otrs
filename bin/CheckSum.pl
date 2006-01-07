94e16cf0a36db29d46063dc4c036efd083f6ce9c
Martin Edenhofer <martin.edenhofer@otrs.com>
2006-01-07 16:22:31 +0000

    added bin/CheckSum.pl and also ARCHIVE file to include md5 sum archive of current distribution

---
+++ bin/CheckSum.pl
@@ -0,0 +1,133 @@
+#!/usr/bin/perl -w
+# --
+# bin/CheckSum.pl - a tool to compare changes in a installation
+# Copyright (C) 2001-2006 Martin Edenhofer <martin+code@otrs.org>
+# --
+# $Id: CheckSum.pl,v 1.2 2006-01-07 16:22:31 martin Exp $
+# --
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# --
+
+# use ../ as lib location
+use File::Basename;
+use FindBin qw($RealBin);
+use lib dirname($RealBin);
+use lib dirname($RealBin)."/Kernel/cpan-lib";
+
+use strict;
+
+use vars qw($VERSION $RealBin);
+$VERSION = '$Revision: 1.2 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+use Getopt::Std;
+use Digest::MD5 qw(md5_hex);
+
+my $Start = $RealBin.'/../';
+my $Archive = '';
+my $Action = 'compare';
+my %Compare = ();
+# --
+# get options
+# --
+my %Opts = ();
+getopt('habd', \%Opts);
+if ($Opts{'h'}) {
+    print "CheckSum.pl <Revision $VERSION> - OTRS check sum\n";
+    print "Copyright (c) 2001-2006 Martin Edenhofer <martin\@otrs.org>\n";
+    print "usage: CheckSum.pl -a create|compare [-b /path/to/ARCHIVE] [-d /path/to/framework]\n";
+    exit 1;
+}
+
+if ($Opts{'a'} && $Opts{'a'} eq 'create') {
+    $Action = $Opts{'a'};
+}
+if ($Opts{'d'}) {
+    $Start = $Opts{'d'};
+}
+if ($Opts{'b'}) {
+    $Archive = $Opts{'b'};
+}
+else {
+    $Archive = $Start.'ARCHIVE';
+}
+
+if ($Action eq 'create') {
+    print "Writing $Archive ...";
+    open(OUT, "> $Archive") || die "ERROR: Can't open: $Archive";
+}
+else {
+    open(IN, "< $Archive") || die "ERROR: Can't open: $Archive";
+    while (<IN>) {
+        my @Row = split(/::/, $_);
+        chomp($Row[1]);
+        $Compare{$Row[1]} = $Row[0];
+    }
+    close(IN);
+}
+
+my @Dirs = ();
+R($Start);
+foreach my $File (sort keys %Compare) {
+#    print "Notice: Removed $Compare{$File}\n";
+    print "Notice: Removed $File\n";
+}
+if ($Action eq 'create') {
+    print " done.\n";
+    close(OUT);
+}
+
+sub R {
+    my $In = shift;
+    my @List = glob("$In/*");
+    foreach my $File (@List) {
+        $File =~ s/\/\//\//g;
+        if (-d $File && $File !~ /CVS/ && $File !~ /^doc\//) {
+            R($File);
+            $File =~ s/$Start//;
+#            print "Directory: $File\n";
+        }
+        else {
+            my $OrigFile = $File;
+            $File =~ s/$Start//;
+            $File =~ s/^\/(.*)$/$1/;
+#            print "File: $File\n";
+            if ($File !~ /Entries|Repository|Root|CVS/ && $File !~ /^doc\//) {
+                my $Content = '';
+                open (IN, "< $OrigFile") || die "ERROR: $!";
+                while (<IN>) {
+                    $Content .= $_;
+                }
+                close (IN);
+                my $Digest = md5_hex($Content);
+                if ($Action eq 'create') {
+                    print OUT $Digest.'::'.$File."\n";
+                }
+                else {
+                    if (! $Compare{$File}) {
+                        print "Notice: New $File\n";
+                    }
+                    elsif ($Compare{$File} ne $Digest) {
+                        print "Notice: Dif $File\n";
+                    }
+                    if (defined($Compare{$File})) {
+                        delete $Compare{$File};
+                    }
+                }
+            }
+        }
+    }
+}
+
--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # --
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
-# Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2006 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: auto_build.sh,v 1.34 2005-11-28 00:17:37 martin Exp $
+# $Id: auto_build.sh,v 1.35 2006-01-07 16:22:30 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.34 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.35 $>"
 echo "Copyright (c) 2001-2005 Martin Edenhofer <martin@otrs.org>"
 
 PATH_TO_CVS_SRC=$1
@@ -169,6 +169,9 @@ rm -rf Kernel/cpan-lib/CGI/
 #$PATH_TO_CVS_SRC/scripts/auto_docbuild.sh $PATH_TO_CVS_SRC/../doc/ > /dev/null
 mkdir doc/manual
 cp -R /tmp/OTRSDOC-package/* doc/manual/
+
+# mk ARCHIVE
+bin/CheckSum.pl -a create
 
 # --
 # create tar
