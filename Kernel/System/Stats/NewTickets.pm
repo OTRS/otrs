757681c9919c3c4e73c8e14d8b1999d3f45c97b3
Thomas Raith <thomas.raith@otrs.com>
2006-01-27 11:22:41 +0000

    fixed spelling mistake

--- Kernel/System/Stats/NewTickets.pm
+++ Kernel/System/Stats/NewTickets.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/Stats/NewTickets.pm - stats module
-# Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2006 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: NewTickets.pm,v 1.5 2005-12-23 09:19:44 martin Exp $
+# $Id: NewTickets.pm,v 1.6 2006-01-27 11:22:41 tr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -17,12 +17,12 @@ use Kernel::System::Queue;
 use Date::Pcalc qw(Today_and_Now Days_in_Month Day_of_Week Day_of_Week_Abbreviation);
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.5 $ ';
+$VERSION = '$Revision: 1.6 $ ';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
 sub new {
-    my $Type = shift;
+    my $Type  = shift;
     my %Param = @_;
 
     # allocate new hash for object
@@ -40,18 +40,20 @@ sub new {
     }
 
     $Self->{TicketObject} = Kernel::System::Ticket->new(%Param);
-    $Self->{QueueObject} = Kernel::System::Queue->new(%Param);
+    $Self->{QueueObject}  = Kernel::System::Queue->new(%Param);
 
     return $Self;
 }
 # --
 sub Param {
-    my $Self = shift;
+    my $Self   = shift;
     my @Params = ();
+
     # get current time
     my ($s,$m,$h, $D,$M,$Y) = $Self->{TimeObject}->SystemTime2Date(
         SystemTime => $Self->{TimeObject}->SystemTime(),
     );
+
     # get one month bevore
     if ($M == 1) {
         $M = 12;
@@ -60,6 +62,7 @@ sub Param {
     else {
         $M = $M -1;
     }
+
     # create possible time selections
     my %Year = ();
     foreach ($Y-10..$Y+1) {
@@ -72,33 +75,33 @@ sub Param {
     }
 
     push (@Params, {
-            Frontend => 'Year',
-            Name => 'Year',
-            Multiple => 0,
-            Size => 0,
+            Frontend   => 'Year',
+            Name       => 'Year',
+            Multiple   => 0,
+            Size       => 0,
             SelectedID => $Y,
-            Data => {
+            Data       => {
                 %Year,
             },
         },
     );
     push (@Params, {
-            Frontend => 'Month',
-            Name => 'Month',
-            Multiple => 0,
-            Size => 0,
+            Frontend   => 'Month',
+            Name       => 'Month',
+            Multiple   => 0,
+            Size       => 0,
             SelectedID => $M,
-            Data => {
+            Data       => {
                 %Month,
             },
         },
     );
     push (@Params, {
-            Frontend => 'Graph Size',
-            Name => 'GraphSize',
-            Multiple => 0,
-            Size => 0,
-            SelectedID => '550x350',
+            Frontend   => 'Graph Size',
+            Name       => 'GraphSize',
+            Multiple   => 0,
+            Size       => 0,
+            SelectedID => '800x600',
             Data => {
                 '800x600' => ' 800x600',
                 '1200x800' => '1200x800',
@@ -109,20 +112,21 @@ sub Param {
 }
 # --
 sub Run {
-    my $Self = shift;
+    my $Self  = shift;
     my %Param = @_;
-    my $Days = Days_in_Month($Param{Year},$Param{Month});
+    my $Days  = Days_in_Month($Param{Year},$Param{Month});
     $Param{Month} = sprintf("%02d", $Param{Month});
     my $Title = "$Param{Year}-$Param{Month}";
     my %TicketStatus = $Self->{TicketObject}->HistoryTicketStatusGet(
-        StartYear => $Param{Year},
+        StartYear  => $Param{Year},
         StartMonth => $Param{Month},
-        StartDay => 1,
-        StopYear => $Param{Year},
+        StartDay   => 1,
+        StopYear   => $Param{Year},
         StopMonth => => $Param{Month},
-        StopDay => $Days,
+        StopDay    => $Days,
     );
-    my %Queue = ();
+
+    my %Queue    = ();
     my @HeadData = ('Queue');
     foreach my $TicketID (keys %TicketStatus) {
         my %Ticket = %{$TicketStatus{$TicketID}};
@@ -131,7 +135,7 @@ sub Run {
         }
     }
 
-    my $Day = Days_in_Month($Param{Year},$Param{Month});
+    my $Day        = Days_in_Month($Param{Year},$Param{Month});
     my $DayCounter = 1;
     while ($Day >= $DayCounter) {
         my $Dow = Day_of_Week($Param{Year}, $Param{Month}, $DayCounter);
@@ -142,7 +146,7 @@ sub Run {
     }
 
     my %Queues = $Self->{QueueObject}->GetAllQueues();
-    my @Data = ();
+    my @Data   = ();
     foreach my $QueueName (sort {$Queue{$a} cmp $Queue{$b}} keys %Queue) {
         $DayCounter = 1;
         my @Row = ($QueueName);
