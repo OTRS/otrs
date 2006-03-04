565ecf7767879443d900ba1c4c864e85276d05f1
Martin Edenhofer <martin.edenhofer@otrs.com>
2006-03-04 11:24:33 +0000

    added responsible feature

--- scripts/DBUpdate-to-2.1.mysql.sql
+++ scripts/DBUpdate-to-2.1.mysql.sql
@@ -2,7 +2,7 @@
 -- Update an existing OTRS database from 2.0 to 2.1
 -- Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 -- --
--- $Id: DBUpdate-to-2.1.mysql.sql,v 1.1 2006-02-05 20:16:07 martin Exp $
+-- $Id: DBUpdate-to-2.1.mysql.sql,v 1.2 2006-03-04 11:24:33 martin Exp $
 -- --
 --
 -- usage: cat DBUpdate-to-2.1.mysql.sql | mysql -f -u root otrs
@@ -12,6 +12,7 @@
 --
 -- ticket
 --
+ALTER TABLE ticket ADD responsible_user_id INTEGER NOT NULL;
 ALTER TABLE ticket ADD freekey9 VARCHAR (80);
 ALTER TABLE ticket ADD freetext9 VARCHAR (150);
 ALTER TABLE ticket ADD freekey10 VARCHAR (80);
@@ -28,4 +29,9 @@ ALTER TABLE ticket ADD freekey15 VARCHAR (80);
 ALTER TABLE ticket ADD freetext15 VARCHAR (150);
 ALTER TABLE ticket ADD freekey16 VARCHAR (80);
 ALTER TABLE ticket ADD freetext16 VARCHAR (150);
+
+INSERT INTO ticket_history_type
+        (name, valid_id, create_by, create_time, change_by, change_time)
+        VALUES
+        ('ResponsibleUpdate', 1, 1, current_timestamp, 1, current_timestamp);
 
