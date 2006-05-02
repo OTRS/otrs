f8617e7a41aa7f06fbde4276f9ad8ad471763b01
Thomas Raith <thomas.raith@otrs.com>
2006-05-02 07:28:21 +0000

    changed 123 in $Param{ID}

--- Kernel/System/LinkObject/Ticket.pm
+++ Kernel/System/LinkObject/Ticket.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/LinkObject/Ticket.pm - to link ticket objects
-# Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2006 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Ticket.pm,v 1.5 2005-09-18 13:53:16 martin Exp $
+# $Id: Ticket.pm,v 1.5.2.1 2006-05-02 07:28:21 tr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::System::LinkObject::Ticket;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.5 $';
+$VERSION = '$Revision: 1.5.2.1 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub Init {
@@ -160,7 +160,7 @@ sub LinkItemData {
     }
     return (
         %Ticket,
-        ID => 123,
+        ID => $Param{ID},
         Title => $Ticket{Title},
         Number => $Ticket{TicketNumber},
         Body => $Body,
