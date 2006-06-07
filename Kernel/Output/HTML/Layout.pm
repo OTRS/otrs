1473d8b27ac2dadda59959013ffa418c3686281c
Martin Edenhofer <martin.edenhofer@otrs.com>
2006-06-07 23:02:01 +0000

    improved max text size of Ascii2HTML() to 16000 chars

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2006 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Layout.pm,v 1.5 2006-06-05 15:44:20 martin Exp $
+# $Id: Layout.pm,v 1.6 2006-06-07 23:02:01 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -17,7 +17,7 @@ use strict;
 use Kernel::Language;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.5 $';
+$VERSION = '$Revision: 1.6 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -1332,7 +1332,7 @@ sub Ascii2Html {
         $Text =~ s/^(.{$Max}).*$/$1\[\.\.\]/gs;
     }
     # newline
-    if ($NewLine && length($Text) < 8000) {
+    if ($NewLine && length($Text) < 16000) {
         $Text =~ s/(\n\r|\r\r\n|\r\n)/\n/g;
         $Text =~ s/\r/\n/g;
         $Text =~ s/(.{4,$NewLine})(?:\s|\z)/$1\n/gm;
@@ -2626,6 +2626,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.5 $ $Date: 2006-06-05 15:44:20 $
+$Revision: 1.6 $ $Date: 2006-06-07 23:02:01 $
 
 =cut
