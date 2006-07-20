6a579ee0b6ec31765d6d5c86e3952f8b0da2baac
Martin Edenhofer <martin.edenhofer@otrs.com>
2006-07-20 17:04:39 +0000

    fixed link to roles / users

--- Kernel/Modules/AdminRoleUser.pm
+++ Kernel/Modules/AdminRoleUser.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AdminRoleUser.pm - to add/update/delete role <-> users
-# Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2001-2006 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AdminRoleUser.pm,v 1.6 2005-03-27 11:50:50 martin Exp $
+# $Id: AdminRoleUser.pm,v 1.7 2006-07-20 17:04:39 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Modules::AdminRoleUser;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.6 $';
+$VERSION = '$Revision: 1.7 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -188,6 +188,7 @@ sub MaskAdminUserGroupChangeForm {
                 InputBox => $Input,
                 Type => $Type,
                 NeType => $NeType,
+                ID => $_,
             },
         );
     }
