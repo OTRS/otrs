a2034133942885ea709d0f76ad8074cc761fd83e
Martin Edenhofer <martin.edenhofer@otrs.com>
2006-08-01 19:03:31 +0000

    removed &quot;/" html quote

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2006 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Layout.pm,v 1.8 2006-07-31 13:26:29 martin Exp $
+# $Id: Layout.pm,v 1.9 2006-08-01 19:03:31 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -17,7 +17,7 @@ use strict;
 use Kernel::Language;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.8 $';
+$VERSION = '$Revision: 1.9 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -1372,7 +1372,7 @@ sub Ascii2Html {
     $Text =~ s/&/&amp;/g;
     $Text =~ s/</&lt;/g;
     $Text =~ s/>/&gt;/g;
-    $Text =~ s/"/&quot;/g;
+#    $Text =~ s/"/&quot;/g;
     $Text =~ s/'/&apos;/g;
     # text -> html format quoting
     if ($Param{LinkFeature} && %LinkHash) {
@@ -2644,6 +2644,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.8 $ $Date: 2006-07-31 13:26:29 $
+$Revision: 1.9 $ $Date: 2006-08-01 19:03:31 $
 
 =cut
