de433bfb3473979033af23e61d0ef6ba84c70fe2
Manuel Hecht <manuel.hecht@otrs.com>
2006-10-09 16:01:57 +0000

    only 16 ticketfreetext fields

--- Kernel/Output/HTML/LayoutTicket.pm
+++ Kernel/Output/HTML/LayoutTicket.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/LayoutTicket.pm - provides generic ticket HTML output
 # Copyright (C) 2001-2006 OTRS GmbH, http://otrs.org/
 # --
-# $Id: LayoutTicket.pm,v 1.3 2006-10-09 15:42:14 mh Exp $
+# $Id: LayoutTicket.pm,v 1.4 2006-10-09 16:01:57 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Output::HTML::LayoutTicket;
 use strict;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.3 $';
+$VERSION = '$Revision: 1.4 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub TicketStdResponseString {
@@ -210,7 +210,7 @@ sub AgentFreeText {
         %Config = %{$Param{Config}};
     }
     my %Data = ();
-    foreach (1..18) {
+    foreach (1..16) {
         # key
         if (ref($Config{"TicketFreeKey$_"}) eq 'HASH' && %{$Config{"TicketFreeKey$_"}}) {
             my $Counter = 0;
