64fa68e7ee309ccbb9fab710f3e7c999e6b17758
Manuel Hecht <manuel.hecht@otrs.com>
2006-10-12 09:15:14 +0000

    code cleanup

--- Kernel/Language/en.pm
+++ Kernel/Language/en.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/en.pm - provides en languag translation
 # Copyright (C) 2001-2006 OTRS GmbH, http://otrs.org/
 # --
-# $Id: en.pm,v 1.14 2006-10-03 14:36:01 mh Exp $
+# $Id: en.pm,v 1.15 2006-10-12 09:15:14 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -13,7 +13,7 @@ package Kernel::Language::en;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.14 $';
+$VERSION = '$Revision: 1.15 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub Data {
@@ -68,11 +68,9 @@ sub Data {
       'History::TicketLinkDelete' => 'Deleted link to ticket "%s".',
       'History::SystemRequest' => 'System Request (%s).',
     );
-
     # $$STOP$$
 
     $Self->{Translation} = \%Hash;
-
 }
 
 1;
