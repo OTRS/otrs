b2ddb49feafba040af45c748219ed27397b09e7b
Martin Edenhofer <martin.edenhofer@otrs.com>
2006-10-18 09:43:03 +0000

    improved ChangeLog view of remote package

--- Kernel/Modules/AdminPackageManager.pm
+++ Kernel/Modules/AdminPackageManager.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminPackageManager.pm - manage software packages
 # Copyright (C) 2001-2006 OTRS GmbH, http://otrs.org/
 # --
-# $Id: AdminPackageManager.pm,v 1.37 2006-10-17 21:14:47 martin Exp $
+# $Id: AdminPackageManager.pm,v 1.38 2006-10-18 09:43:03 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::Package;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.37 $';
+$VERSION = '$Revision: 1.38 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub new {
@@ -401,7 +401,7 @@ sub Run {
             }
             elsif (ref($Structur{$Key}) eq 'ARRAY') {
                 foreach my $Hash (@{$Structur{$Key}}) {
-                    if ($Key =~ /^(Description)$/) {
+                    if ($Key =~ /^(Description|ChangeLog)$/) {
                         $Self->{LayoutObject}->Block(
                             Name => "PackageItem$Key",
                             Data => { %{$Hash}, Tag => $Key, },
