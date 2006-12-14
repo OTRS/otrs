dab6ced2f7138a00ae66d5a23e810cae05d2e952
Manuel Hecht <manuel.hecht@otrs.com>
2006-12-14 20:02:31 +0000

    rename file test.pm to testfile.pm

--- Kernel/cpan-lib/Mail/Mailer/testfile.pm
+++ Kernel/cpan-lib/Mail/Mailer/testfile.pm
@@ -1,11 +1,14 @@
 
 package Mail::Mailer::testfile;
 use vars qw(@ISA $VERSION %config);
+
 require Mail::Mailer::rfc822;
 @ISA = qw(Mail::Mailer::rfc822);
 $VERSION = '0.02';
 
-%config = ( outfile=>'mailer.testfile', );
+use Mail::Util qw/mailaddress/;
+
+%config = (outfile => 'mailer.testfile');
 
 sub can_cc { 0 }
 
@@ -15,7 +18,9 @@ sub exec {
     my($self, $exe, $args, $to) = @_;
     open F,'>>', $Mail::Mailer::testfile::config{outfile};
     print F "\n===\ntest ", ++$num, " ",
-            (scalar localtime), "\nto: " . join(' ',@{$to}) . "\n\n";
+            (scalar localtime),
+            "\nfrom: " . mailaddress(),
+            "\nto: " . join(' ',@{$to}), "\n\n";
     close F;
     untie(*$self) if tied *$self;
     tie *$self, 'Mail::Mailer::testfile::pipe', $self;
