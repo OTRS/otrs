614b83fff267a7d17138a9c220ecc10818ff5295
Manuel Hecht <manuel.hecht@otrs.com>
2007-01-30 12:28:15 +0000

    add missing UPDATE

--- scripts/DBUpdate-to-2.2.oracle.sql
+++ scripts/DBUpdate-to-2.2.oracle.sql
@@ -2,7 +2,7 @@
 -- Update an existing OTRS database from 2.1 to 2.2
 -- Copyright (C) 2001-2006 OTRS GmbH, http://otrs.org/
 -- --
--- $Id: DBUpdate-to-2.2.oracle.sql,v 1.1 2007-01-30 09:47:47 mh Exp $
+-- $Id: DBUpdate-to-2.2.oracle.sql,v 1.2 2007-01-30 12:28:15 mh Exp $
 -- --
 --
 -- usage: cat DBUpdate-to-2.2.oracle.sql | sqlplus "user/password"
@@ -13,3 +13,5 @@
 -- ticket_priority
 --
 ALTER TABLE ticket_priority ADD valid_id INTEGER;
+UPDATE ticket_priority SET valid_id = 1;
+
