fe9dc3eadf63f081971cfab7cf2fe628684f1f10
Martin Edenhofer <martin.edenhofer@otrs.com>
2007-02-26 11:21:32 +0000

    Fixed hard line rewrap of body content in frontend modules.

--- Kernel/Modules/AgentTicketNote.pm
+++ Kernel/Modules/AgentTicketNote.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketNote.pm - to add notes to a ticket
 # Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
 # --
-# $Id: AgentTicketNote.pm,v 1.19.2.1 2007-02-26 11:12:28 martin Exp $
+# $Id: AgentTicketNote.pm,v 1.19.2.2 2007-02-26 11:21:32 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -16,7 +16,7 @@ use Kernel::System::State;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.19.2.1 $';
+$VERSION = '$Revision: 1.19.2.2 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub new {
@@ -178,8 +178,8 @@ sub Run {
     }
     # rewrap body if exists
     if ($GetParam{Body}) {
-#        my $Size = $Self->{ConfigObject}->Get('Ticket::Frontend::TextAreaNote') || 70;
-#        $GetParam{Body} =~ s/(^>.+|.{4,$Size})(?:\s|\z)/$1\n/gm;
+        my $Size = $Self->{ConfigObject}->Get('Ticket::Frontend::TextAreaNote') || 70;
+        $GetParam{Body} =~ s/(^>.+|.{4,$Size})(?:\s|\z)/$1\n/gm;
     }
 
     if ($Self->{Subaction} eq 'Store') {
