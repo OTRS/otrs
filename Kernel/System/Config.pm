ff4e7ad763154de22bd5fa27f906322ff4bc8b3f
Martin Edenhofer <martin.edenhofer@otrs.com>
2007-03-04 23:05:11 +0000

    Added check of Deonload() if Kernel/Config/Files/ZZZAuto.pm exists.

--- Kernel/System/Config.pm
+++ Kernel/System/Config.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Config.pm - all system config tool functions
 # Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
 # --
-# $Id: Config.pm,v 1.57.2.2 2007-02-26 09:34:09 martin Exp $
+# $Id: Config.pm,v 1.57.2.3 2007-03-04 23:05:11 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::Main;
 use Kernel::Config;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.57.2.2 $';
+$VERSION = '$Revision: 1.57.2.3 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 =head1 NAME
@@ -276,8 +276,11 @@ sub Download {
             return;
         }
     }
-    if (!open(IN, "< $Home/Kernel/Config/Files/ZZZAuto.pm")) {
-        $Self->{LogObject}->Log(Priority => 'error', Message => "Can't open $Home/Kernel/Config/Files/ZZZAuto.pm!");
+    if (! -e "$Home/Kernel/Config/Files/ZZZAuto.pm") {
+        return $File;
+    }
+    elsif (!open(IN, "< $Home/Kernel/Config/Files/ZZZAuto.pm")) {
+        $Self->{LogObject}->Log(Priority => 'error', Message => "Can't open $Home/Kernel/Config/Files/ZZZAuto.pm: $!");
         return '';
     }
     else {
@@ -1437,6 +1440,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.57.2.2 $ $Date: 2007-02-26 09:34:09 $
+$Revision: 1.57.2.3 $ $Date: 2007-03-04 23:05:11 $
 
 =cut
