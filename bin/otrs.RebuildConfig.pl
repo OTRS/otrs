5f3a0b42819c64131a2593bd0ea5604ce2b8d04d
Martin Edenhofer <martin.edenhofer@otrs.com>
2007-05-07 15:38:13 +0000

    Added cmd bin/otrs.RebuildConfig.pl to rebuild/build default Kernel/Config/Files/ZZZAAuto.pm based on Kernel/Config/Files/*.xml config files.

---
+++ bin/otrs.RebuildConfig.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+# --
+# bin/otrs.RebuildConfig.pl - rebuild config
+# Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
+# --
+# $Id: otrs.RebuildConfig.pl,v 1.1 2007-05-07 15:38:13 martin Exp $
+# --
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# --
+
+# use ../ as lib location
+use File::Basename;
+use FindBin qw($RealBin);
+use lib dirname($RealBin);
+use lib dirname($RealBin)."/Kernel/cpan-lib";
+
+use strict;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+use Kernel::Config;
+use Kernel::System::Time;
+use Kernel::System::Log;
+use Kernel::System::Main;
+use Kernel::System::DB;
+use Kernel::System::Config;
+
+# --
+# common objects
+# --
+my %CommonObject = ();
+$CommonObject{ConfigObject} = Kernel::Config->new();
+$CommonObject{LogObject} = Kernel::System::Log->new(
+    LogPrefix => 'OTRS-otrs.RebuildConfig.pl',
+    %CommonObject,
+);
+$CommonObject{TimeObject} = Kernel::System::Time->new(%CommonObject);
+$CommonObject{MainObject} = Kernel::System::Main->new(%CommonObject);
+$CommonObject{DBObject} = Kernel::System::DB->new(%CommonObject);
+$CommonObject{SysConfigObject} = Kernel::System::Config->new(%CommonObject);
+
+# --
+# rebuild
+# --
+print "otrs.RebuildConfig.pl <Revision $VERSION> - OTRS rebuild default config\n";
+print "Copyright (c) 2001-2007 OTRS GmbH, http://otrs.org/\n";
+if ($CommonObject{SysConfigObject}->WriteDefault()) {
+    exit;
+}
+else {
+    $CommonObject{LogObject}->Log(Priority => 'error');
+    exit 1;
+}
