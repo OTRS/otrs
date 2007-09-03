33bd11f4241619763c42a1ae4de37ed332e74dc6
Martin Edenhofer <martin.edenhofer@otrs.com>
2007-09-03 20:35:49 +0000

    Added cache mechanism to escalation calculation, removed lock option.

--- Kernel/Output/HTML/NotificationAgentTicketEscalation.pm
+++ Kernel/Output/HTML/NotificationAgentTicketEscalation.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/NotificationAgentTicketEscalation.pm
 # Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
 # --
-# $Id: NotificationAgentTicketEscalation.pm,v 1.7 2007-07-26 13:45:53 martin Exp $
+# $Id: NotificationAgentTicketEscalation.pm,v 1.8 2007-09-03 20:35:49 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,9 +14,10 @@ package Kernel::Output::HTML::NotificationAgentTicketEscalation;
 use strict;
 use Kernel::System::Lock;
 use Kernel::System::State;
+use Kernel::System::Cache;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.7 $';
+$VERSION = '$Revision: 1.8 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub new {
@@ -34,6 +35,7 @@ sub new {
 
     $Self->{LockObject} = Kernel::System::Lock->new(%Param);
     $Self->{StateObject} = Kernel::System::State->new(%Param);
+    $Self->{CacheObject} = Kernel::System::Cache->new(%Param);
 
     return $Self;
 }
@@ -46,57 +48,75 @@ sub Run {
     }
     # get all open rw ticket
     my @TicketIDs = ();
-    my @ViewableStateIDs = $Self->{StateObject}->StateGetStatesByType(
-        Type => 'Viewable',
-        Result => 'ID',
+    my $Cache = $Self->{CacheObject}->Get(
+        Key => "Ticket::EscalationIndex::$Self->{UserID}",
     );
-    my @ViewableLockIDs = $Self->{LockObject}->LockViewableLock(Type => 'ID');
-    my $SQL = "SELECT st.id, st.tn, st.escalation_start_time, st.escalation_response_time, st.escalation_solution_time, ".
-        "st.ticket_state_id, st.service_id, st.sla_id, st.create_time, st.queue_id, st.ticket_lock_id ".
-        " FROM ".
-        " queue q, ticket st ".
-        " WHERE ".
-        " st.ticket_state_id IN ( ${\(join ', ', @ViewableStateIDs)} ) " .
-        " AND " .
-        " q.id = st.queue_id ".
-        " AND ";
-    my @GroupIDs = $Self->{GroupObject}->GroupMemberList(
-        UserID => $Self->{UserID},
-        Type => 'rw',
-        Result => 'ID',
-        Cached => 1,
-    );
-    $SQL .= " q.group_id IN ( ${\(join ', ', @GroupIDs)} ) AND ";
-    # check if user is in min. one group! if not, return here
-    if (!@GroupIDs) {
-        return;
+    if ($Cache && ref($Cache) eq 'ARRAY') {
+        @TicketIDs = @{$Cache};
     }
-    $SQL .= " st.ticket_lock_id IN ( ${\(join ', ', @ViewableLockIDs)} ) ";
-    $SQL .= " ORDER BY st.escalation_start_time ASC";
-    $Self->{DBObject}->Prepare(SQL => $SQL, Limit => 5000);
-    while (my @Row = $Self->{DBObject}->FetchrowArray()) {
-        my $TicketData = {
-            TicketID => $Row[0],
-            TicketNumber => $Row[1],
-            EscalationStartTime => $Row[2],
-            EscalationResponseTime => $Row[3],
-            EscalationSolutionTime => $Row[4],
-            StateID => $Row[5],
-            ServiceID => $Row[6],
-            SLAID => $Row[7],
-            Created => $Row[8],
-            QueueID => $Row[9],
-            LockID => $Row[10],
-        };
-        push (@TicketIDs, $TicketData);
-    }
-    # get state infos
-    foreach my $TicketData (@TicketIDs) {
-        # get state info
-        my %StateData = $Self->{StateObject}->StateGet(ID => $TicketData->{StateID}, Cache => 1);
-        $TicketData->{StateType} = $StateData{TypeName};
-        $TicketData->{State} = $StateData{Name};
-        $TicketData->{Lock} = $Self->{LockObject}->LockLookup(LockID => $TicketData->{LockID});
+    else {
+        my @ViewableStateIDs = $Self->{StateObject}->StateGetStatesByType(
+            Type => 'Viewable',
+            Result => 'ID',
+        );
+        my $SQL = "SELECT st.id, st.tn, st.escalation_start_time, st.escalation_response_time, st.escalation_solution_time, ".
+            "st.ticket_state_id, st.service_id, st.sla_id, st.create_time, st.queue_id, st.ticket_lock_id ".
+            " FROM ".
+            " ticket st, queue q ".
+            " WHERE ".
+            " st.queue_id = q.id ".
+            " AND " .
+            " st.ticket_state_id IN ( ${\(join ', ', @ViewableStateIDs)} ) ".
+            " AND ";
+        my @GroupIDs = $Self->{GroupObject}->GroupMemberList(
+            UserID => $Self->{UserID},
+            Type => 'rw',
+            Result => 'ID',
+            Cached => 1,
+        );
+        $SQL .= " q.group_id IN ( ${\(join ', ', @GroupIDs)} )";
+        # check if user is in min. one group! if not, return here
+        if (!@GroupIDs) {
+            return;
+        }
+        $SQL .= " ORDER BY st.escalation_start_time ASC";
+        $Self->{DBObject}->Prepare(SQL => $SQL, Limit => 5000);
+        while (my @Row = $Self->{DBObject}->FetchrowArray()) {
+            my $TicketData = {
+                TicketID => $Row[0],
+                TicketNumber => $Row[1],
+                EscalationStartTime => $Row[2],
+                EscalationResponseTime => $Row[3],
+                EscalationSolutionTime => $Row[4],
+                StateID => $Row[5],
+                ServiceID => $Row[6],
+                SLAID => $Row[7],
+                Created => $Row[8],
+                QueueID => $Row[9],
+                LockID => $Row[10],
+            };
+            push (@TicketIDs, $TicketData);
+        }
+        # get state infos
+        foreach my $TicketData (@TicketIDs) {
+            # get state info
+            my %StateData = $Self->{StateObject}->StateGet(ID => $TicketData->{StateID}, Cache => 1);
+            $TicketData->{StateType} = $StateData{TypeName};
+            $TicketData->{State} = $StateData{Name};
+            $TicketData->{Lock} = $Self->{LockObject}->LockLookup(LockID => $TicketData->{LockID});
+        }
+        my $TTL = 2.5*60;
+        if ($#TicketIDs > 2000) {
+            $TTL = 10.5*60;
+        }
+        elsif ($#TicketIDs > 500) {
+            $TTL = 5.5*60;
+        }
+        $Self->{CacheObject}->Set(
+            Key => "Ticket::EscalationIndex::$Self->{UserID}",
+            Value => \@TicketIDs,
+            TTL => $TTL,
+        );
     }
     # get escalations
     my $ResponseTime = '';
