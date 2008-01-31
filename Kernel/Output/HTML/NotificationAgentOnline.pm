59ab823528a8ceae8e63ed7757ecedc1576f8636
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-01-31 02:05:20 +0000

    Improved online agent and customerwith idle timeout to configure (See bug# 2656).

--- Kernel/Output/HTML/NotificationAgentOnline.pm
+++ Kernel/Output/HTML/NotificationAgentOnline.pm
@@ -1,12 +1,12 @@
 # --
 # Kernel/Output/HTML/NotificationAgentOnline.pm
-# Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: NotificationAgentOnline.pm,v 1.5 2007-10-02 10:41:39 mh Exp $
+# $Id: NotificationAgentOnline.pm,v 1.6 2008-01-31 02:05:20 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 # --
 
 package Kernel::Output::HTML::NotificationAgentOnline;
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::AuthSession;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.5 $) [1];
+$VERSION = qw($Revision: 1.6 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -27,7 +27,7 @@ sub new {
     bless( $Self, $Type );
 
     # get needed objects
-    for (qw(ConfigObject LogObject DBObject LayoutObject UserID)) {
+    for (qw(ConfigObject LogObject DBObject LayoutObject TimeObject UserID)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
     $Self->{SessionObject} = Kernel::System::AuthSession->new(%Param);
@@ -38,12 +38,15 @@ sub Run {
     my ( $Self, %Param ) = @_;
 
     # get session info
-    my %Online   = ();
-    my @Sessions = $Self->{SessionObject}->GetAllSessionIDs();
+    my %Online      = ();
+    my @Sessions    = $Self->{SessionObject}->GetAllSessionIDs();
+    my $IdleMinutes = $Param{Config}->{IdleMinutes} || 60*2;
     for (@Sessions) {
         my %Data = $Self->{SessionObject}->GetSessionIDData( SessionID => $_, );
         if (   $Self->{UserID} ne $Data{UserID}
             && $Data{UserType} eq 'User'
+            && $Data{UserLastRequest}
+            && $Data{UserLastRequest}+($IdleMinutes*60) > $Self->{TimeObject}->SystemTime()
             && $Data{UserFirstname}
             && $Data{UserLastname} )
         {
