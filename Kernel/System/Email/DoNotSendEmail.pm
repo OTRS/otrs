d72e59523054023c1758f5a25780afe949f774f6
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-02-21 00:47:51 +0000

    Added dummy sendmail backend module.

---
+++ Kernel/System/Email/DoNotSendEmail.pm
@@ -0,0 +1,74 @@
+# --
+# Kernel/System/Email/DoNotSendEmail.pm - modul dummy to send no emails
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: DoNotSendEmail.pm,v 1.1 2008-02-21 00:47:51 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::System::Email::DoNotSendEmail;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = { %Param };
+    bless( $Self, $Type );
+
+    # debug
+    $Self->{Debug} = $Param{Debug} || 0;
+
+    # check all needed objects
+    for (qw(ConfigObject LogObject)) {
+        die "Got no $_" if ( !$Self->{$_} );
+    }
+
+    return $Self;
+}
+
+sub Send {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(Header Body ToArray)) {
+        if ( !$Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+
+    # from
+    if ( !defined $Param{From} ) {
+        $Param{From} = '';
+    }
+
+    # recipient
+    my $ToString = '';
+    for my $To ( @{ $Param{ToArray} } ) {
+        if ( $ToString ) {
+            $ToString .= ", ";
+        }
+        $ToString .= $To;
+    }
+
+    # debug
+    if ( $Self->{Debug} > 2 ) {
+        $Self->{LogObject}->Log(
+            Priority => 'notice',
+            Message  => "Sent email to '$ToString' from '$Param{From}'.",
+        );
+    }
+
+    return 1;
+}
+
+1;
