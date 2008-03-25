9457ede90fd2432051495c8c75adb8f0fa210485
Udo Bretz <udo.bretz@otrs.com>
2008-03-25 16:00:55 +0000

    Fixed bug# 2732 - Service name is truncated in dropdown lists

--- Kernel/Modules/AdminSLA.pm
+++ Kernel/Modules/AdminSLA.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminSLA.pm - admin frontend to manage slas
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminSLA.pm,v 1.12 2008-03-25 13:33:48 ub Exp $
+# $Id: AdminSLA.pm,v 1.13 2008-03-25 16:00:55 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::SLA;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12 $) [1];
+$VERSION = qw($Revision: 1.13 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -225,6 +225,7 @@ sub Run {
             TreeView     => $TreeView,
             Sort         => 'TreeView',
             Translation  => 0,
+            Max          => 200,
         );
 
         # output overview
