daa090187da78ca37dec99acac06ae3d790b09db
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-03-28 11:54:44 +0000

    Added POP3/POP3S/IMAP/IMAPS support for PostMaster sub system (new bin/PostMasterMailbox.pl is replacing old bin/PostMasterPOP3.pl). Thanks to Igor Stradwo for this patch!

---
+++ Kernel/System/MailAccount.pm
@@ -0,0 +1,386 @@
+# --
+# Kernel/System/MailAccount.pm - lib for mail accounts
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: MailAccount.pm,v 1.1 2008-03-28 11:54:44 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::System::MailAccount;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::System::MailAccount - to manage mail accounts
+
+=head1 SYNOPSIS
+
+All functions to manage the mail accounts.
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create a object
+
+    use Kernel::Config;
+    use Kernel::System::Log;
+    use Kernel::System::DB;
+    use Kernel::System::MailAccount;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        LogObject => $LogObject,
+    );
+    my $MailAccount = Kernel::System::MailAccount->new(
+        LogObject => $LogObject,
+        ConfigObject => $ConfigObject,
+        DBObject => $DBObject,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = { %Param };
+    bless( $Self, $Type );
+
+    # check all needed objects
+    for (qw(DBObject ConfigObject LogObject)) {
+        die "Got no $_" if !$Self->{$_};
+    }
+
+    return $Self;
+}
+
+=item MailAccountAdd()
+
+adds a new mail account
+
+    $MailAccount->MailAccountAdd(
+        Login         => 'mail',
+        Password      => 'SomePassword',
+        Host          => 'pop3.example.com',
+        Type          => 'POP3',
+        ValidID       => 1,
+        Trusted       => 0,
+        DispatchingBy => 'Queue', # Queue|From
+        QueueID       => 12,
+        UserID        => 123,
+    );
+
+=cut
+
+sub MailAccountAdd {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(Login Password Host ValidID Trusted DispatchingBy QueueID UserID)) {
+        if ( !defined $Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "$_ not defined!" );
+            return;
+        }
+    }
+    for (qw(Login Password Host Type ValidID UserID)) {
+        if ( !$Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+
+    # check if dispatching is by From
+    if ( $Param{DispatchingBy} eq 'From' ) {
+        $Param{QueueID} = 0;
+    }
+    elsif ( $Param{DispatchingBy} eq 'Queue' && !$Param{QueueID} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => "Need QueueID for dispatching!" );
+        return;
+    }
+
+    # sql
+    return if ! $Self->{DBObject}->Do(
+        SQL => "INSERT INTO mail_account (login, pw, host, account_type, valid_id, comments, queue_id, "
+            . " trusted, create_time, create_by, change_time, change_by)"
+            . " VALUES (?, ?, ?, ?, ?, ?, ?, ?, current_timestamp, ?, current_timestamp, ?)",
+        Bind => [
+            \$Param{Login}, \$Param{Password}, \$Param{Host}, \$Param{Type},
+            \$Param{ValidID}, \$Param{Comment}, \$Param{QueueID}, \$Param{Trusted},
+            \$Param{UserID}, \$Param{UserID},
+        ],
+    );
+
+    return if ! $Self->{DBObject}->Prepare(
+        SQL  => "SELECT id FROM mail_account WHERE login = ? AND host = ? AND account_type = ?",
+        Bind => [ \$Param{Login}, \$Param{Host}, \$Param{Type} ],
+    );
+    my $ID;
+    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        $ID = $Row[0];
+    }
+    return $ID;
+}
+
+=item MailAccountGet()
+
+returns a hash of mail account data
+
+    my %MailAccount = $MailAccount->MailAccountGet(
+        ID => 123,
+    );
+
+(returns: ID, Login, Password, Host, Type, QueueID, Trusted, Comment, DispatchingBy, ValidID)
+
+=cut
+
+sub MailAccountGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{ID} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => "Need ID!" );
+        return;
+    }
+
+    # db quote
+    for (qw(ID)) {
+        $Param{$_} = $Self->{DBObject}->Quote( $Param{$_}, 'Integer' );
+    }
+
+    # sql
+    return if ! $Self->{DBObject}->Prepare(
+        SQL => "SELECT login, pw, host, account_type, queue_id, trusted, comments, valid_id"
+            . " FROM mail_account WHERE id = ?",
+        Bind => [ \$Param{ID} ],
+    );
+    my %Data = ();
+    while ( my @Data = $Self->{DBObject}->FetchrowArray() ) {
+        %Data = (
+            ID       => $Param{ID},
+            Login    => $Data[0],
+            Password => $Data[1],
+            Host     => $Data[2],
+            Type     => $Data[3] || 'POP3', # compat for old setups
+            QueueID  => $Data[4],
+            Trusted  => $Data[5],
+            Comment  => $Data[6],
+            ValidID  => $Data[7],
+        );
+    }
+    if ( $Data{QueueID} == 0 ) {
+        $Data{DispatchingBy} = 'From';
+    }
+    else {
+        $Data{DispatchingBy} = 'Queue';
+    }
+    return %Data;
+}
+
+=item MailAccountUpdate()
+
+update a new mail account
+
+    $MailAccount->MailAccountUpdate(
+        ID            => 1,
+        Login         => 'mail',
+        Password      => 'SomePassword',
+        Host          => 'pop3.example.com',
+        Type          => 'POP3',
+        ValidID       => 1,
+        Trusted       => 0,
+        DispatchingBy => 'Queue', # Queue|From
+        QueueID       => 12,
+        UserID        => 123,
+    );
+
+=cut
+
+sub MailAccountUpdate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(ID Login Password Host Type ValidID Trusted DispatchingBy QueueID UserID)) {
+        if ( !defined $Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+
+    # check if dispatching is by From
+    if ( $Param{DispatchingBy} eq 'From' ) {
+        $Param{QueueID} = 0;
+    }
+    elsif ( $Param{DispatchingBy} eq 'Queue' && !$Param{QueueID} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => "Need QueueID for dispatching!" );
+        return;
+    }
+
+    # sql
+    return $Self->{DBObject}->Do(
+        SQL => "UPDATE mail_account SET login = ?, pw = ?, host = ?, account_type = ?, "
+            . " comments = ?, trusted = ?, valid_id = ?, change_time = current_timestamp, "
+            . " change_by = ?, queue_id = ? WHERE id = ?",
+        Bind => [
+            \$Param{Login}, \$Param{Password}, \$Param{Host}, \$Param{Type},
+            \$Param{Comment}, \$Param{Trusted}, \$Param{ValidID}, \$Param{UserID},
+            \$Param{QueueID}, \$Param{ID},
+        ],
+    );
+}
+
+=item MailAccountDelete()
+
+deletes a mail account
+
+    $MailAccount->MailAccountDelete(
+        ID => 123,
+    );
+
+=cut
+
+sub MailAccountDelete {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{ID} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => "Need ID!" );
+        return;
+    }
+
+    # sql
+
+    return $Self->{DBObject}->Do(
+        SQL  => "DELETE FROM mail_account WHERE id = ?",
+        Bind => [ \$Param{ID} ],
+    );
+}
+
+=item MailAccountList()
+
+returns a list (Key, Name) of all mail accounts
+
+    my %List = $MailAccount->MailAccountList(
+        Valid => 0, # just valid/all accounts
+    );
+
+=cut
+
+sub MailAccountList {
+    my ( $Self, %Param ) = @_;
+
+    my $Valid = $Param{Valid} || 0;
+    return $Self->{DBObject}->GetTableData(
+        What  => 'id, login, host',
+        Valid => $Valid,
+        Clamp => 1,
+        Table => 'mail_account',
+    );
+}
+
+=item MailAccountBackendList()
+
+returns a list usable backends
+
+    my %List = $MailAccount->MailAccountBackendList();
+
+=cut
+
+sub MailAccountBackendList {
+    my ( $Self, %Param ) = @_;
+
+    my %Backends = ();
+    my $Directory = $Self->{ConfigObject}->Get('Home').'/Kernel/System/MailAccount/*.pm';
+    my @List = glob $Directory;
+    foreach my $File (@List) {
+
+        # remove .pm
+        $File =~ s/^.*\/(.+?)\.pm$/$1/;
+        my $GenericModule = "Kernel::System::MailAccount::$File";
+
+        # try to load module $GenericModule
+        if ( eval "require $GenericModule" ) {
+            if ( eval { $GenericModule->new( %{ $Self } ) } ) {
+                $Backends{$File} = $File;
+            }
+        }
+    }
+
+    return %Backends;
+}
+
+=item MailAccountFetch()
+
+fetch emails by using backend
+
+    my $Ok = $MailAccount->MailAccountFetch(
+        Login         => 'mail',
+        Password      => 'SomePassword',
+        Host          => 'pop3.example.com',
+        ValidID       => 1,
+        Trusted       => 0,
+        DispatchingBy => 'Queue', # Queue|From
+        QueueID       => 12,
+        UserID        => 123,
+    );
+
+=cut
+
+sub MailAccountFetch {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(Login Password Host Type Trusted DispatchingBy QueueID UserID)) {
+        if ( !defined $Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+    # load backend
+    my $GenericModule = "Kernel::System::MailAccount::$Param{Type}";
+
+    # try to load module $GenericModule
+    if ( ! $Self->{MainObject}->Require($GenericModule) ) {
+        return;
+    }
+
+    # fetch mails
+    my $Backend = $GenericModule->new( %{ $Self } );
+    return $Backend->Fetch( %Param );
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2008-03-28 11:54:44 $
+
+=cut
---
+++ Kernel/System/MailAccount/IMAPS.pm
@@ -0,0 +1,187 @@
+# --
+# Kernel/System/MailAccount/IMAPS.pm - lib for imap accounts
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: IMAPS.pm,v 1.1 2008-03-28 11:54:44 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::System::MailAccount::IMAPS;
+
+use strict;
+use warnings;
+use Net::IMAP::Simple::SSL;
+use Kernel::System::PostMaster;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = { %Param };
+    bless( $Self, $Type );
+
+    # check all needed objects
+    for (qw(DBObject LogObject ConfigObject TimeObject MainObject)) {
+        die "Got no $_" if !$Self->{$_};
+    }
+
+    return $Self;
+}
+
+sub Fetch {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(Login Password Host Trusted QueueID)) {
+        if ( !defined $Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "$_ not defined!" );
+            return;
+        }
+    }
+    for (qw(Login Password Host)) {
+        if ( !$Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+
+    my $Debug = $Param{Debug} || 0;
+    my $Limit = $Param{Limit} || 5000;
+    my $CMD   = $Param{CMD} || 0;
+
+    # MaxEmailSize
+    my $MaxEmailSize = $Self->{ConfigObject}->Get('PostMasterMaxEmailSize') || 1024 * 6;
+
+    # MaxPopEmailSession
+    my $MaxPopEmailSession = $Self->{ConfigObject}->Get('PostMasterReconnectMessage') || 20;
+
+    my $Timeout      = 60;
+    my $FetchCounter = 0;
+    my $Reconnect    = 0;
+    my $AuthType     = 'IMAPS';
+
+    # connect to host
+    my $IMAPObject = Net::IMAP::Simple::SSL->new( $Param{Host}, timeout => $Timeout, debug => $Debug );
+    if ( !$IMAPObject ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => "$AuthType: Can't connect to $Param{Host}",
+        );
+        return;
+    }
+
+    # authentcation
+    my $Auth = $IMAPObject->login( $Param{Login}, $Param{Password} );
+    if ( !defined $Auth ) {
+        $IMAPObject->quit();
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => "$AuthType: Auth for user $Param{Login}/$Param{Host} failed!",
+        );
+        return;
+    }
+    my $NOM = $IMAPObject->select('INBOX');
+    # fetch messages
+    if ( $NOM > 0 ) {
+        for(my $Messageno = 1; $Messageno <= $NOM; $Messageno++) {
+
+            # check if reconnect is needed
+            if ( ( $FetchCounter + 1 ) > $MaxPopEmailSession ) {
+                $Reconnect = 1;
+                if ( $CMD ) {
+                    print "$AuthType: Reconnect Session after $MaxPopEmailSession messages...\n";
+                }
+                last;
+            }
+            if ( $CMD ) {
+                print "$AuthType: Message $Messageno/$NOM ($Param{Login}/$Param{Host})\n";
+            }
+
+            # check message size
+            my $MessageSize = int( $IMAPObject->list($Messageno) / 1024 );
+            if ( $MessageSize > $MaxEmailSize ) {
+                $Self->{LogObject}->Log(
+                    Priority => 'error',
+                    Message  => "$AuthType: Can't fetch email $NOM from $Param{Login}/$Param{Host}. Email to "
+                        . "big ($MessageSize KB - max $MaxEmailSize KB)!",
+                );
+            }
+            else {
+                # safety protection
+                $FetchCounter++;
+                if ( $FetchCounter > 10 && $FetchCounter < 25 ) {
+                    if ( $CMD ) {
+                        print "$AuthType: Safety protection waiting 2 second till processing next mail...\n";
+                    }
+                    sleep 2;
+                }
+                elsif ( $FetchCounter > 25 ) {
+                    if ( $CMD ) {
+                        print "$AuthType: Safety protection waiting 3 seconds till processing next mail...\n";
+                    }
+                    sleep 3;
+                }
+
+                # get message (header and body)
+                my $Lines = $IMAPObject->get($Messageno);
+                my $PostMasterObject = Kernel::System::PostMaster->new(
+                    %{ $Self },
+                    Email   => $Lines,
+                    Trusted => $Param{Trusted} || 0,
+                    Debug   => $Debug,
+                );
+                my @Return = $PostMasterObject->Run( QueueID => $Param{QueueID} || 0 );
+                if ( !$Return[0] ) {
+                    $Self->{LogObject}->Log(
+                        Priority => 'error',
+                        Message  => "$AuthType: Can't process mail, see log sub system!",
+                    );
+                }
+                undef $PostMasterObject;
+                $IMAPObject->delete($Messageno);
+
+                # check limit
+                $Self->{Limit}++;
+                if ( $Self->{Limit} >= $Limit ) {
+                    $Reconnect = 0;
+                    last;
+                }
+            }
+            if ( $CMD ) {
+                print "\n";
+            }
+        }
+    }
+    else {
+        if ( $CMD ) {
+            print "$AuthType: No messages ($Param{Login}/$Param{Host})\n";
+        }
+    }
+
+    # log status
+    if ( $Debug > 0 || $FetchCounter ) {
+        $Self->{LogObject}->Log(
+            Priority => 'notice',
+            Message  => "$AuthType: Fetched $FetchCounter email(s) from $Param{Login}/$Param{Host}.",
+        );
+    }
+    $IMAPObject->expunge_mailbox('INBOX');
+    $IMAPObject->quit();
+    if ( $CMD ) {
+        print "$AuthType: Connection to $Param{Host} closed.\n\n";
+    }
+
+    # fetch again if still messages on the account
+    if ($Reconnect) {
+        $Self->Fetch(%Param);
+    }
+    return 1;
+}
+
+1;
