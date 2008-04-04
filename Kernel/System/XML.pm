2830db6926121126fbefd7213d1f9885a9dbe650
Thomas Raith <thomas.raith@otrs.com>
2008-04-04 10:38:43 +0000

    Improved perldoc.

--- Kernel/System/XML.pm
+++ Kernel/System/XML.pm
@@ -2,7 +2,7 @@
 # Kernel/System/XML.pm - lib xml
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: XML.pm,v 1.71 2008-04-04 10:36:20 tr Exp $
+# $Id: XML.pm,v 1.72 2008-04-04 10:38:43 tr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::Encode;
 use Kernel::System::Cache;
 
 use vars qw($VERSION $S);
-$VERSION = qw($Revision: 1.71 $) [1];
+$VERSION = qw($Revision: 1.72 $) [1];
 
 =head1 NAME
 
@@ -40,6 +40,7 @@ create an object
 
     use Kernel::Config;
     use Kernel::System::Log;
+    use Kernel::System::Main;
     use Kernel::System::DB;
     use Kernel::System::XML;
 
@@ -1412,6 +1413,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.71 $ $Date: 2008-04-04 10:36:20 $
+$Revision: 1.72 $ $Date: 2008-04-04 10:38:43 $
 
 =cut
