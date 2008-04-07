f4f864c9e2509e0e49ed4a246b85666ff03a33dc
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-04-07 11:19:37 +0000

    Fixed bug# 2814 - BCC emails are visible to all receipiants in email header.

--- Kernel/System/Email.pm
+++ Kernel/System/Email.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Email.pm - the global email send module
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: Email.pm,v 1.37 2008-04-02 04:52:27 tr Exp $
+# $Id: Email.pm,v 1.38 2008-04-07 11:19:37 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::Encode;
 use Kernel::System::Crypt;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.37 $) [1];
+$VERSION = qw($Revision: 1.38 $) [1];
 
 =head1 NAME
 
@@ -224,7 +224,7 @@ sub Send {
 
     # build header
     my %Header = ();
-    for (qw(From To Cc Bcc Subject Charset Reply-To)) {
+    for (qw(From To Cc Subject Charset Reply-To)) {
         if ( $Param{$_} ) {
             $Header{$_} = $Param{$_};
         }
@@ -237,7 +237,7 @@ sub Send {
     }
 
     # do some encode
-    for (qw(From To Cc Bcc Subject)) {
+    for (qw(From To Cc Subject)) {
         if ( $Header{$_} && $Param{Charset} ) {
             $Header{$_} = encode_mimewords( Encode::encode( $Param{Charset}, $Header{$_} ),
                 Charset => $Param{Charset} )
@@ -651,6 +651,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.37 $ $Date: 2008-04-02 04:52:27 $
+$Revision: 1.38 $ $Date: 2008-04-07 11:19:37 $
 
 =cut
