881ffd3cf4df964256acb9f8198ceac6e7b1a581
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-05-06 22:31:54 +0000

    Improved code layout.

--- Kernel/Modules/AgentTicketWatcher.pm
+++ Kernel/Modules/AgentTicketWatcher.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketWatcher.pm - a ticketwatcher module
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketWatcher.pm,v 1.8 2008-01-31 06:22:12 tr Exp $
+# $Id: AgentTicketWatcher.pm,v 1.9 2008-05-06 22:31:54 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -69,36 +69,36 @@ sub Run {
     if ( $Self->{Subaction} eq 'Subscribe' ) {
 
         # set subscribe
-        if ($Self->{TicketObject}->TicketWatchSubscribe(
-                TicketID => $Self->{TicketID},
-                UserID   => $Self->{UserID},
-            )
-            )
-        {
+        my $Subscribe = $Self->{TicketObject}->TicketWatchSubscribe(
+            TicketID    => $Self->{TicketID},
+            WatchUserID => $Self->{UserID},
+            UserID      => $Self->{UserID},
+        );
 
-            # redirect
-            return $Self->{LayoutObject}->Redirect( OP => $Self->{LastScreenView} );
-        }
-        else {
+        if ( !$Subscribe ) {
             return $Self->{LayoutObject}->ErrorScreen();
         }
+
+        # redirect
+        return $Self->{LayoutObject}->Redirect( OP => $Self->{LastScreenView} );
     }
 
     # ------------------------------------------------------------ #
     # unsubscribe a ticket
     # ------------------------------------------------------------ #
     elsif ( $Self->{Subaction} eq 'Unsubscribe' ) {
-        if ($Self->{TicketObject}->TicketWatchUnsubscribe(
-                TicketID => $Self->{TicketID},
-                UserID   => $Self->{UserID},
-            )
-            )
-        {
-            return $Self->{LayoutObject}->Redirect( OP => $Self->{LastScreenView} );
-        }
-        else {
+        my $Unsubscribe = $Self->{TicketObject}->TicketWatchUnsubscribe(
+            TicketID    => $Self->{TicketID},
+            WatchUserID => $Self->{UserID},
+            UserID      => $Self->{UserID},
+        );
+
+        if ( !$Unsubscribe ) {
             return $Self->{LayoutObject}->ErrorScreen();
         }
+
+        # redirect
+        return $Self->{LayoutObject}->Redirect( OP => $Self->{LastScreenView} );
     }
 }
 
