7bd84ed1f8c8d95cc318cd6ccbe4ea60ce3e8f69
Manuel Hecht <manuel.hecht@otrs.com>
2008-05-09 14:01:21 +0000

    Wrong filename.

--- scripts/DBUpdate-to-2.3-schema.oracle.sql
+++
@@ -1,120 +0,0 @@
--- ----------------------------------------------------------
---  driver: oracle, generated: 2008-05-09 15:39:53
--- ----------------------------------------------------------
-SET DEFINE OFF;
--- ----------------------------------------------------------
---  create table queue_preferences
--- ----------------------------------------------------------
-CREATE TABLE queue_preferences (
-    queue_id NUMBER (12, 0) NOT NULL,
-    preferences_key VARCHAR2 (150) NOT NULL,
-    preferences_value VARCHAR2 (250)
-);
-CREATE INDEX queue_preferences_queue_id ON queue_preferences (queue_id);
-CREATE INDEX user_preferences_user_id ON user_preferences (user_id);
-CREATE INDEX group_user_user_id ON group_user (user_id);
-CREATE INDEX group_user_group_id ON group_user (group_id);
-CREATE INDEX group_role_role_id ON group_role (role_id);
-CREATE INDEX group_role_group_id ON group_role (group_id);
-CREATE INDEX group_customer_user_id ON group_customer_user (user_id);
-CREATE INDEX group_customer_user_group_id ON group_customer_user (group_id);
-CREATE INDEX role_user_user_id ON role_user (user_id);
-CREATE INDEX role_user_role_id ON role_user (role_id);
-CREATE INDEX personal_queues_user_id ON personal_queues (user_id);
-CREATE INDEX personal_queues_queue_id ON personal_queues (queue_id);
--- ----------------------------------------------------------
---  alter table queue
--- ----------------------------------------------------------
-ALTER TABLE queue ADD first_response_notify NUMBER (5, 0);
--- ----------------------------------------------------------
---  alter table queue
--- ----------------------------------------------------------
-ALTER TABLE queue ADD update_notify NUMBER (5, 0);
--- ----------------------------------------------------------
---  alter table queue
--- ----------------------------------------------------------
-ALTER TABLE queue ADD solution_notify NUMBER (5, 0);
-CREATE INDEX queue_group_id ON queue (group_id);
-CREATE INDEX ticket_title ON ticket (title);
-CREATE INDEX ticket_customer_user_id ON ticket (customer_user_id);
-CREATE INDEX ticket_customer_id ON ticket (customer_id);
-CREATE INDEX ticket_queue_id ON ticket (queue_id);
-CREATE INDEX ticket_ticket_lock_id ON ticket (ticket_lock_id);
-CREATE INDEX ticket_responsible_user_id ON ticket (responsible_user_id);
-CREATE INDEX ticket_ticket_state_id ON ticket (ticket_state_id);
-CREATE INDEX ticket_ticket_priority_id ON ticket (ticket_priority_id);
-CREATE INDEX index_object_link_a_id ON object_link (object_link_a_id);
-CREATE INDEX index_object_link_b_id ON object_link (object_link_b_id);
-CREATE INDEX index_object_link_a_object ON object_link (object_link_a_object);
-CREATE INDEX index_object_link_b_object ON object_link (object_link_b_object);
-CREATE INDEX index_object_link_type ON object_link (object_link_type);
-CREATE INDEX ticket_history_history_type_id ON ticket_history (history_type_id);
-CREATE INDEX ticket_history_queue_id ON ticket_history (queue_id);
-CREATE INDEX ticket_history_type_id ON ticket_history (type_id);
-CREATE INDEX ticket_history_owner_id ON ticket_history (owner_id);
-CREATE INDEX ticket_history_priority_id ON ticket_history (priority_id);
-CREATE INDEX ticket_history_state_id ON ticket_history (state_id);
--- ----------------------------------------------------------
---  alter table sla
--- ----------------------------------------------------------
-ALTER TABLE sla ADD first_response_notify NUMBER (5, 0);
--- ----------------------------------------------------------
---  alter table sla
--- ----------------------------------------------------------
-ALTER TABLE sla ADD update_notify NUMBER (5, 0);
--- ----------------------------------------------------------
---  alter table sla
--- ----------------------------------------------------------
-ALTER TABLE sla ADD solution_notify NUMBER (5, 0);
--- ----------------------------------------------------------
---  create table service_sla
--- ----------------------------------------------------------
-CREATE TABLE service_sla (
-    service_id NUMBER (12, 0) NOT NULL,
-    sla_id NUMBER (12, 0) NOT NULL,
-    CONSTRAINT service_sla UNIQUE (service_id, sla_id)
-);
-CREATE INDEX FK_service_sla_service_id ON service_sla (service_id);
-CREATE INDEX FK_service_sla_sla_id ON service_sla (sla_id);
-CREATE INDEX article_article_type_id ON article (article_type_id);
-CREATE INDEX article_sender_type_id ON article (article_sender_type_id);
-CREATE INDEX ticket_watcher_user_id ON ticket_watcher (user_id);
-ALTER TABLE ticket_watcher ADD CONSTRAINT FK_ticket_watcher_ticket_id_id FOREIGN KEY (ticket_id) REFERENCES ticket(id);
-ALTER TABLE ticket_watcher ADD CONSTRAINT FK_ticket_watcher_user_id_id FOREIGN KEY (user_id) REFERENCES system_user(id);
-ALTER TABLE ticket_watcher ADD CONSTRAINT FK_ticket_watcher_create_by_id FOREIGN KEY (create_by) REFERENCES system_user(id);
-ALTER TABLE ticket_watcher ADD CONSTRAINT FK_ticket_watcher_change_by_id FOREIGN KEY (change_by) REFERENCES system_user(id);
-CREATE INDEX ticket_index_queue_id ON ticket_index (queue_id);
-CREATE INDEX ticket_index_group_id ON ticket_index (group_id);
-ALTER TABLE ticket_index ADD CONSTRAINT FK_ticket_index_ticket_id_id FOREIGN KEY (ticket_id) REFERENCES ticket(id);
-ALTER TABLE ticket_index ADD CONSTRAINT FK_ticket_index_queue_id_id FOREIGN KEY (queue_id) REFERENCES queue(id);
-ALTER TABLE ticket_index ADD CONSTRAINT FK_ticket_index_group_id_id FOREIGN KEY (group_id) REFERENCES groups(id);
-CREATE INDEX postmaster_filter_f_name ON postmaster_filter (f_name);
-CREATE INDEX generic_agent_job_name ON generic_agent_jobs (job_name);
--- ----------------------------------------------------------
---  alter table mail_account
--- ----------------------------------------------------------
-ALTER TABLE pop3_account RENAME TO mail_account;
--- ----------------------------------------------------------
---  alter table mail_account
--- ----------------------------------------------------------
-ALTER TABLE mail_account ADD account_type VARCHAR2 (20);
--- ----------------------------------------------------------
---  alter table article
--- ----------------------------------------------------------
-ALTER TABLE article MODIFY a_body CLOB NOT NULL;
--- ----------------------------------------------------------
---  alter table xml_storage
--- ----------------------------------------------------------
-ALTER TABLE xml_storage MODIFY xml_content_value CLOB NULL;
--- ----------------------------------------------------------
---  insert into table notifications
--- ----------------------------------------------------------
-INSERT INTO notifications (notification_type, notification_charset, notification_language, subject, text, create_by, create_time, change_by, change_time)
-    VALUES
-    ('Agent::EscalationNotifyBefore', 'iso-8859-1', 'en', 'Ticket Escalation Warning! (<OTRS_CUSTOMER_SUBJECT[24]>)', 'Hi <OTRS_UserFirstname>,the ticket "<OTRS_TICKET_TicketNumber>" will escalate!Escalation at: <OTRS_TICKET_EscalationDestinationDate>Escalation in: <OTRS_TICKET_EscalationDestinationIn><OTRS_CUSTOMER_FROM>wrote:<snip><OTRS_CUSTOMER_EMAIL[30]><snip>Please have a look at:<OTRS_CONFIG_HttpType>://<OTRS_CONFIG_FQDN>/<OTRS_CONFIG_ScriptAlias>index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_TicketID>Your OTRS Notification Master', 1, current_timestamp, 1, current_timestamp);
--- ----------------------------------------------------------
---  insert into table notifications
--- ----------------------------------------------------------
-INSERT INTO notifications (notification_type, notification_charset, notification_language, subject, text, create_by, create_time, change_by, change_time)
-    VALUES
-    ('Agent::EscalationNotifyBefore', 'iso-8859-1', 'de', 'Ticket Eskalations-Warnung! (<OTRS_CUSTOMER_SUBJECT[24]>)', 'Hallo <OTRS_UserFirstname> <OTRS_UserLastname>,das Ticket "<OTRS_TICKET_TicketNumber>" wird bald eskalieren!Eskalation um: <OTRS_TICKET_EscalationDestinationDate>Eskalation in: <OTRS_TICKET_EscalationDestinationIn><OTRS_CUSTOMER_FROM>schrieb:<snip><OTRS_CUSTOMER_EMAIL[30]><snip>Bitte um Bearbeitung:<OTRS_CONFIG_HttpType>://<OTRS_CONFIG_FQDN>/<OTRS_CONFIG_ScriptAlias>index.pl?Action=AgentZoom&TicketID=<OTRS_TICKET_TicketID>Ihr OTRS Benachrichtigungs-Master', 1, current_timestamp, 1, current_timestamp);
