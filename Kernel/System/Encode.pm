0523fbd1ac3eb7f7c7929dcb024b599a453b68d8
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-05-22 17:17:00 +0000

    Added safty check to Convert() if given string is defined as utf8 but no utf8 in contained.

--- Kernel/System/Encode.pm
+++ Kernel/System/Encode.pm
@@ -1,12 +1,12 @@
 # --
 # Kernel/System/Encode.pm - character encodings
-# Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: Encode.pm,v 1.17.2.1 2007-10-16 15:22:04 martin Exp $
+# $Id: Encode.pm,v 1.17.2.2 2008-05-22 17:17:00 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 # --
 
 package Kernel::System::Encode;
@@ -16,7 +16,7 @@ use warnings;
 
 use vars qw(@ISA $VERSION);
 
-$VERSION = qw($Revision: 1.17.2.1 $) [1];
+$VERSION = qw($Revision: 1.17.2.2 $) [1];
 
 =head1 NAME
 
@@ -197,6 +197,13 @@ sub Convert {
 
     # if no encode is needed
     if ( $Param{From} =~ /^$Param{To}$/i ) {
+
+        # check if string is valid utf8
+        if ( !eval { Encode::from_to( $Param{Text}, $Param{From}, $Param{To}, 1 ) } ) {
+            print STDERR "No valid '$Param{To}' string: '$Param{Text}'!\n";
+            return $Param{Text};
+        }
+
         if ( $Param{To} =~ /^utf(-8|8)/i ) {
             Encode::_utf8_on( $Param{Text} );
         }
@@ -364,12 +371,12 @@ This software is part of the OTRS project (http://otrs.org/).
 
 This software comes with ABSOLUTELY NO WARRANTY. For details, see
 the enclosed file COPYING for license information (GPL). If you
-did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =cut
 
 =head1 VERSION
 
-$Revision: 1.17.2.1 $ $Date: 2007-10-16 15:22:04 $
+$Revision: 1.17.2.2 $ $Date: 2008-05-22 17:17:00 $
 
 =cut
