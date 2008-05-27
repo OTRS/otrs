c774423e65277aeaad0f55888abd1e033962dbc8
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-05-27 08:08:30 +0000

    Updated (c).

--- Kernel/Language/ct.pm
+++ Kernel/Language/ct.pm
@@ -1,8 +1,8 @@
 # --
-# Kernel/Language/ct.pm - provides es language translation
-# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# Kernel/Language/ct.pm - provides ct language translation
+# Copyright (C) 2008 Sistemes OTIC (ibsalut) - Antonio Linde
 # --
-# $Id: ct.pm,v 1.1.2.2 2008-05-09 12:46:10 martin Exp $
+# $Id: ct.pm,v 1.1.2.3 2008-05-27 08:08:30 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1.2.2 $) [1];
+$VERSION = qw($Revision: 1.1.2.3 $) [1];
 
 sub Data {
     my ( $Self, %Param ) = @_;
