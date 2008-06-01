22c54147d079319292d41412007b4c3394ce4082
Manuel Hecht <manuel.hecht@otrs.com>
2008-06-01 15:25:58 +0000

    Removed useless load of LinkObject.pm.

--- Kernel/Modules/AgentZoom.pm
+++ Kernel/Modules/AgentZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentZoom.pm - to get a closer view
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentZoom.pm,v 1.92 2008-05-08 09:36:37 mh Exp $
+# $Id: AgentZoom.pm,v 1.93 2008-06-01 15:25:58 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,10 +15,9 @@ use strict;
 use warnings;
 
 use Kernel::System::CustomerUser;
-use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.92 $) [1];
+$VERSION = qw($Revision: 1.93 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
