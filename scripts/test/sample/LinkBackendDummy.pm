cbea6472f287260418cf83060f825ff9996a3d76
Manuel Hecht <manuel.hecht@otrs.com>
2008-06-23 10:26:08 +0000

    Init.

---
+++ scripts/test/sample/LinkBackendDummy.pm
@@ -0,0 +1,325 @@
+# --
+# Kernel/System/LinkObject/Dummy.pm - to link dummy objects
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: LinkBackendDummy.pm,v 1.1 2008-06-23 10:26:08 mh Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::System::LinkObject::Dummy;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for (qw(DBObject ConfigObject LogObject MainObject LinkObject)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    return $Self;
+}
+
+=item LinkListWithData()
+
+fill up the link list with data
+
+    $Success = $LinkObjectBackend->LinkListWithData(
+        LinkList => $HashRef,
+        UserID   => 1,
+    );
+
+=cut
+
+sub LinkListWithData {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(LinkList UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # check link list
+    if ( ref $Param{LinkList} ne 'HASH' ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'LinkList must be a hash reference!',
+        );
+        return;
+    }
+
+    for my $LinkType ( keys %{ $Param{LinkList} } ) {
+
+        for my $Direction ( keys %{ $Param{LinkList}->{$LinkType} } ) {
+
+            for my $DummyID ( keys %{ $Param{LinkList}->{$LinkType}->{$Direction} } ) {
+
+                # add dummy data
+                $Param{LinkList}->{$LinkType}->{$Direction}->{$DummyID} = { Dummy => 1 };
+            }
+        }
+    }
+
+    return 1;
+}
+
+=item ObjectDescriptionGet()
+
+return a hash of object descriptions
+
+Return
+    %Description = (
+        Normal => "Dummy# 1234455",
+        Long   => "Dummy# 1234455: The Dummy Title",
+    );
+
+    %Description = $LinkObject->ObjectDescriptionGet(
+        Key     => 123,
+        UserID  => 1,
+    );
+
+=cut
+
+sub ObjectDescriptionGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Object Key UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # create description
+    my %Description = (
+        Normal => "Dummy# 123",
+        Long   => "Dummy# 123: test",
+    );
+
+    return %Description;
+}
+
+=item ObjectSearch()
+
+return a hash list of the search results
+
+Return
+    $SearchList = {
+        NOTLINKED => {
+            Source => {
+                12  => $DataOfItem12,
+                212 => $DataOfItem212,
+                332 => $DataOfItem332,
+            },
+        },
+    };
+
+    $SearchList = $LinkObjectBackend->ObjectSearch(
+        SearchParams => $HashRef,  # (optional)
+        UserID       => 1,
+    );
+
+=cut
+
+sub ObjectSearch {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{UserID} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'Need UserID!',
+        );
+        return;
+    }
+
+    return {};
+}
+
+=item LinkAddPre()
+
+link add pre event module
+
+    $True = $LinkObject->LinkAddPre(
+        Key          => 123,
+        SourceObject => 'Dummy',
+        SourceKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+    or
+
+    $True = $LinkObject->LinkAddPre(
+        Key          => 123,
+        TargetObject => 'Dummy',
+        TargetKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+=cut
+
+sub LinkAddPre {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Type UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return 1;
+}
+
+=item LinkAddPost()
+
+link add pre event module
+
+    $True = $LinkObject->LinkAddPost(
+        Key          => 123,
+        SourceObject => 'Dummy',
+        SourceKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+    or
+
+    $True = $LinkObject->LinkAddPost(
+        Key          => 123,
+        TargetObject => 'Dummy',
+        TargetKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+=cut
+
+sub LinkAddPost {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Type UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return 1;
+}
+
+=item LinkDeletePre()
+
+link delete pre event module
+
+    $True = $LinkObject->LinkDeletePre(
+        Key          => 123,
+        SourceObject => 'Dummy',
+        SourceKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+    or
+
+    $True = $LinkObject->LinkDeletePre(
+        Key          => 123,
+        TargetObject => 'Dummy',
+        TargetKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+=cut
+
+sub LinkDeletePre {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Type UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return 1;
+}
+
+=item LinkDeletePost()
+
+link delete post event module
+
+    $True = $LinkObject->LinkDeletePost(
+        Key          => 123,
+        SourceObject => 'Dummy',
+        SourceKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+    or
+
+    $True = $LinkObject->LinkDeletePost(
+        Key          => 123,
+        TargetObject => 'Dummy',
+        TargetKey    => 321,
+        Type         => 'Normal',
+        UserID       => 1,
+    );
+
+=cut
+
+sub LinkDeletePost {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Type UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return 1;
+}
+
+1;
