f3ba26c3f710e48d42ae29cc0412551398273d0f
Udo Bretz <udo.bretz@otrs.com>
2008-06-30 14:24:47 +0000

    Fixed wrong comment.

--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPhone.pm - to handle phone calls
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPhone.pm,v 1.75 2008-06-23 09:58:58 ub Exp $
+# $Id: AgentTicketPhone.pm,v 1.76 2008-06-30 14:24:47 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -23,7 +23,7 @@ use Kernel::System::LinkObject;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.75 $) [1];
+$VERSION = qw($Revision: 1.76 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1235,7 +1235,7 @@ sub _GetTypes {
 
     my %Type = ();
 
-    # get priority
+    # get type
     if ( $Param{QueueID} || $Param{TicketID} ) {
         %Type = $Self->{TicketObject}->TicketTypeList(
             %Param,
