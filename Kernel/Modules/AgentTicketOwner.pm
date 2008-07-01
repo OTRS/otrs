b24c57644bc479be58384a228ab7dee902998eb3
Udo Bretz <udo.bretz@otrs.com>
2008-07-01 11:32:23 +0000

    Small code layout change, added comma.

--- Kernel/Modules/AgentTicketOwner.pm
+++ Kernel/Modules/AgentTicketOwner.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketOwner.pm - set ticket owner
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketOwner.pm,v 1.41 2008-06-25 22:48:27 martin Exp $
+# $Id: AgentTicketOwner.pm,v 1.42 2008-07-01 11:32:23 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::State;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.41 $) [1];
+$VERSION = qw($Revision: 1.42 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -704,7 +704,7 @@ sub Run {
         $Output .= $Self->{LayoutObject}->NavigationBar();
         $Output .= $Self->_Mask(
             %GetParam, %Ticket, %TicketFreeTextHTML, %TicketFreeTimeHTML,
-            %ArticleFreeTextHTML
+            %ArticleFreeTextHTML,
         );
         $Output .= $Self->{LayoutObject}->Footer();
         return $Output;
--- Kernel/Modules/AgentTicketPending.pm
+++ Kernel/Modules/AgentTicketPending.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPending.pm - set ticket to pending
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPending.pm,v 1.46 2008-06-25 22:48:27 martin Exp $
+# $Id: AgentTicketPending.pm,v 1.47 2008-07-01 11:32:23 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::State;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.46 $) [1];
+$VERSION = qw($Revision: 1.47 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -704,7 +704,7 @@ sub Run {
         $Output .= $Self->{LayoutObject}->NavigationBar();
         $Output .= $Self->_Mask(
             %GetParam, %Ticket, %TicketFreeTextHTML, %TicketFreeTimeHTML,
-            %ArticleFreeTextHTML
+            %ArticleFreeTextHTML,
         );
         $Output .= $Self->{LayoutObject}->Footer();
         return $Output;
