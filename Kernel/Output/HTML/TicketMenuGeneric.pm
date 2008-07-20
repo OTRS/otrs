bc89012c93ab1c7d8df60369a7a6837b612a64f5
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-07-20 10:10:08 +0000

    Fixed bug# 3102 - (Optional) group-Permissions for Frontend::Agent::Ticket::MenuModule(Pre).

--- Kernel/Output/HTML/TicketMenuGeneric.pm
+++ Kernel/Output/HTML/TicketMenuGeneric.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/TicketMenuGeneric.pm
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: TicketMenuGeneric.pm,v 1.8 2008-05-08 09:36:57 mh Exp $
+# $Id: TicketMenuGeneric.pm,v 1.9 2008-07-20 10:10:08 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -25,7 +25,7 @@ sub new {
     bless( $Self, $Type );
 
     # get needed objects
-    for (qw(ConfigObject LogObject DBObject LayoutObject UserID TicketObject)) {
+    for (qw(ConfigObject LogObject DBObject LayoutObject UserID GroupObject TicketObject)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
 
@@ -45,17 +45,13 @@ sub Run {
     if ( $Self->{ConfigObject}->Get("Ticket::Frontend::$Param{Config}->{Action}") ) {
         my $Config = $Self->{ConfigObject}->Get("Ticket::Frontend::$Param{Config}->{Action}");
         if ( $Config->{Permission} ) {
-            if (
-                !$Self->{TicketObject}->Permission(
-                    Type     => $Config->{Permission},
-                    TicketID => $Param{TicketID},
-                    UserID   => $Self->{UserID},
-                    LogNo    => 1,
-                )
-                )
-            {
-                return $Param{Counter};
-            }
+            my $AccessOk = $Self->{TicketObject}->Permission(
+                Type     => $Config->{Permission},
+                TicketID => $Param{TicketID},
+                UserID   => $Self->{UserID},
+                LogNo    => 1,
+            );
+            return $Param{Counter} if !$AccessOk;
         }
         if ( $Config->{RequiredLock} ) {
             if ( $Self->{TicketObject}->LockIsTicketLocked( TicketID => $Param{TicketID} ) ) {
@@ -63,10 +59,39 @@ sub Run {
                     TicketID => $Param{TicketID},
                     OwnerID  => $Self->{UserID},
                 );
-
                 return $Param{Counter} if !$AccessOk;
             }
         }
+    }
+
+    # group check
+    if ( $Param{Config}->{Group} ) {
+        my @Items = split /;/, $Param{Config}->{Group};
+        my $AccessOk;
+        for my $Item (@Items) {
+            my ($Permission, $Name) = split /:/, $Item;
+            if ( !$Permission || !$Name ) {
+                $Self->{LogObject}->Log(
+                    Priority => 'error',
+                    Message  => "Invalid config for Key Group: '$Item'! "
+                        . "Need something like '\$Permission:\$Group;'",
+                );
+            }
+            my @Groups = $Self->{GroupObject}->GroupMemberList(
+                UserID => $Self->{UserID},
+                Type   => $Permission,
+                Result => 'Name',
+            );
+            next if !@Groups;
+
+            for my $Group (@Groups) {
+                if ( $Group eq $Name ) {
+                    $AccessOk = 1;
+                    last;
+                }
+            }
+        }
+        return $Param{Counter} if !$AccessOk;
     }
 
     # check acl
