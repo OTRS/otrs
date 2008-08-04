a67af92de9318035089ff681ad6ba2c20149474a
Manuel Hecht <manuel.hecht@otrs.com>
2008-08-04 14:08:43 +0000

    Small code cleanup.

--- Kernel/System/Ticket/Acl/CloseParentAfterClosedChilds.pm
+++ Kernel/System/Ticket/Acl/CloseParentAfterClosedChilds.pm
@@ -3,7 +3,7 @@
 # - allow no parent close till all clients are closed -
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: CloseParentAfterClosedChilds.pm,v 1.10 2008-08-01 06:53:12 mh Exp $
+# $Id: CloseParentAfterClosedChilds.pm,v 1.11 2008-08-04 14:08:43 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,7 +18,7 @@ use warnings;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -50,103 +50,104 @@ sub Run {
     }
 
     # check if child tickets are not closed
-    if ( $Param{TicketID} && $Param{UserID} ) {
+    return 1 if !$Param{TicketID} || !$Param{UserID};
+
+    # get ticket
+    my %Ticket = $Self->{TicketObject}->TicketGet(
+        TicketID => $Param{TicketID},
+    );
+
+    # create new instance of the link object
+    if ( !$Self->{LinkObject} ) {
+        $Self->{LinkObject} = Kernel::System::LinkObject->new(
+            %{$Self},
+            %Param,
+        );
+    }
+
+    # link tickets
+    my $Links = $Self->{LinkObject}->LinkList(
+        Object => 'Ticket',
+        Key    => $Param{TicketID},
+        State  => 'Valid',
+        Type   => 'ParentChild',
+        UserID => $Param{UserID},
+    );
+
+    return 1 if !$Links;
+    return 1 if ref $Links ne 'HASH';
+    return 1 if !$Links->{Ticket};
+    return 1 if ref $Links->{Ticket} ne 'HASH';
+    return 1 if !$Links->{Ticket}->{ParentChild};
+    return 1 if ref $Links->{Ticket}->{ParentChild} ne 'HASH';
+    return 1 if !$Links->{Ticket}->{ParentChild}->{Target};
+    return 1 if ref $Links->{Ticket}->{ParentChild}->{Target} ne 'ARRAY';
+
+    my $OpenSubTickets = 0;
+    TICKETID:
+    for my $TicketID ( sort @{ $Links->{Ticket}->{ParentChild}->{Target} } ) {
 
         # get ticket
         my %Ticket = $Self->{TicketObject}->TicketGet(
-            TicketID => $Param{TicketID},
+            TicketID => $TicketID,
         );
 
-        # create new instance of the link object
-        if ( !$Self->{LinkObject} ) {
-            $Self->{LinkObject} = Kernel::System::LinkObject->new(
-                %{$Self},
-                %Param,
-            );
-        }
-
-        # link tickets
-        my $Links = $Self->{LinkObject}->LinkList(
-            Object => 'Ticket',
-            Key    => $Param{TicketID},
-            State  => 'Valid',
-            Type   => 'ParentChild',
-            UserID => $Param{UserID},
-        );
-
-        return 1 if !$Links;
-        return 1 if ref $Links ne 'HASH';
-        return 1 if !$Links->{Ticket};
-        return 1 if ref $Links->{Ticket} ne 'HASH';
-        return 1 if !$Links->{Ticket}->{ParentChild};
-        return 1 if ref $Links->{Ticket}->{ParentChild} ne 'HASH';
-        return 1 if !$Links->{Ticket}->{ParentChild}->{Target};
-        return 1 if ref $Links->{Ticket}->{ParentChild}->{Target} ne 'ARRAY';
-
-        my $OpenSubTickets = 0;
-        for my $TicketID ( sort @{ $Links->{Ticket}->{ParentChild}->{Target} } ) {
-
-            # get ticket
-            my %Ticket = $Self->{TicketObject}->TicketGet(
-                TicketID => $TicketID,
-            );
-
-            if ( $Ticket{StateType} !~ /^(close|merge|remove)/ ) {
-                $OpenSubTickets = 1;
-                last;
-            }
-        }
-
-        # generate acl
-        if ($OpenSubTickets) {
-            $Param{Acl}->{CloseParentAfterClosedChilds} = {
-
-                # match properties
-                Properties => {
-
-                    # current ticket match properties
-                    Ticket => {
-                        TicketID => [ $Param{TicketID} ],
-                    },
-                },
-
-                # return possible options (black list)
-                PossibleNot => {
-
-                    # possible ticket options (black list)
-                    Ticket => {
-                        State => $Param{Config}->{State},
-                    },
-                },
-
-                # return possible options (white list)
-                Possible => {
-
-                    # possible action options
-                    Action => {
-                        AgentTicketLock        => 1,
-                        AgentTicketZoom        => 1,
-                        AgentTicketClose       => 0,
-                        AgentTicketPending     => 1,
-                        AgentTicketNote        => 1,
-                        AgentTicketHistory     => 1,
-                        AgentTicketPriority    => 1,
-                        AgentTicketFreeText    => 1,
-                        AgentTicketHistory     => 1,
-                        AgentTicketCompose     => 1,
-                        AgentTicketBounce      => 1,
-                        AgentTicketForward     => 1,
-                        AgentLinkObject        => 1,
-                        AgentTicketPrint       => 1,
-                        AgentTicketPhone       => 1,
-                        AgentTicketCustomer    => 1,
-                        AgentTicketOwner       => 1,
-                        AgentTicketResponsible => 1,
-                    },
-                },
-            };
+        if ( $Ticket{StateType} !~ m{ \A (close|merge|remove) }xms ) {
+            $OpenSubTickets = 1;
+            last TICKETID;
         }
     }
+
+    # generate acl
+    if ($OpenSubTickets) {
+        $Param{Acl}->{CloseParentAfterClosedChilds} = {
+
+            # match properties
+            Properties => {
+
+                # current ticket match properties
+                Ticket => {
+                    TicketID => [ $Param{TicketID} ],
+                },
+            },
+
+            # return possible options (black list)
+            PossibleNot => {
+
+                # possible ticket options (black list)
+                Ticket => {
+                    State => $Param{Config}->{State},
+                },
+            },
+
+            # return possible options (white list)
+            Possible => {
+
+                # possible action options
+                Action => {
+                    AgentTicketLock        => 1,
+                    AgentTicketZoom        => 1,
+                    AgentTicketClose       => 0,
+                    AgentTicketPending     => 1,
+                    AgentTicketNote        => 1,
+                    AgentTicketHistory     => 1,
+                    AgentTicketPriority    => 1,
+                    AgentTicketFreeText    => 1,
+                    AgentTicketHistory     => 1,
+                    AgentTicketCompose     => 1,
+                    AgentTicketBounce      => 1,
+                    AgentTicketForward     => 1,
+                    AgentLinkObject        => 1,
+                    AgentTicketPrint       => 1,
+                    AgentTicketPhone       => 1,
+                    AgentTicketCustomer    => 1,
+                    AgentTicketOwner       => 1,
+                    AgentTicketResponsible => 1,
+                },
+            },
+        };
+    }
+
     return 1;
 }
 
