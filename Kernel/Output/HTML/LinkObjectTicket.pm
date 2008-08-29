f2d2b6e0fab17efc00ef1e56385823c52c07d4c2
Manuel Hecht <manuel.hecht@otrs.com>
2008-08-29 15:36:29 +0000

    Code cleanup with PerlTidy.

--- Kernel/Output/HTML/LinkObjectTicket.pm
+++ Kernel/Output/HTML/LinkObjectTicket.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/LinkObjectTicket.pm - layout backend module
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: LinkObjectTicket.pm,v 1.16 2008-08-29 14:32:30 mh Exp $
+# $Id: LinkObjectTicket.pm,v 1.17 2008-08-29 15:36:29 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,7 +18,7 @@ use Kernel::Output::HTML::Layout;
 use Kernel::System::State;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 =head1 NAME
 
@@ -50,9 +50,11 @@ sub new {
     bless( $Self, $Type );
 
     # check needed objects
-    for my $Object (qw(ConfigObject LogObject MainObject DBObject UserObject EncodeObject
+    for my $Object (
+        qw(ConfigObject LogObject MainObject DBObject UserObject EncodeObject
         QueueObject GroupObject ParamObject TimeObject LanguageObject UserLanguage UserID)
-    ){
+        )
+    {
         $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
     }
     $Self->{LayoutObject} = Kernel::Output::HTML::Layout->new( %{$Self} );
@@ -534,6 +536,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.16 $ $Date: 2008-08-29 14:32:30 $
+$Revision: 1.17 $ $Date: 2008-08-29 15:36:29 $
 
 =cut
