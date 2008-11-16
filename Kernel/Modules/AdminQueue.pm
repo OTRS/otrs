b79cba438ef5750fd8ecb78027bb352d38125393
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-11-16 18:17:38 +0000

    Moved from 20 to 50 possible calendars.

--- Kernel/Modules/AdminQueue.pm
+++ Kernel/Modules/AdminQueue.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminQueue.pm - to add/update/delete queues
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminQueue.pm,v 1.43 2008-06-20 16:55:33 mh Exp $
+# $Id: AdminQueue.pm,v 1.43.2.1 2008-11-16 18:17:38 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -21,7 +21,7 @@ use Kernel::System::Signature;
 use Kernel::System::SystemAddress;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.43 $) [1];
+$VERSION = qw($Revision: 1.43.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -411,7 +411,7 @@ sub _Mask {
     );
     my %Calendar = ( '' => '-' );
 
-    for ( '', 1 .. 20 ) {
+    for ( '', 1 .. 50 ) {
         if ( $Self->{ConfigObject}->Get("TimeVacationDays::Calendar$_") ) {
             $Calendar{$_} = "Calendar $_ - "
                 . $Self->{ConfigObject}->Get( "TimeZone::Calendar" . $_ . "Name" );
