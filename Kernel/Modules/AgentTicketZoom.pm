be79fc1f507d5e7618c33bb8eaf9b4a2e38c2800
Udo Bretz <udo.bretz@otrs.com>
2008-11-26 14:06:24 +0000

    Fixed bug# 3207 - Expanded articles in wrong order.

--- Kernel/Modules/AgentTicketZoom.pm
+++ Kernel/Modules/AgentTicketZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketZoom.pm - to get a closer view
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketZoom.pm,v 1.61.2.2 2008-11-01 14:12:52 ub Exp $
+# $Id: AgentTicketZoom.pm,v 1.61.2.3 2008-11-26 14:06:24 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::CustomerUser;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.61.2.2 $) [1];
+$VERSION = qw($Revision: 1.61.2.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -335,6 +335,7 @@ sub MaskAgentZoom {
 
     # resort article order
     if ( $Self->{ZoomExpandSort} eq 'reverse' ) {
+        @ArticleBox    = reverse(@ArticleBox);
         @NewArticleBox = reverse(@NewArticleBox);
     }
 
