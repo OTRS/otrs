dd9219a978623992a68672703970e26d35bbb700
tt <>
2009-01-20 13:32:25 +0000

    Fixed bug# 3447 - CustomerUser-Renaming to existing UserLogin was possible.

--- Kernel/System/CustomerUser.pm
+++ Kernel/System/CustomerUser.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/CustomerUser.pm - some customer user functions
-# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: CustomerUser.pm,v 1.47 2008-12-04 16:28:27 martin Exp $
+# $Id: CustomerUser.pm,v 1.48 2009-01-20 13:32:25 tt Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -16,7 +16,7 @@ use warnings;
 use Kernel::System::CustomerCompany;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.47 $) [1];
+$VERSION = qw($Revision: 1.48 $) [1];
 
 =head1 NAME
 
@@ -368,6 +368,18 @@ sub CustomerUserUpdate {
         return;
     }
 
+    # check for UserLogin-renaming and if new UserLogin already exists...
+    if ( ($Param{ID}) &&  ($Param{UserLogin} ne $Param{ID}) ) {
+        my %User = $Self->CustomerUserDataGet( User => $Param{UserLogin} );
+        if (%User) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "User already exists '$Param{UserLogin}'!",
+            );
+            return;
+        }
+    }
+
     # check if user exists
     my %User = $Self->CustomerUserDataGet( User => $Param{ID} || $Param{UserLogin} );
     if ( !%User ) {
@@ -584,6 +596,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.47 $ $Date: 2008-12-04 16:28:27 $
+$Revision: 1.48 $ $Date: 2009-01-20 13:32:25 $
 
 =cut
