99fc23b66b32617e985a39f6f52dc03e75685dc7
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-01-31 17:54:42 +0000

    Improved security validations.

--- scripts/login.pl
+++ scripts/login.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
 # login.pl - a simple external login page for OTRS
-# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: login.pl,v 1.6 2008-04-24 17:32:15 tr Exp $
+# $Id: login.pl,v 1.6.2.1 2009-01-31 17:54:42 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -45,6 +45,11 @@ my $ReasonTranslation = {
     ''               => '',
 };
 
+# html quote
+$User                         = _Ascii2Html($User);
+$RequestedURL                 = _Ascii2Html($RequestedURL);
+$ReasonTranslation->{$Reason} = _Ascii2Html( $ReasonTranslation->{$Reason} );
+
 # html page
 print "Content-Type: text/html
 
@@ -57,28 +62,39 @@ print "Content-Type: text/html
 <h1>login page</h1>
 
 <p>
-<font color='red'>$ReasonTranslation->{$Reason}</font>
+<font color=\"red\">$ReasonTranslation->{$Reason}</font>
 </p>
 
-<form action='$OTRSLocation' method='get' enctype='application/x-www-form-urlencoded'>
-<input type='hidden' name='Action' value='Login'>
-<input type='hidden' name='RequestedURL' value='$RequestedURL'>
+<form action=\"$OTRSLocation\" method=\"get\" enctype=\"application/x-www-form-urlencoded\">
+<input type=\"hidden\" name=\"Action\" value=\"Login\">
+<input type=\"hidden\" name=\"RequestedURL\" value=\"$RequestedURL\">
 
-<table cellspacing='8' cellpadding='2'>
+<table cellspacing=\"8\" cellpadding=\"2\">
 <tr>
     <td>Username:</td>
-    <td><input type='text' name='User' value='$User' size='18'></td>
+    <td><input type=\"text\" name=\"User\" value=\"$User\" size=\"18\"></td>
 </tr>
 <tr>
     <td>Password:</td>
-    <td><input type='password' name='Password' size='18'></td>
+    <td><input type=\"password\" name=\"Password\" size=\"18\"></td>
 </tr>
 </table>
 
-<input type='submit' value='Login'>
+<input type=\"submit\" value=\"Login\">
 </form>
 
 </body>
 </html>
 
 ";
+
+sub _Ascii2Html {
+    my $Text = shift;
+
+    $Text =~ s/&/&amp;/g;
+    $Text =~ s/</&lt;/g;
+    $Text =~ s/>/&gt;/g;
+    $Text =~ s/"/&quot;/g;
+
+    return $Text;
+}
