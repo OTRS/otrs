3d1da4ad6cdd338b02070051f0da2258298d8f75
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-02-17 00:19:32 +0000

    Added perferences setting for watcher notification.

---
+++ Kernel/Output/HTML/PreferencesTicketWatcher.pm
@@ -0,0 +1,119 @@
+# --
+# Kernel/Output/HTML/PreferencesTicketWatcher.pm
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: PreferencesTicketWatcher.pm,v 1.1 2009-02-17 00:19:32 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::PreferencesTicketWatcher;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(ConfigObject LogObject DBObject LayoutObject UserID ParamObject ConfigItem)) {
+        die "Got no $_!" if !$Self->{$_};
+    }
+
+    return $Self;
+}
+
+sub Param {
+    my ( $Self, %Param ) = @_;
+
+    # check if feature is aktive
+    return if !$Self->{ConfigObject}->Get('Ticket::Watcher');
+
+    # check access
+    my @Groups;
+    if ( $Self->{ConfigObject}->Get('Ticket::WatcherGroup') ) {
+        @Groups = @{ $Self->{ConfigObject}->Get('Ticket::WatcherGroup') };
+    }
+    my $Access = 0;
+    if ( !@Groups ) {
+        $Access = 1;
+    }
+    else {
+        for my $Group (@Groups) {
+            next if !$Self->{LayoutObject}->{"UserIsGroup[$Group]"};
+            if ( $Self->{LayoutObject}->{"UserIsGroup[$Group]"} eq 'Yes' ) {
+                $Access = 1;
+                last;
+            }
+        }
+    }
+
+    # return on no access
+    return if !$Access;
+
+    my @Params = ();
+    push(
+        @Params,
+        {
+            %Param,
+            Name       => $Self->{ConfigItem}->{PrefKey},
+            SelectedID => $Param{UserData}->{UserSendWatcherNotification} || 0,
+            Block      => 'Option',
+        },
+    );
+    return @Params;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    for my $Key ( keys %{ $Param{GetParam} } ) {
+        my @Array = @{ $Param{GetParam}->{$Key} };
+        for (@Array) {
+
+            # pref update db
+            if ( !$Self->{ConfigObject}->Get('DemoSystem') ) {
+                $Self->{UserObject}->SetPreferences(
+                    UserID => $Param{UserData}->{UserID},
+                    Key    => $Key,
+                    Value  => $_,
+                );
+            }
+
+            # update SessionID
+            if ( $Param{UserData}->{UserID} eq $Self->{UserID} ) {
+                $Self->{SessionObject}->UpdateSessionID(
+                    SessionID => $Self->{SessionID},
+                    Key       => $Key,
+                    Value     => $_,
+                );
+            }
+        }
+    }
+
+    $Self->{Message} = 'Preferences updated successfully!';
+    return 1;
+}
+
+sub Error {
+    my ( $Self, %Param ) = @_;
+
+    return $Self->{Error} || '';
+}
+
+sub Message {
+    my ( $Self, %Param ) = @_;
+
+    return $Self->{Message} || '';
+}
+
+1;
