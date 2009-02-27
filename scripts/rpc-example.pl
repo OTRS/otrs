1d2f6e68922a1adeefaf20686daf5d47788b9c18
Thomas Raith <thomas.raith@otrs.com>
2009-02-27 08:40:53 +0000

    Add more examples to the test script.

--- scripts/rpc-example.pl
+++ scripts/rpc-example.pl
@@ -3,7 +3,7 @@
 # scripts/rpc-example.pl - soap example client
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: rpc-example.pl,v 1.4 2009-02-16 12:50:17 tr Exp $
+# $Id: rpc-example.pl,v 1.5 2009-02-27 08:40:53 tr Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -39,3 +39,34 @@ my %Ticket = $RPC->Dispatch( $User, $Pw, 'TicketObject', 'TicketGet', TicketID =
 print "NOTICE: Ticket Number is: $Ticket{TicketNumber}\n";
 print "NOTICE: Ticket State is:  $Ticket{State}\n";
 print "NOTICE: Ticket Queue is:  $Ticket{Queue}\n";
+
+# create a ticket
+my %TicketData = (
+    Title        => 'rpc-example.pl test ticket',
+    Queue        => 'Raw',
+    Lock         => 'unlock',
+    Priority     => '3 normal',
+    State        => 'new',
+    CustomerID   => 'www.otrs.com',
+    CustomerUser => 'customer@example.com',
+    OwnerID      => 1,
+    UserID       => 1,
+);
+
+print "NOTICE: TicketObject->TicketCreate(%TicketData)\n";
+my $TicketID = $RPC->Dispatch( $User, $Pw, 'TicketObject', 'TicketCreate', %TicketData => 1 );
+print "NOTICE: TicketID is $TicketID\n";
+
+# delete the ticket
+print "NOTICE: TicketObject->TicketDelete(TicketID => $TicketID)\n";
+my $Feedback = $RPC->Dispatch( $User, $Pw, 'TicketObject', 'TicketDelete', TicketID => $TicketID, UserID => 1 );
+my $Message = $Feedback ? 'was successful' : 'was not successful';
+print "NOTICE: Delete Ticket with ID $TicketID $Message\n";
+
+# check if the customer exits
+print "NOTICE: CustomerUserObject->CustomerName(UserLogin => 'test-user')\n";
+my $Name = $RPC->Dispatch( $User, $Pw, 'CustomerUserObject', 'CustomerName', UserLogin => 'test-user' );
+$Message = $Name ? 'exists' : 'does not exists' ;
+print "NOTICE: The customer with the login 'test-user' $Message\n";
+
+exit 0;
