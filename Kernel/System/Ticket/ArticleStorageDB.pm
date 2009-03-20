b00013b2a2751ec52af0c04c5505624de5e3feb5
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-03-20 18:13:00 +0000

    Fixed not working file attachment compat. mode to file located attachmens created with OTRS 2.1 and lower.

--- Kernel/System/Ticket/ArticleStorageDB.pm
+++ Kernel/System/Ticket/ArticleStorageDB.pm
@@ -1,12 +1,12 @@
 # --
 # Kernel/System/Ticket/ArticleStorageDB.pm - article storage module for OTRS kernel
-# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: ArticleStorageDB.pm,v 1.55 2008-05-08 09:36:21 mh Exp $
+# $Id: ArticleStorageDB.pm,v 1.55.2.1 2009-03-20 18:13:00 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (GPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
 package Kernel::System::Ticket::ArticleStorageDB;
@@ -18,7 +18,7 @@ use MIME::Base64;
 use MIME::Words qw(:all);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.55 $) [1];
+$VERSION = qw($Revision: 1.55.2.1 $) [1];
 
 sub ArticleStorageInit {
     my ( $Self, %Param ) = @_;
@@ -541,6 +541,7 @@ sub ArticleAttachment {
                         );
                         if ($Content) {
                             $Data{ContentType} = $Content->[0];
+                            my $Counter = 0;
                             for my $Line ( @{$Content} ) {
                                 if ($Counter) {
                                     $Data{Content} .= $Line;
