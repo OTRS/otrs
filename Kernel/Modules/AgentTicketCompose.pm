2687dda1c0e1cc87c71060a55f80393fea4424d6
Stefan Bedorf <stefan.bedorf@otrs.com>
2009-03-25 14:22:42 +0000

    Quote original html email on div as well.

--- Kernel/Modules/AgentTicketCompose.pm
+++ Kernel/Modules/AgentTicketCompose.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketCompose.pm - to compose and send a message
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketCompose.pm,v 1.50 2009-03-25 13:54:31 sb Exp $
+# $Id: AgentTicketCompose.pm,v 1.51 2009-03-25 14:22:42 sb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::TemplateGenerator;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.50 $) [1];
+$VERSION = qw($Revision: 1.51 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -738,7 +738,7 @@ sub Run {
                     HTMLResultMode => 1,
                 );
                 if ($Quote) {
-                    $Data{BodyHTML} =~ s/(<(br|p).*?>)/$1$Quote /ig;
+                    $Data{BodyHTML} =~ s/(<(br|p|div).*?>)/$1$Quote /ig;
                     $Data{BodyHTML} = "<br/>$Quote " . $Data{BodyHTML};
                 }
                 else {
