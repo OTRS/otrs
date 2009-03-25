3be7ec1373b091875f1fbcfc64d7acf8fe365d68
Stefan Bedorf <stefan.bedorf@otrs.com>
2009-03-25 13:38:10 +0000

    Use no real filename for html body saved as attachment.

--- Kernel/System/EmailParser.pm
+++ Kernel/System/EmailParser.pm
@@ -2,7 +2,7 @@
 # Kernel/System/EmailParser.pm - the global email parser module
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: EmailParser.pm,v 1.77 2009-03-25 08:52:32 martin Exp $
+# $Id: EmailParser.pm,v 1.78 2009-03-25 13:38:10 sb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Mail::Address;
 use Kernel::System::Encode;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.77 $) [1];
+$VERSION = qw($Revision: 1.78 $) [1];
 
 =head1 NAME
 
@@ -794,7 +794,7 @@ sub CheckMessageBody {
                     Charset     => $Self->GetCharset(),
                     ContentType => $Self->GetReturnContentType(),
                     Content     => $Self->{MessageBody},
-                    Filename    => 'orig-html-email.html',
+                    Filename    => 'file-1',
                 }
             );
         }
@@ -1215,6 +1215,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.77 $ $Date: 2009-03-25 08:52:32 $
+$Revision: 1.78 $ $Date: 2009-03-25 13:38:10 $
 
 =cut
