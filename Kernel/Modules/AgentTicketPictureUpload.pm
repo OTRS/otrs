ab8705dda9b1c879cfbd1062ab71cb58af971eec
Stefan Bedorf <stefan.bedorf@otrs.com>
2009-03-25 13:42:42 +0000

    Added image uploader to yui rich text editor.

---
+++ Kernel/Modules/AgentTicketPictureUpload.pm
@@ -0,0 +1,137 @@
+# --
+# Kernel/Modules/AgentTicketPictureUpload.pm - get picture uploads
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: AgentTicketPictureUpload.pm,v 1.1 2009-03-25 13:42:42 sb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentTicketPictureUpload;
+
+use strict;
+use warnings;
+
+use Kernel::System::Web::UploadCache;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for (qw(ParamObject DBObject TicketObject LayoutObject LogObject ConfigObject)) {
+        if ( !$Self->{$_} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
+        }
+    }
+
+    $Self->{UploadCachObject} = Kernel::System::Web::UploadCache->new(%Param);
+
+    $Self->{FormID} = $Self->{ParamObject}->GetParam( Param => 'FormID' );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $Output = "Content-Type: text/html; charset=" . $Self->{ConfigObject}->Get('DefaultCharset') . ";\n\n";
+
+    if ( !$Self->{FormID} ) {
+        $Output .= "{status:'Got no FormID!'}";
+        return $Output;
+    }
+
+    if ( $Self->{ParamObject}->GetParam( Param => 'ContentID' ) ) {
+        my $ContentID = $Self->{ParamObject}->GetParam( Param => 'ContentID' ) || '';
+
+        # return image inline
+        my @AttachmentData = $Self->{UploadCachObject}->FormIDGetAllFilesData( FormID => $Self->{FormID} );
+        ATTACHMENTDATA:
+        for my $TmpAttachment (@AttachmentData) {
+            next ATTACHMENTDATA if $TmpAttachment->{ContentID} ne $ContentID;
+            return $Self->{LayoutObject}->Attachment(
+                Type => 'inline',
+                %{ $TmpAttachment },
+            );
+        }
+    }
+
+    # upload new picture
+    my %File = $Self->{ParamObject}->GetUploadAll(
+        Param  => 'param_name',
+        Source => 'string',
+    );
+
+    if ( !%File ) {
+        $Output .= "{status:'Got no File!'}";
+        return $Output;
+    }
+
+    if ( $File{Filename} !~ /\.(png|gif|jpg|jpeg)$/i ) {
+        $Output .= "{status:'Only gif, jp(e)g and png images allowed!'}";
+        return $Output;
+    }
+
+    my @AttachmentMeta = $Self->{UploadCachObject}->FormIDGetAllFilesMeta(
+        FormID => $Self->{FormID}
+    );
+    my $TmpFilename = $File{Filename};
+    my $TmpSuffix = 0;
+    my $UniqueFilename = '';
+    while ( !$UniqueFilename ) {
+        $UniqueFilename = $TmpFilename;
+        NEWNAME:
+        for my $TmpAttachment ( reverse @AttachmentMeta ) {
+            next NEWNAME if $TmpFilename ne $TmpAttachment->{Filename};
+
+            # name exists -> change
+            ++$TmpSuffix;
+            if ( $File{Filename} =~ /^(.*)\.(.+?)$/ ) {
+                $TmpFilename = "$1-$TmpSuffix.$2";
+            }
+            else {
+                $TmpFilename = "$File{Filename}-$TmpSuffix";
+            }
+            $UniqueFilename = '';
+            last NEWNAME;
+        }
+    }
+    $Self->{UploadCachObject}->FormIDAddFile(
+        FormID      => $Self->{FormID},
+        Filename    => $TmpFilename,
+        Content     => $File{Content},
+        ContentType => $File{ContentType} . '; name="' . $TmpFilename . '"',
+        Disposition => 'inline',
+    );
+    my $ContentID = '';
+    @AttachmentMeta = $Self->{UploadCachObject}->FormIDGetAllFilesMeta(
+        FormID => $Self->{FormID}
+    );
+    CONTENTID:
+    for my $TmpAttachment ( @AttachmentMeta ) {
+        next CONTENTID if $TmpFilename ne $TmpAttachment->{Filename};
+        $ContentID = $TmpAttachment->{ContentID};
+        last CONTENTID;
+    }
+
+    $Output .= "{status:'UPLOADED', image_url:'"
+        . $Self->{LayoutObject}->{Baselink}
+        . "Action=AgentTicketPictureUpload"
+        . "&FormID="
+        . $Self->{FormID}
+        . "&ContentID="
+        . $ContentID
+        . "'}";
+    return $Output;
+}
+
+1;
---
+++ var/httpd/htdocs/js/yui-image-uploader.js
@@ -0,0 +1,108 @@
+// --
+// yui-image-uploader.js - provides image upload functionality for yui rich text editor
+// Copyright (C) 2001-2009 OTRS AG, http://otrs.org/\n";
+// --
+// $Id: yui-image-uploader.js,v 1.1 2009-03-25 13:42:42 sb Exp $
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+/*
+yuiImgUploader
+variables: 
+ rte: The YAHOO.widget.Editor instance
+ upload_url: the url to post the file to
+ upload_image_name: the name of the post parameter to send the file as
+
+Your server must handle the posted image.  You must return a JSON object
+with the result url that the image can be viewed at on your server.  If 
+the upload fails, you can return an error message.  For successful
+uploads, the status must be set to UPLOADED.  All other status messages,
+or the lack of a status message is interpreted as an error.  IE will 
+try to open a new document window when the response is returned if your
+content-type header on your response is not set to 'text/javascript'
+
+Example Success:
+{status:'UPLOADED', image_url:'/somedirectory/filename'}
+Example Failure:
+{status:'We only allow JPEG Images.'}
+
+*/
+
+function yuiImgUploader(rte, editor_name, upload_url, upload_image_name) {
+   // customize the editor img button 
+
+
+   YAHOO.log( "Adding Click Listener" ,'debug');
+   rte.addListener('toolbarLoaded',function() {
+       rte.toolbar.addListener ( 'insertimageClick', function(o) {
+           try {
+               var imgPanel=new YAHOO.util.Element(editor_name + '-panel');
+               imgPanel.on ( 'contentReady', function() {
+                   try {
+                       var Dom=YAHOO.util.Dom;
+
+                       if (! Dom.get(editor_name + '_insertimage_upload'))
+                       {
+                           var label=document.createElement('label');
+                           label.innerHTML='<strong>Upload:</strong>'+
+                           '<input type="file" id="' +
+                           editor_name + '_insertimage_upload" name="'+upload_image_name+
+                           '" size="10" style="width: 300px" />'+
+                           '</label>';
+
+                           var img_elem=Dom.get(editor_name + '_insertimage_url');
+
+                           Dom.insertAfter(
+                               label,
+                               img_elem.parentNode);
+
+                           YAHOO.util.Event.on ( editor_name + '_insertimage_upload', 'change', function(ev) {
+                               YAHOO.util.Event.stopEvent(ev); // no default click action
+                               // remember action, set new action to upload picture
+                               var OrgAction = img_elem.form.Action.value;
+                               img_elem.form.Action.value = 'AgentTicketPictureUpload';
+                               YAHOO.util.Connect.setForm ( img_elem.form, true, true );
+                               var c=YAHOO.util.Connect.asyncRequest(
+                               'POST', upload_url, {
+                                   upload:function(r){
+                                       try {
+                                           // strip pre tags if they got added somehow
+                                           resp=r.responseText.replace( /<pre>/i, '').replace ( /<\/pre>/i, '');
+                                           var o=eval('('+resp+')');
+                                           if (o.status=='UPLOADED') {
+                                               Dom.get(editor_name + '_insertimage_upload').value='';
+                                               Dom.get(editor_name + '_insertimage_url').value=o.image_url;
+                                               // tell the image panel the url changed
+                                               // hack instead of fireEvent('blur')
+                                               // which for some reason isn't working
+                                               Dom.get(editor_name + '_insertimage_url').focus();
+                                               Dom.get(editor_name + '_insertimage_upload').focus();
+                                           } else {
+                                               alert ( "Upload Failed: "+o.status );
+                                           }
+
+                                       } catch ( eee ) {
+                                           YAHOO.log( eee.message, 'error' );
+                                       }
+                                   }
+                               }
+                               );
+                               // reset action
+                              img_elem.form.Action.value = OrgAction;
+                               return false;
+                           });
+                       }
+                   }
+                   catch ( ee ) { YAHOO.log( ee.message, 'error' ); }
+               });
+           } catch ( e ) {
+               YAHOO.log( e.message, 'error' );
+           }
+       });
+   });
+
+}
+
