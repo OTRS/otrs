df0de240768467fbcdf9f4bd2b56f670aa8f7d22
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-04-07 06:20:38 +0000

    Moved to current package.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: auto_build.sh,v 1.56 2009-02-26 11:10:53 tr Exp $
+# $Id: auto_build.sh,v 1.57 2009-04-07 06:20:38 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -21,8 +21,8 @@
 # or see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.56 $>"
-echo "Copyright (C) 2001-2009 OTRS AG, http://otrs.org/"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.57 $>"
+echo "Copyright (C) 2001-2009 OTRS AG, http://otrs.org/\n";
 
 PATH_TO_CVS_SRC=$1
 PRODUCT=OTRS
@@ -187,7 +187,7 @@ bin/CheckSum.pl -a create
 
 # add pre installed packages
 mkdir var/packages/
-wget 'http://ftp.otrs.org/pub/otrs/packages/Support-1.0.7.opm'
+wget 'http://ftp.otrs.org/pub/otrs/packages/Support-1.0.14.opm'
 mv Support*.opm var/packages/
 
 # --
