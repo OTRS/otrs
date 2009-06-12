dba5d01620b763588deaadeeeaced91c15ae0b85
Jens Bothe <jens.bothe@otrs.com>
2009-06-12 21:56:02 +0000

    Location in SysConfig

--- Kernel/Output/HTML/DashboardImage.pm
+++ Kernel/Output/HTML/DashboardImage.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/DashboardImage.pm
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: DashboardImage.pm,v 1.2 2009-06-12 21:06:08 martin Exp $
+# $Id: DashboardImage.pm,v 1.3 2009-06-12 21:56:02 jb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -43,7 +43,7 @@ sub Run {
     );
 
     $Self->{LayoutObject}->Block(
-        Name => 'ContentLarge',
+        Name => $Self->{Config}->{Block},
         Data => {
             %{ $Self->{Config} },
             Name    => $Self->{Name},
