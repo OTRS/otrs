f64a249e751a9ee0cb6aa60a2f3d706cae5a3034
Jens Bothe <jens.bothe@otrs.com>
2009-06-12 19:52:30 +0000

    Added simple picture viewer for dashboard

---
+++ Kernel/Output/HTML/DashboardImage.pm
@@ -0,0 +1,59 @@
+# --
+# Kernel/Output/HTML/DashboardImage.pm
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: DashboardImage.pm,v 1.1 2009-06-12 19:51:05 jb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::DashboardImage;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(Config Name ConfigObject LogObject DBObject LayoutObject ParamObject TicketObject UserID)) {
+        die "Got no $_!" if ( !$Self->{$_} );
+    }
+
+    $Self->{CacheObject} = Kernel::System::Cache->new(%Param);
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $Content = $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentDashboardImage',
+        Data         => {
+            %{ $Self->{Config} },
+        },
+    );
+
+    $Self->{LayoutObject}->Block(
+        Name => 'ContentLarge',
+        Data => {
+            %{ $Self->{Config} },
+            Name    => $Self->{Name},
+            Content => $Content,
+        },
+    );
+
+    return 1;
+}
+
+1;
