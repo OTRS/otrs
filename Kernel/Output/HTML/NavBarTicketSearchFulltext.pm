4c6c5d1266a6f37144716ad25424caab106b79cb
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-06-19 08:19:02 +0000

    Init version.

---
+++ Kernel/Output/HTML/NavBarTicketSearchFulltext.pm
@@ -0,0 +1,51 @@
+# --
+# Kernel/Output/HTML/NavBarTicketSearchFulltext.pm
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: NavBarTicketSearchFulltext.pm,v 1.1 2009-06-19 08:19:02 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::NavBarTicketSearchFulltext;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(ConfigObject LogObject DBObject TicketObject LayoutObject UserID UserObject)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my %Return = ();
+    $Return{'0990001'} = {
+        Block       => $Param{Config}->{Block},
+        Description => $Param{Config}->{Description},
+        Name        => $Param{Config}->{Name},
+        Size        => $Param{Config}->{Size},
+        Fulltext    => '',
+        Image       => '',
+        AccessKey   => '',
+    };
+    return %Return;
+}
+
+1;
