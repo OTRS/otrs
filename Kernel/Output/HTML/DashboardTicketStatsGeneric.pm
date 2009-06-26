87877e90b8c83f421a4cc7d39e41f115974d2081
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-06-26 00:59:23 +0000

    Fixed bug# 3911 - Not all Dashboard Functions e. g. charts available in Internet Explorer 6.

--- Kernel/Output/HTML/DashboardTicketStatsGeneric.pm
+++ Kernel/Output/HTML/DashboardTicketStatsGeneric.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/DashboardTicketStatsGeneric.pm
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: DashboardTicketStatsGeneric.pm,v 1.3 2009-06-13 16:21:06 mh Exp $
+# $Id: DashboardTicketStatsGeneric.pm,v 1.4 2009-06-26 00:59:23 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -116,7 +116,7 @@ sub Run {
         TemplateFile => 'AgentDashboardTicketStats',
         Data         => {
             %{ $Self->{Config} },
-            Key    => rand 99999,
+            Key    => int rand 99999,
             Max    => $Max,
             Source => $Source,
         },
