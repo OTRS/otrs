bdb54a7c7ead85f5a54bb91bcff36655ea2e01dc
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-08-12 06:50:58 +0000

    Added compat. module, redirect to new frontend module.

---
+++ Kernel/Modules/AgentTicketMailbox.pm
@@ -0,0 +1,46 @@
+# --
+# Kernel/Modules/AgentTicketMailbox.pm - just for compat. redirect to new screen
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: AgentTicketMailbox.pm,v 1.33 2009-08-12 06:50:58 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentTicketMailbox;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.33 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for (qw(ParamObject DBObject LayoutObject LogObject ConfigObject )) {
+        if ( !$Self->{$_} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
+        }
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # compat link
+    return $Self->{LayoutObject}->Redirect(
+        OP => $Self->{LayoutObject}->{Baselink} . 'Action=AgentTicketLockedView',
+    );
+}
+
+1;
