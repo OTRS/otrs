c6d13b888e998598794934fb00bf52f7d3380f94
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-08-25 14:34:17 +0000

    Removed debug info.

--- Kernel/Modules/AgentTicketMove.pm
+++ Kernel/Modules/AgentTicketMove.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketMove.pm - move tickets to queues
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketMove.pm,v 1.36 2009-08-25 14:32:55 martin Exp $
+# $Id: AgentTicketMove.pm,v 1.37 2009-08-25 14:34:17 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::State;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.36 $) [1];
+$VERSION = qw($Revision: 1.37 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -194,7 +194,6 @@ sub Run {
 
     # check subject if body exists
     if ( $GetParam{Body} && !$GetParam{Subject} ) {
-        print STDERR "--$GetParam{Body}--\n";
         $Error{'Subject invalid'} = 'invalid';
     }
 
