0de96c7aff3a79677a4f65b2c3dab186151d4a9c
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-09-08 08:15:08 +0000

    SpellChecker Customer-Interface is not working.

---
+++ Kernel/Modules/SpellingInline.pm
@@ -0,0 +1,121 @@
+# --
+# Kernel/Modules/SpellingInline.pm - spelling module
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: SpellingInline.pm,v 1.1 2009-09-08 08:15:08 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::SpellingInline;
+
+use strict;
+use warnings;
+
+use Kernel::System::Spelling;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check all needed objects
+    for (qw(TicketObject ParamObject DBObject QueueObject LayoutObject ConfigObject LogObject)) {
+        if ( !$Self->{$_} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
+        }
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get params
+    my $SpellLanguage = $Self->{ParamObject}->GetParam( Param => 'SpellLanguage' )
+        || $Self->{UserSpellDict}
+        || $Self->{ConfigObject}->Get('SpellCheckerDictDefault');
+
+    # inline spell checker of richt text
+
+    my $JSData = '';
+    my @Text = $Self->{ParamObject}->GetArray( Param => 'textinputs[]' );
+
+    my $TextAll = '';
+    for ( my $i = 0; $i <= $#Text; $i++ ) {
+        my $Line = $Self->{LayoutObject}->JSONQuote(
+            Data => $Text[$i],
+        );
+        $JSData .= "textinputs[$i] = decodeURIComponent('$Line')\n";
+
+        # change hex escapes to the proper characters
+        $Text[$i] =~ s/%([a-fA-F0-9]{2})/pack "H2", $1/eg;
+
+        my @Lines = split( /\n/, $Text[$i] );
+        for my $Line (@Lines) {
+            $TextAll .= $Line;
+        }
+    }
+
+    # do spell check
+    my $SpellingObject = Kernel::System::Spelling->new( %{$Self} );
+    $TextAll = $Self->{LayoutObject}->RichText2Ascii(
+        String => $TextAll,
+    );
+
+    my %SpellCheck = $SpellingObject->Check(
+        Text          => $TextAll,
+        SpellLanguage => $SpellLanguage,
+    );
+
+    # check error
+    if ( $SpellingObject->Error() ) {
+        return $Self->{LayoutObject}->ErrorScreen();
+    }
+
+    $JSData .= "words[0] = [];\n";
+    $JSData .= "suggs[0] = [];\n";
+    my $Count = 0;
+    for ( sort { $a <=> $b } keys %SpellCheck ) {
+        my $Word = $Self->{LayoutObject}->Ascii2Html(
+            Text => $SpellCheck{$_}->{Word},
+            Type => 'JSText',
+        );
+        my $JS = $Self->{LayoutObject}->JSON(
+            Data => $SpellCheck{$_}->{Replace},
+        );
+
+        $JSData .= "words[0][$Count] = '$Word';\n";
+        $JSData .= "suggs[0][$Count] = $JS;\n";
+        $JSData .= "\n";
+        $Count++;
+    }
+
+    $Self->{LayoutObject}->Block(
+        Name => 'SpellCheckerInline',
+        Data => {
+            JSData => $JSData,
+        },
+    );
+
+    my $Output = $Self->{LayoutObject}->Output(
+        TemplateFile => 'SpellingInline',
+        Data         => \%Param,
+    );
+    return $Self->{LayoutObject}->Attachment(
+        ContentType => 'text/html; charset=' . $Self->{LayoutObject}->{Charset},
+        Content     => $Output,
+        Type        => 'inline',
+        NoCache     => 1,
+    );
+}
+
+1;
