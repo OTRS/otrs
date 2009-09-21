9bbc08c4df7804ed78c6090cda779ed398113a75
Martin Gruner <martin.gruner@otrs.com>
2009-09-21 14:37:13 +0000

    Fixed bug#2532 - Problem when setting queues and subqueues to invalid.

--- Kernel/Modules/AgentTicketQueue.pm
+++ Kernel/Modules/AgentTicketQueue.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketQueue.pm - the queue view of all tickets
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketQueue.pm,v 1.66 2009-04-02 13:50:00 mh Exp $
+# $Id: AgentTicketQueue.pm,v 1.66.2.1 2009-09-21 14:37:13 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::State;
 use Kernel::System::Lock;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.66 $) [1];
+$VERSION = qw($Revision: 1.66.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -221,7 +221,7 @@ sub BuildQueueView {
     );
 
     # build output ...
-    my %AllQueues = $Self->{QueueObject}->GetAllQueues();
+    my %AllQueues = $Self->{QueueObject}->QueueList( Valid => 0 );
     return $Self->_MaskQueueView(
         %Data,
         QueueID         => $Self->{QueueID},
