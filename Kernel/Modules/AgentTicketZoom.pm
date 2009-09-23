9e4eef348986edeb4074075c71b4b6ba5697a99a
Martin Gruner <martin.gruner@otrs.com>
2009-09-23 10:53:33 +0000

    Fixed bug#3923 - Frontend::Output::FilterText###OutputFilterTextAutoLink does not work.

--- Kernel/Modules/AgentTicketZoom.pm
+++ Kernel/Modules/AgentTicketZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketZoom.pm - to get a closer view
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketZoom.pm,v 1.75.2.1 2009-09-22 15:48:05 sb Exp $
+# $Id: AgentTicketZoom.pm,v 1.75.2.2 2009-09-23 10:53:33 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::CustomerUser;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.75.2.1 $) [1];
+$VERSION = qw($Revision: 1.75.2.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -560,9 +560,11 @@ sub MaskAgentZoom {
         }
 
         # show body
+        # Create a reference to an anonymous copy of %Article and pass it to
+        # the LayoutObject, because %Article may be modified afterwards.
         $Self->{LayoutObject}->Block(
             Name => $ViewMode,
-            Data => \%Article,
+            Data => {%Article},
         );
 
         # restore plain body for further processing by ArticleViewModules
--- Kernel/Output/HTML/OutputFilterTextAutoLink.pm
+++ Kernel/Output/HTML/OutputFilterTextAutoLink.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/OutputFilterTextAutoLink.pm - Auto article links filter
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: OutputFilterTextAutoLink.pm,v 1.4 2009-07-26 12:45:04 martin Exp $
+# $Id: OutputFilterTextAutoLink.pm,v 1.4.2.1 2009-09-23 10:53:33 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.4 $) [1];
+$VERSION = qw($Revision: 1.4.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -124,7 +124,7 @@ sub Post {
                 $URL = "<a href=\"$URL\" target=\"$URLRef->{Target}\">";
                 $URL     .= "<img border=\"0\" src=\"$Image\" ";
                 $URL     .= " alt=\"$URLRef->{Description}: $KeywordQuote\"";
-                $URL     .= " title=\"$URLRef->{Description}: $KeywordQuote\"></img></a>";
+                $URL     .= " title=\"$URLRef->{Description}: $KeywordQuote\"/></a>";
                 $URLLink .= ' ' if ($URLLink);
                 $URLLink .= $URL;
             }
