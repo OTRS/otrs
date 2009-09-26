c07c803ce2204d3b2dc1bcb0b27c60cb3d4fafdd
Martin Edenhofer <martin.edenhofer@otrs.com>
2009-09-26 10:22:22 +0000

    Fixed bug#4302 - Dashboard: Tickets Stats graph incorrect (permission related).

--- Kernel/Output/HTML/DashboardTicketStatsGeneric.pm
+++ Kernel/Output/HTML/DashboardTicketStatsGeneric.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/DashboardTicketStatsGeneric.pm
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: DashboardTicketStatsGeneric.pm,v 1.8 2009-07-13 23:23:53 martin Exp $
+# $Id: DashboardTicketStatsGeneric.pm,v 1.8.2.1 2009-09-26 10:22:22 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.8.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -117,7 +117,7 @@ sub Run {
 
             CustomerID => $Param{Data}->{UserCustomerID},
             Result     => 'COUNT',
-            Permission => 'ro',
+            Permission => $Self->{Config}->{Permission} || 'ro',
             UserID     => $Self->{UserID},
         );
         $Data[$Key]->{Closed} = $CountClosed;
