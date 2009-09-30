abf87856b0e16bc9976bffcce6f73b61bef87258
Michiel Beijen <michiel.beijen@otrs.com>
2009-09-30 09:10:15 +0000

    Fixed bug #4355: SLA dropdown does not populate in AgentTicketEmail.

--- Kernel/Modules/AgentTicketEmail.pm
+++ Kernel/Modules/AgentTicketEmail.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketEmail.pm - to compose initial email to customer
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketEmail.pm,v 1.99 2009-08-25 14:32:55 martin Exp $
+# $Id: AgentTicketEmail.pm,v 1.99.2.1 2009-09-30 09:10:15 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::State;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.99 $) [1];
+$VERSION = qw($Revision: 1.99.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -975,6 +975,7 @@ sub Run {
             %GetParam,
             CustomerUserID => $CustomerUser || '',
             QueueID        => $QueueID      || 1,
+            Services       => $Services,
         );
 
         # get free text config options
