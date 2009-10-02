006e037b3d679baac8304a10c1f358cce9bd6f25
Stefan Bedorf <stefan.bedorf@otrs.com>
2009-10-02 13:15:37 +0000

    Fixed bug#4379 - Array parameters not working in mkStats.pl.

--- bin/mkStats.pl
+++ bin/mkStats.pl
@@ -3,7 +3,7 @@
 # mkStats.pl - send stats output via email
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: mkStats.pl,v 1.63 2009-09-29 07:27:50 mb Exp $
+# $Id: mkStats.pl,v 1.64 2009-10-02 13:15:37 sb Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.63 $) [1];
+$VERSION = qw($Revision: 1.64 $) [1];
 
 use Getopt::Std;
 use Kernel::Config;
@@ -439,7 +439,7 @@ sub GetArray {
     my @P = split( /&/, $Opts{p} || '' );
     my @Array;
     for (@P) {
-        my ( $Key, $Value ) = split( /=/, $_, 1 );
+        my ( $Key, $Value ) = split( /=/, $_, 2 );
         if ( $Key eq $Param{Param} ) {
             push( @Array, $Value );
         }
