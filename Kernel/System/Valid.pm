62f97be3c067e3179d3e5195058a16a8d98b8a71
Markus Esche <markus.esche@otrs.com>
2009-10-12 17:58:46 +0000

    Improved POD.

--- Kernel/System/Valid.pm
+++ Kernel/System/Valid.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Valid.pm - all valid functions
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: Valid.pm,v 1.16 2009-10-05 08:27:00 martin Exp $
+# $Id: Valid.pm,v 1.17 2009-10-12 17:58:46 mae Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::CacheInternal;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 =head1 NAME
 
@@ -129,7 +129,7 @@ sub ValidList {
 returns the id or the name of a valid
 
     my $ValidID = $ValidObject->ValidLookup(
-        Valid => '3 normal',
+        Valid => 'valid',
     );
 
     my $Valid = $ValidObject->ValidLookup(
@@ -244,6 +244,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.16 $ $Date: 2009-10-05 08:27:00 $
+$Revision: 1.17 $ $Date: 2009-10-12 17:58:46 $
 
 =cut
