b4f1368f25cf9975875cb10da97116eb89eaef29
Michiel Beijen <michiel.beijen@otrs.com>
2009-10-12 11:27:46 +0000

    Fixed bug#4410 - TTL required in Kernel::System::Cache::Set but not checked.

--- Kernel/System/Cache.pm
+++ Kernel/System/Cache.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Cache.pm - all cache functions
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: Cache.pm,v 1.15 2009-04-17 08:36:44 tr Exp $
+# $Id: Cache.pm,v 1.16 2009-10-12 11:27:46 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.15 $) [1];
+$VERSION = qw($Revision: 1.16 $) [1];
 
 =head1 NAME
 
@@ -106,7 +106,7 @@ sub Set {
     my ( $Self, %Param ) = @_;
 
     # check needed stuff
-    for (qw(Type Key Value)) {
+    for (qw(Type Key Value TTL)) {
         if ( !defined $Param{$_} ) {
             $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
             return;
@@ -238,6 +238,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.15 $ $Date: 2009-04-17 08:36:44 $
+$Revision: 1.16 $ $Date: 2009-10-12 11:27:46 $
 
 =cut
