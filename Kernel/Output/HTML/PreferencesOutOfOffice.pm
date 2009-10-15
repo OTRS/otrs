e565951e0eb4428f44719ae8cdf67b76a7c2f742
Michiel Beijen <michiel.beijen@otrs.com>
2009-10-15 07:12:11 +0000

    Modified BuildDateSelection

--- Kernel/Output/HTML/PreferencesOutOfOffice.pm
+++ Kernel/Output/HTML/PreferencesOutOfOffice.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/PreferencesOutOfOffice.pm
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: PreferencesOutOfOffice.pm,v 1.3 2009-02-16 11:16:22 tr Exp $
+# $Id: PreferencesOutOfOffice.pm,v 1.4 2009-10-15 07:12:11 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -51,6 +51,8 @@ sub Param {
         OutOfOfficeStartDay    => $Self->{OutOfOfficeStartDay},
         OutOfOfficeStartHour   => 0,
         OutOfOfficeStartMinute => 0,
+        YearPeriodPast         => 1,
+        YearPeriodFuture       => 5,
     );
     $Param{OptionEnd} = $Self->{LayoutObject}->BuildDateSelection(
         Format               => 'DateInputFormat',
@@ -62,6 +64,8 @@ sub Param {
         OutOfOfficeEndDay    => $Self->{OutOfOfficeEndDay},
         OutOfOfficeEndHour   => 0,
         OutOfOfficeEndMinute => 0,
+        YearPeriodPast       => 1,
+        YearPeriodFuture     => 5,
     );
 
     push(
