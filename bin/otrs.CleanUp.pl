330c6eb65ab3b16f16562ee1b5c86d20e7400fca
Marc Nilius <marc.nilius@otrs.com>
2009-11-03 14:32:18 +0000

    Renamed CleanUp.pl to otrs.CleanUp.pl.

--- bin/otrs.CleanUp.pl
+++ bin/otrs.CleanUp.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
-# bin/CleanUp.pl - to cleanup, remove used tmp data of ipc, database or fs
+# bin/otrs.CleanUp.pl - to cleanup, remove used tmp data of ipc, database or fs
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: CleanUp.pl,v 1.12 2009-03-20 18:24:14 martin Exp $
+# $Id: otrs.CleanUp.pl,v 1.1 2009-11-03 14:32:18 mn Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 use Getopt::Std;
 use Kernel::Config;
