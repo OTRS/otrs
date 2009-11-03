1f5bb26c9388f3a5685a0327b62b876acf395772
Marc Nilius <marc.nilius@otrs.com>
2009-11-03 16:17:15 +0000

    Renamed XMLMaster.pl to otrs.XMLMaster.pl.

--- bin/otrs.XMLMaster.pl
+++ bin/otrs.XMLMaster.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
-# XMLMaster.pl - the global xml handle for xml2db
+# bin/otrs.XMLMaster.pl - the global xml handle for xml2db
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: XMLMaster.pl,v 1.14 2009-05-15 10:12:02 martin Exp $
+# $Id: otrs.XMLMaster.pl,v 1.1 2009-11-03 16:17:15 mn Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.14 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 use Getopt::Std;
 use Kernel::Config;
