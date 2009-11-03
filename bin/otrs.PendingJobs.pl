b93269b8d7185d76fb83d9c2d8de73e7b728342f
Marc Nilius <marc.nilius@otrs.com>
2009-11-03 16:09:41 +0000

    Renamed PendingJobs.pl to otrs.PendingJobs.pl.

--- bin/otrs.PendingJobs.pl
+++ bin/otrs.PendingJobs.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
-# PendingJobs.pl - check pending tickets
+# bin/otrs.PendingJobs.pl - check pending tickets
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: PendingJobs.pl,v 1.43 2009-07-20 10:36:05 mh Exp $
+# $Id: otrs.PendingJobs.pl,v 1.1 2009-11-03 16:09:41 mn Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . '/Kernel/cpan-lib';
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.43 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 use Date::Pcalc qw(Day_of_Week Day_of_Week_Abbreviation);
 use Kernel::Config;
