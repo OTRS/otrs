48bfc706532274305ea162c7dab8448173c81391
Marc Nilius <marc.nilius@otrs.com>
2009-11-03 14:29:08 +0000

    Renamed CheckDB.pl to otrs.CheckDB.pl.

--- bin/otrs.CheckDB.pl
+++ bin/otrs.CheckDB.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
-# bin/CheckDB.pl - to check the db access
+# bin/otrs.CheckDB.pl - to check the db access
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: CheckDB.pl,v 1.22 2009-04-03 14:15:00 martin Exp $
+# $Id: otrs.CheckDB.pl,v 1.1 2009-11-03 14:29:08 mn Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -33,7 +33,7 @@ use lib dirname($RealBin) . "/Kernel/cpan-lib";
 use Getopt::Std;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.22 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 use Kernel::Config;
 use Kernel::System::Encode;
