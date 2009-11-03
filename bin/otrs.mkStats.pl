76c261bc230a090a6d66b842951ebf4afaa7d0f4
Marc Nilius <marc.nilius@otrs.com>
2009-11-03 14:36:01 +0000

    Renamed mkStats.pl to otrs.mkStats.pl.

--- bin/otrs.mkStats.pl
+++ bin/otrs.mkStats.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
-# mkStats.pl - send stats output via email
+# bin/otrs.mkStats.pl - send stats output via email
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: mkStats.pl,v 1.64 2009-10-02 13:15:37 sb Exp $
+# $Id: otrs.mkStats.pl,v 1.1 2009-11-03 14:36:01 mn Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.64 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 use Getopt::Std;
 use Kernel::Config;
