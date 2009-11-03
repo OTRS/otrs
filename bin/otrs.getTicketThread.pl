7b2c605c5d53b250f273fd4edc029c3302c4ee78
Marc Nilius <marc.nilius@otrs.com>
2009-11-03 16:08:23 +0000

    Renamed otrs.getTicketThread to otrs.getTicketThread.pl.

--- bin/otrs.getTicketThread.pl
+++ bin/otrs.getTicketThread.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
-# bin/otrs.getTicketThread - to print the whole ticket thread to STDOUT
+# bin/otrs.getTicketThread.pl - to print the whole ticket thread to STDOUT
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: otrs.getTicketThread,v 1.14 2009-04-03 14:15:00 martin Exp $
+# $Id: otrs.getTicketThread.pl,v 1.1 2009-11-03 16:08:23 mn Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.14 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 use Kernel::Config;
 use Kernel::System::Encode;
