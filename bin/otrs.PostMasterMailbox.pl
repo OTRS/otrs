4d6977cdf22ac6da8fd423dd2d2d82b79ce58ee7
Marc Nilius <marc.nilius@otrs.com>
2009-11-03 16:11:50 +0000

    Renamed PostMasterMailbox.pl to otrs.PostMasterMailbox.pl.

--- bin/otrs.PostMasterMailbox.pl
+++ bin/otrs.PostMasterMailbox.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
-# bin/PostMasterMailbox.pl - the global eMail handle for email2db
+# bin/otrs.PostMasterMailbox.pl - the global eMail handle for email2db
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: PostMasterMailbox.pl,v 1.10 2009-08-18 12:52:55 mh Exp $
+# $Id: otrs.PostMasterMailbox.pl,v 1.1 2009-11-03 16:11:50 mn Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 use Getopt::Std;
 use Kernel::Config;
