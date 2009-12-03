1ab74df3b868e9c842941d99ff19492f2c5d23c5
Manuel Hecht <manuel.hecht@otrs.com>
2009-12-03 10:11:07 +0000

    Fixed iteration variable.

--- Kernel/Modules/AgentTicketEmail.pm
+++ Kernel/Modules/AgentTicketEmail.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketEmail.pm - to compose initial email to customer
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketEmail.pm,v 1.107 2009-11-25 15:15:28 mg Exp $
+# $Id: AgentTicketEmail.pm,v 1.108 2009-12-03 10:11:07 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::State;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.107 $) [1];
+$VERSION = qw($Revision: 1.108 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -388,12 +388,12 @@ sub Run {
 
             # check required FreeTextField (if configured)
             if (
-                $Self->{Config}->{TicketFreeText}->{$_} == 2
-                && $GetParam{"TicketFreeText$_"} eq ''
+                $Self->{Config}->{TicketFreeText}->{$Count} == 2
+                && $GetParam{"TicketFreeText$Count"} eq ''
                 && $ExpandCustomerName == 0
                 )
             {
-                $Error{"TicketFreeTextField$_ invalid"} = 'invalid';
+                $Error{"TicketFreeTextField$Count invalid"} = 'invalid';
             }
         }
         my %TicketFreeTextHTML = $Self->{LayoutObject}->AgentFreeText(
--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPhone.pm - to handle phone calls
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPhone.pm,v 1.119 2009-11-25 15:15:28 mg Exp $
+# $Id: AgentTicketPhone.pm,v 1.120 2009-12-03 10:11:07 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -23,7 +23,7 @@ use Kernel::System::LinkObject;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.119 $) [1];
+$VERSION = qw($Revision: 1.120 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -505,12 +505,12 @@ sub Run {
 
             # check required FreeTextField (if configured)
             if (
-                $Self->{Config}->{TicketFreeText}->{$_} == 2
-                && $GetParam{"TicketFreeText$_"} eq ''
+                $Self->{Config}->{TicketFreeText}->{$Count} == 2
+                && $GetParam{"TicketFreeText$Count"} eq ''
                 && $ExpandCustomerName == 0
                 )
             {
-                $Error{"TicketFreeTextField$_ invalid"} = 'invalid';
+                $Error{"TicketFreeTextField$Count invalid"} = 'invalid';
             }
         }
         my %TicketFreeTextHTML = $Self->{LayoutObject}->AgentFreeText(
