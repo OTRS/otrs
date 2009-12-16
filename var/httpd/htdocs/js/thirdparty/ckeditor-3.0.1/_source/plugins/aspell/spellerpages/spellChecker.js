4ea65931ebfa298a33016d6b761197083dd906d9
Marc Nilius <marc.nilius@otrs.com>
2009-12-16 11:12:18 +0000

    Optimized integration of CKEditor (generated individual ckeditor.js file, including only the plugins OTRS needs, added patch files).

--- var/httpd/htdocs/js/thirdparty/ckeditor-3.0.1/_source/plugins/aspell/spellerpages/spellChecker.js
+++ var/httpd/htdocs/js/thirdparty/ckeditor-3.0.1/_source/plugins/aspell/spellerpages/spellChecker.js
@@ -19,7 +19,7 @@ function spellChecker( textObject ) {
 //	this.popUpProps = "menu=no,width=440,height=350,top=70,left=120,resizable=yes,status=yes";	// by FredCK
 	this.popUpProps = null ;																	// by FredCK
 //	this.spellCheckScript = '/speller/server-scripts/spellchecker.php';		// by FredCK
-//  this.spellCheckScript = '/cgi-bin/spellchecker.pl';
+//	this.spellCheckScript = '/cgi-bin/spellchecker.pl';
 
 	// values used to keep track of what happened to a word
 	this.replWordFlag = "R";	// single replace
