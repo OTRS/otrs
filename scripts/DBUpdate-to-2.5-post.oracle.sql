81094fd27688db8702cf5f402f61d132846272b9
Michiel Beijen <michiel.beijen@otrs.com>
2009-12-24 10:47:28 +0000

    Added init database post update files for 2.5

---
+++ scripts/DBUpdate-to-2.5-post.oracle.sql
@@ -0,0 +1,6 @@
+-- ----------------------------------------------------------
+--  driver: oracle, generated: 2009-12-24 11:45:44
+-- ----------------------------------------------------------
+SET DEFINE OFF;
+DROP TABLE theme CASCADE CONSTRAINTS;
+SET DEFINE OFF;
