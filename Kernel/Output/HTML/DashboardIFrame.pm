542d4cc6057b6889b5bfed0bf4c14cb5772a6bfa
Jens Bothe <jens.bothe@otrs.com>
2010-01-06 18:45:51 +0000

    Added Dashboard IFrame Widget

---
+++ Kernel/Output/HTML/DashboardIFrame.pm
@@ -0,0 +1,65 @@
+# --
+# Kernel/Output/HTML/DashboardIFrame.pm
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id:
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::DashboardIFrame;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (
+        qw(Config Name ConfigObject LogObject DBObject LayoutObject ParamObject TicketObject UserID)
+        )
+    {
+        die "Got no $_!" if ( !$Self->{$_} );
+    }
+
+    return $Self;
+}
+
+sub Preferences {
+    my ( $Self, %Param ) = @_;
+
+    return;
+}
+
+sub Config {
+    my ( $Self, %Param ) = @_;
+
+    return (
+        %{ $Self->{Config} }
+    );
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $Content = $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentDashboardIFrame',
+        Data         => {
+            %{ $Self->{Config} },
+        },
+    );
+
+    return $Content;
+}
+
+1;
