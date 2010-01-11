9ad8bb8707a392613be963ea3907b98290e812b0
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-01-11 11:27:57 +0000

    Removed directories of HEAD.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: auto_build.sh,v 1.61.2.3 2010-01-07 20:16:52 martin Exp $
+# $Id: auto_build.sh,v 1.61.2.4 2010-01-11 11:27:57 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -21,7 +21,7 @@
 # or see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.61.2.3 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.61.2.4 $>"
 echo "Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
 
 PATH_TO_CVS_SRC=$1
@@ -170,6 +170,10 @@ rm -rf Kernel/Config.pm
 rm -rf install
 rm -rf Kernel/Display
 rm -rf var/sesstions
+rm -rf var/httpd/htdocs/js/thirdparty/ckeditor*
+rm -rf var/httpd/htdocs/js/thirdparty/fckeditor*
+rm -rf var/httpd/htdocs/js/thirdparty/flot
+rm -rf var/httpd/htdocs/js/thirdparty/jquery
 rm -rf Kernel/System/Ticket/Compress
 rm -rf Kernel/System/Ticket/Crypt
 
