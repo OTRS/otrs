d020fd894688aac9542df96bc5552f0fbdddd410
Michiel Beijen <michiel.beijen@otrs.com>
2010-01-14 14:20:59 +0000

    Removed debug statement

--- Kernel/Output/HTML/DashboardRSS.pm
+++ Kernel/Output/HTML/DashboardRSS.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/DashboardRSS.pm
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: DashboardRSS.pm,v 1.12.2.1 2010-01-14 14:09:45 mb Exp $
+# $Id: DashboardRSS.pm,v 1.12.2.2 2010-01-14 14:20:59 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use XML::FeedPP;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12.2.1 $) [1];
+$VERSION = qw($Revision: 1.12.2.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -61,7 +61,6 @@ sub Run {
     if ($Proxy) {
         $ENV{CGI_HTTP_PROXY} = $Proxy;
     }
-    print STDERR "UserCharset = $Self->{LayoutObject}->{UserCharset} \n";
 
     # get content
     my %Options;
