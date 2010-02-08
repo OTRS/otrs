0be630ff67e35dac5bf7d701325430c60fcff8e4
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-02-08 23:52:35 +0000

    Fixed bug# 4664 - Missing quoting for underscore.

--- Kernel/System/DB/mysql.pm
+++ Kernel/System/DB/mysql.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/DB/mysql.pm - mysql database backend
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: mysql.pm,v 1.50 2009-09-23 08:01:24 mg Exp $
+# $Id: mysql.pm,v 1.51 2010-02-08 23:52:35 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.50 $) [1];
+$VERSION = qw($Revision: 1.51 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -36,6 +36,7 @@ sub LoadPreferences {
     $Self->{'DB::QuoteSingle'}        = '\\';
     $Self->{'DB::QuoteBack'}          = '\\';
     $Self->{'DB::QuoteSemicolon'}     = '\\';
+    $Self->{'DB::QuoteUnderscore'}    = '\\';
     $Self->{'DB::NoLowerInLargeText'} = 0;
 
     # DBI/DBD::mysql attributes
@@ -72,6 +73,12 @@ sub LoadPreferences {
 
 sub Quote {
     my ( $Self, $Text, $Type ) = @_;
+
+    if ( $Type && $Type eq 'Like' ) {
+        if ( $Self->{'DB::QuoteUnderscore'} ) {
+            ${$Text} =~ s/_/$Self->{'DB::QuoteUnderscore'}_/g;
+        }
+    }
 
     if ( defined ${$Text} ) {
         if ( $Self->{'DB::QuoteBack'} ) {
