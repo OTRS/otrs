fddae220af01e0cfe27ff8d650b0892407044a61
Renée Bäcker <renee.baecker@otrs.com>
2010-03-15 08:12:54 +0000

    Fixed bug#3830: Framework part of the fix. Set current date for time related stat fields (if it is not 'fixed').

--- Kernel/System/Stats.pm
+++ Kernel/System/Stats.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/Stats.pm - all stats core functions
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Stats.pm,v 1.80.2.1 2009-09-28 13:12:39 ub Exp $
+# $Id: Stats.pm,v 1.80.2.2 2010-03-15 08:12:54 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Date::Pcalc qw(:all);
 use Kernel::System::XML;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.80.2.1 $) [1];
+$VERSION = qw($Revision: 1.80.2.2 $) [1];
 
 =head1 SYNOPSIS
 
@@ -335,7 +335,7 @@ sub StatsGet {
                         )
                         )
                     {
-                        if ( $Ref->{$_} ) {
+                        if ( $Ref->{$_} && ( !$Attribute->{$_} || $Ref->{Fixed} ) ) {
                             $Attribute->{$_} = $Ref->{$_};
                         }
                     }
@@ -3223,6 +3223,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.80.2.1 $ $Date: 2009-09-28 13:12:39 $
+$Revision: 1.80.2.2 $ $Date: 2010-03-15 08:12:54 $
 
 =cut
