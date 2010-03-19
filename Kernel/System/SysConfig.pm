9d76ab9f13f19b6a99587f9c97cf1d512d3718d8
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2010-03-19 15:57:23 +0000

    Added a check for the EncodeObject, as the XMLObject requires it.

--- Kernel/System/SysConfig.pm
+++ Kernel/System/SysConfig.pm
@@ -2,7 +2,7 @@
 # Kernel/System/SysConfig.pm - all system config tool functions
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: SysConfig.pm,v 1.2 2010-02-08 19:25:20 martin Exp $
+# $Id: SysConfig.pm,v 1.3 2010-03-19 15:57:23 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::XML;
 use Kernel::Config;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 =head1 NAME
 
@@ -89,7 +89,7 @@ sub new {
     bless( $Self, $Type );
 
     # check needed objects
-    for (qw(DBObject ConfigObject LogObject TimeObject MainObject)) {
+    for (qw(DBObject ConfigObject LogObject TimeObject MainObject EncodeObject)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
 
@@ -1728,6 +1728,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.2 $ $Date: 2010-02-08 19:25:20 $
+$Revision: 1.3 $ $Date: 2010-03-19 15:57:23 $
 
 =cut
