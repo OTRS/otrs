dbe727c5e5d80458fa17b8cef4686b12bc4a79f1
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-03-24 11:28:45 +0000

    Small code layout improvement.

--- Kernel/Modules/AgentTicketWatcher.pm
+++ Kernel/Modules/AgentTicketWatcher.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AgentTicketWatcher.pm - a ticketwatcher module
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketWatcher.pm,v 1.11 2009-02-16 11:20:53 tr Exp $
+# $Id: AgentTicketWatcher.pm,v 1.12 2010-03-24 11:28:45 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.11 $) [1];
+$VERSION = qw($Revision: 1.12 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -47,20 +47,21 @@ sub Run {
     # ------------------------------------------------------------ #
     # check access
     # ------------------------------------------------------------ #
-    my @Groups = ();
+    my @Groups;
     if ( $Self->{ConfigObject}->Get('Ticket::WatcherGroup') ) {
         @Groups = @{ $Self->{ConfigObject}->Get('Ticket::WatcherGroup') };
     }
-    my $Access = 0;
-    if ( !@Groups ) {
-        $Access = 1;
-    }
-    else {
+    my $Access = 1;
+    if (@Groups) {
+        $Access = 0;
         for my $Group (@Groups) {
             if ( $Self->{LayoutObject}->{"UserIsGroup[$Group]"} eq 'Yes' ) {
                 $Access = 1;
             }
         }
+    }
+    if ( !$Access ) {
+        return $Self->{Layout}->NoPermission();
     }
 
     # ------------------------------------------------------------ #
