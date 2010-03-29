fa8112204bd4a42d0872b4f1e45ae9100d9b670c
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-03-29 12:14:00 +0000

    Small code layout improvement.

--- Kernel/Output/HTML/NavBarCustomerCompany.pm
+++ Kernel/Output/HTML/NavBarCustomerCompany.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Output/HTML/NavBarCustomerCompany.pm
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: NavBarCustomerCompany.pm,v 1.4 2009-08-29 12:20:43 martin Exp $
+# $Id: NavBarCustomerCompany.pm,v 1.5 2010-03-29 12:14:00 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.4 $) [1];
+$VERSION = qw($Revision: 1.5 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -47,7 +47,7 @@ sub Run {
     }
 
     # reset nav bar item, remove customer company feature
-    my %Return = ();
+    my %Return;
     $Return{'ItemArea0009100'} = {};
     $Return{'ItemPre0009100'}  = {};
     return %Return;
