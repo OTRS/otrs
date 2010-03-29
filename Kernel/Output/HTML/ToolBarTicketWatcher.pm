6c97de681604dd301c9fe08b485ac09070897cf4
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-03-29 12:13:36 +0000

    Fixed block name.

--- Kernel/Output/HTML/ToolBarTicketWatcher.pm
+++ Kernel/Output/HTML/ToolBarTicketWatcher.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/ToolBarTicketWatcher.pm
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: ToolBarTicketWatcher.pm,v 1.1 2010-03-28 10:57:54 martin Exp $
+# $Id: ToolBarTicketWatcher.pm,v 1.2 2010-03-29 12:13:36 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -88,7 +88,7 @@ sub Run {
     }
     my %Return;
     $Return{'0999978'} = {
-        Block       => 'ItemPersonal',
+        Block       => 'ToolBarItem',
         Description => $Text,
         Count       => $CountNotify,
 
