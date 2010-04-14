ba1e26f0d339ea1094dca924398953ee7ae193d3
Martha Pascual <martha.pascual@otrs.com>
2010-04-14 21:38:32 +0000

    Change for overview block

--- Kernel/Modules/AdminMailAccount.pm
+++ Kernel/Modules/AdminMailAccount.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AdminMailAccount.pm - to add/update/delete MailAccount acounts
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminMailAccount.pm,v 1.13 2009-12-15 21:30:16 mb Exp $
+# $Id: AdminMailAccount.pm,v 1.14 2010-04-14 21:38:32 mp Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::MailAccount;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.13 $) [1];
+$VERSION = qw($Revision: 1.14 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -162,6 +162,23 @@ sub Run {
             if ( !$Backend{ $Data{Type} } ) {
                 $Data{Type} .= '(not installed!)';
             }
+
+            my @List = $Self->{ValidObject}->ValidIDsGet();
+
+            for (@List) {
+                if ( $Data{ValidID} eq $_ ) {
+                    $Data{Invalid} = '';
+                    last;
+                }
+                else {
+                    $Data{Invalid} = 'Invalid';
+                }
+            }
+
+            $Data{ShownValid} = $Self->{ValidObject}->ValidLookup(
+                ValidID => $Data{ValidID},
+            );
+
             $Self->{LayoutObject}->Block(
                 Name => 'OverviewResultRow',
                 Data => \%Data,
