7175f506625d925033fd1dd97bc1b8d8f4727697
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-04-15 21:26:55 +0000

    Added action row support.

--- Kernel/Output/HTML/TicketOverviewSmall.pm
+++ Kernel/Output/HTML/TicketOverviewSmall.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/TicketOverviewSmall.pm
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: TicketOverviewSmall.pm,v 1.23 2010-04-12 21:28:46 martin Exp $
+# $Id: TicketOverviewSmall.pm,v 1.24 2010-04-15 21:26:55 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.23 $) [1];
+$VERSION = qw($Revision: 1.24 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -232,6 +232,100 @@ sub Run {
         }
     }
 
+    my $Counter = 0;
+    my @ArticleBox;
+    for my $TicketID ( @{ $Param{TicketIDs} } ) {
+        $Counter++;
+        if ( $Counter >= $Param{StartHit} && $Counter < ( $Param{PageShown} + $Param{StartHit} ) ) {
+
+            # get last customer article
+            my %Article = $Self->{TicketObject}->ArticleLastCustomerArticle(
+                TicketID => $TicketID,
+            );
+
+            # prepare subject
+            $Article{Subject} = $Self->{TicketObject}->TicketSubjectClean(
+                TicketNumber => $Article{TicketNumber},
+                Subject => $Article{Subject} || '',
+            );
+
+            # create human age
+            $Article{Age} = $Self->{LayoutObject}->CustomerAge(
+                Age   => $Article{Age},
+                Space => ' ',
+            );
+
+            # get acl actions
+            $Self->{TicketObject}->TicketAcl(
+                Data          => '-',
+                Action        => $Self->{Action},
+                TicketID      => $Article{TicketID},
+                ReturnType    => 'Action',
+                ReturnSubType => '-',
+                UserID        => $Self->{UserID},
+            );
+            my %AclAction = $Self->{TicketObject}->TicketAclActionData();
+
+            # run ticket pre menu modules
+            my @ActionItems;
+            if ( ref $Self->{ConfigObject}->Get('Ticket::Frontend::PreMenuModule') eq 'HASH' ) {
+                my %Menus = %{ $Self->{ConfigObject}->Get('Ticket::Frontend::PreMenuModule') };
+                my @Items;
+                for my $Menu ( sort keys %Menus ) {
+
+                    # load module
+                    if ( !$Self->{MainObject}->Require( $Menus{$Menu}->{Module} ) ) {
+                        return $Self->{LayoutObject}->FatalError();
+                    }
+                    my $Object
+                        = $Menus{$Menu}->{Module}->new( %{$Self}, TicketID => $Article{TicketID}, );
+
+                    # run module
+                    my $Item = $Object->Run(
+                        %Param,
+                        Ticket => \%Article,
+                        ACL    => \%AclAction,
+                        Config => $Menus{$Menu},
+                    );
+                    next if !$Item;
+                    next if ref $Item ne 'HASH';
+                    for my $Key (qw(Name Link Description)) {
+                        next if !$Item->{$Key};
+                        $Item->{$Key} = $Self->{LayoutObject}->Output(
+                            Template => $Item->{$Key},
+                            Data     => \%Article,
+                        );
+                    }
+
+                    # create id
+                    $Item->{ID} = $Item->{Name};
+                    $Item->{ID} =~ s/(\s|&|;)//ig;
+
+                    $Self->{LayoutObject}->Block(
+                        Name => 'DocumentMenuItem',
+                        Data => $Item,
+                    );
+                    my $Output = $Self->{LayoutObject}->Output(
+                        TemplateFile => 'AgentTicketOverviewSmall',
+                        Data         => $Item,
+                    );
+                    $Output =~ s/\n+//g;
+                    $Output =~ s/\s+/ /g;
+                    $Output =~ s/<\!--.+?-->//g;
+
+                    push @ActionItems, {
+                        HTML   => $Output,
+                        ID     => $Item->{ID},
+                        Link   => $Self->{LayoutObject}->{Baselink} . $Item->{Link},
+                        Target => $Item->{Target},
+                    };
+                    $Article{ActionItems} = \@ActionItems;
+                }
+            }
+            push @ArticleBox, \%Article;
+        }
+    }
+
     $Self->{LayoutObject}->Block(
         Name => 'DocumentContent',
         Data => \%Param,
@@ -259,175 +353,113 @@ sub Run {
         );
     }
 
-    my $Output  = '';
-    my $Counter = 0;
-    for my $TicketID ( @{ $Param{TicketIDs} } ) {
-        $Counter++;
-        if ( $Counter >= $Param{StartHit} && $Counter < ( $Param{PageShown} + $Param{StartHit} ) ) {
+    for my $ArticleRef (@ArticleBox) {
 
-            # get last customer article
-            my %Article = $Self->{TicketObject}->ArticleLastCustomerArticle(
-                TicketID => $TicketID,
-            );
+        # get last customer article
+        my %Article = %{$ArticleRef};
 
-            # prepare subject
-            $Article{Subject} = $Self->{TicketObject}->TicketSubjectClean(
-                TicketNumber => $Article{TicketNumber},
-                Subject => $Article{Subject} || '',
-            );
-
-            # create human age
-            $Article{Age} = $Self->{LayoutObject}->CustomerAge(
-                Age   => $Article{Age},
+        # escalation human times
+        if ( $Article{EscalationTime} ) {
+            $Article{EscalationTimeHuman} = $Self->{LayoutObject}->CustomerAgeInHours(
+                Age   => $Article{EscalationTime},
                 Space => ' ',
             );
+            $Article{EscalationTimeWorkingTime} = $Self->{LayoutObject}->CustomerAgeInHours(
+                Age   => $Article{EscalationTimeWorkingTime},
+                Space => ' ',
+            );
+        }
 
-            # escalation human times
-            if ( $Article{EscalationTime} ) {
-                $Article{EscalationTimeHuman} = $Self->{LayoutObject}->CustomerAgeInHours(
-                    Age   => $Article{EscalationTime},
-                    Space => ' ',
-                );
-                $Article{EscalationTimeWorkingTime} = $Self->{LayoutObject}->CustomerAgeInHours(
-                    Age   => $Article{EscalationTimeWorkingTime},
-                    Space => ' ',
+        # customer info (customer name)
+        if ( $Param{Config}->{CustomerInfo} ) {
+            if ( $Article{CustomerUserID} ) {
+                $Article{CustomerName} = $Self->{CustomerUserObject}->CustomerName(
+                    UserLogin => $Article{CustomerUserID},
                 );
             }
+        }
 
-            # customer info (customer name)
-            if ( $Param{Config}->{CustomerInfo} ) {
-                if ( $Article{CustomerUserID} ) {
-                    $Article{CustomerName} = $Self->{CustomerUserObject}->CustomerName(
-                        UserLogin => $Article{CustomerUserID},
-                    );
-                }
+        # user info
+        my %UserInfo = $Self->{UserObject}->GetUserData(
+            UserID => $Article{OwnerID},
+        );
+
+        $Self->{LayoutObject}->Block(
+            Name => 'Record',
+            Data => { %Article, %UserInfo },
+        );
+
+        # check if bulk feature is enabled
+        if ($BulkFeature) {
+            $Self->{LayoutObject}->Block(
+                Name => 'Bulk',
+                Data => { %Article, %UserInfo },
+            );
+        }
+
+        # show ticket flags
+        my @TicketMetaItems = $Self->{LayoutObject}->TicketMetaItems(
+            Ticket => \%Article,
+        );
+        for my $Item (@TicketMetaItems) {
+            $Self->{LayoutObject}->Block(
+                Name => 'ContentLargeTicketGenericRowMeta',
+                Data => $Item,
+            );
+            if ($Item) {
+                $Self->{LayoutObject}->Block(
+                    Name => 'ContentLargeTicketGenericRowMetaImage',
+                    Data => $Item,
+                );
             }
+        }
 
-            # user info
-            my %UserInfo = $Self->{UserObject}->GetUserData(
-                UserID => $Article{OwnerID},
+        # show escalation
+        if ( $Param{Escalation} ) {
+            $Self->{LayoutObject}->Block(
+                Name => 'RecordEscalationTime',
+                Data => { %Article, %UserInfo },
+            );
+            if ( $Article{EscalationTime} < 60 * 60 * 1 ) {
+                $Self->{LayoutObject}->Block(
+                    Name => 'RecordEscalationTimeFontStart',
+                    Data => { %Article, %UserInfo },
+                );
+                $Self->{LayoutObject}->Block(
+                    Name => 'RecordEscalationTimeFontStop',
+                    Data => { %Article, %UserInfo },
+                );
+            }
+        }
+
+        # check if last customer subject or ticket title should be shown
+        if ( $Self->{SmallViewColumnHeader} eq 'LastCustomerSubject' ) {
+            $Self->{LayoutObject}->Block(
+                Name => 'RecordLastCustomerSubject',
+                Data => { %Article, %UserInfo },
+            );
+        }
+        elsif ( $Self->{SmallViewColumnHeader} eq 'TicketTitle' ) {
+            $Self->{LayoutObject}->Block(
+                Name => 'RecordTicketTitle',
+                Data => { %Article, %UserInfo },
+            );
+        }
+
+        # add action items as js
+        if ( $Article{ActionItems} ) {
+
+            my $JSON = $Self->{LayoutObject}->JSONEncode(
+                Data => $Article{ActionItems},
             );
 
             $Self->{LayoutObject}->Block(
-                Name => 'Record',
-                Data => { %Article, %UserInfo },
+                Name => 'DocumentReadyActionRowAdd',
+                Data => {
+                    TicketID => $Article{TicketID},
+                    Data     => $JSON,
+                },
             );
-
-            # check if bulk feature is enabled
-            if ($BulkFeature) {
-                $Self->{LayoutObject}->Block(
-                    Name => 'Bulk',
-                    Data => { %Article, %UserInfo },
-                );
-            }
-
-            # show ticket flags
-            my @TicketMetaItems = $Self->{LayoutObject}->TicketMetaItems(
-                Ticket => \%Article,
-            );
-            for my $Item (@TicketMetaItems) {
-                $Self->{LayoutObject}->Block(
-                    Name => 'ContentLargeTicketGenericRowMeta',
-                    Data => $Item,
-                );
-                if ($Item) {
-                    $Self->{LayoutObject}->Block(
-                        Name => 'ContentLargeTicketGenericRowMetaImage',
-                        Data => $Item,
-                    );
-                }
-            }
-
-            # show escalation
-            if ( $Param{Escalation} ) {
-                $Self->{LayoutObject}->Block(
-                    Name => 'RecordEscalationTime',
-                    Data => { %Article, %UserInfo },
-                );
-                if ( $Article{EscalationTime} < 60 * 60 * 1 ) {
-                    $Self->{LayoutObject}->Block(
-                        Name => 'RecordEscalationTimeFontStart',
-                        Data => { %Article, %UserInfo },
-                    );
-                    $Self->{LayoutObject}->Block(
-                        Name => 'RecordEscalationTimeFontStop',
-                        Data => { %Article, %UserInfo },
-                    );
-                }
-            }
-
-            # check if last customer subject or ticket title should be shown
-            if ( $Self->{SmallViewColumnHeader} eq 'LastCustomerSubject' ) {
-                $Self->{LayoutObject}->Block(
-                    Name => 'RecordLastCustomerSubject',
-                    Data => { %Article, %UserInfo },
-                );
-            }
-            elsif ( $Self->{SmallViewColumnHeader} eq 'TicketTitle' ) {
-                $Self->{LayoutObject}->Block(
-                    Name => 'RecordTicketTitle',
-                    Data => { %Article, %UserInfo },
-                );
-            }
-
-            # get acl actions
-            $Self->{TicketObject}->TicketAcl(
-                Data          => '-',
-                Action        => $Self->{Action},
-                TicketID      => $Article{TicketID},
-                ReturnType    => 'Action',
-                ReturnSubType => '-',
-                UserID        => $Self->{UserID},
-            );
-            my %AclAction = $Self->{TicketObject}->TicketAclActionData();
-
-            # run ticket pre menu modules
-            if ( ref $Self->{ConfigObject}->Get('Ticket::Frontend::PreMenuModule') eq 'HASH' ) {
-                my %Menus = %{ $Self->{ConfigObject}->Get('Ticket::Frontend::PreMenuModule') };
-                my @Items;
-                for my $Menu ( sort keys %Menus ) {
-
-                    # load module
-                    if ( !$Self->{MainObject}->Require( $Menus{$Menu}->{Module} ) ) {
-                        return $Self->{LayoutObject}->FatalError();
-                    }
-                    my $Object = $Menus{$Menu}->{Module}->new( %{$Self}, TicketID => $TicketID, );
-
-                    # run module
-                    my $Item = $Object->Run(
-                        %Param,
-                        Ticket => \%Article,
-                        ACL    => \%AclAction,
-                        Config => $Menus{$Menu},
-                    );
-                    next if !$Item;
-                    next if ref $Item ne 'HASH';
-                    for my $Key (qw(Name Link Description)) {
-                        next if !$Item->{$Key};
-                        $Item->{$Key} = $Self->{LayoutObject}->Output(
-                            Template => $Item->{$Key},
-                            Data     => \%Article,
-                        );
-                    }
-                    push @Items, {
-                        Name        => $Item->{Name},
-                        Link        => $Self->{LayoutObject}->{Baselink} . $Item->{Link},
-                        Description => $Item->{Description},
-                    };
-                }
-
-                my $JSON = $Self->{LayoutObject}->JSONEncode(
-                    Data => \@Items,
-                );
-
-                $Self->{LayoutObject}->Block(
-                    Name => 'DocumentReadyActionRowAdd',
-                    Data => {
-                        TicketID => $TicketID,
-                        Data     => $JSON,
-                    },
-                );
-            }
         }
     }
 
@@ -438,7 +470,7 @@ sub Run {
     );
 
     # use template
-    $Output .= $Self->{LayoutObject}->Output(
+    my $Output = $Self->{LayoutObject}->Output(
         TemplateFile => 'AgentTicketOverviewSmall',
         Data => { %Param, Type => $Self->{ViewType}, },
     );
