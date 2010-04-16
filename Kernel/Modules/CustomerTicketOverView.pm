d51bc68234c8e5f402167bc8ae5b7790a6fffe8e
Felix Niklas <felix.niklas@otrs.com>
2010-04-16 18:06:47 +0000

    Had to change one file name because the dtl's name changed.

--- Kernel/Modules/CustomerTicketOverView.pm
+++ Kernel/Modules/CustomerTicketOverView.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/CustomerTicketOverView.pm - status for all open tickets
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: CustomerTicketOverView.pm,v 1.51 2009-11-25 15:39:15 mg Exp $
+# $Id: CustomerTicketOverView.pm,v 1.52 2010-04-16 18:06:47 fn Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::State;
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.51 $) [1];
+$VERSION = qw($Revision: 1.52 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -164,7 +164,7 @@ sub Run {
     # build NavigationBar
     $Output .= $Self->{LayoutObject}->CustomerNavigationBar();
     $Output .= $Self->{LayoutObject}->Output(
-        TemplateFile => 'CustomerStatusView',
+        TemplateFile => 'CustomerTicketOverView',
         Data         => {
             Type       => $Self->{Type},
             ShowClosed => $ShowClosed,
