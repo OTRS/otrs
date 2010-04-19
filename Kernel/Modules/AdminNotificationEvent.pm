ab47a3430e80aa6bbcd08c19f6f1da051486892d
Martin Gruner <martin.gruner@otrs.com>
2010-04-19 18:36:09 +0000

    Corrected new class names for validation.

--- Kernel/Modules/AdminNotificationEvent.pm
+++ Kernel/Modules/AdminNotificationEvent.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminNotificationEvent.pm - to manage event-based notifications
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminNotificationEvent.pm,v 1.14 2010-04-19 17:56:02 cg Exp $
+# $Id: AdminNotificationEvent.pm,v 1.15 2010-04-19 18:36:09 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::Type;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.14 $) [1];
+$VERSION = qw($Revision: 1.15 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -355,7 +355,7 @@ sub _Edit {
         Name       => 'Events',
         Multiple   => 1,
         Size       => 5,
-        Class      => 'OTRS_Validate_Required' . " " . $Param{"EventsServerError"},
+        Class      => "Validate_Required " . $Param{"EventsServerError"},
         SelectedID => $Param{Data}->{Events},
     );
 
