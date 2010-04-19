5497e6811088caf26f07c1d8fe8d6e3e0a7a697f
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-04-19 18:58:23 +0000

    Fixed default theme selection.

--- Kernel/Output/HTML/PreferencesTheme.pm
+++ Kernel/Output/HTML/PreferencesTheme.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Output/HTML/PreferencesTheme.pm
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: PreferencesTheme.pm,v 1.10 2009-12-24 10:37:50 mb Exp $
+# $Id: PreferencesTheme.pm,v 1.11 2010-04-19 18:58:23 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -56,19 +56,17 @@ sub Param {
         }
     }
 
-    my @Params = ();
+    my @Params;
     push(
         @Params,
         {
             %Param,
-            Name      => $Self->{ConfigItem}->{PrefKey},
-            Data      => \%ActiveThemes,
-            HTMLQuote => 0,
-            SelectedID =>
-                $Self->{ParamObject}->GetParam( Param => 'UserLanguage' )
-                || $Param{UserData}->{UserLanguage}
-                || $Self->{LayoutObject}->{UserLanguage}
-                || $Self->{ConfigObject}->Get('DefaultLanguage'),
+            Name       => $Self->{ConfigItem}->{PrefKey},
+            Data       => \%ActiveThemes,
+            HTMLQuote  => 0,
+            SelectedID => $Self->{ParamObject}->GetParam( Param => 'UserTheme' )
+                || $Param{UserData}->{UserTheme}
+                || $Self->{ConfigObject}->Get('DefaultTheme'),
             Block => 'Option',
             Max   => 100,
         },
