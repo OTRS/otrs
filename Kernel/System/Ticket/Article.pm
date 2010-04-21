abb7182ccd99bf037efb17a1bc21085aa35a2a4f
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-04-21 16:49:23 +0000

    Fixed bug#5266 - Ticket Zoom shows wrong html content if there is no text but two html attachments in there.

--- Kernel/System/Ticket/Article.pm
+++ Kernel/System/Ticket/Article.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/Ticket/Article.pm - global article module for OTRS kernel
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Article.pm,v 1.232.2.1 2009-12-23 22:48:22 martin Exp $
+# $Id: Article.pm,v 1.232.2.2 2010-04-21 16:49:22 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::TemplateGenerator;
 use Kernel::System::Notification;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.232.2.1 $) [1];
+$VERSION = qw($Revision: 1.232.2.2 $) [1];
 
 =head1 NAME
 
@@ -1360,6 +1360,8 @@ sub ArticleContentIndex {
             my $AttachmentIDHTML  = 0;
             for my $AttachmentID ( keys %AtmIndex ) {
                 my %File = %{ $AtmIndex{$AttachmentID} };
+
+                # find plain attachment
                 if (
                     $File{Filename} eq 'file-1'
                     && $File{ContentType} =~ /text\/plain/i
@@ -1367,8 +1369,12 @@ sub ArticleContentIndex {
                 {
                     $AttachmentIDPlain = $AttachmentID;
                 }
+
+                # find html attachment
+                #  o file-[12], is plain+html attachment
+                #  o file-1.html, is only html attachment
                 if (
-                    $File{Filename} =~ /^file-[12]$/
+                    ( $File{Filename} =~ /^file-[12]$/ || $File{Filename} eq 'file-1.html' )
                     && $File{ContentType} =~ /text\/html/i
                     )
                 {
@@ -3064,6 +3070,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.232.2.1 $ $Date: 2009-12-23 22:48:22 $
+$Revision: 1.232.2.2 $ $Date: 2010-04-21 16:49:22 $
 
 =cut
