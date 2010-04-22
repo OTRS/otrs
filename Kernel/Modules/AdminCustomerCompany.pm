a5c34c88c5fa257353249ad339d63ffa6fb2f3fb
Carlos Garcia <carlos.garcia@otrs.com>
2010-04-22 14:58:56 +0000

    ported code (navigation)

--- Kernel/Modules/AdminCustomerCompany.pm
+++ Kernel/Modules/AdminCustomerCompany.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminCustomerCompany.pm - to add/update/delete system addresses
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminCustomerCompany.pm,v 1.12 2010-04-16 16:31:36 mn Exp $
+# $Id: AdminCustomerCompany.pm,v 1.13 2010-04-22 14:58:56 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::CustomerCompany;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12 $) [1];
+$VERSION = qw($Revision: 1.13 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -213,6 +213,10 @@ sub _Edit {
             Search => $Search,
         },
     );
+
+    $Self->{LayoutObject}->Block( Name => 'ActionList' );
+    $Self->{LayoutObject}->Block( Name => 'ActionOverview' );
+
     $Self->{LayoutObject}->Block(
         Name => 'OverviewUpdate',
         Data => {
@@ -344,6 +348,11 @@ sub _Overview {
             Search => $Search,
         },
     );
+
+    $Self->{LayoutObject}->Block( Name => 'ActionList' );
+    $Self->{LayoutObject}->Block( Name => 'ActionSearch' );
+    $Self->{LayoutObject}->Block( Name => 'ActionAdd' );
+
     $Self->{LayoutObject}->Block(
         Name => 'OverviewResult',
         Data => {
