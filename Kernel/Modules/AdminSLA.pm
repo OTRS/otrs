5f1446bf147f9635e6dbea56afb093779cc964d7
Daniel Zamorano <daniel.zamorano@otrs.com>
2010-04-29 18:58:02 +0000

    New navigation for admin interface.

--- Kernel/Modules/AdminSLA.pm
+++ Kernel/Modules/AdminSLA.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminSLA.pm - admin frontend to manage slas
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminSLA.pm,v 1.28 2010-04-27 18:53:57 en Exp $
+# $Id: AdminSLA.pm,v 1.29 2010-04-29 18:58:02 dz Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::SLA;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.28 $) [1];
+$VERSION = qw($Revision: 1.29 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -149,6 +149,10 @@ sub Run {
                 %Param,
             },
         );
+
+        $Self->{LayoutObject}->Block( Name => 'ActionList' );
+        $Self->{LayoutObject}->Block( Name => 'ActionOverview' );
+
         $Self->{LayoutObject}->Block(
             Name => 'SLAEdit',
             Data => {
@@ -326,6 +330,9 @@ sub Run {
                 %Param,
             },
         );
+
+        $Self->{LayoutObject}->Block( Name => 'ActionList' );
+        $Self->{LayoutObject}->Block( Name => 'ActionAdd' );
 
         # output overview result
         $Self->{LayoutObject}->Block(
