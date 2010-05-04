7630fc11d2f08a39b7619f75cb5ce70efe56aca1
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-05-04 01:09:29 +0000

    Fixed small typo.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Layout.pm,v 1.237 2010-04-30 11:11:09 mn Exp $
+# $Id: Layout.pm,v 1.238 2010-05-04 01:09:29 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Kernel::System::JSON;
 use Mail::Address;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.237 $) [1];
+$VERSION = qw($Revision: 1.238 $) [1];
 
 =head1 NAME
 
@@ -30,7 +30,7 @@ Kernel::Output::HTML::Layout - all generic html functions
 
 =head1 SYNOPSIS
 
-All generic html finctions. E. g. to get options fields, template processing, ...
+All generic html functions. E. g. to get options fields, template processing, ...
 
 =head1 PUBLIC INTERFACE
 
@@ -4633,6 +4633,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.237 $ $Date: 2010-04-30 11:11:09 $
+$Revision: 1.238 $ $Date: 2010-05-04 01:09:29 $
 
 =cut
