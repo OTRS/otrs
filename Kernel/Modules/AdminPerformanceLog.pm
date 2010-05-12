147ecaaff7df542652c1419db77b41f271913ce3
Martha Pascual <martha.pascual@otrs.com>
2010-05-12 17:18:33 +0000

    Change for display gradient if feature is disable

--- Kernel/Modules/AdminPerformanceLog.pm
+++ Kernel/Modules/AdminPerformanceLog.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminPerformanceLog.pm - provides a log view for admins
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminPerformanceLog.pm,v 1.18 2010-05-05 10:25:50 mg Exp $
+# $Id: AdminPerformanceLog.pm,v 1.19 2010-05-12 17:18:33 mp Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.18 $) [1];
+$VERSION = qw($Revision: 1.19 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -50,6 +50,9 @@ sub Run {
         $Self->{LayoutObject}->Block(
             Name => 'Disabled',
             Data => { %Param, },
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'OverviewDisabled',
         );
     }
 
