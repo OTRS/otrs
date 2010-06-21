49f496c153045642b5cbe36b39932ebc3a803453
Marc Nilius <marc.nilius@otrs.com>
2010-06-21 09:30:04 +0000

    Added support for the config option 'TimeInputFormat', which defines, if the date is entered in three select boxes or three input fields.

--- var/httpd/htdocs/js_new/Core.UI.Datepicker.js
+++ var/httpd/htdocs/js_new/Core.UI.Datepicker.js
@@ -2,7 +2,7 @@
 // Core.UI.Datepicker.js - Datepicker
 // Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
 // --
-// $Id: Core.UI.Datepicker.js,v 1.5 2010-06-18 09:04:33 mn Exp $
+// $Id: Core.UI.Datepicker.js,v 1.6 2010-06-21 09:30:04 mn Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
@@ -71,6 +71,13 @@ Core.UI.Datepicker = (function (TargetNS) {
      * @return nothing
      */
     TargetNS.Init = function (Element, $Image) {
+        function LeadingZero(Number) {
+            if (Number.toString().length === 1) {
+                return '0' + Number;
+            }
+            else return Number;
+        }
+
         var $DatepickerElement,
             HasDateSelectBoxes = false,
             I;
@@ -82,20 +89,19 @@ Core.UI.Datepicker = (function (TargetNS) {
         // Increment number of initialized datepickers on this site
         DatepickerCount++;
 
-        // Check, if datepicker is used with a single input element or with three select boxes
-        if (isJQueryObject(Element) && Element.is(':text')) {
-            $DatepickerElement = Element;
-        }
-        else if (typeof Element === 'object' &&
-                 typeof Element['Day'] !== 'undefined' &&
-                 typeof Element['Month'] !== 'undefined' &&
-                 typeof Element['Year'] !== 'undefined' &&
-                 isJQueryObject(Element['Day'], Element['Month'], Element['Year'])) {
-            HasDateSelectBoxes = true;
+        // Check, if datepicker is used with three input element or with three select boxes
+        if (typeof Element === 'object' &&
+            typeof Element['Day'] !== 'undefined' &&
+            typeof Element['Month'] !== 'undefined' &&
+            typeof Element['Year'] !== 'undefined' &&
+            isJQueryObject(Element['Day'], Element['Month'], Element['Year'])) {
 
-            // Create a new hidden input field for the datepicker
             $DatepickerElement = $('<input>').attr('type', 'hidden').attr('id', 'Datepicker' + DatepickerCount);
             Element['Year'].after($DatepickerElement);
+
+            if (Element['Day'].is('select') && Element['Month'].is('select') && Element['Year'].is('select')) {
+                HasDateSelectBoxes = true;
+            }
         }
         else {
             return false;
@@ -119,25 +125,29 @@ Core.UI.Datepicker = (function (TargetNS) {
             isRTL: LocalizationData.IsRTL
         };
 
-        if (HasDateSelectBoxes) {
-            Options.onSelect = function (DateText, Instance) {
-                var Year = Instance.selectedYear,
-                    Month = Instance.selectedMonth + 1,
-                    Day = Instance.selectedDay;
+        Options.onSelect = function (DateText, Instance) {
+            var Year = Instance.selectedYear,
+                Month = Instance.selectedMonth + 1,
+                Day = Instance.selectedDay;
 
-                // Update the three select boxes
+            // Update the three select boxes
+            if (HasDateSelectBoxes) {
                 Element['Year'].find('option[value=' + Year + ']').attr('selected', 'selected');
                 Element['Month'].find('option[value=' + Month + ']').attr('selected', 'selected');
                 Element['Day'].find('option[value=' + Day + ']').attr('selected', 'selected');
-            };
-            Options.beforeShow = function (Input, Instance) {
-                $(Input).val('');
-                return {
-                    defaultDate: new Date(Element.Year.val(), Element.Month.val() - 1, Element.Day.val())
-                }
-            };
-
-        }
+            }
+            else {
+                Element['Year'].val(Year);
+                Element['Month'].val(LeadingZero(Month));
+                Element['Day'].val(LeadingZero(Day));
+            }
+        };
+        Options.beforeShow = function (Input, Instance) {
+            $(Input).val('');
+            return {
+                defaultDate: new Date(Element.Year.val(), Element.Month.val() - 1, Element.Day.val())
+            }
+        };
 
         $DatepickerElement.datepicker(Options);
 
