e96d43459397d11de161c24f3bd0333001f6bb61
Manuel Hecht <manuel.hecht@otrs.com>
2010-06-22 09:55:02 +0000

    Updated CPAN module XML::Parser to version 0.712.

--- Kernel/cpan-lib/XML/Parser/Lite.pm
+++ Kernel/cpan-lib/XML/Parser/Lite.pm
@@ -5,16 +5,16 @@
 # SOAP::Lite is free software; you can redistribute it
 # and/or modify it under the same terms as Perl itself.
 #
-# $Id: Lite.pm,v 1.6 2009-12-07 10:51:27 martin Exp $
+# $Id: Lite.pm,v 1.7 2010-06-22 09:55:02 mh Exp $
 #
 # ======================================================================
 
 package XML::Parser::Lite;
 
 use strict;
-use vars qw($VERSION);
-#use version; $VERSION = qv('0.710.10');
-$VERSION = '0.710.10';
+use warnings;
+
+our $VERSION = 0.712;
 
 sub new {
     my $class = shift;
@@ -33,7 +33,8 @@ sub setHandlers {
     my $self = shift;
 
     # allow symbolic refs, avoid "subroutine redefined" warnings
-    no strict 'refs'; local $^W;
+    no strict 'refs';
+    no warnings qw(redefine);
     # clear all handlers if called without parameters
     if (not @_) {
         for (qw(Start End Char Final Init Comment Doctype XMLDecl)) {
@@ -183,6 +184,7 @@ sub _char {
 }
 
 sub _end {
+    no warnings qw(uninitialized);
     pop(@stack) eq $_[0] or die "mismatched tag '$_[0]'\n";
     End(__PACKAGE__, $_[0]);
 }
