0dc8c48ed78aef4ed98696f452be5a0f7b572ecf
Felix Niklas <felix.niklas@otrs.com>
2010-06-22 15:46:14 +0000

    Matched with the fixed table javascript.

--- var/httpd/htdocs/js_new/Core.Agent.TicketZoom.js
+++ var/httpd/htdocs/js_new/Core.Agent.TicketZoom.js
@@ -2,7 +2,7 @@
 // Core.Agent.TicketZoom.js - provides the special module functions for TicketZoom
 // Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
 // --
-// $Id: Core.Agent.TicketZoom.js,v 1.2 2010-06-14 20:56:51 fn Exp $
+// $Id: Core.Agent.TicketZoom.js,v 1.3 2010-06-22 15:46:14 fn Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
@@ -27,8 +27,16 @@ Core.Agent.TicketZoom = (function (TargetNS) {
      *      This function initializes the special module functions
      */
     TargetNS.Init = function () {
+        var $THead = $('#FixedTable thead'), 
+            $TBody = $('#FixedTable tbody');
+        
         Core.UI.Resizable.Init(".ArticleTableBody");
+        Core.UI.initTableHead($THead, $TBody);
 
+        $(window).resize(function () {
+            Core.UI.AdjustTableHead($THead, $TBody);
+        });
+        
         Core.UI.Dialog.RegisterAttachmentDialog($('.TableSmall tbody td a.Attachment'));
     };
 
