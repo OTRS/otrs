c4e4c8b2f2457c99f8e0a69dfe80fd5a2eb73c5c
Elias Probst <elias.probst@otrs.com>
2010-06-23 05:41:09 +0000

    Changed LogPrefix (OTRS-getTicketThread -> OTRS-GetTicketThread) to match the renaming of the script file.

--- bin/otrs.GetTicketThread.pl
+++ bin/otrs.GetTicketThread.pl
@@ -3,7 +3,7 @@
 # bin/otrs.GetTicketThread.pl - to print the whole ticket thread to STDOUT
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: otrs.GetTicketThread.pl,v 1.1 2010-04-05 10:12:20 mb Exp $
+# $Id: otrs.GetTicketThread.pl,v 1.2 2010-06-23 05:41:09 ep Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 use Kernel::Config;
 use Kernel::System::Encode;
@@ -47,7 +47,7 @@ my %CommonObject = ();
 $CommonObject{ConfigObject} = Kernel::Config->new();
 $CommonObject{EncodeObject} = Kernel::System::Encode->new(%CommonObject);
 $CommonObject{LogObject}    = Kernel::System::Log->new(
-    LogPrefix => 'OTRS-getTicketThread',
+    LogPrefix => 'OTRS-GetTicketThread',
     %CommonObject,
 );
 $CommonObject{TimeObject}   = Kernel::System::Time->new(%CommonObject);
