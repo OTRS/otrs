59937e9b8d9569413a035a2ba379c833aa3040c1
Elias Probst <elias.probst@otrs.com>
2010-06-24 14:21:27 +0000

    Fixed typo ('zu setzt' -> 'zu setzen').

--- Kernel/Language/de.pm
+++ Kernel/Language/de.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de.pm - provides German language translation
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: de.pm,v 1.227 2010-05-05 09:51:32 mg Exp $
+# $Id: de.pm,v 1.228 2010-06-24 14:21:27 ep Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -14,7 +14,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.227 $) [1];
+$VERSION = qw($Revision: 1.228 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -1525,7 +1525,7 @@ sub Data {
         'Accept license' => 'Lizenz akzeptieren',
         'Don\'t accept license' => 'Lizenz _nicht_ akzeptieren',
         'Admin-User' => 'Admin-Benutzer',
-        'If you have set a root password for your database, it must be entered here. If not, leave this field empty. For security reasons we do recommend setting a root password. For more information please refer to your database documentation.' => 'Falls ein Root-Passwort für die Datenbank gesetzt ist, muss es hier eingegeben werden. Ist kein Passwort gesetzt, muss das Feld leer gelassen werden. Aus Sicherheitsgründen empfehlen wir ein Root-Passwort zu setzt. Weitere Informationen hierzu finden Sie in der Dokumentation Ihrer Datenbank.',
+        'If you have set a root password for your database, it must be entered here. If not, leave this field empty. For security reasons we do recommend setting a root password. For more information please refer to your database documentation.' => 'Falls ein Root-Passwort für die Datenbank gesetzt ist, muss es hier eingegeben werden. Ist kein Passwort gesetzt, muss das Feld leer gelassen werden. Aus Sicherheitsgründen empfehlen wir ein root-Passwort zu setzen. Weitere Informationen hierzu finden Sie in der Dokumentation Ihrer Datenbank.',
         'Admin-Password' => 'Admin-Passwort',
         'Database-User' => 'Datenbank-Benutzer',
         'default \'hot\'' => 'voreingestellt \'hot\'',
