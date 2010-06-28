d83cedc0e15633839b7f95127e291a29cd637dd5
Marc Nilius <marc.nilius@otrs.com>
2010-06-28 13:46:31 +0000

    First (not completed) version of the porting of option dialogs of AgentTicketEmail (adress book, spell check, customer).

--- Kernel/Modules/AgentBook.pm
+++ Kernel/Modules/AgentBook.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentBook.pm - addressbook module
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentBook.pm,v 1.16 2010-06-27 09:33:57 bes Exp $
+# $Id: AgentBook.pm,v 1.17 2010-06-28 13:46:31 mn Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -60,14 +60,21 @@ sub Run {
             $List{ $CustomerUserData{UserEmail} } = $CustomerUserList{$_};
         }
     }
-    for ( reverse sort { $List{$b} cmp $List{$a} } keys %List ) {
+
+    if (%List) {
         $Self->{LayoutObject}->Block(
-            Name => 'Row',
-            Data => {
-                Name  => $List{$_},
-                Email => $_,
-            },
+            Name => 'SearchResult',
         );
+
+        for ( reverse sort { $List{$b} cmp $List{$a} } keys %List ) {
+            $Self->{LayoutObject}->Block(
+                Name => 'Row',
+                Data => {
+                    Name  => $List{$_},
+                    Email => $_,
+                },
+            );
+        }
     }
 
     # start with page ...
