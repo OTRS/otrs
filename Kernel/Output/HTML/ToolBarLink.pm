e917123abc438a62f0b612f75790da9114266b7c
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-07-01 09:45:04 +0000

    Added all possible default tool bar items.

---
+++ Kernel/Output/HTML/ToolBarLink.pm
@@ -0,0 +1,68 @@
+# --
+# Kernel/Output/HTML/ToolBarLink.pm
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: ToolBarLink.pm,v 1.1 2010-07-01 09:45:04 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::ToolBarLink;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(ConfigObject LogObject DBObject TicketObject LayoutObject UserID)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(Config)) {
+        if ( !$Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+
+    # check if frontend module is used
+    my $Action = $Param{Config}->{Action};
+    if ($Action) {
+        return if !$Self->{ConfigObject}->Get('Frontend::Module')->{$Action};
+    }
+
+    # get item definition
+    my $Text      = $Self->{LayoutObject}->{LanguageObject}->Get( $Param{Config}->{Name} );
+    my $URL       = $Self->{LayoutObject}->{Baselink} . $Param{Config}->{Link};
+    my $Priority  = $Param{Config}->{Priority};
+    my $AccessKey = $Param{Config}->{AccessKey};
+    my $CssClass  = $Param{Config}->{CssClass};
+    my %Return;
+    $Return{$Priority} = {
+        Block       => 'ToolBarItem',
+        Description => $Text,
+        Class       => $CssClass,
+        Link        => $URL,
+        AccessKey   => $AccessKey,
+    };
+    return %Return;
+}
+
+1;
