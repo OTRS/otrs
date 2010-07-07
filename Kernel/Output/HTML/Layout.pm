99f16e9531a349b02fadd6b3ec9ca75d51fc1c41
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-07-07 09:13:14 +0000

    Only show toolbar wrapper if content exists.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Layout.pm,v 1.271 2010-07-02 09:34:35 mg Exp $
+# $Id: Layout.pm,v 1.272 2010-07-07 09:13:14 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Kernel::System::JSON;
 use Mail::Address;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.271 $) [1];
+$VERSION = qw($Revision: 1.272 $) [1];
 
 =head1 NAME
 
@@ -1280,9 +1280,18 @@ sub Header {
         }
 
         # show tool bar items
+        my $ToolBarShown = 0;
         for my $Key ( sort keys %Modules ) {
             next if !%{ $Modules{$Key} };
 
+            # show tool bar wrapper
+            if ( !$ToolBarShown ) {
+                $ToolBarShown = 1;
+                $Self->Block(
+                    Name => 'ToolBar',
+                    Data => \%Param,
+                );
+            }
             $Self->Block(
                 Name => $Modules{$Key}->{Block},
                 Data => $Modules{$Key},
@@ -4651,6 +4660,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.271 $ $Date: 2010-07-02 09:34:35 $
+$Revision: 1.272 $ $Date: 2010-07-07 09:13:14 $
 
 =cut
