9428a0b4adb7ccec1e077717c45f1136bcca1008
Martin Gruner <martin.gruner@otrs.com>
2010-07-09 07:56:35 +0000

    Add comment.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.354 2010-07-09 07:56:11 mg Exp $
+# $Id: Defaults.pm,v 1.355 2010-07-09 07:56:35 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -25,7 +25,7 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.354 $) [1];
+$VERSION = qw($Revision: 1.355 $) [1];
 
 use File::stat;
 use Digest::MD5;
@@ -1958,7 +1958,7 @@ sub Set {
 # ConfigChecksum
 #
 # This function returns an MD5 sum that is generated from all available
-#   config files (Kernel/Config.pm, Kernel/Config/Files/*.pm) and their
+#   config files (Kernel/Config.pm, Kernel/Config/Defaults.pm, Kernel/Config/Files/*.pm) and their
 #   modification timestamps. Whenever a file is changed, added or removed,
 #   this checksum will change.
 #
@@ -2205,6 +2205,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.354 $ $Date: 2010-07-09 07:56:11 $
+$Revision: 1.355 $ $Date: 2010-07-09 07:56:35 $
 
 =cut
