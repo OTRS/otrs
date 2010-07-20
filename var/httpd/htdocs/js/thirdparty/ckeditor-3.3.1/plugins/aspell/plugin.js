52337eee9f5199f0dc007e386536138c37c39e95
Marc Nilius <marc.nilius@otrs.com>
2010-07-20 12:23:49 +0000

    Fixed aspell plugin because of error in CKPackager. JavaScript delete statement does nothing on variables declared with "var". Statement can only work on object properties or implicitly declared (global) variables. So, the deleted lines are useless and only cause errors on generating minified CKEditor package.

--- var/httpd/htdocs/js/thirdparty/ckeditor-3.3.1/plugins/aspell/plugin.js
+++ var/httpd/htdocs/js/thirdparty/ckeditor-3.3.1/plugins/aspell/plugin.js
@@ -8,23 +8,22 @@ CKEDITOR.plugins.add('aspell', {
 	init: function (editor) {
 		// Create dialog-based command named "aspell"
 		editor.addCommand('aspell', new CKEDITOR.dialogCommand('aspell'));
-		
+
 		// Add button to toolbar. Not sure why only that name works for me.
 		editor.ui.addButton('SpellCheck', {
 			label: editor.lang.spellCheck.toolbar,
 			command: 'aspell'
 		});
-		
+
 		// Add link dialog code
 		CKEDITOR.dialog.add('aspell', this.path + 'dialogs/aspell.js');
-		
+
 		// Add CSS
 		var aspellCSS = document.createElement('link');
 		aspellCSS.setAttribute( 'rel', 'stylesheet');
 		aspellCSS.setAttribute('type', 'text/css');
 		aspellCSS.setAttribute('href', this.path+'aspell.css');
 		document.getElementsByTagName("head")[0].appendChild(aspellCSS);
-		delete aspellCSS;
 	},
 	requires: ['toolbar']
 });
