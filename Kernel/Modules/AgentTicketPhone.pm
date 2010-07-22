807637a1f18d0ba80b8b46778bc5811ce2ec16c2
Elva Novoa <elva.novoa@otrs.com>
2010-07-22 20:27:37 +0000

    Added server side validation for the time units.

--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPhone.pm - to handle phone calls
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPhone.pm,v 1.152 2010-07-22 18:58:10 en Exp $
+# $Id: AgentTicketPhone.pm,v 1.153 2010-07-22 20:27:26 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -23,7 +23,7 @@ use Kernel::System::LinkObject;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.152 $) [1];
+$VERSION = qw($Revision: 1.153 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -733,6 +733,13 @@ sub Run {
             }
             if ( !$GetParam{Body} && $ExpandCustomerName == 0 ) {
                 $Error{'RichTextInvalid'} = ' ServerError';
+            }
+            if (
+                ( $Self->{ConfigObject}->Get('Ticket::Frontend::AccountTime') )
+                && !$GetParam{TimeUnits}
+                )
+            {
+                $Error{'TimeUnitsInvalid'} = ' ServerError';
             }
         }
 
