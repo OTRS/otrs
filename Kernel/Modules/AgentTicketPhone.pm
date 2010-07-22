1dccff701a5a9c7f3cb93f966b670a368f44d406
Elva Novoa <elva.novoa@otrs.com>
2010-07-22 18:58:10 +0000

    QA of the server side validations. Check if the ticket type is enabled, before sending the error.

--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPhone.pm - to handle phone calls
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPhone.pm,v 1.151 2010-07-21 23:36:43 en Exp $
+# $Id: AgentTicketPhone.pm,v 1.152 2010-07-22 18:58:10 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -23,7 +23,7 @@ use Kernel::System::LinkObject;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.151 $) [1];
+$VERSION = qw($Revision: 1.152 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -702,7 +702,7 @@ sub Run {
         # check email address
         for my $Email ( Mail::Address->parse( $GetParam{From} ) ) {
             if ( !$Self->{CheckItemObject}->CheckEmail( Address => $Email->address() ) ) {
-                $Error{'From invalid'} .= $Self->{CheckItemObject}->CheckError();
+                $Error{'FromInvalid'} = ' ServerError';
             }
         }
         if ( !$IsUpload ) {
@@ -728,10 +728,10 @@ sub Run {
             {
                 $Error{'ServiceInvalid'} = ' ServerError';
             }
-            if ( !$GetParam{TypeID} ) {
+            if ( ( !$GetParam{TypeID} ) && ( $Self->{ConfigObject}->Get('Ticket::Type') ) ) {
                 $Error{'TypeIDInvalid'} = ' ServerError';
             }
-            if ( !$GetParam{Body} ) {
+            if ( !$GetParam{Body} && $ExpandCustomerName == 0 ) {
                 $Error{'RichTextInvalid'} = ' ServerError';
             }
         }
