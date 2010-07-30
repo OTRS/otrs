a95d5b594a9d6d62ecce52fcc83fd9a9a132082e
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-07-30 00:36:56 +0000

    Added not longer used directory.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: auto_build.sh,v 1.74 2010-07-28 14:53:07 martin Exp $
+# $Id: auto_build.sh,v 1.75 2010-07-30 00:36:56 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -21,7 +21,7 @@
 # or see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.74 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.75 $>"
 echo "Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
 
 PATH_TO_CVS_SRC=$1
@@ -162,6 +162,7 @@ rm -rf Kernel/Display
 rm -rf var/sesstions
 rm -rf var/httpd/htdocs/yui
 rm -rf var/httpd/htdocs/js/fckeditor*
+rm -rf var/httpd/htdocs/css
 rm -rf Kernel/System/Ticket/Compress
 rm -rf Kernel/System/Ticket/Crypt
 # remove not used skins
