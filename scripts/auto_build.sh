cc31b77fb1b9c1faf73b908c5fe1565d5ee22dca
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-08-19 08:42:12 +0000

    Added iphone package per default.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: auto_build.sh,v 1.61.2.7 2010-05-13 16:30:47 cg Exp $
+# $Id: auto_build.sh,v 1.61.2.8 2010-08-19 08:42:12 martin Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -21,7 +21,7 @@
 # or see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.61.2.7 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.61.2.8 $>"
 echo "Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
 
 PATH_TO_CVS_SRC=$1
@@ -39,6 +39,7 @@ PACKAGE_TMP_SPEC="/tmp/$PACKAGE.spec"
 #RPM_BUILD="rpmbuild"
 RPM_BUILD="rpm"
 SUPPORT_PACKAGE="http://ftp.otrs.org/pub/otrs/packages/Support-1.1.1.opm"
+IPHONE_PACKAGE="http://ftp.otrs.org/pub/otrs/packages/iPhoneHandle-0.9.5.opm"
 
 if ! test $PATH_TO_CVS_SRC || ! test $VERSION || ! test $RELEASE; then
     # --
@@ -200,6 +201,9 @@ bin/CheckSum.pl -a create
 mkdir var/packages/
 wget "$SUPPORT_PACKAGE"
 mv Support*.opm var/packages/
+
+wget "$IPHONE_PACKAGE"
+mv iPhoneHandle*.opm var/packages/
 
 # --
 # create tar
