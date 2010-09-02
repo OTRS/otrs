83c6bb8e4e9c8a6aae92a6e74505f23afc45895b
Elva Novoa <elva.novoa@otrs.com>
2010-09-02 17:46:49 +0000

    Deleted duplicate comment.

--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPhone.pm - to handle phone calls
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPhone.pm,v 1.158 2010-08-19 16:47:22 en Exp $
+# $Id: AgentTicketPhone.pm,v 1.159 2010-09-02 17:46:49 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -23,7 +23,7 @@ use Kernel::System::LinkObject;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.158 $) [1];
+$VERSION = qw($Revision: 1.159 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1599,7 +1599,6 @@ sub _MaskPhoneNew {
         Class            => $Param{Errors}->{DateInvalid},
     );
 
-    # show owner selection
     # show owner selection
     if ( $Self->{ConfigObject}->Get('Ticket::Frontend::NewOwnerSelection') ) {
         $Self->{LayoutObject}->Block(
