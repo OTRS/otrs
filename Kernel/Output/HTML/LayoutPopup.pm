016c06f5ae5114b16042d1595aac20747a83bfa4
Martin Gruner <martin.gruner@otrs.com>
2010-09-06 13:02:37 +0000

    Added new function to close a popup and initiate a main window reload.

--- Kernel/Output/HTML/LayoutPopup.pm
+++ Kernel/Output/HTML/LayoutPopup.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/LayoutPopup.pm - provides generic HTML output
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: LayoutPopup.pm,v 1.1 2010-07-13 21:18:40 cg Exp $
+# $Id: LayoutPopup.pm,v 1.2 2010-09-06 13:02:37 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 =head1 NAME
 
@@ -33,14 +33,19 @@ All valid functions.
 
 =item PopupClose()
 
-Generate the call Header() and Footer() for Pop-Up Close.
+Generate a small HTML page which closes the popup window and
+executes an action in the main window.
 
+    # load specific URL in main window
     $LayoutObject->PopupClose(
-        URL => "The URL"
+        URL => "Action=AgentTicketZoom;TicketID=$TicketID"
     );
 
+    or
+
+    # reload main window
     $Self->{LayoutObject}->PopupClose(
-        URL => "Action=AgentTicketZoom;TicketID=$Self->{TicketID};ArticleID=$ArticleID",
+        Reload => 1,
     );
 
 =cut
@@ -48,19 +53,27 @@ Generate the call Header() and Footer() for Pop-Up Close.
 sub PopupClose {
     my ( $Self, %Param ) = @_;
 
-    if ( !$Param{URL} ) {
-        $Self->{LogObject}->Log( Priority => 'error', Message => 'Need URL!' );
+    if ( !$Param{URL} && !$Param{Reload} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => 'Need URL or Reload!' );
         return;
     }
 
     # Generate the call Header() and Footer(
     my $Output = $Self->Header( Type => 'Small' );
-    $Self->Block(
-        Name => 'LoadParentURLAndClose',
-        Data => {
-            URL => $Param{URL},
-        },
-    );
+
+    if ( $Param{URL} ) {
+        $Self->Block(
+            Name => 'LoadParentURLAndClose',
+            Data => {
+                URL => $Param{URL},
+            },
+        );
+    }
+    else {
+        $Self->Block(
+            Name => 'ReloadParentAndClose',
+        );
+    }
     $Output .= $Self->Output( TemplateFile => 'AgentTicketActionPopupClose' );
     $Output .= $Self->Footer( Type => 'Small' );
     return $Output;
@@ -82,6 +95,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.1 $ $Date: 2010-07-13 21:18:40 $
+$Revision: 1.2 $ $Date: 2010-09-06 13:02:37 $
 
 =cut
