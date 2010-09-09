ead3359489809d2451e61f343ad906255386aa28
Martin Edenhofer <martin.edenhofer@otrs.com>
2010-09-09 12:58:26 +0000

    Fixed bug#5462 - Kernel::System::Ticket::TicketEscalationIndexBuild() does not invalidate the cache.

--- Kernel/System/Ticket.pm
+++ Kernel/System/Ticket.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket.pm - all ticket functions
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Ticket.pm,v 1.469 2010-09-07 08:55:47 mb Exp $
+# $Id: Ticket.pm,v 1.470 2010-09-09 12:58:26 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -35,7 +35,7 @@ use Kernel::System::LinkObject;
 use Kernel::System::EventHandler;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.469 $) [1];
+$VERSION = qw($Revision: 1.470 $) [1];
 
 =head1 NAME
 
@@ -2352,6 +2352,10 @@ sub TicketEscalationIndexBuild {
                 Bind => [ \$Time, \$Ticket{TicketID}, ]
             );
         }
+
+        # clear ticket cache
+        delete $Self->{ 'Cache::GetTicket' . $Param{TicketID} };
+
         return 1;
     }
 
@@ -2568,6 +2572,9 @@ sub TicketEscalationIndexBuild {
             Bind => [ \$EscalationTime, \$Ticket{TicketID}, ]
         );
     }
+
+    # clear ticket cache
+    delete $Self->{ 'Cache::GetTicket' . $Param{TicketID} };
 
     return 1;
 }
@@ -8109,6 +8116,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.469 $ $Date: 2010-09-07 08:55:47 $
+$Revision: 1.470 $ $Date: 2010-09-09 12:58:26 $
 
 =cut
