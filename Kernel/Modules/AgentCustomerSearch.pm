df474819a44dabd9960bc565d41717a0dd113450
Martin Gruner <martin.gruner@otrs.com>
2010-09-24 08:21:26 +0000

    Fixed sorting in customer history table.

--- Kernel/Modules/AgentCustomerSearch.pm
+++ Kernel/Modules/AgentCustomerSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentCustomerSearch.pm - a module used for the autocomplete feature
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentCustomerSearch.pm,v 1.24 2010-09-23 22:22:32 cg Exp $
+# $Id: AgentCustomerSearch.pm,v 1.25 2010-09-24 08:21:26 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.24 $) [1];
+$VERSION = qw($Revision: 1.25 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -210,6 +210,9 @@ sub Run {
                 LinkSort   => $LinkSort,
                 LinkFilter => $LinkFilter,
                 Output     => 'raw',
+
+                OrderBy => $OrderBy,
+                SortBy  => $SortBy,
             );
         }
 
