c33e9a249fcc499e1f37f10035dea8bcb29d1858
Martin Gruner <martin.gruner@otrs.com>
2010-10-01 12:27:54 +0000

    Accessibility improvements, problems were found with FredHTMLCheck.

--- Kernel/Modules/AgentTicketZoom.pm
+++ Kernel/Modules/AgentTicketZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketZoom.pm - to get a closer view
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketZoom.pm,v 1.125 2010-09-30 09:33:35 mg Exp $
+# $Id: AgentTicketZoom.pm,v 1.126 2010-10-01 12:22:55 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::CustomerUser;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.125 $) [1];
+$VERSION = qw($Revision: 1.126 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1247,6 +1247,7 @@ sub _ArticleItem {
                         Class                 => 'AsPopup',
                         Action                => 'AgentTicketCompose',
                         FormID                => 'Reply',
+                        ResponseElementID     => 'ResponseID',
                     },
                 );
                 $Self->{LayoutObject}->Block(
@@ -1276,6 +1277,7 @@ sub _ArticleItem {
                         Action                => 'AgentTicketCompose',
                         FormID                => 'ReplyAll',
                         ReplyAll              => 1,
+                        ResponseElementID     => 'ResponseIDAll',
                     },
                 );
                 $Self->{LayoutObject}->Block(
