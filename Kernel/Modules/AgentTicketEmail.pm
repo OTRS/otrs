c8052e4544dbdd15f2f0540fa529efd693fe96e7
Michiel Beijen <michiel.beijen@otrs.com>
2010-10-17 20:17:20 +0000

    Fixed bug#6109 - '0' is not recognized as a valid input for TimeUnits. http://bugs.otrs.org/show_bug.cgi?id=6109

--- Kernel/Modules/AgentTicketEmail.pm
+++ Kernel/Modules/AgentTicketEmail.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketEmail.pm - to compose initial email to customer
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketEmail.pm,v 1.145 2010-10-13 09:29:03 cg Exp $
+# $Id: AgentTicketEmail.pm,v 1.146 2010-10-17 20:17:20 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::State;
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.145 $) [1];
+$VERSION = qw($Revision: 1.146 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -703,7 +703,7 @@ sub Run {
             if (
                 $Self->{ConfigObject}->Get('Ticket::Frontend::AccountTime')
                 && $Self->{ConfigObject}->Get('Ticket::Frontend::NeedAccountedTime')
-                && !$GetParam{TimeUnits}
+                && !defined $GetParam{TimeUnits}
                 )
             {
                 $Error{'TimeUnitsInvalid'} = 'ServerError';
