461a541f22f25d12cde1a2d3d23cff89f3729435
Martin Gruner <martin.gruner@otrs.com>
2010-10-28 12:19:10 +0000

    Added unit tests for Core.Data.

--- var/httpd/htdocs/js/test/Core.Data.UnitTest.js
+++ var/httpd/htdocs/js/test/Core.Data.UnitTest.js
@@ -2,7 +2,7 @@
 // Core.UI.Accessibility.UnitTest.js - UnitTests
 // Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
 // --
-// $Id: Core.Data.UnitTest.js,v 1.1 2010-07-13 09:46:45 mg Exp $
+// $Id: Core.Data.UnitTest.js,v 1.2 2010-10-28 12:19:10 mg Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ Core.Data = (function (Namespace) {
         module('Core.Data');
         test('Core.Data.Set()', function(){
 
-            expect(3);
+            expect(5);
 
             /*
              * Create a div containter for the tests
@@ -36,16 +36,22 @@ Core.Data = (function (Namespace) {
             var ObjectOne = $('#ElementOne');
             var ObjectTwo = $('#ElementTwo');
 
+            var ResultOneEmpty = Core.Data.Get(ObjectOne,'One');
+            same(ResultOneEmpty, {}, 'information not yet stored');
+
+            var NonexistingResuult = Core.Data.Get($('#nonexisting_selector'),'One');
+            same(NonexistingResuult, {}, 'nonexisting element');
+
             Core.Data.Set(ObjectOne,'One',Sign);
             Core.Data.Set(ObjectTwo,'Two',Sign);
-            
+
             var ResultOne = Core.Data.Get(ObjectOne,'One');
             var ResultTwo = Core.Data.Get(ObjectTwo,'Two');
 
             equals(ResultOne, Sign, 'okay');
             equals(ResultTwo, Sign, 'okay');
             equals(ResultOne, ResultTwo, 'okay');
-            
+
              /*
              * Cleanup div container and contents
              */
