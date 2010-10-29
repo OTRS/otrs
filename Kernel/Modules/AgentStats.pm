50089388af4aa4f7b3b8916c544305a065c2abfc
Elva Novoa <elva.novoa@otrs.com>
2010-10-29 18:41:40 +0000

    Displaying the name of the graphic, instead of its package name for the bug fix of bug 6084.

--- Kernel/Modules/AgentStats.pm
+++ Kernel/Modules/AgentStats.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentStats.pm - stats module
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentStats.pm,v 1.102 2010-10-29 18:32:16 en Exp $
+# $Id: AgentStats.pm,v 1.103 2010-10-29 18:41:40 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -21,7 +21,7 @@ use Kernel::System::CSV;
 use Kernel::System::PDF;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.102 $) [1];
+$VERSION = qw($Revision: 1.103 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1364,7 +1364,7 @@ sub Run {
             for my $FormatName ( keys %$AvailableFormats ) {
                 my %Format;
                 $Format{Key}      = $FormatName;
-                $Format{Value}    = $FormatName;
+                $Format{Value}    = $AvailableFormats->{$FormatName};
                 $Format{Disabled} = ( $FormatName =~ m/graph/gi ) ? 1 : 0;
                 push @ResultFormat, \%Format;
             }
