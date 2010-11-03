e917eee99766b815408524d759d3ff158bfcd65c
Michiel Beijen <michiel.beijen@otrs.com>
2010-11-03 14:37:32 +0000

    Made page title in Customer interface consistent.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Layout.pm,v 1.319 2010-11-03 10:07:24 mg Exp $
+# $Id: Layout.pm,v 1.320 2010-11-03 14:37:32 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Kernel::System::JSON;
 use Mail::Address;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.319 $) [1];
+$VERSION = qw($Revision: 1.320 $) [1];
 
 =head1 NAME
 
@@ -3045,7 +3045,7 @@ sub CustomerLogin {
     my ( $Self, %Param ) = @_;
 
     my $Output = '';
-    $Param{TitleArea} = ' :: ' . $Self->{LanguageObject}->Get('Login');
+    $Param{TitleArea} = $Self->{LanguageObject}->Get('Login') . ' - ';
 
     # set Action parameter for the loader
     $Self->{Action} = 'CustomerLogin';
@@ -4891,6 +4891,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.319 $ $Date: 2010-11-03 10:07:24 $
+$Revision: 1.320 $ $Date: 2010-11-03 14:37:32 $
 
 =cut
