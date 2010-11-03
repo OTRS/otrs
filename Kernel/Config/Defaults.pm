aab9d6dcad883d6ea2232971f79980663e762052
Michiel Beijen <michiel.beijen@otrs.com>
2010-11-03 16:23:40 +0000

    Added Serbian translation, thanks to Milorad Jovanović!

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.370 2010-10-28 13:00:07 mn Exp $
+# $Id: Defaults.pm,v 1.371 2010-11-03 16:23:40 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -25,7 +25,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.370 $) [1];
+$VERSION = qw($Revision: 1.371 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 use File::Basename;
@@ -155,38 +155,40 @@ sub LoadDefaults {
     # used languages
     # (short name = long name and file)
     $Self->{DefaultUsedLanguages} = {
-        ar_SA => 'Arabic (Saudi Arabia)',
-        bg    => 'Bulgarian (&#x0411;&#x044a;&#x043b;&#x0433;&#x0430;&#x0440;&#x0441;&#x043a;&#x0438;)',
-        ct    => 'Catal&agrave;',
-        cz    => 'Czech (&#x010c;esky)',
-        da    => 'Dansk',
-        de    => 'Deutsch',
-        en    => 'English (United States)',
-        en_CA => 'English (Canada)',
-        en_GB => 'English (United Kingdom)',
-        el    => 'Greek (&#x0395;&#x03bb;&#x03bb;&#x03b7;&#x03bd;&#x03b9;&#x03ba;&#x03ac;)',
-        es    => 'Espa&ntilde;ol',
-        es_MX => 'Espa&ntilde;ol (M&eacute;xico)',
-        et    => 'Eesti',
-        fa    => 'Persian (&#x0641;&#x0627;&#x0631;&#x0633;&#x0649;)',
-        fr    => 'Fran&ccedil;ais',
-        fi    => 'Suomi',
-        hu    => 'Magyar',
-        it    => 'Italiano',
-        lv    => 'Latvijas',
-        nl    => 'Nederlands',
-        nb_NO => 'Norsk bokm&aring;l',
-        pt_BR => 'Portugu&ecirc;s Brasileiro',
-        pt    => 'Portugu&ecirc;s',
-        pl    => 'Polski',
-        ru    => 'Russian (&#x0420;&#x0443;&#x0441;&#x0441;&#x043a;&#x0438;&#x0439;)',
-        sk_SK => 'Slovak (Sloven&#x010d;ina)',
-        sv    => 'Svenska',
-        tr    => 'T&uuml;rk&ccedil;e',
-        uk    => 'Ukrainian (&#x0423;&#x043a;&#x0440;&#x0430;&#x0457;&#x043d;&#x0441;&#x044c;&#x043a;&#x0430;)',
-        vi_VN => 'Vietnam (Vi&#x0246;t Nam)',
-        zh_CN => 'Chinese (Sim.) (&#x7b80;&#x4f53;&#x4e2d;&#x6587;)',
-        zh_TW => 'Chinese (Tradi.) (&#x6b63;&#x9ad4;&#x4e2d;&#x6587;)',
+        ar_SA   => 'Arabic (Saudi Arabia)',
+        bg      => 'Bulgarian (&#x0411;&#x044a;&#x043b;&#x0433;&#x0430;&#x0440;&#x0441;&#x043a;&#x0438;)',
+        ct      => 'Catal&agrave;',
+        cz      => 'Czech (&#x010c;esky)',
+        da      => 'Dansk',
+        de      => 'Deutsch',
+        en      => 'English (United States)',
+        en_CA   => 'English (Canada)',
+        en_GB   => 'English (United Kingdom)',
+        el      => 'Greek (&#x0395;&#x03bb;&#x03bb;&#x03b7;&#x03bd;&#x03b9;&#x03ba;&#x03ac;)',
+        es      => 'Espa&ntilde;ol',
+        es_MX   => 'Espa&ntilde;ol (M&eacute;xico)',
+        et      => 'Eesti',
+        fa      => 'Persian (&#x0641;&#x0627;&#x0631;&#x0633;&#x0649;)',
+        fr      => 'Fran&ccedil;ais',
+        fi      => 'Suomi',
+        hu      => 'Magyar',
+        it      => 'Italiano',
+        lv      => 'Latvijas',
+        nl      => 'Nederlands',
+        nb_NO   => 'Norsk bokm&aring;l',
+        pt_BR   => 'Portugu&ecirc;s Brasileiro',
+        pt      => 'Portugu&ecirc;s',
+        pl      => 'Polski',
+        ru      => 'Russian (&#x0420;&#x0443;&#x0441;&#x0441;&#x043a;&#x0438;&#x0439;)',
+        sk_SK   => 'Slovak (Sloven&#x010d;ina)',
+        sr_Cyrl => 'Serbian Latin (Srpski)',
+        sr_Latn => 'Serbian Cyrillic (&#1089;&#1088;&#1087;&#1089;&#1082;&#1080;)',
+        sv      => 'Svenska',
+        tr      => 'T&uuml;rk&ccedil;e',
+        uk      => 'Ukrainian (&#x0423;&#x043a;&#x0440;&#x0430;&#x0457;&#x043d;&#x0441;&#x044c;&#x043a;&#x0430;)',
+        vi_VN   => 'Vietnam (Vi&#x0246;t Nam)',
+        zh_CN   => 'Chinese (Sim.) (&#x7b80;&#x4f53;&#x4e2d;&#x6587;)',
+        zh_TW   => 'Chinese (Tradi.) (&#x6b63;&#x9ad4;&#x4e2d;&#x6587;)',
 
 #            th => 'Thai (&#x0e44;&#x0e17;&#x0e22;)',
 #            ro => 'Rom&acirc;n&auml;',
@@ -2221,6 +2223,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.370 $ $Date: 2010-10-28 13:00:07 $
+$Revision: 1.371 $ $Date: 2010-11-03 16:23:40 $
 
 =cut
