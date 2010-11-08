89c81e02778cb03a82e669824c278d38188015db
Elva Novoa <elva.novoa@otrs.com>
2010-11-08 21:35:24 +0000

    Added class to validate email addresses and changed the tooltips showed for client side validation.

--- Kernel/Modules/AdminCustomerUser.pm
+++ Kernel/Modules/AdminCustomerUser.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminCustomerUser.pm - to add/update/delete customer user and preferences
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminCustomerUser.pm,v 1.83 2010-11-08 19:35:26 en Exp $
+# $Id: AdminCustomerUser.pm,v 1.84 2010-11-08 21:35:24 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::Valid;
 use Kernel::System::CheckItem;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.83 $) [1];
+$VERSION = qw($Revision: 1.84 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -654,6 +654,11 @@ sub _Edit {
             $Param{RequiredLabelCharacter} = '';
         }
 
+        # add class to validate emails
+        if ( $Entry->[0] eq 'UserEmail' ) {
+            $Param{RequiredClass} .= ' Validate_Email';
+        }
+
         # build selections or input fields
         if ( $Self->{ConfigObject}->Get( $Param{Source} )->{Selections}->{ $Entry->[0] } ) {
             $Block = 'Option';
@@ -755,12 +760,26 @@ sub _Edit {
                 },
             );
 
+            # add the correct client side error msg
+            if ( $Block eq 'Input' && $Entry->[0] eq 'UserEmail' ) {
+                $Self->{LayoutObject}->Block(
+                    Name => 'PreferencesUserEmailErrorMsg',
+                    Data => { Name => $Entry->[0] },
+                );
+            }
+            else {
+                $Self->{LayoutObject}->Block(
+                    Name => "PreferencesGenericErrorMsg",
+                    Data => { Name => $Entry->[0] },
+                );
+            }
+
             # add the correct server error msg
             if ( $Block eq 'Input' && $Param{UserEmail} && $Entry->[0] eq 'UserEmail' ) {
 
                 # display server error msg according with the occurred email error type
                 $Self->{LayoutObject}->Block(
-                    Name => 'PreferencesUserEmail' . $Param{Errors}->{ErrorType} . 'Msg',
+                    Name => 'PreferencesUserEmail' . $Param{Errors}->{ErrorType} . 'ServerErrorMsg',
                     Data => { Name => $Entry->[0] },
                 );
             }
