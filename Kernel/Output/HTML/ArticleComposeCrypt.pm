7431f9746389e88e109215acd94a8dca071f48a0
Daniel Zamorano <daniel.zamorano@otrs.com>
2010-11-23 20:42:30 +0000

    bug fix#6131: Lack of warning for revoked and expired PGP keys  in email compose screens.

--- Kernel/Output/HTML/ArticleComposeCrypt.pm
+++ Kernel/Output/HTML/ArticleComposeCrypt.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Output/HTML/ArticleComposeCrypt.pm
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: ArticleComposeCrypt.pm,v 1.19 2009-12-23 22:25:41 martin Exp $
+# $Id: ArticleComposeCrypt.pm,v 1.20 2010-11-23 20:42:30 dz Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Mail::Address;
 use Kernel::System::Crypt;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.19 $) [1];
+$VERSION = qw($Revision: 1.20 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -134,13 +134,23 @@ sub Data {
                     $Expires = "[$DataRef->{Expires}]";
                 }
 
+                my $Status = '';
+                $Status = '[' . $DataRef->{Status} . ']';
+                if ( $DataRef->{Status} eq 'expired' ) {
+                    $Status = '[WARNING: EXPIRED KEY]';
+                }
+                elsif ( $DataRef->{Status} eq 'revoked' ) {
+                    $Status = '[WARNING: REVOKED KEY]';
+                }
+
                 # disable inline pgp if rich text is enabled
                 if ( !$Self->{LayoutObject}->{BrowserRichText} ) {
                     $KeyList{"PGP::Inline::$DataRef->{Key}"}
-                        = "PGP-Inline: $DataRef->{Key} $Expires $DataRef->{Identifier}";
+                        = "PGP-Inline: $Status $DataRef->{Key} $Expires $DataRef->{Identifier}";
                 }
+
                 $KeyList{"PGP::Detached::$DataRef->{Key}"}
-                    = "PGP-Detached: $DataRef->{Key} $Expires $DataRef->{Identifier}";
+                    = "PGP-Detached: $Status $DataRef->{Key} $Expires $DataRef->{Identifier}";
             }
         }
 
