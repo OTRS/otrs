38f63ce8767f8255c65628b18cb1b69726c84782
Martin Gruner <martin.gruner@otrs.com>
2010-11-24 11:13:43 +0000

    Added new Japanese translation file, thanks to Kaz Kamimura Thomas!

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.372 2010-11-18 11:14:05 mg Exp $
+# $Id: Defaults.pm,v 1.373 2010-11-24 11:13:43 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -25,7 +25,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.372 $) [1];
+$VERSION = qw($Revision: 1.373 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 use File::Basename;
@@ -173,6 +173,7 @@ sub LoadDefaults {
         fi      => 'Suomi',
         hu      => 'Magyar',
         it      => 'Italiano',
+        ja      => 'Japanese (&#x65e5;&#x672c;&#x8a9e)',
         lv      => 'Latvijas',
         nl      => 'Nederlands',
         nb_NO   => 'Norsk bokm&aring;l',
@@ -2223,6 +2224,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.372 $ $Date: 2010-11-18 11:14:05 $
+$Revision: 1.373 $ $Date: 2010-11-24 11:13:43 $
 
 =cut
