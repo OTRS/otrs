5bd061ce132ae799d0531e9a15f463679e7d871d
Elva Novoa <elva.novoa@otrs.com>
2010-11-25 18:13:54 +0000

    Reverted last change, but added a new parameter to choose the size of the subject to be displayed.

--- Kernel/Modules/CustomerTicketZoom.pm
+++ Kernel/Modules/CustomerTicketZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/CustomerTicketZoom.pm - to get a closer view
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: CustomerTicketZoom.pm,v 1.72 2010-11-24 19:42:13 en Exp $
+# $Id: CustomerTicketZoom.pm,v 1.73 2010-11-25 18:13:54 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::Web::UploadCache;
 use Kernel::System::State;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.72 $) [1];
+$VERSION = qw($Revision: 1.73 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -536,6 +536,12 @@ sub _Mask {
         $Article{Age} = $Self->{LayoutObject}->CustomerAge(
             Age   => $Article{AgeTimeUnix},
             Space => ' ',
+        );
+
+        $Article{Subject} = $Self->{TicketObject}->TicketSubjectClean(
+            TicketNumber => $Article{TicketNumber},
+            Subject      => $Article{Subject} || '',
+            Size         => 150,
         );
 
         $LastSenderType = $Article{SenderType};
