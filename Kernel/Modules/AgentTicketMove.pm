dd8860baffd1b09efc47aaacaa8c2bacf6573b4a
Michiel Beijen <michiel.beijen@otrs.com>
2010-11-29 08:12:04 +0000

    Fixed bug#5981 - Warnings from TransfromDateSelection() in AgentTicketMove. This fix was already applied to the 3.0 branch. http://bugs.otrs.org/show_bug.cgi?id=5981

--- Kernel/Modules/AgentTicketMove.pm
+++ Kernel/Modules/AgentTicketMove.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketMove.pm - move tickets to queues
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketMove.pm,v 1.37.2.9 2010-09-24 08:19:33 ep Exp $
+# $Id: AgentTicketMove.pm,v 1.37.2.10 2010-11-29 08:12:04 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::State;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.37.2.9 $) [1];
+$VERSION = qw($Revision: 1.37.2.10 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -117,7 +117,7 @@ sub Run {
         $GetParam{$Parameter} = $Self->{ParamObject}->GetParam( Param => $Parameter ) || '';
     }
     for my $Parameter (qw(Year Month Day Hour Minute)) {
-        $GetParam{$Parameter} = $Self->{ParamObject}->GetParam( Param => $Parameter ) || 0;
+        $GetParam{$Parameter} = $Self->{ParamObject}->GetParam( Param => $Parameter );
     }
 
     # get ticket free text params
