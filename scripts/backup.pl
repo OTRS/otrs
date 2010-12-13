aa6a6fd1c64923ba470bb75f9242f6056a54a187
Martin Gruner <martin.gruner@otrs.com>
2010-12-13 10:26:53 +0000

    fixed - bug 6520: backup.pl doesn't backup with strong password http://bugs.otrs.org/show_bug.cgi?id=6520

--- scripts/backup.pl
+++ scripts/backup.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
 # scripts/backup.pl - the backup script
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: backup.pl,v 1.17 2009-04-16 11:20:40 tr Exp $
+# $Id: backup.pl,v 1.17.2.1 2010-12-13 10:26:53 mg Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.17 $) [1];
+$VERSION = qw($Revision: 1.17.2.1 $) [1];
 
 use Getopt::Std;
 use Kernel::Config;
@@ -52,7 +52,7 @@ my $DBDump      = '';
 getopt( 'hcrtd', \%Opts );
 if ( $Opts{'h'} ) {
     print "backup.pl <Revision $VERSION> - backup script\n";
-    print "Copyright (C) 2001-2009 OTRS AG, http://otrs.org/\n";
+    print "Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
     print
         "usage: backup.pl -d /data_backup_dir/ [-c gzip|bzip2] [-r 30] [-t fullbackup|nofullbackup]\n";
     exit 1;
@@ -250,7 +250,7 @@ if ( $DB =~ /mysql/i ) {
     }
     if (
         !system(
-            "$DBDump -u $DatabaseUser $DatabasePw -h $DatabaseHost $Database > $Directory/DatabaseBackup.sql"
+            "$DBDump -u $DatabaseUser '$DatabasePw' -h $DatabaseHost $Database > $Directory/DatabaseBackup.sql"
         )
         )
     {
