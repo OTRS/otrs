e572af85a28fa47be1569e1e62cd2129d55a89f6
Martha Pascual <martha.pascual@otrs.com>
2011-01-17 18:50:05 +0000

    Fixed bug#6599 Ticket sub-sorting in dashlets isn't working

--- Kernel/Output/HTML/DashboardTicketGeneric.pm
+++ Kernel/Output/HTML/DashboardTicketGeneric.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Output/HTML/DashboardTicketGeneric.pm
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: DashboardTicketGeneric.pm,v 1.37 2010-12-07 17:01:21 martin Exp $
+# $Id: DashboardTicketGeneric.pm,v 1.38 2011-01-17 18:47:59 mp Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.37 $) [1];
+$VERSION = qw($Revision: 1.38 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -147,6 +147,7 @@ sub Run {
         elsif ( !ref $TicketSearch{$Key} ) {
             my $ValueTmp = $TicketSearch{$Key};
             $TicketSearch{$Key} = [$ValueTmp];
+            push @{ $TicketSearch{$Key} }, $Value;
         }
         else {
             push @{ $TicketSearch{$Key} }, $Value;
