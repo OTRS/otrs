5d1a01494777d3b39c0b0b3ebd2f709c5d42ff5d
Udo Bretz <udo.bretz@otrs.com>
2011-01-24 17:20:51 +0000

    Fixed error for that caused a problem with RTL language files.

--- bin/otrs.CreateTranslationFile.pl
+++ bin/otrs.CreateTranslationFile.pl
@@ -3,7 +3,7 @@
 # bin/otrs.CreateTranslationFile.pl - create new translation file
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: otrs.CreateTranslationFile.pl,v 1.25 2011-01-12 08:54:08 mg Exp $
+# $Id: otrs.CreateTranslationFile.pl,v 1.26 2011-01-24 17:20:51 ub Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -30,7 +30,7 @@ use FindBin qw($RealBin);
 use lib dirname($RealBin);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.25 $) [1];
+$VERSION = qw($Revision: 1.26 $) [1];
 
 use Getopt::Std qw();
 use Kernel::Config;
@@ -436,7 +436,7 @@ EOF
 
                 if ( $LanguageCoreObject->{TextDirection} ) {
                     $NewOut .= <<"EOF";
-    # TextDirection rtl or ltr\n";
+    # TextDirection rtl or ltr
     \$Self->{TextDirection} = '$LanguageCoreObject->{TextDirection}';
 
 EOF
