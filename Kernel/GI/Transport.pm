deaefd87d354290ab68b37bcbe91d804905fff7d
Martin Gruner <martin.gruner@otrs.com>
2011-02-02 14:38:52 +0000

    Added another stub.

---
+++ Kernel/GI/Transport.pm
@@ -0,0 +1,221 @@
+# --
+# Kernel/GI/Transport.pm - GenericInterface network transport interface
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: Transport.pm,v 1.1 2011-02-02 14:38:52 mg Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::GI::Transport;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::GI::Transport
+
+=head1 SYNOPSIS
+
+GenericInterface network transport interface.
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object. This will return the transport backend for the current web service.
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::Time;
+    use Kernel::System::Main;
+    use Kernel::System::DB;
+    use Kernel::GI::Transport;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $TimeObject = Kernel::System::Time->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $TransportObject = Kernel::GI::Transport->new(
+        ConfigObject       => $ConfigObject,
+        LogObject          => $LogObject,
+        DBObject           => $DBObject,
+        MainObject         => $MainObject,
+        TimeObject         => $TimeObject,
+        EncodeObject       => $EncodeObject,
+
+        TransportConfig   => {
+            Type => 'HTTP::SOAP',
+            ...
+        },
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    #my $Self = {};
+    #bless( $Self, $Type );
+
+    # check needed objects
+    #for (qw(MainObject ConfigObject LogObject EncodeObject TimeObject DBObject)) {
+    #    $Self->{$_} = $Param{$_} || die "Got no $_!";
+    #}
+
+    # TODO: implement backend loading and returning
+
+    return;
+}
+
+=item ProviderProcessRequest()
+
+process an incoming web service request.
+
+    my $Result = $TransportObject->ProviderProcessRequest(
+        $RequestContent => $Content,    # complete request, such as HTTP request
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Operation       => 'DesiredOperation',  # name of the operation to perform
+        Data            => {                    # data payload of request
+            ...
+        },
+    };
+
+=cut
+
+sub ProviderProcessRequest {
+    my ( $Self, %Param ) = @_;
+
+    #TODO: implement
+}
+
+=item ProviderGenerateResponse()
+
+generate response for an incoming web service request.
+
+    my $Result = $TransportObject->ProviderGenerateResponse(
+        Success         => 1,       # 1 or 0
+        ErrorMessage    => '',      # in case of an error
+        Data            => {        # data payload for response
+            ...
+        },
+
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Response        => $Response,           # complete response
+    };
+
+=cut
+
+sub ProviderGenerateResponse {
+    my ( $Self, %Param ) = @_;
+
+    #TODO: implement
+}
+
+=item RequesterGenerateRequest()
+
+generate an outgoing web service request.
+
+    my $Result = $TransportObject->RequesterGenerateRequest(
+        Operation       => 'remote_op', # name of remote operation to perform
+        Data            => {            # data payload for request
+            ...
+        },
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Request        => $Request,             # complete request
+    };
+
+=cut
+
+sub RequesterGenerateRequest {
+    my ( $Self, %Param ) = @_;
+
+    #TODO: implement
+}
+
+=item RequesterProcessResponse()
+
+process response of an incoming web service request.
+
+    my $Result = $TransportObject->RequesterProcessResponse(
+        $RequestContent => $Content,    # complete request, such as HTTP response
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Data            => {                    # data payload of response
+            ...
+        },
+    };
+
+=cut
+
+sub RequesterProcessResponse {
+    my ( $Self, %Param ) = @_;
+
+    #TODO: implement
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2011-02-02 14:38:52 $
+
+=cut
