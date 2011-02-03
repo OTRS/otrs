ff0176707fe7f02dae33b4af048c34539aeddb5a
Martin Gruner <martin.gruner@otrs.com>
2011-02-03 10:18:22 +0000

    Added POD.

--- Kernel/GI/Transport/HTTP/REST.pm
+++ Kernel/GI/Transport/HTTP/REST.pm
@@ -2,7 +2,7 @@
 # Kernel/GI/Transport/HTTP/REST.pm - GenericInterface network transport interface for HTTP::REST
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: REST.pm,v 1.1 2011-02-02 14:41:38 mg Exp $
+# $Id: REST.pm,v 1.2 2011-02-03 10:18:22 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,21 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
+
+=head1 NAME
+
+Kernel::GI::Transport::REST
+
+=head1 SYNOPSIS
+
+GenericInterface network transport interface for HTTP::REST
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -59,3 +73,21 @@ sub RequesterProcessResponse {
 }
 
 1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.2 $ $Date: 2011-02-03 10:18:22 $
+
+=cut
