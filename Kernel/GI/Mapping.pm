8ab7ed6813ff25711ffdde1af64dd78ac9fed2c9
Martin Gruner <martin.gruner@otrs.com>
2011-02-03 10:19:07 +0000

    Added new stubs for mapping.

---
+++ Kernel/GI/Mapping.pm
@@ -0,0 +1,146 @@
+# --
+# Kernel/GI/Mapping.pm - GenericInterface data mapping interface
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: Mapping.pm,v 1.1 2011-02-03 10:19:07 mg Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::GI::Mapping;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::GI::Mapping
+
+=head1 SYNOPSIS
+
+GenericInterface data mapping interface.
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object. This will return the Mapping backend for the current web service configuration.
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::Time;
+    use Kernel::System::Main;
+    use Kernel::System::DB;
+    use Kernel::GI::Mapping;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $TimeObject = Kernel::System::Time->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $MappingObject = Kernel::GI::Mapping->new(
+        ConfigObject       => $ConfigObject,
+        LogObject          => $LogObject,
+        DBObject           => $DBObject,
+        MainObject         => $MainObject,
+        TimeObject         => $TimeObject,
+        EncodeObject       => $EncodeObject,
+
+        MappingConfig   => {
+            Type => 'MappingSimple',
+            ...
+        },
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    #my $Self = {};
+    #bless( $Self, $Type );
+
+    # check needed objects
+    #for (qw(MainObject ConfigObject LogObject EncodeObject TimeObject DBObject)) {
+    #    $Self->{$_} = $Param{$_} || die "Got no $_!";
+    #}
+
+    # TODO: implement backend loading and returning
+
+    return;
+}
+
+=item Map()
+
+perform data mapping
+
+    my $Result = $MappingObject->Map(
+        Data => {                               # data payload before mapping
+            ...
+        },
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Data            => {                    # data payload of after mapping
+            ...
+        },
+    };
+
+=cut
+
+sub Map {
+    my ( $Self, %Param ) = @_;
+
+    #TODO: implement
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2011-02-03 10:19:07 $
+
+=cut
---
+++ Kernel/GI/Mapping/Simple.pm
@@ -0,0 +1,75 @@
+# --
+# Kernel/GI/Mapping/Simple.pm - GenericInterface simle data mapping backend
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: Simple.pm,v 1.1 2011-02-03 10:19:07 mg Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::GI::Mapping::Simple;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::GI::Mapping
+
+=head1 SYNOPSIS
+
+GenericInterface simple data mapping backend
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    #my $Self = {};
+    #bless( $Self, $Type );
+
+    # check needed objects
+    #for (qw(MainObject ConfigObject LogObject EncodeObject TimeObject DBObject)) {
+    #    $Self->{$_} = $Param{$_} || die "Got no $_!";
+    #}
+
+    # TODO: implement backend loading and returning
+
+    return;
+}
+
+sub Map {
+    my ( $Self, %Param ) = @_;
+
+    #TODO: implement
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2011-02-03 10:19:07 $
+
+=cut
