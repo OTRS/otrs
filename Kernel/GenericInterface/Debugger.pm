2d2901fa16c9f5e71738e61a1268900e17a9441a
Stefan Bedorf <stefan.bedorf@otrs.com>
2011-02-08 13:49:02 +0000

    Fixed typo in pod.

--- Kernel/GenericInterface/Debugger.pm
+++ Kernel/GenericInterface/Debugger.pm
@@ -2,7 +2,7 @@
 # Kernel/GenericInterface/Debugger.pm - GenericInterface data debugger interface
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Debugger.pm,v 1.2 2011-02-08 09:21:57 mg Exp $
+# $Id: Debugger.pm,v 1.3 2011-02-08 13:49:02 sb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 =head1 NAME
 
@@ -121,7 +121,7 @@ add one piece of data to the logging of this communication process.
     );
 
 Available debug levels are: 'debug', 'info', 'notice' and 'error'.
-Any messages with 'erorr' priority will also be written to Kernel::System::Log.
+Any messages with 'error' priority will also be written to Kernel::System::Log.
 
 =cut
 
@@ -160,6 +160,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.2 $ $Date: 2011-02-08 09:21:57 $
+$Revision: 1.3 $ $Date: 2011-02-08 13:49:02 $
 
 =cut
