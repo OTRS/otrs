8761b08fec16c9c273ebaad8c792fed4ce4e949e
Martin Gruner <martin.gruner@otrs.com>
2011-02-15 10:26:33 +0000

    Added a simple invoker test backend.

---
+++ Kernel/GenericInterface/Invoker/Test/TestSimple.pm
@@ -0,0 +1,151 @@
+# --
+# Kernel/GenericInterface/Invoker/TestSimple.pm - GenericInterface test data Invoker backend
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: TestSimple.pm,v 1.1 2011-02-15 10:26:33 mg Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::GenericInterface::Invoker::Test::TestSimple;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(IsString IsStringWithData);
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::GenericInterface::Invoker::Test::Test
+
+=head1 SYNOPSIS
+
+GenericInterface test Invoker backend
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::GenericInterface::Invoker->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed params
+    for my $Needed (qw(DebuggerObject MainObject TimeObject)) {
+        if ( !$Param{$Needed} ) {
+            return {
+                Success      => 0,
+                ErrorMessage => "Got no $Needed!"
+            };
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    return $Self;
+}
+
+=item PrepareRequest()
+
+prepare the invocation of the configured remote webservice.
+
+    my $Result = $InvokerObject->PrepareRequest(
+        Data => {                               # data payload
+            ...
+        },
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Data            => {                    # data payload after Invoker
+            ...
+        },
+    };
+
+=cut
+
+sub PrepareRequest {
+    my ( $Self, %Param ) = @_;
+
+    return {
+        Success => 1,
+        Data    => $Param{Data},
+    };
+}
+
+=item HandleResponse()
+
+handle response data of the configured remote webservice.
+
+    my $Result = $InvokerObject->HandleResponse(
+        ResponseSuccess      => 1,              # success status of the remote webservice
+        ResponseErrorMessage => '',             # in case of webservice error
+        Data => {                               # data payload
+            ...
+        },
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Data            => {                    # data payload after Invoker
+            ...
+        },
+    };
+
+=cut
+
+sub HandleResponse {
+    my ( $Self, %Param ) = @_;
+
+    # if there was an error in the response, forward it
+    if ( !$Param{ResponseSuccess} ) {
+        return {
+            Success      => 0,
+            ErrorMessage => $Param{ResponseErrorMessage},
+        };
+    }
+
+    return {
+        Success => 1,
+        Data    => $Param{Data},
+    };
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2011-02-15 10:26:33 $
+
+=cut
