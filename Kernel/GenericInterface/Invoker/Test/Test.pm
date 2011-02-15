53fa72d2438747cdf868a9afb94e8e68d9220ddc
Martin Gruner <martin.gruner@otrs.com>
2011-02-15 09:37:03 +0000

    Fix API.

--- Kernel/GenericInterface/Invoker/Test/Test.pm
+++ Kernel/GenericInterface/Invoker/Test/Test.pm
@@ -2,7 +2,7 @@
 # Kernel/GenericInterface/Invoker/Test.pm - GenericInterface test data Invoker backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Test.pm,v 1.8 2011-02-14 10:25:37 martin Exp $
+# $Id: Test.pm,v 1.9 2011-02-15 09:37:03 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::VariableCheck qw(IsString IsStringWithData);
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 =head1 NAME
 
@@ -193,16 +193,15 @@ sub HandleResponse {
     my ( $Self, %Param ) = @_;
 
     # if there was an error in the response, forward it
-    if ( !IsStringWithData( $Param{Data}->{ResponseSuccess} ) || !$Param{Data}->{ResponseSuccess} )
-    {
-        if ( !IsStringWithData( $Param{Data}->{ResponseErrorMessage} ) ) {
+    if ( !$Param{ResponseSuccess} ) {
+        if ( !IsStringWithData( $Param{ResponseErrorMessage} ) ) {
             return $Self->{DebuggerObject}->Error(
                 Summary => 'Got response error, but no response error message!',
             );
         }
         return {
             Success      => 0,
-            ErrorMessage => $Param{Data}->{ResponseErrorMessage},
+            ErrorMessage => $Param{ResponseErrorMessage},
         };
     }
 
@@ -270,6 +269,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.8 $ $Date: 2011-02-14 10:25:37 $
+$Revision: 1.9 $ $Date: 2011-02-15 09:37:03 $
 
 =cut
