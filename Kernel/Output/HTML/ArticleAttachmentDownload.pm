344beb450bf62f5bcccab9543a3d3ca1580e2a3c
Marc Nilius <marc.nilius@otrs.com>
2011-02-24 13:21:44 +0000

    Rollback of bug#6912 for OTRS 3.0.6.

--- Kernel/Output/HTML/ArticleAttachmentDownload.pm
+++ Kernel/Output/HTML/ArticleAttachmentDownload.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/ArticleAttachmentDownload.pm
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: ArticleAttachmentDownload.pm,v 1.12.4.1 2011-02-18 23:55:00 mp Exp $
+# $Id: ArticleAttachmentDownload.pm,v 1.12.4.2 2011-02-24 13:21:43 mn Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12.4.1 $) [1];
+$VERSION = qw($Revision: 1.12.4.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -59,8 +59,8 @@ sub Run {
         Action => 'Download',
         Link =>
             "\$Env{\"CGIHandle\"}/$EncodedFilename?Action=AgentTicketAttachment;ArticleID=$Param{Article}->{ArticleID};FileID=$Param{File}->{FileID}",
+        Image  => 'disk-s.png',
         Target => $Target,
-        Class  => 'DownloadAttachment',
     );
 }
 
