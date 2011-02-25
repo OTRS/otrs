3790107e481c8aba160e44b75af03529ed06e154
Elva Novoa <elva.novoa@otrs.com>
2011-02-25 19:26:38 +0000

    Fixed bug 6822. Underscore in username problem in 3.0.5. Fixed by escaping the underscore correctly: surrounding it with squared brackets.

--- Kernel/System/DB/mssql.pm
+++ Kernel/System/DB/mssql.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DB/mssql.pm - mssql database backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: mssql.pm,v 1.57 2011-02-25 07:52:57 martin Exp $
+# $Id: mssql.pm,v 1.58 2011-02-25 19:26:38 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.57 $) [1];
+$VERSION = qw($Revision: 1.58 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -31,14 +31,15 @@ sub LoadPreferences {
     my ( $Self, %Param ) = @_;
 
     # db settings
-    $Self->{'DB::Limit'}            = 0;
-    $Self->{'DB::DirectBlob'}       = 0;
-    $Self->{'DB::QuoteSingle'}      = '\'';
-    $Self->{'DB::QuoteBack'}        = 0;
-    $Self->{'DB::QuoteSemicolon'}   = '';
-    $Self->{'DB::QuoteUnderscore'}  = '\\';
-    $Self->{'DB::CaseInsensitive'}  = 1;
-    $Self->{'DB::LikeEscapeString'} = '';
+    $Self->{'DB::Limit'}                = 0;
+    $Self->{'DB::DirectBlob'}           = 0;
+    $Self->{'DB::QuoteSingle'}          = '\'';
+    $Self->{'DB::QuoteBack'}            = 0;
+    $Self->{'DB::QuoteSemicolon'}       = '';
+    $Self->{'DB::QuoteUnderscoreStart'} = '[';
+    $Self->{'DB::QuoteUnderscoreEnd'}   = ']';
+    $Self->{'DB::CaseInsensitive'}      = 1;
+    $Self->{'DB::LikeEscapeString'}     = '';
 
     # dbi attributes
     $Self->{'DB::Attribute'} = {
@@ -81,8 +82,9 @@ sub Quote {
         }
         if ( $Type && $Type eq 'Like' ) {
             ${$Text} =~ s/\[/[[]/g;
-            if ( $Self->{'DB::QuoteUnderscore'} ) {
-                ${$Text} =~ s/_/$Self->{'DB::QuoteUnderscore'}_/g;
+            if ( $Self->{'DB::QuoteUnderscoreStart'} || $Self->{'DB::QuoteUnderscoreEnd'} ) {
+                ${$Text}
+                    =~ s/_/$Self->{'DB::QuoteUnderscoreStart'}_$Self->{'DB::QuoteUnderscoreEnd'}/g;
             }
         }
     }
