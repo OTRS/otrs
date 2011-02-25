689fbbf5e5a3bf33b237b8bb212c7b637d60cf7f
Martin Gruner <martin.gruner@otrs.com>
2011-02-25 10:22:13 +0000

    Added Hindi translation, thanks to Chetan Nagaonkar!

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.377 2011-01-06 09:54:05 mn Exp $
+# $Id: Defaults.pm,v 1.377.2.1 2011-02-25 10:22:13 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -25,7 +25,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.377 $) [1];
+$VERSION = qw($Revision: 1.377.2.1 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 use File::Basename;
@@ -171,6 +171,7 @@ sub LoadDefaults {
         fa      => 'Persian (&#x0641;&#x0627;&#x0631;&#x0633;&#x0649;)',
         fr      => 'Fran&ccedil;ais',
         fi      => 'Suomi',
+        hi      => 'Hindi',
         hu      => 'Magyar',
         it      => 'Italiano',
         ja      => 'Japanese (&#x65e5;&#x672c;&#x8a9e)',
@@ -2226,6 +2227,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.377 $ $Date: 2011-01-06 09:54:05 $
+$Revision: 1.377.2.1 $ $Date: 2011-02-25 10:22:13 $
 
 =cut
