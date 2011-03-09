b33650d1fe409eb717c83e0ec800701c052de447
Dennis Schmelter <dennis.schmelter@otrs.com>
2011-03-09 13:02:21 +0000

    added =head1 NAME

--- Kernel/System/Stats.pm
+++ Kernel/System/Stats.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Stats.pm - all stats core functions
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Stats.pm,v 1.101 2011-02-09 11:30:19 mb Exp $
+# $Id: Stats.pm,v 1.102 2011-03-09 13:02:21 des Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,11 @@ use Date::Pcalc qw(:all);
 use Kernel::System::XML;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.101 $) [1];
+$VERSION = qw($Revision: 1.102 $) [1];
+
+=head1 NAME
+
+Kernel::System::Stats - stats lib
 
 =head1 SYNOPSIS
 
@@ -3300,6 +3304,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.101 $ $Date: 2011-02-09 11:30:19 $
+$Revision: 1.102 $ $Date: 2011-03-09 13:02:21 $
 
 =cut
