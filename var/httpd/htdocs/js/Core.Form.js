4f7d491959eabcc374afb060b0125cd9df958e46
Marc Nilius <marc.nilius@otrs.com>
2011-03-10 10:56:30 +0000

    Fixed bug#6946: SelectAllXXX checkbox is not checked when you first enter to the module.

--- var/httpd/htdocs/js/Core.Form.js
+++ var/httpd/htdocs/js/Core.Form.js
@@ -1,8 +1,8 @@
 // --
 // Core.Form.js - provides functions for form handling
-// Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
+// Copyright (C) 2001-2011 OTRS AG, http://otrs.org/\n";
 // --
-// $Id: Core.Form.js,v 1.9 2010-08-12 13:46:08 mg Exp $
+// $Id: Core.Form.js,v 1.9.2.1 2011-03-10 10:55:10 mn Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
@@ -160,7 +160,7 @@ Core.Form = (function (TargetNS) {
      */
     TargetNS.InitSelectAllCheckboxes = function ($Checkboxes, $SelectAllCheckbox) {
         if (isJQueryObject($Checkboxes, $SelectAllCheckbox)) {
-            if ($Checkboxes.length === $Checkboxes.filter(':checked').length) {
+            if ($Checkboxes.filter('[id!=' + $SelectAllCheckbox.attr('id') + ']').length === $Checkboxes.filter(':checked').length) {
                 $SelectAllCheckbox.attr('checked', 'checked');
             }
         }
