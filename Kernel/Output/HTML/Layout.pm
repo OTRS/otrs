1cfb4b0ae4efbaa209dadbb674234d83d8ad3b78
Martin Gruner <martin.gruner@otrs.com>
2011-03-16 11:40:15 +0000

    Improved parameter quoting on error and warning pages.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Layout.pm,v 1.351.2.3 2011-03-08 13:58:54 mb Exp $
+# $Id: Layout.pm,v 1.351.2.4 2011-03-16 11:40:15 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::JSON;
 use Mail::Address;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.351.2.3 $) [1];
+$VERSION = qw($Revision: 1.351.2.4 $) [1];
 
 =head1 NAME
 
@@ -1113,10 +1113,6 @@ sub Error {
             Type => 'Error',
             What => $_
         ) || '';
-        $Param{$Backend} = $Self->Ascii2Html(
-            Text           => $Param{$Backend},
-            HTMLResultMode => 1,
-        );
     }
     if ( !$Param{BackendMessage} && !$Param{BackendTraceback} ) {
         $Self->{LogObject}->Log(
@@ -1129,10 +1125,6 @@ sub Error {
                 Type => 'Error',
                 What => $_
             ) || '';
-            $Param{$Backend} = $Self->Ascii2Html(
-                Text           => $Param{$Backend},
-                HTMLResultMode => 1,
-            );
         }
     }
     if ( !$Param{Message} ) {
@@ -1163,10 +1155,6 @@ sub Warning {
         Type => 'Error',
         What => 'Message',
         ) || '';
-    $Param{BackendMessage} = $Self->Ascii2Html(
-        Text           => $Param{BackendMessage},
-        HTMLResultMode => 1,
-    );
 
     if ( !$Param{Message} ) {
         $Param{Message} = $Param{BackendMessage};
@@ -4900,6 +4888,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.351.2.3 $ $Date: 2011-03-08 13:58:54 $
+$Revision: 1.351.2.4 $ $Date: 2011-03-16 11:40:15 $
 
 =cut
