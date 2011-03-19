2ffc9bd7f4d5fc2fa8fd8f57b1b91d1bb273027a
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-03-19 15:55:47 +0000

    Initial Version for GI 05.5 To perform common actions that are repeated in several SolMan Invokers

---
+++ Kernel/GenericInterface/Invoker/SolMan/SolManCommon.pm
@@ -0,0 +1,366 @@
+# --
+# Kernel/GenericInterface/Operation/SolManCommon.pm - SolMan common invoker functions
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: SolManCommon.pm,v 1.1 2011-03-19 15:55:47 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::GenericInterface::Invoker::SolMan::SolManCommon;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::GenericInterface::Operation::Common - common operation functions
+
+=head1 SYNOPSIS
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::Time;
+    use Kernel::System::Main;
+    use Kernel::System::DB;
+    use Kernel::GenericInterface::Invoker::SolMan::SolManCommon;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $TimeObject = Kernel::System::Time->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $SolManCommonObject = Kernel::GenericInterface::Invoker::SolMan::SolManCommon->new(
+        ConfigObject       => $ConfigObject,
+        LogObject          => $LogObject,
+        DBObject           => $DBObject,
+        MainObject         => $MainObject,
+        TimeObject         => $TimeObject,
+        EncodeObject       => $EncodeObject,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (qw(DebuggerObject MainObject TimeObject)) {
+        if ( !$Param{$Needed} ) {
+            return {
+                Success      => 0,
+                ErrorMessage => "Got no $Needed!"
+            };
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    return $Self;
+}
+
+=item HandleErrors()
+
+    my $Result = $SolManCommonObject->HandleErrors(
+        Errors     => {
+            item => {
+                ErrorCode => '01'
+                Val1      =>  '[SystemErr]',
+                Val2      =>  'Error Detail 1',
+                Val3      =>  'Error Detail 2',
+                Val4      =>  'Error Detail 3',
+            }
+        }
+    );
+
+    my $Result = $SolManCommonObject->HandleErrors(
+        Errors     => {
+            item => [
+                {
+                    ErrorCode => '01'
+                    Val1      =>  'Error Description',
+                    Val2      =>  'Error Detail 1',
+                    Val3      =>  'Error Detail 2',
+                    Val4      =>  'Error Detail 3',
+                },
+                {
+                    ErrorCode => '04'
+                    Val1      =>  'Error Description',
+                    Val2      =>  'Error Detail 1',
+                    Val3      =>  'Error Detail 2',
+                    Val4      =>  'Error Detail 3',
+                },
+            ],
+        }
+    );
+
+    $Result = {
+        Success      => 1,             # 1 or 0
+        ErrorMessage => 'Error 01 System Error Details: Error Detail 1 Error Detail 2 Error Detail 1 | .',
+    };
+
+=cut
+
+sub HandleErrors {
+    my ( $Self, %Param ) = @_;
+
+    # check for needed objects
+    for my $Needed (qw(Errors Invoker)) {
+        if ( !$Param{$Needed} ) {
+            return {
+                Success      => 0,
+                ErrorMessage => "Can't handle errors Got no $Needed!"
+            };
+        }
+    }
+
+    # to store the error message(s)
+    my $ErrorMessage;
+
+    # to store each error item
+    my @ErrorItems;
+
+    # check for multimple errors
+    if ( IsArrayRefWithData( $Param{Errors}->{item} ) ) {
+
+        # get all errors
+        for my $Item ( @{ $Param{Errors}->{Item} } ) {
+            if ( IsHashRefWithData($Item) ) {
+                push @ErrorItems, $Item;
+            }
+        }
+    }
+
+    # only one error
+    elsif ( IsHashRefWithData( $Param{Errors}->{item} ) ) {
+        push @ErrorItems, $Param{Errors}->{item};
+    }
+
+    if ( scalar @ErrorItems gt 0 ) {
+
+        # cicle trough all error items
+        for my $Item (@ErrorItems) {
+
+            # check error code
+            if ( IsStringWithData( $Item->{ErrorCode} ) ) {
+                $ErrorMessage .= "Error Code $Item->{ErrorCode} ";
+            }
+            else {
+                $ErrorMessage .= 'An error message was received but no Error Code found! ';
+            }
+
+            # set the erros description
+            if ( IsStringWithData( $Item->{Val1} ) ) {
+                $ErrorMessage .= "$Item->{Val1} ";
+            }
+            $ErrorMessage .= 'Details: ';
+
+            # cicle trough all details
+            for my $Val qw(Val2 Val2 Val3) {
+                if ( IsStringWithData( $Item->{"$Val"} ) ) {
+                    $ErrorMessage .= "$Item->{$Val}  ";
+                }
+            }
+
+            $ErrorMessage .= " | ";
+        }
+    }
+    else {
+        $ErrorMessage = "An Error was returned from remote server but can't get any data";
+    }
+
+    # write in debug log
+    $Self->{DebuggerObject}->Error(
+        Summary => "$Param{Invoker} return error",
+        Data    => $ErrorMessage,
+    );
+
+    return {
+        Success      => 1,
+        ErrorMessage => $ErrorMessage,
+    };
+
+}
+
+=item HandlePersonMaps()
+
+    my $Result = $SolManCommonObject->HandlePersonMaps(
+        Errors     => {
+            item => {
+                PersonId    => '0001',
+                PersonIdExt => '5050',
+            }
+        }
+    );
+
+    my $Result = $SolManCommonObject->HandlePersonMaps(
+        PersonMaps     => {
+            item => [
+                {
+                    PersonId    => '0001',
+                    PersonIdExt => '5050',
+                },
+                {
+                    PersonId    => '0002',
+                    PersonIdExt => '5051',
+                },
+            ],
+        }
+    );
+
+    $Result = {
+        Success      => 1,                # 1 or 0
+        ErrorMessage => ' ',              # optional if there is an error hadling the person maps
+        PersonMap    => [
+                {
+                    PersonId    => '0001',
+                    PersonIdExt => '5050',
+                },
+                {
+                    PersonId    => '0002',
+                    PersonIdExt => '5051',
+                },
+        ],
+    };
+=cut
+
+sub HandlePersonMaps {
+    my ( $Self, %Param ) = @_;
+
+    # check for needed objects
+    for my $Needed (qw(PersonMaps Invoker)) {
+        if ( !$Param{$Needed} ) {
+            return {
+                Success      => 0,
+                ErrorMessage => "Can't handle person maps Got no $Needed!"
+            };
+        }
+    }
+
+    # to store each preson map item
+    my @PersonMaps;
+
+    # to store possible errors
+    my $ErrorMessage;
+
+    # check for multimple person maps
+    if ( IsArrayRefWithData( $Param{PersonMaps}->{item} ) ) {
+
+        # get all person maps
+        for my $Item ( @{ $Param{PersonMaps}->{Item} } ) {
+            if ( IsHashRefWithData($Item) ) {
+
+                # check for valid data
+                if (
+                    !IsStringWithData( $Item->{PersonId} )
+                    || !IsStringWithData( $Item->{PersonIdExt} )
+                    )
+                {
+
+                    $ErrorMessage = 'PersonId or PersonIdExt is empty';
+
+                    # write in debug log
+                    $Self->{DebuggerObject}->Error(
+                        Summary => "$Param{Invoker} return error",
+                        Data    => $ErrorMessage,
+                    );
+
+                    return {
+                        Success      => 0,
+                        ErrorMessage => $ErrorMessage,
+                    };
+                }
+                push @PersonMaps, $Item;
+            }
+        }
+    }
+
+    # only one person map
+    elsif ( IsHashRefWithData( $Param{Errors}->{item} ) ) {
+
+        # check for valid data
+        if (
+            !IsHashRefWithData( $Param{Errors}->{item}->{PersonId} )
+            || !IsHashRefWithData( $Param{Errors}->{item}->{PersonIdExt} )
+            )
+        {
+
+            $ErrorMessage = 'PersonId or PersonIdExt is empty';
+
+            # write in debug log
+            $Self->{DebuggerObject}->Error(
+                Summary => "$Param{Invoker} return error",
+                Data    => $ErrorMessage,
+            );
+
+            return {
+                Success      => 0,
+                ErrorMessage => $ErrorMessage,
+            };
+        }
+        push @PersonMaps, $Param{Errors}->{item};
+    }
+
+    return {
+        Success    => 1,
+        PersonMaps => \@PersonMaps,
+    };
+}
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2011-03-19 15:55:47 $
+
+=cut
