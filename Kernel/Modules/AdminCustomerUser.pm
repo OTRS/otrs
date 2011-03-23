231a809cb2e186d00edae3d1379cc98767290979
Martha Pascual <martha.pascual@otrs.com>
2011-03-23 21:49:01 +0000

    QA. Bug 6710 Restored creation of the select option to choose the backed  to create a customer.

--- Kernel/Modules/AdminCustomerUser.pm
+++ Kernel/Modules/AdminCustomerUser.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminCustomerUser.pm - to add/update/delete customer user and preferences
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminCustomerUser.pm,v 1.89.2.4 2011-03-09 22:10:53 en Exp $
+# $Id: AdminCustomerUser.pm,v 1.89.2.5 2011-03-23 21:49:01 mp Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::Valid;
 use Kernel::System::CheckItem;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.89.2.4 $) [1];
+$VERSION = qw($Revision: 1.89.2.5 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -501,6 +501,13 @@ sub Run {
 
 sub _Overview {
     my ( $Self, %Param ) = @_;
+
+    # build source string
+    $Param{SourceOption} = $Self->{LayoutObject}->BuildSelection(
+        Data       => { $Self->{CustomerUserObject}->CustomerSourceList() },
+        Name       => 'Source',
+        SelectedID => $Param{Source} || '',
+    );
 
     $Self->{LayoutObject}->Block(
         Name => 'Overview',
