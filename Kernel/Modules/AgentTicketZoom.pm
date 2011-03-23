dbfe5134543160503783beba3c442d2fb73b8fc8
Martha Pascual <martha.pascual@otrs.com>
2011-03-23 16:44:40 +0000

    Rollback changes for bug 6912, for maintain the original version for the next release, and after modify the way it create links for attachments.

--- Kernel/Modules/AgentTicketZoom.pm
+++ Kernel/Modules/AgentTicketZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketZoom.pm - to get a closer view
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketZoom.pm,v 1.145.2.2 2011-03-10 00:08:11 mp Exp $
+# $Id: AgentTicketZoom.pm,v 1.145.2.3 2011-03-23 16:44:40 mp Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::EmailParser;
 use Kernel::System::SystemAddress;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.145.2.2 $) [1];
+$VERSION = qw($Revision: 1.145.2.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1760,9 +1760,6 @@ sub _ArticleItem {
                     Article => \%Article,
                 );
                 next JOB if !%Data;
-                if ( $Job eq '1-Download' ) {
-                    $Data{DataFileSize} = ", " . $Data{Filesize};
-                }
                 $Self->{LayoutObject}->Block(
                     Name => $Data{Block} || 'ArticleAttachmentRowLink',
                     Data => {%Data},
