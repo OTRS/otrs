68712c7c6f4112c8d58045bf93e77d3a7c4b2c85
Martin Gruner <martin.gruner@otrs.com>
2011-03-25 10:25:49 +0000

    Updated jquery to 1.5.1 and jquery.validator to 1.8.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.380 2011-03-11 13:28:54 mg Exp $
+# $Id: Defaults.pm,v 1.381 2011-03-25 10:25:49 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -29,7 +29,7 @@ use warnings;
 use 5.008_006;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.380 $) [1];
+$VERSION = qw($Revision: 1.381 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 use File::Basename;
@@ -910,7 +910,7 @@ sub LoadDefaults {
 
     # Customer Common JS
     $Self->{'Loader::Customer::CommonJS'}->{'000-Framework'} =  [
-      'thirdparty/jquery-1.4.4/jquery.js',
+      'thirdparty/jquery-1.5.1/jquery.js',
       'thirdparty/stacktrace/stacktrace.js',
       'Core.Debug.js',
       'Core.Exception.js',
@@ -924,9 +924,9 @@ sub LoadDefaults {
     # Agent Common JS
     $Self->{'Loader::Agent::CommonJS'}->{'000-Framework'} =  [
       'thirdparty/json/json2.js',
-      'thirdparty/jquery-1.4.4/jquery.js',
+      'thirdparty/jquery-1.5.1/jquery.js',
       'thirdparty/jquery-ui-1.8.7/jquery-ui.js',
-      'thirdparty/jquery-validate-1.7/jquery.validate.js',
+      'thirdparty/jquery-validate-1.8/jquery.validate.js',
       'thirdparty/stacktrace/stacktrace.js',
       'Core.JavaScriptEnhancements.js',
       'Core.Debug.js',
@@ -2218,6 +2218,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.380 $ $Date: 2011-03-11 13:28:54 $
+$Revision: 1.381 $ $Date: 2011-03-25 10:25:49 $
 
 =cut
