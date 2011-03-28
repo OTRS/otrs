8c3a50d6d891845b98ac71cf4223b2a7c50cecc4
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-03-28 16:55:36 +0000

    Fixed customer data on the request

--- Kernel/GenericInterface/Invoker/SolMan/ReplicateIncident.pm
+++ Kernel/GenericInterface/Invoker/SolMan/ReplicateIncident.pm
@@ -2,7 +2,7 @@
 # Kernel/GenericInterface/Invoker/SolMan/ReplicateIncident.pm - GenericInterface SolMan ReplicateIncident Invoker backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: ReplicateIncident.pm,v 1.18 2011-03-28 16:20:09 cr Exp $
+# $Id: ReplicateIncident.pm,v 1.19 2011-03-28 16:55:36 cr Exp $
 # $OldId: ReplicateIncident.pm,v 1.7 2011/03/24 06:06:29 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
@@ -23,7 +23,7 @@ use Kernel::System::User;
 use MIME::Base64;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.18 $) [1];
+$VERSION = qw($Revision: 1.19 $) [1];
 
 =head1 NAME
 
@@ -193,8 +193,7 @@ sub PrepareRequest {
         User => $Ticket{CustomerUserID},
     );
     my %IctCustomerUser = (
-        PersonId    => $CustomerUser{CustomerUserID} || '',    # type="n0:char32"
-#        PersonIdExt => $CustomerUser{CustomerID} || '',        # type="n0:char32"
+        PersonId    => $CustomerUser{UserEmail} || '',    # type="n0:char32"
         PersonIdExt => '',                                     # type="n0:char32"
         Sex         => '',                                     # type="n0:char1"
         FirstName   => $CustomerUser{UserFirstname} || '',     # type="n0:char40"
@@ -339,7 +338,7 @@ sub PrepareRequest {
             RequesterGuid    => $LocalSystemGuid,                # type="n0:char32"
             ProviderGuid     => $RemoteSystemGuid,               # type="n0:char32"
             AgentId          => $Ticket{OwnerID},                # type="n0:char32"
-            ReporterId       => $Ticket{CustomerID},             # type="n0:char32"
+            ReporterId       => $CustomerUser{UserEmail},        # type="n0:char32"
             ShortDescription => substr( $Ticket{Title}, 0, 40 ), # type="n0:char40"
             Priority         => $Ticket{PriorityID},             # type="n0:char32"
             Language         => $Language,                       # type="n0:char2"
@@ -546,6 +545,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.18 $ $Date: 2011-03-28 16:20:09 $
+$Revision: 1.19 $ $Date: 2011-03-28 16:55:36 $
 
 =cut
