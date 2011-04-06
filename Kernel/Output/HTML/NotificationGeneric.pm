d346d1cd7744e0ec15e6e839002290a5c41e423f
Manuel Hecht <manuel.hecht@otrs.com>
2011-04-06 10:56:21 +0000

    Added generic notification module to show messages in the agent interface.

---
+++ Kernel/Output/HTML/NotificationGeneric.pm
@@ -0,0 +1,84 @@
+# --
+# Kernel/Output/HTML/NotificationGeneric.pm
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: NotificationGeneric.pm,v 1.1 2011-04-06 10:56:21 mh Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::NotificationGeneric;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(ConfigObject LogObject DBObject LayoutObject TimeObject MainObject UserID)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # define default value
+    my %Arguments = (
+        Priority => 'Warning',
+    );
+
+    # switch to priotity error
+    if ( $Param{Config}->{Priority} && $Param{Config}->{Priority} eq 'Error' ) {
+        $Arguments{Priority} = 'Error';
+    }
+
+    if ( $Param{Config}->{Text} ) {
+        $Arguments{Info} = $Param{Config}->{Text};
+    }
+    elsif ( $Param{Config}->{File} ) {
+
+        $Param{Config}->{File} =~ s{<OTRS_CONFIG_(.+?)>}{$Self->{ConfigObject}->Get($1)}egx;
+
+        return '' if !-e $Param{Config}->{File};
+
+        # try to read the file
+        my $FileContent = $Self->{MainObject}->FileRead(
+            Location => $Param{Config}->{File},
+            Mode     => 'utf8',
+            Type     => 'Local',
+            Result   => 'SCALAR',
+        );
+
+        return '' if !$FileContent;
+        return '' if ref $FileContent ne 'SCALAR';
+
+        $Arguments{Info} = ${$FileContent};
+    }
+    else {
+        return '';
+    }
+
+    # add link if available
+    if ( $Param{Config}->{Link} ) {
+        $Arguments{Link} = $Param{Config}->{Link};
+    }
+
+    return '' if !$Arguments{Info};
+
+    return $Self->{LayoutObject}->Notify(%Arguments);
+}
+
+1;
