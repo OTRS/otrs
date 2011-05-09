23cc722459fd2bc02bf5d48e80f34b3bb215aabc
Michiel Beijen <michiel.beijen@otrs.com>
2011-05-09 18:04:54 +0000

    Charset to ISO-8859-2.

--- Kernel/Language/hr.pm
+++ Kernel/Language/hr.pm
@@ -3,7 +3,7 @@
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # Copyright (C) 2011 Damir Dzeko <damir punkt dzeko masterspace inet.hr>
 # --
-# $Id: hr.pm,v 1.2 2011-05-09 18:00:03 mb Exp $
+# $Id: hr.pm,v 1.3 2011-05-09 18:04:54 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub Data {
     my $Self = shift;
