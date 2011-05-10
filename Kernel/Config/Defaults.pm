09bc11c9a67fbe93905adcb3cc1806f5450d2bdb
Michiel Beijen <michiel.beijen@otrs.com>
2011-05-10 07:38:43 +0000

    Restored 'hu'.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.377.2.3 2011-05-09 17:57:03 mb Exp $
+# $Id: Defaults.pm,v 1.377.2.4 2011-05-10 07:38:43 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -29,7 +29,7 @@ use warnings;
 use 5.008_006;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.377.2.3 $) [1];
+$VERSION = qw($Revision: 1.377.2.4 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 use File::Basename;
@@ -177,6 +177,7 @@ sub LoadDefaults {
         fi      => 'Suomi',
         hi      => 'Hindi',
         hr      => 'Hrvatski',
+        hu      => 'Magyar',
         it      => 'Italiano',
         ja      => 'Japanese (&#x65e5;&#x672c;&#x8a9e)',
         lv      => 'Latvijas',
@@ -2217,6 +2218,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.377.2.3 $ $Date: 2011-05-09 17:57:03 $
+$Revision: 1.377.2.4 $ $Date: 2011-05-10 07:38:43 $
 
 =cut
