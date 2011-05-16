402a8ce70b57ed0801fdd2687da03bfbe039d1f3
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-05-16 20:36:47 +0000

    Added Action ChangeAction Added Action Add Added Webservices path Fixed some ID generations

--- Kernel/Modules/AdminGenericInterfaceWebservice.pm
+++ Kernel/Modules/AdminGenericInterfaceWebservice.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminGenericInterfaceWebservice.pm - provides a webservice view for admins
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminGenericInterfaceWebservice.pm,v 1.6 2011-05-13 21:54:53 cr Exp $
+# $Id: AdminGenericInterfaceWebservice.pm,v 1.7 2011-05-16 20:36:47 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,8 +15,9 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.6 $) [1];
+$VERSION = qw($Revision: 1.7 $) [1];
 
+use Kernel::System::VariableCheck qw(:all);
 use Kernel::System::GenericInterface::Webservice;
 use Kernel::System::Valid;
 use Kernel::System::JSON;
@@ -58,6 +59,9 @@ sub Run {
 
     my $WebserviceID = $Self->{ParamObject}->GetParam( Param => 'WebserviceID' ) || '';
 
+    # ------------------------------------------------------------ #
+    # subaction Change: load webservice and show edit screen
+    # ------------------------------------------------------------ #
     if ( $Self->{Subaction} eq 'Change' ) {
 
         # check for WebserviceID
@@ -85,9 +89,130 @@ sub Run {
         );
     }
 
+    # ------------------------------------------------------------ #
+    # subaction ChangeAction: write config and return to overview
+    # ------------------------------------------------------------ #
+    elsif ( $Self->{Subaction} eq 'ChangeAction' ) {
+
+        # check for WebserviceID
+        if ( !$WebserviceID ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "Need WebserviceID!",
+            );
+        }
+
+        # get webserice configuration
+        my $WebserviceData = $Self->{WebserviceObject}->WebserviceGet( ID => $WebserviceID );
+
+        # check for valid webservice configuration
+        if ( !IsHashRefWithData($WebserviceData) ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "Could not get data for WebserviceID $WebserviceID",
+            );
+        }
+
+        # get parameter from web browser
+        my $GetParam = $Self->_GetParams;
+
+        # set new confguration
+        $WebserviceData->{Name}                                 = $GetParam->{Name};
+        $WebserviceData->{Config}->{Name}                       = $GetParam->{Name};
+        $WebserviceData->{Config}->{Description}                = $GetParam->{Description};
+        $WebserviceData->{Config}->{RemoteSystem}               = $GetParam->{RemoteSystem};
+        $WebserviceData->{Config}->{Protocol}                   = $GetParam->{Protocol};
+        $WebserviceData->{Config}->{Debugger}->{DebugThreshold} = $GetParam->{DebugThreshold};
+        $WebserviceData->{Config}->{Debugger}->{TestMode}       = $GetParam->{TestMode};
+        $WebserviceData->{ValidID}                              = $GetParam->{ValidID};
+
+        for my $CommunicationType (qw( Provider Requester )) {
+
+            # check if selected type is different from the one on the current configuration
+            if (
+                $WebserviceData->{Config}->{$CommunicationType}->{Transport}->{Type} ne
+                $GetParam->{ $CommunicationType . 'Transport' }
+                )
+            {
+
+                # delete current communication type transport
+                delete $WebserviceData->{Config}->{$CommunicationType}->{Transport};
+
+                # replace the current transport type with the new selected one
+                # the rest of the configuration will be empty
+                # the trasport need to be configured impendently
+                $WebserviceData->{Config}->{$CommunicationType}->{Transport}->{Type} =
+                    $GetParam->{ $CommunicationType . 'Transport' };
+            }
+        }
+
+        # check required parameters
+        my %Error;
+        if ( !$GetParam->{Name} ) {
+
+            # add server error error class
+            $Error{NameServerError}        = 'ServerError';
+            $Error{NameServerErrorMessage} = 'This field is required';
+        }
+
+        # check if name is duplicated
+        my %WebserviceList = %{ $Self->{WebserviceObject}->WebserviceList() };
+
+        %WebserviceList = reverse %WebserviceList;
+
+        if (
+            $WebserviceList{ $GetParam->{Name} } &&
+            $WebserviceList{ $GetParam->{Name} } ne $WebserviceID
+            )
+        {
+
+            # add server error error class
+            $Error{NameServerError}        = 'ServerError';
+            $Error{NameServerErrorMessage} = 'There is another webservice with the same name.';
+        }
+
+        # if there is an error return to edit screen
+        if ( IsHashRefWithData( \%Error ) ) {
+            return $Self->_ShowEdit(
+                %Error,
+                %Param,
+                WebserviceID   => $WebserviceID,
+                WebserviceData => $WebserviceData,
+                Action         => 'Change',
+            );
+        }
+
+        # otherwise save configuration and return to overview screen
+        my $Success = $Self->{WebserviceObject}->WebserviceUpdate(
+            ID      => $WebserviceID,
+            Name    => $WebserviceData->{Name},
+            Config  => $WebserviceData->{Config},
+            ValidID => $WebserviceData->{ValidID},
+            UserID  => $Self->{UserID},
+        );
+
+        return $Self->_ShowOverview(
+            %Param,
+            Action => 'Overview',
+        );
+    }
+
+    # ------------------------------------------------------------ #
+    # subaction Add: show edit screen (empty)
+    # ------------------------------------------------------------ #
+    if ( $Self->{Subaction} eq 'Add' ) {
+
+        return $Self->_ShowEdit(
+
+            #            %Param,
+            Action => 'Add',
+        );
+    }
+
+    # ------------------------------------------------------------ #
     # default: show start screen
+    # ------------------------------------------------------------ #
     return $Self->_ShowOverview(
         %Param,
+        Action => 'Overview',
     );
 }
 
@@ -101,6 +226,14 @@ sub _ShowOverview {
     $Self->{LayoutObject}->Block(
         Name => 'Main',
         Data => \%Param,
+    );
+    $Self->{LayoutObject}->Block(
+        Name => 'WebservicePathElement',
+        Data => {
+            Name => 'Webservices',
+            Link => 'Action=AdminGenericInterfaceWebservice',
+            Nav  => '',
+        },
     );
     $Self->{LayoutObject}->Block( Name => 'ActionList' );
     $Self->{LayoutObject}->Block( Name => 'ActionAdd' );
@@ -163,11 +296,11 @@ sub _ShowEdit {
 
     my $WebserviceData = $Param{WebserviceData};
 
-    my $DebuggerData = $WebserviceData->{Config}->{Debugger};
+    my $DebuggerData = $WebserviceData->{Config}->{Debugger} || {};
 
-    my $ProviderData = $WebserviceData->{Config}->{Provider};
+    my $ProviderData = $WebserviceData->{Config}->{Provider} || {};
 
-    my $RequesterData = $WebserviceData->{Config}->{Requester};
+    my $RequesterData = $WebserviceData->{Config}->{Requester} || {};
 
     my $Output = $Self->{LayoutObject}->Header();
     $Output .= $Self->{LayoutObject}->NavigationBar();
@@ -177,32 +310,70 @@ sub _ShowEdit {
         Name => 'Main',
         Data => \%Param,
     );
+
+    $Self->{LayoutObject}->Block(
+        Name => 'WebservicePathElement',
+        Data => {
+            Name => 'Webservices',
+            Link => 'Action=AdminGenericInterfaceWebservice',
+            Nav  => '',
+        },
+    );
+    if ( $Param{Action} eq 'Change' && $WebserviceData->{Name} ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'WebservicePathElement',
+            Data => {
+                Name => $WebserviceData->{Name},
+                Link => 'Action=AdminGenericInterfaceWebservice;Subaction=' . $Param{Action}
+                    . ';WebserviceID=' . $Param{WebserviceID},
+                Nav => '',
+            },
+        );
+    }
+    elsif ( $Param{Action} eq 'Add' ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'WebservicePathElementNoLink',
+            Data => {
+                Name => 'New Webservice',
+            },
+        );
+    }
+
     $Self->{LayoutObject}->Block( Name => 'ActionList' );
     $Self->{LayoutObject}->Block( Name => 'ActionOverview' );
-    $Self->{LayoutObject}->Block(
-        Name => 'ActionClone',
-        Data => \%Param,
-    );
-    $Self->{LayoutObject}->Block(
-        Name => 'ActionExport',
-        Data => \%Param,
-    );
-    $Self->{LayoutObject}->Block(
-        Name => 'ActionImport',
-        Data => \%Param,
-    );
-    $Self->{LayoutObject}->Block(
-        Name => 'ActionHistory',
-        Data => \%Param,
-    );
-    $Self->{LayoutObject}->Block(
-        Name => 'ActionDelete',
-        Data => \%Param,
-    );
-    $Self->{LayoutObject}->Block(
-        Name => 'ActionDebugger',
-        Data => \%Param,
-    );
+
+    if ( $Param{Action} eq 'Change' ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionClone',
+            Data => \%Param,
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionExport',
+            Data => \%Param,
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionImport',
+            Data => \%Param,
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionHistory',
+            Data => \%Param,
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionDelete',
+            Data => \%Param,
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionDebugger',
+            Data => \%Param,
+        );
+    }
+    elsif ( $Param{Action} eq 'Add' ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionImport',
+            Data => \%Param,
+        );
+    }
 
     my %GeneralData = (
         Name         => $WebserviceData->{Name},
@@ -219,21 +390,34 @@ sub _ShowEdit {
         error  => 'Error',
     );
 
+    # create the DebugThreshold select
     my $DebugThresholdStrg = $Self->{LayoutObject}->BuildSelection(
-        Data          => \%DebugThreshold,
-        Name          => 'DebugThreshold',
-        SelectedValue => $DebugThreshold{ $DebuggerData->{DebugThreshold} },
-        PossibleNone  => 0,
-        Translate     => 1,
+        Data         => \%DebugThreshold,
+        Name         => 'DebugThreshold',
+        SelectedID   => $DebuggerData->{DebugThreshold} || '',
+        PossibleNone => 0,
+        Translate    => 1,
     );
 
+    # create the DebugT test select
     my $DebugTestStrg = $Self->{LayoutObject}->BuildSelection(
         Data => {
             0 => 'No',
             1 => 'Yes',
         },
-        Name         => 'DebugTest',
-        SelectedID   => $DebuggerData->{DebugTest},
+        Name         => 'TestMode',
+        SelectedID   => $DebuggerData->{TestMode},
+        PossibleNone => 0,
+        Translate    => 1,
+    );
+
+    my %ValidList = $Self->{ValidObject}->ValidList();
+
+    # create the Validty select
+    my $ValidtyStrg = $Self->{LayoutObject}->BuildSelection(
+        Data         => \%ValidList,
+        Name         => 'ValidID',
+        SelectedID   => $WebserviceData->{ValidID},
         PossibleNone => 0,
         Translate    => 1,
     );
@@ -245,6 +429,7 @@ sub _ShowEdit {
             %GeneralData,
             DebugThresholdStrg => $DebugThresholdStrg,
             DebugTestStrg      => $DebugTestStrg,
+            ValidtyStrg        => $ValidtyStrg,
             }
     );
 
@@ -340,32 +525,45 @@ sub _ShowEdit {
                 }
         );
 
-        # output Opertions and Invokers tables
-        for my $ActionName ( keys %{ $CommTypeConfig{$CommunicationType}->{ActionsConfig} } ) {
-
-            # get control information
-            my $ActionDails = $CommTypeConfig{$CommunicationType}->{ActionsConfig}->{$ActionName};
-
-            my $Mapping = 'No';
-            if ( $ActionDails->{MappingInbound} or $ActionDails->{MappingInbound} ) {
-                $Mapping = 'Yes';
-            }
-
-            my %ActionData = (
-                Name       => $ActionName,
-                Controller => $ActionDails->{Type},
-                Mapping    => $Mapping,
-                Module     => $GIControllers{ $ActionDails->{Type} },
-                ActionLink => $CommTypeConfig{$CommunicationType}->{ActionType} . "=" . $ActionName,
-            );
-
+        if ( !IsHashRefWithData( $CommTypeConfig{$CommunicationType}->{ActionsConfig} ) ) {
             $Self->{LayoutObject}->Block(
-                Name => 'DetailsActionsRow',
-                Data => {
-                    %Param,
-                    %ActionData,
-                    }
+                Name => 'DetailsActionsNoDataFoundMsg',
+                Data => {},
             );
+        }
+        else {
+
+            # output Opertions and Invokers tables
+            for my $ActionName ( keys %{ $CommTypeConfig{$CommunicationType}->{ActionsConfig} } ) {
+
+                # get control information
+                my $ActionDails
+                    = $CommTypeConfig{$CommunicationType}->{ActionsConfig}->{$ActionName};
+
+                # set mapping string
+                my $Mapping = 'No';
+                if ( $ActionDails->{MappingInbound} or $ActionDails->{MappingInbound} ) {
+                    $Mapping = 'Yes';
+                }
+
+                # create output data
+                my %ActionData = (
+                    Name       => $ActionName,
+                    Controller => $ActionDails->{Type},
+                    Mapping    => $Mapping,
+                    Module     => $GIControllers{ $ActionDails->{Type} },
+                    ActionLink => $CommTypeConfig{$CommunicationType}->{ActionType} . "="
+                        . $ActionName,
+                );
+
+                $Self->{LayoutObject}->Block(
+                    Name => 'DetailsActionsRow',
+                    Data => {
+                        %Param,
+                        %ActionData,
+                    },
+                );
+            }
         }
     }
 
@@ -408,4 +606,25 @@ sub _OutputGIConfig {
     );
 }
 
+sub _GetParams {
+    my ( $Self, %Param ) = @_;
+
+    my $GetParam;
+
+    # get parameters from web browser
+    for my $ParamName (
+        qw( Name Description RemoteSystem Protocol DebugThreshold TestMode ValidID )
+        )
+    {
+        $GetParam->{$ParamName} = $Self->{ParamObject}->GetParam( Param => $ParamName ) || '';
+    }
+
+    $GetParam->{ProviderTransport} =
+        $Self->{ParamObject}->GetParam( Param => 'ProviderTransportList' ) || '';
+
+    $GetParam->{RequesterTransport} =
+        $Self->{ParamObject}->GetParam( Param => 'RequesterTransportList' ) || '';
+
+    return $GetParam;
+}
 1;
