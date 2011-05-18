c942c4b15791a65e2dbe2603336bedbd9a20a64c
Elva Novoa <elva.novoa@otrs.com>
2011-05-18 17:55:54 +0000

    Fixed bub 7287. Ticket Type in Customer Interface is not indicated as a required field. If TicketType is enabled, this is now a required field in the customer interface.

--- Kernel/Modules/CustomerTicketMessage.pm
+++ Kernel/Modules/CustomerTicketMessage.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/CustomerTicketMessage.pm - to handle customer messages
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: CustomerTicketMessage.pm,v 1.84 2011-02-18 12:01:29 mb Exp $
+# $Id: CustomerTicketMessage.pm,v 1.85 2011-05-18 17:55:54 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::Queue;
 use Kernel::System::State;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.84 $) [1];
+$VERSION = qw($Revision: 1.85 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -401,22 +401,34 @@ sub Run {
 
         # check queue
         if ( !$NewQueueID && !$IsUpload && !$GetParam{Expand} ) {
-            $Error{'QueueInvalid'} = 'ServerError';
+            $Error{QueueInvalid} = 'ServerError';
         }
 
         # check subject
         if ( !$GetParam{Subject} && !$IsUpload ) {
-            $Error{'SubjectInvalid'} = 'ServerError';
+            $Error{SubjectInvalid} = 'ServerError';
         }
 
         # check body
         if ( !$GetParam{Body} && !$IsUpload ) {
-            $Error{'BodyInvalid'} = 'ServerError';
+            $Error{BodyInvalid} = 'ServerError';
         }
         if ( $GetParam{Expand} ) {
             %Error = ();
             $Error{Expand} = 1;
         }
+
+        # check type
+        if (
+            $Self->{ConfigObject}->Get('Ticket::Type')
+            && !$GetParam{TypeID}
+            && !$IsUpload
+            && !$GetParam{Expand}
+            )
+        {
+            $Error{TypeIDInvalid} = 'ServerError';
+        }
+
         if (%Error) {
 
             # html output
@@ -715,10 +727,14 @@ sub _MaskNew {
             PossibleNone => 1,
             Sort         => 'AlphanumericValue',
             Translation  => 0,
+            Class        => "Validate_Required " . ( $Param{Errors}->{TypeIDInvalid} || '' ),
         );
         $Self->{LayoutObject}->Block(
             Name => 'TicketType',
-            Data => \%Param,
+            Data => {
+                %Param,
+                TypeIDInvalid => $Param{Errors}->{TypeIDInvalid},
+                }
         );
     }
 
