87f21ced14c71140f08e0c86a184ecd12cc8bdf0
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-05-18 22:24:58 +0000

    Initial Version

---
+++ var/httpd/htdocs/js/Core.Agent.Admin.GenericInterfaceWebservice.js
@@ -0,0 +1,142 @@
+// --
+// Core.Agent.Admin.SysGenericInterfaceWebservice.js - provides the special module functions for the GenericInterface webservice.
+// Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+// --
+// $Id: Core.Agent.Admin.GenericInterfaceWebservice.js,v 1.1 2011-05-18 22:24:58 cr Exp $
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core = Core || {};
+Core.Agent = Core.Agent || {};
+Core.Agent.Admin = Core.Agent.Admin || {};
+
+/**
+ * @namespace
+ * @exports TargetNS as Core.Agent.Admin.GenericInterfaceWebservice
+ * @description
+ *      This namespace contains the special module functions for the GenericInterface webservice module.
+ */
+Core.Agent.Admin.GenericInterfaceWebservice = (function (TargetNS) {
+
+    TargetNS.Init = function (Params) {
+        TargetNS.WebserviceID = parseInt(Params.WebserviceID, 10);
+        TargetNS.Localization = Params.Localization;
+    };
+
+    TargetNS.ShowDeleteDialog = function(Event){
+        Core.UI.Dialog.ShowContentDialog(
+            $('#DeleteDialogContainer'),
+            TargetNS.Localization.DeleteWebserviceMsg,
+            '240px',
+            'Center',
+            true,
+            [
+               {
+                   Label: TargetNS.Localization.DeleteMsg,
+                   Function: function () {
+                       var Data = {
+                            Action: 'AdminGenericInterfaceWebservice',
+                            Subaction: 'Delete',
+                            WebserviceID: TargetNS.WebserviceID
+                        };
+
+                        Core.AJAX.FunctionCall(Core.Config.Get('CGIHandle'), Data, function (Response) {
+                            if (!Response || !Response.Success) {
+                                alert(TargetNS.Localization.CommunicationErrorMsg);
+                                return;
+                            }
+
+                        }, 'json');
+
+                        window.location.href = Core.Config.Get('Baselink') + 'Action=' + Data.Action;
+                   }
+               },
+               {
+                   Label: TargetNS.Localization.CancelMsg,
+                   Function: function () {
+                       Core.UI.Dialog.CloseDialog($('#DeleteDialog'));
+                   }
+               }
+           ]
+        );
+
+        Event.stopPropagation();
+    };
+
+    TargetNS.ShowCloneDialog = function(Event){
+
+        var CurrentDate, CloneName;
+
+        Core.UI.Dialog.ShowContentDialog(
+            $('#CloneDialogContainer'),
+            TargetNS.Localization.CloneWebserviceMsg,
+            '240px',
+            'Center',
+            true,
+            [
+               {
+                   Label: TargetNS.Localization.CloneMsg,
+                   Function: function () {
+                       var Data = {
+                            Action: 'AdminGenericInterfaceWebservice',
+                            Subaction: 'Clone',
+                            WebserviceID: TargetNS.WebserviceID,
+                            CloneName: $('#CloneName').val()
+                        };
+
+                       Core.AJAX.FunctionCall(Core.Config.Get('CGIHandle'), Data, function (Response) {
+                            if (!Response || !Response.Success) {
+                                alert(TargetNS.Localization.CommunicationErrorMsg);
+                                return;
+                            }
+
+                       }, 'json');
+
+                       // if success return to Overvew
+                       location.href = Core.Config.Get('Baselink') + 'Action=' + Data.Action;
+
+                   }
+               },
+               {
+                   Label: TargetNS.Localization.CancelMsg,
+                   Function: function () {
+                       Core.UI.Dialog.CloseDialog($('#CloneDialog'));
+                   }
+               }
+           ]
+        );
+
+        CurrentDate = new Date();
+        CloneName = $('#Name').val() + "-" +CurrentDate.getTime();
+        $('.CloneName').val( CloneName );
+
+        Event.stopPropagation();
+    };
+
+    TargetNS.ShowImportDialog = function(Event){
+
+        Core.UI.Dialog.ShowContentDialog(
+            $('#ImportDialogContainer'),
+            TargetNS.Localization.ImportWebserviceMsg,
+            '240px',
+            'Center',
+            true
+        );
+        $('#CancelButtonAction').bind('click', function() {
+            Core.UI.Dialog.CloseDialog($('#ImportDialog'));
+        });
+
+        $('#ImportButtonAction').bind('click', function() {
+            $('#ImportForm').submit();
+        });
+
+        Event.stopPropagation();
+    };
+
+    return TargetNS;
+}(Core.Agent.Admin.GenericInterfaceWebservice || {}));
