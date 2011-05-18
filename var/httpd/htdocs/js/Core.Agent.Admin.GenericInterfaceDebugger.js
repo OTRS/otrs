03d56cdcda59676c9100385ff277292392664876
Martin Gruner <martin.gruner@otrs.com>
2011-05-18 09:25:05 +0000

    Corrected comment.

--- var/httpd/htdocs/js/Core.Agent.Admin.GenericInterfaceDebugger.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.GenericInterfaceDebugger.js
@@ -1,8 +1,8 @@
 // --
-// Core.Agent.Admin.SysConfig.js - provides the special module functions for the AdminSysConfig
+// Core.Agent.Admin.SysGenericInterfaceDebugger.js - provides the special module functions for the GenericInterface debugger.
 // Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 // --
-// $Id: Core.Agent.Admin.GenericInterfaceDebugger.js,v 1.1 2011-05-18 09:24:26 mg Exp $
+// $Id: Core.Agent.Admin.GenericInterfaceDebugger.js,v 1.2 2011-05-18 09:25:05 mg Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
