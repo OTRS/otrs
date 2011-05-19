476594b13cb384aa01f3c4ee9763573dc8b5e468
Martin Gruner <martin.gruner@otrs.com>
2011-05-19 10:24:12 +0000

    Use a more generic ID so that it can be reused.

--- var/httpd/htdocs/js/Core.Agent.Admin.GenericInterfaceDebugger.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.GenericInterfaceDebugger.js
@@ -2,7 +2,7 @@
 // Core.Agent.Admin.GenericInterfaceDebugger.js - provides the special module functions for the GenericInterface debugger.
 // Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 // --
-// $Id: Core.Agent.Admin.GenericInterfaceDebugger.js,v 1.3 2011-05-19 10:13:29 mg Exp $
+// $Id: Core.Agent.Admin.GenericInterfaceDebugger.js,v 1.4 2011-05-19 10:24:12 mg Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
@@ -180,13 +180,13 @@ Core.Agent.Admin.GenericInterfaceDebugger = (function (TargetNS) {
 
                         }, 'json');
 
-                       Core.UI.Dialog.CloseDialog($('#DebuggerDeleteDialog'));
+                       Core.UI.Dialog.CloseDialog($('#DeleteDialog'));
                    }
                },
                {
                    Label: TargetNS.Localization.CancelMsg,
                    Function: function () {
-                       Core.UI.Dialog.CloseDialog($('#DebuggerDeleteDialog'));
+                       Core.UI.Dialog.CloseDialog($('#DeleteDialog'));
                    }
                }
            ]
