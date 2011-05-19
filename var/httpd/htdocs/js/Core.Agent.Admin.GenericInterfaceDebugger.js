82e82fb1f94d78d3ed55a64d5dd2dbfb899b9b6d
Martin Gruner <martin.gruner@otrs.com>
2011-05-19 10:13:29 +0000

    Fixed typo.

--- var/httpd/htdocs/js/Core.Agent.Admin.GenericInterfaceDebugger.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.GenericInterfaceDebugger.js
@@ -1,8 +1,8 @@
 // --
-// Core.Agent.Admin.SysGenericInterfaceDebugger.js - provides the special module functions for the GenericInterface debugger.
+// Core.Agent.Admin.GenericInterfaceDebugger.js - provides the special module functions for the GenericInterface debugger.
 // Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 // --
-// $Id: Core.Agent.Admin.GenericInterfaceDebugger.js,v 1.2 2011-05-18 09:25:05 mg Exp $
+// $Id: Core.Agent.Admin.GenericInterfaceDebugger.js,v 1.3 2011-05-19 10:13:29 mg Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
