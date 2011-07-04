1b88d2d10c9cd64e3c2193f81e5ea071e830db43
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-07-04 22:13:37 +0000

    Fixed html validator issues.

--- Kernel/Modules/AdminGenericInterfaceMappingSolMan.pm
+++ Kernel/Modules/AdminGenericInterfaceMappingSolMan.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminGenericInterfaceMappingSolMan.pm - provides a Mapping SolMan view for admins
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminGenericInterfaceMappingSolMan.pm,v 1.5 2011-07-04 21:47:41 cr Exp $
+# $Id: AdminGenericInterfaceMappingSolMan.pm,v 1.6 2011-07-04 22:13:37 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.5 $) [1];
+$VERSION = qw($Revision: 1.6 $) [1];
 
 use Kernel::System::VariableCheck qw(:all);
 use Kernel::System::GenericInterface::Webservice;
@@ -401,7 +401,7 @@ sub _ShowEdit {
                 Data => {
                     KeyName          => $MappingKeyType,
                     ValueNameError   => $ValueNameError,
-                    ValueNameTooltip => $ValueNameTooltip,
+                    ValueNameTooltip => $ValueNameTooltip || 'This field is required.',
                     ValueName        => $NewValClone,
                     ValueIndex       => $ValueIndex,
                     ValueMapNew      => $MappingConfig->{$MappingKeyType}->{$NewVal},
