9a7b4bd8f566fce71a5241017a79c1fdc8adf3ce
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-07-08 21:17:37 +0000

    Changed from Binary to ASCII

--- Kernel/Modules/AdminGenericInterfaceMappingSimple.pm
+++ Kernel/Modules/AdminGenericInterfaceMappingSimple.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminGenericInterfaceMappingSimple.pm - provides a TransportHTTPSOAP view for admins
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminGenericInterfaceMappingSimple.pm,v 1.8 2011-07-08 21:15:14 cr Exp $
+# $Id: AdminGenericInterfaceMappingSimple.pm,v 1.9 2011-07-08 21:17:37 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 use Kernel::System::VariableCheck qw(:all);
 use Kernel::System::GenericInterface::Webservice;
