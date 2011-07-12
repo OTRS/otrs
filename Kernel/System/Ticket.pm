607617139ab12f4ef0be53e22b2e7040884ee112
Martin Edenhofer <martin.edenhofer@otrs.com>
2011-07-12 14:20:19 +0000

    Fixed bug#7015 - Closing split ticked assigned to different agents causes "Locked Tickets Total" to be negative (or off by one).

--- Kernel/System/Ticket.pm
+++ Kernel/System/Ticket.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket.pm - all ticket functions
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Ticket.pm,v 1.509 2011-07-12 13:58:58 martin Exp $
+# $Id: Ticket.pm,v 1.510 2011-07-12 14:20:19 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -35,7 +35,7 @@ use Kernel::System::LinkObject;
 use Kernel::System::EventHandler;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.509 $) [1];
+$VERSION = qw($Revision: 1.510 $) [1];
 
 =head1 NAME
 
@@ -4015,7 +4015,7 @@ sub TicketSearch {
         # add ticket flag extension
         if ( $Param{TicketFlag} ) {
 
-            $SQL = 'SELECT COUNT(DISTINCT(st.id, st.tn';
+            $SQL = 'SELECT COUNT(DISTINCT(st.id';
             my $TicketFlagUserID = $Param{TicketFlagUserID} || $Param{UserID};
             return if !defined $TicketFlagUserID;
 
@@ -4027,7 +4027,7 @@ sub TicketSearch {
                 $SQL .= ",tf$Index.ticket_key,tf$Index.ticket_value,tf$Index.create_by";
                 $Index++;
             }
-            $SQL .= ')';
+            $SQL .= '))';
         }
     }
     else {
@@ -8511,6 +8511,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.509 $ $Date: 2011-07-12 13:58:58 $
+$Revision: 1.510 $ $Date: 2011-07-12 14:20:19 $
 
 =cut
