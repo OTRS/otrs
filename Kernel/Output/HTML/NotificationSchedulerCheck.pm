10894d72ece30589c59ebf80cff199152a984e74
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-07-19 18:00:48 +0000

    Changed link to point to the correct module. this can also be changed to #

--- Kernel/Output/HTML/NotificationSchedulerCheck.pm
+++ Kernel/Output/HTML/NotificationSchedulerCheck.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/NotificationSchedulerCheck.pm
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: NotificationSchedulerCheck.pm,v 1.3 2011-07-18 18:20:28 mb Exp $
+# $Id: NotificationSchedulerCheck.pm,v 1.4 2011-07-19 18:00:48 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use Kernel::System::Group;
 use Kernel::System::PID;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -115,7 +115,7 @@ sub Run {
             # show error notification, if scheduler is not running
             return $Self->{LayoutObject}->Notify(
                 %NotificationDetails,
-                Link      => '$Env{"Baselink"}Action=AdminUser',
+                Link      => '$Env{"Baselink"}Action=AdminScheduler',
                 LinkClass => 'StartScheduler',
             );
             last GROUP;
