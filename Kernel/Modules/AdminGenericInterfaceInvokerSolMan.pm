e7645883946a53dc32a041eb85b3490c9558bef1
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-07-28 01:52:32 +0000

    Fixed problem with the event list that mixes article and ticket events for article event types.

--- Kernel/Modules/AdminGenericInterfaceInvokerSolMan.pm
+++ Kernel/Modules/AdminGenericInterfaceInvokerSolMan.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminGenericInterfaceInvokerSolMan.pm - provides a log view for admins
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminGenericInterfaceInvokerSolMan.pm,v 1.3 2011-07-08 18:09:36 cr Exp $
+# $Id: AdminGenericInterfaceInvokerSolMan.pm,v 1.4 2011-07-28 01:52:32 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 use Kernel::System::GenericInterface::Webservice;
 
@@ -602,7 +602,6 @@ sub _ShowScreen {
     }
 
     my @EventTypeList;
-    my @EventList;
 
     my $SelectedEventType = $Self->{ParamObject}->GetParam( Param => 'EventType' ) || 'Ticket';
 
@@ -610,6 +609,9 @@ sub _ShowScreen {
     TYPE:
     for my $Type ( keys %{$RegisteredEvents} ) {
         next TYPE if !$Type;
+
+        # refresh event list for each event type
+        my @EventList;
 
         EVENT:
         for my $Event ( keys %{ $RegisteredEvents->{$Type} } ) {
