ba1ff25ce44825938338d318734368bbc58aed1f
Martin Gruner <martin.gruner@otrs.com>
2011-08-11 09:37:44 +0000

    Fixed bug#7550  - 2011-08-10 Fixed bug#7550 - SLA and Service is not written to history.

--- Kernel/System/Ticket.pm
+++ Kernel/System/Ticket.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket.pm - all ticket functions
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Ticket.pm,v 1.488.2.16 2011-07-18 08:56:51 martin Exp $
+# $Id: Ticket.pm,v 1.488.2.17 2011-08-11 09:37:44 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -35,7 +35,7 @@ use Kernel::System::LinkObject;
 use Kernel::System::EventHandler;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.488.2.16 $) [1];
+$VERSION = qw($Revision: 1.488.2.17 $) [1];
 
 =head1 NAME
 
@@ -511,6 +511,29 @@ sub TicketCreate {
         Name => "\%\%$Param{TN}\%\%$Param{Queue}\%\%$Param{Priority}\%\%$Param{State}\%\%$TicketID",
         CreateUserID => $Param{UserID},
     );
+
+    if ( $Self->{ConfigObject}->Get('Ticket::Service') ) {
+
+        # history insert for service so that initial values can be seen
+        my $HistoryService   = $Param{Service}   || 'NULL';
+        my $HistoryServiceID = $Param{ServiceID} || '';
+        $Self->HistoryAdd(
+            TicketID     => $TicketID,
+            HistoryType  => 'ServiceUpdate',
+            Name         => "\%\%$HistoryService\%\%$HistoryServiceID\%\%NULL\%\%",
+            CreateUserID => $Param{UserID},
+        );
+
+        # history insert for SLA
+        my $HistorySLA   = $Param{SLA}   || 'NULL';
+        my $HistorySLAID = $Param{SLAID} || '';
+        $Self->HistoryAdd(
+            TicketID     => $TicketID,
+            HistoryType  => 'SLAUpdate',
+            Name         => "\%\%$HistorySLA\%\%$HistorySLAID\%\%NULL\%\%",
+            CreateUserID => $Param{UserID},
+        );
+    }
 
     # set customer data if given
     if ( $Param{CustomerNo} || $Param{CustomerID} || $Param{CustomerUser} ) {
@@ -8454,6 +8477,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.488.2.16 $ $Date: 2011-07-18 08:56:51 $
+$Revision: 1.488.2.17 $ $Date: 2011-08-11 09:37:44 $
 
 =cut
