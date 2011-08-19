f8ffa4ebaf9f6a78b535ba6249144e66e256c7a8
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-08-19 21:03:40 +0000

    Initial version.

---
+++ Kernel/Modules/AdminDynamicFieldTextArea.pm
@@ -0,0 +1,398 @@
+# --
+# Kernel/Modules/AdminDynamicFieldTextArea.pm - provides a dynamic fields text config view for admins
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: AdminDynamicFieldTextArea.pm,v 1.1 2011-08-19 21:03:40 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AdminDynamicFieldTextArea;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+use Kernel::System::Valid;
+use Kernel::System::CheckItem;
+use Kernel::System::DynamicField;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    for (qw(ParamObject LayoutObject LogObject ConfigObject)) {
+        if ( !$Self->{$_} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
+        }
+    }
+
+    # create additional objects
+    $Self->{ValidObject} = Kernel::System::Valid->new( %{$Self} );
+
+    $Self->{DynamicFieldObject} = Kernel::System::DynamicField->new( %{$Self} );
+
+    # get configured object types
+    $Self->{ObjectTypeConfig} = $Self->{ConfigObject}->Get('DynamicFields::ObjectType');
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    if ( $Self->{Subaction} eq 'Add' ) {
+        return $Self->_Add(
+            %Param,
+        );
+    }
+    elsif ( $Self->{Subaction} eq 'AddAction' ) {
+        return $Self->_AddAction(
+            %Param,
+        );
+    }
+    if ( $Self->{Subaction} eq 'Change' ) {
+        return $Self->_Change(
+            %Param,
+        );
+    }
+    elsif ( $Self->{Subaction} eq 'ChangeAction' ) {
+        return $Self->_ChangeAction(
+            %Param,
+        );
+    }
+    return $Self->{LayoutObject}->ErrorScreen(
+        Message => "Undefined subaction.",
+    );
+}
+
+sub _Add {
+    my ( $Self, %Param ) = @_;
+
+    my $ObjectType = $Self->{ParamObject}->GetParam( Param => 'ObjectType' );
+
+    if ( !$ObjectType ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ObjectType",
+        );
+    }
+
+    # get the object type display name
+    my $ObjectTypeName = $Self->{ObjectTypeConfig}->{$ObjectType}->{DisplayName};
+
+    return $Self->_ShowScreen(
+        %Param,
+        Mode           => 'Add',
+        ObjectType     => $ObjectType,
+        ObjectTypeName => $ObjectTypeName,
+    );
+}
+
+sub _AddAction {
+    my ( $Self, %Param ) = @_;
+
+    my %Errors;
+    my %GetParam;
+
+    for my $Needed (qw(Name Label)) {
+        $GetParam{$Needed} = $Self->{ParamObject}->GetParam( Param => $Needed );
+        if ( !$GetParam{$Needed} ) {
+            $Errors{ $Needed . 'ServerError' }        = 'ServerError';
+            $Errors{ $Needed . 'ServerErrorMessage' } = 'This field is required.';
+        }
+    }
+
+    if ( $GetParam{Name} ) {
+
+        # check if name is lowercase
+        if ( $GetParam{Name} !~ m{\A ( ?: [a-z] | \d )+ \Z}xms ) {
+
+            # add server error error class
+            $Errors{NameServerError} = 'ServerError';
+            $Errors{NameServerErrorMessage} =
+                'The field does not contain only lower case letters and numbers.';
+        }
+
+        # check if name is duplicated
+        my %DynamicFieldsList = %{
+            $Self->{DynamicFieldObject}->DynamicFieldList(
+                Valid      => 0,
+                ResultType => 'HASH',
+                )
+            };
+
+        %DynamicFieldsList = reverse %DynamicFieldsList;
+
+        if ( $DynamicFieldsList{ $GetParam{Name} } ) {
+
+            # add server error error class
+            $Errors{NameServerError}        = 'ServerError';
+            $Errors{NameServerErrorMessage} = 'There is another field with the same name.';
+        }
+    }
+
+    for my $ConfigParam (qw(ObjectType ObjectTypeName FieldType DefaultValue ValidID)) {
+        $GetParam{$ConfigParam} = $Self->{ParamObject}->GetParam( Param => $ConfigParam );
+    }
+
+    # uncorrectable errors
+    if ( !$GetParam{ValidID} ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ValidID",
+        );
+    }
+
+    # return to add screen if errors
+    if (%Errors) {
+        return $Self->_ShowScreen(
+            %Param,
+            %Errors,
+            %GetParam,
+            Mode => 'Add',
+        );
+    }
+
+    # set specific config
+    my $FieldConfig = {
+        DefaultValue => $GetParam{DefaultValue},
+    };
+
+    # create a new field
+    my $FieldID = $Self->{DynamicFieldObject}->DynamicFieldAdd(
+        Name       => $GetParam{Name},
+        Label      => $GetParam{Label},
+        FieldType  => 'TextArea',
+        ObjectType => $GetParam{ObjectType},
+        Config     => $FieldConfig,
+        ValidID    => $GetParam{ValidID},
+        UserID     => $Self->{UserID},
+    );
+
+    if ( !$FieldID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not create the new field",
+        );
+    }
+
+    # load new URL in parent window and close popup
+    return $Self->{LayoutObject}->PopupClose(
+        URL => "Action=AdminDynamicField",
+    );
+}
+
+sub _Change {
+    my ( $Self, %Param ) = @_;
+
+    my $ObjectType = $Self->{ParamObject}->GetParam( Param => 'ObjectType' );
+
+    if ( !$ObjectType ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ObjectType",
+        );
+    }
+
+    # get the object type display name
+    my $ObjectTypeName = $Self->{ObjectTypeConfig}->{$ObjectType}->{DisplayName};
+
+    my $FieldID = $Self->{ParamObject}->GetParam( Param => 'ID' );
+
+    if ( !$FieldID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ID",
+        );
+    }
+
+    # get dynamic filed data
+    my $DynamicFieldData = $Self->{DynamicFieldObject}->DynamicFieldGet(
+        ID => $FieldID,
+    );
+
+    # check for valid dynamic field configuration
+    if ( !IsHashRefWithData($DynamicFieldData) ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not get data for dynamic field $FieldID",
+        );
+    }
+
+    return $Self->_ShowScreen(
+        %Param,
+        %${DynamicFieldData},
+        ID             => $FieldID,
+        Mode           => 'Change',
+        ObjectType     => $ObjectType,
+        ObjectTypeName => $ObjectTypeName,
+    );
+}
+
+sub _ChangeAction {
+    my ( $Self, %Param ) = @_;
+
+    my %Errors;
+    my %GetParam;
+
+    for my $Needed (qw(Name Label)) {
+        $GetParam{$Needed} = $Self->{ParamObject}->GetParam( Param => $Needed );
+        if ( !$GetParam{$Needed} ) {
+            $Errors{ $Needed . 'ServerError' }        = 'ServerError';
+            $Errors{ $Needed . 'ServerErrorMessage' } = 'This field is required.';
+        }
+    }
+
+    my $FieldID = $Self->{ParamObject}->GetParam( Param => 'ID' );
+    if ( !$FieldID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ID",
+        );
+    }
+
+    if ( $GetParam{Name} ) {
+
+        # check if name is lowercase
+        if ( $GetParam{Name} !~ m{\A ( ?: [a-z] | \d )+ \Z}xms ) {
+
+            # add server error error class
+            $Errors{NameServerError} = 'ServerError';
+            $Errors{NameServerErrorMessage} =
+                'The field does not contain only lower case letters and numbers.';
+        }
+
+        # check if name is duplicated
+        my %DynamicFieldsList = %{
+            $Self->{DynamicFieldObject}->DynamicFieldList(
+                Valid      => 0,
+                ResultType => 'HASH',
+                )
+            };
+
+        %DynamicFieldsList = reverse %DynamicFieldsList;
+
+        if (
+            $DynamicFieldsList{ $GetParam{Name} } &&
+            $DynamicFieldsList{ $GetParam{Name} } ne $FieldID
+            )
+        {
+
+            # add server error class
+            $Errors{NameServerError}        = 'ServerError';
+            $Errors{NameServerErrorMessage} = 'There is another field with the same name.';
+        }
+    }
+
+    for my $ConfigParam (qw(ObjectType ObjectTypeName FieldType DefaultValue ValidID)) {
+        $GetParam{$ConfigParam} = $Self->{ParamObject}->GetParam( Param => $ConfigParam );
+    }
+
+    # uncorrectable errors
+    if ( !$GetParam{ValidID} ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ValidID",
+        );
+    }
+
+    # get dynamic filed data
+    my $DynamicFieldData = $Self->{DynamicFieldObject}->DynamicFieldGet(
+        ID => $FieldID,
+    );
+
+    # check for valid dynamic field configuration
+    if ( !IsHashRefWithData($DynamicFieldData) ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not get data for dynamic field $FieldID",
+        );
+    }
+
+    # return to add screen if errors
+    if (%Errors) {
+        return $Self->_ShowScreen(
+            %Param,
+            %Errors,
+            %GetParam,
+            ID   => $FieldID,
+            Mode => 'Change',
+        );
+    }
+
+    # set specific config
+    my $FieldConfig = {
+        DefaultValue => $GetParam{DefaultValue},
+    };
+
+    # update dynamic field (FieldType and ObjectType cannot be changed; use old values)
+    my $UpdateSuccess = $Self->{DynamicFieldObject}->DynamicFieldUpdate(
+        ID         => $FieldID,
+        Name       => $GetParam{Name},
+        Label      => $GetParam{Label},
+        FieldType  => $DynamicFieldData->{FieldType},
+        ObjectType => $DynamicFieldData->{ObjectType},
+        Config     => $FieldConfig,
+        ValidID    => $GetParam{ValidID},
+        UserID     => $Self->{UserID},
+    );
+
+    if ( !$UpdateSuccess ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not update the field $GetParam{Name}",
+        );
+    }
+
+    # load new URL in parent window and close popup
+    return $Self->{LayoutObject}->PopupClose(
+        URL => "Action=AdminDynamicField",
+    );
+}
+
+sub _ShowScreen {
+    my ( $Self, %Param ) = @_;
+
+    $Param{DisplayFieldName} = 'New';
+
+    if ( $Param{Mode} eq 'Change' ) {
+        $Param{ShowWarning}      = 'ShowWarning';
+        $Param{DisplayFieldName} = $Param{Name};
+    }
+
+    # header
+    my $Output = $Self->{LayoutObject}->Header( Type => 'Small' );
+
+    my %ValidList = $Self->{ValidObject}->ValidList();
+
+    # create the Validity select
+    my $ValidityStrg = $Self->{LayoutObject}->BuildSelection(
+        Data       => \%ValidList,
+        Name       => 'ValidID',
+        SelectedID => 1,
+
+        #        SelectedID   => $DynamicFieldData->{ValidID} || 1,
+        PossibleNone => 0,
+        Translate    => 1,
+    );
+
+    my %Config;
+
+    # send the configuration options if any
+    if ( IsHashRefWithData( $Param{Config} ) ) {
+        %Config = %{ $Param{Config} };
+    }
+
+    # generate output
+    $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => 'AdminDynamicFieldTextArea',
+        Data         => {
+            %Param,
+            %Config,
+            ValidityStrg => $ValidityStrg,
+            }
+    );
+
+    $Output .= $Self->{LayoutObject}->Footer();
+
+    return $Output;
+}
+1;
