bea1d2acc87033ad31d6dbe832c76206eb5b6a03
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-08-22 17:37:57 +0000

    Added FieldType to field edit link Fixed Validate_LowercaseNumbers validation Added Validate_PositiveNetativeNumbers validation (required for date and date / time fields.

--- var/httpd/htdocs/js/Core.Agent.Admin.DynamicField.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.DynamicField.js
@@ -2,7 +2,7 @@
 // Core.Agent.Admin.DynamicField.js - provides the special module functions for the Dynamic Fields.
 // Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 // --
-// $Id: Core.Agent.Admin.DynamicField.js,v 1.5 2011-08-20 13:50:11 cr Exp $
+// $Id: Core.Agent.Admin.DynamicField.js,v 1.6 2011-08-22 17:37:57 cr Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
@@ -33,17 +33,24 @@ Core.Agent.Admin.DynamicField = (function (TargetNS) {
         Action = DynamicFieldsConfig[ FieldType ];
 
         // redirect to correct url
-        URL = Core.Config.Get('Baselink') + 'Action=' + Action + ';Subaction=Add' + ';ObjectType=' + ObjectType;
+        URL = Core.Config.Get('Baselink') + 'Action=' + Action + ';Subaction=Add' + ';ObjectType=' + ObjectType + ';FieldType=' + FieldType;
         Core.UI.Popup.OpenPopup(URL, 'Action');
     };
 
     TargetNS.ValidationInit = function() {
 
         Core.Form.Validate.AddRule("Validate_LowercaseNumbers", {
-            Validate_TimeUnits: true
+            Validate_LowercaseNumbers: true
         });
         Core.Form.Validate.AddMethod("Validate_LowercaseNumbers", function (Value, Element) {
             return ( /^[a-z0-9]+$/.test(Value));
+        }, "");
+
+        Core.Form.Validate.AddRule("Validate_PositiveNegativeNumbers", {
+            Validate_PositiveNegativeNumbers: true
+        });
+        Core.Form.Validate.AddMethod("Validate_PositiveNegativeNumbers", function (Value, Element) {
+            return ( /^-?[0-9]+$/.test(Value));
         }, "");
 
     }
