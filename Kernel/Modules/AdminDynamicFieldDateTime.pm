dab6f5f54b47b65d17900d12b834e4ad15d62937
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-08-22 17:45:18 +0000

    Initial version.

---
+++ Kernel/Modules/AdminDynamicFieldDateTime.pm
@@ -0,0 +1,471 @@
+# --
+# Kernel/Modules/AdminDynamicFieldDateTime.pm - provides a dynamic fields Date Time config view for admins
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: AdminDynamicFieldDateTime.pm,v 1.1 2011-08-22 17:45:18 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AdminDynamicFieldDateTime;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+use Kernel::System::Valid;
+use Kernel::System::CheckItem;
+use Kernel::System::DynamicField;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    for (qw(ParamObject LayoutObject LogObject ConfigObject)) {
+        if ( !$Self->{$_} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
+        }
+    }
+
+    # create additional objects
+    $Self->{ValidObject} = Kernel::System::Valid->new( %{$Self} );
+
+    $Self->{DynamicFieldObject} = Kernel::System::DynamicField->new( %{$Self} );
+
+    # get configured object types
+    $Self->{ObjectTypeConfig} = $Self->{ConfigObject}->Get('DynamicFields::ObjectType');
+
+    # get the field display name
+    $Self->{FieldTypeConfig} = $Self->{ConfigObject}->Get('DynamicFields::Backend') || {};
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    if ( $Self->{Subaction} eq 'Add' ) {
+        return $Self->_Add(
+            %Param,
+        );
+    }
+    elsif ( $Self->{Subaction} eq 'AddAction' ) {
+        return $Self->_AddAction(
+            %Param,
+        );
+    }
+    if ( $Self->{Subaction} eq 'Change' ) {
+        return $Self->_Change(
+            %Param,
+        );
+    }
+    elsif ( $Self->{Subaction} eq 'ChangeAction' ) {
+        return $Self->_ChangeAction(
+            %Param,
+        );
+    }
+    return $Self->{LayoutObject}->ErrorScreen(
+        Message => "Undefined subaction.",
+    );
+}
+
+sub _Add {
+    my ( $Self, %Param ) = @_;
+
+    my %GetParam;
+    for my $Needed (qw(ObjectType FieldType)) {
+        $GetParam{$Needed} = $Self->{ParamObject}->GetParam( Param => $Needed );
+        if ( !$Needed ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "Need $Needed",
+            );
+        }
+    }
+
+    # get the object type and field type display name
+    my $ObjectTypeName = $Self->{ObjectTypeConfig}->{ $GetParam{ObjectType} }->{DisplayName} || '';
+    my $FieldTypeName  = $Self->{FieldTypeConfig}->{ $GetParam{FieldType} }->{DisplayName}   || '';
+
+    return $Self->_ShowScreen(
+        %Param,
+        %GetParam,
+        Mode           => 'Add',
+        ObjectTypeName => $ObjectTypeName,
+        FieldTypeName  => $FieldTypeName,
+    );
+}
+
+sub _AddAction {
+    my ( $Self, %Param ) = @_;
+
+    my %Errors;
+    my %GetParam;
+
+    for my $Needed (qw(Name Label)) {
+        $GetParam{$Needed} = $Self->{ParamObject}->GetParam( Param => $Needed );
+        if ( !$GetParam{$Needed} ) {
+            $Errors{ $Needed . 'ServerError' }        = 'ServerError';
+            $Errors{ $Needed . 'ServerErrorMessage' } = 'This field is required.';
+        }
+    }
+
+    if ( $GetParam{Name} ) {
+
+        # check if name is lowercase
+        if ( $GetParam{Name} !~ m{\A ( ?: [a-z] | \d )+ \Z}xms ) {
+
+            # add server error error class
+            $Errors{NameServerError} = 'ServerError';
+            $Errors{NameServerErrorMessage} =
+                'The field does not contain only lower case letters and numbers.';
+        }
+
+        # check if name is duplicated
+        my %DynamicFieldsList = %{
+            $Self->{DynamicFieldObject}->DynamicFieldList(
+                Valid      => 0,
+                ResultType => 'HASH',
+                )
+            };
+
+        %DynamicFieldsList = reverse %DynamicFieldsList;
+
+        if ( $DynamicFieldsList{ $GetParam{Name} } ) {
+
+            # add server error error class
+            $Errors{NameServerError}        = 'ServerError';
+            $Errors{NameServerErrorMessage} = 'There is another field with the same name.';
+        }
+    }
+
+    # get numeric parameters
+    for my $DateConfigParam (qw(DefaultValue YearsInFuture YearsInPast)) {
+        $GetParam{$DateConfigParam} = $Self->{ParamObject}->GetParam( Param => $DateConfigParam )
+            || 0;
+
+        # check if numeric values are valid
+        if ( $GetParam{$DateConfigParam} !~ m{\A -? [\d]+ \Z}xms ) {
+
+            # add server error error class
+            $Errors{ $DateConfigParam . 'ServerError' } = 'ServerError';
+        }
+    }
+
+    for my $ConfigParam (qw(ObjectType ObjectTypeName FieldType YearsPeriod ValidID)) {
+        $GetParam{$ConfigParam} = $Self->{ParamObject}->GetParam( Param => $ConfigParam );
+    }
+
+    # uncorrectable errors
+    if ( !$GetParam{ValidID} ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ValidID",
+        );
+    }
+
+    # return to add screen if errors
+    if (%Errors) {
+        return $Self->_ShowScreen(
+            %Param,
+            %Errors,
+            %GetParam,
+            Mode => 'Add',
+        );
+    }
+
+    # set specific config
+    my $FieldConfig = {
+        DefaultValue  => $GetParam{DefaultValue},
+        YearsPeriod   => $GetParam{YearsPeriod},
+        YearsInFuture => $GetParam{YearsInFuture},
+        YearsInPast   => $GetParam{YearsInPast},
+    };
+
+    # create a new field
+    my $FieldID = $Self->{DynamicFieldObject}->DynamicFieldAdd(
+        Name       => $GetParam{Name},
+        Label      => $GetParam{Label},
+        FieldType  => $GetParam{FieldType},
+        ObjectType => $GetParam{ObjectType},
+        Config     => $FieldConfig,
+        ValidID    => $GetParam{ValidID},
+        UserID     => $Self->{UserID},
+    );
+
+    if ( !$FieldID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not create the new field",
+        );
+    }
+
+    # load new URL in parent window and close popup
+    return $Self->{LayoutObject}->PopupClose(
+        URL => "Action=AdminDynamicField",
+    );
+}
+
+sub _Change {
+    my ( $Self, %Param ) = @_;
+
+    my %GetParam;
+    for my $Needed (qw(ObjectType FieldType)) {
+        $GetParam{$Needed} = $Self->{ParamObject}->GetParam( Param => $Needed );
+        if ( !$Needed ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "Need $Needed",
+            );
+        }
+    }
+
+    # get the object type and field type display name
+    my $ObjectTypeName = $Self->{ObjectTypeConfig}->{ $GetParam{ObjectType} }->{DisplayName} || '';
+    my $FieldTypeName  = $Self->{FieldTypeConfig}->{ $GetParam{FieldType} }->{DisplayName}   || '';
+
+    my $FieldID = $Self->{ParamObject}->GetParam( Param => 'ID' );
+
+    if ( !$FieldID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ID",
+        );
+    }
+
+    # get dynamic filed data
+    my $DynamicFieldData = $Self->{DynamicFieldObject}->DynamicFieldGet(
+        ID => $FieldID,
+    );
+
+    # check for valid dynamic field configuration
+    if ( !IsHashRefWithData($DynamicFieldData) ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not get data for dynamic field $FieldID",
+        );
+    }
+
+    my %Config = ();
+
+    # extract configuration
+    if ( IsHashRefWithData( $DynamicFieldData->{Config} ) ) {
+        %Config = %{ $DynamicFieldData->{Config} };
+    }
+
+    return $Self->_ShowScreen(
+        %Param,
+        %GetParam,
+        %${DynamicFieldData},
+        %Config,
+        ID             => $FieldID,
+        Mode           => 'Change',
+        ObjectTypeName => $ObjectTypeName,
+        FieldTypeName  => $FieldTypeName,
+    );
+}
+
+sub _ChangeAction {
+    my ( $Self, %Param ) = @_;
+
+    my %Errors;
+    my %GetParam;
+
+    for my $Needed (qw(Name Label)) {
+        $GetParam{$Needed} = $Self->{ParamObject}->GetParam( Param => $Needed );
+        if ( !$GetParam{$Needed} ) {
+            $Errors{ $Needed . 'ServerError' }        = 'ServerError';
+            $Errors{ $Needed . 'ServerErrorMessage' } = 'This field is required.';
+        }
+    }
+
+    my $FieldID = $Self->{ParamObject}->GetParam( Param => 'ID' );
+    if ( !$FieldID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ID",
+        );
+    }
+
+    if ( $GetParam{Name} ) {
+
+        # check if name is lowercase
+        if ( $GetParam{Name} !~ m{\A ( ?: [a-z] | \d )+ \Z}xms ) {
+
+            # add server error error class
+            $Errors{NameServerError} = 'ServerError';
+            $Errors{NameServerErrorMessage} =
+                'The field does not contain only lower case letters and numbers.';
+        }
+
+        # check if name is duplicated
+        my %DynamicFieldsList = %{
+            $Self->{DynamicFieldObject}->DynamicFieldList(
+                Valid      => 0,
+                ResultType => 'HASH',
+                )
+            };
+
+        %DynamicFieldsList = reverse %DynamicFieldsList;
+
+        if (
+            $DynamicFieldsList{ $GetParam{Name} } &&
+            $DynamicFieldsList{ $GetParam{Name} } ne $FieldID
+            )
+        {
+
+            # add server error class
+            $Errors{NameServerError}        = 'ServerError';
+            $Errors{NameServerErrorMessage} = 'There is another field with the same name.';
+        }
+    }
+
+    # get numeric parameters
+    for my $DateConfigParam (qw(DefaultValue YearsInFuture YearsInPast)) {
+        $GetParam{$DateConfigParam} = $Self->{ParamObject}->GetParam( Param => $DateConfigParam )
+            || 0;
+
+        # check if numeric values are valid
+        if ( $GetParam{$DateConfigParam} !~ m{\A -? [\d]+ \Z}xms ) {
+
+            # add server error error class
+            $Errors{ $DateConfigParam . 'ServerError' } = 'ServerError';
+        }
+    }
+
+    for my $ConfigParam (qw(ObjectType ObjectTypeName FieldType YearsPeriod ValidID)) {
+        $GetParam{$ConfigParam} = $Self->{ParamObject}->GetParam( Param => $ConfigParam );
+    }
+
+    # uncorrectable errors
+    if ( !$GetParam{ValidID} ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Need ValidID",
+        );
+    }
+
+    # get dynamic filed data
+    my $DynamicFieldData = $Self->{DynamicFieldObject}->DynamicFieldGet(
+        ID => $FieldID,
+    );
+
+    # check for valid dynamic field configuration
+    if ( !IsHashRefWithData($DynamicFieldData) ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not get data for dynamic field $FieldID",
+        );
+    }
+
+    # return to add screen if errors
+    if (%Errors) {
+        return $Self->_ShowScreen(
+            %Param,
+            %Errors,
+            %GetParam,
+            ID   => $FieldID,
+            Mode => 'Change',
+        );
+    }
+
+    # set specific config
+    my $FieldConfig = {
+        DefaultValue  => $GetParam{DefaultValue},
+        YearsPeriod   => $GetParam{YearsPeriod},
+        YearsInFuture => $GetParam{YearsInFuture},
+        YearsInPast   => $GetParam{YearsInPast},
+    };
+
+    # update dynamic field (FieldType and ObjectType cannot be changed; use old values)
+    my $UpdateSuccess = $Self->{DynamicFieldObject}->DynamicFieldUpdate(
+        ID         => $FieldID,
+        Name       => $GetParam{Name},
+        Label      => $GetParam{Label},
+        FieldType  => $DynamicFieldData->{FieldType},
+        ObjectType => $DynamicFieldData->{ObjectType},
+        Config     => $FieldConfig,
+        ValidID    => $GetParam{ValidID},
+        UserID     => $Self->{UserID},
+    );
+
+    if ( !$UpdateSuccess ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not update the field $GetParam{Name}",
+        );
+    }
+
+    # load new URL in parent window and close popup
+    return $Self->{LayoutObject}->PopupClose(
+        URL => "Action=AdminDynamicField",
+    );
+}
+
+sub _ShowScreen {
+    my ( $Self, %Param ) = @_;
+
+    $Param{DisplayFieldName} = 'New';
+
+    if ( $Param{Mode} eq 'Change' ) {
+        $Param{ShowWarning}      = 'ShowWarning';
+        $Param{DisplayFieldName} = $Param{Name};
+    }
+
+    # header
+    my $Output = $Self->{LayoutObject}->Header( Type => 'Small' );
+
+    my %ValidList = $Self->{ValidObject}->ValidList();
+
+    # create the Validity select
+    my $ValidityStrg = $Self->{LayoutObject}->BuildSelection(
+        Data         => \%ValidList,
+        Name         => 'ValidID',
+        SelectedID   => $Param{ValidID} || 1,
+        PossibleNone => 0,
+        Translate    => 1,
+        Class        => 'W50pc',
+    );
+
+    # define config field specific settings
+    my $DefaultValue  = $Param{DefaultValue}  || 0;
+    my $YearsPeriod   = $Param{YearsPeriod}   || 0;
+    my $YearsInFuture = $Param{YearsInFuture} || 5;
+    my $YearsInPast   = $Param{YearsInPast}   || 5;
+
+    # create the Default Value Type select
+    my $YearsPeriodStrg = $Self->{LayoutObject}->BuildSelection(
+        Data => {
+            0 => 'No',
+            1 => 'Yes',
+        },
+        Name         => 'YearsPeriod',
+        SelectedID   => $YearsPeriod,
+        PossibleNone => 0,
+        Translate    => 1,
+        Class        => 'W50pc'
+    );
+
+    # show or hide the years in future and back fields
+    my $ClassYearsPeriod = 'Hidden';
+    if ( $YearsPeriod && $YearsPeriod eq '1' ) {
+        $ClassYearsPeriod = '';
+    }
+
+    # generate output
+    $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => 'AdminDynamicFieldDateTime',
+        Data         => {
+            %Param,
+            ValidityStrg     => $ValidityStrg,
+            YearsPeriodStrg  => $YearsPeriodStrg,
+            ClassYearsPeriod => $ClassYearsPeriod,
+            DefaultValue     => $DefaultValue,
+            YearsInFuture    => $YearsInFuture,
+            YearsInPast      => $YearsInPast,
+
+            }
+    );
+
+    $Output .= $Self->{LayoutObject}->Footer();
+
+    return $Output;
+}
+1;
---
+++ var/httpd/htdocs/js/Core.Agent.Admin.DynamicFieldDateTime.js
@@ -0,0 +1,46 @@
+// --
+// Core.Agent.Admin.DynamicFieldDateTime.js - provides the special module functions for the Date Time Dynamic Fields.
+// Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+// --
+// $Id: Core.Agent.Admin.DynamicFieldDateTime.js,v 1.1 2011-08-22 17:42:58 cr Exp $
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core = Core || {};
+Core.Agent = Core.Agent || {};
+Core.Agent.Admin = Core.Agent.Admin || {};
+
+/**
+ * @namespace
+ * @exports TargetNS as Core.Agent.Admin.DynamicFielddropdown
+ * @description
+ *      This namespace contains the special module functions for the DynamicFieldDropdown module.
+ */
+Core.Agent.Admin.DynamicFieldDateTime = (function (TargetNS) {
+
+    /**
+     * @function
+     * @param {bool} YearsPeriod, 0 or 1 to show or hide the Years in Future and in Past fields.
+     * @return nothing
+     *      This function shows or hide two fields (Years In future and Years in Past) depending
+     *      on the YearsPeriod parameter
+     */
+
+    TargetNS.ToogleYearsPeriod = function (YearsPeriod){
+
+        if (YearsPeriod === '1') {
+            $('#YearsPeriodOption').removeClass('Hidden');
+        }
+        else {
+            $('#YearsPeriodOption').addClass('Hidden');
+        }
+        return false;
+    };
+
+    return TargetNS;
+}(Core.Agent.Admin.DynamicFieldDateTime || {}));
