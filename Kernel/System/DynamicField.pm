11055a5db3acc6eff5efc312f8c507764f37be16
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-08-24 03:55:49 +0000

    Changed from Binary to ASCII

--- Kernel/System/DynamicField.pm
+++ Kernel/System/DynamicField.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField.pm - DynamicFields configuration backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: DynamicField.pm,v 1.23 2011-08-24 03:37:23 cr Exp $
+# $Id: DynamicField.pm,v 1.24 2011-08-24 03:55:49 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -21,7 +21,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Kernel::System::Cache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.23 $) [1];
+$VERSION = qw($Revision: 1.24 $) [1];
 
 =head1 NAME
 
@@ -815,6 +815,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.23 $ $Date: 2011-08-24 03:37:23 $
+$Revision: 1.24 $ $Date: 2011-08-24 03:55:49 $
 
 =cut
