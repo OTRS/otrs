9037fee6ed6bc0d1f987db04d9d6cf740cf0cc6f
Stefan Bedorf <stefan.bedorf@otrs.com>
2011-09-03 16:40:50 +0000

    Added missing check for ConfigObject.

--- Kernel/Modules/AgentTicketMove.pm
+++ Kernel/Modules/AgentTicketMove.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketMove.pm - move tickets to queues
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketMove.pm,v 1.86 2011-08-18 19:11:53 en Exp $
+# $Id: AgentTicketMove.pm,v 1.87 2011-09-03 16:40:50 sb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::State;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.86 $) [1];
+$VERSION = qw($Revision: 1.87 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -28,7 +28,7 @@ sub new {
     bless( $Self, $Type );
 
     # check needed objects
-    for my $Needed (qw(ParamObject DBObject TicketObject LayoutObject LogObject)) {
+    for my $Needed (qw(ParamObject DBObject TicketObject LayoutObject LogObject ConfigObject)) {
         if ( !$Self->{$Needed} ) {
             $Self->{LayoutObject}->FatalError( Message => "Got no $Needed!" );
         }
