b0c6476548e877b2e7325cff5702e98377ad941f
Carlos Garcia <carlos.garcia@otrs.com>
2011-09-06 17:41:46 +0000

    Initial version for EditFieldRender function.

--- Kernel/System/DynamicField/Backend/Text.pm
+++ Kernel/System/DynamicField/Backend/Text.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField/Backend/Text.pm - Delegate for DynamicField Text backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Text.pm,v 1.10 2011-09-06 12:17:07 mg Exp $
+# $Id: Text.pm,v 1.11 2011-09-06 17:41:46 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Kernel::System::DynamicFieldValue;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 =head1 NAME
 
@@ -167,6 +167,81 @@ sub SearchSQLOrderFieldGet {
     my ( $Self, %Param ) = @_;
 
     return "$Param{TableAlias}.value_text";
+}
+
+=item EditFieldRender()
+
+creates the field HTML to be used in edit masks.
+
+    my $FieldHTML = $DynamicFieldTextObject->EditFieldRender(
+        DynamicFieldConfig   => $DynamicFieldConfig,      # complete config of the DynamicField
+        PossibleValuesFilter => ['value1', 'value2'],     # Optional. Some backends may support this.
+                                                          #     This may be needed to realize ACL support for ticket masks,
+                                                          #     where the possible values can be limited with and ACL.
+        Mandatory          => 1,                          # 0 or 1,
+    );
+
+=cut
+
+sub EditFieldRender {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{DynamicFieldConfig} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => "Need DynamicFieldConfig!" );
+        return;
+    }
+
+    # check DynamicFieldConfig (general)
+    if ( !IsHashRefWithData( $Param{DynamicFieldConfig} ) ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => "The field configuration is invalid",
+        );
+        return;
+    }
+
+    # check DynamicFieldConfig (internally)
+    for my $Needed (qw(ID Config Name )) {
+        if ( !$Param{DynamicFieldConfig}->{$Needed} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Needed in DynamicFieldConfig!"
+            );
+            return;
+        }
+    }
+    my $FieldConfig = $Param{DynamicFieldConfig}->{Config};
+
+    my $HTMLString =
+        '<input type="text" ' .
+        'class="DynamicFieldText" ' .
+        'id="' . $FieldConfig->{Name} . '"' .
+        'name="' . $FieldConfig->{Name} . '"' .
+        'title="' . $FieldConfig->{Label} . '"' .
+        'value="" ' .
+        '/>';
+
+    if ( $Param{Mandatory} ) {
+
+        # for client side validation
+        $HTMLString .=
+            '<div id="' . $FieldConfig->{Name} . 'Error" ' .
+            'class="TooltipErrorMessage">' .
+            '<p>$Text{"This field is required."}</p>' .
+            '</div>';
+
+        # for server side validation
+        $HTMLString .=
+            '<div id="' . $FieldConfig->{Name} . 'ServerError" ' .
+            'class="TooltipErrorMessage">' .
+            '<p>$Text{"This field is required."}</p>' .
+            '</div>';
+
+    }
+
+    return $HTMLString;
+
 }
 
 1;
