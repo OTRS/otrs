ce5dccd2a2324d002f954a8fc8b033b67e8190af
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-09-08 19:58:09 +0000

    fixed POD

--- Kernel/System/DynamicField/Backend/Checkbox.pm
+++ Kernel/System/DynamicField/Backend/Checkbox.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField/Backend/Checkbox.pm - Delegate for DynamicField Checkbox backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Checkbox.pm,v 1.10 2011-09-08 18:44:05 cr Exp $
+# $Id: Checkbox.pm,v 1.11 2011-09-08 19:58:09 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::DynamicFieldValue;
 use Kernel::System::DynamicField::Backend::BackendCommon;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 =head1 NAME
 
@@ -280,7 +280,7 @@ extracts the value of a dynamic field from the param object
 
     Returns
 
-    $Value = 'a text';
+    $Value = 1;
 
 =cut
 
