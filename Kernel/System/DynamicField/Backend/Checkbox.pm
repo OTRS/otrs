45a2a6812275f96cfb53ad69d47dfc35d6d61ca4
Carlos Garcia <carlos.garcia@otrs.com>
2011-09-13 22:30:20 +0000

    Removed commented line.

--- Kernel/System/DynamicField/Backend/Checkbox.pm
+++ Kernel/System/DynamicField/Backend/Checkbox.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField/Backend/Checkbox.pm - Delegate for DynamicField Checkbox backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Checkbox.pm,v 1.16 2011-09-13 22:25:33 cg Exp $
+# $Id: Checkbox.pm,v 1.17 2011-09-13 22:30:20 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::DynamicFieldValue;
 use Kernel::System::DynamicField::Backend::BackendCommon;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 =head1 NAME
 
@@ -176,7 +176,6 @@ sub EditFieldRender {
     my $FieldValue = $Self->EditFieldValueGet(
         %Param,
     );
-    print STDERR $Self->{MainObject}->Dump($FieldValue);
 
     # set values from ParamObject if present
     if ( defined $FieldValue ) {
