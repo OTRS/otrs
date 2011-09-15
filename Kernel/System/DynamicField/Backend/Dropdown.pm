d2f453ff25caf87a17572aa7f060cdfa7a9f8067
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-09-15 18:11:54 +0000

    Fixed EditFieldRender(), Translation and PossibleNone settings when builds the select.

--- Kernel/System/DynamicField/Backend/Dropdown.pm
+++ Kernel/System/DynamicField/Backend/Dropdown.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField/Backend/Dropdown.pm - Delegate for DynamicField Dropdown backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Dropdown.pm,v 1.18 2011-09-13 15:45:46 cg Exp $
+# $Id: Dropdown.pm,v 1.19 2011-09-15 18:11:54 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::DynamicFieldValue;
 use Kernel::System::DynamicField::Backend::BackendCommon;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.18 $) [1];
+$VERSION = qw($Revision: 1.19 $) [1];
 
 =head1 NAME
 
@@ -243,8 +243,8 @@ sub EditFieldRender {
         Data         => $SelectionData,
         Name         => $FieldName,
         SelectedID   => $Value,
-        Translation  => 0,
-        PossibleNone => 1,
+        Translation  => $FieldConfig->{TranslatableValues} || 0,
+        PossibleNone => $FieldConfig->{PossibleNone} || 0,
         Class        => $FieldClass,
         HTMLQuote    => 1,
     );
