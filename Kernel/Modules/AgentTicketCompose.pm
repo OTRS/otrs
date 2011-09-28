c21ab39ca85848908e0c38ba11c3414b9f25f1e9
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-09-28 02:39:08 +0000

    Added Perl code to support Dynamic Fields order customization DTL blocks, and variable names simplification.

--- Kernel/Modules/AgentTicketCompose.pm
+++ Kernel/Modules/AgentTicketCompose.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketCompose.pm - to compose and send a message
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketCompose.pm,v 1.138 2011-09-20 22:02:42 cr Exp $
+# $Id: AgentTicketCompose.pm,v 1.139 2011-09-28 02:39:08 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -27,7 +27,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.138 $) [1];
+$VERSION = qw($Revision: 1.139 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1160,10 +1160,19 @@ sub _Mask {
         $Self->{LayoutObject}->Block(
             Name => 'DynamicField',
             Data => {
-                DynamicFieldLabel => $DynamicFieldHTML->{Label},
-                DynamicField      => $DynamicFieldHTML->{Field},
+                Label => $DynamicFieldHTML->{Label},
+                Field => $DynamicFieldHTML->{Field},
             },
         );
+
+        # example of dynamic fields order customization
+        #        $Self->{LayoutObject}->Block(
+        #            Name => 'DynamicField_' . $DynamicFieldConfig->{Name},
+        #            Data => {
+        #                Label => $DynamicFieldHTML->{Label},
+        #                Field      => $DynamicFieldHTML->{Field},
+        #            },
+        #        );
     }
 
     # show time accounting box
