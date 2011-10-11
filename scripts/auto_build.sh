5053b24571b58b3beeb3e1c845f65cd14cb8a8bd
Martin Gruner <martin.gruner@otrs.com>
2011-10-11 11:39:08 +0000

    Added diagnostic output. Remove source code packages before bulding them to avoid stale files from older builds.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: auto_build.sh,v 1.61.2.14 2011-10-11 07:30:00 mg Exp $
+# $Id: auto_build.sh,v 1.61.2.15 2011-10-11 11:39:08 mg Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -21,7 +21,7 @@
 # or see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.61.2.14 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.61.2.15 $>"
 echo "Copyright (C) 2001-2011 OTRS AG, http://otrs.org/\n";
 
 PATH_TO_CVS_SRC=$1
@@ -217,6 +217,8 @@ mv iPhoneHandle*.opm var/packages/
 # --
 cd $PACKAGE_BUILD_DIR/ || exit 1;
 SOURCE_LOCATION=$SYSTEM_SOURCE_DIR/$PACKAGE-$VERSION.tar.gz
+rm $SOURCE_LOCATION
+echo "Building tar.gz..."
 tar -czf $SOURCE_LOCATION $ARCHIVE_DIR/ || exit 1;
 cp $SOURCE_LOCATION $PACKAGE_DEST_DIR/
 
@@ -225,6 +227,8 @@ cp $SOURCE_LOCATION $PACKAGE_DEST_DIR/
 # --
 cd $PACKAGE_BUILD_DIR/ || exit 1;
 SOURCE_LOCATION=$SYSTEM_SOURCE_DIR/$PACKAGE-$VERSION.tar.bz2
+rm $SOURCE_LOCATION
+echo "Building tar.bz2..."
 tar -cjf $SOURCE_LOCATION $ARCHIVE_DIR/ || exit 1;
 cp $SOURCE_LOCATION $PACKAGE_DEST_DIR/
 
@@ -233,6 +237,8 @@ cp $SOURCE_LOCATION $PACKAGE_DEST_DIR/
 # --
 cd $PACKAGE_BUILD_DIR/ || exit 1;
 SOURCE_LOCATION=$SYSTEM_SOURCE_DIR/$PACKAGE-$VERSION.zip
+rm $SOURCE_LOCATION
+echo "Building zip..."
 zip -r $SOURCE_LOCATION $ARCHIVE_DIR/ || exit 1;
 cp $SOURCE_LOCATION $PACKAGE_DEST_DIR/
 
