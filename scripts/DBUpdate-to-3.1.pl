d614f85b040d985e816a69acd41ff96ea1244d4a
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-10-26 18:44:53 +0000

    Changed from Binary to ASCII.

--- scripts/DBUpdate-to-3.1.pl
+++ scripts/DBUpdate-to-3.1.pl
@@ -3,7 +3,7 @@
 # DBUpdate-to-3.1.pl - update script to migrate OTRS 3.0.x to 3.1.x
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: DBUpdate-to-3.1.pl,v 1.27 2011-09-30 13:03:13 mg Exp $
+# $Id: DBUpdate-to-3.1.pl,v 1.28 2011-10-26 18:44:53 cr Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . '/Kernel/cpan-lib';
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.27 $) [1];
+$VERSION = qw($Revision: 1.28 $) [1];
 
 use Getopt::Std qw();
 use Kernel::Config;
