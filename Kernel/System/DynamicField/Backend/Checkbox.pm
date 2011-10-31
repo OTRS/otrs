828cc48f05829da82b01b312d59015271328df73
Martin Gruner <martin.gruner@otrs.com>
2011-10-31 09:36:16 +0000

    String cutting not needed.

--- Kernel/System/DynamicField/Backend/Checkbox.pm
+++ Kernel/System/DynamicField/Backend/Checkbox.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField/Backend/Checkbox.pm - Delegate for DynamicField Checkbox backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Checkbox.pm,v 1.34 2011-10-27 17:47:04 cg Exp $
+# $Id: Checkbox.pm,v 1.35 2011-10-31 09:36:16 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::DynamicFieldValue;
 use Kernel::System::DynamicField::Backend::BackendCommon;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.34 $) [1];
+$VERSION = qw($Revision: 1.35 $) [1];
 
 =head1 NAME
 
@@ -495,10 +495,6 @@ sub ReadableValueRender {
 
     # Title is always equal to Value
     my $Title = $Value;
-
-    # cut strings if needed
-    $Value = substr $Value, 0, $Param{ValueMaxChars} if $Param{ValueMaxChars};
-    $Title = substr $Title, 0, $Param{TitleMaxChars} if $Param{TitleMaxChars};
 
     # create return structure
     my $Data = {
--- Kernel/System/DynamicField/Backend/Date.pm
+++ Kernel/System/DynamicField/Backend/Date.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField/Backend/Date.pm - Delegate for DynamicField Date backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Date.pm,v 1.31 2011-10-27 17:47:04 cg Exp $
+# $Id: Date.pm,v 1.32 2011-10-31 09:36:16 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::Time;
 use Kernel::System::DynamicField::Backend::BackendCommon;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.31 $) [1];
+$VERSION = qw($Revision: 1.32 $) [1];
 
 =head1 NAME
 
@@ -670,10 +670,6 @@ sub ReadableValueRender {
 
     # Title is always equal to Value
     my $Title = $Value;
-
-    # cut strings if needed
-    $Value = substr $Value, 0, $Param{ValueMaxChars} if $Param{ValueMaxChars};
-    $Title = substr $Title, 0, $Param{TitleMaxChars} if $Param{TitleMaxChars};
 
     my $Data = {
         Value => $Value,
