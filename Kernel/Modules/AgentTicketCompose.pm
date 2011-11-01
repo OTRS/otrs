017dbc65afa2f2ce1a70a2e8c9de805f6a2dce97
Dennis Schmelter <dennis.schmelter@otrs.com>
2011-11-01 09:55:49 +0000

    Fixed bug#7823 - ACL could remove default state from ticket masks.

--- Kernel/Modules/AgentTicketCompose.pm
+++ Kernel/Modules/AgentTicketCompose.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketCompose.pm - to compose and send a message
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketCompose.pm,v 1.140 2011-10-24 21:47:16 cr Exp $
+# $Id: AgentTicketCompose.pm,v 1.141 2011-11-01 09:55:49 des Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -27,7 +27,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.140 $) [1];
+$VERSION = qw($Revision: 1.141 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1046,9 +1046,8 @@ sub _Mask {
     my ( $Self, %Param ) = @_;
 
     # build next states string
-    if ( !$Self->{Config}->{StateDefault} ) {
-        $Param{NextStates}->{''} = '-';
-    }
+    $Param{NextStates}->{''} = '-';
+
     my %State;
     if ( $Param{GetParam}->{StateID} ) {
         $State{SelectedID} = $Param{GetParam}->{StateID};
--- Kernel/Modules/AgentTicketForward.pm
+++ Kernel/Modules/AgentTicketForward.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketForward.pm - to forward a message
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketForward.pm,v 1.114 2011-10-24 21:47:16 cr Exp $
+# $Id: AgentTicketForward.pm,v 1.115 2011-11-01 09:54:51 des Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -26,7 +26,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.114 $) [1];
+$VERSION = qw($Revision: 1.115 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -867,9 +867,7 @@ sub _Mask {
     my ( $Self, %Param ) = @_;
 
     # add none selection to the list
-    if ( !$Self->{Config}->{StateDefault} ) {
-        $Param{NextStates}->{''} = '-';
-    }
+    $Param{NextStates}->{''} = '-';
 
     # build next states string
     my %State;
