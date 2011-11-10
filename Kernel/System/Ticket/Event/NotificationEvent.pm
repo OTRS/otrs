227128b079ff55dcda41014eba153d74b507f509
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-11-10 22:38:22 +0000

    Fixed typo.

--- Kernel/System/Ticket/Event/NotificationEvent.pm
+++ Kernel/System/Ticket/Event/NotificationEvent.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket/Event/NotificationEvent.pm - a event module to send notifications
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: NotificationEvent.pm,v 1.32 2011-11-08 10:25:19 mg Exp $
+# $Id: NotificationEvent.pm,v 1.33 2011-11-10 22:38:22 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::NotificationEvent;
 use Kernel::System::SystemAddress;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.32 $) [1];
+$VERSION = qw($Revision: 1.33 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -509,7 +509,7 @@ sub _SendNotification {
                 Name => $FieldName,
             );
 
-            # get dysplay value
+            # get display value
             my $ValueStrg = $Self->{TicketObject}->{DynamicFieldBackendObject}->ReadableValueRender(
                 DynamicFieldConfig => $DynamicField,
                 Value              => $DisplayValue,
