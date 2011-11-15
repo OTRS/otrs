6e776172bef790504760ce92d390743ec7a061a0
Dennis Schmelter <dennis.schmelter@otrs.com>
2011-11-15 10:21:22 +0000

    Improved code for fixed bug#7879 - Ticket forward not working if content-id exists.

--- Kernel/Modules/AgentTicketForward.pm
+++ Kernel/Modules/AgentTicketForward.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketForward.pm - to forward a message
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketForward.pm,v 1.118 2011-11-10 11:55:18 des Exp $
+# $Id: AgentTicketForward.pm,v 1.119 2011-11-15 10:21:22 des Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -26,7 +26,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.118 $) [1];
+$VERSION = qw($Revision: 1.119 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1039,10 +1039,15 @@ sub _Mask {
 
     # show attachments
     for my $Attachment ( @{ $Param{Attachments} } ) {
-        next
-            if $Attachment->{ContentID}
-                && $Self->{LayoutObject}->{BrowserRichText}
-                && ( $Attachment->{ContentType} =~ /image/i );
+        if (
+            $Attachment->{ContentID}
+            && $Self->{LayoutObject}->{BrowserRichText}
+            && ( $Attachment->{ContentType} =~ /image/i )
+            )
+        {
+            next;
+        }
+
         $Self->{LayoutObject}->Block(
             Name => 'Attachment',
             Data => $Attachment,
