38b77af0191be920cbf1fa2690d1d3786255f13c
Dennis Schmelter <dennis.schmelter@otrs.com>
2011-11-17 15:28:08 +0000

    Added comment that function SendCustomerNotification is deprecated.

--- Kernel/System/Ticket/Article.pm
+++ Kernel/System/Ticket/Article.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket/Article.pm - global article module for OTRS kernel
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Article.pm,v 1.294 2011-11-16 20:43:53 mb Exp $
+# $Id: Article.pm,v 1.295 2011-11-17 15:28:08 des Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -23,7 +23,7 @@ use Kernel::System::EmailParser;
 use Kernel::System::VariableCheck qw(:all);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.294 $) [1];
+$VERSION = qw($Revision: 1.295 $) [1];
 
 =head1 NAME
 
@@ -2458,6 +2458,8 @@ sub SendAgentNotification {
 
 =item SendCustomerNotification()
 
+DEPRECATED. This function is incompatible with the rich text editor, don't use it any more!
+
 send a customer notification via email
 
     my $ArticleID = $TicketObject->SendCustomerNotification(
@@ -3532,6 +3534,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.294 $ $Date: 2011-11-16 20:43:53 $
+$Revision: 1.295 $ $Date: 2011-11-17 15:28:08 $
 
 =cut
