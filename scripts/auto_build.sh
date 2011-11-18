8c6a5e1448d8f09592853b89548f983039de0bd1
Martin Gruner <martin.gruner@otrs.com>
2011-11-18 10:01:06 +0000

    Disable IPhonePackage until it is ported.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: auto_build.sh,v 1.101 2011-11-18 09:56:22 mg Exp $
+# $Id: auto_build.sh,v 1.102 2011-11-18 10:01:06 mg Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -21,7 +21,7 @@
 # or see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.101 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.102 $>"
 echo "Copyright (C) 2001-2011 OTRS AG, http://otrs.org/\n";
 
 PATH_TO_CVS_SRC=$1
@@ -40,7 +40,7 @@ RPM_BUILD="rpmbuild"
 #RPM_BUILD="rpm"
 
 SUPPORT_PACKAGE="http://ftp2.otrs.org/pub/otrs/packages/Support-1.3.1.opm"
-IPHONE_PACKAGE="http://ftp.otrs.org/pub/otrs/packages/iPhoneHandle-1.0.3.opm"
+#IPHONE_PACKAGE="http://ftp.otrs.org/pub/otrs/packages/iPhoneHandle-1.0.3.opm"
 MANUAL_EN="http://ftp.otrs.org/pub/otrs/doc/doc-admin/3.1/en/pdf/otrs_admin_book.pdf"
 MANUAL_DE="http://ftp.otrs.org/pub/otrs/doc/doc-admin/3.1/de/pdf/otrs_admin_book.pdf"
 
@@ -171,11 +171,16 @@ bin/otrs.CheckSum.pl -a create
 
 # add pre installed packages
 mkdir var/packages/
-wget "$SUPPORT_PACKAGE"
-mv Support*.opm var/packages/
 
-wget "$IPHONE_PACKAGE"
-mv iPhoneHandle*.opm var/packages/
+if test $SUPPORT_PACKAGE
+    wget "$SUPPORT_PACKAGE"
+    mv Support*.opm var/packages/
+fi
+
+if test $IPHONE_PACKAGE
+    wget "$IPHONE_PACKAGE"
+    mv iPhoneHandle*.opm var/packages/
+fi
 
 # --
 # create tar
