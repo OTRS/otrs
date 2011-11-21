2ec8c68d343dfd5d54bab4b5072e9c8041793be0
Martin Gruner <martin.gruner@otrs.com>
2011-11-21 10:29:13 +0000

    Improved HTTPS detection, thanks to Michiel.

--- Kernel/System/Web/InterfaceAgent.pm
+++ Kernel/System/Web/InterfaceAgent.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Web/InterfaceAgent.pm - the agent interface file (incl. auth)
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: InterfaceAgent.pm,v 1.61 2011-11-21 10:20:47 mg Exp $
+# $Id: InterfaceAgent.pm,v 1.62 2011-11-21 10:29:13 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION @INC);
-$VERSION = qw($Revision: 1.61 $) [1];
+$VERSION = qw($Revision: 1.62 $) [1];
 
 # all framework needed modules
 use Kernel::Config;
@@ -334,7 +334,7 @@ sub Run {
         }
 
         my $SecureAttribute;
-        if ( $ENV{HTTPS} && $ENV{HTTPS} =~ m/^on$/ismx ) {
+        if ( $Self->{ConfigObject}->Get('HttpType') eq 'https' ) {
 
             # Restrict Cookie to HTTPS if it is used.
             $SecureAttribute = 1;
@@ -935,6 +935,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.61 $ $Date: 2011-11-21 10:20:47 $
+$Revision: 1.62 $ $Date: 2011-11-21 10:29:13 $
 
 =cut
