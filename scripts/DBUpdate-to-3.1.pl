68114ea31353ee5ca530525e92d1901fdd4d037d
Udo Bretz <udo.bretz@otrs.com>
2011-11-24 16:40:54 +0000

    Small comment fix.

--- scripts/DBUpdate-to-3.1.pl
+++ scripts/DBUpdate-to-3.1.pl
@@ -3,7 +3,7 @@
 # DBUpdate-to-3.1.pl - update script to migrate OTRS 3.0.x to 3.1.x
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: DBUpdate-to-3.1.pl,v 1.63 2011-11-23 18:30:17 cg Exp $
+# $Id: DBUpdate-to-3.1.pl,v 1.64 2011-11-24 16:40:54 ub Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -31,7 +31,7 @@ use lib dirname($RealBin);
 use lib dirname($RealBin) . '/Kernel/cpan-lib';
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.63 $) [1];
+$VERSION = qw($Revision: 1.64 $) [1];
 
 use Getopt::Std qw();
 use Kernel::Config;
@@ -1212,7 +1212,8 @@ sub _MigrateFreeFieldsConfiguration {
 
                 if ( $FreeField eq 'ArticleFreeText' ) {
 
-         # If the corresponding key has only one possible value for this entry, use it as the label.
+                    # If the corresponding key has only one possible value for this entry,
+                    # use it as the label.
                     my $KeyName      = 'ArticleFreeKey' . $Index;
                     my $PossibleKeys = my $PossibleValues
                         = $CommonObject->{ConfigObject}->Get($KeyName);
