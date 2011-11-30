6998083f91f30f0d9a95abb7d7b09231bad8f899
Michiel Beijen <michiel.beijen@otrs.com>
2011-11-30 09:10:30 +0000

    Fixed bug#6715 - Setting CustomerID with otrs.AddCustomerUser.pl

--- bin/otrs.AddCustomerUser.pl
+++ bin/otrs.AddCustomerUser.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl -w
 # --
 # bin/otrs.AddCustomerUser.pl - Add User from CLI
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -30,10 +29,10 @@ use lib dirname($RealBin) . "/Kernel/cpan-lib";
 
 use vars qw (%opts);
 use Getopt::Std;
-getopt( 'flpge', \%opts );
+getopt( 'flpgec', \%opts );
 unless ( $ARGV[0] ) {
     print
-        "$FindBin::Script [-f firstname] [-l lastname] [-p password] [-g groupname] [-e email] username\n";
+        "$FindBin::Script [-f firstname] [-l lastname] [-p password] [-g groupname] [-e email] [-c CustomerID] username\n";
     print "\tif you define -g with a valid group name then the user will be added that group\n";
     print "\n";
     exit;
@@ -52,7 +51,7 @@ my %CommonObject = ();
 $CommonObject{ConfigObject} = Kernel::Config->new(%CommonObject);
 $CommonObject{EncodeObject} = Kernel::System::Encode->new(%CommonObject);
 $CommonObject{LogObject}
-    = Kernel::System::Log->new( %CommonObject, LogPrefix => 'otrs.AddCustomerUser.pl' );
+    = Kernel::System::Log->new( %CommonObject, LogPrefix => 'OTRS-otrs.AddCustomerUser.pl' );
 $CommonObject{TimeObject} = Kernel::System::Time->new(%CommonObject);
 $CommonObject{MainObject} = Kernel::System::Main->new(%CommonObject);
 $CommonObject{DBObject}   = Kernel::System::DB->new(%CommonObject);
@@ -70,7 +69,7 @@ $Param{ValidID} = '1';
 $Param{Source}         = 'CustomerUser';
 $Param{UserFirstname}  = $opts{f};
 $Param{UserLastname}   = $opts{l};
-$Param{UserCustomerID} = $ARGV[0];
+$Param{UserCustomerID} = defined $opts{c} ? $opts{c} : $ARGV[0];
 $Param{UserLogin}      = $ARGV[0];
 $Param{UserPassword}   = $opts{p};
 $Param{UserEmail}      = $opts{e};
