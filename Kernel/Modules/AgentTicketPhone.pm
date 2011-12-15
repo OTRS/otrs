2ea755b7f93673861cabc1b5f5c84546add329a2
Carlos Garcia <carlos.garcia@otrs.com>
2011-12-15 14:24:44 +0000

    Added check for duplicated entries on address list.

--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPhone.pm - to handle phone calls
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPhone.pm,v 1.219 2011-12-09 22:23:23 cr Exp $
+# $Id: AgentTicketPhone.pm,v 1.220 2011-12-15 14:24:44 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -27,7 +27,7 @@ use Mail::Address;
 use Kernel::System::Service;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.219 $) [1];
+$VERSION = qw($Revision: 1.220 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -106,6 +106,9 @@ sub Run {
         = $Self->{ParamObject}->GetParam( Param => 'CustomerTicketCounterFromCustomer' ) || 0;
     my $Selected = $Self->{ParamObject}->GetParam( Param => 'CustomerSelected' ) || '';
 
+    # hash for check duplicated entries
+    my %AddressesList;
+
     if ($CustomersNumber) {
         my $CustomerCounter = 1;
         for my $Count ( 1 ... $CustomersNumber ) {
@@ -135,6 +138,12 @@ sub Run {
                         }
                     }
 
+                    # check for duplicated entries
+                    if ( defined $AddressesList{$CustomerElement} && $CustomerError eq '' ) {
+                        $CustomerErrorMsg = 'IsDuplicatedServerErrorMsg';
+                        $CustomerError    = 'ServerError';
+                    }
+
                     if ( $CustomerError ne '' ) {
                         $CustomerDisabled = 'disabled="disabled"';
                         $CountAux         = $Count . 'Error';
@@ -151,6 +160,7 @@ sub Run {
                     CustomerDisabled => $CustomerDisabled,
                 };
             }
+            $AddressesList{$CustomerElement} = 1;
         }
     }
 
