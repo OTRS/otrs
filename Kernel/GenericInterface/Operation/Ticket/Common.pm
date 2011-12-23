21127b258e23f95fcc61eb8af81a9c8f55f18958
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-12-23 18:43:08 +0000

    Fixed error parameter to be just one hash.

--- Kernel/GenericInterface/Operation/Ticket/Common.pm
+++ Kernel/GenericInterface/Operation/Ticket/Common.pm
@@ -2,7 +2,7 @@
 # Kernel/GenericInterface/Operation/Ticket/Common.pm - Ticket common operation functions
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: Common.pm,v 1.1 2011-12-23 18:15:13 cr Exp $
+# $Id: Common.pm,v 1.2 2011-12-23 18:43:08 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -135,12 +135,10 @@ sub _ReturnError {
         Success      => 1,
         ErrorMessage => "$Param{ErrorCode}: $Param{ErrorMessage}",
         Data         => {
-            Errors => [
-                {
-                    ErrorCode    => $Param{ErrorCode},
-                    ErrorMessage => $Param{ErrorMessage},
-                }
-            ],
+            Error => {
+                ErrorCode    => $Param{ErrorCode},
+                ErrorMessage => $Param{ErrorMessage},
+            },
         },
     };
 }
@@ -163,6 +161,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.1 $ $Date: 2011-12-23 18:15:13 $
+$Revision: 1.2 $ $Date: 2011-12-23 18:43:08 $
 
 =cut
