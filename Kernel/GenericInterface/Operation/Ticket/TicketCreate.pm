9d4ace32cee4c6e1ef0658b80e563eed49a94e80
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-12-24 00:39:50 +0000

    Initial version.

---
+++ Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm
@@ -0,0 +1,165 @@
+# --
+# Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm - GenericInterface Ticket TicketCreate operation backend
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: TicketCreate.pm,v 1.1 2011-12-24 00:39:50 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::GenericInterface::Operation::Ticket::TicketCreate;
+
+use strict;
+use warnings;
+
+use Kernel::GenericInterface::Operation::Ticket::Common;
+use Kernel::System::VariableCheck qw(IsArrayRefWithData IsHashRefWithData IsStringWithData);
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::GenericInterface::Operation::Ticket::TicketCreate - GenericInterface Ticket TicketCreate Operation backend
+
+=head1 SYNOPSIS
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::GenericInterface::Operation->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (
+        qw(
+        DebuggerObject ConfigObject MainObject LogObject TimeObject DBObject EncodeObject
+        WebserviceID
+        )
+        )
+    {
+        if ( !$Param{$Needed} ) {
+            return {
+                Success      => 0,
+                ErrorMessage => "Got no $Needed!"
+            };
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    $Self->{TicketCommonObject}
+        = Kernel::GenericInterface::Operation::Ticket::Common->new( %{$Self} );
+
+    return $Self;
+}
+
+=item Run()
+
+perform TicketCreate Operation. This will return the created ticket number.
+
+    my $Result = $OperationObject->Run(
+        Data => {
+        },
+    );
+
+    $Result = {
+        Success         => 1,                       # 0 or 1
+        ErrorMessage    => '',                      # in case of error
+        Data            => {                        # result data payload after Operation
+            TicketID    => 123,                     # Ticket  ID number in OTRS (help desk system)
+            ArticleID   => 43,                      # Article ID number in OTRS (help desk system)
+            Errors => {                         # should not return errors
+                item => {
+                    ErrorCode1   => 1
+                    ErrorCode2   => 32
+                    ErrorCode3   => undef
+                    ErrorMessage => 'Error Description'
+                },
+            },
+        },
+    };
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(UserLogin)) {
+        if ( !$Param{Data}->{$Needed} ) {
+            return $Self->{TicketCommonObject}->ReturnError(
+                ErrorCode    => 'TicketCreate.MissingParameter',
+                ErrorMessage => "TicketCreate: $Needed parameter is missing!",
+            );
+        }
+    }
+
+    # check needed hashes
+    for my $Needed (qw(Ticket Article)) {
+        if ( !IsHashRefWithData( $Param{Data}->{$Needed} ) ) {
+            return $Self->{TicketCommonObject}->ReturnError(
+                ErrorCode    => 'TicketCreate.MissingParameter',
+                ErrorMessage => "TicketCreate: $Needed parameter is missing or not valid!",
+            );
+        }
+    }
+
+    # check optional array/hashes
+    for my $Optional (qw(DynamicField Attachment)) {
+        if (
+            defined $Param{Data}->{$Optional}
+            && !IsHashRefWithData( $Param{Data}->{$Optional} )
+            && !IsArrayRefWithData( $Param{Data}->{$Optional} )
+            )
+        {
+            return $Self->{TicketCommonObject}->ReturnError(
+                ErrorCode    => 'TicketCreate.MissingParameter',
+                ErrorMessage => "TicketCreate: $Optional parameter is missing or not valid!",
+            );
+        }
+    }
+
+    return {
+        Success => 1,
+        Data    => {
+            Test => 'Test OK',
+        },
+        }
+
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2011-12-24 00:39:50 $
+
+=cut
