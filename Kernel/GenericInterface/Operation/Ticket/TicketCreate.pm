d6fc0cbcc2137879a89f7e8097e219da6b8a6d19
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-12-26 18:26:57 +0000

    Added Type validation.

--- Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm
@@ -2,7 +2,7 @@
 # Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm - GenericInterface Ticket TicketCreate operation backend
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: TicketCreate.pm,v 1.3 2011-12-26 17:56:39 cr Exp $
+# $Id: TicketCreate.pm,v 1.4 2011-12-26 18:26:57 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::GenericInterface::Operation::Ticket::Common;
 use Kernel::System::VariableCheck qw(IsArrayRefWithData IsHashRefWithData IsStringWithData);
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 =head1 NAME
 
@@ -176,6 +176,32 @@ sub Run {
         );
     }
 
+    # check Ticket->Type
+    # Ticket type could be required or not depending on sysconfig option
+    if (
+        !$Ticket->{TypeID}
+        && !$Ticket->{Type}
+        && $Self->{ConfigObject}->Get('Ticket::Type')
+        )
+    {
+        return $Self->{TicketCommonObject}->ReturnError(
+            ErrorCode    => 'TicketCreate.MissingParameter',
+            ErrorMessage => "TicketCreate: Ticket->TypeID or Ticket->Type parameter is required"
+                . " by sysconfig option!",
+        );
+    }
+    if ( $Ticket->{TypeID} || $Ticket->{Type} ) {
+        if ( !$Self->{TicketCommonObject}->ValidateType( %{$Ticket} ) ) {
+            return $Self->{TicketCommonObject}->ReturnError(
+                ErrorCode => 'TicketCreate.InvalidParameter',
+                ErrorMessage =>
+                    "TicketCreate: Ticket->TypeID or Ticket->Type parameter is invalid!",
+            );
+        }
+    }
+
+    # check for not required parameters
+
     return {
         Success => 1,
         Data    => {
@@ -200,6 +226,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.3 $ $Date: 2011-12-26 17:56:39 $
+$Revision: 1.4 $ $Date: 2011-12-26 18:26:57 $
 
 =cut
