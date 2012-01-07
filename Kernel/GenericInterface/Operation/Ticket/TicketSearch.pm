ddcf3dc9be1e722d57c92ca0b4501b2abb8079ee
Carlos Garcia <carlos.garcia@otrs.com>
2012-01-07 00:06:37 +0000

    Modified from Binary to ASCII.

--- Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm - GenericInterface Ticket Get operation backend
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: TicketSearch.pm,v 1.1 2012-01-07 00:06:00 cg Exp $
+# $Id: TicketSearch.pm,v 1.2 2012-01-07 00:06:37 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::GenericInterface::Operation::Ticket::Common;
 use Kernel::System::VariableCheck qw(IsArrayRefWithData IsHashRefWithData IsStringWithData);
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 =head1 NAME
 
@@ -533,6 +533,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.1 $ $Date: 2012-01-07 00:06:00 $
+$Revision: 1.2 $ $Date: 2012-01-07 00:06:37 $
 
 =cut
