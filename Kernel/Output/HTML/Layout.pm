f27ec7c74f2baae4a1a07480fec5000a9e238a90
Martin Gruner <martin.gruner@otrs.com>
2012-01-23 13:45:30 +0000

    Improved wording.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Layout.pm,v 1.379 2012-01-21 20:32:52 sb Exp $
+# $Id: Layout.pm,v 1.380 2012-01-23 13:45:30 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Mail::Address;
 use URI::Escape qw();
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.379 $) [1];
+$VERSION = qw($Revision: 1.380 $) [1];
 
 =head1 NAME
 
@@ -591,8 +591,8 @@ sub Output {
         TemplateFile => $Param{TemplateFile} || '',
     );
 
-    # improve dtl performance of larg pages, see also bug#7267
-    # thanks to Stelios Gikas <stelios.gikas@noris.net>
+    # Improve dtl performance of large pages, see also bug#7267.
+    # Thanks to Stelios Gikas <stelios.gikas@noris.net>!
     my @OutputLines = split /\n/, $Output;
     for my $Output (@OutputLines) {
 
@@ -4905,6 +4905,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.379 $ $Date: 2012-01-21 20:32:52 $
+$Revision: 1.380 $ $Date: 2012-01-23 13:45:30 $
 
 =cut
