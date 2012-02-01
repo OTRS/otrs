c01333a257e6d69dabb1d37654fd3a5d9cbd0875
Carlos Garcia <carlos.garcia@otrs.com>
2012-02-01 21:56:48 +0000

    Modified POD.

--- Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm - GenericInterface Ticket Search operation backend
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: TicketSearch.pm,v 1.12 2012-01-26 15:01:09 mg Exp $
+# $Id: TicketSearch.pm,v 1.13 2012-02-01 21:56:48 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Kernel::GenericInterface::Operation::Common;
 use Kernel::GenericInterface::Operation::Ticket::Common;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.12 $) [1];
+$VERSION = qw($Revision: 1.13 $) [1];
 
 =head1 NAME
 
@@ -83,22 +83,192 @@ sub new {
 perform TicketSearch Operation. This will return a Ticket ID list.
 
     my $Result = $OperationObject->Run(
-        Data => {
-            TicketID => '32,33',
-            DynamicFields     => 0, # Optional, 0 as default
-            Extended          => 1, # Optional 0 as default
-            AllArticles       => 1, # Optional 0 as default
-            ArticleSenderType => [ $ArticleSenderType1, $ArticleSenderType2 ], # Optional, only requested article sender types
-            ArticleOrder      => 'DESC', # Optional, DESC,ASC - default is ASC
-            ArticleLimit      => 5, # Optional
-            Attachments       => 1, # Optional, 1 as default
+        # ticket number (optional) as STRING or as ARRAYREF
+        TicketNumber => '%123546%',
+        TicketNumber => ['%123546%', '%123666%'],
+
+        # ticket title (optional) as STRING or as ARRAYREF
+        Title => '%SomeText%',
+        Title => ['%SomeTest1%', '%SomeTest2%'],
+
+        Queues   => ['system queue', 'other queue'],
+        QueueIDs => [1, 42, 512],
+
+        # use also sub queues of Queue|Queues in search
+        UseSubQueues => 0,
+
+        # You can use types like normal, ...
+        Types   => ['normal', 'change', 'incident'],
+        TypeIDs => [3, 4],
+
+        # You can use states like new, open, pending reminder, ...
+        States   => ['new', 'open'],
+        StateIDs => [3, 4],
+
+        # (Open|Closed) tickets for all closed or open tickets.
+        StateType => 'Open',
+
+        # You also can use real state types like new, open, closed,
+        # pending reminder, pending auto, removed and merged.
+        StateType    => ['open', 'new'],
+        StateTypeIDs => [1, 2, 3],
+
+        Priorities  => ['1 very low', '2 low', '3 normal'],
+        PriorityIDs => [1, 2, 3],
+
+        Services   => ['Service A', 'Service B'],
+        ServiceIDs => [1, 2, 3],
+
+        SLAs   => ['SLA A', 'SLA B'],
+        SLAIDs => [1, 2, 3],
+
+        Locks   => ['unlock'],
+        LockIDs => [1, 2, 3],
+
+        OwnerIDs => [1, 12, 455, 32]
+
+        ResponsibleIDs => [1, 12, 455, 32]
+
+        WatchUserIDs => [1, 12, 455, 32]
+
+        # CustomerID (optional) as STRING or as ARRAYREF
+        CustomerID => '123',
+        CustomerID => ['123', 'ABC'],
+
+        # CustomerUserLogin (optional) as STRING as ARRAYREF
+        CustomerUserLogin => 'uid123',
+        CustomerUserLogin => ['uid123', 'uid777'],
+
+        # create ticket properties (optional)
+        CreatedUserIDs     => [1, 12, 455, 32]
+        CreatedTypes       => ['normal', 'change', 'incident'],
+        CreatedTypeIDs     => [1, 2, 3],
+        CreatedPriorities  => ['1 very low', '2 low', '3 normal'],
+        CreatedPriorityIDs => [1, 2, 3],
+        CreatedStates      => ['new', 'open'],
+        CreatedStateIDs    => [3, 4],
+        CreatedQueues      => ['system queue', 'other queue'],
+        CreatedQueueIDs    => [1, 42, 512],
+
+        # DynamicFields
+        #   At least one operator must be specified. Operators will be connected with AND,
+        #       values in an operator with OR.
+        #   You can also pass more than one argument to an operator: ['value1', 'value2']
+        DynamicField_FieldNameX => {
+            Equals            => 123,
+            Like              => 'value*',                # "equals" operator with wildcard support
+            GreaterThan       => '2001-01-01 01:01:01',
+            GreaterThanEquals => '2001-01-01 01:01:01',
+            LowerThan         => '2002-02-02 02:02:02',
+            LowerThanEquals   => '2002-02-02 02:02:02',
+        }
+
+        # article stuff (optional)
+        From    => '%spam@example.com%',
+        To      => '%support@example.com%',
+        Cc      => '%client@example.com%',
+        Subject => '%VIRUS 32%',
+        Body    => '%VIRUS 32%',
+
+        # use full article text index if configured (optional, default off)
+        FullTextIndex => 1,
+
+        # article content search (AND or OR for From, To, Cc, Subject and Body) (optional)
+        ContentSearch => 'AND',
+
+        # content conditions for From,To,Cc,Subject,Body
+        # Title,CustomerID and CustomerUserLogin (all optional)
+        ConditionInline => 1,
+
+        # articles created more than 60 minutes ago (article older than 60 minutes) (optional)
+        ArticleCreateTimeOlderMinutes => 60,
+        # articles created less than 120 minutes ago (article newer than 60 minutes) (optional)
+        ArticleCreateTimeNewerMinutes => 120,
+
+        # articles with create time after ... (article newer than this date) (optional)
+        ArticleCreateTimeNewerDate => '2006-01-09 00:00:01',
+        # articles with created time before ... (article older than this date) (optional)
+        ArticleCreateTimeOlderDate => '2006-01-19 23:59:59',
+
+        # tickets created more than 60 minutes ago (ticket older than 60 minutes)  (optional)
+        TicketCreateTimeOlderMinutes => 60,
+        # tickets created less than 120 minutes ago (ticket newer than 120 minutes) (optional)
+        TicketCreateTimeNewerMinutes => 120,
+
+        # tickets with create time after ... (ticket newer than this date) (optional)
+        TicketCreateTimeNewerDate => '2006-01-09 00:00:01',
+        # tickets with created time before ... (ticket older than this date) (optional)
+        TicketCreateTimeOlderDate => '2006-01-19 23:59:59',
+
+        # tickets changed more than 60 minutes ago (optional)
+        TicketChangeTimeOlderMinutes => 60,
+        # tickets changed less than 120 minutes ago (optional)
+        TicketChangeTimeNewerMinutes => 120,
+
+        # tickets with changed time after ... (ticket changed newer than this date) (optional)
+        TicketChangeTimeNewerDate => '2006-01-09 00:00:01',
+        # tickets with changed time before ... (ticket changed older than this date) (optional)
+        TicketChangeTimeOlderDate => '2006-01-19 23:59:59',
+
+        # tickets closed more than 60 minutes ago (optional)
+        TicketCloseTimeOlderMinutes => 60,
+        # tickets closed less than 120 minutes ago (optional)
+        TicketCloseTimeNewerMinutes => 120,
+
+        # tickets with closed time after ... (ticket closed newer than this date) (optional)
+        TicketCloseTimeNewerDate => '2006-01-09 00:00:01',
+        # tickets with closed time before ... (ticket closed older than this date) (optional)
+        TicketCloseTimeOlderDate => '2006-01-19 23:59:59',
+
+        # tickets with pending time of more than 60 minutes ago (optional)
+        TicketPendingTimeOlderMinutes => 60,
+        # tickets with pending time of less than 120 minutes ago (optional)
+        TicketPendingTimeNewerMinutes => 120,
+
+        # tickets with pending time after ... (optional)
+        TicketPendingTimeNewerDate => '2006-01-09 00:00:01',
+        # tickets with pending time before ... (optional)
+        TicketPendingTimeOlderDate => '2006-01-19 23:59:59',
+
+        # you can use all following escalation options with this four different ways of escalations
+        # TicketEscalationTime...
+        # TicketEscalationUpdateTime...
+        # TicketEscalationResponseTime...
+        # TicketEscalationSolutionTime...
+
+        # ticket escalation time of more than 60 minutes ago (optional)
+        TicketEscalationTimeOlderMinutes => -60,
+        # ticket escalation time of less than 120 minutes ago (optional)
+        TicketEscalationTimeNewerMinutes => -120,
+
+        # tickets with escalation time after ... (optional)
+        TicketEscalationTimeNewerDate => '2006-01-09 00:00:01',
+        # tickets with escalation time before ... (optional)
+        TicketEscalationTimeOlderDate => '2006-01-09 23:59:59',
+
+        # search in archive (optional)
+        ArchiveFlags => ['y', 'n'],
+
+        # OrderBy and SortBy (optional)
+        OrderBy => 'Down',  # Down|Up
+        SortBy  => 'Age',   # Owner|Responsible|CustomerID|State|TicketNumber|Queue|Priority|Age|Type|Lock
+                            # Changed|Title|Service|SLA|PendingTime|EscalationTime
+                            # EscalationUpdateTime|EscalationResponseTime|EscalationSolutionTime
+                            # DynamicField_FieldNameX
+                            # TicketFreeTime1-6|TicketFreeKey1-16|TicketFreeText1-16
+
+        # OrderBy and SortBy as ARRAY for sub sorting (optional)
+        OrderBy => ['Down', 'Up'],
+        SortBy  => ['Priority', 'Age'],
         },
     );
 
     $Result = {
         Success      => 1,                                # 0 or 1
         ErrorMessage => '',                               # In case of an error
-        Data         => {},
+        Data         => {
+            Item => [ 1, 2, 3, 4 ],
+        },
     };
 
 =cut
@@ -191,7 +361,7 @@ sub _GetParams {
     my %GetParam;
 
     for my $Item (
-        qw(TicketNumber Title From To Cc Subject Body CustomerID CustomerUserLogin StateType
+        qw(TicketNumber Title From To Cc Subject Body
         Agent ResultForm TimeSearchType ChangeTimeSearchType CloseTimeSearchType UseSubQueues
         ArticleTimeSearchType SearchInArchive
         Fulltext ShownAttributes
@@ -237,12 +407,13 @@ sub _GetParams {
     # get array params
     for my $Item (
         qw( StateIDs StateTypeIDs QueueIDs PriorityIDs OwnerIDs
-        CreatedQueueIDs CreatedUserIDs WatchUserIDs ResponsibleIDs
+        CreatedUserIDs WatchUserIDs ResponsibleIDs
         TypeIDs ServiceIDs SLAIDs LockIDs Queues Types States
-        Priorities Services SLAs Locks CreatedTypes CreatedUserIDs
+        Priorities Services SLAs Locks
         CreatedTypes CreatedTypeIDs CreatedPriorities
         CreatedPriorityIDs CreatedStates CreatedStateIDs
-        CreatedQueues CreatedQueueIDs )
+        CreatedQueues CreatedQueueIDs StateType CustomerID
+        CustomerUserLogin )
         )
     {
 
@@ -567,6 +738,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.12 $ $Date: 2012-01-26 15:01:09 $
+$Revision: 1.13 $ $Date: 2012-02-01 21:56:48 $
 
 =cut
