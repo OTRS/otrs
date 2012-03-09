3b359d9619ea1788b52c89488665573110fdba47
Martin Gruner <martin.gruner@otrs.com>
2012-03-09 13:00:30 +0000

    Fixed bug#8286 - Adressbook forgets selected entries.

--- Kernel/Modules/AgentBook.pm
+++ Kernel/Modules/AgentBook.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AgentBook.pm - addressbook module
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentBook.pm,v 1.19 2011-11-10 08:55:39 mab Exp $
+# $Id: AgentBook.pm,v 1.20 2012-03-09 13:00:30 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.19 $) [1];
+$VERSION = qw($Revision: 1.20 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -43,7 +43,7 @@ sub Run {
     my ( $Self, %Param ) = @_;
 
     # get params
-    for (qw(To Cc Bcc)) {
+    for (qw(ToCustomer CcCustomer BccCustomer)) {
         $Param{$_} = $Self->{ParamObject}->GetParam( Param => $_ );
     }
 
