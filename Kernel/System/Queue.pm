e1e94aced441a0934dd2998bfb2588381240ce7a
Manuel Hecht <manuel.hecht@otrs.com>
2012-03-20 21:06:09 +0000

    Fixed creation of cache key.

--- Kernel/System/Queue.pm
+++ Kernel/System/Queue.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Queue.pm - lib for queue functions
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Queue.pm,v 1.135 2012-03-20 20:33:48 mb Exp $
+# $Id: Queue.pm,v 1.136 2012-03-20 21:06:09 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -23,7 +23,7 @@ use Kernel::System::Time;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.135 $) [1];
+$VERSION = qw($Revision: 1.136 $) [1];
 
 =head1 NAME
 
@@ -380,7 +380,11 @@ sub GetAllQueues {
         return if !@GroupIDs;
 
         my $GroupString = join ', ', sort @GroupIDs;
-        $CacheKey = 'GetAllQueues::CustomerUserID::' . $Type . '::' . $Param{CustomerUserID};
+        $CacheKey
+            = 'GetAllQueues::CustomerUserID::'
+            . $Type . '::'
+            . $GroupString . '::'
+            . $Param{CustomerUserID};
 
         # check cache
         my $Cache = $Self->{CacheInternalObject}->Get(
@@ -1176,6 +1180,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.135 $ $Date: 2012-03-20 20:33:48 $
+$Revision: 1.136 $ $Date: 2012-03-20 21:06:09 $
 
 =cut
