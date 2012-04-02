1fa377e7df4432e48940e9990a7b4a3b1ff1eaff
Martin Gruner <martin.gruner@otrs.com>
2012-04-02 11:46:35 +0000

    Fixed uninit value error.

--- Kernel/System/DynamicField/Backend/Dropdown.pm
+++ Kernel/System/DynamicField/Backend/Dropdown.pm
@@ -2,7 +2,7 @@
 # Kernel/System/DynamicField/Backend/Dropdown.pm - Delegate for DynamicField Dropdown backend
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Dropdown.pm,v 1.62 2012-03-30 17:24:18 cr Exp $
+# $Id: Dropdown.pm,v 1.63 2012-04-02 11:46:35 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::DynamicFieldValue;
 use Kernel::System::DynamicField::Backend::BackendCommon;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.62 $) [1];
+$VERSION = qw($Revision: 1.63 $) [1];
 
 =head1 NAME
 
@@ -280,7 +280,7 @@ EOF
 
         my $FieldSelector = '#' . $FieldName;
 
-        my $FieldsToUpdate;
+        my $FieldsToUpdate = '';
         if ( IsArrayRefWithData( $Param{UpdatableFields} ) ) {
             my $FirstItem = 1;
             FIELD:
