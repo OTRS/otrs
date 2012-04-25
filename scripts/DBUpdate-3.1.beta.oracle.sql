4b1cd5838f572752d9b4dbd5a7507fe452f36b99
Martin Gruner <martin.gruner@otrs.com>
2012-04-25 11:20:42 +0000

    Removed 3.1 upgrade files from 3.2.

--- scripts/DBUpdate-3.1.beta.oracle.sql
+++
@@ -1,6 +0,0 @@
--- ----------------------------------------------------------
---  driver: oracle, generated: 2011-12-08 11:52:21
--- ----------------------------------------------------------
-SET DEFINE OFF;
-ALTER TABLE dynamic_field_value MODIFY value_text VARCHAR2 (3800) DEFAULT NULL;
-SET DEFINE OFF;
