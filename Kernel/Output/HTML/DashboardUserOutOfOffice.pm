7228341c72ec58111d814eddb0557a64f27f41f1
Michiel Beijen <michiel.beijen@otrs.com>
2012-05-21 07:07:52 +0000

    Added feature Ideascale#72 / bug#5471 - Out-of-Office dashboard widget.

---
+++ Kernel/Output/HTML/DashboardUserOutOfOffice.pm
@@ -0,0 +1,211 @@
+# --
+# Kernel/Output/HTML/DashboardUserOutOfOffice.pm
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# --
+# $Id: DashboardUserOutOfOffice.pm,v 1.1 2012-05-21 07:07:52 mb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::DashboardUserOutOfOffice;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (
+        qw(Config Name ConfigObject LogObject DBObject LayoutObject ParamObject TicketObject UserID)
+        )
+    {
+        die "Got no $_!" if ( !$Self->{$_} );
+    }
+
+    $Self->{SessionObject} = Kernel::System::AuthSession->new(%Param);
+
+    # get current starthit and preferences
+    my $Name = $Self->{ParamObject}->GetParam( Param => 'Name' ) || '';
+    my $PreferencesKey = 'UserDashboardUserOutOfOffice' . $Self->{Name};
+
+    $Self->{PrefKey}   = 'UserDashboardPref' . $Self->{Name} . '-Shown';
+    $Self->{PageShown} = $Self->{LayoutObject}->{ $Self->{PrefKey} } || $Self->{Config}->{Limit};
+    $Self->{StartHit}  = int( $Self->{ParamObject}->GetParam( Param => 'StartHit' ) || 1 );
+    $Self->{CacheKey}  = $Self->{Name};
+    return $Self;
+}
+
+sub Preferences {
+    my ( $Self, %Param ) = @_;
+
+    my @Params = (
+        {
+            Desc  => 'Shown',
+            Name  => $Self->{PrefKey},
+            Block => 'Option',
+            Data  => {
+                5  => ' 5',
+                10 => '10',
+                15 => '15',
+                20 => '20',
+                25 => '25',
+            },
+            SelectedID  => $Self->{PageShown},
+            Translation => 0,
+        },
+    );
+
+    return @Params;
+}
+
+sub Config {
+    my ( $Self, %Param ) = @_;
+
+    return (
+        %{ $Self->{Config} },
+
+        # remember, do not allow to use page cache
+        # (it's not working because of internal filter)
+        CacheKey => undef,
+        CacheTTL => undef,
+    );
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get config settings
+    my $SortBy = $Self->{Config}->{SortBy} || 'UserLastname';
+
+    # check cache
+    my $OutOfOffice = $Self->{CacheObject}->Get(
+        Type => 'Dashboard',
+        Key  => $Self->{CacheKey},
+    );
+
+    # get session info
+    my $CacheUsed = 1;
+    if ( !$OutOfOffice ) {
+        $CacheUsed   = 0;
+        $OutOfOffice = {
+            User      => {},
+            UserCount => 0,
+            UserData  => {},
+        };
+
+        my %UserList = $Self->{UserObject}->SearchPreferences(
+            Key   => 'OutOfOffice',
+            Value => 1,
+        );
+        for my $UserID ( keys %UserList ) {
+            my %Data = $Self->{UserObject}->GetUserData(
+                UserID        => $UserID,
+                NoOutOfOffice => 1,
+            );
+
+            my $Time  = $Self->{TimeObject}->SystemTime();
+            my $Start = sprintf(
+                "%04d-%02d-%02d 00:00:00",
+                $Data{OutOfOfficeStartYear}, $Data{OutOfOfficeStartMonth},
+                $Data{OutOfOfficeStartDay}
+            );
+            my $TimeStart = $Self->{TimeObject}->TimeStamp2SystemTime(
+                String => $Start,
+            );
+            my $End = sprintf(
+                "%04d-%02d-%02d 23:59:59",
+                $Data{OutOfOfficeEndYear}, $Data{OutOfOfficeEndMonth}, $Data{OutOfOfficeEndDay}
+            );
+            my $TimeEnd = $Self->{TimeObject}->TimeStamp2SystemTime(
+                String => $End,
+            );
+            if ( $TimeStart < $Time && $TimeEnd > $Time ) {
+                $Data{OutOfOfficeUntil} = $End;
+            }
+
+            # remember user and data
+            $OutOfOffice->{User}->{ $Data{UserID} } = $Data{$SortBy};
+            $OutOfOffice->{UserCount}++;
+            $OutOfOffice->{UserData}->{ $Data{UserID} } = \%Data;
+        }
+
+        # do not show widget if there are no users out-of-office
+        return if !$OutOfOffice->{UserCount};
+    }
+
+    # set cache
+    if ( !$CacheUsed && $Self->{Config}->{CacheTTLLocal} ) {
+        $Self->{CacheObject}->Set(
+            Type  => 'Dashboard',
+            Key   => $Self->{CacheKey},
+            Value => $OutOfOffice,
+            TTL   => $Self->{Config}->{CacheTTLLocal} * 60,
+        );
+    }
+
+    # add page nav bar if needed
+    my $Total = $OutOfOffice->{UserCount} || 0;
+    if ( $OutOfOffice->{UserCount} > $Self->{PageShown} ) {
+        my $LinkPage = 'Subaction=Element;Name=' . $Self->{Name} . ';';
+        my %PageNav  = $Self->{LayoutObject}->PageNavBar(
+            StartHit       => $Self->{StartHit},
+            PageShown      => $Self->{PageShown},
+            AllHits        => $Total || 1,
+            Action         => 'Action=' . $Self->{LayoutObject}->{Action},
+            Link           => $LinkPage,
+            WindowSize     => 5,
+            AJAXReplace    => 'Dashboard' . $Self->{Name},
+            IDPrefix       => 'Dashboard' . $Self->{Name},
+            KeepScriptTags => $Param{AJAX},
+        );
+
+        $Self->{LayoutObject}->Block(
+            Name => 'ContentSmallTicketGenericNavBar',
+            Data => {
+                %{ $Self->{Config} },
+                Name => $Self->{Name},
+                %PageNav,
+            },
+        );
+    }
+
+    # show data
+    my %OutOfOfficeUser = %{ $OutOfOffice->{User} };
+    my %OutOfOfficeData = %{ $OutOfOffice->{UserData} };
+    my $Count           = 0;
+    my $Limit           = $Self->{LayoutObject}->{ $Self->{PrefKey} } || $Self->{Config}->{Limit};
+
+    for my $UserID ( sort { $OutOfOfficeUser{$a} cmp $OutOfOfficeUser{$b} } keys %OutOfOfficeUser )
+    {
+        $Count++;
+        next if $Count < $Self->{StartHit};
+        last if $Count >= ( $Self->{StartHit} + $Self->{PageShown} );
+        $Self->{LayoutObject}->Block(
+            Name => 'ContentSmallUserOutOfOfficeRow',
+            Data => $OutOfOfficeData{$UserID},
+        );
+    }
+
+    my $Content = $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentDashboardUserOutOfOffice',
+        Data         => {
+            %{ $Self->{Config} },
+            Name => $Self->{Name},
+        },
+        KeepScriptTags => $Param{AJAX},
+    );
+
+    return $Content;
+}
+
+1;
