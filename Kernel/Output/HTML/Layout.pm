9b23db4a080cadf74c8ad344d3d93dc1a212d2d0
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2012-05-29 22:53:04 +0000

    Fixed code style by removing duplicated code.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Layout.pm,v 1.381.2.2 2012-05-25 11:35:41 mg Exp $
+# $Id: Layout.pm,v 1.381.2.3 2012-05-29 22:53:04 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Mail::Address;
 use URI::Escape qw();
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.381.2.2 $) [1];
+$VERSION = qw($Revision: 1.381.2.3 $) [1];
 
 =head1 NAME
 
@@ -4763,11 +4763,7 @@ sub _BuildSelectionDataRefCreate {
     # if ArrayRef was given
     elsif ( ref $Param{Data} eq 'ARRAY' ) {
 
-        if (
-            ( $OptionRef->{Sort} eq 'IndividualValue' || $OptionRef->{Sort} eq 'IndividualValue' )
-            && $OptionRef->{SortIndividual}
-            )
-        {
+        if ( $OptionRef->{Sort} eq 'IndividualValue' && $OptionRef->{SortIndividual} ) {
             my %List = map { $_ => 1 } @{ $Param{Data} };
             $Param{Data} = [];
             for my $Key ( @{ $OptionRef->{SortIndividual} } ) {
@@ -5075,6 +5071,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.381.2.2 $ $Date: 2012-05-25 11:35:41 $
+$Revision: 1.381.2.3 $ $Date: 2012-05-29 22:53:04 $
 
 =cut
