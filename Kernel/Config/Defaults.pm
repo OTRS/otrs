7b4d100341b0441bc7f497fd9f81a2b131c1254a
Michiel Beijen <michiel.beijen@otrs.com>
2012-05-31 17:36:28 +0000

    There is no longer a Kernel/Config/Ticket.pm file.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.411 2012-05-31 17:27:21 mb Exp $
+# $Id: Defaults.pm,v 1.412 2012-05-31 17:36:28 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,8 +15,6 @@
 #
 #   -->> All changes of this file will be lost after an update! <<--
 #
-#  Attention:
-#   -->> Ticket Settings are in Kernel/Config/Files/Ticket.pm <<--
 # --
 
 package Kernel::Config::Defaults;
@@ -29,7 +27,7 @@ use warnings;
 use 5.008_006;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.411 $) [1];
+$VERSION = qw($Revision: 1.412 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 # find path of parent directory, typically this is /opt/otrs
@@ -1952,6 +1950,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.411 $ $Date: 2012-05-31 17:27:21 $
+$Revision: 1.412 $ $Date: 2012-05-31 17:36:28 $
 
 =cut
