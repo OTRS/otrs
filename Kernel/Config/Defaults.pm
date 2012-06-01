86f99806fdb88ff8eb81a2fa9e2b78e6ef2b4a11
Michiel Beijen <michiel.beijen@otrs.com>
2012-06-01 05:25:02 +0000

    Rollback Fixed bug#8533 - it causes problems on CGI.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.405.2.4 2012-05-31 17:31:11 mb Exp $
+# $Id: Defaults.pm,v 1.405.2.5 2012-06-01 05:25:02 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -29,19 +29,14 @@ use warnings;
 use 5.008_006;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.405.2.4 $) [1];
+$VERSION = qw($Revision: 1.405.2.5 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
-# find path of parent directory, typically this is /opt/otrs
 use File::Basename;
-my $FrameworkDirectory = dirname(dirname(__FILE__));
-
-use lib $FrameworkDirectory;
-use lib $FrameworkDirectory . '/Kernel/cpan-lib';
-use lib $FrameworkDirectory . '/Custom';
-
-use File::stat;
-use Digest::MD5;
+use FindBin qw($Bin);
+use lib dirname($Bin);
+use lib dirname($Bin) . '/Kernel/cpan-lib';
+use lib dirname($Bin) . '/Custom';
 
 use File::stat;
 use Digest::MD5;
@@ -1954,6 +1949,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.405.2.4 $ $Date: 2012-05-31 17:31:11 $
+$Revision: 1.405.2.5 $ $Date: 2012-06-01 05:25:02 $
 
 =cut
