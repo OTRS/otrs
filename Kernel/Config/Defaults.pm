5d00c743dd9665b61de307c773aa75126ecccca7
Michiel Beijen <michiel.beijen@otrs.com>
2012-06-01 15:32:48 +0000

    Fixed bug#8533 - Apache will not start using mod_perl on Fedora 16.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.405.2.6 2012-06-01 09:52:41 mb Exp $
+# $Id: Defaults.pm,v 1.405.2.7 2012-06-01 15:32:48 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -29,7 +29,15 @@ use warnings;
 use 5.008_006;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.405.2.6 $) [1];
+$VERSION = qw($Revision: 1.405.2.7 $) [1];
+
+# prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
+# but not under mod_perl because $0 is not good and @INC is set by apache2-perl-startup.pl
+use if (!$ENV{MOD_PERL}), 'File::Basename';
+use if (!$ENV{MOD_PERL}), 'FindBin' => '$Bin';
+use if (!$ENV{MOD_PERL}), 'lib' => dirname($Bin);
+use if (!$ENV{MOD_PERL}), 'lib' => dirname($Bin) . '/Kernel/cpan-lib';
+use if (!$ENV{MOD_PERL}), 'lib' => dirname($Bin) . '/Custom';
 
 use File::stat;
 use Digest::MD5;
@@ -1942,6 +1950,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.405.2.6 $ $Date: 2012-06-01 09:52:41 $
+$Revision: 1.405.2.7 $ $Date: 2012-06-01 15:32:48 $
 
 =cut
