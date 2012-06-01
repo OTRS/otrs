80d10bb5a08056d08100f394e74cff01647a1dd7
Michiel Beijen <michiel.beijen@otrs.com>
2012-06-01 09:52:41 +0000

    Followup fix for bug#8533 - Apache will not start using mod_perl on Fedora 16

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.405.2.5 2012-06-01 05:25:02 mb Exp $
+# $Id: Defaults.pm,v 1.405.2.6 2012-06-01 09:52:41 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -29,14 +29,7 @@ use warnings;
 use 5.008_006;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.405.2.5 $) [1];
-
-# prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
-use File::Basename;
-use FindBin qw($Bin);
-use lib dirname($Bin);
-use lib dirname($Bin) . '/Kernel/cpan-lib';
-use lib dirname($Bin) . '/Custom';
+$VERSION = qw($Revision: 1.405.2.6 $) [1];
 
 use File::stat;
 use Digest::MD5;
@@ -1949,6 +1942,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.405.2.5 $ $Date: 2012-06-01 05:25:02 $
+$Revision: 1.405.2.6 $ $Date: 2012-06-01 09:52:41 $
 
 =cut
