99389681e29a3a2c51b1594d7815e8476c6f2060
Alex Mazur <alex.mazur@otrs.com>
2012-06-19 16:42:09 +0000

    applied the bugfix #7879 on 3.0. added UT for attachments.

--- Kernel/System/Email.pm
+++ Kernel/System/Email.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Email.pm - the global email send module
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Email.pm,v 1.72.2.4 2012-05-10 05:52:33 cg Exp $
+# $Id: Email.pm,v 1.72.2.5 2012-06-19 16:42:09 alm Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -21,7 +21,7 @@ use Kernel::System::Crypt;
 use Kernel::System::HTMLUtils;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.72.2.4 $) [1];
+$VERSION = qw($Revision: 1.72.2.5 $) [1];
 
 =head1 NAME
 
@@ -369,6 +369,13 @@ sub Send {
 
             # ignore attachment if no filename is given
             next ATTACHMENT if !defined $Upload->{Filename};
+
+            # prepare ContentType for Entity Type. $Upload->{ContentType} has
+            # useless `name` parameter, we don't need to send it to the `attach`
+            # constructor. For more details see Bug #7879 and MIME::Entity.
+            if ( $Upload->{ContentType} =~ /;/ ) {
+                $Upload->{ContentType} = ( split ';', $Upload->{ContentType} )[0];
+            }
 
             # if it's a html email, add the first attachment as alternative (to show it
             # as alternative content)
@@ -885,6 +892,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.72.2.4 $ $Date: 2012-05-10 05:52:33 $
+$Revision: 1.72.2.5 $ $Date: 2012-06-19 16:42:09 $
 
 =cut
