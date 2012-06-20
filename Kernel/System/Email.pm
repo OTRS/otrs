3d2ce4547133b9a95a57818d8738aaf2336cb5dd
Alex Mazur <alex.mazur@otrs.com>
2012-06-20 12:26:55 +0000

    fixed typo

--- Kernel/System/Email.pm
+++ Kernel/System/Email.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Email.pm - the global email send module
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Email.pm,v 1.75.2.3 2012-06-20 12:15:20 alm Exp $
+# $Id: Email.pm,v 1.75.2.4 2012-06-20 12:26:55 alm Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -21,7 +21,7 @@ use Kernel::System::Crypt;
 use Kernel::System::HTMLUtils;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.75.2.3 $) [1];
+$VERSION = qw($Revision: 1.75.2.4 $) [1];
 
 =head1 NAME
 
@@ -374,8 +374,8 @@ sub Send {
             # useless `name` parameter, we don't need to send it to the `attach`
             # constructor. For more details see Bug #7879 and MIME::Entity.
             # Note: we should remove `name` attribute only.
-            my @tConentType = grep { !/\s*name=/ } ( split /;/, $Upload->{ContentType} );
-            $Upload->{ContentType} = join ';', @tConentType;
+            my @tContentType = grep { !/\s*name=/ } ( split /;/, $Upload->{ContentType} );
+            $Upload->{ContentType} = join ';', @tContentType;
 
             # if it's a html email, add the first attachment as alternative (to show it
             # as alternative content)
@@ -892,6 +892,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.75.2.3 $ $Date: 2012-06-20 12:15:20 $
+$Revision: 1.75.2.4 $ $Date: 2012-06-20 12:26:55 $
 
 =cut
