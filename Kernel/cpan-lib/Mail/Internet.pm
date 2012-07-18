a19e817690e66347b80ea489bafaee0c35db31c4
Manuel Hecht <manuel.hecht@otrs.com>
2012-07-18 09:43:05 +0000

    Updated CPAN module Mail::Address to version 2.09.

--- Kernel/cpan-lib/Mail/Internet.pm
+++ Kernel/cpan-lib/Mail/Internet.pm
@@ -1,10 +1,10 @@
-# Copyrights 1995-2011 by Mark Overmeer <perl@overmeer.net>.
+# Copyrights 1995-2012 by Mark Overmeer <perl@overmeer.net>.
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
 # Pod stripped from pm file by OODoc 2.00.
 package Mail::Internet;
 use vars '$VERSION';
-$VERSION = '2.08';
+$VERSION = '2.09';
 
 use strict;
 # use warnings?  probably breaking too much code
