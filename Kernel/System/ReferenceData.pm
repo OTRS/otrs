fa5a87eb6ff6da3af7c5ba75f2d78e5d60870ccd
Carlos Garcia <carlos.garcia@otrs.com>
2012-07-19 15:58:43 +0000

    Modified parameter for 'CODESET' on all_country_name_names function call, from '1' to 'alpha-2', a valid code-set value for 2 letter codes.

--- Kernel/System/ReferenceData.pm
+++ Kernel/System/ReferenceData.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ReferenceData.pm - Provides reference data to OTRS
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: ReferenceData.pm,v 1.3 2012-05-29 06:46:11 mb Exp $
+# $Id: ReferenceData.pm,v 1.4 2012-07-19 15:58:43 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Locale::Codes::Country qw(all_country_names);
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 =head1 NAME
 
@@ -114,7 +114,7 @@ sub CountryList {
         return \%CountryJustNames;
     }
 
-    my @CountryNames = all_country_names(1);
+    my @CountryNames = all_country_names('alpha-2');
 
     if ( $Param{Result} ) {
 
@@ -148,6 +148,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.3 $ $Date: 2012-05-29 06:46:11 $
+$Revision: 1.4 $ $Date: 2012-07-19 15:58:43 $
 
 =cut
