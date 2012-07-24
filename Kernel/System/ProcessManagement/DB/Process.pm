747e724792f47084a159a297f481c3bbd3127796
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2012-07-24 14:52:33 +0000

    Adjusted Process Output to match OTRSTicketProcesses expected results.

--- Kernel/System/ProcessManagement/DB/Process.pm
+++ Kernel/System/ProcessManagement/DB/Process.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ProcessManagement/Process.pm - Process Management DB Process backend
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Process.pm,v 1.13 2012-07-23 22:22:09 cr Exp $
+# $Id: Process.pm,v 1.14 2012-07-24 14:52:33 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -26,7 +26,7 @@ use Kernel::System::ProcessManagement::DB::Transition;
 use Kernel::System::ProcessManagement::DB::TransitionAction;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.13 $) [1];
+$VERSION = qw($Revision: 1.14 $) [1];
 
 =head1 NAME
 
@@ -1067,7 +1067,8 @@ sub ProcessDump {
             Name                => $ProcessData->{Name},
             CreateTime          => $ProcessData->{CreateTime},
             ChangeTime          => $ProcessData->{ChangeTime},
-            State               => $ProcessData->{StateEntityID},
+            StateEntityID       => $ProcessData->{StateEntityID},
+            State               => $ProcessData->{State},
             StartActivity       => $ProcessData->{Config}->{StartActivity} || '',
             StartActivityDialog => $ProcessData->{Config}->{StartActivityDialog} || '',
             Path                => $ProcessData->{Config}->{Path} || {},
@@ -1254,6 +1255,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.13 $ $Date: 2012-07-23 22:22:09 $
+$Revision: 1.14 $ $Date: 2012-07-24 14:52:33 $
 
 =cut
