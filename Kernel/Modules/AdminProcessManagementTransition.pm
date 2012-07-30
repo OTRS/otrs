a3b53188abc748db500ade2722a483ff50ba575b
Marc Bonsels <marc.bonsels@otrs.com>
2012-07-30 15:29:11 +0000

    Implemented transition new/edit (wip).

--- Kernel/Modules/AdminProcessManagementTransition.pm
+++ Kernel/Modules/AdminProcessManagementTransition.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminProcessManagementTransition.pm - process management transition
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminProcessManagementTransition.pm,v 1.1 2012-07-19 14:14:16 mn Exp $
+# $Id: AdminProcessManagementTransition.pm,v 1.2 2012-07-30 15:29:11 mab Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,7 +22,7 @@ use Kernel::System::ProcessManagement::DB::Transition;
 use Kernel::System::VariableCheck qw(:all);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -56,8 +56,8 @@ sub Run {
 
     $Self->{Subaction} = $Self->{ParamObject}->GetParam( Param => 'Subaction' ) || '';
 
-    my $ActivityID = $Self->{ParamObject}->GetParam( Param => 'ID' )       || '';
-    my $EntityID   = $Self->{ParamObject}->GetParam( Param => 'EntityID' ) || '';
+    my $TransitionID = $Self->{ParamObject}->GetParam( Param => 'ID' )       || '';
+    my $EntityID     = $Self->{ParamObject}->GetParam( Param => 'EntityID' ) || '';
 
     my %SessionData = $Self->{SessionObject}->GetSessionIDData(
         SessionID => $Self->{SessionID},
@@ -83,6 +83,144 @@ sub Run {
     # TransitionNewAction
     # ------------------------------------------------------------ #
     elsif ( $Self->{Subaction} eq 'TransitionNewAction' ) {
+
+        # challenge token check for write action
+        $Self->{LayoutObject}->ChallengeTokenCheck();
+
+        # get transition action data
+        my $TransitionData;
+
+        # get parameter from web browser
+        my $GetParam = $Self->_GetParams;
+
+        # set new configuration
+        $TransitionData->{Name}   = $GetParam->{Name};
+        $TransitionData->{Config} = $GetParam->{Config};
+
+        # check required parameters
+        my %Error;
+        if ( !$GetParam->{Name} ) {
+
+            # add server error error class
+            $Error{NameServerError}        = 'ServerError';
+            $Error{NameServerErrorMessage} = 'This field is required';
+        }
+
+        if ( !$GetParam->{Config} ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "At least one valid condition is required.",
+            );
+        }
+
+        # if there is an error return to edit screen
+        if ( IsHashRefWithData( \%Error ) ) {
+            return $Self->_ShowEdit(
+                %Error,
+                %Param,
+                TransitionData => $TransitionData,
+                Action         => 'New',
+            );
+        }
+
+        # generate entity ID
+        my $EntityID = $Self->{EntityObject}->EntityIDGenerate(
+            EntityType => 'Transition',
+            UserID     => $Self->{UserID},
+        );
+
+        # show error if can't generate a new EntityID
+        if ( !$EntityID ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "There was an error generating a new EntityID for this Transition",
+            );
+        }
+
+        # otherwise save configuration and return process screen
+        my $TransitionID = $Self->{TransitionObject}->TransitionAdd(
+            Name     => $TransitionData->{Name},
+            EntityID => $EntityID,
+            Config   => $TransitionData->{Config},
+            UserID   => $Self->{UserID},
+        );
+
+        # show error if can't create
+        if ( !$TransitionID ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "There was an error creating the Transition",
+            );
+        }
+
+        # set entitty sync state
+        my $Success = $Self->{EntityObject}->EntitySyncStateSet(
+            EntityType => 'Transition',
+            EntityID   => $EntityID,
+            SyncState  => 'not_sync',
+            UserID     => $Self->{UserID},
+        );
+
+        # show error if can't set
+        if ( !$Success ) {
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "There was an error setting the entity sync status for Transition "
+                    . "entity:$EntityID",
+            );
+        }
+
+        # remove this screen from session screen path
+        $Self->_PopSessionScreen( OnlyCurrent => 1 );
+
+        my $Redirect = $Self->{ParamObject}->GetParam( Param => 'PopupRedirect' ) || '';
+
+        # check if needed to open another window or if popup should go back
+        if ( $Redirect && $Redirect eq '1' ) {
+
+            $Self->_PushSessionScreen(
+                ID        => $TransitionID,
+                EntityID  => $TransitionData->{EntityID},
+                Subaction => 'TransitionEdit'               # always use edit screen
+            );
+
+            my $RedirectAction
+                = $Self->{ParamObject}->GetParam( Param => 'PopupRedirectAction' ) || '';
+            my $RedirectSubaction
+                = $Self->{ParamObject}->GetParam( Param => 'PopupRedirectSubaction' ) || '';
+            my $RedirectID = $Self->{ParamObject}->GetParam( Param => 'PopupRedirectID' ) || '';
+            my $RedirectEntityID
+                = $Self->{ParamObject}->GetParam( Param => 'PopupRedirectEntityID' ) || '';
+
+            # redirect to another popup window
+            return $Self->_PopupResponse(
+                Redirect => 1,
+                Screen   => {
+                    Action    => $RedirectAction,
+                    Subaction => $RedirectSubaction,
+                    ID        => $RedirectID,
+                    EntityID  => $RedirectID,
+                },
+            );
+        }
+        else {
+
+            # remove last screen
+            my $LastScreen = $Self->_PopSessionScreen();
+
+            # check if needed to return to main screen or to be redirected to last screen
+            if ( $LastScreen->{Action} eq 'AdminProcessManagement' ) {
+
+                # close the popup
+                return $Self->_PopupResponse(
+                    ClosePopup => 1,
+                );
+            }
+            else {
+
+                # redirect to last screen
+                return $Self->_PopupResponse(
+                    Redirect => 1,
+                    Screen   => $LastScreen
+                );
+            }
+        }
 
     }
 
@@ -145,6 +283,111 @@ sub _ShowEdit {
     );
 
     $Output .= $Self->{LayoutObject}->Footer();
+
+    return $Output;
+}
+
+sub _GetParams {
+    my ( $Self, %Param ) = @_;
+
+    my $GetParam;
+
+    # get parameters from web browser
+    $GetParam->{Name} = $Self->{ParamObject}->GetParam( Param => 'Name' ) || '';
+
+    $GetParam->{Config} = {};
+    $GetParam->{Config}->{Condition} = {
+        1 => "bla",
+    };
+
+    return $GetParam;
+}
+
+sub _PopSessionScreen {
+    my ( $Self, %Param ) = @_;
+
+    my $LastScreen;
+
+    if ( defined $Param{OnlyCurrent} && $Param{OnlyCurrent} == 1 ) {
+
+        # check if last screen action is current screen action
+        if ( $Self->{ScreensPath}->[-1]->{Action} eq $Self->{Action} ) {
+
+            # remove last screen
+            $LastScreen = pop @{ $Self->{ScreensPath} };
+        }
+    }
+    else {
+
+        # remove last screen
+        $LastScreen = pop @{ $Self->{ScreensPath} };
+    }
+
+    # convert screens path to string (JSON)
+    my $JSONScreensPath = $Self->{LayoutObject}->JSONEncode(
+        Data => $Self->{ScreensPath},
+    );
+
+    # update session
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'ProcessManagementScreensPath',
+        Value     => $JSONScreensPath,
+    );
+
+    return $LastScreen;
+}
+
+sub _PushSessionScreen {
+    my ( $Self, %Param ) = @_;
+
+    # add screen to the screen path
+    push @{ $Self->{ScreensPath} }, {
+        Action => $Self->{Action} || '',
+        Subaction => $Param{Subaction},
+        ID        => $Param{ID},
+        EntityID  => $Param{EntityID},
+    };
+
+    # convert screens path to string (JSON)
+    my $JSONScreensPath = $Self->{LayoutObject}->JSONEncode(
+        Data => $Self->{ScreensPath},
+    );
+
+    # update session
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'ProcessManagementScreensPath',
+        Value     => $JSONScreensPath,
+    );
+
+    return 1;
+}
+
+sub _PopupResponse {
+    my ( $Self, %Param ) = @_;
+
+    if ( $Param{Redirect} && $Param{Redirect} eq 1 ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'Redirect',
+            Data => {
+                %{ $Param{Screen} },
+            },
+        );
+    }
+    elsif ( $Param{ClosePopup} && $Param{ClosePopup} eq 1 ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'ClosePopup',
+            Data => {},
+        );
+    }
+
+    my $Output = $Self->{LayoutObject}->Header( Type => 'Small' );
+    $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => "AdminProcessManagementPopupResponse",
+        Data         => {},
+    );
+    $Output .= $Self->{LayoutObject}->Footer( Type => 'Small' );
 
     return $Output;
 }
