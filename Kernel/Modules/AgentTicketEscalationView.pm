9fb39b4815cfcf61cd6700152303c8ac711f8db7
Manuel Hecht <manuel.hecht@otrs.com>
2012-07-31 13:48:23 +0000

    Added config option for ticket permission in the escalation view.

--- Kernel/Modules/AgentTicketEscalationView.pm
+++ Kernel/Modules/AgentTicketEscalationView.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AgentTicketEscalationView.pm - status for all open tickets
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketEscalationView.pm,v 1.16 2011-12-15 15:14:51 mg Exp $
+# $Id: AgentTicketEscalationView.pm,v 1.17 2012-07-31 13:48:23 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -101,7 +101,7 @@ sub Run {
                 OrderBy                       => $Self->{OrderBy},
                 SortBy                        => $Self->{SortBy},
                 UserID                        => $Self->{UserID},
-                Permission                    => 'ro',
+                Permission                    => $Self->{Config}->{'TicketPermisson'},
             },
         },
         Tomorrow => {
@@ -112,7 +112,7 @@ sub Run {
                 OrderBy                       => $Self->{OrderBy},
                 SortBy                        => $Self->{SortBy},
                 UserID                        => $Self->{UserID},
-                Permission                    => 'ro',
+                Permission                    => $Self->{Config}->{'TicketPermisson'},
             },
         },
         NextWeek => {
@@ -123,7 +123,7 @@ sub Run {
                 OrderBy                       => $Self->{OrderBy},
                 SortBy                        => $Self->{SortBy},
                 UserID                        => $Self->{UserID},
-                Permission                    => 'ro',
+                Permission                    => $Self->{Config}->{'TicketPermisson'},
             },
         },
     );
