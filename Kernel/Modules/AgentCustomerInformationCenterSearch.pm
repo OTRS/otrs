986e4a4a0ca26f812c1607dd4f8413e668f25244
Martin Gruner <martin.gruner@otrs.com>
2012-09-10 12:56:33 +0000

    Added CIC search autocomplete.

--- Kernel/Modules/AgentCustomerInformationCenterSearch.pm
+++ Kernel/Modules/AgentCustomerInformationCenterSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentCustomerInformationCenterSearch.pm - customer information
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentCustomerInformationCenterSearch.pm,v 1.1 2012-09-10 10:07:17 mg Exp $
+# $Id: AgentCustomerInformationCenterSearch.pm,v 1.2 2012-09-10 12:56:33 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -14,11 +14,12 @@ package Kernel::Modules::AgentCustomerInformationCenterSearch;
 use strict;
 use warnings;
 
-#use Kernel::System::Cache;
+use Kernel::System::CustomerUser;
+use Kernel::System::CustomerCompany;
 use Kernel::System::VariableCheck qw(:all);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -34,7 +35,8 @@ sub new {
         }
     }
 
-    #    $Self->{CacheObject} = Kernel::System::Cache->new(%Param);
+    $Self->{CustomerUserObject}    = Kernel::System::CustomerUser->new(%Param);
+    $Self->{CustomerCompanyObject} = Kernel::System::CustomerCompany->new(%Param);
 
     $Self->{SlaveDBObject}     = $Self->{DBObject};
     $Self->{SlaveTicketObject} = $Self->{TicketObject};
@@ -67,14 +69,40 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $AutoCompleteConfig
+        = $Self->{ConfigObject}->Get('Ticket::Frontend::CustomerSearchAutoComplete');
+
+    my $MaxResults = $AutoCompleteConfig->{MaxResultsDisplayed} || 20;
+
     if ( $Self->{Subaction} eq 'SearchCustomerID' ) {
+
+        my @CustomerIDs = $Self->{CustomerUserObject}->CustomerIDList(
+            SearchTerm => $Self->{ParamObject}->GetParam( Param => 'Term' ) || '',
+        );
+
+        my @Result;
+
+        my $Count = 1;
+
+        CUSTOMERID:
+        for my $CustomerID (@CustomerIDs) {
+            my %CustomerCompanyData = $Self->{CustomerCompanyObject}->CustomerCompanyGet(
+                CustomerID => $CustomerID,
+            );
+
+            my $Label = $CustomerID;
+
+            if ( $CustomerCompanyData{CustomerCompanyName} ) {
+                $Label .= " ($CustomerCompanyData{CustomerCompanyName})";
+            }
+
+            push @Result, { Label => $Label || $CustomerID, Value => $CustomerID };
+
+            last CUSTOMERID if $Count++ >= $MaxResults;
+        }
+
         my $JSON = $Self->{LayoutObject}->JSONEncode(
-            Data => [
-                {
-                    Label => '123 - 123123',
-                    Value => '123',
-                },
-            ],
+            Data => \@Result,
         );
 
         return $Self->{LayoutObject}->Attachment(
@@ -84,6 +112,49 @@ sub Run {
             NoCache     => 1,
         );
     }
+    elsif ( $Self->{Subaction} eq 'SearchCustomerUser' ) {
+
+        my %CustomerList = $Self->{CustomerUserObject}->CustomerSearch(
+            Search => $Self->{ParamObject}->GetParam( Param => 'Term' ) || '',
+        );
+
+        my @Result;
+
+        my $Count = 1;
+
+        CUSTOMERLOGIN:
+        for my $CustomerLogin ( keys %CustomerList ) {
+            my %CustomerData = $Self->{CustomerUserObject}->CustomerUserDataGet(
+                User => $CustomerLogin,
+            );
+            push @Result,
+                { Label => $CustomerList{$CustomerLogin}, Value => $CustomerData{UserCustomerID} };
+
+            last CUSTOMERLOGIN if $Count++ >= $MaxResults;
+        }
+
+        my $JSON = $Self->{LayoutObject}->JSONEncode(
+            Data => \@Result,
+        );
+
+        return $Self->{LayoutObject}->Attachment(
+            ContentType => 'application/json; charset=' . $Self->{LayoutObject}->{Charset},
+            Content     => $JSON || '',
+            Type        => 'inline',
+            NoCache     => 1,
+        );
+    }
+
+    # build customer search autocomplete fields
+    $Self->{LayoutObject}->Block(
+        Name => 'CustomerSearchAutoComplete',
+        Data => {
+            ActiveAutoComplete  => $AutoCompleteConfig->{Active},
+            minQueryLength      => $AutoCompleteConfig->{MinQueryLength} || 2,
+            queryDelay          => $AutoCompleteConfig->{QueryDelay} || 100,
+            maxResultsDisplayed => $AutoCompleteConfig->{MaxResultsDisplayed} || 20,
+        },
+    );
 
     my $Output .= $Self->{LayoutObject}->Output(
         TemplateFile => 'AgentCustomerInformationCenterSearch',
