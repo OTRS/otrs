de5ce92da529bcd72e1477d775b123c1c9efc3f5
Michiel Beijen <michiel.beijen@otrs.com>
2012-10-01 11:37:53 +0000

    Re-added removed file.

---
+++ Kernel/Output/HTML/ToolBarTicketSearchFulltext.pm
@@ -0,0 +1,52 @@
+# --
+# Kernel/Output/HTML/ToolBarTicketSearchFulltext.pm
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# --
+# $Id: ToolBarTicketSearchFulltext.pm,v 1.4 2012-10-01 11:37:53 mb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::ToolBarTicketSearchFulltext;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.4 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(ConfigObject LogObject DBObject TicketObject LayoutObject UserID UserObject)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $Priority = $Param{Config}->{'Priority'};
+    my %Return   = ();
+    $Return{ $Priority++ } = {
+        Block       => $Param{Config}->{Block},
+        Description => $Param{Config}->{Description},
+        Name        => $Param{Config}->{Name},
+        Size        => $Param{Config}->{Size},
+        Fulltext    => '',
+        Image       => '',
+        AccessKey   => '',
+    };
+    return %Return;
+}
+
+1;
