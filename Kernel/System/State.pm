f683fe027f7ca1a4643fa6fa26e84123603df61b
Carlos Garcia <carlos.garcia@otrs.com>
2012-10-09 06:08:46 +0000

    First attempt for adding alphabetic sorting on ticket search.

--- Kernel/System/State.pm
+++ Kernel/System/State.pm
@@ -2,7 +2,7 @@
 # Kernel/System/State.pm - All ticket state related functions
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: State.pm,v 1.56 2012-03-18 22:06:23 mh Exp $
+# $Id: State.pm,v 1.56.2.1 2012-10-09 06:08:46 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::Time;
 use Kernel::System::Valid;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.56 $) [1];
+$VERSION = qw($Revision: 1.56.2.1 $) [1];
 
 =head1 NAME
 
@@ -536,6 +536,40 @@ sub StateList {
     return %Data;
 }
 
+=item GetTranslatedSortedList()
+
+get state list as a hash of ID, Name pairs
+
+    my %List = $StateObject->GetTranslatedSortedList(
+        UserID => 123,
+    );
+
+returns
+
+=cut
+
+sub GetTranslatedSortedList {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{UserID} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => 'UserID!' );
+        return;
+    }
+
+    my %List = $Self->StateList(
+        UserID => $Param{UserID},
+    );
+
+    my %Tickets = %{ $Param{Tickets} };
+    for my $Ticket ( sort keys %Tickets ) {
+        my $Key = $Tickets{$Ticket}->{ $Param{Item} };
+        $Tickets{$Ticket}->{ $Param{Item} } = $List{$Key};
+    }
+
+    return \%Tickets;
+}
+
 =item StateLookup()
 
 returns the id or the name of a state
@@ -721,6 +755,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.56 $ $Date: 2012-03-18 22:06:23 $
+$Revision: 1.56.2.1 $ $Date: 2012-10-09 06:08:46 $
 
 =cut
