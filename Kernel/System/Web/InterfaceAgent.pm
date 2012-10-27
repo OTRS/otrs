79fbb55f96be2734820553b1820a84ff658cba3b
Manuel Hecht <manuel.hecht@otrs.com>
2012-10-27 09:01:19 +0000

    Fixed typo in comment.

--- Kernel/System/Web/InterfaceAgent.pm
+++ Kernel/System/Web/InterfaceAgent.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Web/InterfaceAgent.pm - the agent interface file (incl. auth)
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: InterfaceAgent.pm,v 1.68 2012-10-25 13:49:08 mh Exp $
+# $Id: InterfaceAgent.pm,v 1.69 2012-10-27 09:01:19 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION @INC);
-$VERSION = qw($Revision: 1.68 $) [1];
+$VERSION = qw($Revision: 1.69 $) [1];
 
 use Kernel::Config;
 use Kernel::System::Log;
@@ -283,7 +283,7 @@ sub Run {
             UserType        => 'User',
         );
 
-        # show error message if ne session id has been created
+        # show error message if no session id has been created
         if ( !$NewSessionID ) {
 
             # get error message
@@ -936,6 +936,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.68 $ $Date: 2012-10-25 13:49:08 $
+$Revision: 1.69 $ $Date: 2012-10-27 09:01:19 $
 
 =cut
