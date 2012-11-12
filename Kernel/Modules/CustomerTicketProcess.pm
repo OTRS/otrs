a8a890d04a0fd01f4cafa44097a770c0f0432f0e
Manuel Hecht <manuel.hecht@otrs.com>
2012-11-12 12:56:02 +0000

    Fixed license header.

--- Kernel/Modules/CustomerTicketProcess.pm
+++ Kernel/Modules/CustomerTicketProcess.pm
@@ -2,11 +2,11 @@
 # Kernel/Modules/CustomerTicketProcess.pm - to create process tickets
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: CustomerTicketProcess.pm,v 1.6 2012-11-12 12:16:36 mh Exp $
+# $Id: CustomerTicketProcess.pm,v 1.7 2012-11-12 12:56:02 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
 package Kernel::Modules::CustomerTicketProcess;
@@ -26,7 +26,7 @@ use Kernel::System::CustomerUser;
 use Kernel::System::VariableCheck qw(:all);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.6 $) [1];
+$VERSION = qw($Revision: 1.7 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
