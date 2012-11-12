89cb81042354395f077fe8e3d845b712e2db92e8
Manuel Hecht <manuel.hecht@otrs.com>
2012-11-12 13:15:09 +0000

    Code cleanup.

--- Kernel/GenericInterface/Debugger.pm
+++ Kernel/GenericInterface/Debugger.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/GenericInterface/Debugger.pm - GenericInterface data debugger interface
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Debugger.pm,v 1.17 2011-02-24 14:38:06 mg Exp $
+# $Id: Debugger.pm,v 1.18 2012-11-12 13:15:09 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::VariableCheck qw(IsString IsStringWithData IsHashRefWithData
 use Kernel::System::GenericInterface::DebugLog;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.17 $) [1];
+$VERSION = qw($Revision: 1.18 $) [1];
 
 =head1 NAME
 
@@ -134,8 +134,10 @@ sub new {
 
     # check correct CommunicationType
     if ( lc $Self->{CommunicationType} !~ /^(provider|requester)/i ) {
-        $Self->{LogObject}
-            ->Log( Priority => 'error', Message => 'CommunicationType is not allowed.' );
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'CommunicationType is not allowed.'
+        );
         return;
     }
 
@@ -378,6 +380,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.17 $ $Date: 2011-02-24 14:38:06 $
+$Revision: 1.18 $ $Date: 2012-11-12 13:15:09 $
 
 =cut
