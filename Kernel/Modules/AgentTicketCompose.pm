09a29d8e4d3a5ea473ca5c3753ee12ff701b8444
Martin Gruner <martin.gruner@otrs.com>
2012-11-16 12:35:51 +0000

    Code style.

--- Kernel/Modules/AgentTicketCompose.pm
+++ Kernel/Modules/AgentTicketCompose.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketCompose.pm - to compose and send a message
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketCompose.pm,v 1.172 2012-11-16 12:20:47 mab Exp $
+# $Id: AgentTicketCompose.pm,v 1.173 2012-11-16 12:35:51 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -27,7 +27,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.172 $) [1];
+$VERSION = qw($Revision: 1.173 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1835,7 +1835,7 @@ sub _Mask {
         Data         => {
             FormID => $Self->{FormID},
             %Param,
-            }
+        },
     );
 }
 
