097e94d16e336a8b2836257e2e0ff9951e1ba062
Marc Nilius <marc.nilius@otrs.com>
2012-12-11 08:38:11 +0000

    Updated StacktraceJS to version 0.4.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -2,7 +2,7 @@
 # Kernel/Config/Defaults.pm - Default Config file for OTRS kernel
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Defaults.pm,v 1.429 2012-12-11 08:32:40 mn Exp $
+# $Id: Defaults.pm,v 1.430 2012-12-11 08:38:11 mn Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -28,7 +28,7 @@ use utf8;
 use 5.008_006;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.429 $) [1];
+$VERSION = qw($Revision: 1.430 $) [1];
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 use File::Basename;
@@ -889,7 +889,7 @@ sub LoadDefaults {
     # Customer Common JS
     $Self->{'Loader::Customer::CommonJS'}->{'000-Framework'} =  [
       'thirdparty/jquery-1.6.4/jquery.js',
-      'thirdparty/stacktrace-0.3/stacktrace.js',
+      'thirdparty/stacktrace-0.4/stacktrace.js',
       'Core.Debug.js',
       'Core.Exception.js',
       'Core.Config.js',
@@ -905,7 +905,7 @@ sub LoadDefaults {
       'thirdparty/jquery-1.6.4/jquery.js',
       'thirdparty/jquery-ui-1.8.21/jquery-ui.js',
       'thirdparty/jquery-validate-1.10/jquery.validate.js',
-      'thirdparty/stacktrace-0.3/stacktrace.js',
+      'thirdparty/stacktrace-0.4/stacktrace.js',
       'Core.JavaScriptEnhancements.js',
       'Core.Debug.js',
       'Core.Data.js',
@@ -1944,6 +1944,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.429 $ $Date: 2012-12-11 08:32:40 $
+$Revision: 1.430 $ $Date: 2012-12-11 08:38:11 $
 
 =cut
