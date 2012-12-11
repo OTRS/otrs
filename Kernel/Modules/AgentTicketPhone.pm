0cf77897f8d41584aae6fd0cdb7096d29d5338d4
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2012-12-11 22:29:35 +0000

    Applied new fix for Bug#8230 to make it work as in OTRS 3.0.x.

--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPhone.pm - to handle phone calls
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketPhone.pm,v 1.236.2.8 2012-10-11 20:02:41 cr Exp $
+# $Id: AgentTicketPhone.pm,v 1.236.2.9 2012-12-11 22:29:35 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -26,7 +26,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.236.2.8 $) [1];
+$VERSION = qw($Revision: 1.236.2.9 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -825,6 +825,13 @@ sub Run {
                 }
                 if ( $CustomerUserData{UserLogin} ) {
                     $CustomerUser = $CustomerUserData{UserLogin};
+                    $FromExternalCustomer{Customer} = $CustomerUserData{UserLogin};
+                }
+                if ( $FromExternalCustomer{Customer} ) {
+                    my %ExternalCustomerUserData = $Self->{CustomerUserObject}->CustomerUserDataGet(
+                        User => $FromExternalCustomer{Customer},
+                    );
+                    $FromExternalCustomer{Email} = $ExternalCustomerUserData{UserEmail};
                 }
             }
 
