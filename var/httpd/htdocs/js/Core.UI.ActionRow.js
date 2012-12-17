b6e006afc8e225c84b48e2e7965588839a27ce8f
Marc Nilius <marc.nilius@otrs.com>
2012-12-17 14:09:43 +0000

    Fixed bug#8589 - Bulk-Action not possible for single ticket.

--- var/httpd/htdocs/js/Core.UI.ActionRow.js
+++ var/httpd/htdocs/js/Core.UI.ActionRow.js
@@ -2,7 +2,7 @@
 // Core.UI.ActionRow.js - provides all functions for the Action row
 // Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 // --
-// $Id: Core.UI.ActionRow.js,v 1.6.2.3 2012-08-10 11:43:08 mn Exp $
+// $Id: Core.UI.ActionRow.js,v 1.6.2.4 2012-12-17 14:09:43 mn Exp $
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
@@ -117,8 +117,8 @@ Core.UI.ActionRow = (function (TargetNS) {
         }
         // Exactly one checkbox is selected
         else if ($Checkboxes.length === 1 && !$('#SelectAllTickets').is(':checked') ) {
-            // Update actions and deactivate bulk action
-            $ActionRow.find('#BulkAction').addClass('Inactive');
+            // Update actions and activate bulk action
+            $ActionRow.find('#BulkAction').removeClass('Inactive');
 
             // Find the element which is active (it must not be the clicked element!)
             // and get the data
