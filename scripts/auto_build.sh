47f523ea3a1a713cf1102e4ec963428ddfb093c7
Martin Gruner <martin.gruner@otrs.com>
2012-12-17 10:05:48 +0000

    Include 3.2 PDF manual, do not include german version at this point.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -3,7 +3,7 @@
 # auto_build.sh - build automatically OTRS tar, rpm and src-rpm
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: auto_build.sh,v 1.115 2012-12-11 16:48:43 mb Exp $
+# $Id: auto_build.sh,v 1.116 2012-12-17 10:05:48 mg Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -21,7 +21,7 @@
 # or see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.115 $>"
+echo "auto_build.sh - build automatically OTRS tar, rpm and src-rpm <\$Revision: 1.116 $>"
 echo "Copyright (C) 2001-2012 OTRS AG, http://otrs.org/\n";
 
 PATH_TO_CVS_SRC=$1
@@ -38,8 +38,8 @@ RPM_BUILD="rpmbuild"
 
 SUPPORT_PACKAGE="http://ftp.otrs.org/pub/otrs/packages/Support-1.4.1.opm"
 #IPHONE_PACKAGE="http://ftp.otrs.org/pub/otrs/packages/iPhoneHandle-1.1.1.opm"
-MANUAL_EN="http://ftp.otrs.org/pub/otrs/doc/doc-admin/3.1/en/pdf/otrs_admin_book.pdf"
-MANUAL_DE="http://ftp.otrs.org/pub/otrs/doc/doc-admin/3.1/de/pdf/otrs_admin_book.pdf"
+MANUAL_EN="http://ftp.otrs.org/pub/otrs/doc/doc-admin/3.2/en/pdf/otrs_admin_book.pdf"
+#MANUAL_DE="http://ftp.otrs.org/pub/otrs/doc/doc-admin/3.2/de/pdf/otrs_admin_book.pdf"
 
 
 if ! test $PATH_TO_CVS_SRC || ! test $VERSION || ! test $RELEASE; then
@@ -155,9 +155,9 @@ mkdir -p doc/manual/en
 wget "$MANUAL_EN" || exit 1;
 mv otrs_admin_book.pdf doc/manual/en
 
-mkdir -p doc/manual/de
-wget "$MANUAL_DE" || exit 1;
-mv otrs_admin_book.pdf doc/manual/de
+#mkdir -p doc/manual/de
+#wget "$MANUAL_DE" || exit 1;
+#mv otrs_admin_book.pdf doc/manual/de
 
 # mk ARCHIVE
 bin/otrs.CheckSum.pl -a create
