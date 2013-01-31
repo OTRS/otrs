25fd5dff7d1734d6e8b6c2bf10bb32d96facc3b8
Martin Gruner <martin.gruner@otrs.com>
2013-01-31 08:22:55 +0000

    Fixed typo, simplified code.

--- Kernel/System/Ticket/Event/NotificationEvent.pm
+++ Kernel/System/Ticket/Event/NotificationEvent.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket/Event/NotificationEvent.pm - a event module to send notifications
 # Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 # --
-# $Id: NotificationEvent.pm,v 1.49 2013-01-30 17:23:42 cr Exp $
+# $Id: NotificationEvent.pm,v 1.50 2013-01-31 08:22:55 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -21,7 +21,7 @@ use Kernel::System::DynamicField::Backend;
 use Kernel::System::VariableCheck qw(:all);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.49 $) [1];
+$VERSION = qw($Revision: 1.50 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -554,13 +554,9 @@ sub _SendNotification {
     );
 
     # COMPAT
+    # use Ticket information as a fallback (if ticket has no Articles)
+    my $TicketNumber = $Article{TicketNumber} || $Ticket{TicketNumber};
     $Notification{Body} =~ s/<OTRS_TICKET_ID>/$Param{TicketID}/gi;
-    my $TicketNumber = $Article{TicketNumber};
-
-    # use Ticket information as a fallbak (if ticket has no Articles)
-    if ( !$TicketNumber ) {
-        $TicketNumber = $Ticket{TicketNumber};
-    }
     $Notification{Body} =~ s/<OTRS_TICKET_NUMBER>/$TicketNumber/gi;
 
     # prepare customer realname
