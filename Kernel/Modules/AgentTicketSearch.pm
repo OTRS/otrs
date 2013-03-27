35730bc952cc6d2a001c93cac0ddd6b1db77f2d4
Martin Gruner <martin.gruner@otrs.com>
2013-03-27 14:42:33 +0100

    Fixed bug#9295 - Article dynamic field is not searchable.

--- Kernel/Modules/AgentTicketSearch.pm
+++ Kernel/Modules/AgentTicketSearch.pm
@@ -88,7 +88,7 @@ sub new {
     # get the dynamic fields for ticket object
     $Self->{DynamicField} = $Self->{DynamicFieldObject}->DynamicFieldListGet(
         Valid       => 1,
-        ObjectType  => ['Ticket'],
+        ObjectType  => ['Ticket', 'Article'],
         FieldFilter => $Self->{DynamicFieldFilter} || {},
     );
 
