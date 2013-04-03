7597247b434314b3747afc32174cb3e69ac87036
Martin Gruner <martin.gruner@otrs.com>
2013-04-03 12:05:26 +0200

    Revert "Improved framework version check, thanks to Johannes!"
    This reverts commit e63f4b4c19e1fd14c1d771fbcf6c34083f3b51c0.

--- Kernel/System/Package.pm
+++ Kernel/System/Package.pm
@@ -21,6 +21,8 @@ use Kernel::System::SysConfig;
 use Kernel::System::WebUserAgent;
 use Kernel::System::XML;
 
+use vars qw($S);
+
 =head1 NAME
 
 Kernel::System::Package - to manage application packages/modules
@@ -2208,17 +2210,10 @@ sub _CheckFramework {
 
             # regexp modify
             my $Framework = $FW->{Content};
-            $Framework =~ s/x.*$/0/gi;
+            $Framework =~ s/\./\\\./g;
+            $Framework =~ s/x/.+?/gi;
 
-            my $CurrentFrameworkCheck = $CurrentFramework;
-            $CurrentFrameworkCheck =~ s/x.*$/0/gi;
-
-            next FW if ! $Self->_CheckVersion(
-                VersionNew          => $Framework,
-                VersionInstalled    => $CurrentFrameworkCheck,
-                Type                => 'Min',
-            );
-
+            next FW if $CurrentFramework !~ /^$Framework$/i;
 
             $FWCheck = 1;
 
@@ -2571,7 +2566,6 @@ sub _FileInstall {
         if ( $Param{File}->{Type} && $Param{File}->{Type} =~ /^replace$/i ) {
             if ( !$Param{Reinstall} || ( $Param{Reinstall} && !-e "$RealFile.backup" ) ) {
                 move( $RealFile, "$RealFile.backup" );
-                print STDERR "Notice: Create backup: $RealFile.backup\n";
             }
         }
         else {
@@ -2598,7 +2592,6 @@ sub _FileInstall {
             # if it's no reinstall or reinstall and framework file but different, back it up
             if ( !$Param{Reinstall} || ( $Param{Reinstall} && $Save ) ) {
                 move( $RealFile, "$RealFile.save" );
-                print STDERR "Notice: Create backup: $RealFile.save\n";
             }
         }
     }
