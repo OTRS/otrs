8ea9b92e6b73604808f742e2a6b38dfccf6835fe
Rolf Schmidt <rolf.schmidt@otrs.com>
2013-04-09 11:06:32 +0200

    Fixed bug#9182 - Customer Search Function -> If you go into a ticket and go back you got not the search results.

--- Kernel/Modules/CustomerTicketSearch.pm
+++ Kernel/Modules/CustomerTicketSearch.pm
@@ -227,6 +227,11 @@ sub Run {
     # show result page
     if ( $Self->{Subaction} eq 'Search' && !$Self->{EraseTemplate} ) {
 
+        # fill up profile name (e.g. with last-search)
+        if ( !$Self->{Profile} || !$Self->{SaveProfile} ) {
+            $Self->{Profile} = 'last-search';
+        }
+
         # store search URL in LastScreenOverview to make sure the
         # customer can use the "back" link as expected
         my $URL
@@ -237,11 +242,6 @@ sub Run {
             Key       => 'LastScreenOverview',
             Value     => $URL,
         );
-
-        # fill up profile name (e.g. with last-search)
-        if ( !$Self->{Profile} || !$Self->{SaveProfile} ) {
-            $Self->{Profile} = 'last-search';
-        }
 
         # save search profile (under last-search or real profile name)
         $Self->{SaveProfile} = 1;
