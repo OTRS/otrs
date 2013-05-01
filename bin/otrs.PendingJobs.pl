39ca21aedbd63b0de2ca1e63903a2d01e1e9beac
Michiel Beijen <michiel.beijen@otrs.com>
2013-05-01 05:46:58 -0400

    Code optimization, use API instead of direct DB query.

--- bin/otrs.PendingJobs.pl
+++ bin/otrs.PendingJobs.pl
@@ -61,217 +61,183 @@ print "otrs.PendingJobs.pl - check pending tickets\n";
 print "Copyright (C) 2001-2013 OTRS AG, http://otrs.com/\n";
 
 # do ticket auto jobs
-my @PendingAutoStateIDs = $CommonObject{StateObject}->StateGetStatesByType(
-    Type   => 'PendingAuto',
-    Result => 'ID',
+my @TicketIDs = $CommonObject{TicketObject}->TicketSearch(
+    Result    => 'ARRAY',
+    StateType => 'pending auto',
+    UserID    => 1,
 );
 
-if (@PendingAutoStateIDs) {
+TICKETID:
+for my $TicketID (@TicketIDs) {
 
-    # ask the database
-    $CommonObject{DBObject}->Prepare(
-        SQL => "SELECT st.id FROM ticket st WHERE "
-            . " st.ticket_state_id IN ( ${\(join ', ', @PendingAutoStateIDs)} ) ",
+    # get ticket data
+    my %Ticket = $CommonObject{TicketObject}->TicketGet(
+        TicketID      => $TicketID,
+        UserID        => 1,
+        DynamicFields => 0,
     );
 
-    # fetch the result
-    my @TicketIDs;
-    while ( my @Row = $CommonObject{DBObject}->FetchrowArray() ) {
-        push @TicketIDs, $Row[0];
+    next TICKETID if $Ticket{UntilTime} >= 1;
+
+    my %States = %{ $CommonObject{ConfigObject}->Get('Ticket::StateAfterPending') };
+
+    # error handling
+    if ( !$States{ $Ticket{State} } ) {
+        print STDERR
+            "ERROR: No Ticket::StateAfterPending found for '$Ticket{State}' in Kernel/Config.pm!\n";
+        next TICKETID;
     }
 
-    TICKETID:
-    for my $TicketID (@TicketIDs) {
+    print STDOUT
+        " Update ticket state for ticket $Ticket{TicketNumber} ($TicketID) to '$States{$Ticket{State}}'...";
 
-        # get ticket data
-        my %Ticket = $CommonObject{TicketObject}->TicketGet(
-            TicketID      => $TicketID,
-            UserID        => 1,
-            DynamicFields => 0,
-        );
+    # set new state
+    my $Success = $CommonObject{TicketObject}->StateSet(
+        TicketID => $TicketID,
+        State    => $States{ $Ticket{State} },
+        UserID   => 1,
+    );
 
-        next TICKETID if $Ticket{UntilTime} >= 1;
-
-        my %States = %{ $CommonObject{ConfigObject}->Get('Ticket::StateAfterPending') };
-
-        # error handling
-        if ( !$States{ $Ticket{State} } ) {
-            print STDERR
-                "ERROR: No Ticket::StateAfterPending found for '$Ticket{State}' in Kernel/Config.pm!\n";
-            next TICKETID;
-        }
-
-        print STDOUT
-            " Update ticket state for ticket $Ticket{TicketNumber} ($TicketID) to '$States{$Ticket{State}}'...";
-
-        # set new state
-        my $Success = $CommonObject{TicketObject}->StateSet(
-            TicketID => $TicketID,
-            State    => $States{ $Ticket{State} },
-            UserID   => 1,
-        );
-
-        # error handling
-        if ( !$Success ) {
-            print STDOUT " failed.\n";
-            next TICKETID;
-        }
-
-        # get state type for new state
-        my %State = $CommonObject{StateObject}->StateGet(
-            Name => $States{ $Ticket{State} },
-        );
-        if ( $State{TypeName} eq 'closed' ) {
-
-            # set new ticket lock
-            $CommonObject{TicketObject}->LockSet(
-                TicketID     => $TicketID,
-                Lock         => 'unlock',
-                UserID       => 1,
-                Notification => 0,
-            );
-        }
-        print STDOUT " done.\n";
+    # error handling
+    if ( !$Success ) {
+        print STDOUT " failed.\n";
+        next TICKETID;
     }
+
+    # get state type for new state
+    my %State = $CommonObject{StateObject}->StateGet(
+        Name => $States{ $Ticket{State} },
+    );
+    if ( $State{TypeName} eq 'closed' ) {
+
+        # set new ticket lock
+        $CommonObject{TicketObject}->LockSet(
+            TicketID     => $TicketID,
+            Lock         => 'unlock',
+            UserID       => 1,
+            Notification => 0,
+        );
+    }
+    print STDOUT " done.\n";
 }
 
 # do ticket reminder notification jobs
-
-# get pending states
-my @PendingReminderStateIDs = $CommonObject{StateObject}->StateGetStatesByType(
-    Type   => 'PendingReminder',
-    Result => 'ID',
+@TicketIDs = $CommonObject{TicketObject}->TicketSearch(
+    Result    => 'ARRAY',
+    StateType => 'pending reminder',
+    UserID    => 1,
 );
 
-# check if pending time has been reached and send notification
-if (@PendingReminderStateIDs) {
+TICKETID:
+for my $TicketID (@TicketIDs) {
 
-    # ask the database
-    $CommonObject{DBObject}->Prepare(
-        SQL => "SELECT st.tn, st.id, st.user_id FROM "
-            . " ticket st, ticket_state tsd WHERE "
-            . " st.ticket_state_id = tsd.id AND "
-            . " st.ticket_state_id IN ( ${\(join ', ', @PendingReminderStateIDs)} ) ",
+    # get ticket data
+    my %Ticket = $CommonObject{TicketObject}->TicketGet(
+        TicketID      => $TicketID,
+        UserID        => 1,
+        DynamicFields => 0,
     );
 
-    # fetch the result
-    my @TicketIDs;
-    while ( my @RowTmp = $CommonObject{DBObject}->FetchrowArray() ) {
-        push @TicketIDs, $RowTmp[1];
+    next TICKETID if $Ticket{UntilTime} >= 1;
+
+    # check if it is during business hours, then send reminder
+    my $CountedTime = $CommonObject{TimeObject}->WorkingTime(
+        StartTime => $CommonObject{TimeObject}->SystemTime() - ( 30 * 60 ),
+        StopTime => $CommonObject{TimeObject}->SystemTime(),
+    );
+
+    # error handling
+    if ( !$CountedTime ) {
+        if ( $CommonObject{Debug} ) {
+            $CommonObject{LogObject}->Log(
+                Priority => 'debug',
+                Message =>
+                    "Did not send pending reminder for Ticket $Ticket{TicketNumber}/$Ticket{TicketID} because it's currently outside business hours!",
+            );
+        }
+        next TICKETID;
     }
 
-    TICKETID:
-    for my $TicketID (@TicketIDs) {
+    # send the reminder to the ticket owner, if ticket is locked
+    my @UserID;
+    if (
+        $CommonObject{ConfigObject}->Get('Ticket::PendingNotificationOnlyToOwner')
+        || $Ticket{Lock} eq 'lock'
+        )
+    {
+        @UserID = ( $Ticket{OwnerID} );
+    }
 
-        # get ticket data
-        my %Ticket = $CommonObject{TicketObject}->TicketGet(
-            TicketID      => $TicketID,
-            UserID        => 1,
-            DynamicFields => 0,
+    # send the reminder to all queue subscribers and owner, if ticket is unlocked
+    else {
+        @UserID = $CommonObject{TicketObject}->GetSubscribedUserIDsByQueueID(
+            QueueID => $Ticket{QueueID},
+        );
+        push @UserID, $Ticket{OwnerID};
+    }
+
+    # add the responsible agent to the notification list
+    if (
+        !$CommonObject{ConfigObject}->Get('Ticket::PendingNotificationNotToResponsible')
+        && $CommonObject{ConfigObject}->Get('Ticket::Responsible')
+        && $Ticket{ResponsibleID} ne 1
+        )
+    {
+        push @UserID, $Ticket{ResponsibleID};
+    }
+
+    # send the reminder notification
+    print STDOUT " Send reminder notification (TicketID=$TicketID)\n";
+
+    my %AlreadySent;
+    USERID:
+    for my $UserID (@UserID) {
+
+        # remember if reminder got already sent
+        next if $AlreadySent{$UserID};
+        $AlreadySent{$UserID} = 1;
+
+        # get user data
+        my %Preferences = $CommonObject{UserObject}->GetUserData(
+            UserID => $UserID,
         );
 
-        next TICKETID if $Ticket{UntilTime} >= 1;
+        # check if a reminder has already been sent today
+        my ( $Sec, $Min, $Hour, $Day, $Month, $Year )
+            = $CommonObject{TimeObject}->SystemTime2Date(
+            SystemTime => $CommonObject{TimeObject}->SystemTime(),
+            );
 
-        # check if it is during business hours, then send reminder
-        my $CountedTime = $CommonObject{TimeObject}->WorkingTime(
-            StartTime => $CommonObject{TimeObject}->SystemTime() - ( 30 * 60 ),
-            StopTime => $CommonObject{TimeObject}->SystemTime(),
+        # get ticket history
+        my @Lines = $CommonObject{TicketObject}->HistoryGet(
+            TicketID => $Ticket{TicketID},
+            UserID   => 1,
         );
 
-        # error handling
-        if ( !$CountedTime ) {
-            if ( $CommonObject{Debug} ) {
-                $CommonObject{LogObject}->Log(
-                    Priority => 'debug',
-                    Message =>
-                        "Did not send pending reminder for Ticket $Ticket{TicketNumber}/$Ticket{TicketID} because it's currently outside business hours!",
-                );
-            }
-            next TICKETID;
-        }
-
-        # send the reminder to the ticket owner, if ticket is locked
-        my @UserID;
-        if (
-            $CommonObject{ConfigObject}->Get('Ticket::PendingNotificationOnlyToOwner')
-            || $Ticket{Lock} eq 'lock'
-            )
-        {
-            @UserID = ( $Ticket{OwnerID} );
-        }
-
-        # send the reminder to all queue subscribers and owner, if ticket is unlocked
-        else {
-            @UserID = $CommonObject{TicketObject}->GetSubscribedUserIDsByQueueID(
-                QueueID => $Ticket{QueueID},
-            );
-            push @UserID, $Ticket{OwnerID};
-        }
-
-        # add the responsible agent to the notification list
-        if ( !$CommonObject{ConfigObject}->Get('Ticket::PendingNotificationNotToResponsible') ) {
-            if ( $CommonObject{ConfigObject}->Get('Ticket::Responsible') ) {
-                if ( $Ticket{ResponsibleID} ne 1 ) {
-                    push @UserID, $Ticket{ResponsibleID};
-                }
+        my $Sent = 0;
+        for my $Line (@Lines) {
+            if (
+                $Line->{Name} =~ /PendingReminder/
+                && $Line->{Name} =~ /\Q$Preferences{UserEmail}\E/i
+                && $Line->{CreateTime} =~ /$Year-$Month-$Day/
+                )
+            {
+                $Sent = 1;
             }
         }
 
-        # send the reminder notification
-        print STDOUT " Send reminder notification (TicketID=$TicketID)\n";
+        next USERID if $Sent;
 
-        my %AlreadySent;
-        USERID:
-        for my $UserID (@UserID) {
-
-            # remember if reminder got already sent
-            next if $AlreadySent{$UserID};
-            $AlreadySent{$UserID} = 1;
-
-            # get user data
-            my %Preferences = $CommonObject{UserObject}->GetUserData(
-                UserID => $UserID,
-            );
-
-            # check if a reminder has already been sent today
-            my ( $Sec, $Min, $Hour, $Day, $Month, $Year )
-                = $CommonObject{TimeObject}->SystemTime2Date(
-                SystemTime => $CommonObject{TimeObject}->SystemTime(),
-                );
-
-            # get ticket history
-            my @Lines = $CommonObject{TicketObject}->HistoryGet(
-                TicketID => $Ticket{TicketID},
-                UserID   => 1,
-            );
-
-            my $Sent = 0;
-            for my $Line (@Lines) {
-                if (
-                    $Line->{Name} =~ /PendingReminder/
-                    && $Line->{Name} =~ /\Q$Preferences{UserEmail}\E/i
-                    && $Line->{CreateTime} =~ /$Year-$Month-$Day/
-                    )
-                {
-                    $Sent = 1;
-                }
-            }
-
-            next USERID if $Sent;
-
-            # send agent notification
-            $CommonObject{TicketObject}->SendAgentNotification(
-                TicketID              => $Ticket{TicketID},
-                Type                  => 'PendingReminder',
-                RecipientID           => $UserID,
-                CustomerMessageParams => {
-                    TicketNumber => $CommonObject{TicketObject}->TicketNumberLookup(
-                        TicketID => $Ticket{TicketID}
-                    ),
-                },
-                UserID => 1,
-            );
-        }
+        # send agent notification
+        $CommonObject{TicketObject}->SendAgentNotification(
+            TicketID              => $Ticket{TicketID},
+            Type                  => 'PendingReminder',
+            RecipientID           => $UserID,
+            CustomerMessageParams => {
+                TicketNumber => $Ticket{TicketNumber},
+            },
+            UserID => 1,
+        );
     }
 }
 
