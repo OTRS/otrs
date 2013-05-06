989757be05dc13cf2469949c851a54d84f815661
Stefan Bedorf <stefan.bedorf@otrs.com>
2013-05-06 21:06:20 +0200

    Fixed bug#[9360](http://bugs.otrs.org/show_bug.cgi?id=9360) - DynamicField Names shown in CSV output.

--- Kernel/Modules/AgentTicketSearch.pm
+++ Kernel/Modules/AgentTicketSearch.pm
@@ -752,9 +752,23 @@ sub Run {
 
         # CSV output
         if ( $GetParam{ResultForm} eq 'CSV' ) {
-            my @CSVHead;
-            my @CSVData;
 
+            # create head (actual head and head for data fill)
+            my @TmpCSVHead = @{ $Self->{Config}->{SearchCSVData} };
+            my @CSVHead = @{ $Self->{Config}->{SearchCSVData} };
+
+            # include the selected dynamic fields in CVS results
+            DYNAMICFIELD:
+            for my $DynamicFieldConfig ( @{ $Self->{CSVDynamicField} } ) {
+                next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
+                next DYNAMICFIELD if !$DynamicFieldConfig->{Name};
+                next DYNAMICFIELD if $DynamicFieldConfig->{Name} eq '';
+
+                push @TmpCSVHead, 'DynamicField_' . $DynamicFieldConfig->{Name};
+                push @CSVHead, $DynamicFieldConfig->{Label};
+            }
+
+            my @CSVData;
             for (@ViewableTicketIDs) {
 
                 # get first article data
@@ -807,22 +821,8 @@ sub Run {
                         $Self->{TicketObjectSearch}->TicketAccountedTimeGet( TicketID => $_ ),
                 );
 
-                # csv quote
-                if ( !@CSVHead ) {
-                    @CSVHead = @{ $Self->{Config}->{SearchCSVData} };
-
-                    # include the selected dynamic fields on CVS resutls
-                    DYNAMICFIELD:
-                    for my $DynamicFieldConfig ( @{ $Self->{CSVDynamicField} } ) {
-                        next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
-                        next DYNAMICFIELD if !$DynamicFieldConfig->{Name};
-                        next DYNAMICFIELD if $DynamicFieldConfig->{Name} eq '';
-
-                        push @CSVHead, 'DynamicField_' . $DynamicFieldConfig->{Name};
-                    }
-                }
                 my @Data;
-                for (@CSVHead) {
+                for (@TmpCSVHead) {
 
                     # check if header is a dynamic field and get the value from dynamic field
                     # backend
