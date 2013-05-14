939f206b244873348aebca34cb706e2357bfb1be
Marc Nilius <marc.nilius@otrs.com>
2013-05-14 12:22:49 +0200

    Fixed bug#9410 - SessionID isn't added to URL when using ajax customer search without cookies active.

--- var/httpd/htdocs/js/Core.Agent.CustomerInformationCenterSearch.js
+++ var/httpd/htdocs/js/Core.Agent.CustomerInformationCenterSearch.js
@@ -31,11 +31,18 @@ Core.Agent.CustomerInformationCenterSearch = (function (TargetNS) {
     }
 
     function Redirect(CustomerID, Event) {
+        var Session = '';
 
         Event.preventDefault();
         Event.stopPropagation();
         ShowWaitingDialog();
-        window.location.href = Core.Config.Get('Baselink') + 'Action=AgentCustomerInformationCenter;CustomerID=' + encodeURIComponent(CustomerID);
+
+        // add session data, if needed
+        if (!Core.Config.Get('SessionIDCookie')) {
+            Session = ';' + Core.Config.Get('SessionName') + '=' + Core.Config.Get('SessionID');
+        }
+
+        window.location.href = Core.Config.Get('Baselink') + 'Action=AgentCustomerInformationCenter;CustomerID=' + encodeURIComponent(CustomerID) + Session;
     }
 
     /**
