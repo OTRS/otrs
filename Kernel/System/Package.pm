1f110b1de73cbf552e1156a481cd4d86ce3c4216
Martin Gruner <martin.gruner@otrs.com>
2013-05-21 11:17:47 +0200

    Typo.

--- Kernel/System/Package.pm
+++ Kernel/System/Package.pm
@@ -1587,7 +1587,7 @@ sub PackageVerifyAll {
 
         # process result
         $Result{$Package} = $PackageVerify;
-        
+
         # set cache
         $Self->{CacheObject}->Set(
             Type  => 'PackageVerification',
@@ -1629,7 +1629,7 @@ build an opm package
                 Lang    => 'de',
                 Content => 'german description',
             },
-d        ],
+        ],
         Filelist = [
             {
                 Location   => 'Kernel/System/Lala.pm'
