8c8b43874cff91a241bfd4d59f2c0357775a7044
Manuel Hecht <manuel.hecht@otrs.com>
2013-05-24 11:06:50 +0200

    Fixed bug 9462 - Package Management page timeout due to HTTPS disabled on Proxy connections.

--- Kernel/System/WebUserAgent.pm
+++ Kernel/System/WebUserAgent.pm
@@ -123,7 +123,7 @@ sub Request {
 
     # set proxy
     if ( $Self->{Proxy} ) {
-        $UserAgent->proxy( [ 'http', 'ftp' ], $Self->{Proxy} );
+        $UserAgent->proxy( [ 'http', 'https', 'ftp' ], $Self->{Proxy} );
     }
 
     # get file
