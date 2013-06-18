5b1817c18107d50e545821a28bfd94050c82f854
Rolf Schmidt <rolf.schmidt@otrs.com>
2013-06-18 10:35:42 +0200

    Fixed bug#9425 - Wrong created date for queue view.

--- Kernel/Output/HTML/TicketOverviewSmall.pm
+++ Kernel/Output/HTML/TicketOverviewSmall.pm
@@ -150,12 +150,18 @@ sub Run {
                 DynamicFields => 0,
             );
 
+            # get ticket data
+            my %Ticket = $Self->{TicketObject}->TicketGet(
+                TicketID      => $Param{TicketID},
+                DynamicFields => 0,
+            );
+
+            # show ticket create time in current view
+            $Article{Created} = $Ticket{Created};
+
             # Fallback for tickets without articles: get at least basic ticket data
             if ( !%Article ) {
-                %Article = $Self->{TicketObject}->TicketGet(
-                    TicketID      => $TicketID,
-                    DynamicFields => 0,
-                );
+                %Article = %Ticket;
                 if ( !$Article{Title} ) {
                     $Article{Title} = $Self->{LayoutObject}->{LanguageObject}->Get(
                         'This ticket has no title or subject'
