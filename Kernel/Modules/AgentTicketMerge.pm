c67b75aa3b34cf522c34d6317a2f0cc50d75ac8f
Stefan Bedorf <stefan.bedorf@otrs.com>
2013-07-10 13:54:15 +0200

    Completed page reload handling in merge and bounce masks. This fixes bug 9595.

--- Kernel/Modules/AgentTicketMerge.pm
+++ Kernel/Modules/AgentTicketMerge.pm
@@ -241,6 +241,8 @@ sub Run {
 
             }
 
+            $Param{InformSenderChecked} = $GetParam{InformSender} ? 'checked="checked"' : '';
+
             $Output .= $Self->{LayoutObject}->Output(
                 TemplateFile => 'AgentTicketMerge',
                 Data => { %Param, %GetParam, %Ticket, %Error },
