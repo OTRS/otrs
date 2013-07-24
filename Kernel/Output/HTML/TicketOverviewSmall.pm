3bdcdfb0cfef8fd1c0e3a0d71860a091869e097d
Michiel Beijen <michiel.beijen@otrs.com>
2013-07-24 17:26:39 +0200

    Add Sessions to URL strings for menu items.
    Fixes bug#9622 - http://bugs.otrs.org/show_bug.cgi?id=9622
    Actions in Small ticket overview don't work when cookies are turned off.

--- Kernel/Output/HTML/TicketOverviewSmall.pm
+++ Kernel/Output/HTML/TicketOverviewSmall.pm
@@ -217,6 +217,14 @@ sub Run {
                         );
                     }
 
+                    # add session id if needed
+                    if ( !$Self->{LayoutObject}->{SessionIDCookie} && $Item->{Link} ) {
+                        $Item->{Link}
+                            .= ';'
+                            . $Self->{LayoutObject}->{SessionName} . '='
+                            . $Self->{LayoutObject}->{SessionID};
+                    }
+
                     # create id
                     $Item->{ID} = $Item->{Name};
                     $Item->{ID} =~ s/(\s|&|;)//ig;
