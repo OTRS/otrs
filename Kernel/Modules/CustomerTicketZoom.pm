1c96380ec728773915f2908d340d140fea0c40d6
Michiel Beijen <michiel.beijen@otrs.com>
2013-08-06 10:28:53 +0200

    Follow-up fix for bug#9613 - JSLint errors when no Dynamic Fields are defined

--- Kernel/Modules/CustomerTicketZoom.pm
+++ Kernel/Modules/CustomerTicketZoom.pm
@@ -1511,7 +1511,7 @@ sub _Mask {
 
         # create a string with the quoted dynamic field names separated by commas
         if ( IsArrayRefWithData($DynamicFieldNames) ) {
-            for my $Field ( @{$DynamicFieldNames} ) 
+            for my $Field ( @{$DynamicFieldNames} ) {
                 $Param{DynamicFieldNamesStrg} .= ", '" . $Field . "'";
             }
         }
