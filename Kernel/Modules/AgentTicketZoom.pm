1125325c44308e3b36287d26a6a49df6c56d651c
Nils Leideck <nils.leideck@leidex.net>
2013-08-12 15:53:33 +0200

    Renamed article mark buttons.

--- Kernel/Modules/AgentTicketZoom.pm
+++ Kernel/Modules/AgentTicketZoom.pm
@@ -2209,9 +2209,9 @@ sub _ArticleItem {
 
         my $Link
             = 'Action=AgentTicketZoom;Subaction=MarkAsImportant;TicketID=$Data{"TicketID"};ArticleID=$Data{"ArticleID"}';
-        my $Description = 'Mark article as important';
+        my $Description = 'Mark';
         if ($ArticleIsImportant) {
-            $Description = 'Remove important mark';
+            $Description = 'Unmark';
         }
 
         # set important menu item
