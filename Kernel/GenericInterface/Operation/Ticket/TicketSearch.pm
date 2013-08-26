7104815f32f5d2c0b8cec9136b9a396556cbd328
Martin Gruner <martin.gruner@otrs.com>
2013-08-26 10:38:06 +0200

    Allow TicketSearch GI operation to also work for logged in customers.
    This merges https://github.com/OTRS/otrs/pull/86 with slight modifications.

--- Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketSearch.pm
@@ -300,7 +300,7 @@ sub Run {
     my %DynamicFieldSearchParameters = $Self->_GetDynamicFields( %{ $Param{Data} } );
 
     # perform ticket search
-    $UserType = ($UserType eq 'Customer') ? 'CustomerUserID' : 'UserID';
+    $UserType = ( $UserType eq 'Customer' ) ? 'CustomerUserID' : 'UserID';
     my @TicketIDs = $Self->{TicketObject}->TicketSearch(
         %GetParam,
         %DynamicFieldSearchParameters,
@@ -308,7 +308,7 @@ sub Run {
         SortBy              => $Self->{SortBy},
         OrderBy             => $Self->{OrderBy},
         Limit               => $Self->{SearchLimit},
-        $userType           => $UserID,
+        $UserType           => $UserID,
         ConditionInline     => $Self->{Config}->{ExtendedSearchCondition},
         ContentSearchPrefix => '*',
         ContentSearchSuffix => '*',
