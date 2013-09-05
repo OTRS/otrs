7d60b2242a3134e566aa5905de922d21780e1b28
Michiel Beijen <michiel.beijen@otrs.com>
2013-09-05 21:37:17 +0200

    Added non-matching subject to log for better debugging.

--- Kernel/System/PostMaster/Filter/ExternalTicketNumberRecognition.pm
+++ Kernel/System/PostMaster/Filter/ExternalTicketNumberRecognition.pm
@@ -92,7 +92,7 @@ sub Run {
             if ( $Self->{Debug} >= 1 ) {
                 $Self->{LogObject}->Log(
                     Priority => 'debug',
-                    Message  => 'No number found in subject.',
+                    Message => "No number found in subject: '" . join( '', @SubjectLines ) . "'",
                 );
             }
         }
