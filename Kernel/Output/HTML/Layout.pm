48109178110752421258e4c8ff56fd58e1263425
Michiel Beijen <michiel.beijen@otrs.com>
2013-10-18 12:29:04 +0200

    Fixed Redirect on IIS 7 web server.
    The redirect had a double slash, this caused a redirect to the login screen
    if you use session cookies.
    
    This patch removes the double slash.
    
    The redirect is no longer needed for IIS 7 and up, so the workaround is now
    only applied for IIS6 (IIS5 came with Windows 2000 Server and is no longer
    supported.)

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -980,14 +980,15 @@ sub Redirect {
         $Param{Redirect} .= $Param{OP};
     }
 
-    # check if IIS is used, add absolute url for IIS workaround
+    # check if IIS 6 is used, add absolute url for IIS workaround
     # see also:
     #  o http://bugs.otrs.org/show_bug.cgi?id=2230
+    #  o http://bugs.otrs.org/show_bug.cgi?id=9835
     #  o http://support.microsoft.com/default.aspx?scid=kb;en-us;221154
-    if ( $ENV{SERVER_SOFTWARE} =~ /^microsoft\-iis/i ) {
+    if ( $ENV{SERVER_SOFTWARE} =~ /^microsoft\-iis\/6/i ) {
         my $Host = $ENV{HTTP_HOST} || $Self->{ConfigObject}->Get('FQDN');
         my $HttpType = $Self->{ConfigObject}->Get('HttpType');
-        $Param{Redirect} = $HttpType . '://' . $Host . '/' . $Param{Redirect};
+        $Param{Redirect} = $HttpType . '://' . $Host . $Param{Redirect};
     }
     my $Output = $Cookies . $Self->Output( TemplateFile => 'Redirect', Data => \%Param );
 
