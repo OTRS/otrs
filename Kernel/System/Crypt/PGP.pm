81b5bc1cbe7a4e6c7665f8e507c6ce3cd78e92e8
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2013-11-12 11:52:28 -0600

    Follow-up fix for bug#9921 - Reply to previously signed email breaks PGP verification.
    -reworked regular expressions

--- Kernel/System/Crypt/PGP.pm
+++ Kernel/System/Crypt/PGP.pm
@@ -519,16 +519,10 @@ sub Verify {
         }
     }
 
-    # looks for text before and after the signature (but ignote quoted text)
+    # looks for text before and after the signature (but ignore if is in quoted text)
     if (
-        (
-            $Param{Message} =~ m{ \s* \S+ \s* \Q-----BEGIN PGP SIGNED MESSAGE-----\E }xmsg
-            || $Param{Message} =~ m{ \Q-----END PGP SIGNATURE-----\E \s* \S+ \s* }xmsg
-        )
-        && (
-            $Param{Message} !~ m{ (> \s)+ \Q-----BEGIN PGP SIGNED MESSAGE-----\E }xmsg
-            && $Param{Message} !~ m{ (> \s)+ \Q-----END PGP SIGNATURE-----\E }xmsg
-        )
+        $Param{Message} =~ m{ \s* \S+ \s* ^ \s* -----BEGIN [ ] PGP [ ] SIGNED [ ] MESSAGE----- }xmsg
+        || $Param{Message} =~ m{ ^ \s* -----END [ ] PGP [ ] SIGNATURE----- \s* \S+ \s* }xmsg
         )
     {
         push @Warnings, {
