3ac49959e3f62614fd176da59e45c5147cf1e9bd
Marc Nilius <marc.nilius@otrs.com>
2013-11-18 14:34:55 +0100

    Updated CKEditor to version 4.3.

---
+++ var/httpd/htdocs/js/thirdparty/ckeditor-4.3/plugins/placeholder/lang/ca.js
@@ -0,0 +1,5 @@
+﻿/*
+ Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang("placeholder","ca",{title:"Propietats del marcador de posició",toolbar:"Crear Marcador de posició",name:"Placeholder Name",invalidName:"The placeholder can not be empty and can not contain any of following characters: [, ], <, >",pathName:"placeholder"});
