63cd4b2eca42a1c12c3bb2ecb54f5cbf69d204c5
Marc Nilius <marc.nilius@libertello.de>
2013-11-18 14:32:08 +0100

    Updated CKEditor to version 4.3.

---
+++ var/httpd/htdocs/js/thirdparty/ckeditor-4.3/plugins/image2/lang/zh-cn.js
@@ -0,0 +1,1 @@
+﻿CKEDITOR.plugins.setLang("image2","zh-cn",{alt:"替换文本",btnUpload:"上传到服务器",captioned:"Captioned image",infoTab:"图象",lockRatio:"锁定比例",menu:"图象属性",pathName:"image",pathNameCaption:"caption",resetSize:"原始尺寸",resizer:"Click and drag to resize",title:"图象属性",uploadTab:"Upload",urlMissing:"缺少图像源文件地址"});
