e2d0b112ecee37dacc13baf204170bc98025c874
Carlos Garcia <carlos.garcia@otrs.com>
2013-11-28 23:34:42 -0600

    Fixed bug#9911 - Column filter by 'Queue' shouldn't be available on 'Queue view'.

--- Kernel/Output/HTML/TicketOverviewSmall.pm
+++ Kernel/Output/HTML/TicketOverviewSmall.pm
@@ -176,6 +176,11 @@ sub new {
         'SLA'            => 1,
     };
 
+    # remove queue from filters on AgentTicketQueue
+    if ( $Self->{Action} eq 'AgentTicketQueue') {
+        delete $Self->{AvailableFilterableColumns}->{Queue};
+    }
+
     # get filtrable dynamic fields
     # cycle trough the activated dynamic fields for this screen
     DYNAMICFIELD:
