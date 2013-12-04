7283e8d0fd4f60fe19cc546fe85aea2614444467
Carlos Garcia <carlos.garcia@otrs.com>
2013-12-04 14:04:12 -0600

    Implemented QA suggestions, bug#9950 - Ticket Split takes uses system address as the customer.

--- Kernel/Modules/AgentTicketPhone.pm
+++ Kernel/Modules/AgentTicketPhone.pm
@@ -281,8 +281,9 @@ sub Run {
             # set To as article from
             my $FromQueueID;
             if ( IsStringWithData( $Article{To} ) ) {
-                $FromQueueID = $Self->{QueueObject}->QueueLookup( Queue => $Article{To} );
-                if ( !defined $FromQueueID ) {
+                my %Queues = $Self->{QueueObject}->QueueList();
+                my %QueueLookup = reverse %Queues;
+                if ( !defined $QueueLookup{ $Article{To} } ) {
                     $ArticleFrom = $Article{To};
                 }
             }
