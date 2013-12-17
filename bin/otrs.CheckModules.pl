86e884a4f9beb5c86b0eb719dfcc7624996f7fe5
Manuel Hecht <manuel.hecht@otrs.com>
2013-12-17 12:32:03 +0100

    Added List::Util::XS to check if Scalar::Util is installed in the required c version.

--- bin/otrs.CheckModules.pl
+++ bin/otrs.CheckModules.pl
@@ -171,6 +171,11 @@ my @NeededModules = (
         ],
     },
     {
+        Module   => 'List::Util::XS',
+        Required => 1,
+        Comment  => "Do a 'force install Scalar::Util' via cpan shell to fix this problem. Please make sure to have an c compiler and make installed before.",
+    },
+    {
         Module   => 'Locale::Codes',
         Required => 1,
     },
