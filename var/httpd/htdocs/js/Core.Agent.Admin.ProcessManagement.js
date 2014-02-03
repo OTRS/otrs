209ad768ff45ee26e9a795f8685a99f1e076d830
Marc Nilius <marc.nilius@libertello.de>
2014-02-03 14:25:20 +0100

    Fixed bug#9616 - Too long activities and transitions are not displayed correctly.

--- var/httpd/htdocs/js/Core.Agent.Admin.ProcessManagement.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.ProcessManagement.js
@@ -1162,8 +1162,8 @@ Core.Agent.Admin.ProcessManagement = (function (TargetNS) {
         // set current start and end activity (just for information purposes, not changeable)
         $.each(PathInfo, function(Activity, Transition) {
             if (Activity === StartActivityID && typeof Transition[CurrentTransitionEntityID] !== 'undefined') {
-                $('#StartActivity').text(ActivityInfo[Activity].Name);
-                $('#EndActivity').text(ActivityInfo[Transition[CurrentTransitionEntityID].ActivityEntityID].Name);
+                $('#StartActivity').attr('title', ActivityInfo[Activity].Name).text(ActivityInfo[Activity].Name);
+                $('#EndActivity').attr('title', ActivityInfo[Transition[CurrentTransitionEntityID].ActivityEntityID].Name).text(ActivityInfo[Transition[CurrentTransitionEntityID].ActivityEntityID].Name);
 
                 StartActivityEntityID     = Activity;
                 EndActivityEntityID       = Transition[CurrentTransitionEntityID].ActivityEntityID;
