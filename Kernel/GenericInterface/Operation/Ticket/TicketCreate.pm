acf6aa35ed1c60ba1306ba9f9429e2043e69e013
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2014-02-07 08:29:45 -0600

    Followup for bug#10205 - GenericInterface: Mandatory TimeUnits can't be 0.

--- Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm
@@ -876,7 +876,7 @@ sub _CheckArticle {
     # check Article->TimeUnit
     # TimeUnit could be required or not depending on sysconfig option
     if (
-        !defined $Article->{TimeUnit}
+        ( !defined $Article->{TimeUnit} || !IsStringWithData( $Article->{TimeUnit} ) )
         && $Self->{ConfigObject}->{'Ticket::Frontend::AccountTime'}
         && $Self->{ConfigObject}->{'Ticket::Frontend::NeedAccountedTime'}
         )
