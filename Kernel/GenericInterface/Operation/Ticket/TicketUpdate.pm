2b78d957169749a0d265e9efab106c4b3c00f612
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2014-02-07 16:24:23 -0600

    Fixed: Value "0" for DynamicsFields prevents TicketCreation (bug#10214).

--- Kernel/GenericInterface/Operation/Ticket/TicketUpdate.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketUpdate.pm
@@ -457,9 +457,9 @@ sub Run {
         for my $DynamicFieldItem (@DynamicFieldList) {
             if ( !IsHashRefWithData($DynamicFieldItem) ) {
                 return {
-                    ErrorCode => 'TicketCreate.InvalidParameter',
+                    ErrorCode => 'TicketUpdate.InvalidParameter',
                     ErrorMessage =>
-                        "TicketCreate: Ticket->DynamicField parameter is invalid!",
+                        "TicketUpdate: Ticket->DynamicField parameter is invalid!",
                 };
             }
 
@@ -1039,7 +1039,7 @@ sub _CheckDynamicField {
 
     # check DynamicField item internally
     for my $Needed (qw(Name Value)) {
-        if ( !$DynamicField->{$Needed} ) {
+        if ( !defined $DynamicField->{$Needed} || !IsStringWithData( $DynamicField->{$Needed} ) ) {
             return {
                 ErrorCode    => 'TicketUpdate.MissingParameter',
                 ErrorMessage => "TicketUpdate: DynamicField->$Needed  parameter is missing!",
