a1dc0cae202092009a8ce3d3611c8a296e41e752
Martin Gruner <martin.gruner@otrs.com>
2014-03-18 08:58:41 +0100

    Fixed: OTRS does not protect against clickjacking (bug#10374).

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2131,6 +2131,8 @@ sub Attachment {
         $Output .= "Pragma: no-cache\n";
     }
     $Output .= "Content-Length: $Param{Size}\n";
+    $Output .= "X-UA-Compatible: IE=edge,chrome=1\n";
+    $Output .= "X-Frame-Options: SAMEORIGIN\n";
 
     if ( $Param{Charset} ) {
         $Output .= "Content-Type: $Param{ContentType}; charset=$Param{Charset};\n\n";
