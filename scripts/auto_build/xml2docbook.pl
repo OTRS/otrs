f39cb16637ec0dcc9bdb300002f7ff2e27b97392
Martin Gruner <martin.gruner@otrs.com>
2014-03-18 14:19:09 +0100

    Fixed XML2Docbook script.

--- scripts/auto_build/xml2docbook.pl
+++ scripts/auto_build/xml2docbook.pl
@@ -30,25 +30,17 @@ use lib dirname($RealBin) . "/../Kernel/cpan-lib";
 
 use Getopt::Std;
 
-use Kernel::Config;
-use Kernel::System::Log;
-use Kernel::System::Main;
-use Kernel::System::Time;
-use Kernel::System::DB;
-use Kernel::System::SysConfig;
+use Kernel::System::ObjectManager;
 
 # create common objects
-my %CommonObject = ();
-$CommonObject{ConfigObject} = Kernel::Config->new();
-$CommonObject{LogObject}    = Kernel::System::Log->new(
-    LogPrefix => 'OTRS-xml2docbook',
-    %CommonObject,
+local $Kernel::OM = Kernel::System::ObjectManager->new(
+    LogObject => {
+        LogPrefix => 'OTRS-xml2docbook.pl',
+    },
 );
-$CommonObject{MainObject}      = Kernel::System::Main->new(%CommonObject);
-$CommonObject{TimeObject}      = Kernel::System::Time->new(%CommonObject);
-$CommonObject{EncodeObject}    = Kernel::System::Encode->new(%CommonObject);
-$CommonObject{DBObject}        = Kernel::System::DB->new(%CommonObject);
-$CommonObject{SysConfigObject} = Kernel::System::SysConfig->new(%CommonObject);
+my %CommonObject = $Kernel::OM->ObjectHash(
+    Objects => [qw(ConfigObject EncodeObject LogObject MainObject DBObject SysConfigObject)],
+);
 
 # get options
 my %Opts = ();
