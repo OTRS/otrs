d7924afa10f1a13f23e85a5bae52b231d428309b
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2014-04-03 17:34:05 -0600

    Followup for bug#10378 - SessionID created with generic interface wont work using it as Agent.
    Conflicts:
    	Kernel/GenericInterface/Operation/Session/Common.pm

--- Kernel/GenericInterface/Operation/Session/Common.pm
+++ Kernel/GenericInterface/Operation/Session/Common.pm
@@ -14,12 +14,13 @@ use warnings;
 
 use vars qw(@ISA);
 
-use Kernel::System::User;
 use Kernel::System::Auth;
-use Kernel::System::Group;
 use Kernel::System::AuthSession;
-use Kernel::System::CustomerUser;
 use Kernel::System::CustomerAuth;
+use Kernel::System::CustomerGroup;
+use Kernel::System::CustomerUser;
+use Kernel::System::Group;
+use Kernel::System::User;
 use Kernel::System::VariableCheck qw(:all);
 
 =head1 NAME
@@ -125,8 +126,9 @@ sub new {
     $Self->{GroupObject}   = Kernel::System::Group->new( %{$Self} );
     $Self->{AuthObject}    = Kernel::System::Auth->new( %{$Self} );
 
-    $Self->{CustomerUserObject} = Kernel::System::CustomerUser->new( %{$Self} );
-    $Self->{CustomerAuthObject} = Kernel::System::CustomerAuth->new( %{$Self} );
+    $Self->{CustomerUserObject}  = Kernel::System::CustomerUser->new( %{$Self} );
+    $Self->{CustomerGroupObject} = Kernel::System::CustomerGroup->new( %{$Self} );
+    $Self->{CustomerAuthObject}  = Kernel::System::CustomerAuth->new( %{$Self} );
 
     return $Self;
 }
@@ -196,9 +198,11 @@ sub CreateSessionID {
     # login is invalid
     return if !$User;
 
+    my $GroupObject = $UserType eq 'User' ? 'GroupObject' : 'CustomerGroupObject';
+
     # get groups rw/ro
     for my $Type (qw(rw ro)) {
-        my %GroupData = $Self->{GroupObject}->GroupMemberList(
+        my %GroupData = $Self->{$GroupObject}->GroupMemberList(
             Result => 'HASH',
             Type   => $Type,
             UserID => $UserData{UserID},
