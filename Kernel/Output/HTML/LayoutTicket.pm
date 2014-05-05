4feba3fdada7b1db410f394c6ea8183ccdb78aa9
Dennis Schmelter <dennis.schmelter@otrs.com>
2014-05-05 18:44:22 +0200

    Fixed bug#9350 - Initial notes by ticket split not visible it the ticket zoom.

--- Kernel/Output/HTML/LayoutTicket.pm
+++ Kernel/Output/HTML/LayoutTicket.pm
@@ -666,6 +666,8 @@ sub ArticleQuote {
         my %ArticleIndex = $Self->{TicketObject}->ArticleAttachmentIndex(
             ArticleID => $Param{ArticleID},
             UserID    => $Self->{UserID},
+            StripPlainBodyAsAttachment => 3,
+            Article => \%Article,
         );
         for my $Index ( sort keys %ArticleIndex ) {
             my %Attachment = $Self->{TicketObject}->ArticleAttachment(
