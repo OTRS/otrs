99982c22074da1b433f498951ff351d4b2e1323e
Martin Gruner <martin.gruner@otrs.com>
2014-06-23 10:18:51 +0200

    Fixed typo.

--- Kernel/Modules/AgentTicketMove.pm
+++ Kernel/Modules/AgentTicketMove.pm
@@ -1031,7 +1031,7 @@ sub AgentMove {
         my $Counter = 1;
         for my $User ( reverse @{ $Param{OldUser} } ) {
             next if $UserHash{ $User->{UserID} };
-            $UserHash{ $User->{UserID} } = "$Counter: $User->{UserFullname} $User->{UserFirstname} ($User->{UserLogin})";
+            $UserHash{ $User->{UserID} } = "$Counter: $User->{UserFullname} ($User->{UserLogin})";
             $Counter++;
         }
     }
