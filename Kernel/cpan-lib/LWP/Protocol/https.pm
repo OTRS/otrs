f9c427f90ff996b94fa0b3979071ea722c62679f
Manuel Hecht <manuel.hecht@otrs.com>
2014-08-25 14:16:24 +0200

    Updated CPAN module LWP::Protocol::https to version 6.06.

--- Kernel/cpan-lib/LWP/Protocol/https.pm
+++ Kernel/cpan-lib/LWP/Protocol/https.pm
@@ -1,10 +1,11 @@
 package LWP::Protocol::https;
 
 use strict;
-our $VERSION = "6.04";
+our $VERSION = "6.06";
 
 require LWP::Protocol::http;
 our @ISA = qw(LWP::Protocol::http);
+require Net::HTTPS;
 
 sub socket_type
 {
@@ -33,10 +34,10 @@ sub _extra_sock_opts
 Can't verify SSL peers without knowing which Certificate Authorities to trust
 
 This problem can be fixed by either setting the PERL_LWP_SSL_CA_FILE
-envirionment variable or by installing the Mozilla::CA module.
+environment variable or by installing the Mozilla::CA module.
 
 To disable verification of SSL peers set the PERL_LWP_SSL_VERIFY_HOSTNAME
-envirionment variable to 0.  If you do this you can't be sure that you
+environment variable to 0.  If you do this you can't be sure that you
 communicate with the expected peer.
 EOT
 		}
@@ -49,17 +50,78 @@ EOT
     return (%ssl_opts, $self->SUPER::_extra_sock_opts);
 }
 
+#------------------------------------------------------------
+# _cn_match($common_name, $san_name) 
+#  common_name: an IA5String
+#  san_name: subjectAltName
+# initially we were only concerned with the dNSName
+# and the 'left-most' only wildcard as noted in 
+#   https://tools.ietf.org/html/rfc6125#section-6.4.3
+# this method does not match any wildcarding in the
+# domain name as listed in section-6.4.3.3
+#
+sub _cn_match {
+    my( $me, $common_name, $san_name ) = @_;
+
+    # /CN has a '*.' prefix
+    # MUST be an FQDN -- fishing?
+    return 0 if( $common_name =~ /^\*\./ );
+    
+    my $re = q{}; # empty string
+
+     # turn a leading "*." into a regex
+    if( $san_name =~ /^\*\./ ) {
+        $san_name =~ s/\*//;
+        $re = "[^.]+";
+    }
+
+      # quotemeta the rest and match anchored
+    if( $common_name =~ /^$re\Q$san_name\E$/ ) {
+        return 1;
+    }
+    return 0;
+}
+
+#-------------------------------------------------------
+# _in_san( cn, cert )
+#  'cn' of the form  /CN=host_to_check ( "Common Name" form )
+#  'cert' any object that implements a peer_certificate('subjectAltNames') method
+#   which will return an array of  ( type-id, value ) pairings per
+#   http://tools.ietf.org/html/rfc5280#section-4.2.1.6
+# if there is no subjectAltNames there is nothing more to do.
+# currently we have a _cn_match() that will allow for simple compare.
+sub _in_san
+{
+    my($me, $cn, $cert) = @_;
+	
+	  # we can return early if there are no SAN options.
+	my @sans = $cert->peer_certificate('subjectAltNames');
+	return unless scalar @sans; 
+	
+	(my $common_name = $cn) =~ s/.*=//; # strip off the prefix.
+   
+      # get the ( type-id, value ) pairwise
+      # currently only the basic CN to san_name check
+    while( my ( $type_id, $value ) = splice( @sans, 0, 2 ) ) {
+        return 'ok' if $me->_cn_match($common_name,$value);
+    }
+    return;
+}
+
 sub _check_sock
 {
     my($self, $req, $sock) = @_;
     my $check = $req->header("If-SSL-Cert-Subject");
     if (defined $check) {
-	my $cert = $sock->get_peer_certificate ||
-	    die "Missing SSL certificate";
-	my $subject = $cert->subject_name;
-	die "Bad SSL certificate subject: '$subject' !~ /$check/"
-	    unless $subject =~ /$check/;
-	$req->remove_header("If-SSL-Cert-Subject");  # don't pass it on
+        my $cert = $sock->get_peer_certificate ||
+            die "Missing SSL certificate";
+        my $subject = $cert->subject_name;
+        unless ( $subject =~ /$check/ ) {
+            my $ok = $self->_in_san( $check, $cert);
+            die "Bad SSL certificate subject: '$subject' !~ /$check/"
+                unless $ok;
+        }
+        $req->remove_header("If-SSL-Cert-Subject");  # don't pass it on
     }
 }
 
@@ -83,10 +145,24 @@ sub _get_sock_info
     $res->header("Client-SSL-Socket-Class" => $Net::HTTPS::SSL_SOCKET_CLASS);
 }
 
+# upgrade plain socket to SSL, used for CONNECT tunnel when proxying https
+# will only work if the underlying socket class of Net::HTTPS is
+# IO::Socket::SSL, but code will only be called in this case
+if ( $Net::HTTPS::SSL_SOCKET_CLASS->can('start_SSL')) {
+    *_upgrade_sock = sub {
+	my ($self,$sock,$url) = @_;
+	$sock = LWP::Protocol::https::Socket->start_SSL( $sock,
+	    SSL_verifycn_name => $url->host,
+	    $self->_extra_sock_opts,
+	);
+	$@ = LWP::Protocol::https::Socket->errstr if ! $sock;
+	return $sock;
+    }
+}
+
 #-----------------------------------------------------------
 package LWP::Protocol::https::Socket;
 
-require Net::HTTPS;
 our @ISA = qw(Net::HTTPS LWP::Protocol::http::SocketMethods);
 
 1;
