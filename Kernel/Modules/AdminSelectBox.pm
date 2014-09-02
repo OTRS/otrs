3bcf75df670103e5786f2970e9921b3e5c3b7b41
Martin Gruner <martin.gruner@otrs.com>
2014-09-02 12:24:40 +0200

    Also allow queries for database meta information.
    Conflicts:
    	Kernel/Modules/AdminSelectBox.pm

--- Kernel/Modules/AdminSelectBox.pm
+++ Kernel/Modules/AdminSelectBox.pm
@@ -65,8 +65,7 @@ sub Run {
             $Errors{ErrorType}  = 'FieldRequired';
         }
 
-        # check SQL query, only select queries are allowed
-        if ( uc( $Param{SQL} ) !~ m{\A\s*SELECT}smx ) {
+        if ( uc($Param{SQL}) !~ m{ \A \s* (?:SELECT|SHOW|DESC) }smx ) {
             $Errors{SQLInvalid} = 'ServerError';
             $Errors{ErrorType}  = 'SQLIsNotSelect';
         }
