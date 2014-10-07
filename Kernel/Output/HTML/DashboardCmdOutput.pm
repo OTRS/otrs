3b3d716e3b1bc9baed7e3a6bfbe99339bea822d6
Martin Gruner <martin.gruner@otrs.com>
2014-10-07 09:31:07 +0200

    Added new dashboard module that shows the output of an external command, thanks to ib.pl.

---
+++ Kernel/Output/HTML/DashboardCmdOutput.pm
@@ -0,0 +1,69 @@
+# --
+# Kernel/Output/HTML/DashboardCmdOutput.pm
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::DashboardCmdOutput;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::Output::HTML::Layout',
+    'Kernel::System::Encode',
+);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    for (qw(Config Name UserID)) {
+        die "Got no $_!" if ( !$Self->{$_} );
+    }
+
+    return $Self;
+}
+
+sub Preferences {
+    my ( $Self, %Param ) = @_;
+
+    return;
+}
+
+sub Config {
+    my ( $Self, %Param ) = @_;
+
+    return (
+        %{ $Self->{Config} }
+    );
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # command to run
+    my $Cmd = $Self->{Config}->{Cmd};
+
+    my $CmdOutput = qx{$Cmd 2>&1};
+
+    $Kernel::OM->Get('Kernel::System::Encode')->EncodeInput( \$CmdOutput );
+
+    my $Content = $Kernel::OM->Get('Kernel::Output::HTML::Layout')->Output(
+        TemplateFile => 'AgentDashboardCmdOutput',
+        Data         => {
+            CmdOutput => $CmdOutput,
+            %{ $Self->{Config} },
+        },
+    );
+
+    return $Content;
+}
+
+1;
