e6455626c91f80edae9a70f4980ca5aa5246f55a
Marc Nilius <marc.nilius@libertello.de>
2014-10-14 10:30:38 +0200

    Removed not needed onchange plugin of CKEditor.

--- var/httpd/htdocs/js/Core.UI.RichTextEditor.js
+++ var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -92,7 +92,7 @@ Core.UI.RichTextEditor = (function (TargetNS) {
             disableNativeSpellChecker: false,
             toolbar: CheckFormID().length ? Core.Config.Get('RichText.Toolbar') : Core.Config.Get('RichText.ToolbarWithoutImage'),
             filebrowserUploadUrl: Core.Config.Get('Baselink'),
-            extraPlugins: Core.Config.Get('RichText.SpellChecker') ? 'aspell,onchange,splitquote' : 'onchange,splitquote',
+            extraPlugins: Core.Config.Get('RichText.SpellChecker') ? 'aspell,splitquote' : 'splitquote',
             entities: false
         });
         if (CheckFormID().length) {
