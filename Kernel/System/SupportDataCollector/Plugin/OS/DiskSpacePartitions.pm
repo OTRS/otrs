d378351e2b4a8509874b1dba5d7e8f84b9cc66fe
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2014-11-23 18:56:48 -0600

    Fixed: Missing log name partitions in Service Center (bug#10893).

--- Kernel/System/SupportDataCollector/Plugin/OS/DiskSpacePartitions.pm
+++ Kernel/System/SupportDataCollector/Plugin/OS/DiskSpacePartitions.pm
@@ -23,7 +23,7 @@ sub GetDisplayPath {
 sub Run {
     my $Self = shift;
 
-    # Check if used OS is a linux system
+    # Check if used OS is a Linux system
     if ( $^O !~ /(linux|unix|netbsd|freebsd|darwin)/i ) {
         return $Self->GetResults();
     }
@@ -35,36 +35,72 @@ sub Run {
         $Commandline = "df -l";
     }
 
-    my %UsedIdentifiers;
-
-    my $In;
-    if ( open( $In, "-|", "$Commandline" ) ) {
-
-        my @Partitions;
-
-        while (<$In>) {
-            if ( $_ =~ /^(.+?)\s.*\s(\d+)%.+?$/ ) {
-                my ( $Partition, $UsedPercent ) = $_ =~ /^(.+?)\s.*?\s(\d+)%.+?$/;
-
-                my $Identifier = $Partition;
-                if ( defined $UsedIdentifiers{$Partition} ) {
-                    $Identifier .= '_' . $UsedIdentifiers{$Partition};
-                    $UsedIdentifiers{$Partition}++;
-                }
-                else {
-                    $UsedIdentifiers{$Partition} = 1;
-                }
-
-                push @Partitions, {
-                    Identifier => $Identifier,
-                    Label      => $Partition,
-                    Value      => $UsedPercent . '%',
-                };
-            }
-        }
+    # run the command an store the result on an array
+    my @Lines;
+    if ( open( my $In, "-|", "$Commandline" ) ) {
+        @Lines = <$In>;
         close($In);
-        for my $Patition (@Partitions) {
-            $Self->AddResultInformation( %{$Patition} );
+    }
+
+    # clean results, in some systems when partition is too long it splits the line in two, it is
+    #   needed to have all partition information in just one line for example
+    #   From:
+    #   /dev/mapper/system-tmp
+    #                   2064208    85644   1873708   5% /tmp
+    #   To:
+    #   /dev/mapper/system-tmp                   2064208    85644   1873708   5% /tmp
+    my @CleanLines;
+    my $PreviousLine;
+
+    LINE:
+    for my $Line (@Lines) {
+
+        chomp $Line;
+
+        # if line does not have percent number (then it should only contain the partition)
+        if ( $Line !~ m{\d+%} ) {
+
+            # remember the line
+            $PreviousLine = $Line;
+            next LINE;
+        }
+
+        # if line starts with just spaces and have a percent number
+        elsif ( $Line =~ m{\A \s+ (:? \d+ | \s+)+ \d+ % .+? \z}msx ) {
+
+            # concatenate previous line and store it
+            push @CleanLines, $PreviousLine . $Line;
+            next LINE;
+        }
+
+        # otherwise store the line as it is
+        push @CleanLines, $Line;
+    }
+
+    my %UsedIdentifiers;
+    LINE:
+    for my $Line (@CleanLines) {
+
+        # remove leading white spaces in line
+        $Line =~ s{\A\s+}{};
+
+        if ( $Line =~ m{\A .+? \s .* \s \d+ % .+? \z}msx ) {
+            my ( $Partition, $UsedPercent ) = $Line =~ m{\A (.+?) \s .*? \s (\d+)%.+? \z}msx;
+
+            my $Identifier = $Partition;
+            if ( defined $UsedIdentifiers{$Partition} ) {
+                $Identifier .= '_' . $UsedIdentifiers{$Partition};
+                $UsedIdentifiers{$Partition}++;
+            }
+            else {
+                $UsedIdentifiers{$Partition} = 1;
+            }
+
+            $Self->AddResultInformation(
+                Identifier => $Identifier,
+                Label      => $Partition,
+                Value      => $UsedPercent . '%',
+            );
         }
     }
 
