d0bd52befcd5b9049346c2e07f10e519e8e7c6a4
Dennis Schmelter <dennis.schmelter@otrs.com>
2014-12-04 13:55:48 +0100

    Removed incorrect notice about SQL query length.

--- Kernel/System/DB.pm
+++ Kernel/System/DB.pm
@@ -428,16 +428,6 @@ sub Do {
         );
     }
 
-    # check length, don't use more than 4 k
-    if ( bytes::length( $Param{SQL} ) > 4 * 1024 ) {
-        $Kernel::OM->Get('Kernel::System::Log')->Log(
-            Caller   => 1,
-            Priority => 'notice',
-            Message  => 'Your SQL is longer than 4k, this does not work on many '
-                . 'databases. Use bind instead! SQL: ' . $Param{SQL},
-        );
-    }
-
     # send sql to database
     if ( !$Self->{dbh}->do( $Param{SQL}, undef, @Array ) ) {
         $Kernel::OM->Get('Kernel::System::Log')->Log(
