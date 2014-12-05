49aff5f69d22d274625ced74fe4f65e56b96d27f
Marc A. Bonsels <marc.bonsels@otrs.com>
2014-12-05 13:53:58 +0100

    Classes where not set correctly, thanks to @Retiebralanak.

--- Kernel/Modules/AgentTicketProcess.pm
+++ Kernel/Modules/AgentTicketProcess.pm
@@ -2286,7 +2286,7 @@ sub _RenderDynamicField {
     );
 
     my %Data = (
-        Name    => $DynamicFieldHTML->{Name},
+        Name    => $DynamicFieldConfig->{Name},
         Label   => $DynamicFieldHTML->{Label},
         Content => $DynamicFieldHTML->{Field},
     );
