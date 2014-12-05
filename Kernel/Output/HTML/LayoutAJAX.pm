6246d4d4f105ee21c5a9787819c9c8ddb35c9d09
Martin Gruner <martin.gruner@otrs.com>
2014-12-05 09:32:05 +0100

    Fixed: JS error if no entry in queue list (bug#10932).

--- Kernel/Output/HTML/LayoutAJAX.pm
+++ Kernel/Output/HTML/LayoutAJAX.pm
@@ -55,7 +55,7 @@ sub BuildSelectionJSON {
         my %Param = %{$Data};
 
         # check needed stuff
-        for (qw(Name Data)) {
+        for (qw(Name)) {
             if ( !defined $Param{$_} ) {
                 $Self->{LogObject}->Log(
                     Priority => 'error',
@@ -65,7 +65,17 @@ sub BuildSelectionJSON {
             }
         }
 
-        if ( ref $Param{Data} eq '' ) {
+        if ( !defined( $Param{Data} ) ) {
+            if ( !$Param{PossibleNone} ) {
+                $Self->{LogObject}->Log(
+                    Priority => 'error',
+                    Message  => "Need Data!"
+                );
+                return;
+            }
+            $DataHash{''} = '-';
+        }
+        elsif ( ref $Param{Data} eq '' ) {
             $DataHash{ $Param{Name} } = $Param{Data};
         }
         elsif ( defined $Param{KeepData} && $Param{KeepData} ) {
