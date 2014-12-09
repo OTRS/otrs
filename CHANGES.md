dd3eae7f098ffe233c85bf05e99888cf24aaba6c
Marc Nilius <marc.nilius@libertello.de>
2014-12-09 10:29:07 +0100

    Fixed bug#10899 - SplitQuote in WYSIWYG editor creates unnecessary empty quote line when splitting quote at end of line.

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,5 @@
 #4.0.3 2014-??-??
+ - 2014-12-09 Fixed bug#[10899](http://bugs.otrs.org/show_bug.cgi?id=10899) - SplitQuote in WYSIWYG editor creates unnecessary empty quote line when splitting quote at end of line.
  - 2014-12-08 Fixed bug#[10830](http://bugs.otrs.org/show_bug.cgi?id=10830) - Textarea Limitation in Generic Agent.
  - 2014-12-08 Fixed bug#[10914](http://bugs.otrs.org/show_bug.cgi?id=10914) - Migration issue to 4.0.1 (mssql).
  - 2014-12-08 Fixed bug#[10920](http://bugs.otrs.org/show_bug.cgi?id=10920) - ProcessManagement: Deleting Activities from canvas does not update process layout.
--- var/httpd/htdocs/js/thirdparty/ckeditor-4.4.5/plugins/splitquote/plugin.js
+++ var/httpd/htdocs/js/thirdparty/ckeditor-4.4.5/plugins/splitquote/plugin.js
@@ -5,12 +5,39 @@ CKEDITOR.plugins.add('splitquote', {
 
         editor.addCommand('splitQuote', {
             exec: function(editor) {
-                var helper, quote, text, range;
+                var helper, quote, text, range, cursorNode, cursorNodeLength, cursorNodeOffset;
 
                 // is the cursor position within a quote (otrs-style)?
                 quote = editor.elementPath().contains('div', false, true);
                 if (quote !== null && quote.hasAttribute('type') && quote.getAttribute('type') === 'cite') {
-                    // create a helper element to insetr at cursor position
+
+                    // Special handling:
+                    // If the cursor is at the end of a line, the <br> of this line is still behind
+                    // the cursor. If the quote is now splitted, it splits before the <br>.
+                    // We try to find out, if the cursor is at the end of a line just before a <br>
+                    // and than move the cursor behind the <br>, which at the end moves the cursor
+                    // to the beginning of the next line.
+                    // Splitting the quote there will keep the <br> at the end of the previous line and
+                    // wil not create an unnecessary empty new line after the split position.
+                    // This will still not work correctly if a formatting ends at the end of line,
+                    // like e.g. <div>blabla<u>test</u><br>blabla</div>
+
+                    // get current cursor position, container node, length and offset
+                    range = editor.getSelection().getRanges()[0];
+                    cursorNode = range.startContainer;
+                    cursorNodeLength = cursorNode.getLength();
+                    cursorNodeOffset = range.startOffset;
+
+                    // if cursor position is at the end of text node and next element is <br>
+                    if (cursorNodeOffset === cursorNodeLength && cursorNode.hasNext() && cursorNode.getNext().getName() === 'br') {
+                        range = editor.createRange();
+                        // move cursor position behind the next element (= before the next next ;-) )
+                        range.setStart(cursorNode.getNext().getNext(), 0);
+                        range.setEnd(cursorNode.getNext().getNext(), 0);
+                        editor.getSelection().selectRanges([range]);
+                    }
+
+                    // create a helper element to insert at cursor position
                     helper = CKEDITOR.dom.element.createFromHtml('<br />');
                     editor.insertElement(helper);
                     // break quote at the position of the helper element
