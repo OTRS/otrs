0683ac198a073040ffea731ec0920fc0e889e6e1
Marc A. Bonsels <marc.bonsels@otrs.com>
2014-12-18 14:22:29 +0100

    Fixed bug#10945 - Invalid CustomerCompanys are not marked invalid in CIC

--- Kernel/Output/HTML/DashboardCustomerCompanyInformation.pm
+++ Kernel/Output/HTML/DashboardCustomerCompanyInformation.pm
@@ -68,10 +68,15 @@ sub Run {
 
     # get layout object
     my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $ValidObject  = $Kernel::OM->Get('Kernel::System::Valid');
+    my $CompanyIsValid;
 
     # make ValidID readable
     if ( $CustomerCompany{ValidID} ) {
-        $CustomerCompany{ValidID} = $Kernel::OM->Get('Kernel::System::Valid')->ValidLookup(
+        my @ValidIDs = $ValidObject->ValidIDsGet();
+        $CompanyIsValid = grep { $CustomerCompany{ValidID} == $_ } @ValidIDs;
+
+        $CustomerCompany{ValidID} = $ValidObject->ValidLookup(
             ValidID => $CustomerCompany{ValidID},
         );
 
@@ -131,6 +136,12 @@ sub Run {
                 Value => $CustomerCompany{$Key},
             },
         );
+
+        if ( $Key eq 'CustomerCompanyName' && defined $CompanyIsValid && !$CompanyIsValid ) {
+            $LayoutObject->Block(
+                Name => 'ContentSmallCustomerCompanyInvalid',
+            );
+        }
     }
 
     my $Content = $LayoutObject->Output(
