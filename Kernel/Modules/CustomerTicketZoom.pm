28e1381fd4791ea9fecb598c9f419d6069fae41d
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2015-01-05 22:56:34 -0600

    Fixed: Dynamic Field shown information in customer interface is not consistent with agent interface (bug#10895).
    Conflicts:
    	CHANGES.md

--- Kernel/Modules/CustomerTicketZoom.pm
+++ Kernel/Modules/CustomerTicketZoom.pm
@@ -1164,7 +1164,7 @@ sub _Mask {
     for my $DynamicFieldConfig ( @{$DynamicField} ) {
         next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
 
-        # skip the dynamic field if is not desinged for customer interface
+        # skip the dynamic field if is not designed for customer interface
         my $IsCustomerInterfaceCapable = $Self->{BackendObject}->HasBehavior(
             DynamicFieldConfig => $DynamicFieldConfig,
             Behavior           => 'IsCustomerInterfaceCapable',
@@ -1176,7 +1176,7 @@ sub _Mask {
             ObjectID           => $Param{TicketID},
         );
 
-        next DYNAMICFIELD if !$Value;
+        next DYNAMICFIELD if !defined $Value;
         next DYNAMICFIELD if $Value eq "";
 
         # get print string for this dynamic field
