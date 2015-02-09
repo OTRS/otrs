84acb6479897f513fa81f357c37210c4d97b5a03
Marc A. Bonsels <marc.bonsels@otrs.com>
2015-02-09 10:06:50 +0100

    Improved wording.

--- Kernel/Modules/AdminSystemMaintenance.pm
+++ Kernel/Modules/AdminSystemMaintenance.pm
@@ -237,7 +237,7 @@ sub Run {
             # add notification
             push @NotifyData, {
                 Priority => 'Notice',
-                Info     => 'All sessions has been killed, except for the current one!',
+                Info     => 'All sessions have been killed, except for your own.',
             };
 
             # set class for expanding sessions widget
