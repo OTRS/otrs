5bb90592017cdba9b706ff0f4aba6a3d2ff84814
Marc A. Bonsels <marc.bonsels@otrs.com>
2015-05-08 11:37:30 +0200

    Moved process item to new module structure location.

--- Kernel/Output/HTML/Ticket/MenuProcess.pm
+++ Kernel/Output/HTML/Ticket/MenuProcess.pm
@@ -6,7 +6,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::TicketMenuProcess;
+package Kernel::Output::HTML::Ticket::MenuProcess;
 
 use strict;
 use warnings;
