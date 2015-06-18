e72c01582dd722fd12fc8d57e4a1f0c160a6cb90
Martin Gruner <martin.gruner@otrs.com>
2015-06-18 08:55:38 +0200

    Fixed navbar highlighting (S7).

--- Kernel/Modules/AdminCustomerCompany.pm
+++ Kernel/Modules/AdminCustomerCompany.pm
@@ -30,7 +30,7 @@ sub Run {
     my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 
     my $Nav = $ParamObject->GetParam( Param => 'Nav' ) || 0;
-    my $NavigationBarType = $Nav eq 'Agent' ? 'Companies' : 'Admin';
+    my $NavigationBarType = $Nav eq 'Agent' ? 'Customers' : 'Admin';
     my $Search = $ParamObject->GetParam( Param => 'Search' );
     $Search
         ||= $ConfigObject->Get('AdminCustomerCompany::RunInitialWildcardSearch') ? '*' : '';
