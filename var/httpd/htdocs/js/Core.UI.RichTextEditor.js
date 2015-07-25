6cd3dc11a81450d41329b6f69c9458a3b11662ae
Marc Nilius <marc.nilius@otrs.com>
2015-07-25 16:36:30 +0200

    Fixed CKEditor error message for OTRS dialogs without possibility to upload images.

--- var/httpd/htdocs/js/Core.UI.RichTextEditor.js
+++ var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -129,7 +129,7 @@ Core.UI.RichTextEditor = (function (TargetNS) {
             width: Core.Config.Get('RichText.Width', 620),
             resize_minWidth: Core.Config.Get('RichText.Width', 620),
             height: Core.Config.Get('RichText.Height', 320),
-            removePlugins: 'elementspath,scayt,menubutton',
+            removePlugins: CheckFormID().length ? 'elementspath,scayt,menubutton' : 'elementspath,scayt,menubutton,image2,uploadimage',
             forcePasteAsPlainText: false,
             format_tags: 'p;h1;h2;h3;h4;h5;h6;pre',
             fontSize_sizes: '8px;10px;12px;16px;18px;20px;22px;24px;26px;28px;30px;',
