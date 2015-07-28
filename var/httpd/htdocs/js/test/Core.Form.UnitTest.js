b8cfa506698c5f27ad00149886e9333a54929828
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2015-07-28 17:16:41 -0500

    Updated copyright.

--- var/httpd/htdocs/js/test/Core.Form.UnitTest.js
+++ var/httpd/htdocs/js/test/Core.Form.UnitTest.js
@@ -1,5 +1,5 @@
 // --
-// Copyright (C) 2001-2012 OTRS AG, http://otrs.org/\n";
+// Copyright (C) 2001-2015 OTRS AG, http://otrs.com/\n";
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
