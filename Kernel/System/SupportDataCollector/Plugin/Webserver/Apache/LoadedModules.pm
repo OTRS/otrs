87815ea3b039de61c05983b05e45f5e678dd318a
Manuel Hecht <manuel.hecht@otrs.com>
2015-07-29 09:37:14 +0200

    Tidied.

--- Kernel/System/SupportDataCollector/Plugin/Webserver/Apache/LoadedModules.pm
+++ Kernel/System/SupportDataCollector/Plugin/Webserver/Apache/LoadedModules.pm
@@ -26,10 +26,12 @@ sub Run {
     my %Environment = %ENV;
 
     # No apache webserver with mod_perl, skip this check
-    if (  !$ENV{SERVER_SOFTWARE}
+    if (
+        !$ENV{SERVER_SOFTWARE}
         || $ENV{SERVER_SOFTWARE} !~ m{apache}i
         || !$ENV{MOD_PERL}
-        || !eval { require Apache2::Module; } )
+        || !eval { require Apache2::Module; }
+        )
     {
         return $Self->GetResults();
     }
