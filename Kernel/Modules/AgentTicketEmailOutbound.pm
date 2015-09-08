fdd5b67c59a5d030c7899f6ed43c99c3cff77fd7
Martin Gruner <martin.gruner@otrs.com>
2015-09-08 09:22:41 +0200

    Remove unneeded fields.

--- Kernel/Modules/AgentTicketEmailOutbound.pm
+++ Kernel/Modules/AgentTicketEmailOutbound.pm
@@ -57,7 +57,7 @@ sub new {
 
     # get params
     for my $Param (
-        qw(From To Cc Bcc Subject Body InReplyTo References ComposeStateID ArticleTypeID
+        qw(From To Cc Bcc Subject Body ComposeStateID ArticleTypeID
         ArticleID TimeUnits Year Month Day Hour Minute FormID)
         )
     {
@@ -719,11 +719,6 @@ sub Form {
         Attachments         => \@Attachments,
         %Data,
         %GetParam,
-
-        # We start a new communication here, so don't send any references.
-        #   This might lead to information disclosure (domain names; see bug#11246).
-        InReplyTo        => '',
-        References       => '',
         DynamicFieldHTML => \%DynamicFieldHTML,
     );
     $Output .= $Self->{LayoutObject}->Footer(
@@ -1128,8 +1123,10 @@ sub SendEmail {
         Subject        => $GetParam{Subject},
         UserID         => $Self->{UserID},
         Body           => $GetParam{Body},
-        InReplyTo      => $GetParam{InReplyTo},
-        References     => $GetParam{References},
+        # We start a new communication here, so don't send any references.
+        #   This might lead to information disclosure (domain names; see bug#11246).
+        InReplyTo        => '',
+        References       => '',
         Charset        => $Self->{LayoutObject}->{UserCharset},
         MimeType       => $MimeType,
         Attachment     => \@AttachmentData,
