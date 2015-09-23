0778515ad8c541e11eb5dc5dbe29b15114b0238e
Martin Gruner <martin.gruner@otrs.com>
2015-09-23 15:15:36 +0200

    Simplified check.

--- Kernel/System/HTMLUtils.pm
+++ Kernel/System/HTMLUtils.pm
@@ -1083,7 +1083,7 @@ sub Safety {
 
                 # remove on action attributes
                 $Replaced += $Tag =~ s{
-                    (?:\s|/) on[a-zA-Z]+\s*=("[^"]+"|'[^']+'|.+?)($TagEnd|\s)
+                    (?:\s|/) on[a-z]+\s*=("[^"]+"|'[^']+'|.+?)($TagEnd|\s)
                 }
                 {$2}sgxim;
 
