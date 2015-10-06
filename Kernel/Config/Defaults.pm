7da9123cb65f45207f185946acb18df962c14bad
Marc Nilius <marc.nilius@otrs.com>
2015-10-06 12:26:38 +0200

    Updated jQuery Validate to version 1.14.0.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -944,7 +944,7 @@ sub LoadDefaults {
     $Self->{'Loader::Customer::CommonJS'}->{'000-Framework'} =  [
         'thirdparty/jquery-2.1.4/jquery.js',
         'thirdparty/jquery-browser-detection/jquery-browser-detection.js',
-        'thirdparty/jquery-validate-1.13.1/jquery.validate.js',
+        'thirdparty/jquery-validate-1.14.0/jquery.validate.js',
         'thirdparty/jquery-ui-1.11.4/jquery-ui.js',
         'thirdparty/stacktrace-0.6.4/stacktrace.js',
         'thirdparty/jquery-pubsub/pubsub.js',
@@ -980,7 +980,7 @@ sub LoadDefaults {
         'thirdparty/jquery-browser-detection/jquery-browser-detection.js',
         'thirdparty/jquery-ui-1.11.4/jquery-ui.js',
         'thirdparty/jquery-ui-touch-punch-0.2.3/jquery.ui.touch-punch.js',
-        'thirdparty/jquery-validate-1.13.1/jquery.validate.js',
+        'thirdparty/jquery-validate-1.14.0/jquery.validate.js',
         'thirdparty/stacktrace-0.6.4/stacktrace.js',
         'thirdparty/jquery-pubsub/pubsub.js',
         'thirdparty/jquery-jstree-3.2.1/jquery.jstree.js',
