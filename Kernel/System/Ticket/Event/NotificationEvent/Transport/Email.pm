e18790e1e4ada4aa6238426f256a5703387e317b
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2015-10-19 18:14:33 -0500

    Followup for bug#11566 - Once per day notification setting is not honored if sendmail backend is used under certain circumstances.

--- Kernel/System/Ticket/Event/NotificationEvent/Transport/Email.pm
+++ Kernel/System/Ticket/Event/NotificationEvent/Transport/Email.pm
@@ -180,20 +180,14 @@ sub SendNotification {
                 Message  => "'$Notification{Name}' notification could not be sent to agent '$Recipient{UserEmail} ",
             );
 
-            # avoid returning a fail value for allowing to have
-            # a ticket history entry that can be use later
-            # for debug propose, see Bug 11566
-            # this return can be re-added if root cause of not closing file handle in sendmail module
-            # if found and fixed
-            # return;
+            return;
         }
-        else {
-            # log event
-            $Kernel::OM->Get('Kernel::System::Log')->Log(
-                Priority => 'info',
-                Message  => "Sent agent '$Notification{Name}' notification to '$Recipient{UserEmail}'.",
-            );
-        }
+
+        # log event
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'info',
+            Message  => "Sent agent '$Notification{Name}' notification to '$Recipient{UserEmail}'.",
+        );
 
         # set event data
         $Self->{EventData} = {
