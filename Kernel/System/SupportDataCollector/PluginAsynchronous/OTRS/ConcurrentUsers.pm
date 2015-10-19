4d0a287861dde860caa5e916d8d9dff89dcecedd
Dominik Klein <dominik.klein@otrs.com>
2015-10-19 16:30:13 +0200

    Fixed a problem with the max values in the concurrent users plugin.

--- Kernel/System/SupportDataCollector/PluginAsynchronous/OTRS/ConcurrentUsers.pm
+++ Kernel/System/SupportDataCollector/PluginAsynchronous/OTRS/ConcurrentUsers.pm
@@ -67,6 +67,7 @@ sub Run {
         ENTRY:
         for my $Entry ( @{$ConcurrentUsers} ) {
 
+            next ENTRY if !$Entry->{$Identifier};
             next ENTRY if $Entry->{$Identifier} && $Entry->{$Identifier} <= $MaxValue;
 
             $MaxValue = $Entry->{$Identifier} || 0;
