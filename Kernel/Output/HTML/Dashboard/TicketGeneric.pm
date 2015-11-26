037b61df881e48373d91ab114d9a060b97fdb38f
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2015-11-26 16:01:21 -0600

    Fixed: Dashboard - Queue - Attributes of the Widget are not used in the  'Tickets in my Service' view (bug#11574).

--- Kernel/Output/HTML/Dashboard/TicketGeneric.pm
+++ Kernel/Output/HTML/Dashboard/TicketGeneric.pm
@@ -2133,6 +2133,7 @@ sub _SearchParamsGet {
 
     if ( defined $TicketSearch{QueueIDs} || defined $TicketSearch{Queues} ) {
         delete $TicketSearchSummary{MyQueues};
+        delete $TicketSearchSummary{MyServices}->{QueueIDs};
     }
 
     if ( !$Self->{UseTicketService} ) {
