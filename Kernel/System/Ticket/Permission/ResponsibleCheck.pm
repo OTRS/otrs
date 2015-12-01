5707f4bc3ba33a6a6739735561e8467d297a45ff
Manuel Hecht <manuel.hecht@otrs.com>
2015-12-01 15:47:00 +0100

    Improved ticket permission modules and the possibility to restrict it on queues.

--- Kernel/System/Ticket/Permission/ResponsibleCheck.pm
+++ Kernel/System/Ticket/Permission/ResponsibleCheck.pm
@@ -13,6 +13,7 @@ use warnings;
 
 our @ObjectDependencies = (
     'Kernel::Config',
+    'Kernel::System::CheckItem',
     'Kernel::System::Log',
     'Kernel::System::Ticket',
 );
@@ -35,13 +36,13 @@ sub Run {
         if ( !$Param{$_} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
-                Message  => "Need $_!"
+                Message  => "Need $_!",
             );
             return;
         }
     }
 
-    # return if no responible feature is active
+    # return if no responsible feature is active
     return if !$Kernel::OM->Get('Kernel::Config')->Get('Ticket::Responsible');
 
     # get ticket data
@@ -50,7 +51,46 @@ sub Run {
         DynamicFields => 0,
     );
 
-    # check ticket owner, return access if current user is ticket owner
+    return if !%Ticket;
+    return if !$Ticket{ResponsibleID};
+
+    # get queue config
+    my $Queues = $Kernel::OM->Get('Kernel::Config')->Get('Ticket::Permission::ResponsibleCheck::Queues');
+
+    # check queues
+    if ( $Queues && ref $Queues eq 'HASH' && %{$Queues} && $Ticket{Queue} ) {
+
+        return if !$Queues->{ $Ticket{Queue} };
+
+        # get check item object
+        my $CheckItemObject = $Kernel::OM->Get('Kernel::System::CheckItem');
+
+        # extract permission list
+        my @PermissionList = split ',', $Queues->{ $Ticket{Queue} };
+
+        my %PermissionList;
+        STRING:
+        for my $String (@PermissionList) {
+
+            next STRING if !$String;
+
+            # trim the string
+            $CheckItemObject->StringClean(
+                StringRef => \$String,
+            );
+
+            next STRING if !$String;
+
+            $PermissionList{$String} = 1;
+        }
+
+        # if a permission like 'note' is given, the 'ro' permission is required to calculate the right permission
+        $PermissionList{ro} = 1;
+
+        return if !$PermissionList{rw} && !$PermissionList{ $Param{Type} };
+    }
+
+    # check ticket responsible, return access if current user is ticket responsible
     return 1 if $Ticket{ResponsibleID} eq $Param{UserID};
 
     # return no access
