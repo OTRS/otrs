b9417cd33d64b6072a96d67a51ec25442a26d0b7
Jan Steinweg <jan.steinweg@otrs.com>
2015-12-16 18:18:17 +0100

    Fixed: Queue overview displays Queues with no avaible tickets (bug#11696).

--- Kernel/Modules/AgentTicketQueue.pm
+++ Kernel/Modules/AgentTicketQueue.pm
@@ -606,6 +606,7 @@ sub _MaskQueueView {
     }
 
     # build queue string
+    QUEUE:
     for my $QueueRef (@ListedQueues) {
         my $QueueStrg = '';
         my %Queue     = %$QueueRef;
@@ -620,6 +621,16 @@ sub _MaskQueueView {
         my $ShortQueueName = $QueueName[-1];
         $Queue{MaxAge} = $Queue{MaxAge} / 60;
         $Queue{QueueID} = 0 if ( !$Queue{QueueID} );
+
+        # skip empty Queues (or only locked tickets)
+        if (
+            $Counter{ $Queue{Queue} } < 1
+            && $Queue{Queue} ne $LayoutObject->{LanguageObject}->Translate('My Queues')
+            && $Config->{HideEmptyQueues}
+            )
+        {
+            next QUEUE;
+        }
 
         my $View   = $Kernel::OM->Get('Kernel::System::Web::Request')->GetParam( Param => 'View' )   || '';
         my $Filter = $Kernel::OM->Get('Kernel::System::Web::Request')->GetParam( Param => 'Filter' ) || 'Unlocked';
