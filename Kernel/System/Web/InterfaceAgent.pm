24b78320d84d546411f0cafb1c62b05734687651
Martin Gruner <martin.gruner@otrs.com>
2015-12-18 09:19:32 +0100

    Typo.

--- Kernel/System/Web/InterfaceAgent.pm
+++ Kernel/System/Web/InterfaceAgent.pm
@@ -162,7 +162,7 @@ sub Run {
             Lang         => $Param{Lang},
             UserLanguage => $Param{Lang},
         },
-        'Kernel::Lanugage' => {
+        'Kernel::Language' => {
             UserLanguage => $Param{Lang}
         },
     );
