9b159f1cc832225a171e9ca44528792be0d48464
Martin Gruner <martin.gruner@otrs.com>
2016-02-09 13:05:24 +0100

    Experiment with travis (WIP).

--- scripts/tools/travis/setup.sh
+++ scripts/tools/travis/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cp -i $TRAVIS_BUILD_DIR/scripts/tools/travis/Config.pm.travis.postgresql $TRAVIS_BUILD_DIR/Kernel/Config.pm
+cp -i $TRAVIS_BUILD_DIR/scripts/tools/travis/Config.pm.travis.mysql $TRAVIS_BUILD_DIR/Kernel/Config.pm
+mysql -uroot -e "CREATE DATABASE otrs";
+mysql -uroot -e "GRANT ALL PRIVILEGES ON otrs.* TO 'otrs'@'localhost' IDENTIFIED BY 'otrs'";
+mysql -uroot -e "FLUSH PRIVILEGES";
