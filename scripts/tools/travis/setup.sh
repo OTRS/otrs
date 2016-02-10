7c1d916f993166ddf92163d5c9864a53a5146d58
Martin Gruner <martin.gruner@otrs.com>
2016-02-10 11:27:25 +0100

    Add PostgreSQL support for travis.

--- scripts/tools/travis/setup.sh
+++ scripts/tools/travis/setup.sh
@@ -15,7 +15,7 @@ fi
 if [ $DB = 'postgresql' ]; then
     cp -i $TRAVIS_BUILD_DIR/scripts/tools/travis/Config.pm.travis.postgresql $TRAVIS_BUILD_DIR/Kernel/Config.pm
 
-    psql -U postgres -c "CREATE ROLE otrs NOSUPERUSER NOCREATEDB NOCREATEROLE"
+    psql -U postgres -c "CREATE ROLE otrs LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE"
     psql -U postgres -c "CREATE DATABASE otrs OWNER otrs ENCODING 'UTF8'"
     psql -U otrs otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-schema.postgresql.sql > /dev/null
     psql -U otrs otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-initial_insert.postgresql.sql > /dev/null
