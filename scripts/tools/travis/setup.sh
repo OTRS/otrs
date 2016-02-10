ff4b69668942f3f5dd6a97c162f16b58785663b6
Martin Gruner <martin.gruner@otrs.com>
2016-02-10 11:21:57 +0100

    Add PostgreSQL support for travis.

--- scripts/tools/travis/setup.sh
+++ scripts/tools/travis/setup.sh
@@ -15,12 +15,12 @@ fi
 if [ $DB = 'postgresql' ]; then
     cp -i $TRAVIS_BUILD_DIR/scripts/tools/travis/Config.pm.travis.postgresql $TRAVIS_BUILD_DIR/Kernel/Config.pm
 
-    psql -U postgres -c "CREATE DATABASE otrs ENCODING 'UTF8'"
-    psql -U postgres -c "CREATE USER otrs NOSUPERUSER NOCREATEDB NOCREATEROLE PASSWORD 'otrs'"
-    psql -U postgres otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-schema.postgresql.sql
-    psql -U postgres otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-initial_insert.postgresql.sql
-    psql -U postgres otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-schema-post.postgresql.sql
-    psql -U postgres -c "ALTER DATABASE otrs OWNER TO otrs"
+    psql -U postgres -c "CREATE ROLE otrs NOSUPERUSER NOCREATEDB NOCREATEROLE"
+    psql -U postgres -c "CREATE DATABASE otrs OWNER otrs ENCODING 'UTF8'"
+    psql -U otrs otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-schema.postgresql.sql > /dev/null
+    psql -U otrs otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-initial_insert.postgresql.sql > /dev/null
+    psql -U otrs otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-schema-post.postgresql.sql > /dev/null
+    psql -U postgres -c "ALTER ROLE otrs PASSWORD 'otrs'"
 fi
 
 mkdir -p $TRAVIS_BUILD_DIR/.ssl/certs
