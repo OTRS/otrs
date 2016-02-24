70b84460b129347f276c2e812c47d1a4ed146c0d
Carlos Garcia <carlos.garcia@otrs.com>
2016-02-23 20:37:01 -0600

    Fixed bug#11882 - Untraslatable string.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2260,8 +2260,8 @@ sub NoPermission {
             $Self->Block(
                 Name => 'PossibleNextActionRow',
                 Data => {
-                    Link        => $PossibleNextActions->{$Key},
-                    Description => $Key,
+                    Link        => $Key,
+                    Description => $PossibleNextActions->{$Key},
                 },
             );
         }
