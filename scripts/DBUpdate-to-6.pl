5ddac1b33e06583e25f62ce2dc9451cd21667014
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2016-02-29 21:34:38 -0600

    Follow-up for bug#11882 - Untraslatable string.

--- scripts/DBUpdate-to-6.pl
+++ scripts/DBUpdate-to-6.pl
@@ -88,6 +88,10 @@ Please run it as the 'otrs' user or with the help of su:
             Message => 'Drop deprecated table gi_object_lock_state',
             Command => \&_DropObjectLockState,
         },
+        {
+            Message => 'Migrate PossibleNextActions Setting',
+            Command => \&_MigratePossibleNextActions,
+        },
 
         # ...
 
@@ -222,7 +226,7 @@ Drop deprecated gi_object_lock_state table if empty.
 sub _DropObjectLockState {
 
     # get needed objects
-    my $DBObject     = $Kernel::OM->Get('Kernel::System::DB');
+    my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
 
     my %Tables = map { lc($_) => 1 } $DBObject->ListTables();
     return 1 if !$Tables{gi_object_lock_state};
@@ -278,6 +282,44 @@ sub _DropObjectLockState {
     }
 
     return 1;
+}
+
+sub _MigratePossibleNextActions {
+    my $PossibleNextActions = $Kernel::OM->Get('Kernel::Config')->Get('PossibleNextActions') || {};
+
+    # create a lookup array to no not modify the looping variable
+    my @Actions = sort keys $PossibleNextActions;
+
+    my $Updated;
+    ACTION:
+    for my $Action (@Actions) {
+
+        # skip all keys that looks like an URL (e.g. TT Env('CGIHandle')
+        #   or staring with http(s), ftp(s), matilto:, ot www.)
+        next ACTION if $Action =~ m{\A \[% [\s]+ Env\( }msxi;
+        next ACTION if $Action =~ m{\A http [s]? :// }msxi;
+        next ACTION if $Action =~ m{\A ftp [s]? :// }msxi;
+        next ACTION if $Action =~ m{\A mailto : }msxi;
+        next ACTION if $Action =~ m{\A www \. }msxi;
+
+        # remember the value for the current key
+        my $ActionValue = $PossibleNextActions->{$Action};
+
+        # remove current key and add the inverted key value pair
+        delete $PossibleNextActions->{$Action};
+        $PossibleNextActions->{$ActionValue} = $Action;
+
+        $Updated = 1;
+    }
+
+    return 1 if !$Updated;
+
+    my $Success = $Kernel::OM->Get('Kernel::System::SysConfig')->ConfigItemUpdate(
+        Valid => 1,
+        Key   => 'PossibleNextActions',
+        Value => $PossibleNextActions,
+    );
+    return $Success;
 }
 
 1;
