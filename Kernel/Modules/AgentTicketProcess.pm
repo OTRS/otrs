09520a259b2dcd92d79b605df92a42c1fa5d4cb2
Martin Gruner <martin.gruner@otrs.com>
2016-03-01 14:20:54 +0100

    Added missing use statements.

--- Kernel/Modules/AgentTicketProcess.pm
+++ Kernel/Modules/AgentTicketProcess.pm
@@ -12,6 +12,7 @@ package Kernel::Modules::AgentTicketProcess;
 use strict;
 use warnings;
 
+use Kernel::Language qw(Translatable);
 use Kernel::System::VariableCheck qw(:all);
 
 our $ObjectManagerDisabled = 1;
