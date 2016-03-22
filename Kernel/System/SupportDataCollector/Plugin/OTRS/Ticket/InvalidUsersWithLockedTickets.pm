8ed04691975e731b2114ac31ad7afe47e01e7539
Martin Gruner <martin.gruner@otrs.com>
2016-03-22 17:55:53 +0100

    Follow-up for bug#11956: use more appropriate name.

--- Kernel/System/SupportDataCollector/Plugin/OTRS/Ticket/InvalidUsersWithLockedTickets.pm
+++ Kernel/System/SupportDataCollector/Plugin/OTRS/Ticket/InvalidUsersWithLockedTickets.pm
@@ -6,7 +6,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::System::SupportDataCollector::Plugin::OTRS::Ticket::LockedTicketsOfInvalidUsers;
+package Kernel::System::SupportDataCollector::Plugin::OTRS::Ticket::InvalidUsersWithLockedTickets;
 
 use strict;
 use warnings;
