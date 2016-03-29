c2a4b28eca6215aa6d136842bbc8a1b1301af806
Martin Gruner <martin.gruner@otrs.com>
2016-03-29 10:34:21 +0200

    Fixed error message, thanks to Pawel Boguslawski.

--- Kernel/System/HTMLUtils.pm
+++ Kernel/System/HTMLUtils.pm
@@ -1184,7 +1184,7 @@ sub EmbeddedImagesExtract {
     if ( ref $Param{AttachmentsRef} ne 'ARRAY' ) {
         $Kernel::OM->Get('Kernel::System::Log')->Log(
             Priority => 'error',
-            Message  => "Need DocumentRef!"
+            Message  => "Need AttachmentsRef!"
         );
         return;
     }
