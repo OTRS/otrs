9c9e59f0b9810bac6318110aa40224e709f4d1bd
Martin Gruner <martin.gruner@otrs.com>
2016-04-29 10:40:58 +0200

    Fixed: Wrong default article type in reply (bug#12039).

--- Kernel/Modules/AgentTicketCompose.pm
+++ Kernel/Modules/AgentTicketCompose.pm
@@ -1614,6 +1614,7 @@ sub Run {
             References       => "$References",
             TicketBackType   => $TicketBackType,
             DynamicFieldHTML => \%DynamicFieldHTML,
+            ArticleTypeID    => $GetParam{ArticleTypeID},    # don't use the ArticleID from the previous article
         );
         $Output .= $LayoutObject->Footer(
             Type => 'Small',
