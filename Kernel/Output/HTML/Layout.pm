03a3634992c6f1ec062a6beccfb295c596d59709
Marc A. Bonsels <marc.bonsels@otrs.com>
2016-05-04 14:00:25 +0200

    Fixed: SubSelected class not applied to My Tickets in CustomerTicketOverview Navigation (bug#12031).

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -3697,8 +3697,7 @@ sub CustomerNavigationBar {
             # check if we must mark the parent element as selected
             if ( $ItemSub->{Link} ) {
                 if (
-                    !$SelectedFlag
-                    && $ItemSub->{Link} =~ /Action=$Self->{Action}/
+                    $ItemSub->{Link} =~ /Action=$Self->{Action}/
                     && $ItemSub->{Link} =~ /$Self->{Subaction}/    # Subaction can be empty
                     )
                 {
