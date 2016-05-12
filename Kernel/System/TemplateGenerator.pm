2461f609b1af7fb479c1a06a6235d0ade892ce42
Martin Gruner <martin.gruner@otrs.com>
2016-05-12 15:07:06 +0200

    Fixed: From field with dot is filled with spaces (bug#12062).

--- Kernel/System/TemplateGenerator.pm
+++ Kernel/System/TemplateGenerator.pm
@@ -341,7 +341,7 @@ sub Sender {
     }
 
     # prepare realname quote
-    if ( $Address{RealName} =~ /(,|@|\(|\)|:)/ && $Address{RealName} !~ /^("|')/ ) {
+    if ( $Address{RealName} =~ /([.]|,|@|\(|\)|:)/ && $Address{RealName} !~ /^("|')/ ) {
         $Address{RealName} =~ s/"//g;    # remove any quotes that are already present
         $Address{RealName} = '"' . $Address{RealName} . '"';
     }
