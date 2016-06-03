7ad893ef9a157e7d34a1bee0665067414292115a
Martin Gruner <martin.gruner@otrs.com>
2016-06-03 14:36:26 +0200

    Fixed: CIC search links don't work when customer login contains parentheses (bug#12085).

--- Kernel/Output/HTML/Dashboard/CustomerUserList.pm
+++ Kernel/Output/HTML/Dashboard/CustomerUserList.pm
@@ -302,12 +302,15 @@ sub Run {
             CacheTTL             => $Self->{Config}->{CacheTTLLocal} * 60,
         );
 
+        my $CustomerKeySQL = $Kernel::OM->Get('Kernel::System::DB')->QueryStringEscape( QueryString => $CustomerKey );
+
         $LayoutObject->Block(
             Name => 'ContentLargeCustomerUserListRowCustomerUserTicketsOpen',
             Data => {
                 %Param,
-                Count       => $TicketCountOpen,
-                CustomerKey => $CustomerKey,
+                Count          => $TicketCountOpen,
+                CustomerKey    => $CustomerKey,
+                CustomerKeySQL => $CustomerKeySQL,
             },
         );
 
@@ -324,8 +327,9 @@ sub Run {
             Name => 'ContentLargeCustomerUserListRowCustomerUserTicketsClosed',
             Data => {
                 %Param,
-                Count       => $TicketCountClosed,
-                CustomerKey => $CustomerKey,
+                Count          => $TicketCountClosed,
+                CustomerKey    => $CustomerKey,
+                CustomerKeySQL => $CustomerKeySQL,
             },
         );
 
