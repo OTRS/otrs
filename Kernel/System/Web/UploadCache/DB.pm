8550a10b960c35d7a1ec59a5be5190c8a9e885cc
Martin Gruner <martin.gruner@otrs.com>
2016-06-03 13:31:04 +0200

    Fixed Internal Server Error on attachment removal page reload, thanks to Pawel Boguslawski.

--- Kernel/System/Web/UploadCache/DB.pm
+++ Kernel/System/Web/UploadCache/DB.pm
@@ -132,6 +132,10 @@ sub FormIDRemoveFile {
     }
 
     my @Index = @{ $Self->FormIDGetAllFilesMeta(%Param) };
+
+    # finish if files have been already removed by other process
+    return if !@Index;
+
     my $ID    = $Param{FileID} - 1;
     $Param{Filename} = $Index[$ID]->{Filename};
 
