97f5c6992a26389be7602dc6a51d135fe7cf0e5c
Marc A. Bonsels <marc.bonsels@otrs.com>
2016-06-08 13:07:16 +0200

    JS refactoring.

---
+++ var/httpd/htdocs/js/Core.Agent.Admin.QueueTemplates.js
@@ -0,0 +1,49 @@
+// --
+// Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core = Core || {};
+Core.Agent = Core.Agent || {};
+Core.Agent.Admin = Core.Agent.Admin || {};
+
+/**
+ * @namespace Core.Agent.Admin.QueueTemplates
+ * @memberof Core.Agent.Admin
+ * @author OTRS AG
+ * @description
+ *      This namespace contains the special module function for QueueTemplates selection.
+ */
+ Core.Agent.Admin.QueueTemplates = (function (TargetNS) {
+
+    /*
+    * @name Init
+    * @memberof Core.Agent.Admin.QueueTemplates
+    * @function
+    * @description
+    *      This function initializes filter and "SelectAll" actions.
+    */
+    TargetNS.Init = function () {
+
+        // initialize "SelectAll" checkbox and bind click event on "SelectAll" for each relation item
+        Core.Form.InitSelectAllCheckboxes($('table td input[type="checkbox"][name=ItemsSelected]'), $('#SelectAllItemsSelected'));
+
+        $('input[type="checkbox"][name=ItemsSelected]').click(function () {
+            Core.Form.SelectAllCheckboxes($(this), $('#SelectAllItemsSelected'));
+        });
+
+        // initialize table filters
+        Core.UI.Table.InitTableFilter($("#Filter"), $("#ItemsTable"));
+        Core.UI.Table.InitTableFilter($("#FilterTemplates"), $("#Templates"));
+        Core.UI.Table.InitTableFilter($("#FilterQueues"), $("#Queues"));
+    };
+
+    Core.Init.RegisterNamespace(TargetNS, 'APP_MODULE');
+
+    return TargetNS;
+}(Core.Agent.Admin.QueueTemplates || {}));
