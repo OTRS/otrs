19e79c7e7f05c13c2d57b01da28edecb67e74295
Marc A. Bonsels <marc.bonsels@otrs.com>
2016-06-09 15:06:07 +0200

    Fixed: Tooltip error messages in combination with modern input fields (bug#12116).

--- var/httpd/htdocs/js/Core.UI.InputFields.js
+++ var/httpd/htdocs/js/Core.UI.InputFields.js
@@ -302,14 +302,14 @@ Core.UI.InputFields = (function (TargetNS) {
         {
 
             // Disable the field, add the tooltip and dash string
-            $SearchObj.attr('disabled', 'disabled')
+            $SearchObj.data('disabled', 'disabled')
                 .attr('title', Core.Config.Get('InputFieldsNotAvailable'))
                 .val(Config.SelectionNotAvailable);
         }
         else {
 
             // Enable the field, remove the tooltip and dash string
-            $SearchObj.removeAttr('disabled')
+            $SearchObj.data('disabled', '')
                 .removeAttr('title')
                 .val('');
         }
@@ -376,6 +376,9 @@ Core.UI.InputFields = (function (TargetNS) {
 
         // Give up if field is expanded
         if ($SearchObj.attr('aria-expanded')) return;
+
+        // Give up is field is disabled
+        if ($SearchObj.data('disabled')) return;
 
         // Remove any existing boxes in supplied container
         $InputContainerObj.find('.InputField_Selection').remove();
@@ -592,7 +595,7 @@ Core.UI.InputFields = (function (TargetNS) {
         }
 
         // Clear search field
-        if ($SearchObj.val() !== Config.SelectionNotAvailable && !$SearchObj.attr('disabled')) {
+        if ($SearchObj.val() !== Config.SelectionNotAvailable && !$SearchObj.data('disabled')) {
             $SearchObj.val('');
         }
 
@@ -1181,9 +1184,11 @@ Core.UI.InputFields = (function (TargetNS) {
                         PossibleNone,
                         $ClearAllObj,
                         $SelectAllObj,
-                        $ConfirmObj;
+                        $ConfirmObj,
+                        ErrorTooltipPosition = 'TongueTop';
 
                     function CalculateListPosition() {
+
                         // calculate available height to bottom of page
                         AvailableHeightBottom = parseInt(
                             $(window).scrollTop() + $(window).height()
@@ -1207,6 +1212,12 @@ Core.UI.InputFields = (function (TargetNS) {
                         // decide wether list should be positioned on top or at the bottom of the input field
                         if (AvailableHeightTop > AvailableHeightBottom) {
                             AvailableMaxHeight = AvailableHeightTop;
+
+                            // add a class to the searchobj itself to be able to react on it properly
+                            // e.g. to show the error tooltip
+                            $SearchObj.removeClass('ExpandToBottom')
+                                .addClass('ExpandToTop');
+
                             $ListContainerObj
                                 .removeClass('ExpandToBottom')
                                 .addClass('ExpandToTop')
@@ -1217,6 +1228,10 @@ Core.UI.InputFields = (function (TargetNS) {
                         }
                         else {
                             AvailableMaxHeight = AvailableHeightBottom;
+
+                            $SearchObj.removeClass('ExpandToTop')
+                                .addClass('ExpandToBottom');
+
                             $ListContainerObj
                                 .removeClass('ExpandToTop')
                                 .addClass('ExpandToBottom')
@@ -1234,19 +1249,32 @@ Core.UI.InputFields = (function (TargetNS) {
                         }
                     });
 
-                    // Show error tooltip if needed
-                    if ($SelectObj.attr('id')) {
-                        if ($SelectObj.hasClass(Config.ErrorClass)) {
-                            Core.Form.ErrorTooltips.ShowTooltip(
-                                $SearchObj, $('#' + Core.App.EscapeSelector($SelectObj.attr('id')) + Config.ErrorClass).html(), 'TongueTop'
-                            );
-                        }
-                        if ($SelectObj.hasClass(Config.ServerErrorClass)) {
-                            Core.Form.ErrorTooltips.ShowTooltip(
-                                $SearchObj, $('#' + Core.App.EscapeSelector($SelectObj.attr('id')) + Config.ServerErrorClass).html(), 'TongueTop'
-                            );
+                    function ShowErrorToolTip() {
+
+                        // Show error tooltip if needed
+                        if ($SelectObj.attr('id')) {
+
+                            if ($SearchObj.hasClass('ExpandToTop')) {
+                                ErrorTooltipPosition = 'TongueBottom';
+                            }
+                            else {
+                                ErrorTooltipPosition = 'TongueTop';
+                            }
+
+                            if ($SelectObj.hasClass(Config.ErrorClass)) {
+                                Core.Form.ErrorTooltips.ShowTooltip(
+                                    $SearchObj, $('#' + Core.App.EscapeSelector($SelectObj.attr('id')) + Config.ErrorClass).html(), ErrorTooltipPosition
+                                );
+                            }
+                            if ($SelectObj.hasClass(Config.ServerErrorClass)) {
+                                Core.Form.ErrorTooltips.ShowTooltip(
+                                    $SearchObj, $('#' + Core.App.EscapeSelector($SelectObj.attr('id')) + Config.ServerErrorClass).html(), ErrorTooltipPosition
+                                );
+                            }
                         }
                     }
+
+                    ShowErrorToolTip();
 
                     // Focus tracking
                     Focused = this;
@@ -1254,6 +1282,11 @@ Core.UI.InputFields = (function (TargetNS) {
 
                     // Do nothing if already expanded
                     if ($SearchObj.attr('aria-expanded')) {
+                        return false;
+                    }
+
+                    // Do nothing if disabled
+                    if ($SearchObj.data('disabled')) {
                         return false;
                     }
 
@@ -1286,6 +1319,7 @@ Core.UI.InputFields = (function (TargetNS) {
                         }
 
                         CalculateListPosition();
+                        ShowErrorToolTip();
 
                         // This checks, if an inner element is scrolled (e.g. dialog)
                         // we only need to hide the list in this case, because scrolling the main window
