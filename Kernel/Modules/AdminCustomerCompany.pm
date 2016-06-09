dc70cf8dae020c1ac17737fefbb9ab8dd2ad820e
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2016-06-09 15:56:16 -0500

    Fixed: Can not store 0 in Admin Customer screen (bug#12128).

--- Kernel/Modules/AdminCustomerCompany.pm
+++ Kernel/Modules/AdminCustomerCompany.pm
@@ -85,7 +85,7 @@ sub Run {
         $GetParam{CustomerCompanyID} = $Self->{ParamObject}->GetParam( Param => 'CustomerCompanyID' );
 
         for my $Entry ( @{ $Self->{ConfigObject}->Get('CustomerCompany')->{Map} } ) {
-            $GetParam{ $Entry->[0] } = $Self->{ParamObject}->GetParam( Param => $Entry->[0] ) || '';
+            $GetParam{ $Entry->[0] } = $Self->{ParamObject}->GetParam( Param => $Entry->[0] ) // '';
 
             # check mandatory fields
             if ( !$GetParam{ $Entry->[0] } && $Entry->[4] ) {
@@ -208,7 +208,7 @@ sub Run {
         my $CustomerCompanyID;
 
         for my $Entry ( @{ $Self->{ConfigObject}->Get('CustomerCompany')->{Map} } ) {
-            $GetParam{ $Entry->[0] } = $Self->{ParamObject}->GetParam( Param => $Entry->[0] ) || '';
+            $GetParam{ $Entry->[0] } = $Self->{ParamObject}->GetParam( Param => $Entry->[0] ) // '';
 
             # check mandatory fields
             if ( !$GetParam{ $Entry->[0] } && $Entry->[4] ) {
