d1138e92655ee1fa120f10573b7e1d4b60614714
Marc A. Bonsels <marc.bonsels@otrs.com>
2016-06-14 18:13:25 +0200

    JS refactoring.

---
+++ var/httpd/htdocs/js/Core.Agent.Admin.QueueAutoResponse.js
@@ -0,0 +1,39 @@
+// --
+// Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core = Core || {};
+Core.Agent = Core.Agent || {};
+Core.Agent.Admin = Core.Agent.Admin || {};
+
+/**
+ * @namespace Core.Agent.Admin.QueueAutoResponse
+ * @memberof Core.Agent.Admin
+ * @author OTRS AG
+ * @description
+ *      This namespace contains the special function for AdminQueueAutoResponse module.
+ */
+ Core.Agent.Admin.QueueAutoResponse = (function (TargetNS) {
+
+    /*
+    * @name Init
+    * @memberof Core.Agent.Admin.QueueAutoResponse
+    * @function
+    * @description
+    *      This function initializes filters.
+    */
+    TargetNS.Init = function () {
+        Core.UI.Table.InitTableFilter($('#FilterQueues'), $('#Queues'));
+        Core.UI.Table.InitTableFilter($('#FilterAutoResponses'), $('#AutoResponses'));
+    };
+
+    Core.Init.RegisterNamespace(TargetNS, 'APP_MODULE');
+
+    return TargetNS;
+}(Core.Agent.Admin.QueueAutoResponse || {}));
