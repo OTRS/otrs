ea46e39c10ffd693aee4b62da6f38fe266a54ce7
Marc Nilius <marc.nilius@otrs.com>
2016-06-15 16:14:43 +0200

    Refactored JavaScript for AdminQueue.

---
+++ var/httpd/htdocs/js/Core.Agent.Admin.Queue.js
@@ -0,0 +1,38 @@
+// --
+// Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core = Core || {};
+Core.Agent = Core.Agent || {};
+Core.Agent.Admin = Core.Agent.Admin || {};
+
+/**
+ * @namespace Core.Agent.Admin.Queue
+ * @memberof Core.Agent.Admin
+ * @author OTRS AG
+ * @description
+ *      This namespace contains the special module function for AdminQueue.
+ */
+ Core.Agent.Admin.Queue = (function (TargetNS) {
+
+    /*
+    * @name Init
+    * @memberof Core.Agent.Admin.Queue
+    * @function
+    * @description
+    *      This function initializes filter.
+    */
+    TargetNS.Init = function () {
+        Core.UI.Table.InitTableFilter($("#FilterQueues"), $("#Queues"));
+    };
+
+    Core.Init.RegisterNamespace(TargetNS, 'APP_MODULE');
+
+    return TargetNS;
+}(Core.Agent.Admin.Queue || {}));
