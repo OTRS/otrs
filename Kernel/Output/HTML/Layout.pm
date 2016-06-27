457ed066d1132e6b2560c1818fec84eeead1d955
Marc A. Bonsels <marc.bonsels@otrs.com>
2016-06-27 18:51:17 +0200

    Fixed: Wrong class paramenter for Admin Menu in Navbar (bug#11986).

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2884,8 +2884,10 @@ sub NavigationBar {
             },
         );
 
-        # show sub menu
+        # show sub menu (only if sub elements available)
         next ITEM if !$Sub;
+        next ITEM if !keys %{$Sub};
+
         $Self->Block(
             Name => 'ItemAreaSub',
             Data => $Item,
