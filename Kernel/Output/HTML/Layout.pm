5d0ca406a508c24f0fc277f2bfa4a7a35a62970c
Marc A. Bonsels <marc.bonsels@otrs.com>
2016-07-05 14:39:17 +0200

    Fixed error screen layout.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -3745,7 +3745,7 @@ sub CustomerFatalError {
         Area  => 'Frontend',
         Title => 'Fatal Error'
     );
-    $Output .= $Self->Error(%Param);
+    $Output .= $Self->CustomerError(%Param);
     $Output .= $Self->CustomerFooter();
     $Self->Print( Output => \$Output );
     exit;
