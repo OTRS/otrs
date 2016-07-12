3d684f112924ab2bfff339ebee417998d48dcc6e
Dominik Klein <dominik.klein@otrs.com>
2016-07-12 11:41:05 +0200

    Fixed: Object StateAction -> Names are not readable (bug#12181).

--- Kernel/System/Stats/Static/StateAction.pm
+++ Kernel/System/Stats/Static/StateAction.pm
@@ -91,7 +91,6 @@ sub Run {
     my @PossibleStates;
     for my $StateID ( sort { $States{$a} cmp $States{$b} } keys %States ) {
         $States{$StateID} = $LanguageObject->Translate( $States{$StateID} );
-        $States{$StateID} =~ s/^(.{18}).*$/$1\.\.\./;
         push @PossibleStates, $States{$StateID};
     }
 
