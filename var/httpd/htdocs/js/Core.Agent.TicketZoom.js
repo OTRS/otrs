5c1562a025e419f1366f553473cb9bfebb73bf21
Martin Gruner <martin.gruner@otrs.com>
2016-07-14 08:20:00 +0200

    Marked string as translatable, thanks to Ur Balazs.

--- var/httpd/htdocs/js/Core.Agent.TicketZoom.js
+++ var/httpd/htdocs/js/Core.Agent.TicketZoom.js
@@ -344,7 +344,7 @@ Core.Agent.TicketZoom = (function (TargetNS) {
             var Position;
             if ($(this).attr('rel') && $('#' + $(this).attr('rel')).length) {
                 Position = $(this).offset();
-                Core.UI.Dialog.ShowContentDialog($('#' + $(this).attr('rel'))[0].innerHTML, 'Attachments', Position.top - $(window).scrollTop(), parseInt(Position.left, 10) + 25);
+                Core.UI.Dialog.ShowContentDialog($('#' + $(this).attr('rel'))[0].innerHTML, Core.Language.Translate('Attachments'), Position.top - $(window).scrollTop(), parseInt(Position.left, 10) + 25);
             }
             Event.preventDefault();
             Event.stopPropagation();
