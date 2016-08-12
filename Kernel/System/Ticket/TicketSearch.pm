ea2289271be3813e296a4ab9bda6d7507be97299
Dusan Vuckovic <dusan.vuckovic@otrs.com>
2016-08-12 15:56:16 +0200

    Follow-up fix for bug#4439.

--- Kernel/System/Ticket/TicketSearch.pm
+++ Kernel/System/Ticket/TicketSearch.pm
@@ -2160,7 +2160,7 @@ sub TicketSearch {
                 # include first and last name in select
                 $SQLSelect
                     .= ', ' . $SortOptions{ $SortByArray[$Count] }
-                    . ", CONCAT(u.first_name, ' ', u.last_name) ";
+                    . ", u.first_name, u.last_name ";
 
                 # join the users table on user's id
                 $SQLFrom
@@ -2168,7 +2168,8 @@ sub TicketSearch {
                     . ' ON ' . $SortOptions{ $SortByArray[$Count] } . ' = u.id ';
 
                 # sort by first and last name
-                $SQLExt .= " CONCAT(u.first_name, ' ', u.last_name) ";
+                my $OrderBySuffix = $OrderByArray[$Count] eq 'Up' ? 'ASC' : 'DESC';
+                $SQLExt .= " u.first_name $OrderBySuffix, u.last_name ";
             }
             else {
 
