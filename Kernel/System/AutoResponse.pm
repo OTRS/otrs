ad432645fb4a61e871b712fb0ebe7c280bc58737
Martin Gruner <martin.gruner@otrs.com>
2016-08-15 12:14:17 +0200

    Fixed POD.

--- Kernel/System/AutoResponse.pm
+++ Kernel/System/AutoResponse.pm
@@ -38,7 +38,7 @@ create an object
 
     use Kernel::System::ObjectManager;
     local $Kernel::OM = Kernel::System::ObjectManager->new();
-    my $AutoResponseObject = $Kernel::OM->Get('Kernel::System::AutoReponse');
+    my $AutoResponseObject = $Kernel::OM->Get('Kernel::System::AutoResponse');
 
 =cut
 
