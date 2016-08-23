42f5a3729ea96f27d0f02ae7576fcab7b1c6d8f0
Manuel Hecht <manuel.hecht@otrs.com>
2016-08-23 14:37:52 +0200

    Code cleanup in Ticket.pm.

--- Kernel/System/Ticket.pm
+++ Kernel/System/Ticket.pm
@@ -2681,7 +2681,7 @@ sub TicketEscalationIndexBuild {
         if ( !defined $Param{$Needed} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
-                Message  => "Need $Needed!"
+                Message  => "Need $Needed!",
             );
             return;
         }
@@ -2697,7 +2697,7 @@ sub TicketEscalationIndexBuild {
     my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
 
     # do no escalations on (merge|close|remove) tickets
-    if ( $Ticket{StateType} =~ /^(merge|close|remove)/i ) {
+    if ( $Ticket{StateType} && $Ticket{StateType} =~ /^(merge|close|remove)/i ) {
 
         # update escalation times with 0
         my %EscalationTimes = (
@@ -2729,10 +2729,13 @@ sub TicketEscalationIndexBuild {
     }
 
     # get escalation properties
-    my %Escalation = $Self->TicketEscalationPreferences(
-        Ticket => \%Ticket,
-        UserID => $Param{UserID},
-    );
+    my %Escalation;
+    if (%Ticket) {
+        %Escalation = $Self->TicketEscalationPreferences(
+            Ticket => \%Ticket,
+            UserID => $Param{UserID},
+        );
+    }
 
     # find escalation times
     my $EscalationTime = 0;
