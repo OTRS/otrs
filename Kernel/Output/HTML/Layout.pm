f7b2bfd244222a42849492b0287d030faf3573c4
Martin Gruner <martin.gruner@otrs.com>
2016-08-26 09:33:55 +0200

    Fixed: Time-related search attributes only cover last 10 years (bug#12263).

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -3189,7 +3189,7 @@ sub BuildDateSelection {
             }
         }
         else {
-            for ( $Y - 10 .. $Y + 1 + ( $Param{YearDiff} || 0 ) ) {
+            for ( 2001 .. $Y + 1 + ( $Param{YearDiff} || 0 ) ) {
                 $Year{$_} = $_;
             }
         }
