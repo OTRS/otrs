397d8ebf56721555c28c4664fe6aa7c934f25c8c
Manuel Hecht <manuel.hecht@otrs.com>
2016-08-26 13:40:22 +0200

    Improved GetRandomNumber() in Helper.pm.

--- Kernel/System/UnitTest/Helper.pm
+++ Kernel/System/UnitTest/Helper.pm
@@ -143,7 +143,8 @@ my %GetRandomNumberPrevious;
 
 sub GetRandomNumber {
 
-    my $Prefix = $$ . substr time(), -5, 5;
+    my $PID = sprintf '%.6d', $$;
+    my $Prefix = $PID . substr time(), -5, 5;
 
     return $Prefix . $GetRandomNumberPrevious{$Prefix}++ || 0;
 }
