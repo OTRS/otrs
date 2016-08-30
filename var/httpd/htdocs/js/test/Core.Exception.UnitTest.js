0846ac9e8d5fffbdb507d39c4af732ebd7128039
Marc Nilius <marc.nilius@otrs.com>
2016-08-30 10:53:26 +0200

    Added JavaScript unit test for Core.Exception.

---
+++ var/httpd/htdocs/js/test/Core.Exception.UnitTest.js
@@ -0,0 +1,51 @@
+// --
+// Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core = Core || {};
+
+Core.Exception = (function (Namespace) {
+    Namespace.RunUnitTests = function(){
+        QUnit.module('Core.Exception');
+
+        // ApplicationError
+        QUnit.test('Core.Exception.ApplicationError()', function(Assert){
+            var ErrorMessage = 'Test error message',
+                ErrorTypes = ['Error', 'InternalError', 'TypeError', 'CommunicationError'],
+                ExceptionObject;
+
+            Assert.expect(12);
+
+            Core.Exception.HandleFinalError = function (Exception, ErrorType) {
+                Assert.equal(Exception.GetType(), ErrorType, "Handle error type - " + ErrorType + ', error message - ' + ErrorMessage);
+            };
+
+            $.each(ErrorTypes, function() {
+                try {
+                    ExceptionObject = new Core.Exception.ApplicationError(ErrorMessage, this);
+                    Core.Exception.HandleFinalError(ExceptionObject, this);
+
+                    // test IsErrorOfType
+                    Assert.equal(Core.Exception.IsErrorOfType(ExceptionObject,this), true, 'Exception type is - ' +  this);
+
+                    // test Thrown function
+                    Core.Exception.Thrown(ErrorMessage, this);
+                }
+                catch (Error) {
+
+                    // check if the exception is caught
+                    Assert.ok(true, 'Error caught, error type \'' + ExceptionObject.GetType() + '\' is not correct!');
+                }
+            });
+
+        });
+    };
+
+    return Namespace;
+}(Core.Exception || {}));
