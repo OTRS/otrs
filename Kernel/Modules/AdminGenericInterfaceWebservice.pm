a958071d1e070094189cddbf794c752a94ad1010
Jaroslav Balaz <jaroslav.balaz@otrs.com>
2016-09-21 14:57:46 +0000

    Disable check for Business solution during import example web service.

--- Kernel/Modules/AdminGenericInterfaceWebservice.pm
+++ Kernel/Modules/AdminGenericInterfaceWebservice.pm
@@ -899,7 +899,7 @@ sub _ShowEdit {
 
         my %Frontend;
 
-        if ( %ExampleWebServicesData && $Kernel::OM->Get('Kernel::System::OTRSBusiness')->OTRSBusinessIsInstalled() ) {
+        if (%ExampleWebServicesData) {
             $Frontend{ExampleWebServiceList} = $Kernel::OM->Get('Kernel::Output::HTML::Layout')->BuildSelection(
                 Name         => 'ExampleWebService',
                 Data         => \%ExampleWebServicesData,
