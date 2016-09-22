cbfb65eae56f8ae893e1ed60a7fbb04d0cfd6650
Dominik Klein <dominik.klein@otrs.com>
2016-09-22 15:55:56 +0200

    Added check for the package verification and framework version to the package deployment support data plugin.

--- Kernel/System/SupportDataCollector/Plugin/OTRS/PackageDeployment.pm
+++ Kernel/System/SupportDataCollector/Plugin/OTRS/PackageDeployment.pm
@@ -31,13 +31,45 @@ sub Run {
     my $PackageObject = $Kernel::OM->Get('Kernel::System::Package');
 
     my @InvalidPackages;
+    my @NotVerifiedPackages;
+    my @WrongFrameworkVersion;
     for my $Package ( $PackageObject->RepositoryList() ) {
+
         my $DeployCheck = $PackageObject->DeployCheck(
             Name    => $Package->{Name}->{Content},
             Version => $Package->{Version}->{Content},
         );
         if ( !$DeployCheck ) {
             push @InvalidPackages, "$Package->{Name}->{Content} $Package->{Version}->{Content}";
+        }
+
+        # get package
+        my $PackageContent = $PackageObject->RepositoryGet(
+            Name    => $Package->{Name}->{Content},
+            Version => $Package->{Version}->{Content},
+            Result  => 'SCALAR',
+        );
+
+        my $Verified = $PackageObject->PackageVerify(
+            Package => $PackageContent,
+            Name    => $Package->{Name}->{Content},
+        ) || 'unknown';
+
+        if ( $Verified ne 'verified' ) {
+            push @NotVerifiedPackages, "$Package->{Name}->{Content} $Package->{Version}->{Content}";
+        }
+
+        my %PackageStructure = $PackageObject->PackageParse(
+            String => $PackageContent,
+        );
+
+        my $CheckFrameworkOk = $PackageObject->_CheckFramework(
+            Framework => $PackageStructure{Framework},
+            NoLog     => 1,
+        );
+
+        if (!$CheckFrameworkOk) {
+            push @WrongFrameworkVersion, "$Package->{Name}->{Content} $Package->{Version}->{Content}";
         }
     }
 
@@ -60,6 +92,52 @@ sub Run {
     else {
         $Self->AddResultOk(
             Label => Translatable('Package Installation Status'),
+            Value => '',
+        );
+    }
+
+    if (@NotVerifiedPackages) {
+        if ( $Kernel::OM->Get('Kernel::Config')->Get('Package::AllowLocalModifications') ) {
+            $Self->AddResultInformation(
+                Label   => Translatable('Package Verification Status'),
+                Value   => join( ', ', @NotVerifiedPackages ),
+                Message => Translatable('Some packages are not verified by the OTRS Group! It is recommended not to use this packages.'),
+            );
+        }
+        else {
+            $Self->AddResultProblem(
+                Label   => Translatable('Package Verification Status'),
+                Value   => join( ', ', @NotVerifiedPackages ),
+                Message => Translatable('Some packages are not verified by the OTRS Group! It is recommended not to use this packages.'),
+            );
+        }
+    }
+    else {
+        $Self->AddResultOk(
+            Label => Translatable('Package Verification Status'),
+            Value => '',
+        );
+    }
+
+    if (@WrongFrameworkVersion) {
+        if ( $Kernel::OM->Get('Kernel::Config')->Get('Package::AllowLocalModifications') ) {
+            $Self->AddResultInformation(
+                Label   => Translatable('Package Framework Version Status'),
+                Value   => join( ', ', @WrongFrameworkVersion ),
+                Message => Translatable('Some packages are not allowed for the current framework version.'),
+            );
+        }
+        else {
+            $Self->AddResultProblem(
+                Label   => Translatable('Package Framework Version Status'),
+                Value   => join( ', ', @WrongFrameworkVersion ),
+                Message => Translatable('Some packages are not allowed for the current framework version.'),
+            );
+        }
+    }
+    else {
+        $Self->AddResultOk(
+            Label => Translatable('Package Framework Version Status'),
             Value => '',
         );
     }
