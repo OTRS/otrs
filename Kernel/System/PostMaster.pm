9ae606d3717fef460f89775368609d19db1afa22
Jaroslav Balaz <jaroslav.balaz@otrs.com>
2016-09-30 10:37:10 +0000

    Fixed: Tickets in status "removed" get reopened from Junk Queue (bug#12272). Fixed bug#12272 - Tickets in status "removed" get reopened from Junk Queue.

--- Kernel/System/PostMaster.pm
+++ Kernel/System/PostMaster.pm
@@ -251,7 +251,7 @@ sub Run {
         );
 
         # create a new ticket
-        if ( $FollowUpPossible =~ /new ticket/i && $State{TypeName} =~ /^close/i ) {
+        if ( $FollowUpPossible =~ /new ticket/i && $State{TypeName} =~ /^(removed|close)/i ) {
 
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'info',
@@ -301,7 +301,7 @@ sub Run {
         }
 
         # reject follow up
-        elsif ( $FollowUpPossible =~ /reject/i && $State{TypeName} =~ /^close/i ) {
+        elsif ( $FollowUpPossible =~ /reject/i && $State{TypeName} =~ /^(removed|close)/i ) {
 
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'info',
