6578a8bcf82529461302291ab3fcb500363b005a
Martin Gruner <martin.gruner@otrs.com>
2016-10-06 10:32:25 +0200

    Improved sandboxing of displayed attachments.

--- Kernel/Modules/AgentTicketAttachment.pm
+++ Kernel/Modules/AgentTicketAttachment.pm
@@ -1,5 +1,4 @@
 # --
-# Kernel/Modules/AgentTicketAttachment.pm - to get the attachments
 # Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
@@ -140,7 +139,8 @@ sub Run {
             %Data,
             ContentType => 'text/html',
             Content     => $Content,
-            Type        => 'inline'
+            Type        => 'inline',
+            Sandbox     => 1,
         );
     }
 
@@ -155,7 +155,10 @@ sub Run {
 
         # just return for non-html attachment (e. g. images)
         if ( $Data{ContentType} !~ /text\/html/i ) {
-            return $Self->{LayoutObject}->Attachment(%Data);
+            return $Self->{LayoutObject}->Attachment(
+                %Data,
+                Sandbox => 1,
+            );
         }
 
         # set filename for inline viewing
@@ -211,11 +214,17 @@ sub Run {
         }
 
         # return html attachment
-        return $Self->{LayoutObject}->Attachment(%Data);
+        return $Self->{LayoutObject}->Attachment(
+            %Data,
+            Sandbox => 1,
+        );
     }
 
     # download it AttachmentDownloadType is configured
-    return $Self->{LayoutObject}->Attachment(%Data);
+    return $Self->{LayoutObject}->Attachment(
+        %Data,
+        Sandbox => 1,
+    );
 }
 
 1;
--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -1,5 +1,4 @@
 # --
-# Kernel/Output/HTML/Layout.pm - provides generic HTML output
 # Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
@@ -2588,6 +2587,8 @@ returns browser output to display/download a attachment
         Filename    => 'FileName.png',  # optional
         ContentType => 'image/png',
         Content     => $Content,
+        Sandbox     => 1,               # optional, default 0; use content security policy to prohibit external
+                                        #   scripts, flash etc.
     );
 
     or for AJAX html snippets
@@ -2675,6 +2676,15 @@ sub Attachment {
     }
     $Output .= "Content-Length: $Param{Size}\n";
     $Output .= "X-Frame-Options: SAMEORIGIN\n";
+
+    if ( $Param{Sandbox} && !$Self->{ConfigObject}->Get('DisableContentSecurityPolicy') ) {
+
+        # Disallow external and inline scripts, active content, frames, but keep allowing inline styles
+        #   as this is a common use case in emails.
+        # Also disallow referrer headers to prevent referrer leaks.
+        $Output
+            .= "Content-Security-Policy: default-src *; script-src 'none'; object-src 'none'; frame-src 'none'; style-src 'unsafe-inline'; referrer no-referrer;\n";
+    }
 
     if ( $Param{Charset} ) {
         $Output .= "Content-Type: $Param{ContentType}; charset=$Param{Charset};\n\n";
