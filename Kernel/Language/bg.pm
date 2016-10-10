88dcd72a2995dfa9f18a516575b116a319e4ce42
Manuel Hecht <manuel.hecht@otrs.com>
2016-10-10 15:24:39 +0200

    Updated copyright.

--- Kernel/Language/bg.pm
+++ Kernel/Language/bg.pm
@@ -1,6 +1,6 @@
 # --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # Copyright (C) 2004 Vladimir Gerdjikov <gerdjikov at gerdjikovs.net>
-# Copyright (C) 2007 Alex Kantchev <ak at otrs.org>
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
