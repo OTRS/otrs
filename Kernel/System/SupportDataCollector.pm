476adc24a7bdac26d3ab2a19842d5b1f3ebb27a8
Dominik Klein <dominik.klein@otrs.com>
2016-10-12 18:44:19 +0200

    Added the possibility to add a own host name for the support data collector and to disable support data collector plugins.

--- Kernel/System/SupportDataCollector.pm
+++ Kernel/System/SupportDataCollector.pm
@@ -100,7 +100,6 @@ collect system data
 sub Collect {
     my ( $Self, %Param ) = @_;
 
-    # check cache
     my $CacheKey = 'DataCollect';
 
     if ( $Param{UseCache} ) {
@@ -116,6 +115,10 @@ sub Collect {
     if ( !$ENV{GATEWAY_INTERFACE} ) {
         return $Self->CollectByWebRequest(%Param);
     }
+
+    # Get the disabled plugins from the config to generate a lookup hash, which can be used to skip these plugins.
+    my $PluginDisabled = $Kernel::OM->Get('Kernel::Config')->Get('SupportDataCollector::DisablePlugins') || [];
+    my %LookupPluginDisabled = map { $_ => 1 } @{$PluginDisabled};
 
     # Look for all plug-ins in the FS
     my @PluginFiles = $Kernel::OM->Get('Kernel::System::Main')->DirectoryRead(
@@ -164,6 +167,9 @@ sub Collect {
         push @Result, @{ $PluginResult{Result} // [] };
     }
 
+    # Remove the disabled plugins after the execution, because some plugins returns more information with a own identifier.
+    @Result = grep { !$LookupPluginDisabled{ $_->{Identifier} } } @Result;
+
     # sort the results from the plug-ins by the short identifier
     @Result = sort { $a->{ShortIdentifier} cmp $b->{ShortIdentifier} } @Result;
 
@@ -172,7 +178,6 @@ sub Collect {
         Result  => \@Result,
     );
 
-    # set cache
     $Kernel::OM->Get('Kernel::System::Cache')->Set(
         Type  => 'SupportDataCollector',
         Key   => $CacheKey,
@@ -212,6 +217,7 @@ sub CollectByWebRequest {
     }
 
     my $Host = $Param{Hostname};
+    $Host ||= $Kernel::OM->Get('Kernel::Config')->Get('SupportDataCollector::HTTPHostname');
 
     # Determine hostname
     if ( !$Host ) {
@@ -318,14 +324,6 @@ sub CollectByWebRequest {
         );
         return %Result;
     }
-
-    # set cache
-    $Kernel::OM->Get('Kernel::System::Cache')->Set(
-        Type  => 'SupportDataCollect',
-        Key   => 'DataCollect',
-        Value => $ResponseData,
-        TTL   => 60 * 10,
-    );
 
     return %{$ResponseData};
 }
