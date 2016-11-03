845a82143b3b692c5f0daf7da44b0f7a9cbb24d4
Manuel Hecht <manuel.hecht@otrs.com>
2016-11-03 21:23:19 +0100

    Tidied.

--- Kernel/Output/HTML/DashboardIFrame.pm
+++ Kernel/Output/HTML/DashboardIFrame.pm
@@ -1,7 +1,6 @@
 # --
 # Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
-# --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
