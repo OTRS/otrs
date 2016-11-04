dda4c135ee626139529ba6c6d49d6ec751f511f5
Martin Gruner <martin.gruner@otrs.com>
2016-11-04 14:02:45 +0100

    Fixed uninit value error.

--- Kernel/Modules/AgentTicketCompose.pm
+++ Kernel/Modules/AgentTicketCompose.pm
@@ -1837,7 +1837,7 @@ sub _Mask {
     }
 
     # set preselected values for To field
-    if ( $Param{To} ne '' && !$CustomerCounter ) {
+    if ( defined $Param{To} && $Param{To} ne '' && !$CustomerCounter ) {
         $Self->{LayoutObject}->Block(
             Name => 'PreFilledTo',
         );
