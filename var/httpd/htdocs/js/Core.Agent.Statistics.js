fe12280f80f6216d801a2cb461f42631b29f8b0d
Dominik Klein <dominik.klein@otrs.com>
2016-11-07 18:42:41 +0100

    Follow-up for bug#12401: Fixed: Same HTML IDs in the statistics edit screen (x-axis, y-axis and filter).

--- var/httpd/htdocs/js/Core.Agent.Statistics.js
+++ var/httpd/htdocs/js/Core.Agent.Statistics.js
@@ -69,7 +69,7 @@ Core.Agent.Statistics = (function (TargetNS) {
             $ContainerElementClone = $ContainerElement.find('.Element' + Core.App.EscapeSelector(ElementName)).clone();
 
         $ContainerElementClone.find("*[id]").each(function() {
-            $(this).attr("id", $(this).attr("id").replace(/_orig/, ''));
+            $(this).attr("id", $(this).attr("id").replace(/_orig/, '_selected'));
         });
         $ContainerElementClone.appendTo($FormFieldsElement);
     };
@@ -87,7 +87,8 @@ Core.Agent.Statistics = (function (TargetNS) {
                 ConfigurationLimit = $(this).data('configuration-limit'),
                 DialogTitle = $(this).data('dialog-title'),
                 $ContainerElement = $('#' + ConfigurationType + 'Container'),
-                $FormFieldsElement = $('#' + ConfigurationType + 'FormFields');
+                $FormFieldsElement = $('#' + ConfigurationType + 'FormFields'),
+                $CloneFormFieldsElement;
 
             function RebuildEditDialogAddSelection() {
                 $('#EditDialog .Add select').empty().append('<option>-</option>');
@@ -153,7 +154,12 @@ Core.Agent.Statistics = (function (TargetNS) {
             $('#EditDialogTemplate').children().clone().appendTo('#EditDialog');
 
             if ($FormFieldsElement.children().length) {
-                $FormFieldsElement.children().clone().appendTo('#EditDialog .Fields');
+                $CloneFormFieldsElement = $FormFieldsElement.children().clone()
+                $CloneFormFieldsElement.find("*[id]").each(function() {
+                    $(this).attr("id", $(this).attr("id").replace(/_selected/, ''));
+                });
+                $CloneFormFieldsElement.appendTo('#EditDialog .Fields');
+
                 Core.UI.InputFields.Activate($('#EditDialog .Fields'));
                 if (ConfigurationLimit && $('#EditDialog .Fields .Element').length >= ConfigurationLimit) {
                     $('#EditDialog .Add').hide();
