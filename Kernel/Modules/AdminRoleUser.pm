be4010f3365da552dcfd079c36ad31cc90e06c32
Manuel Hecht <manuel.hecht@otrs.com>
2017-01-01 22:12:54 +0100

    Updated copyright.

--- Kernel/Modules/AdminRoleUser.pm
+++ Kernel/Modules/AdminRoleUser.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
