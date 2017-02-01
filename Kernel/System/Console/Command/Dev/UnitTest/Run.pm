336046077ef891080cd5e07976fc2d422ff3da83
Martin Gruner <martin.gruner@otrs.com>
2017-02-01 15:22:54 +0100

    Improved UnitTest.pm to talk to the new CI infrastructure.

--- Kernel/System/Console/Command/Dev/UnitTest/Run.pm
+++ Kernel/System/Console/Command/Dev/UnitTest/Run.pm
@@ -23,29 +23,37 @@ sub Configure {
 
     $Self->Description('Executes unit tests.');
     $Self->AddOption(
-        Name        => 'test',
-        Description => "Run single test files, e.g. 'Ticket' or 'Ticket:Queue'.",
-        Required    => 0,
-        HasValue    => 1,
-        ValueRegex  => qr/.*/smx,
+        Name => 'test',
+        Description =>
+            "Run individual test files, e.g. 'Ticket' or 'Ticket/ArchiveFlags' (can be specified several times).",
+        Required   => 0,
+        HasValue   => 1,
+        Multiple   => 1,
+        ValueRegex => qr/.*/smx,
     );
     $Self->AddOption(
         Name        => 'directory',
-        Description => "Run all test files in specified directory.",
+        Description => "Run all test files in the specified directory.",
         Required    => 0,
         HasValue    => 1,
         ValueRegex  => qr/.*/smx,
     );
     $Self->AddOption(
-        Name        => 'output',
-        Description => "Select output format (ASCII|HTML|XML).",
+        Name        => 'verbose',
+        Description => "Show details for all tests, not just failing.",
         Required    => 0,
-        HasValue    => 1,
-        ValueRegex  => qr/^(ASCII|HTML|XML)$/smx,
+        HasValue    => 0,
     );
     $Self->AddOption(
         Name        => 'submit-url',
         Description => "Send unit test results to a server (url).",
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/.*/smx,
+    );
+    $Self->AddOption(
+        Name        => 'submit-auth',
+        Description => "Authentication string for unit test result server.",
         Required    => 0,
         HasValue    => 1,
         ValueRegex  => qr/.*/smx,
@@ -58,17 +66,27 @@ sub Configure {
         HasValue => 0,
     );
     $Self->AddOption(
-        Name        => 'product',
-        Description => "Specify a different product name.",
+        Name        => 'job-id',
+        Description => "Job ID for unit test submission to server.",
         Required    => 0,
         HasValue    => 1,
         ValueRegex  => qr/.*/smx,
     );
     $Self->AddOption(
-        Name        => 'verbose',
-        Description => "Show details for all tests, not just failing.",
+        Name        => 'scenario',
+        Description => "Scenario identifier for unit test submission to server.",
         Required    => 0,
-        HasValue    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/.*/smx,
+    );
+    $Self->AddOption(
+        Name => 'attachment-path',
+        Description =>
+            "Send an additional file to the server, for example to submit the complete command output that has been redirected to a file.",
+        Required   => 0,
+        HasValue   => 1,
+        ValueRegex => qr/.*/smx,
+        Multiple   => 1
     );
 }
 
@@ -86,18 +104,20 @@ sub Run {
 
     $Kernel::OM->ObjectParamAdd(
         'Kernel::System::UnitTest' => {
-            Output => $Self->GetOption('output') || '',
             ANSI => $Self->{ANSI},
         },
     );
 
     my $FunctionResult = $Kernel::OM->Get('Kernel::System::UnitTest')->Run(
-        Name                   => $Self->GetOption('test')                       || '',
-        Directory              => $Self->GetOption('directory')                  || '',
-        Product                => $Self->GetOption('product')                    || '',
-        SubmitURL              => $Self->GetOption('submit-url')                 || '',
+        Tests                  => $Self->GetOption('test'),
+        Directory              => $Self->GetOption('directory'),
+        JobID                  => $Self->GetOption('job-id'),
+        Scenario               => $Self->GetOption('scenario'),
+        SubmitURL              => $Self->GetOption('submit-url'),
+        SubmitAuth             => $Self->GetOption('submit-auth'),
         SubmitResultAsExitCode => $Self->GetOption('submit-result-as-exit-code') || '',
-        Verbose                => $Self->GetOption('verbose')                    || '',
+        Verbose                => $Self->GetOption('verbose'),
+        AttachmentPath         => $Self->GetOption('attachment-path'),
     );
 
     if ($FunctionResult) {
