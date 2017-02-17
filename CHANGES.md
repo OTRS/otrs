ef48826102918431a70948e53610731391c3f8b4
Dominik Klein <dominik.klein@otrs.com>
2017-02-17 13:27:37 +0100

    Follow-up fix for bug#12487: Statistic with a  '+' in the  CustomerId doesn't work.

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,5 @@
 #5.0.17 2017-??-??
+ - 2017-02-17 Follow-up fix for bug#12487: Statistic with a  '+' in the  CustomerId doesn't work.
  - 2017-02-17 Fixed bug#[12595](https://bugs.otrs.org/show_bug.cgi?id=12595) - Not able to search for tickets at customer portal with brackets within login name.
  - 2017-02-17 Fixed bug#[8853](https://bugs.otrs.org/show_bug.cgi?id=8853) - Hardcoded Priority and State in bin/otrs.FillDB.pl.
  - 2017-02-17 Fixed bug#[12555](https://bugs.otrs.org/show_bug.cgi?id=12555) - Deleting tickets via GenericAgent does not remove tickets completely (immediately?).
--- Kernel/System/Stats/Dynamic/TicketList.pm
+++ Kernel/System/Stats/Dynamic/TicketList.pm
@@ -823,6 +823,12 @@ sub GetStatTable {
         'Title'      => 1,
     );
 
+    # Map the CustomerID search parameter to CustomerIDRaw search parameter for the
+    #   exact search match, if the 'Stats::CustomerIDAsMultiSelect' is active.
+    if ( $Kernel::OM->Get('Kernel::Config')->Get('Stats::CustomerIDAsMultiSelect') ) {
+        $Param{Restrictions}->{CustomerIDRaw} = $Param{Restrictions}->{CustomerID};
+    }
+
     ATTRIBUTE:
     for my $Key ( sort keys %{ $Param{Restrictions} } ) {
 
@@ -986,12 +992,6 @@ sub GetStatTable {
         $UnixTimeEnd = $TimeObject->TimeStamp2SystemTime(
             String => $Param{Restrictions}->{HistoricTimeRangeTimeOlderDate}
         );
-    }
-
-    # Map the CustomerID search parameter to CustomerIDRaw search parameter for the
-    #   exact search match, if the 'Stats::CustomerIDAsMultiSelect' is active.
-    if ( $Kernel::OM->Get('Kernel::Config')->Get('Stats::CustomerIDAsMultiSelect') ) {
-        $Param{Restrictions}->{CustomerIDRaw} = $Param{Restrictions}->{CustomerID};
     }
 
     # get the involved tickets
