c688cc86411f0bd87104f1b17ffe82faf396b7eb
Martin Gruner <martin.gruner@otrs.com>
2017-02-22 13:30:29 +0100

    Renamed example ticket backend for improved code policy filtering.

--- Kernel/System/Ticket/CustomExample.pm
+++ Kernel/System/Ticket/CustomExample.pm
@@ -1,13 +1,12 @@
 # --
-# Kernel/System/Ticket/Custom.pm.example - custom ticket changes
-# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::System::Ticket::Custom;
+package Kernel::System::Ticket::CustomExample;
 
 use strict;
 use warnings;
@@ -16,18 +15,18 @@ our $ObjectManagerDisabled = 1;
 
 # disable redefine warnings in this scope
 {
-no warnings 'redefine';
+    no warnings 'redefine';
 
-# as example redefine the TicketXXX() of Kernel::System::Ticket
-sub Kernel::System::Ticket::TicketXXX {
-    my ( $Self, %Param ) = @_;
+    # as example redefine the TicketXXX() of Kernel::System::Ticket
+    sub Kernel::System::Ticket::TicketXXX {
+        my ( $Self, %Param ) = @_;
 
-    # do some new stuff
+        # do some new stuff
 
-    return 1;
-}
+        return 1;
+    }
 
-# reset all warnings
+    # reset all warnings
 }
 
 1;
