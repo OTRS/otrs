7a3d535eaec62df018a835ea7e656b4717120522
Dominik Klein <dominik.klein@otrs.com>
2017-03-10 14:07:34 +0100

    Fixed: DynamicField Date and DateTime only have the default values for 10 years in past and 1 year in future for the search (bug#12678).

--- Kernel/System/DynamicField/Driver/Date.pm
+++ Kernel/System/DynamicField/Driver/Date.pm
@@ -783,6 +783,15 @@ EOF
         return $Data;
     }
 
+    # to set the years range
+    my %YearsPeriodRange;
+    if ( defined $FieldConfig->{YearsPeriod} && $FieldConfig->{YearsPeriod} eq '1' ) {
+        %YearsPeriodRange = (
+            YearPeriodPast   => $FieldConfig->{YearsInPast}   || 0,
+            YearPeriodFuture => $FieldConfig->{YearsInFuture} || 0,
+        );
+    }
+
     # build HTML for start value set
     $HTMLString .= $Param{LayoutObject}->BuildDateSelection(
         %Param,
@@ -792,6 +801,8 @@ EOF
         DiffTime             => -( ( 60 * 60 * 24 ) * 30 ),
         Validate             => 1,
         %{ $Value->{ValueStart} },
+        %YearsPeriodRange,
+        OverrideTimeZone => 1,
     );
 
     # build HTML for "and" separator
@@ -806,6 +817,8 @@ EOF
         DiffTime             => +( ( 60 * 60 * 24 ) * 30 ),
         Validate             => 1,
         %{ $Value->{ValueStop} },
+        %YearsPeriodRange,
+        OverrideTimeZone => 1,
     );
 
     my $AdditionalText;
