60b6e4d9389021b705eb237fc147cae305b23852
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2017-03-23 16:20:53 -0600

    Follow-up fix for: TicketNumber generators uses TicketCheckNumber() in a wrong way (bug#12723).

--- Kernel/System/Ticket/Number/Date.pm
+++ Kernel/System/Ticket/Number/Date.pm
@@ -95,7 +95,7 @@ sub TicketCreateNumber {
     my $Tn = $Year . $Month . $Day . $SystemID . $Count;
 
     # Check ticket number. If exists generate new one!
-    if ( $Self->TicketNumberLookup( Tn => $Tn ) ) {
+    if ( $Self->TicketIDLookup( TicketNumber => $Tn ) ) {
 
         $Self->{LoopProtectionCounter}++;
 
