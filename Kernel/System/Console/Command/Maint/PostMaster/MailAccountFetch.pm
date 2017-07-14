a78c1f70012ec68fb20ed2e7a3e356ac59e5db63
Marco Bucholz <marco.buchholz@otrs.com>
2017-07-14 19:11:18 +0100

    Fixed: MailAccountFetch daemon task sometimes gets stuck (bug#12635). Refactored MailAccountFetch script to use child process. Kill child process after configured amount of seconds in mail fetch.

--- Kernel/System/Console/Command/Maint/PostMaster/MailAccountFetch.pm
+++ Kernel/System/Console/Command/Maint/PostMaster/MailAccountFetch.pm
@@ -6,12 +6,17 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
+##nofilter(TidyAll::Plugin::OTRS::Perl::NoExitInConsoleCommands)
+
 package Kernel::System::Console::Command::Maint::PostMaster::MailAccountFetch;
 
 use strict;
 use warnings;
 
 use parent qw(Kernel::System::Console::BaseCommand);
+
+use POSIX ":sys_wait_h";
+use Time::HiRes qw(sleep);
 
 our @ObjectDependencies = (
     'Kernel::System::Log',
@@ -42,6 +47,14 @@ sub Configure {
         Required    => 0,
         HasValue    => 0,
     );
+    $Self->AddOption(
+        Name => 'timeout',
+        Description =>
+            "Timeout in seconds to kill the child process, that does the mail fetching (Default: 600 seconds)",
+        Required   => 0,
+        HasValue   => 1,
+        ValueRegex => qr{^\d+$}smx,
+    );
 
     return;
 }
@@ -54,7 +67,7 @@ sub PreRun {
     my $PIDCreated = $PIDObject->PIDCreate(
         Name  => $Self->Name(),
         Force => $Self->GetOption('force-pid'),
-        TTL   => 60 * 60 * 24 * 3,
+        TTL   => 600,                             # 10 minutes
     );
     if ( !$PIDCreated ) {
         my $Error = "Unable to register the process in the database. Is another instance still running?\n";
@@ -78,72 +91,167 @@ sub PreRun {
 sub Run {
     my ( $Self, %Param ) = @_;
 
-    $Self->Print("<yellow>Fetching incoming mails from mail accounts...</yellow>\n\n");
-
     my $MailAccountObject = $Kernel::OM->Get('Kernel::System::MailAccount');
-    my $MailAccountID     = $Self->GetOption('mail-account-id');
-
     my %List = $MailAccountObject->MailAccountList( Valid => 1 );
-    my ( $ErrorCount, $FetchedCount );
 
-    KEY:
-    for my $Key ( sort keys %List ) {
-        next KEY if ( $MailAccountID && $Key != $MailAccountID );
-        my %Data = $MailAccountObject->MailAccountGet( ID => $Key );
-        $Self->Print("<yellow>$Data{Host} ($Data{Type})...</yellow>\n");
-        my $Status;
+    my $Debug         = $Self->GetOption('debug');
+    my $Timeout       = $Self->GetOption('timeout') || 600;
+    my $MailAccountID = $Self->GetOption('mail-account-id');
 
-        # It is needed for capture the standard error.
-        my $ErrorMessage;
+    if ( !%List ) {
+        if ($MailAccountID) {
+            $Self->PrintError("Could not find mail account $MailAccountID.");
+            return $Self->ExitCodeError();
+        }
 
-        eval {
+        $Self->Print("\n<yellow>No configured mail accounts found!</yellow>\n\n");
+        return $Self->ExitCodeOk();
+    }
 
-            # Localize the standard error, everything will be restored after the eval block.
-            local *STDERR;
+    # Setup alarm signal handler to kill the running child process if we reach the given timeout.
+    local $SIG{ALRM} = sub {
+        $Self->PrintError("Timeout of $Timeout seconds reached, killing child process!\n");
+        kill 9, $Self->{ChildPID};
+    };
 
-            # Redirect the standard error to a variable.
-            open STDERR, ">>", \$ErrorMessage;
+    # Set the timeout to kill the child.
+    alarm $Timeout;
 
-            $Status = $MailAccountObject->MailAccountFetch(
-                %Data,
-                Debug  => $Self->GetOption('debug'),
-                CMD    => 1,
-                UserID => 1,
+    # Destroy objects for the child processes.
+    $Kernel::OM->ObjectsDiscard(
+        Objects => [
+            'Kernel::System::DB',
+        ],
+        ForcePackageReload => 0,
+    );
+
+    $Self->Print("\n<yellow>Spawning child process to fetch incoming messages from mail accounts...</yellow>\n\n");
+
+    # Create a child process.
+    my $PID = fork;
+
+    # Could not create child.
+    if ( $PID < 0 ) {
+
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => "Unable to fork to a child process for mail account fetching!"
+        );
+        $Self->PrintError("Unable to fork to a child process for mail account fetching!");
+
+        return $Self->ExitCodeError();
+    }
+
+    # We're in the child process.
+    if ( !$PID ) {
+
+        my $ErrorCount;
+        my $FetchedCount;
+
+        KEY:
+        for my $Key ( sort keys %List ) {
+
+            next KEY if ( $MailAccountID && $Key != $MailAccountID );
+
+            my %Data = $MailAccountObject->MailAccountGet( ID => $Key );
+
+            $Self->Print("<yellow>$Data{Host} ($Data{Type})...</yellow>\n");
+
+            # It is needed for capture the standard error.
+            my $Status;
+            my $ErrorMessage;
+
+            eval {
+
+                # Localize the standard error, everything will be restored after the eval block.
+                local *STDERR;
+
+                # Redirect the standard error to a variable.
+                open STDERR, ">>", \$ErrorMessage;
+
+                $Status = $MailAccountObject->MailAccountFetch(
+                    %Data,
+                    Debug  => $Self->GetOption('debug'),
+                    CMD    => 1,
+                    UserID => 1,
+                );
+            };
+
+            # Hide password contained in error message and print message back to standard error.
+            # Please see bug#12829 for more information.
+            if ($ErrorMessage) {
+                $ErrorMessage =~ s/\Q$Data{Password}\E/********/g;
+                print STDERR $ErrorMessage;
+            }
+
+            if ($Status) {
+                $FetchedCount++;
+            }
+            else {
+                $ErrorCount++;
+            }
+        }
+
+        my $ExitCode = $Self->ExitCodeOk();
+
+        if ($ErrorCount) {
+
+            # Error messages printed by backend.
+            $ExitCode = $Self->ExitCodeError();
+        }
+
+        if ( !$FetchedCount && $MailAccountID ) {
+            $Self->PrintError("Could not find mail account $MailAccountID.");
+            $ExitCode = $Self->ExitCodeError();
+        }
+
+        # Close child process at the end.
+        exit $ExitCode;
+    }
+
+    # Remember the child process ID.
+    $Self->{ChildPID} = $PID;
+
+    # Check the status of the child process every 0.1 seconds.
+    #   Wait for the child process to be finished.
+    WAIT:
+    while (1) {
+
+        last WAIT if !$Self->{ChildPID};
+
+        sleep 0.1;
+
+        my $WaitResult = waitpid( $PID, WNOHANG );
+
+        if ( $WaitResult == -1 ) {
+
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Child process exited with errors: $?",
             );
-        };
 
-        # Hide password contained in error message and print message back to standard error.
-        # Please see bug#12829 for more information.
-        if ($ErrorMessage) {
-            $ErrorMessage =~ s/\Q$Data{Password}\E/********/g;
-            print STDERR $ErrorMessage;
+            $Self->{ChildPID} = undef;
         }
 
-        if ($Status) {
-            $FetchedCount++;
-        }
-        else {
-            $ErrorCount++;
-        }
+        $Self->{ChildPID} = undef if $WaitResult;
     }
 
-    if ($ErrorCount) {
+    alarm 0;
 
-        # Error messages printed by backend
-        return $Self->ExitCodeError();
-    }
-
-    if ( !$FetchedCount && $MailAccountID ) {
-        $Self->PrintError("Could not find mail account $MailAccountID.");
-        return $Self->ExitCodeError();
-    }
-
-    $Self->Print("<green>Done.</green>\n");
+    $Self->Print("<green>Done.</green>\n\n");
     return $Self->ExitCodeOk();
 }
 
 sub PostRun {
     my ($Self) = @_;
+
+    # Destroy objects for the child processes.
+    $Kernel::OM->ObjectsDiscard(
+        Objects => [
+            'Kernel::System::DB',
+        ],
+        ForcePackageReload => 0,
+    );
 
     my $Debug = $Self->GetOption('debug');
     my $Name  = $Self->Name();
