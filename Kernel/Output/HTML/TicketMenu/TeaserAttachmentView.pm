29c5517e4083884e96d5612fced0d14f28cc9ca3
Marc A. Bonsels <marc.bonsels@otrs.com>
2017-08-14 12:00:27 +0200

    Added a teaser menu item in misc for ticket attachment view on AgentTicketZoom.

---
+++ Kernel/Output/HTML/TicketMenu/TeaserAttachmentView.pm
@@ -0,0 +1,31 @@
+# --
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::TicketMenu::TeaserAttachmentView;
+
+use parent 'Kernel::Output::HTML::Base';
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::System::OTRSBusiness',
+);
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    if ($Kernel::OM->Get('Kernel::System::OTRSBusiness')->OTRSBusinessIsInstalled()) {
+        return;
+    }
+
+    # return item
+    return { %{ $Param{Config} }, %{ $Param{Ticket} }, %Param };
+}
+
+1;
