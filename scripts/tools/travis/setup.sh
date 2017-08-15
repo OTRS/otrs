ad21fd5edcde026a7bca320b87288a009d9a9bbc
Martin Gruner <martin.gruner@otrs.com>
2017-08-15 11:03:51 +0200

    Fixed mysql database charset on Travis.

--- scripts/tools/travis/setup.sh
+++ scripts/tools/travis/setup.sh
@@ -4,7 +4,7 @@
 if [ $DB = 'mysql' ]; then
     cp -i $TRAVIS_BUILD_DIR/scripts/tools/travis/Config.pm.travis.mysql $TRAVIS_BUILD_DIR/Kernel/Config.pm
 
-    mysql -uroot -e "CREATE DATABASE otrs";
+    mysql -uroot -e "CREATE DATABASE otrs CHARACTER SET utf8";
     mysql -uroot -e "GRANT ALL PRIVILEGES ON otrs.* TO 'otrs'@'localhost' IDENTIFIED BY 'otrs'";
     mysql -uroot -e "FLUSH PRIVILEGES";
     mysql -uroot otrs < $TRAVIS_BUILD_DIR/scripts/database/otrs-schema.mysql.sql
