aab864914d78d6032d0dec14ef43437282d16f4a
Martin Gruner <martin.gruner@otrs.com>
2017-08-16 11:59:28 +0200

    Improved usability of time zone migration module.

--- scripts/DBUpdateTo6/MigrateTimeZoneConfiguration.pm
+++ scripts/DBUpdateTo6/MigrateTimeZoneConfiguration.pm
@@ -21,6 +21,89 @@ our @ObjectDependencies = (
     'Kernel::System::SysConfig',
 );
 
+sub CheckPreviousRequirement {
+    my ( $Self, %Param ) = @_;
+
+    if ( $Param{CommandlineOptions}->{NonInteractive} || !is_interactive() ) {
+        return 1;
+    }
+
+    # Check if configuration was already made.
+    my $OTRSTimeZone        = $Kernel::OM->Get('Kernel::Config')->Get('OTRSTimeZone')        // 'UTC';
+    my $UserDefaultTimeZone = $Kernel::OM->Get('Kernel::Config')->Get('UserDefaultTimeZone') // 'UTC';
+    if ( $OTRSTimeZone ne 'UTC' || $UserDefaultTimeZone ne 'UTC' ) {
+        return 1;
+    }
+
+    #
+    # OTRSTimeZone
+    #
+
+    # Get system time zone
+    my $DateTimeObject = $Kernel::OM->Create(
+        'Kernel::System::DateTime',
+        ObjectParams => {
+            TimeZone => 'UTC',
+        },
+    );
+    my $SystemTimeZone = $DateTimeObject->SystemTimeZoneGet() || 'UTC';
+    $DateTimeObject->ToTimeZone( TimeZone => $SystemTimeZone );
+
+    # Get configured deprecated time zone offset
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $TimeOffset = int( $ConfigObject->Get('TimeZone') || 0 );
+
+    # Calculate complete time offset (server time zone + OTRS time offset)
+    my $SuggestedTimeZone = $TimeOffset ? '' : $SystemTimeZone;
+    $TimeOffset += $DateTimeObject->Format( Format => '%{offset}' ) / 60 / 60;
+
+    # Show suggestions for time zone
+    my %TimeZones = map { $_ => 1 } @{ $DateTimeObject->TimeZoneList() };
+    my $TimeZoneByOffset = $DateTimeObject->TimeZoneByOffsetList();
+    if ( exists $TimeZoneByOffset->{$TimeOffset} ) {
+        print
+            "\n\n        The currently configured time offset is $TimeOffset hours, these are the suggestions for a corresponding OTRS time zone: \n\n";
+
+        print "        " . join( "\n        ", sort @{ $TimeZoneByOffset->{$TimeOffset} } ) . "\n";
+    }
+
+    if ( $SuggestedTimeZone && $TimeZones{$SuggestedTimeZone} ) {
+        print "\n\n        It seems that $SuggestedTimeZone should be the correct time zone to set for your OTRS. \n";
+    }
+
+    $Self->{TargetTimeZones}->{OTRSTimeZone} = $Self->_AskForTimeZone(
+        ConfigKey => 'OTRSTimeZone',
+        TimeZones => \%TimeZones,
+    );
+
+    #
+    # UserDefaultTimeZone
+    #
+    $Self->{TargetTimeZones}->{UserDefaultTimeZone} = $Self->_AskForTimeZone(
+        ConfigKey => 'UserDefaultTimeZone',
+        TimeZones => \%TimeZones,
+    );
+
+    #
+    # TimeZone::Calendar[1..9] (but only those that have already a time offset set)
+    #
+    CALENDAR:
+    for my $Calendar ( 1 .. 9 ) {
+        my $ConfigKey        = "TimeZone::Calendar$Calendar";
+        my $CalendarTimeZone = $ConfigObject->Get($ConfigKey);
+        next CALENDAR if !defined $CalendarTimeZone;
+
+        $Self->{TargetTimeZones}->{$ConfigKey} = $Self->_AskForTimeZone(
+            ConfigKey => $ConfigKey,
+            TimeZones => \%TimeZones,
+        );
+    }
+
+    print "\n";
+
+    return 1;
+}
+
 =head1 NAME
 
 scripts::DBUpdateTo6::MigrateTimeZoneConfiguration - Migrate timezone configuration.
@@ -66,106 +149,27 @@ sub Run {
         return 1;
     }
 
-    #
-    # OTRSTimeZone
-    #
-
-    # Get system time zone
-    my $DateTimeObject = $Kernel::OM->Create(
-        'Kernel::System::DateTime',
-        ObjectParams => {
-            TimeZone => 'UTC',
-        },
-    );
-    my $SystemTimeZone = $DateTimeObject->SystemTimeZoneGet() || 'UTC';
-    $DateTimeObject->ToTimeZone( TimeZone => $SystemTimeZone );
-
-    # Get configured deprecated time zone offset
-    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
-    my $TimeOffset = int( $ConfigObject->Get('TimeZone') || 0 );
-
-    # Calculate complete time offset (server time zone + OTRS time offset)
-    my $SuggestedTimeZone = $TimeOffset ? '' : $SystemTimeZone;
-    $TimeOffset += $DateTimeObject->Format( Format => '%{offset}' ) / 60 / 60;
-
-    # Show suggestions for time zone
-    my %TimeZones = map { $_ => 1 } @{ $DateTimeObject->TimeZoneList() };
-    my $TimeZoneByOffset = $DateTimeObject->TimeZoneByOffsetList();
-    if ( exists $TimeZoneByOffset->{$TimeOffset} ) {
-        print
-            "\n\n        The currently configured time offset is $TimeOffset hours, these are the suggestions for a corresponding OTRS time zone: \n\n";
-
-        print "        " . join( "\n        ", sort @{ $TimeZoneByOffset->{$TimeOffset} } ) . "\n";
-    }
-
-    if ( $SuggestedTimeZone && $TimeZones{$SuggestedTimeZone} ) {
-        print "\n\n        It seems that $SuggestedTimeZone should be the correct time zone to set for your OTRS. \n";
-    }
-
-    my $Success = $Self->_ConfigureTimeZone(
-        ConfigKey => 'OTRSTimeZone',
-        TimeZones => \%TimeZones,
-    );
-
-    return if !$Success;
-
-    #
-    # UserDefaultTimeZone
-    #
-    $Success = $Self->_ConfigureTimeZone(
-        ConfigKey => 'UserDefaultTimeZone',
-        TimeZones => \%TimeZones,
-    );
-
-    return if !$Success;
-
-    #
-    # TimeZone::Calendar[1..9] (but only those that have already a time offset set)
-    #
-    CALENDAR:
-    for my $Calendar ( 1 .. 9 ) {
-        my $ConfigKey        = "TimeZone::Calendar$Calendar";
-        my $CalendarTimeZone = $ConfigObject->Get($ConfigKey);
-        next CALENDAR if !defined $CalendarTimeZone;
-
-        $Success = $Self->_ConfigureTimeZone(
-            ConfigKey => $ConfigKey,
-            TimeZones => \%TimeZones,
-        );
-
-        return if !$Success;
-    }
-
-    print "\n";
-
-    return 1;
-}
-
-sub _ConfigureTimeZone {
-    my ( $Self, %Param ) = @_;
-
-    my $TimeZone = $Self->_AskForTimeZone(
-        ConfigKey => $Param{ConfigKey},
-        TimeZones => $Param{TimeZones},
-    );
-
     my $SysConfigObject = $Kernel::OM->Get('Kernel::System::SysConfig');
 
-    my $ExclusiveLockGUID = $SysConfigObject->SettingLock(
-        Name   => $Param{ConfigKey},
-        Force  => 1,
-        UserID => 1,
-    );
+    for my $ConfigKey ( sort keys %{ $Self->{TargetTimeZones} // {} } ) {
+        my $ExclusiveLockGUID = $SysConfigObject->SettingLock(
+            Name   => $ConfigKey,
+            Force  => 1,
+            UserID => 1,
+        );
 
-    my %Result = $SysConfigObject->SettingUpdate(
-        Name              => $Param{ConfigKey},
-        IsValid           => 1,
-        EffectiveValue    => $TimeZone,
-        ExclusiveLockGUID => $ExclusiveLockGUID,
-        UserID            => 1,
-    );
+        my %Result = $SysConfigObject->SettingUpdate(
+            Name              => $ConfigKey,
+            IsValid           => 1,
+            EffectiveValue    => $Self->{TargetTimeZones}->{$ConfigKey},
+            ExclusiveLockGUID => $ExclusiveLockGUID,
+            UserID            => 1,
+        );
 
-    return $Result{Success};
+        return if !$Result{Success};
+    }
+
+    return 1;
 }
 
 sub _AskForTimeZone {
