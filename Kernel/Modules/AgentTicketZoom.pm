c3577d01bc26c1fb0f81ae284b84f7a39a9c333d
Zoran Ilibasic <zilibasic@s7designcreative.com>
2017-09-05 15:13:46 +0200

    Fixed: There are no dialogs available at this point in the process is not shown (bug#13053).

--- Kernel/Modules/AgentTicketZoom.pm
+++ Kernel/Modules/AgentTicketZoom.pm
@@ -1235,7 +1235,7 @@ sub MaskAgentZoom {
         # get next activity dialogs
         my $NextActivityDialogs;
         if ( $Ticket{$ActivityEntityIDField} ) {
-            $NextActivityDialogs = ${ActivityData}->{ActivityDialog} // {};
+            $NextActivityDialogs = ${ActivityData}->{ActivityDialog} || {};
         }
         my $ActivityName = $ActivityData->{Name};
 
