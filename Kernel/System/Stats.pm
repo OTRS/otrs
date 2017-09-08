d4bfce29f8260732cd3f1a84b33d9bd2a0f566ac
Dominik Klein <dominik.klein@otrs.com>
2017-09-08 12:47:06 +0200

    Improved the regex for the statistic object module.

--- Kernel/System/Stats.pm
+++ Kernel/System/Stats.pm
@@ -1184,11 +1184,11 @@ sub ObjectModuleCheck {
     return if $Param{StatType} ne 'static' && $Param{StatType} ne 'dynamic';
 
     my $CheckFileLocation = 'Kernel::System::Stats::' . ucfirst $Param{StatType};
-    return if $Param{ObjectModule} !~ m{ \A $CheckFileLocation }xms;
+    my $CheckPackageName  = '[A-Z_a-z][0-9A-Z_a-z]*';
+    return if $Param{ObjectModule} !~ m{ \A $CheckFileLocation (?: ::$CheckPackageName)+ \z }xms;
 
     my $ObjectName = [ split( m{::}, $Param{ObjectModule} ) ]->[-1];
     return if !$ObjectName;
-    return if $ObjectName !~ m{[A-Z_a-z][0-9A-Z_a-z]*}xms;
 
     my @RequiredObjectFunctions;
 
