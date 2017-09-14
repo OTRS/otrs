3a99480506a4b3ab60045985c824ba3aaa7598c8
Martin Gruner <martin.gruner@otrs.com>
2017-09-14 14:16:58 +0200

    Added new support data plugins for additional UI stats.

---
+++ Kernel/System/SupportDataCollector/Plugin/OTRS/UI/AgentThemeUsage.pm
@@ -0,0 +1,67 @@
+# --
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::SupportDataCollector::Plugin::OTRS::UI::AgentThemeUsage;
+
+use strict;
+use warnings;
+
+use parent qw(Kernel::System::SupportDataCollector::PluginBase);
+
+use Kernel::Language qw(Translatable);
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::DB',
+    'Kernel::System::User',
+);
+
+sub GetDisplayPath {
+    return Translatable('OTRS') . '/' . Translatable('UI - Agent Theme Usage');
+}
+
+sub Run {
+    my $Self = shift;
+
+    # First get count of all agents. We avoid checking for Valid for performance reasons, as this
+    #   would require fetching of all agent data to check for the preferences.
+    my $DBObject               = $Kernel::OM->Get('Kernel::System::DB');
+    my $AgentsWithDefaultTheme = 1;
+    $DBObject->Prepare( SQL => 'SELECT count(*) FROM users' );
+    while ( my @Row = $DBObject->FetchrowArray() ) {
+        $AgentsWithDefaultTheme = $Row[0];
+    }
+
+    my $DefaultThem = $Kernel::OM->Get('Kernel::Config')->Get('DefaultTheme');
+
+    my %ThemePreferences = $Kernel::OM->Get('Kernel::System::User')->SearchPreferences(
+        Key => 'UserTheme',
+    );
+
+    my %ThemeUsage;
+
+    # Check how many agents have a theme preference configured, assume default theme for the rest.
+    for my $UserID ( sort keys %ThemePreferences ) {
+        $ThemeUsage{ $ThemePreferences{$UserID} }++;
+        $AgentsWithDefaultTheme--;
+    }
+    $ThemeUsage{$DefaultThem} += $AgentsWithDefaultTheme;
+
+    for my $Theme ( sort keys %ThemeUsage ) {
+
+        $Self->AddResultInformation(
+            Identifier => $Theme,
+            Label      => $Theme,
+            Value      => $ThemeUsage{$Theme},
+        );
+    }
+
+    return $Self->GetResults();
+}
+
+1;
