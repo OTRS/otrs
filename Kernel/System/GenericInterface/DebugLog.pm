ab312990a0a9806e63c442297a25ffd0f0a9305c
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2017-10-10 20:48:33 -0500

    Tidied.

--- Kernel/System/GenericInterface/DebugLog.pm
+++ Kernel/System/GenericInterface/DebugLog.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -843,9 +843,9 @@ sub _LogAddChain {
     return 1;
 }
 
-=end Internal:
-
 1;
+
+=end Internal:
 
 =back
 
