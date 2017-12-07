498165ab19bc7ae69842e7eb2d14213a51a5c8a9
Martin Gruner <martin.gruner@otrs.com>
2017-12-07 13:03:57 +0100

    Update Travis configuration.

--- scripts/tools/travis/runtests.sh
+++ scripts/tools/travis/runtests.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ $DB = 'postgresql' ]; then
     # PostgreSQL data is on a small ramdisk in Travis; make sure that it stays small by always performing VACUUM FULL.
-    perl bin/otrs.Console.pl Dev::UnitTest::Run --post-test-script 'psql -U postgres -c "VACUUM FULL"'
+    perl bin/otrs.Console.pl Dev::UnitTest::Run --post-test-script 'psql -U postgres otrs -c "VACUUM FULL"'
 else
     perl bin/otrs.Console.pl Dev::UnitTest::Run
 fi
