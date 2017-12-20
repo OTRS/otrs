22c7d928dac44c0c861287d83c0e35584a692f05
Sanjin Vik <svik@s7designcreative.com>
2017-12-20 16:19:15 +0100

    Fixed: ACL property database does not work in AgentTicketActionCommon for Type field (bug#13483).

--- Kernel/Modules/AgentTicketActionCommon.pm
+++ Kernel/Modules/AgentTicketActionCommon.pm
@@ -2767,8 +2767,9 @@ sub _GetTypes {
     if ( $Param{QueueID} || $Param{TicketID} ) {
         %Type = $Kernel::OM->Get('Kernel::System::Ticket')->TicketTypeList(
             %Param,
-            Action => $Self->{Action},
-            UserID => $Self->{UserID},
+            TicketID => $Self->{TicketID},
+            Action   => $Self->{Action},
+            UserID   => $Self->{UserID},
         );
     }
     return \%Type;
