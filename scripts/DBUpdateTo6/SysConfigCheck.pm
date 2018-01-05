d918dc7b069b07d3288bbf8d3dea787e096b8113
Jaroslav Balaz <jaroslav.balaz@otrs.com>
2018-01-05 13:00:05 +0100

    Fixed: DBUpdate-script isn't checking ZZZAuto.pm for wrong Sysconfig settings (bug#13338).

--- scripts/DBUpdateTo6/SysConfigCheck.pm
+++ scripts/DBUpdateTo6/SysConfigCheck.pm
@@ -19,6 +19,7 @@ our @ObjectDependencies = (
     'Kernel::System::Priority',
     'Kernel::System::Queue',
     'Kernel::System::State',
+    'Kernel::System::SysConfig',
     'Kernel::System::Type',
 );
 
@@ -86,6 +87,17 @@ sub Run {
             if ( !grep { $_ eq $OldEffectiveValue } @AllowedEffectiveValues ) {
                 push @InconsistentSettings, $SettingName;
             }
+        }
+    }
+
+    # Check if there are invalid settings.
+    my @InvalidSettings = $Kernel::OM->Get('Kernel::System::SysConfig')->ConfigurationInvalidList();
+
+    for my $SettingName (@InvalidSettings) {
+
+        # Add it to the list only if setting is not there already.
+        if ( !grep { $_ eq $SettingName } @InconsistentSettings ) {
+            push @InconsistentSettings, $SettingName;
         }
     }
 
