09053fa26de47a4c5b0eee8bb000b489e64d0bcb
Sanjin Vik <svik@s7designcreative.com>
2018-01-30 10:01:15 +0100

    Fixed: ACL with invalid-temporarily status is not included in export file when exporting ACL (bug#13592).

--- Kernel/Modules/AdminACL.pm
+++ Kernel/Modules/AdminACL.pm
@@ -468,9 +468,14 @@ sub Run {
         }
         else {
 
+            # Get all IDs from valid table including invalid-temporarily status.
+            # See bug#13592 (https://bugs.otrs.org/show_bug.cgi?id=13592).
+            my %ValidList = $Kernel::OM->Get('Kernel::System::Valid')->ValidList();
+            my @ValidListIDs = grep { $ValidList{$_} } sort keys %ValidList;
+
             $ACLData = $ACLObject->ACLListGet(
                 UserID   => 1,
-                ValidIDs => [ '1', '2' ],
+                ValidIDs => \@ValidListIDs,
             );
         }
 
