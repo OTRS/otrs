b726f234b6a7c22f27b73f628a305b1fffcdcafc
Milan Rakic <mrakic@s7designcreative.com>
2018-05-10 11:20:09 +0200

    Follow-up fix: Search attributes not consistent (bug#7988).

--- Kernel/Modules/AgentTicketSearch.pm
+++ Kernel/Modules/AgentTicketSearch.pm
@@ -570,7 +570,6 @@ sub Run {
                             . $DynamicFieldConfig->{Name}
                             . $Preference->{Type}
                     }
-                    && ( $DynamicFieldConfig->{FieldType} eq 'Date' || $DynamicFieldConfig->{FieldType} eq 'DateTime' )
                     )
                 {
                     next PREFERENCE;
