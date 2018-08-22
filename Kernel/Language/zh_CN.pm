ca27120a296d64d7bea88b10ac94c7848bad89da
Manuel Hecht <manuel.hecht@otrs.com>
2018-08-22 23:32:45 +0200

    Updated license information.

--- Kernel/Language/zh_CN.pm
+++ Kernel/Language/zh_CN.pm
@@ -8,8 +8,8 @@
 # Copyright (C) 2013 Michael Shi <micshi at 163.com>
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
 # --
 
 package Kernel::Language::zh_CN;
