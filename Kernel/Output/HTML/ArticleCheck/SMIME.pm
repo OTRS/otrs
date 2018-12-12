4c44366112cca87a4d4eddb9ed16f52e5eb2c1e1
Milan Rakic <mrakic@s7designcreative.com>
2018-12-12 13:51:44 +0100

    Fixed: SMIME signed messages cause a lot of database errors on customer ticket zoom, thanks to Renee Backer (bug#14105).

--- Kernel/Output/HTML/ArticleCheck/SMIME.pm
+++ Kernel/Output/HTML/ArticleCheck/SMIME.pm
@@ -19,6 +19,7 @@ our @ObjectDependencies = (
     'Kernel::System::Crypt::SMIME',
     'Kernel::System::Log',
     'Kernel::System::Ticket::Article',
+    'Kernel::Output::HTML::Layout',
 );
 
 sub new {
@@ -48,8 +49,11 @@ sub Check {
     my %SignCheck;
     my @Return;
 
-    # get config object
     my $ConfigObject = $Param{ConfigObject} || $Kernel::OM->Get('Kernel::Config');
+    my $LayoutObject = $Param{LayoutObject} || $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+
+    my $UserType = $LayoutObject->{UserType} // '';
+    my $ChangeUserID = $UserType eq 'Customer' ? $ConfigObject->Get('CustomerPanelUserID') : $Self->{UserID};
 
     # check if smime is enabled
     return if !$ConfigObject->Get('SMIME');
@@ -263,7 +267,7 @@ sub Check {
                 # made if sender and signer addresses does not match
 
                 # get original sender from email
-                my @OrigEmail        = map {"$_\n"} split( /\n/, $Message );
+                my @OrigEmail = map {"$_\n"} split( /\n/, $Message );
                 my $ParserObjectOrig = Kernel::System::EmailParser->new(
                     Email => \@OrigEmail,
                 );
@@ -297,13 +301,13 @@ sub Check {
                     ArticleID => $Self->{ArticleID},
                     Key       => 'Body',
                     Value     => $Body,
-                    UserID    => $Self->{UserID},
+                    UserID    => $ChangeUserID,
                 );
 
                 # delete crypted attachments
                 $ArticleBackendObject->ArticleDeleteAttachment(
                     ArticleID => $Self->{ArticleID},
-                    UserID    => $Self->{UserID},
+                    UserID    => $ChangeUserID,
                 );
 
                 # write attachments to the storage
@@ -311,7 +315,7 @@ sub Check {
                     $ArticleBackendObject->ArticleWriteAttachment(
                         %{$Attachment},
                         ArticleID => $Self->{ArticleID},
-                        UserID    => $Self->{UserID},
+                        UserID    => $ChangeUserID,
                     );
                 }
 
@@ -375,7 +379,7 @@ sub Check {
                 # made if sender and signer addresses does not match
 
                 # get original sender from email
-                my @OrigEmail        = map {"$_\n"} split( /\n/, $Message );
+                my @OrigEmail = map {"$_\n"} split( /\n/, $Message );
                 my $ParserObjectOrig = Kernel::System::EmailParser->new(
                     Email => \@OrigEmail,
                 );
@@ -409,13 +413,13 @@ sub Check {
                     ArticleID => $Self->{ArticleID},
                     Key       => 'Body',
                     Value     => $Body,
-                    UserID    => $Self->{UserID},
+                    UserID    => $ChangeUserID,
                 );
 
                 # delete crypted attachments
                 $ArticleBackendObject->ArticleDeleteAttachment(
                     ArticleID => $Self->{ArticleID},
-                    UserID    => $Self->{UserID},
+                    UserID    => $ChangeUserID,
                 );
 
                 # write attachments to the storage
@@ -423,7 +427,7 @@ sub Check {
                     $ArticleBackendObject->ArticleWriteAttachment(
                         %{$Attachment},
                         ArticleID => $Self->{ArticleID},
-                        UserID    => $Self->{UserID},
+                        UserID    => $ChangeUserID,
                     );
                 }
             }
