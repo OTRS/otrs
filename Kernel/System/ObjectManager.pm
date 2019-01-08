5eaf26c79e5fd850a8cd166ce07c02c09e0e208b
Martin Gruner <martin.gruner@otrs.com>
2019-01-08 14:59:01 +0100

    Fixed UT problem on Fedora 29 (raising internal Perl bug).

--- Kernel/System/ObjectManager.pm
+++ Kernel/System/ObjectManager.pm
@@ -15,7 +15,7 @@ package Kernel::System::ObjectManager;
 use strict;
 use warnings;
 
-use Carp qw(carp confess);
+use Carp ();
 use Scalar::Util qw(weaken);
 
 # use the "standard" modules directly, so that persistent environments
@@ -629,11 +629,9 @@ sub _DieWithError {
             Priority => 'Error',
             Message  => $Param{Error},
         );
-        confess $Param{Error};    # this will die()
     }
 
-    carp $Param{Error};
-    confess $Param{Error};
+    Carp::croak $Param{Error}; # This will die().
 }
 
 sub DESTROY {
