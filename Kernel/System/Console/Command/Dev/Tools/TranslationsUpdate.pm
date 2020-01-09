0d31eefc98f92e2ed469dff82ea680fdefc85e37
Martin Gruner <martin.gruner@otrs.com>
2020-01-09 09:32:19 +0100

    Switch from Transifex to translate.otrs.com.

--- Kernel/System/Console/Command/Dev/Tools/TranslationsUpdate.pm
+++ Kernel/System/Console/Command/Dev/Tools/TranslationsUpdate.pm
@@ -50,7 +50,7 @@ sub Configure {
     $Self->AddOption(
         Name => 'generate-po',
         Description =>
-            "Generate PO (translation content) files. This is only needed if a module is not yet available in Transifex to force initial creation of the gettext files.",
+            "Generate PO (translation content) files. This is only needed if a module is not yet available in Weblate to force initial creation of the gettext files.",
         Required => 0,
         HasValue => 0,
     );
@@ -151,20 +151,20 @@ sub HandleLanguage {
 
     my $DefaultTheme = $Kernel::OM->Get('Kernel::Config')->Get('DefaultTheme');
 
-    # We need to map internal codes to the official ones used by Transifex
-    my %TransifexLanguagesMap = (
+    # We need to map internal codes to the official ones used by Weblate
+    my %WeblateLanguagesMap = (
         sr_Cyrl => 'sr',
         sr_Latn => 'sr',
     );
 
-    my $TransifexLanguage = $TransifexLanguagesMap{$Language} // $Language;
-    my $Home              = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+    my $WeblateLanguage = $WeblateLanguagesMap{$Language} // $Language;
+    my $Home            = $Kernel::OM->Get('Kernel::Config')->Get('Home');
 
     if ( !$Module ) {
         $LanguageFile  = "$Home/Kernel/Language/$Language.pm";
         $TargetFile    = "$Home/Kernel/Language/$Language.pm";
         $TargetPOTFile = "$Home/i18n/otrs/otrs.pot";
-        $TargetPOFile  = "$Home/i18n/otrs/otrs.$TransifexLanguage.po";
+        $TargetPOFile  = "$Home/i18n/otrs/otrs.$WeblateLanguage.po";
     }
     else {
         $IsSubTranslation = 1;
@@ -180,7 +180,7 @@ sub HandleLanguage {
         $TargetFile = "$ModuleDirectory/Kernel/Language/${Language}_$Module.pm";
 
         $TargetPOTFile = "$ModuleDirectory/i18n/$Module/$Module.pot";
-        $TargetPOFile  = "$ModuleDirectory/i18n/$Module/$Module.$TransifexLanguage.po";
+        $TargetPOFile  = "$ModuleDirectory/i18n/$Module/$Module.$WeblateLanguage.po";
     }
 
     my $WritePOT = $Param{WritePO} || -e $TargetPOTFile;
@@ -765,7 +765,7 @@ sub WritePOFile {
     }
 
     # Theoretically we could now also check for removed strings, but since the translations
-    #   are handled by transifex, this will not be needed as Transifex will handle that for us.
+    #   are handled by Weblate, this will not be needed as Weblate will handle that for us.
 
     Locale::PO->save_file_fromarray( $Param{TargetPOFile}, $POEntries )
         || die "Could not save file $Param{TargetPOFile}: $!";
