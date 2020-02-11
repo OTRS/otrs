1288ff74612fc9c69034ea70363f9c052f0bd54c
Stefan Bedorf <stefan.bedorf@otrs.com>
2020-02-11 13:40:56 +0100

    Fixed missing use statements for Time::HiRes.

--- Kernel/System/Console/Command/Maint/Ticket/InvalidUserCleanup.pm
+++ Kernel/System/Console/Command/Maint/Ticket/InvalidUserCleanup.pm
@@ -13,6 +13,8 @@ use warnings;
 
 use parent qw(Kernel::System::Console::BaseCommand);
 
+use Time::HiRes qw(usleep);
+
 our @ObjectDependencies = (
     'Kernel::Config',
     'Kernel::System::DB',
